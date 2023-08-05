67d19ae4c39987c8722e784f49411355bf824149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:26 +0000

    Add package django-mfa/1.0/django-mfa-1.0.tar.gz

---
+++ packages/django-mfa/django-mfa-1.0.tar.gz/django-mfa-1.0/README.rst
@@ -0,0 +1,100 @@
+django-mfa
+==========
+
+.. image:: https://readthedocs.org/projects/django-mfa/badge/?version=latest
+   :target: http://django-mfa.readthedocs.io/en/latest/
+   :alt: Documentation Status
+   
+.. image:: https://travis-ci.org/MicroPyramid/django-mfa.svg?branch=master
+   :target: https://travis-ci.org/MicroPyramid/django-mfa
+
+.. image:: https://img.shields.io/pypi/dm/django-mfa.svg
+    :target: https://pypi.python.org/pypi/django-mfa
+    :alt: Downloads
+
+.. image:: https://img.shields.io/pypi/v/django-mfa.svg
+    :target: https://pypi.python.org/pypi/django-mfa
+    :alt: Latest Release
+    
+.. image:: https://coveralls.io/repos/github/MicroPyramid/django-mfa/badge.svg?branch=master
+   :target: https://coveralls.io/github/MicroPyramid/django-mfa?branch=master
+
+.. image:: https://landscape.io/github/MicroPyramid/django-mfa/master/landscape.svg?style=flat
+   :target: https://landscape.io/github/MicroPyramid/django-mfa/master
+   :alt: Code Health
+
+.. image:: https://img.shields.io/github/license/micropyramid/django-mfa.svg
+    :target: https://pypi.python.org/pypi/django-mfa/
+
+
+Django-mfa is a simple Django app to for providing MFA (Multi-Factor Authentication).
+
+**Documentation** is `available online
+<http://django-mfa.readthedocs.org/>`_, or in the docs
+directory of the project.
+
+Quick start
+-----------
+
+Installation
+~~~~~~~~~~~~
+
+The Git repository can be cloned with this command::
+
+    git clone https://github.com/MicroPyramid/django-mfa
+
+The ``django_mfa`` package, included in the distribution, should be
+placed on the ``PYTHONPATH``.
+
+Otherwise you can just ``easy_install -Z django-mfa``
+or ``pip install django-mfa``.
+
+Settings
+~~~~~~~~
+
+1. Add app name in settings.py::
+
+    INSTALLED_APPS = [
+       '..................',
+       'django_mfa',
+       '..................'
+    ]
+
+2. Add 'django_mfa.middleware.MfaMiddleware' to your project middlewares::
+
+    MIDDLEWARE = [
+       '....................................',
+       'django_mfa.middleware.MfaMiddleware',
+       '....................................',
+    ]
+
+
+Urls
+~~~~
+
+Add the following to your root urls.py file.
+
+.. code:: django
+
+    urlpatterns = [
+        ...
+
+        url(r'^settings/', include('django_mfa.urls', namespace="mfa")),
+    ]
+
+
+Done. With these settings you have now, you will get the MFA features.
+
+You can try it by hosting on your own or deploy to Heroku with a button click.
+
+.. image:: https://www.herokucdn.com/deploy/button.svg
+   :target: https://heroku.com/deploy?template=https://github.com/MicroPyramid/django-mfa.git
+
+Visit our Django web development page `Here`_
+
+We welcome your feedback and support, raise `github ticket`_ if you want to report a bug. Need new features? `Contact us here`_
+
+.. _contact us here: https://micropyramid.com/contact-us/
+.. _Here: https://micropyramid.com/django-development-services/
+.. _github ticket: https://github.com/MicroPyramid/django-mfa/issues
+
---
+++ packages/django-mfa/django-mfa-1.0.tar.gz/django-mfa-1.0/django_mfa/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render
+from . import totp
+import base64
+import codecs
+import random
+import re
+from django.contrib.auth.decorators import login_required
+from django_mfa.models import *
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.conf import settings
+
+
+@login_required
+def security_settings(request):
+    twofactor_enabled = is_mfa_enabled(request.user)
+    return render(request, 'security.html', {"twofactor_enabled": twofactor_enabled})
+
+
+@login_required
+def configure_mfa(request):
+    qr_code = None
+    base_32_secret = None
+    if request.method == "POST":
+        base_32_secret = base64.b32encode(
+            codecs.decode(codecs.encode(
+            '{0:020x}'.format(random.getrandbits(80))
+            ), 'hex_codec')
+        )
+        totp_obj = totp.TOTP(base_32_secret.decode("utf-8"))
+        qr_code = re.sub(r'=+$', '', totp_obj.provisioning_uri(request.user.email))
+
+    return render(request, 'configure.html', {"qr_code": qr_code, "secret_key": base_32_secret})
+
+
+@login_required
+def enable_mfa(request):
+    qr_code = None
+    base_32_secret = None
+    is_verified = False
+    if request.method == "POST":
+        base_32_secret = request.POST['secret_key']
+        totp_obj = totp.TOTP(request.POST['secret_key'])
+        is_verified = totp_obj.verify(request.POST["verification_code"])
+        if is_verified:
+            request.session['verfied_otp'] = True
+            UserOTP.objects.get_or_create(otp_type=request.POST["otp_type"],
+                                          user=request.user,
+                                          secret_key=request.POST['secret_key'])
+        else:
+            totp_obj = totp.TOTP(base_32_secret)
+            qr_code = totp_obj.provisioning_uri(request.user.email)
+
+    return render(request, 'configure.html', {"is_verified": is_verified, "qr_code": qr_code, "secret_key": base_32_secret})
+
+
+@login_required
+def disable_mfa(request):
+    user = request.user
+    if not is_mfa_enabled(user):
+        return HttpResponseRedirect(reverse("mfa:configure_mfa"))
+    if request.method == "POST":
+        user_mfa = user.userotp
+        user_mfa.delete()
+        return HttpResponseRedirect(reverse("mfa:configure_mfa"))
+    return render(request, 'disable_mfa.html')
+
+
+@login_required
+def verify_otp(request):
+    ctx = {}
+    if request.method == "POST":
+        otp_obj = UserOTP.objects.filter(user=request.user).first()
+        totp_obj = totp.TOTP(otp_obj.secret_key)
+        is_verified = totp_obj.verify(request.POST["verification_code"])
+        if is_verified:
+            request.session['verfied_otp'] = True
+            return HttpResponseRedirect(request.POST.get("next", settings.LOGIN_REDIRECT_URL))
+        ctx['error_message'] = "Your code is expired or invalid."
+    ctx['next'] = request.GET.get('next') or settings.LOGIN_REDIRECT_URL
+    return render(request, 'login_verify.html', ctx)
