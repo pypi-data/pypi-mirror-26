03110cf76dc47a0e7795c725cc320c74bddf6d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:07 +0000

    Add package wagtailstreamforms/1.3.0/wagtailstreamforms-1.3.0.tar.gz

---
+++ packages/wagtailstreamforms/wagtailstreamforms-1.3.0.tar.gz/wagtailstreamforms-1.3.0/wagtailstreamforms/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from captcha.fields import ReCaptchaField
+from wagtail.wagtailforms.forms import FormBuilder as OrigFormBuilder
+from wagtailstreamforms.utils import recaptcha_enabled
+
+
+class CopyForm(forms.Form):
+    name = forms.CharField(label=_('New name'))
+
+
+class FormBuilder(OrigFormBuilder):
+
+    def __init__(self, fields, **kwargs):
+        self.add_recaptcha = kwargs.pop('add_recaptcha')
+        super(FormBuilder, self).__init__(fields)
+
+    def create_regex_field(self, field, options):
+        if field.regex_validator:
+            # there is a selected validator so use it
+            options.update({
+                'regex': field.regex_validator.regex,
+                'error_messages': {'invalid': field.regex_validator.error_message}
+            })
+        else:
+            # otherwise allow anything
+            options.update({'regex': '(.*?)'})
+        return forms.RegexField(**options)
+
+    OrigFormBuilder.FIELD_TYPES.update(
+        {'regexfield': create_regex_field}
+    )
+
+    @property
+    def formfields(self):
+        fields = super(FormBuilder, self).formfields
+
+        # add form id to identify the form type
+        fields['form_id'] = forms.CharField(widget=forms.HiddenInput)
+        fields['form_reference'] = forms.CharField(widget=forms.HiddenInput)
+
+        # if enabled add recaptcha field
+        if self.add_recaptcha and recaptcha_enabled():
+            fields['recaptcha'] = ReCaptchaField()
+
+        return fields
