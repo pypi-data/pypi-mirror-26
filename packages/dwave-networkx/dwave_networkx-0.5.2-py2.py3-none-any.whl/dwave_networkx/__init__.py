906d18d46fc4680669a0f48d68fde6895bd70bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:11 +0000

    Add package dwave-networkx/0.5.2/dwave_networkx-0.5.2-py2.py3-none-any.whl

---
+++ packages/dwave-networkx/dwave_networkx-0.5.2-py2.py3-none-any.whl/dwave_networkx/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+
+import sys
+_PY2 = sys.version_info[0] == 2
+
+import dwave_networkx.architectures
+from dwave_networkx.architectures import *
+
+try:
+    import dwave_networkx.x_architectures
+    from dwave_networkx.x_architectures import *
+except ImportError:
+    pass
+
+import dwave_networkx.algorithms
+from dwave_networkx.algorithms import *
+
+import dwave_networkx.utils
+from dwave_networkx.exceptions import *
+
+
+import dwave_networkx.default_sampler
+from dwave_networkx.default_sampler import *
+
+import dwave_networkx.drawing
+from dwave_networkx.drawing import *
+
+__version__ = '0.5.2'
+__author__ = 'D-Wave Systems Inc.'
+__authoremail__ = 'acondello@dwavesys.com'
+__description__ = 'A NetworkX extension providing graphs and algorithms relevent to working with the D-Wave System'
