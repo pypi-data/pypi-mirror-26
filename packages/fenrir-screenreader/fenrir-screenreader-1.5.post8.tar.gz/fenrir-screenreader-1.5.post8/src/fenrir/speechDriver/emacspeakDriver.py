72b8d131260955bd5b40b3987468122efcaaba26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:57 +0000

    Add package fenrir-screenreader/1.5.post8/fenrir-screenreader-1.5.post8.tar.gz

---
+++ packages/fenrir-screenreader/fenrir-screenreader-1.5.post8.tar.gz/fenrir-screenreader-1.5.post8/src/fenrir/speechDriver/emacspeakDriver.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+# generic driver
+
+from core import debug
+from subprocess import Popen, PIPE
+import pexpect
+import sys
+
+class driver():
+    def __init__(self):
+        pass
+    def initialize(self, environment):
+        self._isInitialized = False    
+        try:
+            self.server = pexpect.spawnu('tclsh /home/chrys/Projekte/emacspeak/servers/espeak')   
+        except Exception as e:
+            self.env['runtime']['debug'].writeDebugOut('speechDriver:initialize:' + str(e),debug.debugLevel.ERROR)                
+        self._isInitialized = True
+        self.env = environment
+        
+    def shutdown(self):
+        if self.server:
+            try:
+                self.server.terminate()
+            except Exception as e:
+                self.env['runtime']['debug'].writeDebugOut('speechDriver:shutdown:self.server.terminate():' + str(e),debug.debugLevel.ERROR)    
+
+    def speak(self,text, queueable=True):
+        if not self._isInitialized:
+            return
+        if not queueable: 
+            self.cancel()
+        try:
+            self.server.sendline('tts_say ' + '\"' + text +'\"')   
+            #print(text.replace('"', '\\\"'))      
+            #self.server.sendline('tts_say ' + '\"' + text.replace('"', '\\\"') +'\"') 
+        except Exception as e:
+            self.env['runtime']['debug'].writeDebugOut('speechDriver:speak:self.server.sendline():' + str(e),debug.debugLevel.ERROR)            
+    def cancel(self):
+        if not self._isInitialized:
+            return
+        try:
+            self.server.sendline('s')  
+        except Exception as e:
+            self.env['runtime']['debug'].writeDebugOut('speechDriver:cancel:self.server.sendline():' + str(e),debug.debugLevel.ERROR)   
+    def setCallback(self, callback):
+        print('SpeechDummyDriver: setCallback')    
+
+    def clear_buffer(self):
+        if not self._isInitialized:
+            return
+
+    def setVoice(self, voice):
+        if not self._isInitialized:
+            return
+        #self.server.sendline('s')
+
+    def setPitch(self, pitch):
+        pass
+
+    def setRate(self, rate):
+        if not self._isInitialized:
+            return
+        try:
+            self.server.sendline('tts_set_speech_rate' + str(int(rate * 500)))
+        except Exception as e:
+            self.env['runtime']['debug'].writeDebugOut('speechDriver:setRate:self.server.sendline():' + str(e),debug.debugLevel.ERROR)  
+    def setModule(self, module):
+        pass
+
+    def setLanguage(self, language):
+        if not self._isInitialized:
+            return
+
+    def setVolume(self, volume):
+        pass
---
+++ packages/fenrir-screenreader/fenrir-screenreader-1.5.post8.tar.gz/fenrir-screenreader-1.5.post8/src/fenrir/speechDriver/espeakDriver.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Fenrir TTY screen reader
+# By Chrys, Storm Dragon, and contributers.
+# Espeak driver
+
+from core import debug
+
+class driver():
+    def __init__(self):
+        self._es = None
+        self._isInitialized = False
+
+    def initialize(self, environment):
+        self.env = environment          
+        try:
+            from espeak import espeak 
+            self._es = espeak
+            self._isInitialized = True
+        except Exception as e:
+            self.env['runtime']['debug'].writeDebugOut(str(e),debug.debugLevel.ERROR)         
+            self._initialized = False
+                    
+    def shutdown(self):
+        pass
+
+    def speak(self,text, interrupt=True):
+        if not self._isInitialized:
+            return
+        if not interrupt:
+            self.cancel()
+        self._es.synth(text)
+
+    def cancel(self):
+        if not self._isInitialized:
+            return False
+        self._es.cancel()
+        return True
+
+    def setCallback(self, callback):
+        pass
+
+    def clear_buffer(self):
+        if not self._isInitialized:
+            return
+
+    def setVoice(self, voice):
+        if not self._isInitialized:
+            return
+        if voice =='':
+            return            
+        return self._es.set_voice(voice)
+
+    def setPitch(self, pitch):
+        if not self._isInitialized:
+            return
+        return self._es.set_parameter(self._es.Parameter().Pitch, int(pitch * 99)) 
+        
+    def setRate(self, rate):
+        if not self._isInitialized:
+            return
+        return self._es.set_parameter(self._es.Parameter().Rate, int(rate * 500 + 100))
+
+    def setModule(self, module):
+        if not self._isInitialized:
+            return
+
+    def setLanguage(self, language):
+        if not self._isInitialized:
+            return
+        if language =='':
+            return
+        return self._es.set_voice(language)
+
+    def setVolume(self, volume):
+        if not self._isInitialized:
+            return    
+        return self._es.set_parameter(self._es.Parameter().Volume, int(volume * 200))
