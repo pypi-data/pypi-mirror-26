87f7d223cb346cb9fbc8c0e6c6cc93b265b2cd66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:19 +0000

    Add package zijinlib/0.12/zijinlib-0.12.zip

---
+++ packages/zijinlib/zijinlib-0.12.zip/zijinlib-0.12/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+      name="zijinlib",
+      version="0.12",
+      description="A python package to process files and other things.",
+      long_description=open('zijinlib/README.rst', 'rb').read().decode('utf-8'),
+      author="yuanyuanzijin",
+      author_email='yuanyuanzijin@gmail.com',
+      url="https://github.com/yuanyuanzijin",
+      packages=["zijinlib"],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
