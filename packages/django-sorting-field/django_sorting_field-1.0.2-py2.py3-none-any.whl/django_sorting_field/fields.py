60a4403f52a0e4b453307a82428183feb2e023cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:05 +0000

    Add package django-sorting-field/1.0.2/django_sorting_field-1.0.2-py2.py3-none-any.whl

---
+++ packages/django-sorting-field/django_sorting_field-1.0.2-py2.py3-none-any.whl/django_sorting_field/fields.py
@@ -0,0 +1,36 @@
+import json
+
+from django import forms
+from django.utils.text import force_text
+
+from .utils import clean_order_json, sort_by_order
+from .widgets import SortingWidget
+
+
+class SortedItem(object):
+
+    def __init__(self, identifier, label):
+        self.id = identifier
+        self.label = label
+
+
+class SortingFormField(forms.CharField):
+
+    def __init__(self, *args, **kwargs):
+        kwargs.update({
+            "widget": SortingWidget(),
+            "required": False,
+        })
+        self.items = []
+        super(SortingFormField, self).__init__(*args, **kwargs)
+
+    def populate(self, items):
+        self.items = [SortedItem(item.pk, force_text(item)) for item in items]
+
+    def prepare_value(self, value):
+        value = clean_order_json(value)
+        return sort_by_order(self.items, value)
+
+    def to_python(self, value):
+        value = clean_order_json(value)
+        return json.dumps(value)
