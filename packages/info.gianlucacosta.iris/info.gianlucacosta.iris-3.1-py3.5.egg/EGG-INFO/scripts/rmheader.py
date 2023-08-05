73887666eafbff28a9b6e809a949e2c0d47f229d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package info.gianlucacosta.iris/3.1/info.gianlucacosta.iris-3.1-py3.5.egg

---
+++ packages/info.gianlucacosta.iris/info.gianlucacosta.iris-3.1-py3.5.egg/EGG-INFO/scripts/rmheader.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+Utility script employing HeaderRemover
+
+:copyright: Copyright (C) 2013-2017 Gianluca Costa.
+:license: LGPLv3, see LICENSE for details.
+"""
+
+
+import os
+import sys
+import re
+
+from info.gianlucacosta.iris.io.filetree import HeaderRemover, DefaultOnProcessingFunctions
+
+
+class Program:
+    def _printUsage(self):
+        print("Arguments: <root dir> <file path regex> <trailing regex>")
+        sys.exit(1)
+
+
+    def run(self, args):
+        if len(args) < 3:
+            self._printUsage()
+
+        rootDir = args[0]
+
+        if not os.path.isdir(rootDir):
+            self._printUsage()
+
+        filePathPattern = re.compile(args[1])
+        trailingPattern = re.compile(args[2])
+
+        headerRemover = HeaderRemover(filePathPattern, trailingPattern)
+        headerRemover.onProcessing = DefaultOnProcessingFunctions.printProcessedFile
+        headerRemover.applyTo(rootDir)
+
+
+if __name__ == "__main__":
+    Program().run(sys.argv[1:])
---
+++ packages/info.gianlucacosta.iris/info.gianlucacosta.iris-3.1-py3.5.egg/EGG-INFO/scripts/rmlicense.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+Utility script employing HeaderRemover to remove license headers created by Maven projects extending "jar-plus"
+
+:copyright: Copyright (C) 2013-2017 Gianluca Costa.
+:license: LGPLv3, see LICENSE for details.
+"""
+
+import sys
+
+from rmheader import Program
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Arguments: <root dir>")
+        sys.exit(1)
+
+
+    # For Java
+    Program().run(
+        [
+            sys.argv[1],
+            r".*\.(java|cs|js|c|cpp)$",
+            r"(?s)^.*==========================%##\s+\*/[\r\n]+"
+        ]
+    )
+
+    # For HTML/XML
+    Program().run(
+        [
+            sys.argv[1],
+            r".*\.(htm|html|xml)$",
+            r"(?s)^.*==========================%##\s+\-->[\r\n]+"
+        ]
+    )
+
