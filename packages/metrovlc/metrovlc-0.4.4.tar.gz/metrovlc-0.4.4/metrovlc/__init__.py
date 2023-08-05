c68020f3c29e6c774a165467b1d147b79b876b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:20 +0000

    Add package metrovlc/0.4.4/metrovlc-0.4.4.tar.gz

---
+++ packages/metrovlc/metrovlc-0.4.4.tar.gz/metrovlc-0.4.4/metrovlc/__init__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+__author__ = "Pedro Nicolás"
+__copyright__ = "Copyright 2017"
+__license__ = "MIT"
+__version__ = "0.4.4"
+__email__ = "png1981@gmail.com"
+
+from .metrovlc import metrosaldo
+from .metrovlc import metrohorarios
+
+__all__ = ['metrosaldo', 'metrohorarios']
