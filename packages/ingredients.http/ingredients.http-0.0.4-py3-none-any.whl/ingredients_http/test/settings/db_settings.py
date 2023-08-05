eb80aea20ae9c75fd1b3b72221c4741285faaaae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:23 +0000

    Add package ingredients.http/0.0.4/ingredients.http-0.0.4-py3-none-any.whl

---
+++ packages/ingredients.http/ingredients.http-0.0.4-py3-none-any.whl/ingredients_http/test/settings/db_settings.py
@@ -0,0 +1,11 @@
+####################
+# DATABASE         #
+####################
+
+# Only PostgreSQL is supported
+DATABASE_HOST = '127.0.0.1'
+DATABASE_PORT = '5432'
+DATABASE_USERNAME = 'postgres'
+DATABASE_PASSWORD = ''
+DATABASE_DB = 'test_db'
+DATABASE_POOL_SIZE = 20
