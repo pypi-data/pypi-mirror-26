14c2456c54433b61abb2d7c4bb7e9c58147afc1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:51 +0000

    Add package esub/0.0.6/esub-0.0.6.tar.gz

---
+++ packages/esub/esub-0.0.6.tar.gz/esub-0.0.6/setup.py
@@ -0,0 +1,28 @@
+"""esub python client library"""
+
+
+from setuptools import setup
+import setuphelpers
+
+
+setup(
+    name="esub",
+    version=setuphelpers.git_version(),
+    description="esub python client library",
+    author="Adam Talsma",
+    author_email="se-adam@talsma@ccpgames.com",
+    url="https://github.com/ccpgames/esub-client/",
+    download_url="https://github.com/ccpgames/esub-client/",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    py_modules=["esub"],
+    entry_points={"console_scripts": ["esub = esub:cli"]},
+    long_description=setuphelpers.long_description(),
+    cmdclass=setuphelpers.test_command(cover="esub", pdb=True),
+    tests_require=["pytest", "pytest-cov", "mock"],
+    install_requires=["requests", "websockets", "docopt"],
+)
