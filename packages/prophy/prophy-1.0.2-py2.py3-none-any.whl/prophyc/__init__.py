59974b7fb883e4400fc10dfd4dcd39a5165a30d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:18 +0000

    Add package prophy/1.0.2/prophy-1.0.2-py2.py3-none-any.whl

---
+++ packages/prophy/prophy-1.0.2-py2.py3-none-any.whl/prophyc/__init__.py
@@ -0,0 +1,107 @@
+#! /usr/bin/env python
+
+import sys
+import os
+
+from . import options
+from . import model
+from .file_processor import FileProcessor
+
+__version__ = '1.0.2'
+
+class Emit(object):
+    @staticmethod
+    def warn(msg, location = 'prophyc'):
+        sys.stderr.write(location + ': warning: ' + msg + '\n')
+
+    @staticmethod
+    def error(msg, location = 'prophyc'):
+        sys.exit(location + ': error: ' + msg)
+
+def main():
+    opts = options.parse_options(Emit.error)
+
+    if opts.quiet:
+        Emit.warn = None
+
+    if opts.version:
+        print("prophyc {}".format(__version__))
+        sys.exit(0)
+
+    if not opts.input_files:
+        Emit.error("missing input file")
+
+    parser = get_parser(opts)
+    serializers = get_serializers(opts)
+    patcher = get_patcher(opts)
+
+    if not serializers:
+        Emit.error("missing output directives")
+
+    def content_parser(*parse_args):
+        return parse_content(parser, patcher, *parse_args)
+
+    file_parser = FileProcessor(content_parser, opts.include_dirs)
+
+    for input_file in opts.input_files:
+        try:
+            nodes = file_parser(input_file)
+        except model.ParseError as e:
+            sys.exit('\n'.join(('%s: error: %s' % err for err in e.errors)))
+
+        for serializer in serializers:
+            basename = get_basename(input_file)
+            try:
+                serializer.serialize(nodes, basename)
+            except model.GenerateError as e:
+                Emit.error(str(e))
+
+def get_parser(opts):
+    if opts.isar:
+        from prophyc.parsers.isar import IsarParser
+        return IsarParser(warn = Emit.warn)
+    elif opts.sack:
+        from prophyc.parsers.sack import SackParser
+        status = SackParser.check()
+        if not status:
+            Emit.error(status.error)
+        return SackParser(opts.include_dirs, warn = Emit.warn)
+    else:
+        from prophyc.parsers.prophy import ProphyParser
+        return ProphyParser()
+
+def get_serializers(opts):
+    serializers = []
+    if opts.python_out:
+        from prophyc.generators.python import PythonGenerator
+        serializers.append(PythonGenerator(opts.python_out))
+    if opts.cpp_out:
+        from prophyc.generators.cpp import CppGenerator
+        serializers.append(CppGenerator(opts.cpp_out))
+    if opts.cpp_full_out:
+        from prophyc.generators.cpp_full import CppFullGenerator
+        serializers.append(CppFullGenerator(opts.cpp_full_out))
+    return serializers
+
+def get_patcher(opts):
+    if opts.patch:
+        from prophyc import patch
+        patches = patch.parse(opts.patch)
+        return lambda nodes: patch.patch(nodes, patches)
+
+def parse_content(parser, patcher, *parse_args):
+    nodes = parser.parse(*parse_args)
+    if patcher:
+        patcher(nodes)
+    model.topological_sort(nodes)
+    model.cross_reference(nodes, warn = Emit.warn)
+    model.evaluate_kinds(nodes)
+    model.evaluate_sizes(nodes, warn = Emit.warn)
+    return nodes
+
+def get_basename(path):
+    return os.path.splitext(os.path.basename(path))[0]
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/prophy/prophy-1.0.2-py2.py3-none-any.whl/prophyc/parsers/sack.py
@@ -0,0 +1,191 @@
+import os
+import re
+import ctypes.util
+from .clang.cindex import Config, Index, CursorKind, TypeKind, TranslationUnitLoadError, LibclangError
+
+from prophyc import model
+
+unambiguous_builtins = {
+    TypeKind.UCHAR: 'u8',
+    TypeKind.SCHAR: 'i8',
+    TypeKind.CHAR_S: 'i8',
+    TypeKind.POINTER: 'u32',
+    TypeKind.FLOAT: 'r32',
+    TypeKind.DOUBLE: 'r64',
+    TypeKind.BOOL: 'i32'
+}
+
+def alphanumeric_name(cursor):
+    name = cursor.type.spelling.decode()
+    if name.startswith('struct '):
+        name = name.replace('struct ', '', 1)
+    elif name.startswith('enum '):
+        name = name.replace('enum ', '', 1)
+    elif name.startswith('union '):
+        name = name.replace('union ', '', 1)
+    return re.sub('[^0-9a-zA-Z_]+', '__', name)
+
+def get_enum_member(cursor):
+    name = cursor.spelling.decode()
+    value = cursor.enum_value
+    if value < 0:
+        value = "0x%X" % (0x100000000 + value)
+    else:
+        value = str(value)
+    return model.EnumMember(name, value)
+
+class Builder(object):
+    def __init__(self):
+        self.known = set()
+        self.nodes = []
+
+    def _add_node(self, node):
+        self.known.add(node.name)
+        self.nodes.append(node)
+
+    def _get_field_array_len(self, tp):
+        if tp.kind is TypeKind.CONSTANTARRAY:
+            return tp.element_count
+        return None
+
+    def _build_field_type_name(self, tp):
+        if tp.kind is TypeKind.TYPEDEF:
+            return self._build_field_type_name(tp.get_declaration().underlying_typedef_type)
+        elif tp.kind in (TypeKind.UNEXPOSED, TypeKind.ELABORATED, TypeKind.RECORD):
+            decl = tp.get_declaration()
+            if decl.kind in (CursorKind.STRUCT_DECL, CursorKind.CLASS_DECL):
+                name = alphanumeric_name(decl)
+                if name not in self.known:
+                    self.add_struct(decl)
+                return name
+            elif decl.kind is CursorKind.UNION_DECL:
+                name = alphanumeric_name(decl)
+                if name not in self.known:
+                    self.add_union(decl)
+                return name
+            elif decl.kind is CursorKind.ENUM_DECL:
+                return self._build_field_type_name(decl.type)
+            elif decl.kind is CursorKind.TYPEDEF_DECL:
+                return self._build_field_type_name(decl.underlying_typedef_type)
+            else:
+                raise Exception("Unknown declaration")
+        elif tp.kind in (TypeKind.CONSTANTARRAY, TypeKind.INCOMPLETEARRAY):
+            return self._build_field_type_name(tp.element_type)
+        elif tp.kind is TypeKind.ENUM:
+            decl = tp.get_declaration()
+            name = alphanumeric_name(decl)
+            if name not in self.known:
+                self.add_enum(decl)
+            return name
+
+        if tp.kind in (TypeKind.USHORT, TypeKind.UINT, TypeKind.ULONG, TypeKind.ULONGLONG):
+            return 'u%d' % (tp.get_size() * 8)
+        elif tp.kind in (TypeKind.SHORT, TypeKind.INT, TypeKind.LONG, TypeKind.LONGLONG):
+            return 'i%d' % (tp.get_size() * 8)
+
+        return unambiguous_builtins[tp.kind]
+
+    def _build_struct_member(self, cursor):
+        name = cursor.spelling.decode()
+        type_name = self._build_field_type_name(cursor.type)
+        array_len = self._get_field_array_len(cursor.type)
+        return model.StructMember(name, type_name, size=array_len)
+
+    def _build_union_member(self, cursor, disc):
+        name = cursor.spelling.decode()
+        type_name = self._build_field_type_name(cursor.type)
+        return model.UnionMember(name, type_name, str(disc))
+
+    def add_enum(self, cursor):
+        members = list(map(get_enum_member, cursor.get_children()))
+        node = model.Enum(alphanumeric_name(cursor), members)
+        self._add_node(node)
+
+    def add_struct(self, cursor):
+        members = [self._build_struct_member(x)
+                   for x in cursor.get_children()
+                   if x.kind is CursorKind.FIELD_DECL and not x.is_bitfield()]
+        node = model.Struct(alphanumeric_name(cursor), members)
+        self._add_node(node)
+
+    def add_union(self, cursor):
+        members = [self._build_union_member(x, i)
+                   for i, x in enumerate(cursor.get_children())
+                   if x.kind is CursorKind.FIELD_DECL]
+        node = model.Union(alphanumeric_name(cursor), members)
+        self._add_node(node)
+
+def build_model(tu):
+    builder = Builder()
+    for cursor in tu.cursor.get_children():
+        if cursor.kind is CursorKind.UNEXPOSED_DECL:
+            for in_cursor in cursor.get_children():
+                if in_cursor.kind is CursorKind.STRUCT_DECL and in_cursor.spelling and in_cursor.is_definition():
+                    builder.add_struct(in_cursor)
+        if cursor.kind is CursorKind.STRUCT_DECL and cursor.spelling and cursor.is_definition():
+            builder.add_struct(cursor)
+    return builder.nodes
+
+def _get_location(location):
+    return '%s:%s:%s' % (location.file.name.decode(), location.line, location.column)
+
+def _setup_libclang():
+    if os.environ.get('PROPHY_NOCLANG'):
+        Config.set_library_file('prophy_noclang')
+        return
+
+    versions = [None, '3.5', '3.4', '3.3', '3.2', '3.6', '3.7', '3.8', '3.9']
+    for v in versions:
+        name = v and 'clang-' + v or 'clang'
+        libname = ctypes.util.find_library(name)
+        if libname:
+            Config.set_library_file(libname)
+            break
+
+def _check_libclang():
+    testconf = Config()
+    try:
+        testconf.get_cindex_library()
+        return True
+    except LibclangError:
+        return False
+
+class SackParserStatus(object):
+    def __init__(self, error = None):
+        self.error = error
+
+    def __bool__(self):
+        return not bool(self.error)
+
+    __nonzero__ = __bool__
+
+class SackParser(object):
+    @staticmethod
+    def check():
+        import platform
+        if platform.python_implementation() == 'PyPy':
+            return SackParserStatus("sack input doesn't work under PyPy due to ctypes incompatibilities")
+        if not _check_libclang():
+            return SackParserStatus("sack input requires libclang and it's not installed")
+        return SackParserStatus()
+
+    def __init__(self, include_dirs=[], warn=None):
+        self.include_dirs = include_dirs
+        self.warn = warn
+
+    def parse(self, content, path, process_file):
+        args_ = ["-I" + x for x in self.include_dirs]
+        index = Index.create()
+        path = path.encode()
+        content = content.encode()
+        try:
+            tu = index.parse(path, args_, unsaved_files=((path, content),))
+        except TranslationUnitLoadError:
+            raise model.ParseError([(path.decode(), 'error parsing translation unit')])
+        if self.warn:
+            for diag in tu.diagnostics:
+                self.warn(diag.spelling.decode(), location=_get_location(diag.location))
+        return build_model(tu)
+
+
+_setup_libclang()
