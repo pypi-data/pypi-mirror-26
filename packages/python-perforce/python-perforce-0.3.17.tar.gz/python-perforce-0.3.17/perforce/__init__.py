f460c56e86f250e0d81a5cae88fcb21e4a0b1430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package python-perforce/0.3.17/python-perforce-0.3.17.tar.gz

---
+++ packages/python-perforce/python-perforce-0.3.17.tar.gz/python-perforce-0.3.17/perforce/__init__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+Pythonic Perforce API
+~~~~~~~~~~~~~~~~~~~~~
+
+
+:copyright: (c) 2015 by Brett Dixon
+:license: MIT, see LICENSE for more details
+"""
+
+__title__ = 'perforce'
+__version__ = '0.3.17'
+__author__ = 'Brett Dixon'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 Brett Dixon'
+
+import logging.config
+
+from .models import Connection, Revision, Changelist, ConnectionStatus, ErrorLevel, Client, Stream
+from .api import connect, edit, sync, info, changelist, open
+
+
+CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True
+        }
+    }
+}
+
+logging.config.dictConfig(CONFIG)
+
