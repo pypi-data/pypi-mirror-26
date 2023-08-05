7c41ad06bf88177c0e79ff0556e7ee84122433ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:09 +0000

    Add package PyXiaomiGateway/0.5.2/PyXiaomiGateway-0.5.2.tar.gz

---
+++ packages/PyXiaomiGateway/PyXiaomiGateway-0.5.2.tar.gz/PyXiaomiGateway-0.5.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'PyXiaomiGateway',
+    packages = ['PyXiaomiGateway'],
+    install_requires=['pyCrypto==2.6.1'],
+    version = '0.5.2',
+    description = 'a library to communicate with the Xiaomi Gateway',
+    author='Daniel Hoyer Iversen',
+
+    url='https://github.com/Danielhiversen/PyXiaomiGateway/',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
