713a18d5945472262ba519dd60d7f7338ed6c5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:49 +0000

    Add package declarative/1.0.0.dev4/declarative-1.0.0.dev4-py2.py3-none-any.whl

---
+++ packages/declarative/declarative-1.0.0.dev4-py2.py3-none-any.whl/declarative/bunch/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from .bunch import (
+    Bunch,
+    FrozenBunch,
+    HookBunch,
+)
+
+from .deep_bunch import (
+    DeepBunch,
+    DeepBunchSingleAssign,
+)
+
+from .shadow_bunch import ShadowBunch
+
+from .tag_bunch import TagBunch
+
+
+
+
+
+
