6684f65372e79e4db3e7034a0157b4c058c0088b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:50 +0000

    Add package ttnmqtt/0.8.7/ttnmqtt-0.8.7.tar.gz

---
+++ packages/ttnmqtt/ttnmqtt-0.8.7.tar.gz/ttnmqtt-0.8.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+import os
+import pypandoc
+try:
+    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = long_description.replace("\r","") # YOU  NEED THIS LINE
+except OSError:
+    print("Pandoc not found. Long_description conversion failure.")
+    import io
+    # pandoc is not installed, fallback to using raw contents
+    with io.open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
+
+setup(name='ttnmqtt',
+      version='0.8.7',
+      description='small package to make mqtt connection to ttn',
+      long_description = long_description,
+      author='Emmanuelle Lejeail',
+      author_email='manu.lejeail@gmail.com',
+      license='MIT',
+      packages=['ttnmqtt'],
+      install_requires=[
+          'paho-mqtt',
+          'pydispatch',
+      ],
+      zip_safe=False)
