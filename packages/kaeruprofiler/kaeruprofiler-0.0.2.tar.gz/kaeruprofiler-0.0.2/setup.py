ded7ecfd0388bb75e9690c87d386632cef9f785b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:41 +0000

    Add package kaeruprofiler/0.0.2/kaeruprofiler-0.0.2.tar.gz

---
+++ packages/kaeruprofiler/kaeruprofiler-0.0.2.tar.gz/kaeruprofiler-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='kaeruprofiler',
+    version='0.0.2',
+    description='Profiling with Key',
+    url='https://github.com/harukaeru/Kaeruprofiler',
+    author='harukaeru',
+    author_email='nekoneko.myaomyao@gmail.com',
+    license='MIT',
+    py_modules=['kaeruprofiler']
+)
