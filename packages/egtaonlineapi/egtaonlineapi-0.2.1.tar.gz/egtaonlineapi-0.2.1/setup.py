0c1d85ae89bf8b52546bd390b293eb3e01a55645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:02 +0000

    Add package egtaonlineapi/0.2.1/egtaonlineapi-0.2.1.tar.gz

---
+++ packages/egtaonlineapi/egtaonlineapi-0.2.1.tar.gz/egtaonlineapi-0.2.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+setuptools.setup(
+    name='egtaonlineapi',
+    version='0.2.1',
+    description='Various APIs for egtaonline',
+    url='https://github.com/egtaonline/egtaonline-api.git',
+    author='Strategic Reasoning Group',
+    author_email='strategic.reasoning.group@umich.edu',
+    license='Apache 2.0',
+    entry_points=dict(console_scripts=['eo=egtaonline.__main__:main']),
+    install_requires=[
+        'lxml~=3.8',
+        'requests~=2.18',
+        'tabulate~=0.8',
+        'inflection~=0.3',
+    ],
+    packages=[
+        'egtaonline',
+    ]
+)
