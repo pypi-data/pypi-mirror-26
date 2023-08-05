1d88a6be805e0be28c570038d59e69fa190daa2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:37 +0000

    Add package newputtest/0.5/newputtest-0.5.tar.gz

---
+++ packages/newputtest/newputtest-0.5.tar.gz/newputtest-0.5/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+      name='newputtest',    # This is the name of your PyPI-package.
+      version='0.5',                          # Update the version number for new releases
+      py_modules=['newputtest']
+     )
