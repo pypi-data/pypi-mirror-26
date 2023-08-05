7fb906c59ebd92c50476fc26badf24ce14210309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:23 +0000

    Add package sf_tools/1.1/sf_tools-1.1.tar.gz

---
+++ packages/sf_tools/sf_tools-1.1.tar.gz/sf_tools-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sf_tools',
+    author='sfarrens',
+    author_email='samuel.farrens@gmail.com',
+    version='1.1',
+    url='https://github.com/sfarrens/sf_tools',
+    download_url='https://github.com/sfarrens/sf_tools/archive/0.1.tar.gz',
+    packages=find_packages(),
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
