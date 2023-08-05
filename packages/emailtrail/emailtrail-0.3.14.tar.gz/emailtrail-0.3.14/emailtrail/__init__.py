a43bb907f8f3690b71e43d8993375bce7203dd81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:49 +0000

    Add package emailtrail/0.3.14/emailtrail-0.3.14.tar.gz

---
+++ packages/emailtrail/emailtrail-0.3.14.tar.gz/emailtrail-0.3.14/emailtrail/__init__.py
@@ -0,0 +1,8 @@
+"""
+EmailTrail
+~~~~~~~~~~~~~~~~~~~~~
+Tiny python module to analyse email hops in transit.
+Get structured information about each hop - hostnames, protocol, timestamp, and delay
+"""
+from .module import *     # noqa
+# wildcard import is intentional
