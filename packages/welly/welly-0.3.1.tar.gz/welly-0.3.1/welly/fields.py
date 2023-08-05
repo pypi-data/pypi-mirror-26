23aa7f4b2c91f7b81439fc8920418cd0a4e8836f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:56 +0000

    Add package welly/0.3.1/welly-0.3.1.tar.gz

---
+++ packages/welly/welly-0.3.1.tar.gz/welly-0.3.1/welly/fields.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Field mapping from welly to LAS.
+
+:copyright: 2016 Agile Geoscience
+:license: Apache 2.0
+"""
+
+las_fields = {
+    'well': {
+    },
+    'header': {
+        'name': ('well', 'WELL'),
+        'field': ('well', 'FLD'),
+        'license': ('well', 'LIC'),
+        'uwi': ('well', 'UWI'),
+        'company': ('well', 'COMP'),
+    },
+    'location': {
+        'location': ('well', 'LOC'),
+        'country': ('well', 'CTRY'),
+        'province': ('well', 'PROV'),
+        'state': ('well', 'STAT'),
+        'county': ('well', 'CNTY'),
+        'latitude': ('well', 'LATI'),
+        'longitude': ('well', 'LONG'),
+        'datum': ('well', 'GDAT'),
+        'section': ('well', 'SECT'),
+        'range': ('well', 'RANG'),
+        'township': ('well', 'TOWN'),
+        'kb': ('params', 'EKB'),
+        'gl': ('params', 'EGL'),
+        'td': ('params', 'TDD'),
+        'tdd': ('params', 'TDD'),
+        'tdl': ('params', 'TDL'),
+    },
+    'data': {
+        'start': ('well', 'STRT'),
+        'stop': ('well', 'STOP'),
+        'step': ('well', 'STEP'),
+        'null': ('well', 'NULL'),
+        'run': ('params', 'RUN'),
+        'service_company': ('well', 'SRVC'),
+        'date': ('well', 'DATE'),
+    }
+}
