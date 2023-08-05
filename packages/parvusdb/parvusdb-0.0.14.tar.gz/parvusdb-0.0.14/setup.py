0a5921c85e8615f479afaf4974e7b067202dcc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:07 +0000

    Add package parvusdb/0.0.14/parvusdb-0.0.14.tar.gz

---
+++ packages/parvusdb/parvusdb-0.0.14.tar.gz/parvusdb-0.0.14/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='parvusdb',
+      version='0.0.14',
+      description='A lightweight in-memory graph database',
+      url='http://github.com/fractalego/parvusdb',
+      author='Alberto Cetoli',
+      author_email='alberto@nlulite.com',
+      license='MIT',
+      packages=['parvusdb', 'parvusdb.utils'],
+      install_requires=[
+          'python-igraph',
+          'hy',
+      ],
+      zip_safe=False)
