48a30de69847308bdef48089ee334ed2aa477944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:06 +0000

    Add package gplaycli/3.6/GPlayCli-3.6.tar.gz

---
+++ packages/gplaycli/GPlayCli-3.6.tar.gz/GPlayCli-3.6/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+import os
+import sys
+
+if sys.version_info[0] == 2:
+    basedir = 'python2'
+    python2 = True
+    python3 = False
+else:
+    basedir = 'python3'
+    python2 = False
+    python3 = True
+
+
+setup(name='GPlayCli',
+        version='3.6',
+        description='GPlayCli, a Google play downloader command line interface',
+        author="Matlink",
+        author_email="matlink@matlink.fr",
+        url="https://github.com/matlink/gplaycli",
+        license="AGPLv3",
+        entry_points={
+            'console_scripts': [
+                'gplaycli = gplaycli.gplaycli:main',
+            ],
+        },
+        packages=[
+            'gplaycli',
+        ],
+        package_dir={
+            'gplaycli': 'gplaycli',
+        },
+        data_files=[
+            [os.path.expanduser('~')+'/.config/gplaycli', ['gplaycli.conf','cron/cronjob']],
+        ],
+        install_requires=[
+                'protobuf',
+                'gpapi >= 0.1.5',
+                'pyaxmlparser' if python3 else 'androguard',
+                'enum34' if python2 else '',
+        ],
+)
