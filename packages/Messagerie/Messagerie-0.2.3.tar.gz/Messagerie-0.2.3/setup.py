7bf1245c3f31e3396d951bb9c3f300a5e2f230d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:45 +0000

    Add package Messagerie/0.2.3/Messagerie-0.2.3.tar.gz

---
+++ packages/Messagerie/Messagerie-0.2.3.tar.gz/Messagerie-0.2.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='Messagerie',
+      version='0.2.3',
+      description='Custom wrapper to aws services',
+      long_description=readme(),
+      url='https://github.com/stikks/messagerie',
+      author='stikks',
+      author_email='styccs@gmail.com',
+      include_package_data=True,
+      packages=['messagerie'],
+      install_requires=[
+          'requests',
+          'boto3',
+          'pyshorteners'
+      ])
