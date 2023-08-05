2ba9d4960cecc610f904f9f49a1ce410dc71c23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package pytorch-skipthoughts/0.4.3/pytorch-skipthoughts-0.4.3.tar.gz

---
+++ packages/pytorch-skipthoughts/pytorch-skipthoughts-0.4.3.tar.gz/pytorch-skipthoughts-0.4.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+__VERSION__ = "0.4.3"
+
+setup(
+    name="pytorch-skipthoughts",
+    version=__VERSION__,
+    description="Multi-GPU Customizable Implementation of Skip-Thoughts in PyTorch",
+    url="https://github.com/kaniblu/pytorch-skipthoughts",
+    author="Kang Min Yoo",
+    author_email="k@nib.lu",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ],
+    keywords="pytorch skip-thoughts multi-gpu gpu cuda deep learning nlp",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "pytorch-sru",
+        "pytorch-text-utils",
+        "visdom-pooled",
+        "pyaap",
+        "tqdm",
+    ],
+    package_data={
+        "": ["*.cu"]
+    }
+)
