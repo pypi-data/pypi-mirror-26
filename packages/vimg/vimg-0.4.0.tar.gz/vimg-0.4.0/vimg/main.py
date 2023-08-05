c96fabcc418efe5a5165eda60de7dfa8f7ad8cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:19 +0000

    Add package vimg/0.4.0/vimg-0.4.0.tar.gz

---
+++ packages/vimg/vimg-0.4.0.tar.gz/vimg-0.4.0/vimg/main.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import vimg
+
+def main():
+    if len(sys.argv) < 2:
+        print('Usage: vimg path/to/image'.format(sys.argv[0]))
+        sys.exit()
+    f = os.path.expanduser(sys.argv[1])
+    image = vimg.Image(f)
+    gui = vimg.GUI(image, mode='color')
+    gui.main()
+
+if __name__ == '__main__':
+    main()
