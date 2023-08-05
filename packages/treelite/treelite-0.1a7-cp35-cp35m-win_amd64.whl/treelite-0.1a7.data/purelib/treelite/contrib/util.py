ce4cd3e493e134e7d5e82608e38398802d3cd267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package treelite/0.1a7/treelite-0.1a7-cp35-cp35m-win_amd64.whl

---
+++ packages/treelite/treelite-0.1a7-cp35-cp35m-win_amd64.whl/treelite-0.1a7.data/purelib/treelite/contrib/util.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+"""
+Miscellaneous utilities
+"""
+
+from ..compat import PY3
+
+import shutil
+import inspect
+import time
+
+if PY3:
+  from tempfile import TemporaryDirectory
+else:
+  import tempfile
+  class TemporaryDirectory(object):
+    """Context manager for tempfile.mkdtemp()"""
+    def __enter__(self):
+        self.name = tempfile.mkdtemp()
+        return self.name
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        shutil.rmtree(self.name)
+
+def lineno():
+  """Returns line number"""
+  return inspect.currentframe().f_back.f_lineno
+
+def log_info(filename, linenum, msg):
+  print('[{}] {}:{}: {}'.format(time.strftime('%X'), filename, linenum, msg))
+
+__all__ = ['TemporaryDirectory', 'lineno']
