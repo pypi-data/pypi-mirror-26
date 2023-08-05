4554247935c9721cacbb463458927822911f8dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:28 +0000

    Add package dabin-nester/1.0.0/dabin_nester-1.0.0.tar.gz

---
+++ packages/dabin-nester/dabin_nester-1.0.0.tar.gz/dabin_nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='dabin_nester',
+    version='1.0.0',
+    py_modules=['dabin_nester'],
+    author='Dabin',
+    author_email='1003316758@qq.com',
+    url="http://sunajun.cn",
+    description="This is a list function,you can like it"
+
+)
