3db7a93754832c7db7c22ebdcec254d68ef6e20f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:40 +0000

    Add package my-example-lib/0.13/my-example-lib-0.13.tar.gz

---
+++ packages/my-example-lib/my-example-lib-0.13.tar.gz/my-example-lib-0.13/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='my-example-lib',
+    description='An example lib',
+    version='0.13',
+    author='Alexander M.',
+    author_email='intmainreturn@yandex.com',
+    license='MIT'
+)
