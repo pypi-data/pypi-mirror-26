67d93621414458255a69bc995a06d48135e67553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:56 +0000

    Add package pracode/0.0.0/pracode-0.0.0.tar.gz

---
+++ packages/pracode/pracode-0.0.0.tar.gz/pracode-0.0.0/pracode/leetcode.py
@@ -0,0 +1,18 @@
+
+class Leetcode(object):
+    def __init__(self):
+        self._id = None
+        self._lang = None
+        self._filename = None
+
+    def get(self):
+        pass
+
+    def test(self):
+        pass
+
+    def submit(self):
+        pass
+
+    def list(self):
+        pass
---
+++ packages/pracode/pracode-0.0.0.tar.gz/pracode-0.0.0/pracode/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from .leetcode import Leetcode
+
+def main():
+    print('hi, lcode!')
+
+
+if __name__ == '__main__':
+    main()
