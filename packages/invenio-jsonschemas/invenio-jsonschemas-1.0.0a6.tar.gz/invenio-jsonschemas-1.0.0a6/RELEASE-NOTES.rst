22f525f1af265656734e1b5c48f6a1670fd14446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:04 +0000

    Add package invenio-jsonschemas/1.0.0a6/invenio-jsonschemas-1.0.0a6.tar.gz

---
+++ packages/invenio-jsonschemas/invenio-jsonschemas-1.0.0a6.tar.gz/invenio-jsonschemas-1.0.0a6/RELEASE-NOTES.rst
@@ -0,0 +1,36 @@
+==============================
+ Invenio-JSONSchemas v1.0.0a6
+==============================
+
+Invenio-JSONSchemas v1.0.0a6 was released on October 10, 2017.
+
+About
+-----
+
+Invenio module for building and serving JSONSchemas.
+
+*This is an experimental developer preview release.*
+
+What's new
+----------
+
+- Initial public release.
+
+Installation
+------------
+
+   $ pip install invenio-jsonschemas==1.0.0a6
+
+Documentation
+-------------
+
+   https://invenio-jsonschemas.readthedocs.io/
+
+Happy hacking and thanks for flying Invenio-JSONSchemas.
+
+| Invenio Development Team
+|   Email: info@inveniosoftware.org
+|   IRC: #invenio on irc.freenode.net
+|   Twitter: https://twitter.com/inveniosoftware
+|   GitHub: https://github.com/inveniosoftware/invenio-jsonschemas
+|   URL: http://inveniosoftware.org
---
+++ packages/invenio-jsonschemas/invenio-jsonschemas-1.0.0a6.tar.gz/invenio-jsonschemas-1.0.0a6/setup.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015, 2016, 2017 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Invenio module for building and serving JSONSchemas."""
+
+import os
+
+from setuptools import find_packages, setup
+
+readme = open('README.rst').read()
+history = open('CHANGES.rst').read()
+
+tests_require = [
+    'check-manifest>=0.25',
+    'coverage>=4.0',
+    'isort>=4.2.2',
+    'jsonresolver[jsonschema]>=0.2.1',
+    'pydocstyle>=2.0.0',
+    'pytest-cache>=1.0',
+    'pytest-cov>=1.8.0',
+    'pytest-pep8>=1.0.6',
+    'pytest>=2.8.0',
+    'mock>=1.3.0',
+]
+
+extras_require = {
+    'docs': [
+        'Sphinx>=1.6.2',
+    ],
+    'tests': tests_require,
+}
+
+extras_require['all'] = []
+for reqs in extras_require.values():
+    extras_require['all'].extend(reqs)
+
+setup_requires = [
+    'pytest-runner>=2.6.2',
+]
+
+install_requires = [
+    'Flask>=0.11.1',
+    'jsonref>=0.1',
+]
+
+packages = find_packages()
+
+
+# Get the version string. Cannot be done with import!
+g = {}
+with open(os.path.join('invenio_jsonschemas', 'version.py'), 'rt') as fp:
+    exec(fp.read(), g)
+    version = g['__version__']
+
+setup(
+    name='invenio-jsonschemas',
+    version=version,
+    description=__doc__,
+    long_description=readme + '\n\n' + history,
+    keywords='invenio json schema',
+    license='GPLv2',
+    author='CERN',
+    author_email='info@inveniosoftware.org',
+    url='https://github.com/inveniosoftware/invenio-jsonschemas',
+    packages=packages,
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    entry_points={
+        'invenio_base.apps': [
+            'invenio_jsonschemas = invenio_jsonschemas:InvenioJSONSchemasUI',
+        ],
+        'invenio_base.api_apps': [
+            'invenio_jsonschemas = invenio_jsonschemas:InvenioJSONSchemasAPI',
+        ],
+        'invenio_records.jsonresolver': [
+            'invenio_jsonschemas = invenio_jsonschemas.jsonresolver',
+        ]
+    },
+    extras_require=extras_require,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Development Status :: 4 - Beta',
+    ],
+)
