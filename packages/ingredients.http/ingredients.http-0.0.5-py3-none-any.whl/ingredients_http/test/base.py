6881b75fd02566627070da9060bcdbe97e14b139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:52 +0000

    Add package ingredients.http/0.0.5/ingredients.http-0.0.5-py3-none-any.whl

---
+++ packages/ingredients.http/ingredients.http-0.0.5-py3-none-any.whl/ingredients_http/test/base.py
@@ -0,0 +1,50 @@
+import os
+from abc import ABCMeta, abstractmethod
+
+import pytest
+from simple_settings import settings
+from sqlalchemy_utils import drop_database, create_database, database_exists
+from webtest import TestApp
+
+
+class APITestCase(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def router_cls(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def setup_mounts(self, app):
+        pass
+
+    @abstractmethod
+    def settings_module(self) -> str:
+        raise NotImplementedError
+
+    @pytest.fixture
+    def database(self):
+        if 'CI' in os.environ:
+            uri = "postgresql+psycopg2://postgres@127.0.0.1/test_db"
+        else:
+            uri = os.environ['TEST_DB_URL']
+        if database_exists(uri):
+            drop_database(uri)
+        create_database(uri)
+        yield
+        drop_database(uri)
+
+    @pytest.yield_fixture()
+    def app(self, database):
+        os.environ['settings'] = self.settings_module() + ",ingredients_http.test.settings.db_settings"
+        settings._dict = {}  # Reset settings for every test
+        app = self.router_cls()()
+        self.setup_mounts(app)
+
+        app.setup()
+        yield app
+        app.database.close()
+
+    @pytest.yield_fixture()
+    def wsgi(self, app):
+        yield TestApp(app.wsgi_application)
---
+++ packages/ingredients.http/ingredients.http-0.0.5-py3-none-any.whl/ingredients_http/test/settings/db_settings.py
@@ -0,0 +1,27 @@
+import os
+
+from sqlalchemy.engine.url import make_url
+
+####################
+# DATABASE         #
+####################
+
+
+if 'CI' in os.environ:
+    DATABASE_HOST = '127.0.0.1'
+    DATABASE_PORT = '5432'
+    DATABASE_USERNAME = 'postgres'
+    DATABASE_PASSWORD = ''
+    DATABASE_DB = 'test_db'
+    DATABASE_POOL_SIZE = 20
+else:
+    db_url = make_url(os.environ['TEST_DB_URL'])
+    print("Database url")
+    print(os.environ['TEST_DB_URL'])
+    print(db_url)
+    DATABASE_HOST = db_url.host
+    DATABASE_PORT = db_url.port
+    DATABASE_USERNAME = db_url.username
+    DATABASE_PASSWORD = db_url.password
+    DATABASE_DB = db_url.database
+    DATABASE_POOL_SIZE = 20
