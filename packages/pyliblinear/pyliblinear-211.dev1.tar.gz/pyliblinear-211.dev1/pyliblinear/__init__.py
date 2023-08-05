d351ccd5c20f4bbc4dd8522a7ef4a0e85cfb1a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:31 +0000

    Add package pyliblinear/211.dev1/pyliblinear-211.dev1.tar.gz

---
+++ packages/pyliblinear/pyliblinear-211.dev1.tar.gz/pyliblinear-211.dev1/pyliblinear/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: ascii -*-
+u"""
+:Copyright:
+
+ Copyright 2015 - 2017
+ Andr\xe9 Malo or his licensors, as applicable
+
+:License:
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+======================================
+ pyliblinear - a liblinear python API
+======================================
+
+pyliblinear - a liblinear python API
+"""
+__author__ = u"Andr\xe9 Malo"
+__docformat__ = "restructuredtext en"
+__license__ = "Apache License, Version 2.0"
+__version__ = '211.dev1'
+
+from pyliblinear import _util
+from pyliblinear._liblinear import FeatureMatrix  # noqa
+from pyliblinear._liblinear import Model  # noqa
+from pyliblinear._liblinear import Solver  # noqa
+from pyliblinear._liblinear import SOLVER_TYPES  # noqa
+
+__all__ = _util.find_public(globals())
