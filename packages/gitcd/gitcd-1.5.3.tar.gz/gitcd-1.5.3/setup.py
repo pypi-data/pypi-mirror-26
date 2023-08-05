0f8ab39532d3e7c49e870b307f293926bbb881be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:47 +0000

    Add package gitcd/1.5.3/gitcd-1.5.3.tar.gz

---
+++ packages/gitcd/gitcd-1.5.3.tar.gz/gitcd-1.5.3/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='gitcd',
+    version='1.5.3',
+    description='Tool for continous delivery using git',
+    author='Claudio Walser',
+    author_email='claudio.walser@srf.ch',
+    url='https://github.com/claudio-walser/gitcd',
+    packages=[
+        'gitcd',
+        'gitcd.Cli',
+        'gitcd.Config',
+        'gitcd.Git',
+        'gitcd.Git.Commands'
+    ],
+    install_requires=['pyyaml', 'argparse', 'argcomplete', 'requests'],
+    entry_points={
+        'console_scripts': [
+            'git-cd = gitcd.__main__:main'
+        ]
+    }
+)
