040232a623d6025d6f51738b0924c2bf3b832e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:31 +0000

    Add package python-humble-utils/1.0.3/python-humble-utils-1.0.3.tar.gz

---
+++ packages/python-humble-utils/python-humble-utils-1.0.3.tar.gz/python-humble-utils-1.0.3/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+setup_requirements = [
+    'pytest-runner',
+]
+
+
+setup(
+    name='python-humble-utils',
+    version='1.0.3',
+    url='https://github.com/webyneter/python-humble-utils',
+
+    description="Python utils for everyday use.",
+    long_description=readme + '\n\n' + history,
+
+    author="Nikita P. Shupeyko",
+    author_email='webyneter@gmail.com',
+
+    packages=find_packages(include=['python_humble_utils']),
+    include_package_data=True,
+
+    test_suite='python_humble_utils.tests',
+
+    setup_requires=setup_requirements,
+    zip_safe=False,
+
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords=[
+        'python',
+        'humble',
+        'utility',
+        'utilities',
+        'util',
+        'utils',
+        'helper',
+        'helpers',
+    ],
+)
