a7269d897c2914659da4c2471ffadb3a8e5bc216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:49 +0000

    Add package qrl/0.54.3/qrl-0.54.3.tar.gz

---
+++ packages/qrl/qrl-0.54.3.tar.gz/qrl-0.54.3/qrl/core/GenesisBlock.py
@@ -0,0 +1,101 @@
+# coding=utf-8
+# Distributed under the MIT software license, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+from google.protobuf.json_format import MessageToJson, Parse
+from pyqrllib.pyqrllib import sha2_256, bin2hstr
+
+from qrl.core.blockheader import BlockHeader
+from qrl.core import config
+from qrl.generated import qrl_pb2
+
+
+class Singleton(type):
+    instance = None
+
+    def __call__(cls, *args, **kw):
+        if not cls.instance:
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
+        return cls.instance
+
+
+class GenesisBlock(object, metaclass=Singleton):
+    """
+    # first block has no previous header to reference..
+    >>> GenesisBlock().blockheader.prev_blockheaderhash == b'1a02aa2cbe25c60f491aeb03131976be2f9b5e9d0bc6b6d9e0e7c7fd19c8a076c29e028f5f3924b4'
+    True
+    """
+
+    def __init__(self, protobuf_genesisBlock=None):
+        self._data = protobuf_genesisBlock
+
+        if protobuf_genesisBlock is None:
+            self._data = qrl_pb2.Block()
+            self.blockheader = BlockHeader()
+        else:
+            self.blockheader = BlockHeader(protobuf_genesisBlock.header)
+
+        self._data.header.MergeFrom(self.blockheader._data)
+        self.blockheader._data.hash_header_prev = b'1a02aa2cbe25c60f491aeb03131976be2f9b5e9d0bc6b6d9e0e7c7fd19c8a076c29e028f5f3924b4'
+
+    @property
+    def transactions(self):
+        return self._data.transactions
+
+    @property
+    def duplicate_transactions(self):
+        return self._data.dup_transactions
+
+    @property
+    def state(self):
+        return self._data.state
+
+    @property
+    def stake_list(self):
+        return self._data.stake_list
+
+    def set_chain(self, chain):
+        # FIXME: It is odd that we have a hash equal to 'genesis'
+        """
+        :param chain:
+        :type chain:
+        :return:
+        :rtype:
+
+        >>> GenesisBlock().set_chain(None) is not None
+        True
+        >>> GenesisBlock().set_chain(None).blockheader.epoch
+        0
+        >>> GenesisBlock().set_chain(None).blockheader.block_reward
+        0
+        >>> GenesisBlock().set_chain(None).blockheader.blocknumber
+        0
+        >>> GenesisBlock().set_chain(None).blockheader.fee_reward
+        0
+        >>> GenesisBlock().set_chain(None).blockheader.reveal_hash
+        b'\\x00\\x00\\x00\\x00\\x00\\x00'
+        >>> bin2hstr(GenesisBlock().set_chain(None).blockheader.headerhash)
+        '82a1be60d16457909930a0987547d59320ff5be982cf172440cada812451f66d'
+        >>> bin2hstr(GenesisBlock().set_chain(None).blockheader.prev_blockheaderhash)
+        '3db80b421931369dfdf3fd19b401e789decac370afcd1f3a50acf2c915545d6e'
+        """
+        self.blockheader = self.blockheader.create(chain=chain,
+                                                   blocknumber=0,
+                                                   prev_blockheaderhash=bytes(sha2_256(config.dev.genesis_prev_headerhash.encode())),
+                                                   hashedtransactions=bytes(sha2_256(b'0')),
+                                                   reveal_hash=bytes((0, 0, 0, 0, 0, 0)),
+                                                   fee_reward=0)
+
+        self._data.header.MergeFrom(self.blockheader._data)
+
+        return self
+
+    @staticmethod
+    def from_json(json_data):
+        pbdata = qrl_pb2.Block()
+        Parse(json_data, pbdata)
+        return GenesisBlock(pbdata)
+
+    def to_json(self):
+        # FIXME: Remove once we move completely to protobuf
+        return MessageToJson(self._data)
