5ddb1492ebd883b28fd3d570faf7fc46e4d135a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:59 +0000

    Add package hse-ruz/0.2.4rc2/hse_ruz-0.2.4rc2.tar.gz

---
+++ packages/hse-ruz/hse_ruz-0.2.4rc2.tar.gz/hse_ruz-0.2.4rc2/ruz/__init__.py
@@ -0,0 +1,19 @@
+'''
+    Python wrapper for HSE RUZ API.
+
+    Usage:
+        from ruz import RUZ
+
+        api = RUZ()
+        assert api.v == 1
+        assert api.schedule("mymail@edu.hse.ru")
+'''
+
+import os
+
+from ruz.RUZ import RUZ
+from ruz.utils import REQUEST_SCHEMA, RESPONSE_SCHEMA, EMAIL_DOMAINS
+
+__author__ = "hell03end"
+__version__ = (0, 2, 4)
+__all__ = ("RUZ", "REQUEST_SCHEMA", "RESPONSE_SCHEMA")
