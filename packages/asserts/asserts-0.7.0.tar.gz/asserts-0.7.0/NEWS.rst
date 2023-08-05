1248fa428dc12fa8ec929026712fce462bd6ce63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:23 +0000

    Add package asserts/0.7.0/asserts-0.7.0.tar.gz

---
+++ packages/asserts/asserts-0.7.0.tar.gz/asserts-0.7.0/NEWS.rst
@@ -0,0 +1,35 @@
+News in asserts 0.7.0
+=====================
+
+* Add a stub file.
+
+API Additions
+-------------
+
+* Add assert_count_equal().
+
+News in asserts 0.6
+===================
+
+API Additions
+-------------
+
+* Add assert_less(), assert_less_equal(), assert_greater(), and
+  assert_greater_equal().
+* Add assert_not_is_instance().
+
+Improvements
+------------
+
+* assert_datetime_about_now()/assert_datetime_about_now_utc(): Handle
+  comparison with None more gracefully.
+
+News in asserts 0.5.1
+=====================
+
+* Add the LICENSE file to the distribution.
+
+News in asserts 0.5
+===================
+
+Initial release.
---
+++ packages/asserts/asserts-0.7.0.tar.gz/asserts-0.7.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+import sys
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="asserts",
+    version="0.7.0",
+    description="Stand-alone Assertions",
+    long_description=read("README.rst"),
+    author="Sebastian Rittau",
+    author_email="srittau@rittau.biz",
+    url="https://github.com/srittau/python-asserts",
+    py_modules=["asserts", "test_asserts"],
+    data_files=[(
+        "lib/python{}.{}/site-packages".format(*sys.version_info[:2]),
+        ["asserts.pyi"],
+    )],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+    ],
+)
