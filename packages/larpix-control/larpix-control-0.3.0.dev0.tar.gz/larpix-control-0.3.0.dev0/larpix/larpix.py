6e74627872e83114e6da2932f415510c97c16c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:32 +0000

    Add package larpix-control/0.3.0.dev0/larpix-control-0.3.0.dev0.tar.gz

---
+++ packages/larpix-control/larpix-control-0.3.0.dev0.tar.gz/larpix-control-0.3.0.dev0/larpix/larpix.py
@@ -0,0 +1,495 @@
+'''
+A module to control the LArPix chip.
+
+'''
+
+import time
+import serial
+from bitstring import BitArray, Bits
+
+class Chip(object):
+    '''
+    Represents one LArPix chip and helps with configuration and packet
+    generation.
+
+    '''
+    num_channels = 32
+    def __init__(self, chip_id, io_chain):
+        self.chip_id = chip_id
+        self.io_chain = io_chain
+        self.data_to_send = []
+        self.configuration = Configuration()
+        self.reads = []
+
+    def set_pixel_trim_thresholds(self, thresholds):
+        if len(thresholds) != Chip.num_channels:
+            return 1
+        for i,value in enumerate(thresholds):
+            self.configuration.pixel_trim_thresholds[i] = value
+        return 0
+
+    def set_global_threshold(self, threshold):
+        self.configuration.global_threshold = threshold
+        return 0
+
+    def get_configuration_packets(self, packet_type):
+        conf = self.configuration
+        packets = [Packet() for _ in range(Configuration.num_registers)]
+        packet_register_data = conf.all_data()
+        for i, (packet, data) in enumerate(zip(packets, packet_register_data)):
+            packet.packet_type = packet_type
+            packet.chipid = self.chip_id
+            packet.register_address = i
+            packet.register_data = data
+            packet.assign_parity()
+        return packets
+
+    def set_csa_gain(self, gain):
+        self.configuration.csa_gain = gain
+        return 0
+
+    def enable_channels(self, list_of_channels):
+        for channel in list_of_channels:
+            self.configuration.channel_mask[channel] = 0
+        return 0
+
+    def disable_channels(self, list_of_channels):
+        for channel in list_of_channels:
+            self.configuration.channel_mask[channel] = 1
+        return 0
+
+    def enable_all_channels(self):
+        return self.enable_channels(range(Chip.num_channels))
+
+    def disable_all_channels(self):
+        return self.disable_channels(range(Chip.num_channels))
+
+    def enable_normal_operation(self):
+        return
+
+    def enable_external_trigger(self, list_of_channels):
+        for channel in list_of_channels:
+            self.configuration.external_trigger_mask[channel] = 0
+        return 0
+
+    def disable_external_trigger(self, list_of_channels):
+        for channel in list_of_channels:
+            self.configuration.external_trigger_mask[channel] = 1
+        return 0
+
+    def set_testpulse_dac(self, value):
+        self.configuration.csa_testpulse_dac_amplitude = value
+        return 0
+
+    def enable_testpulse(self, list_of_channels):
+        for channel in list_of_channels:
+            self.configuration.csa_testpulse_enable[channel] = 1
+        return 0
+
+    def disable_testpulse(self, list_of_channels):
+        for channel in list_of_channels:
+            self.configuration.csa_testpulse_enable[channel] = 0
+        return 0
+
+    def enable_fifo_diagnostic(self):
+        self.configuration.fifo_diagnostic = 1
+        return 0
+
+    def diable_fifo_diagnostic(self):
+        self.configuration.fifo_diagnostic = 0
+        return 0
+
+    def set_fifo_test_burst_length(self, value):
+        self.configuration.test_burst_length = value
+        return 0
+
+    def enable_fifo_test_mode(self):
+        self.configuration.test_mode = 0x10
+        return 0
+
+    def disable_fifo_test_mode(self):
+        self.configuration.test_mode = 0x0;
+        return 0
+
+    def enable_analog_monitor(self, list_of_channels):
+        for channel in list_of_channels:
+            self.configuration.csa_monitor_select[channel] = 0
+        return 0
+
+    def disable_analog_monitor(self, list_of_channels):
+        for channel in list_of_channels:
+            self.configuration.csa_monitor_select[channel] = 1
+        return
+
+class Configuration(object):
+    '''
+    Represents the desired configuration state of a LArPix chip.
+
+    '''
+    num_registers = 63
+    pixel_trim_threshold_addresses = list(range(0, 32))
+    global_threshold_address = 32
+    csa_gain_and_bypasses_address = 33
+    csa_bypass_select_addresses = list(range(34, 38))
+    csa_monitor_select_addresses = list(range(38, 42))
+    csa_testpulse_enable_addresses = list(range(42, 46))
+    csa_testpulse_dac_amplitude_address = 46
+    test_mode_xtrig_reset_diag_address = 47
+    sample_cycles_address = 48
+    test_burst_length_addresses = [49, 50]
+    adc_burst_length_address = 51
+    channel_mask_addresses = list(range(52, 56))
+    external_trigger_mask_addresses = list(range(56, 60))
+    reset_cycles_addresses = [60, 61, 62]
+    def __init__(self):
+        self.pixel_trim_thresholds = [0x10] * Chip.num_channels
+        self.global_threshold = 0x10
+        self.csa_gain = 1
+        self.csa_bypass = 0
+        self.internal_bypass = 1
+        self.csa_bypass_select = [0] * Chip.num_channels
+        self.csa_monitor_select = [1] * Chip.num_channels
+        self.csa_testpulse_enable = [0] * Chip.num_channels
+        self.csa_testpulse_dac_amplitude = 0
+        self.test_mode = 0
+        self.cross_trigger_mode = 0
+        self.periodic_reset = 0
+        self.fifo_diagnostic = 0
+        self.sample_cycles = 1
+        self.test_burst_length = 0x00FF
+        self.adc_burst_length = 0
+        self.channel_mask = [0] * Chip.num_channels
+        self.external_trigger_mask = [1] * Chip.num_channels
+        self.reset_cycles = 0x001000
+
+    def all_data(self):
+        bits = []
+        num_channels = Chip.num_channels
+        for channel in range(num_channels):
+            bits.append(self.trim_threshold_data(channel))
+        bits.append(self.global_threshold_data())
+        bits.append(self.csa_gain_and_bypasses_data())
+        for chunk in range(4):
+            bits.append(self.csa_bypass_select_data(chunk))
+        for chunk in range(4):
+            bits.append(self.csa_monitor_select_data(chunk))
+        for chunk in range(4):
+            bits.append(self.csa_testpulse_enable_data(chunk))
+        bits.append(self.csa_testpulse_dac_amplitude_data())
+        bits.append(self.test_mode_xtrig_reset_diag_data())
+        bits.append(self.sample_cycles_data())
+        bits.append(self.test_burst_length_data(0))
+        bits.append(self.test_burst_length_data(1))
+        bits.append(self.adc_burst_length_data())
+        for chunk in range(4):
+            bits.append(self.channel_mask_data(chunk))
+        for chunk in range(4):
+            bits.append(self.external_trigger_mask_data(chunk))
+        bits.append(self.reset_cycles_data(0))
+        bits.append(self.reset_cycles_data(1))
+        bits.append(self.reset_cycles_data(2))
+        return bits
+
+    def _to8bits(self, number):
+        return Bits('uint:8=' + str(number))
+
+    def trim_threshold_data(self, channel):
+        return self._to8bits(self.pixel_trim_thresholds[channel])
+
+    def global_threshold_data(self):
+        return self._to8bits(self.global_threshold)
+
+    def csa_gain_and_bypasses_data(self):
+        return Bits('0b0000') + [self.internal_bypass, 0,
+                self.csa_bypass, self.csa_gain]
+
+    def csa_bypass_select_data(self, chunk):
+        if chunk == 0:
+            return Bits(self.csa_bypass_select[7::-1])
+        else:
+            high_bit = (chunk + 1) * 8 - 1
+            low_bit = chunk * 8 - 1
+            return Bits(self.csa_bypass_select[high_bit:low_bit:-1])
+
+    def csa_monitor_select_data(self, chunk):
+        if chunk == 0:
+            return Bits(self.csa_monitor_select[7::-1])
+        else:
+            high_bit = (chunk + 1) * 8 - 1
+            low_bit = chunk * 8 - 1
+            return Bits(self.csa_monitor_select[high_bit:low_bit:-1])
+
+    def csa_testpulse_enable_data(self, chunk):
+        if chunk == 0:
+            return Bits(self.csa_testpulse_enable[7::-1])
+        else:
+            high_bit = (chunk + 1) * 8 - 1
+            low_bit = chunk * 8 - 1
+            return Bits(self.csa_testpulse_enable[high_bit:low_bit:-1])
+
+    def csa_testpulse_dac_amplitude_data(self):
+        return self._to8bits(self.csa_testpulse_dac_amplitude)
+
+    def test_mode_xtrig_reset_diag_data(self):
+        toReturn = BitArray([0, 0, 0, self.fifo_diagnostic,
+            self.periodic_reset,
+            self.cross_trigger_mode])
+        toReturn.append('uint:2=' + str(self.test_mode))
+        return toReturn
+
+    def sample_cycles_data(self):
+        return self._to8bits(self.sample_cycles)
+
+    def test_burst_length_data(self, chunk):
+        bits = Bits('uint:16=' + str(self.test_burst_length))
+        if chunk == 0:
+            return bits[8:]
+        elif chunk == 1:
+            return bits[:8]
+
+    def adc_burst_length_data(self):
+        return self._to8bits(self.adc_burst_length)
+
+    def channel_mask_data(self, chunk):
+        if chunk == 0:
+            return Bits(self.channel_mask[7::-1])
+        else:
+            high_bit = (chunk + 1) * 8 - 1
+            low_bit = chunk * 8 - 1
+            return Bits(self.channel_mask[high_bit:low_bit:-1])
+
+    def external_trigger_mask_data(self, chunk):
+        if chunk == 0:
+            return Bits(self.external_trigger_mask[7::-1])
+        else:
+            high_bit = (chunk + 1) * 8 - 1
+            low_bit = chunk * 8 - 1
+            return Bits(self.external_trigger_mask[high_bit:low_bit:-1])
+
+    def reset_cycles_data(self, chunk):
+        bits = Bits('uint:24=' + str(self.reset_cycles))
+        if chunk == 0:
+            return bits[16:]
+        elif chunk == 1:
+            return bits[8:16]
+        elif chunk == 2:
+            return bits[:8]
+
+
+class Controller(object):
+    '''
+    Controls a collection of LArPix Chip objects.
+
+    '''
+    start_byte = b'\x73'
+    stop_byte = b'\x71'
+    comma_byte = b'\x0D'
+    def __init__(self, port):
+        self.chips = []
+        self.port = port
+        self.baudrate = 1000000
+        self.timeout = 1
+        self.max_write = 8192
+        self._test_mode = False
+
+    def get_chip(self, chip_id, io_chain):
+        for chip in self.chips:
+            if chip.chip_id == chip_id and chip.io_chain == io_chain:
+                return chip
+        raise ValueError('Could not find chip (%d, %d)' % (chip_id,
+            io_chain))
+
+    def serial_read(self, timelimit):
+        data_in = []
+        start = time.time()
+        with serial.Serial(self.port, baudrate=self.baudrate,
+                timeout=self.timeout) as serial_in:
+            while time.time() - start < timelimit:
+                stream = serial_in.read(self.max_write)
+                if len(stream) > 0:
+                    data_in.append(stream)
+        return data_in
+
+    def serial_write(self, bytestreams):
+        with serial.Serial(self.port, baudrate=self.baudrate,
+                timeout=self.timeout) as output:
+            for bytestream in bytestreams:
+                output.write(bytestream)
+
+    def write_configuration(self, chip, registers=None):
+        if registers is None:
+            registers = list(range(Configuration.num_registers))
+        elif isinstance(registers, int):
+            registers = [registers]
+        else:
+            pass
+        # The configuration must be sent one register at a time
+        configuration_packets = \
+            chip.get_configuration_packets(Packet.CONFIG_WRITE_PACKET);
+        for i in range(len(configuration_packets)-1, -1, -1):
+            if i not in registers:
+                del configuration_packets[i]
+        formatted_packets = [self.format_UART(chip, p) for p in
+                configuration_packets]
+        bytestreams = self.format_bytestream(formatted_packets)
+        if self._test_mode:
+            return bytestreams
+        else:
+            serial_write(bytestreams)
+            return
+
+    def run_testpulse(self, list_of_channels):
+        return
+
+    def run_fifo_test(self):
+        return
+
+    def run_analog_monitor_teest(self):
+        return
+
+    def format_UART(self, chip, packet):
+        packet_bytes = packet.bytes()
+        daisy_chain_byte = (4 + Bits('uint:4=' + str(chip.io_chain))).bytes
+        formatted_packet = (Controller.start_byte + daisy_chain_byte +
+                packet_bytes + Controller.stop_byte)
+        return formatted_packet
+
+    def parse_input(self, bytestream):
+        # parse the bytestream into Packets + metadata
+        byte_packets = []
+        current_stream = bytestream
+        while len(current_stream) >= 9:  # remember to collect the remainder
+            if bytestream[8] == Controller.comma_byte:
+                byte_packets.append((Bits(bytestream[0]),
+                    Packet(bytestream[1:8])))
+                current_stream = current_stream[9:]
+            # TODO: deal with "else" (partial packet or other error)
+        # assign each packet to the corresponding Chip
+        for byte_packet in byte_packets:
+            io_chain = byte_packet[0][4:].uint
+            packet = byte_packet[1]
+            chip_id = packet.chipid
+            self.get_chip(chip_id, io_chain).reads.append(packet)
+        return current_stream  # (the remainder that wasn't read in)
+
+    def format_bytestream(self, formatted_packets):
+        bytestreams = []
+        current_bytestream = bytes()
+        for packet in formatted_packets:
+            if len(current_bytestream) + len(packet) < self.max_write:
+                current_bytestream += packet
+            else:
+                bytestreams.append(current_bytestream)
+                current_bytestream = bytes()
+                current_bytestream += packet
+        bytestreams.append(current_bytestream)
+        return bytestreams
+
+
+
+class Packet(object):
+    '''
+    A single 54-bit LArPix UART data packet.
+
+    '''
+    size = 54
+    num_bytes = 7
+    # These ranges are reversed from the bit addresses given in the
+    # LArPix datasheet because BitArray indexing is big-endian but we
+    # transmit data little-endian-ly. E.g.:
+    # >>> x = BitArray('0b00')
+    # >>> x[0:] = bin(2)  # ('0b10')
+    # >>> x[0]  # returns True (1)
+    # Another way to think of it is BitArray indexing reads the
+    # bitstream from 0:N but all the LArPix datasheet indexing goes from
+    # N:0.
+    packet_type_bits = slice(52, 54)
+    chipid_bits = slice(44, 52)
+    parity_bit = 0
+    channel_id_bits = slice(37, 44)
+    timestamp_bits = slice(13, 37)
+    dataword_bits = slice(3, 13)
+    fifo_half_bit = 2
+    fifo_full_bit = 1
+    register_address_bits = slice(36, 44)
+    register_data_bits = slice(28, 36)
+    config_unused_bits = slice(1, 28)
+    test_bits_11_0 = slice(1, 13)
+    test_bits_15_12 = slice(41, 44)
+
+    TEST_PACKET = Bits('0b00')
+    DATA_PACKET = Bits('0b01')
+    CONFIG_WRITE_PACKET = Bits('0b10')
+    CONFIG_READ_PACKET = Bits('0b11')
+
+    def __init__(self, bytestream=None):
+        self._bit_padding = Bits('0b00')
+        if bytestream is None:
+            self.bits = BitArray(Packet.size)
+            return
+        elif len(bytestream) == Packet.num_bytes:
+            # Parse the bytestream. Remember that bytestream[0] goes at
+            # the 'end' of the BitArray
+            reversed_bytestream = bytestream[::-1]
+            bits_with_padding = BitArray(reversed_bytestream)
+            self.bits = bits_with_padding[len(self._bit_padding):]
+        else:
+            raise ValueError('Invalid number of bytes: %s' %
+                    len(bytestream))
+
+    def bytes(self):
+        # Here's the only other place we have to deal with the
+        # endianness issue by reversing the order
+        padded_output = self._bit_padding + self.bits
+        bytes_output = padded_output.bytes
+        return bytes_output[::-1]
+
+    @property
+    def packet_type(self):
+        return self.bits[Packet.packet_type_bits]
+
+    @packet_type.setter
+    def packet_type(self, value):
+        self.bits[Packet.packet_type_bits] = value
+
+    @property
+    def chipid(self):
+        return self.bits[Packet.chipid_bits].uint
+
+    @chipid.setter
+    def chipid(self, value):
+        self.bits[Packet.chipid_bits] = value
+
+    @property
+    def parity_bit_value(self):
+        return int(self.bits[Packet.parity_bit])
+
+    @parity_bit_value.setter
+    def parity_bit_value(self, value):
+        self.bits[Packet.parity_bit] = value
+
+    def compute_parity(self):
+        return 1 - (self.bits[1:].count(True) % 2)
+
+    def assign_parity(self):
+        self.parity_bit_value = self.compute_parity()
+
+    def has_valid_parity(self):
+        return self.parity_bit_value == self.compute_parity()
+
+    @property
+    def register_address(self):
+        return self.bits[Packet.register_address_bits].uint
+
+    @register_address.setter
+    def register_address(self, value):
+        self.bits[Packet.register_address_bits] = value
+
+    @property
+    def register_data(self):
+        return self.bits[Packet.register_data_bits].uint
+
+    @register_data.setter
+    def register_data(self, value):
+        self.bits[Packet.register_data_bits] = value
+
---
+++ packages/larpix-control/larpix-control-0.3.0.dev0.tar.gz/larpix-control-0.3.0.dev0/larpix/test_larpix.py
@@ -0,0 +1,306 @@
+'''
+Use the pytest framework to write tests for the larpix module.
+
+'''
+import larpix
+from larpix import Chip, Packet, Configuration, Controller
+from bitstring import BitArray
+
+def test_chip_get_configuration_packets():
+    chip = Chip(3, 1)
+    packet_type = Packet.CONFIG_WRITE_PACKET
+    packets = chip.get_configuration_packets(packet_type)
+    # test a sampling of the configuration packets
+    packet = packets[5]
+    assert packet.packet_type == packet_type
+    assert packet.chipid == chip.chip_id
+    assert packet.register_address == 5
+    assert packet.register_data == 16
+
+    packet = packets[40]
+    assert packet.packet_type == packet_type
+    assert packet.chipid == chip.chip_id
+    assert packet.register_address == 40
+    assert packet.register_data == 255
+
+def test_packet_init_default():
+    p = Packet()
+    expected = BitArray(Packet.size)
+    assert p.bits == expected
+
+def test_packet_init_bytestream():
+    bytestream = b'\x3f' + b'\x00' * (Packet.size//8-1) + b'\x3e'
+    p = Packet(bytestream)
+    expected = BitArray(Packet.size)
+    expected[-6:] = [1]*6
+    expected[:5] = [1]*5
+    assert p.bits == expected
+
+def test_packet_bytes_zeros():
+    p = Packet()
+    b = p.bytes()
+    expected = b'\x00' * ((Packet.size + len(p._bit_padding))//8)
+    assert b == expected
+
+def test_packet_bytes_custom():
+    p = Packet()
+    p.bits[-6:] = [1]*6  # First byte is 0b00111111
+    p.bits[:5] = [1]*5  # Last byte is 0b00111110 (2 MSBs are padding)
+    b = p.bytes()
+    expected = b'\x3f' + b'\x00' * (Packet.size//8-1) + b'\x3e'
+    assert b == expected
+
+def test_packet_bytes_properties():
+    p = Packet()
+    p.packet_type = Packet.DATA_PACKET
+    p.chipid = 100
+    expected = b'\x91\x01' + b'\x00' * (Packet.size//8-1)
+    b = p.bytes()
+    assert b == expected
+
+def test_packet_set_packet_type():
+    p = Packet()
+    p.packet_type = Packet.DATA_PACKET
+    packet_bits = p.bits[Packet.packet_type_bits]
+    expected = Packet.DATA_PACKET
+    assert packet_bits == expected
+
+def test_packet_get_packet_type():
+    p = Packet()
+    p.packet_type = Packet.CONFIG_WRITE_PACKET
+    packet_type = p.packet_type
+    expected = Packet.CONFIG_WRITE_PACKET
+    assert packet_type == expected
+
+def test_packet_set_chipid():
+    p = Packet()
+    p.chipid = 121
+    expected = BitArray('uint:8=121')
+    assert p.bits[Packet.chipid_bits] == expected
+
+def test_packet_get_chipid():
+    p = Packet()
+    p.chipid = 18
+    expected = 18
+    assert p.chipid == expected
+
+def test_packet_set_parity_bit_value():
+    p = Packet()
+    p.parity_bit_value = 0
+    assert p.bits[0] == False
+    p.parity_bit_value = 1
+    assert p.bits[0] == True
+
+def test_packet_get_parity_bit_value():
+    p = Packet()
+    p.parity_bit_value = 0
+    assert p.parity_bit_value == 0
+    p.parity_bit_value = 1
+    assert p.parity_bit_value == 1
+
+def test_packet_compute_parity():
+    p = Packet()
+    p.chipid = 121
+    parity = p.compute_parity()
+    expected = 0
+    assert parity == expected
+
+def test_packet_assign_parity():
+    p = Packet()
+    p.chipid = 121
+    p.assign_parity()
+    expected = 0
+    assert p.parity_bit_value == expected
+    p.chipid = 0
+    p.assign_parity()
+    expected = 1
+    assert p.parity_bit_value == expected
+
+def test_packet_set_register_address():
+    p = Packet()
+    p.register_address = 121
+    expected = BitArray('uint:8=121')
+    assert p.bits[Packet.register_address_bits] == expected
+
+def test_packet_get_register_address():
+    p = Packet()
+    p.register_address = 18
+    expected = 18
+    assert p.register_address == expected
+
+def test_packet_set_register_data():
+    p = Packet()
+    p.register_data = 1
+    expected = BitArray('uint:8=1')
+    assert p.bits[Packet.register_data_bits] == expected
+
+def test_packet_get_register_data():
+    p = Packet()
+    p.register_data = 18
+    expected = 18
+    assert p.register_data == expected
+
+def test_configuration_trim_threshold_data():
+    c = Configuration()
+    expected = BitArray('0x10')
+    assert c.trim_threshold_data(0) == expected
+
+def test_configuration_global_threshold_data():
+    c = Configuration()
+    expected = BitArray('0x10')
+    assert c.global_threshold_data() == expected
+
+def test_configuration_csa_gain_and_bypasses_data():
+    c = Configuration()
+    expected = BitArray('0b00001001')
+    assert c.csa_gain_and_bypasses_data() == expected
+
+def test_configuration_csa_bypass_select_data():
+    c = Configuration()
+    c.csa_bypass_select[4] = 1
+    expected = BitArray('0b00010000')
+    assert c.csa_bypass_select_data(0) == expected
+    c.csa_bypass_select[10] = 1
+    expected = BitArray('0b00000100')
+    assert c.csa_bypass_select_data(1) == expected
+    c.csa_bypass_select[20] = 1
+    expected = BitArray('0b00010000')
+    assert c.csa_bypass_select_data(2) == expected
+    c.csa_bypass_select[30] = 1
+    expected = BitArray('0b01000000')
+    assert c.csa_bypass_select_data(3) == expected
+
+def test_configuration_csa_monitor_select_data():
+    c = Configuration()
+    c.csa_monitor_select[4] = 0
+    expected = BitArray('0b11101111')
+    assert c.csa_monitor_select_data(0) == expected
+    c.csa_monitor_select[10] = 0
+    expected = BitArray('0b11111011')
+    assert c.csa_monitor_select_data(1) == expected
+    c.csa_monitor_select[20] = 0
+    expected = BitArray('0b11101111')
+    assert c.csa_monitor_select_data(2) == expected
+    c.csa_monitor_select[30] = 0
+    expected = BitArray('0b10111111')
+    assert c.csa_monitor_select_data(3) == expected
+
+def test_configuration_csa_testpulse_enable_data():
+    c = Configuration()
+    c.csa_testpulse_enable[4] = 1
+    expected = BitArray('0b00010000')
+    assert c.csa_testpulse_enable_data(0) == expected
+    c.csa_testpulse_enable[10] = 1
+    expected = BitArray('0b00000100')
+    assert c.csa_testpulse_enable_data(1) == expected
+    c.csa_testpulse_enable[20] = 1
+    expected = BitArray('0b00010000')
+    assert c.csa_testpulse_enable_data(2) == expected
+    c.csa_testpulse_enable[30] = 1
+    expected = BitArray('0b01000000')
+    assert c.csa_testpulse_enable_data(3) == expected
+
+def test_configuration_csa_testpulse_dac_amplitude_data():
+    c = Configuration()
+    c.csa_testpulse_dac_amplitude = 200;
+    expected = BitArray('0b11001000')
+    assert c.csa_testpulse_dac_amplitude_data() == expected
+
+def test_configuration_test_mode_xtrig_reset_diag_data():
+    c = Configuration()
+    c.test_mode = 2
+    c.fifo_diagnostic = 1
+    expected = BitArray('0b00010010')
+    assert c.test_mode_xtrig_reset_diag_data() == expected
+
+def test_configuration_sample_cycles_data():
+    c = Configuration()
+    c.sample_cycles = 221
+    expected = BitArray('0b11011101')
+    assert c.sample_cycles_data() == expected
+
+def test_configuration_test_burst_length_data():
+    c = Configuration()
+    expected = BitArray('0xFF')
+    assert c.test_burst_length_data(0) == expected
+    expected = BitArray('0x00')
+    assert c.test_burst_length_data(1) == expected
+
+def test_configuration_adc_burst_length_data():
+    c = Configuration()
+    c.adc_burst_length = 140
+    expected = BitArray('0b10001100')
+    assert c.adc_burst_length_data() == expected
+
+def test_configuration_channel_mask_data():
+    c = Configuration()
+    c.channel_mask[4] = 1
+    expected = BitArray('0b00010000')
+    assert c.channel_mask_data(0) == expected
+    c.channel_mask[10] = 1
+    expected = BitArray('0b00000100')
+    assert c.channel_mask_data(1) == expected
+    c.channel_mask[20] = 1
+    expected = BitArray('0b00010000')
+    assert c.channel_mask_data(2) == expected
+    c.channel_mask[30] = 1
+    expected = BitArray('0b01000000')
+    assert c.channel_mask_data(3) == expected
+
+def test_configuration_external_trigger_mask_data():
+    c = Configuration()
+    c.external_trigger_mask[4] = 0
+    expected = BitArray('0b11101111')
+    assert c.external_trigger_mask_data(0) == expected
+    c.external_trigger_mask[10] = 0
+    expected = BitArray('0b11111011')
+    assert c.external_trigger_mask_data(1) == expected
+    c.external_trigger_mask[20] = 0
+    expected = BitArray('0b11101111')
+    assert c.external_trigger_mask_data(2) == expected
+    c.external_trigger_mask[30] = 0
+    expected = BitArray('0b10111111')
+    assert c.external_trigger_mask_data(3) == expected
+
+def test_configuration_reset_cycles_data():
+    c = Configuration()
+    c.reset_cycles = 0xabcdef
+    expected = BitArray('0xef')
+    assert c.reset_cycles_data(0) == expected
+    expected = BitArray('0xcd')
+    assert c.reset_cycles_data(1) == expected
+    expected = BitArray('0xab')
+    assert c.reset_cycles_data(2) == expected
+
+def test_controller_format_UART():
+    controller = Controller(None)
+    chip = Chip(2, 4)
+    packet = chip.get_configuration_packets(Packet.CONFIG_READ_PACKET)[10]
+    result = controller.format_UART(chip, packet)
+    expected = b'\x73\x04' + packet.bytes() + b'\x71'
+    assert result == expected
+
+def test_controller_format_bytestream():
+    controller = Controller(None)
+    chip = Chip(2, 4)
+    packets = chip.get_configuration_packets(Packet.CONFIG_READ_PACKET)
+    fpackets = [controller.format_UART(chip, p) for p in packets]
+    result = controller.format_bytestream(fpackets[:1])
+    assert result == fpackets[:1]
+    result = controller.format_bytestream(fpackets[:2])
+    assert result == [b''.join(fpackets[:2])]
+    result = controller.format_bytestream(fpackets[:1]*2000)
+    expected = []
+    expected.append(b''.join(fpackets[:1]*819))
+    expected.append(b''.join(fpackets[:1]*819))
+    expected.append(b''.join(fpackets[:1]*362))
+    assert result == expected
+
+def test_controller_write_configuration():
+    controller = Controller(None)
+    controller._test_mode = True
+    chip = Chip(2, 4)
+    result = controller.write_configuration(chip, 0)
+    conf_data = chip.get_configuration_packets(Packet.CONFIG_WRITE_PACKET)[0]
+    expected = [controller.format_UART(chip, conf_data)]
+    assert result == expected
