91460f90dbccd281b4c8443f53754bdea91c1fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:08 +0000

    Add package fireant/0.19.0/fireant-0.19.0.tar.gz

---
+++ packages/fireant/fireant-0.19.0.tar.gz/fireant-0.19.0/fireant/dashboards/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+from .schemas import (WidgetGroup, LineChartWidget,
+                      AreaChartWidget, AreaPercentageChartWidget,
+                      BarChartWidget, StackedBarChartWidget,
+                      ColumnChartWidget, StackedColumnChartWidget,
+                      PieChartWidget,
+                      RowIndexTableWidget, ColumnIndexTableWidget)
