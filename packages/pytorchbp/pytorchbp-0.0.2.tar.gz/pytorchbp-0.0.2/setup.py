58d4e6de45acce507a2067f295b78534205cf4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:31 +0000

    Add package pytorchbp/0.0.2/pytorchbp-0.0.2.tar.gz

---
+++ packages/pytorchbp/pytorchbp-0.0.2.tar.gz/pytorchbp-0.0.2/setup.py
@@ -0,0 +1,27 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+from os.path import join, dirname
+from setuptools import setup
+
+def read(fname):
+    return open(join(dirname(__file__), fname)).read()
+
+setup(
+    name = "pytorchbp",
+    version = "0.0.2",
+    author = "John Prothero",
+    author_email = "john.s.prothero@gmail.com",
+    description = "Pytorch Boilerplate Code",
+    license = "MIT",
+    keywords = "pytorch boiler plate boilerplate",
+    url = "https://github.com/jprothero/pytorchbp",
+    packages = ['pytorchbp'],
+    long_description = read('README'),
+    classifiers = [],
+)
+
