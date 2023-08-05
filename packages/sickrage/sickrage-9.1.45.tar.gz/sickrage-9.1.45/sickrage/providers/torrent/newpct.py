82349ab7bd35ffc9df4040e33eaa96fb7b559ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:35 +0000

    Add package sickrage/9.1.45/sickrage-9.1.45.tar.gz

---
+++ packages/sickrage/sickrage-9.1.45.tar.gz/sickrage-9.1.45/sickrage/providers/torrent/newpct.py
@@ -0,0 +1,191 @@
+# coding=utf-8
+# Author: CristianBB
+# Greetings to Mr. Pine-apple
+#
+# URL: https://sickrage.ca
+#
+# This file is part of SickRage.
+#
+# SickRage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SickRage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import re
+from urllib import urlencode
+
+import sickrage
+from sickrage.core.caches.tv_cache import TVCache
+from sickrage.core.helpers import bs4_parser, convert_size, try_int
+from sickrage.providers import TorrentProvider
+
+
+class newpctProvider(TorrentProvider):
+    def __init__(self):
+        super(newpctProvider, self).__init__("Newpct", 'http://www.newpct.com', False)
+
+        self.onlyspasearch = None
+
+        self.cache = TVCache(self, min_time=20)
+
+        self.urls.update({
+            'search': '{base_url}/index.php'.format(**self.urls)
+        })
+
+    def search(self, search_strings, age=0, ep_obj=None):
+        results = []
+
+        search_params = {
+            'l': 'doSearch',
+            'q': '',
+            'category_': 'All',
+            'idioma_': 1,
+            'bus_de_': 'All'
+        }
+
+        lang_info = '' if not ep_obj or not ep_obj.show else ep_obj.show.lang
+
+        for mode in search_strings.keys():
+            sickrage.srCore.srLogger.debug("Search Mode: %s" % mode)
+
+            if self.onlyspasearch:
+                search_params['idioma_'] = 1
+            else:
+                search_params['idioma_'] = 'All'
+
+            # Only search if user conditions are true
+            if self.onlyspasearch and lang_info != 'es' and mode != 'RSS':
+                sickrage.srCore.srLogger.debug("Show info is not spanish, skipping provider search")
+                continue
+
+            search_params['bus_de_'] = 'All' if mode != 'RSS' else 'semana'
+
+            for search_string in search_strings[mode]:
+                search_params['q'] = search_string.strip()
+
+                sickrage.srCore.srLogger.debug(
+                    "Search URL: %s" % self.urls['search'] + '?' + urlencode(search_params))
+
+                try:
+                    data = sickrage.srCore.srWebSession.post(self.urls['search'], data=search_params, timeout=30).text
+                except Exception:
+                    continue
+
+                try:
+                    with bs4_parser(data) as html:
+                        torrent_table = html.find('table', id='categoryTable')
+                        torrent_rows = torrent_table('tr') if torrent_table else []
+
+                        if len(torrent_rows) < 3:
+                            sickrage.srCore.srLogger.debug("Data returned from provider does not contain any torrents")
+                            continue
+
+                        for row in torrent_rows[1:-1]:
+                            try:
+                                cells = row('td')
+
+                                torrent_row = row.find('a')
+                                download_url = torrent_row.get('href', '')
+                                title = self._processTitle(torrent_row.get('title', ''), download_url)
+                                if not all([title, download_url]):
+                                    continue
+
+                                # Provider does not provide seeders/leechers
+                                seeders = 1
+                                leechers = 0
+                                torrent_size = cells[2].get_text(strip=True)
+
+                                size = convert_size(torrent_size, -1)
+                                item = {'title': title, 'link': download_url, 'size': size, 'seeders': seeders,
+                                        'leechers': leechers, 'hash': ''}
+
+                                if mode != 'RSS':
+                                    sickrage.srCore.srLogger.debug('Found result: {}'.format(title))
+
+                                results.append(item)
+                            except (AttributeError, TypeError):
+                                continue
+
+                except Exception:
+                    sickrage.srCore.srLogger.error("Failed parsing provider.")
+
+        results.sort(key=lambda k: try_int(k.get('seeders', 0)), reverse=True)
+
+        return results
+
+    @staticmethod
+    def _processTitle(title, url):
+        # Remove 'Mas informacion sobre ' literal from title
+        title = title[22:]
+        title = re.sub(r'[ ]{2,}', ' ', title, flags=re.I)
+
+        # Quality - Use re module to avoid case sensitive problems with replace
+        title = re.sub(r'\[HDTV 1080p?[^\[]*]', '1080p HDTV x264', title, flags=re.I)
+        title = re.sub(r'\[HDTV 720p?[^\[]*]', '720p HDTV x264', title, flags=re.I)
+        title = re.sub(r'\[ALTA DEFINICION 720p?[^\[]*]', '720p HDTV x264', title, flags=re.I)
+        title = re.sub(r'\[HDTV]', 'HDTV x264', title, flags=re.I)
+        title = re.sub(r'\[DVD[^\[]*]', 'DVDrip x264', title, flags=re.I)
+        title = re.sub(r'\[BluRay 1080p?[^\[]*]', '1080p BluRay x264', title, flags=re.I)
+        title = re.sub(r'\[BluRay Rip 1080p?[^\[]*]', '1080p BluRay x264', title, flags=re.I)
+        title = re.sub(r'\[BluRay Rip 720p?[^\[]*]', '720p BluRay x264', title, flags=re.I)
+        title = re.sub(r'\[BluRay MicroHD[^\[]*]', '1080p BluRay x264', title, flags=re.I)
+        title = re.sub(r'\[MicroHD 1080p?[^\[]*]', '1080p BluRay x264', title, flags=re.I)
+        title = re.sub(r'\[BLuRay[^\[]*]', '720p BluRay x264', title, flags=re.I)
+        title = re.sub(r'\[BRrip[^\[]*]', '720p BluRay x264', title, flags=re.I)
+        title = re.sub(r'\[BDrip[^\[]*]', '720p BluRay x264', title, flags=re.I)
+
+        #detect hdtv/bluray by url
+        #hdtv 1080p example url: http://www.newpct.com/descargar-seriehd/foo/capitulo-610/hdtv-1080p-ac3-5-1/
+        #hdtv 720p example url: http://www.newpct.com/descargar-seriehd/foo/capitulo-26/hdtv-720p-ac3-5-1/
+        #hdtv example url: http://www.newpct.com/descargar-serie/foo/capitulo-214/hdtv/
+        #bluray compilation example url: http://www.newpct.com/descargar-seriehd/foo/capitulo-11/bluray-1080p/
+        title_hdtv = re.search(r'HDTV', title, flags=re.I)
+        title_720p = re.search(r'720p', title, flags=re.I)
+        title_1080p = re.search(r'1080p', title, flags=re.I)
+        title_x264 = re.search(r'x264', title, flags=re.I)
+        title_bluray = re.search(r'bluray', title, flags=re.I)
+        title_serie_hd = re.search(r'descargar-seriehd', title, flags=re.I)
+        url_hdtv = re.search(r'HDTV', url, flags=re.I)
+        url_720p = re.search(r'720p', url, flags=re.I)
+        url_1080p = re.search(r'1080p', url, flags=re.I)
+        url_bluray = re.search(r'bluray', url, flags=re.I)
+
+        if not title_hdtv and url_hdtv:
+            title += ' HDTV'
+            if not title_x264:
+                title += ' x264'
+        if not title_bluray and url_bluray:
+            title += ' BluRay'
+            if not title_x264:
+                title += ' x264'
+        if not title_1080p and url_1080p:
+            title += ' 1080p'
+            title_1080p = True
+        if not title_720p and url_720p:
+            title += ' 720p'
+            title_720p = True
+        if not (title_720p or title_1080p) and title_serie_hd:
+            title += ' 720p'
+
+        # Language
+        title = re.sub(r'\[Spanish[^\[]*]', 'SPANISH AUDIO', title, flags=re.I)
+        title = re.sub(r'\[Castellano[^\[]*]', 'SPANISH AUDIO', title, flags=re.I)
+        title = re.sub(r'\[Español[^\[]*]', 'SPANISH AUDIO', title, flags=re.I)
+        title = re.sub(r'\[AC3 5\.1 Español[^\[]*]', 'SPANISH AUDIO', title, flags=re.I)
+
+        if re.search(r'\[V.O.[^\[]*]', title, flags=re.I):
+            title += '-NEWPCTVO'
+        else:
+            title += '-NEWPCT'
+
+        return title.strip()
---
+++ packages/sickrage/sickrage-9.1.45.tar.gz/sickrage-9.1.45/sickrage/providers/torrent/t411.py
@@ -0,0 +1,176 @@
+# Author: echel0n <echel0n@sickrage.ca>
+# URL: https://sickrage.ca
+#
+# This file is part of SickRage.
+#
+# SickRage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SickRage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import time
+
+from requests.auth import AuthBase
+
+import sickrage
+from sickrage.core.caches.tv_cache import TVCache
+from sickrage.core.helpers import try_int
+from sickrage.providers import TorrentProvider
+
+
+class T411Provider(TorrentProvider):
+    def __init__(self):
+        super(T411Provider, self).__init__("T411", 'http://www.t411.al', True)
+
+        self.urls.update({
+            'search': '{base_url}/torrents/search/%s?cid=%s&limit=100'.format(**self.urls),
+            'rss': '{base_url}/torrents/top/today'.format(**self.urls),
+            'login': '{base_url}/auth'.format(**self.urls),
+            'download': '{base_url}/torrents/download/%s'.format(**self.urls)
+        })
+
+        self.username = None
+        self.password = None
+
+        self.token = None
+        self.tokenLastUpdate = None
+
+        self.subcategories = [433, 637, 455, 639]
+
+        self.minseed = 0
+        self.minleech = 0
+        self.confirmed = False
+
+        self.cache = TVCache(self, min_time=10)
+
+    def login(self):
+
+        if self.token is not None:
+            if time.time() < (self.tokenLastUpdate + 30 * 60):
+                return True
+
+        login_params = {'username': self.username,
+                        'password': self.password}
+
+        try:
+            response = sickrage.srCore.srWebSession.post(self.urls['login'], data=login_params, timeout=30,
+                                                         auth=T411Auth(self.token)).json()
+        except Exception:
+            sickrage.srCore.srLogger.warning("[{}]: Unable to connect to provider".format(self.name))
+            return False
+
+        if 'token' in response:
+            self.token = response['token']
+            self.tokenLastUpdate = time.time()
+            self.uid = response['uid'].encode('ascii', 'ignore')
+            return True
+        else:
+            sickrage.srCore.srLogger.warning("Token not found in authentication response")
+            return False
+
+    def search(self, search_params, age=0, ep_obj=None):
+        results = []
+
+        if not self.login():
+            return results
+
+        for mode in search_params.keys():
+            sickrage.srCore.srLogger.debug("Search Mode: %s" % mode)
+            for search_string in search_params[mode]:
+
+                if mode != 'RSS':
+                    sickrage.srCore.srLogger.debug("Search string: %s " % search_string)
+
+                searchURLS = \
+                    ([self.urls['search'] % (search_string, u) for u in self.subcategories], [self.urls['rss']])[
+                        mode == 'RSS']
+                for searchURL in searchURLS:
+                    sickrage.srCore.srLogger.debug("Search URL: %s" % searchURL)
+
+                    try:
+                        data = sickrage.srCore.srWebSession.get(searchURL, auth=T411Auth(self.token),
+                                                                cache=False).json()
+                    except Exception:
+                        sickrage.srCore.srLogger.debug("No data returned from provider")
+                        continue
+
+                    try:
+                        if 'torrents' not in data and mode != 'RSS':
+                            sickrage.srCore.srLogger.debug("Data returned from provider does not contain any torrents")
+                            continue
+
+                        torrents = data['torrents'] if mode != 'RSS' else data
+
+                        if not torrents:
+                            sickrage.srCore.srLogger.debug("Data returned from provider does not contain any torrents")
+                            continue
+
+                        for torrent in torrents:
+                            if mode == 'RSS' and int(torrent['category']) not in self.subcategories:
+                                continue
+
+                            try:
+                                title = torrent['name']
+                                torrent_id = torrent['id']
+                                download_url = (self.urls['download'] % torrent_id).encode('utf8')
+                                if not all([title, download_url]):
+                                    continue
+
+                                size = int(torrent['size'])
+                                seeders = int(torrent['seeders'])
+                                leechers = int(torrent['leechers'])
+                                verified = bool(torrent['isVerified'])
+
+                                # Filter unseeded torrent
+                                if seeders < self.minseed or leechers < self.minleech:
+                                    if mode != 'RSS':
+                                        sickrage.srCore.srLogger.debug(
+                                            "Discarding torrent because it doesn't meet the minimum seeders or leechers: {0} (S:{1} L:{2})".format(
+                                                title, seeders, leechers))
+                                    continue
+
+                                if self.confirmed and not verified and mode != 'RSS':
+                                    sickrage.srCore.srLogger.debug(
+                                        "Found result " + title + " but that doesn't seem like a verified result so I'm ignoring it")
+                                    continue
+
+                                item = {'title': title, 'link': download_url, 'size': size, 'seeders': seeders,
+                                        'leechers': leechers, 'hash': ''}
+
+                                if mode != 'RSS':
+                                    sickrage.srCore.srLogger.debug("Found result: {}".format(title))
+
+                                results.append(item)
+                            except Exception:
+                                sickrage.srCore.srLogger.debug("Invalid torrent data, skipping result: %s" % torrent)
+                                sickrage.srCore.srLogger.error("Failed parsing provider.")
+                                continue
+                    except Exception:
+                        sickrage.srCore.srLogger.error("Failed parsing provider.")
+
+        # Sort all the items by seeders if available
+        results.sort(key=lambda k: try_int(k.get('seeders', 0)), reverse=True)
+
+        return results
+
+
+
+class T411Auth(AuthBase):
+    """Attaches HTTP Authentication to the given Request object."""
+
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers['Authorization'] = self.token
+        return r
