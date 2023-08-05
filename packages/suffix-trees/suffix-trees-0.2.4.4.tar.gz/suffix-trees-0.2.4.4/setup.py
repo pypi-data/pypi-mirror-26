3a39e694d99e51caa8fa33455b34f54725e3e379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:22 +0000

    Add package suffix-trees/0.2.4.4/suffix-trees-0.2.4.4.tar.gz

---
+++ packages/suffix-trees/suffix-trees-0.2.4.4.tar.gz/suffix-trees-0.2.4.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='suffix-trees',
+    packages=['suffix_trees'],
+    version='0.2.4.4',
+    description='Suffix trees, generalized suffix trees and string processing methods',
+    author='Peter Us',
+    author_email='ptrusr@gmail.com',
+    url='https://github.com/ptrus/suffix-trees',
+    long_description=open('README.rst').read(),
+    package_data={},
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+)
