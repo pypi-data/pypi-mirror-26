e1b98e794887a5863f61d077dc583ef9db562673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:30 +0000

    Add package gybnester2/1.1.0/gybnester2-1.1.0.zip

---
+++ packages/gybnester2/gybnester2-1.1.0.zip/gybnester2-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name             = 'gybnester2',
+    version          = '1.1.0',
+    py_modules       = ['gybnester2'],
+    author           = 'gyb',
+    author_email     = '1970264568@qq.com',
+    url              = 'http://www.headfirstlabs.com',
+    description      = 'A simple printer of nested lists',
+    )
+    
