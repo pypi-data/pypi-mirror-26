1900e1db25f1db61e530a6611f5a4953e941f0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:47 +0000

    Add package qrl/0.54.5/qrl-0.54.5.tar.gz

---
+++ packages/qrl/qrl-0.54.5.tar.gz/qrl-0.54.5/qrl/core/Transaction_subtypes.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+# Distributed under the MIT software license, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+# FIXME: This is moving to protobuf enums
+
+TX_SUBTYPE_TX = 1
+TX_SUBTYPE_STAKE = 2
+TX_SUBTYPE_DESTAKE = 3
+TX_SUBTYPE_COINBASE = 4
+TX_SUBTYPE_LATTICE = 5
+TX_SUBTYPE_DUPLICATE = 6
