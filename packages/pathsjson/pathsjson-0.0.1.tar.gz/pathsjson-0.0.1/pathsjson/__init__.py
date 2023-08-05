d488f8c1d6662f8ada170be34c8a18ee41112524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:00 +0000

    Add package pathsjson/0.0.1/pathsjson-0.0.1.tar.gz

---
+++ packages/pathsjson/pathsjson-0.0.1.tar.gz/pathsjson-0.0.1/pathsjson/__init__.py
@@ -0,0 +1,11 @@
+__title__ = "pathsjson"
+__description__ = ""
+__uri__ = "https://github.com/jbn/pathsjson"
+__doc__ = __description__ + " <" + __uri__ + ">"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2017 John Bjorn Nelson"
+__version__ = "0.0.1"
+__author__ = "John Bjorn Nelson"
+__email__ = "jbn@falsifiable.com"
+
+from pathsjson.impl import PathsJSON
