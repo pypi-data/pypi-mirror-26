4d8378f9075b74d6a4e124d1495471848a3f6e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:28 +0000

    Add package torchlight/0.0.1/torchlight-0.0.1.zip

---
+++ packages/torchlight/torchlight-0.0.1.zip/torchlight-0.0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='torchlight',
+    version='0.0.1',
+    packages=['torchlight'],
+    url='',
+    license='MIT',
+    description='Torch Light',
+    install_requires=[
+    ],
+    extras_require={
+    },
+)
