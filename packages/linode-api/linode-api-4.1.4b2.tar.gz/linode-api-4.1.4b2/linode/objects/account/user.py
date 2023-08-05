dae355dabbcf795fbcaf5a04fc58c05dfe576023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:31 +0000

    Add package linode-api/4.1.4b2/linode-api-4.1.4b2.tar.gz

---
+++ packages/linode-api/linode-api-4.1.4b2.tar.gz/linode-api-4.1.4b2/linode/objects/account/user.py
@@ -0,0 +1,36 @@
+from linode.objects import Base, Property
+
+class User(Base):
+    api_endpoint = "/account/users/{id}"
+    id_attribute = 'username'
+
+    properties = {
+        'email': Property(mutable=True),
+        'username': Property(identifier=True, mutable=True),
+        'restricted': Property(mutable=True),
+    }
+
+    @property
+    def grants(self):
+        from linode.objects.account import UserGrants
+        if not hasattr(self, '_grants'):
+            resp = self._client.get(UserGrants.api_endpoint.format(username=self.username))
+
+            grants = UserGrants(self._client, self.username, resp)
+            self._set('_grants', grants)
+
+        return self._grants
+
+    def invalidate(self):
+        if hasattr(self, '_grants'):
+            del self._grants
+        Base.invalidate(self)
+
+    def change_password(self, password):
+        """
+        Sets this user's password
+        """
+        result = self._client.post('{}/password'.format(User.api_endpoint),
+                model=self, data={ "password": password })
+
+        return True
