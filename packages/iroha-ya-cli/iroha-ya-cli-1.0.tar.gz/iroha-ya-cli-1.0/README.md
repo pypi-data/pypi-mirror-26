a1ea5833bd745e28368cd1f07f5c9ccac55574dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:54 +0000

    Add package iroha-ya-cli/1.0/iroha-ya-cli-1.0.tar.gz

---
+++ packages/iroha-ya-cli/iroha-ya-cli-1.0.tar.gz/iroha-ya-cli-1.0/README.md
@@ -0,0 +1,166 @@
+
+## YAC: Yet Another Cli of Iroha
+[![CircleCI](https://circleci.com/gh/MizukiSonoko/iroha-cli.svg?style=shield)](https://circleci.com/gh/MizukiSonoko/iroha-cli)
+
+[![PyPI version](https://badge.fury.io/py/iroha-ya-cli.svg)](https://badge.fury.io/py/iroha-ya-cli)
+
+[hyperledger/iroha](https://github.com/hyperledger/iroha) is open source, I want to contribute to it...  
+
+## Suppert version  
+
+- python2.7 => ToDo
+- python3.4 => ToDo
+- python3.5 => ⚪️ 
+- python3.6 => ToDo
+
+
+## Simple install
+
+```
+$ pip install iroha-ya-cli
+```
+
+## How to use
+
+#### 0) Make `keypair` and `config.yml` in this.
+```
+$ iroha-ya-cli keygen --account_name mizuki --make_conf
+File not found : mizuki.pub or mizuki.pri 
+Without config mode
+Generate conf.yml!
+success full
+```
+
+Result
+```
+$ ls -lth | head  -n 4
+total 424
+-rw-r--r--   1 mizuki  staff   122B Oct 19 13:45 config.yml
+-r--------   1 mizuki  staff    64B Oct 19 13:45 mizuki.pri
+-r--------   1 mizuki  staff    64B Oct 19 13:45 mizuki.pub
+```
+
+
+#### 1) Change information in generated `config.yml` 
+This file contains target peer information and my account info.
+```config
+account:
+  name: mizuki
+  privateKeyPath: mizuki.pri
+  publicKeyPath: mizuki.pub
+peer:
+  address: localhost
+  port: 50051
+```
+Peer is target where cli send tx to. I guess Iroha uses grpc, so port is 50051.
+
+####  2) Check config is whether correct or not using `config` command. 
+
+```
+$ iroha-ya-cli config
+
+  Config  
+ =========
+
+ name      : mizuki
+ publicKey : be98f280f22572686cbac3977f85be12176d72d45067ade75d81f6b5b626c138
+ privateKey: 908ab**...**cfcff
+ load from : config.yml
+ targetPeer: localhost:50051
+
+success full
+```
+
+#### 3) Send tx like this
+
+```
+$ iroha-ya-cli CreateAsset --domain_id japan --precision 0 --asset_name yen
+generated command: create_asset {
+  asset_name: "yen"
+  domain_id: "japan"
+}
+
+== Grpc happens error ==
+- Server is active?: False 
+- What's happen?   : Connect Failed 
+
+Transaction is not arrived...
+Could you ckeck this => localhost:50051
+
+failed
+```
+I sent. (This error is expected, no problem 😅 😅 )
+
+#### 4) You can know optin using `-h` or `--help` command.
+
+In first, you can see all command use this.
+```
+iroha-ya-cli
+```
+
+In second, you can see detail of each command.
+```
+$ iroha-ya-cli CreateAsset -h
+Create new asset in domain
+-----------
+Arguments
+- precision : how much support .000, default 0
+- domain_id : new account will be in this domain like japan
+- asset_name: asset name like mizuki
+```
+ 
+## Sample
+
+- CreateAsset 
+```
+iroha-ya-cli CreateAsset --domain_id japan --precision 0 --asset_name yen
+```
+
+- CreateAccount
+
+```
+iroha-ya-cli CreateAccount --account_name mizuki --domain_id japan
+```
+
+- CreateDomain
+
+```
+iroha-ya-cli CreateDomain --domain_name aizu
+```
+
+....
+
+
+## Test
+```
+python -m unittest discover
+```
+
+## Develop
+```
+# Compile protofile 
+git clone https://github.com/MizukiSonoko/iroha-cli.git
+cd iroha-cli
+docker run -it -v  $(pwd):/opt/iroha-mizuki-cli mizukisonoko/alpine-grpc-protobuf sh -c  "cd /opt/iroha-mizuki-cli/schema; ls *.proto | xargs -I{} sh -c 'protoc -I=./ --python_out=../ {}; protoc -I=./ --python_out=../ --grpc_out=../ --plugin=protoc-gen-grpc=`which grpc_python_plugin` {}'"
+pip install -r requirements.txt 
+```
+
+
+
+
+## Env
+- protobuf + grpc [mizukisonoko/alpine-grpc-protobuf](https://github.com/MizukiSonoko/alpine-grpc-protobuf)
+
+
+
+## Tips
+
+It happens this
+```
+Running PyYAML-3.12/setup.py -q bdist_egg --dist-dir /tmp/easy_install-wwituzoz/PyYAML-3.12/egg-dist-tmp-kgc91i9t
+build/temp.linux-x86_64-3.5/check_libyaml.c:2:18: fatal error: yaml.h: No such file or directory
+compilation terminated.
+
+libyaml is not found or a compiler error: forcing --without-libyaml
+```
+=> `apt install -y libyaml-dev`
---
+++ packages/iroha-ya-cli/iroha-ya-cli-1.0.tar.gz/iroha-ya-cli-1.0/cli/main.py
@@ -0,0 +1,176 @@
+#! /usr/bin/python
+
+import sys
+import readline
+
+from cli.crypto import KeyPair
+from cli.exception import CliException
+from cli.network import generateTransaction, sendTx
+import cli.commands as commands
+import cli.loader as loader
+import cli.completer as completer
+
+BASE_NAME = "iroha-cli"
+
+
+class ChiekuiCli:
+    def __init__(self, commands, printInfo=False):
+        try:
+            res = loader.load(printInfo)
+            self.withoutConf = False
+            self.location = res["location"]
+            self.name = res["name"]
+
+            self.key_pair = KeyPair(raw_private_key=KeyPair.decode(res["privateKey"]), raw_public_key=KeyPair.decode(res["publicKey"]))
+        except CliException as e:
+            print(e.args[0])
+            print("Without config mode")
+            self.withoutConf = True
+
+        self.printInfo = False
+
+        self.commands = commands.commands
+        self.Type = commands.Type
+        commandNames = self.commands.keys()
+        opt = {}
+        for name in commandNames:
+            opt[name] = list(map(lambda name: name, self.commands[name]["option"].keys())) + ["-h"]
+
+        opt["quit"] = []
+        opt["bye"] = []
+        readline.set_completer(completer.ChiekuiCliBufferCompleter(opt).complete)
+        readline.parse_and_bind('tab: complete')
+
+
+    def exec_command(self, cmd, argv):
+        if cmd in self.commands:
+            if "-h" in argv or "--help" in argv:
+                print(self.commands[cmd]["detail"])
+                print("-----------")
+                print("Arguments")
+                for name, opt in self.commands[cmd]["option"].items():
+                    print("- {:10s}: {:6}".format(name, opt["detail"]))
+                return True
+            else:
+                import getopt
+                # I want to make it more simple....
+                expected = list(map(lambda x: x[0] + "=",
+                                list(filter(lambda o: o[1]['type'] != self.Type.NONE,self.commands[cmd]["option"].items()))
+                            ))
+                expected.extend(list(map(lambda x: x[0],
+                                list(filter(lambda o: o[1]['type'] == self.Type.NONE,self.commands[cmd]["option"].items()))
+                )))
+                try:
+                    optlist, _ = getopt.getopt(argv, '', expected)
+                except getopt.GetoptError as e:
+                    print("Maybe option is wrong, I required {}".format(expected))
+                    return False
+
+                new_argv = {}
+                for opt in optlist:
+                    for name in self.commands[cmd]["option"]:
+                        if name == opt[0].split('--')[-1]:
+                            new_argv[name] = opt[1]
+
+                try:
+                    if cmd == "config":
+                        if self.withoutConf:
+                            print("you can show config under loading config, but not loaded config.yml...".format(cmd))
+                            return False
+                        self.commands[cmd]["function"](
+                            {"name":self.name,
+                             "publicKey":self.key_pair.public_key,
+                             "privateKey":self.key_pair.private_key,
+                             "location":self.location
+                            }
+                        )
+                        return True
+
+                    command = self.commands[cmd]["function"](new_argv)
+                    if command:
+                        print("generated command: {}".format(command))
+                        if self.withoutConf:
+                            print("{} is successful, but not loaded config.yml...".format(cmd))
+                            return 0
+                        tx = generateTransaction(self.name, [command],self.key_pair)
+                        if not sendTx(self.location, tx):
+                            print(
+                                "Transaction is not arrived...\n"
+                                "Could you ckeck this => {}\n"
+                                    .format(self.location)
+                            )
+                            return False
+                except CliException as e:
+                    print(e.message)
+                    return False
+                return True
+
+        if cmd.lower() in ["quit", "bye", "finish", "exit", "end"]:
+            print("Thanks bye! (^o^) ")
+            sys.exit(0)
+        return "command not found: {}".format(cmd)
+
+    def run(self):
+        line = ''
+        while line != 'stop':
+            line = None
+            try:
+                line = input('{}@default [{}] >> '.format(self.name, BASE_NAME))
+            except EOFError:
+                print("\nThanks bye! (^o^) ")
+                sys.exit(0)
+            if line:
+                argv = line.split()[1:]
+                res = self.exec_command(line.split()[0], line.split()[1:])
+                if res:
+                    print(res)
+
+
+def handler(signal, frame):
+    print("\nThanks bye! (^o^) ")
+    sys.exit(0)
+
+
+def main():
+    printInfo = False
+    import signal
+    argv = sys.argv
+    cmdList = commands.CommandList()
+    c = ChiekuiCli(cmdList, printInfo)
+    if len(argv) == 2 and argv[-1] == "--interactive":
+        signal.signal(signal.SIGINT, handler)
+        c.run()
+    elif len(argv) >= 2 and argv[-1] != "--interactive":
+        if c.exec_command(argv[1], argv[2:]):
+            print("success full")
+            sys.exit(0)
+        else:
+            print("failed")
+            sys.exit(1)
+    else:
+        print(
+            "----------------\n"
+            "Iroha-mizuki-cli\n"
+            "----------------\n\n"
+            "Current support commands"
+        )
+        for cmd in cmdList.commands.keys():
+            print("  - {}".format(cmd))
+
+        print(
+            "\n"
+            "Sample:\n\n"
+            "  > python ./cli.py CreateAsset --domain_id japan --precision 0 --asset_name yen"
+        )
+        print("\n")
+
+
+if __name__ == "__main__":
+    import logging
+
+    LOG_FILENAME = '/tmp/{}_cli.log'.format(BASE_NAME)
+    logging.basicConfig(
+        filename=LOG_FILENAME,
+        level=logging.DEBUG,
+    )
+    main()
