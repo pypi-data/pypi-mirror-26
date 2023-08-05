8a2f0c83139abb352709c714fcce10954ee0cab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:57 +0000

    Add package glossarist/1.0.1/glossarist-1.0.1-py3-none-any.whl

---
+++ packages/glossarist/glossarist-1.0.1-py3-none-any.whl/glossarist/utils.py
@@ -0,0 +1,172 @@
+from collections import namedtuple
+from dateutil.parser import parse
+import os
+import json
+
+import arrow
+from wordnik import *
+from tabulate import tabulate
+
+from .errors import ConfigFileError
+
+API_URL = 'http://api.wordnik.com/v4'
+
+WordData = namedtuple('WordData', ['word', 'created_at'])
+
+def create_word_api(API_KEY):
+    client = swagger.ApiClient(API_KEY, API_URL)
+    wordApi = WordApi.WordApi(client)
+    return wordApi
+
+
+HOME_DIR = os.path.expanduser('~') # User's Home Directory
+# Base Directory to store all data related to the application.
+BASE_DIR = os.path.join(HOME_DIR, '.glossarist')
+CONFIG_FILE = os.path.join(BASE_DIR, 'config.json')
+WORDS_FILE = os.path.join(BASE_DIR, 'words.json')
+DB_FILE = os.path.join(BASE_DIR, 'data.json')
+
+def save_api_key(api_key):
+    '''Saves or updates the Wordnik API key in the configuration file.'''
+
+    is_initialized = check_initialization()
+    if not is_initialized:
+        os.mkdir(BASE_DIR)
+
+    if not os.path.isfile(CONFIG_FILE):
+        data = {'API_KEY': api_key}
+    else:
+        with open(CONFIG_FILE, 'r') as file:
+            encoded_data = file.read()
+
+        data = json.loads(encoded_data) # Decoding stored JSON to a Python dict
+        # TODO: Prompt the user if an old API key exists
+        data['API_KEY'] = api_key
+
+    encoded_data = json.dumps(data) # Encoding the data to JSON
+
+    # Write data to the Config file
+    with open(CONFIG_FILE, 'w') as file:
+        file.write(encoded_data) # Writing data to the Configuration file
+
+    return True
+
+
+def load_api_key():
+    '''Return the Wordnik API key saved in the configuration file.
+
+    Raises ConfigFileError exception if the file does not exists.
+    '''
+
+    if os.path.isfile(CONFIG_FILE):
+        with open(CONFIG_FILE, 'r') as file:
+            encoded_data = file.read()
+
+        data = json.loads(encoded_data)
+        api_key = data['API_KEY']
+        return api_key
+    else:
+        raise ConfigFileError('The configuration file does not exists.')
+
+def fetch_word(word):
+    from .word import Word
+
+    if os.path.isfile(WORDS_FILE):
+        with open(WORDS_FILE, 'r') as file:
+            encoded_data = file.read()
+
+        data = json.loads(encoded_data)
+            
+        for datapoint in data:
+            if word in datapoint:
+                word_data = lookup_word(word)
+                if word_data is not None:
+                    return Word(**word_data)
+                break
+
+        
+    word_data = {'word': word}
+    return Word(**word_data)
+
+def lookup_word(word):
+    if not os.path.isfile(DB_FILE):
+        return None
+    else:
+        with open(DB_FILE, 'r') as file:
+            encoded_data = file.read()
+
+        data = json.loads(encoded_data)
+
+        for d in data:
+            word_data = json.loads(d)
+            if word_data['word'] == word:
+                return word_data
+
+
+def save_word(word_object):
+    # Save the word data to the database
+    encoded_data = [word_object.jsonify()]
+    if not os.path.isfile(DB_FILE):
+        with open(DB_FILE, 'w') as file:
+            file.write(json.dumps(encoded_data))
+    else:
+        with open(DB_FILE, 'r') as file:
+            data = file.read()
+
+        json_data = json.loads(data)
+        json_data.extend(encoded_data)
+
+        encoded_data = json.dumps(json_data)
+        with open(DB_FILE, 'w') as file:
+            file.write(encoded_data)
+
+    # Add word to lookup table
+    word = word_object.word
+    word_data = WordData(word, arrow.now().for_json())
+
+    if not os.path.isfile(WORDS_FILE):
+        data = [word_data]
+        encoded_data = json.dumps(data)
+
+        with open(WORDS_FILE, 'w') as file:
+            file.write(encoded_data)
+    else:
+        with open(WORDS_FILE, 'r') as file:
+            encoded_data = file.read()
+
+        data = json.loads(encoded_data)
+        data.append(word_data)
+        # TODO: Do not add word if it is already present in my vocabulary.
+        encoded_data = json.dumps(data)
+
+        with open(WORDS_FILE, 'w') as file:
+            file.write(encoded_data)
+
+    return True
+
+def get_words():
+    '''Fetches list of all the words the user has looked up.'''
+
+    if not os.path.isfile(WORDS_FILE):
+        return 'No words are currently present in your glossarist.'
+    else:
+        with open(WORDS_FILE, 'r') as file:
+            encoded_data = file.read()
+
+        data = json.loads(encoded_data)
+        formatted_data = [(word_data[0], arrow.Arrow.fromdatetime(parse(word_data[1])).humanize()) for word_data in data]
+
+        return formatted_data
+
+def tabulate_words(formatted_data):
+    '''Tabulates the given words for user viewing.'''
+
+    return tabulate(formatted_data, headers=['Word', 'Created'])
+
+def check_initialization():
+    '''Checks whether the application has been initilialized for the user or not.
+
+    Returns True if the application folder, along with the configuration file exists.
+    '''
+
+    return os.path.exists(BASE_DIR) and os.path.exists(CONFIG_FILE)
---
+++ packages/glossarist/glossarist-1.0.1-py3-none-any.whl/glossarist/word.py
@@ -0,0 +1,234 @@
+import json
+
+import click
+from wordnik import *
+
+from .utils import create_word_api, load_api_key
+
+class Word(object):
+    def __init__(self, **kwargs):
+        self.word = kwargs.get('word')
+        self._meanings = kwargs.get('_meanings')
+        self._examples = kwargs.get('_examples')
+        self._hyphenation = kwargs.get('_hyphenation')
+        self._audio = kwargs.get('_audio')
+        self._text_pronunciations = kwargs.get('_text_pronunciations')
+        self._phrases = kwargs.get('_phrases')
+        self._synonyms = kwargs.get('_synonyms')
+        self._antonyms = kwargs.get('_antonyms')
+
+    def __repr__(self):
+        return 'Word({})'.format(self.word)
+
+    @property
+    def meanings(self):
+        if self._meanings is None:
+            self._meanings = Word.get_meanings(self.word)
+
+        return self._meanings
+
+    @property
+    def examples(self):
+        if self._examples is None:
+            self._examples = Word.get_examples(self.word)
+
+        return self._examples
+
+    @property
+    def hyphenation(self):
+        if self._hyphenation is None:
+            self._hyphenation = Word.get_hyphenation(self.word)
+
+        return self._hyphenation
+
+    @property
+    def audio(self):
+        if self._audio is None:
+            self._audio = Word.get_audio(self.word)
+
+        return self._audio
+
+    @property
+    def text_pronunciations(self):
+        if self._text_pronunciations is None:
+            self._text_pronunciations = Word.get_text_pronunciations(self.word)
+
+        return self._text_pronunciations
+
+    @property
+    def phrases(self):
+        if self._phrases is None:
+            self._phrases = Word.get_phrases(self.word)
+
+        return self._phrases
+
+    @property
+    def synonyms(self):
+        if self._synonyms is None:
+            self._synonyms = Word.get_synonyms(self.word)
+
+        return self._synonyms
+
+    @property
+    def antonyms(self):
+        if self._antonyms is None:
+            self._antonyms = Word.get_antonyms(self.word)
+
+        return self._antonyms
+
+    @staticmethod
+    def get_meanings(word):
+        API_KEY = load_api_key()
+        WORD_API = create_word_api(API_KEY)
+        definitions = WORD_API.getDefinitions(word, limit=5)
+        data = [definition.text for definition in definitions]
+        return data
+
+    @staticmethod
+    def get_examples(word):
+        API_KEY = load_api_key()
+        WORD_API = create_word_api(API_KEY)
+        examples = WORD_API.getExamples(word, limit=3)
+        return [example.text for example in examples.examples]
+
+    @staticmethod
+    def get_hyphenation(word):
+        API_KEY = load_api_key()
+        WORD_API = create_word_api(API_KEY)
+        hyphenation = WORD_API.getHyphenation(word)
+        if hyphenation is not None:
+            return '-'.join(syllable.text for syllable in hyphenation)
+
+        return hyphenation
+
+    @staticmethod
+    def get_audio(word):
+        API_KEY = load_api_key()
+        WORD_API = create_word_api(API_KEY)
+        audio = WORD_API.getAudio(word, limit=1)
+        # TODO: Provide flexibility in setting limit
+        if audio is not None:
+            return audio[0].fileUrl
+        else:
+            return None
+
+    @staticmethod
+    def get_text_pronunciations(word):
+        API_KEY = load_api_key()
+        WORD_API = create_word_api(API_KEY)
+        pronunciations = WORD_API.getTextPronunciations(word)
+        return [pronunciation.raw for pronunciation in pronunciations
+                                  if pronunciation.rawType != 'arpabet']
+
+    @staticmethod
+    def get_phrases(word):
+        API_KEY = load_api_key()
+        WORD_API = create_word_api(API_KEY)
+        phrases = WORD_API.getPhrases(word)
+        if phrases is not None:
+            return ['{} {}'.format(phrase.gram1, phrase.gram2) for phrase in phrases]
+        else:
+            return phrases
+
+    @staticmethod
+    def get_synonyms(word):
+        API_KEY = load_api_key()
+        WORD_API = create_word_api(API_KEY)
+        synonyms = WORD_API.getRelatedWords(word, relationshipTypes='synonym')
+        if synonyms is not None:
+            synonym_words = []
+            for synonym in synonyms:
+                if synonym.relationshipType == 'synonym':
+                    synonym_words.extend(synonym.words)
+               
+            return synonym_words
+
+        return None
+
+    @staticmethod
+    def get_antonyms(word):
+        API_KEY = load_api_key()
+        WORD_API = create_word_api(API_KEY)
+        antonyms = WORD_API.getRelatedWords(word, relationshipTypes='antonym')
+        if antonyms is not None:
+            antonym_words = []
+            for antonym in antonyms:
+                if antonym.relationshipType == 'antonym':
+                    antonym_words.extend(antonym.words)
+
+            return antonym_words
+
+        return None
+        
+    def stringify(self):
+
+        # Representation for meanings
+        meanings = list()
+        for index, meaning in enumerate(self.meanings, start=1):
+            meanings.append(click.style('{}. {}'.format(index, meaning)))
+        
+        # Representation for examples
+        examples = list()
+        for index, example in enumerate(self.examples, start=1):
+            examples.append(click.style('{}. {}'.format(index, example), fg='cyan'))
+        
+        hyphenation, audio, phrases, text_pronunciations = None, None, None, None
+        
+        if self.hyphenation is not None:
+            hyphenation = click.style(self.hyphenation, fg='green')
+        
+        if self.audio is not None:
+            audio = click.style(self.audio, bg='black')
+
+        # Representation for text pronunciations
+        if self.text_pronunciations is not None:
+            text_pronunciations = list()
+            for index, pronunciation in enumerate(self.text_pronunciations, start=1):
+                text_pronunciations.append(click.style('{}. {}'.format(index, pronunciation), fg='yellow'))
+
+        # Representation for phrases
+        if self.phrases is not None:
+            phrases = list()
+            for index, phrase in enumerate(self.phrases, start=1):
+                phrases.append(click.style('{}. {}'.format(index, phrase), fg='magenta'))
+        
+        # Representation for synonyms
+        synonyms, antonyms = None, None
+        if self.synonyms is not None:
+            synonyms = click.style(', '.join(self.synonyms), fg='green')
+
+        if self.antonyms is not None:
+            antonyms = click.style(', '.join(self.antonyms), fg='red')
+
+        headings = [
+                    ('Word', click.style(self.word, fg='red', bold=True), '\t'),
+                    ('Meanings', meanings, '\n'), 
+                    ('Synonyms', synonyms, ' '),
+                    ('Antonyms', antonyms, ' '),
+                    ('Examples', examples, '\n'), 
+                    ('Text Pronunciations', text_pronunciations, '\n'),
+                    ('Phrases', phrases, '\n'),
+                    ('Hyphenation', hyphenation, ' '),
+                    ('Audio', audio, ' ')
+                    ]
+
+        s = [create_entry(heading, data, separator) for heading, data, separator in headings if data is not None]
+
+        return '\n\n'.join(s)
+
+    def jsonify(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
+def create_entry(heading, data, separator):
+    HEADING_COLOR = 'blue'
+    complete_heading = heading + ':'
+
+    if isinstance(data, list):
+        complete_data = '\n'.join(data)
+    else:
+        complete_data = data
+
+    #print('Printing:', heading)
+    #print('DATA:', data)
+    return click.style(complete_heading, fg=HEADING_COLOR, underline=True, bold=True) + separator + complete_data
