1e36a159c0e98f1c5df8e45057dab1b0c277a637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:07 +0000

    Add package pycldf/1.0.6/pycldf-1.0.6.tar.gz

---
+++ packages/pycldf/pycldf-1.0.6.tar.gz/pycldf-1.0.6/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    with open(fname) as fp:
+        return fp.read().split('\n\n\n')[0]
+
+
+setup(
+    name='pycldf',
+    version="1.0.6",
+    description='A python library to read and write CLDF datasets',
+    long_description=read("README.rst"),
+    author='Robert Forkel',
+    author_email='forkel@shh.mpg.de',
+    url='https://github.com/glottobank/pycldf',
+    install_requires=[
+        'six',
+        'pybtex',
+        'clldutils>=1.13.10',
+        'uritemplate>=3.0',
+        'python-dateutil',
+    ],
+    license="Apache 2",
+    zip_safe=False,
+    keywords='',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'cldf=pycldf.cli:main',
+        ]
+    },
+    tests_require=['nose', 'coverage', 'mock'],
+)
