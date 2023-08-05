5f707e8e5f1e3ca33d099888b74d206a83f3bab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:58 +0000

    Add package crawl-requests/1.1.0/crawl_requests-1.1.0-py3.6.egg

---
+++ packages/crawl-requests/crawl_requests-1.1.0-py3.6.egg/crawl_requests/config.py
@@ -0,0 +1,4 @@
+# UA_pool = [
+#     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+#     'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36']
+# proxy_pool = [{'http': '62.81.245.12:3128'}]
