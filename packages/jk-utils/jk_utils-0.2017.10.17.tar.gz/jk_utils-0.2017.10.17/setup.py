58283c8872a73a0f61b7b8b9a06ad3910fdb85f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:40 +0000

    Add package jk-utils/0.2017.10.17/jk_utils-0.2017.10.17.tar.gz

---
+++ packages/jk-utils/jk_utils-0.2017.10.17.tar.gz/jk_utils-0.2017.10.17/setup.py
@@ -0,0 +1,35 @@
+﻿from setuptools import setup
+
+
+def readme():
+	with open('README.rst') as f:
+		return f.read()
+
+
+setup(name='jk_utils',
+	version='0.2017.10.17',
+	description='This python module provides various utility functions and classes.',
+	author='Jürgen Knauth',
+	author_email='pubsrc@binary-overflow.de',
+	license='Apache 2.0',
+	url='https://github.com/jkpubsrc/python-module-jk-utils',
+	download_url='https://github.com/jkpubsrc/python-module-jk-utils/tarball/0.2017.8.28',
+	keywords=[
+		'utils'
+	],
+	packages=[
+		'jk_utils'
+	],
+	install_requires=[
+		"netiface",
+		"jk_simpleexec",
+	],
+	include_package_data=True,
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Programming Language :: Python :: 3.5',
+		'License :: OSI Approved :: Apache Software License'
+	],
+	long_description=readme(),
+	zip_safe=False)
+
