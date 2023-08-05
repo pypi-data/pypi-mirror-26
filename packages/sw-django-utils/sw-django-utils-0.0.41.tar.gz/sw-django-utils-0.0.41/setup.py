fc1726510305bd76dc0a3412a40221a2ebb42e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:45 +0000

    Add package sw-django-utils/0.0.41/sw-django-utils-0.0.41.tar.gz

---
+++ packages/sw-django-utils/sw-django-utils-0.0.41.tar.gz/sw-django-utils-0.0.41/setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# python setup.py sdist register bdist_egg upload
+from setuptools import setup, find_packages
+
+setup(
+    name='sw-django-utils',
+    version='0.0.41',
+    description='Soft Way company django utils.',
+    author='Telminov Sergey',
+    author_email='sergey@telminov.ru',
+    url='https://github.com/telminov/sw-django-utils',
+    include_package_data=True,
+    packages=find_packages(),
+    license='The MIT License',
+    install_requires=[
+        'django',
+        'sqlparse==0.1.16',
+        'sw-python-utils',
+    ],
+)
