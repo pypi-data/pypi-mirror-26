7fa5ec3143bb31936df7201e3c0e19d1981f8ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:36 +0000

    Add package clldutils/1.13.10/clldutils-1.13.10.tar.gz

---
+++ packages/clldutils/clldutils-1.13.10.tar.gz/clldutils-1.13.10/setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup, find_packages
+
+
+requires = [
+    'six',
+    'python-dateutil',
+    'configparser>=3.5.0b2',
+    'tabulate>=0.7.7',
+    'colorlog',
+    'attrs>=17.1.0',
+    'uritemplate>=3.0.0',
+    'isodate',
+    'rfc3986',
+]
+
+if sys.version_info.major == 2:
+    requires.append('pathlib2')
+
+
+def read(fname):
+    with open(fname) as fp:
+        return fp.read()
+
+
+setup(
+    name='clldutils',
+    version='1.13.10',
+    description='Utilities for clld apps',
+    long_description=read("README.rst"),
+    author='Robert Forkel',
+    author_email='forkel@shh.mpg.de',
+    url='https://github.com/clld/clldutils',
+    install_requires=requires,
+    license="Apache 2.0",
+    zip_safe=False,
+    keywords='',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    packages=find_packages(),
+    tests_require=[
+        'nose',
+        'coverage',
+        'mock>=2.0',
+    ],
+)
