51e0953df58f9f6cbb17418c60d8a4500bfcad2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:22 +0000

    Add package optimuspyspark/1.1.0/optimuspyspark-1.1.0-py3-none-any.whl

---
+++ packages/optimuspyspark/optimuspyspark-1.1.0-py3-none-any.whl/optimus/version.py
@@ -0,0 +1,8 @@
+def _safe_int(string):
+    try:
+        return int(string)
+    except ValueError:
+        return string
+
+__version__ = '1.1.0'
+VERSION = tuple(_safe_int(x) for x in __version__.split('.'))
