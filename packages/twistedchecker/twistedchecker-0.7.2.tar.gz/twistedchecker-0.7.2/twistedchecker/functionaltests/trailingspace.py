f7a3207a77a25146650b667828c2ff5271ed464d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package twistedchecker/0.7.2/twistedchecker-0.7.2.tar.gz

---
+++ packages/twistedchecker/twistedchecker-0.7.2.tar.gz/twistedchecker-0.7.2/twistedchecker/functionaltests/trailingspace.py
@@ -0,0 +1,8 @@
+# enable: W9010,W9011
+from __future__ import print_function
+# A line with trailing space.
+print("this line has trailing space") 
+
+# next blank line contains a whitespace
+ 
+# end of file
