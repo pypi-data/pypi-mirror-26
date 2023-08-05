0a14df6baf7d7f144f0de29b2f61d3799116e2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:45 +0000

    Add package coach6/1.0.0/coach6-1.0.0.tar.gz

---
+++ packages/coach6/coach6-1.0.0.tar.gz/coach6-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+			name='coach6',
+			version='1.0.0',
+			py_modules=['athletelist'],
+			author='wangyang',
+			author_email='wangyang_1980@hotmail.com',
+			url='',
+			description='A simple test code for athletes list times'
+)
