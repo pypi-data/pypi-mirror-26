21ae4ed4acde1b121507d82ee72edfd2fa0ffbbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:15 +0000

    Add package suzhidong/1.2.1/suzhidong-1.2.1.tar.gz

---
+++ packages/suzhidong/suzhidong-1.2.1.tar.gz/suzhidong-1.2.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	     ='suzhidong',
+	version      ='1.2.1',
+	py_modules   =['suzhidong'],
+	author	     ='su',
+	author_email ='suzhidong2016@163.com',
+	url	     ='https://weibo.com/6063617656/profile?topnav=1&wvr=6',
+	description  ='this is a simple nested list',
+	)
