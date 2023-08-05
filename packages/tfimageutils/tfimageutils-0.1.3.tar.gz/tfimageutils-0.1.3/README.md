f00dd6dae8a6f00ea59f3ca433ebd3a7bcf3e9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:31 +0000

    Add package tfimageutils/0.1.3/tfimageutils-0.1.3.tar.gz

---
+++ packages/tfimageutils/tfimageutils-0.1.3.tar.gz/tfimageutils-0.1.3/README.md
@@ -0,0 +1,12 @@
+# TFImageUtils
+
+This package contains utilities for working with images in Tensorflow.
+
+Currently, it only contains the ImageFeeder, which encapsulates loading images into a Tensorflow representation for further processing via a feed_dict.
+
+
+## Roadmap
+
+* Finding files by going through subfolders
+* Non-flattened images for use with CNN's
+* Other ways of using images in Tensorflow (Datasources)
---
+++ packages/tfimageutils/tfimageutils-0.1.3.tar.gz/tfimageutils-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='tfimageutils',
+    version='0.1.3',
+    packages=['tfimageutils'],
+    url='https://github.com/MrTrustworthy/tfimageutils',
+    download_url='https://github.com/MrTrustworthy/tfimageutils/archive/0.1.3.tar.gz',
+    license='MIT',
+    install_requires=['matplotlib', 'numpy'],
+    author='MrTrustworthy',
+    author_email='tinywritingmakesmailhardtoread@gmail.com',
+    description='A utility package for dealing with image inputs into Tensorflow',
+    keywords=['tensorflow', 'image', 'utils'],
+    classifiers=[]
+)
