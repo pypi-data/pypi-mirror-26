4e86a431e36946931d5bd963e1c6ee6705fe37ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:56 +0000

    Add package lightvolume/0.1/lightvolume-0.1.tar.gz

---
+++ packages/lightvolume/lightvolume-0.1.tar.gz/lightvolume-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='lightvolume',
+    version='0.1',
+    author='Daniel Pope',
+    author_email='mauve@mauveweb.co.uk',
+    url='https://github.com/lordmauve/lightvolume',
+    py_modules=['lightvolume'],
+    install_requires=[
+        'cffi>=1.11',
+    ],
+    setup_requires=['cffi>=1.11'],
+    cffi_modules=["lightvolume_build.py:ffibuilder"],
+)
