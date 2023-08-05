3ec2e69723f9a8baa0ee15f12a390a673c8bfe50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:27 +0000

    Add package sf_tools/1.0.1/sf_tools-1.0.1.tar.gz

---
+++ packages/sf_tools/sf_tools-1.0.1.tar.gz/sf_tools-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from sf_tools import __version__
+
+setup(
+    name='sf_tools',
+    author='sfarrens',
+    author_email='samuel.farrens@gmail.com',
+    version=__version__,
+    url='https://github.com/sfarrens/sf_tools',
+    download_url='https://github.com/sfarrens/sf_tools/archive/0.1.tar.gz',
+    packages=find_packages(),
+    license='MIT',
+    description='Tools for image analysis, signal processing and statistics.',
+    long_description=open('README.txt').read(),
+)
