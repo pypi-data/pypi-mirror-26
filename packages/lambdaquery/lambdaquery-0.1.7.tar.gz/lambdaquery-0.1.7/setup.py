2b46c24a1940401c8eabce2418a5b2f4a42fdd12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:51 +0000

    Add package lambdaquery/0.1.7/lambdaquery-0.1.7.tar.gz

---
+++ packages/lambdaquery/lambdaquery-0.1.7.tar.gz/lambdaquery-0.1.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='lambdaquery',
+    version='0.1.7',
+    description='Composable SQL in Pure Python',
+    author='Chong Wang',
+    author_email='chonw89@gmail.com',
+    packages=['LambdaQuery'],
+    license='MIT',
+    # url=['https://github.com/xnmp/lambdaquery'],
+    setup_requires=['lenses'],
+    keywords='databases query sql orm',
+    install_requires=['lenses'],
+    python_requires='>=3.6'
+)
+
+# python setup.py sdist upload
