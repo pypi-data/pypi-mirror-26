b5049b385486c0ca5c25b84c5afb1d31f55754e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:01 +0000

    Add package pychecks/1.1.0/pychecks-1.1.0.tar.gz

---
+++ packages/pychecks/pychecks-1.1.0.tar.gz/pychecks-1.1.0/pychecks/__main__.py
@@ -0,0 +1,38 @@
+# coding : utf8
+
+import os
+import sys
+import commands
+
+
+def main():
+    if len(sys.argv) < 2:
+        dir = commands.getoutput("pwd")
+    else :
+        dir = sys.argv[1]
+
+    datas = {}
+    for path, dirs, files in os.walk(dir):
+        files = filter(lambda i:i.endswith(".py"), files)
+        if 'egg' in path:
+            continue
+        if not files:
+            continue
+        files = map(lambda i:os.path.join(path,i), files)
+        for file in files:
+            cmd = "pylint {file}".format(file=files[0])
+            output = commands.getoutput(cmd)
+            score = output.split(':')[-1].split('/')[0].strip()
+            try:
+                datas[file] = float(score)
+            except:
+                continue
+
+    print 'All python file is rated by pylint. Rank:'
+    for file, score in sorted(datas.items(), key=lambda x:x[1], reverse=True):
+        print file, score
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
