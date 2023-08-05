844081b598887508bdc2cb2fb8f923a65ecbf7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:02 +0000

    Add package gpymusic/1.2.1/gpymusic-1.2.1-py3-none-any.whl

---
+++ packages/gpymusic/gpymusic-1.2.1-py3-none-any.whl/gpymusic/common.py
@@ -0,0 +1,22 @@
+from gmusicapi import Mobileclient
+# Imports are stupid.
+mc = Mobileclient()  # noqa Our interface to Google Play Music.
+
+from . import nowplaying
+from . import songqueue
+from . import view
+from . import writer
+
+from os.path import expanduser, join
+
+
+# Location where we keep songs, playlists, libraries, and source code.
+DATA_DIR = join(expanduser('~'), '.local', 'share', 'gpymusic')
+# Location where we keep user and mpv configurations.
+CONFIG_DIR = join(expanduser('~'), '.config', 'gpymusic')
+
+q = songqueue.Queue()  # Queue/playlist.
+w = writer.Writer(None, None, None, None, curses=False)  # Output handler.
+v = view.View()  # Main window contents.
+np = nowplaying.NowPlaying()
+client = None  # To be set in the main executable.
