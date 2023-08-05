600f97e2e4083a228a9888487b7eeaf28e78a634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:53 +0000

    Add package cubicweb-sentry/0.1.6/cubicweb-sentry-0.1.6.tar.gz

---
+++ packages/cubicweb-sentry/cubicweb-sentry-0.1.6.tar.gz/cubicweb-sentry-0.1.6/site_cubicweb.py
@@ -0,0 +1,10 @@
+options = (
+    ('sentry-dsn',
+     {'type': 'string',
+      'default': '',
+      'help': 'Sentry DSN (eg. https://public_key:secret_key@sentry.local/project_id?timeout=10 )',
+      'group': 'sentry',
+      'level': 5,
+      }
+     ),
+)
