4f7d4ed0b8fa37163d5cf4dc08f736055f485064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:59 +0000

    Add package easy7zip/0.0.9/easy7zip-0.0.9.zip

---
+++ packages/easy7zip/easy7zip-0.0.9.zip/easy7zip-0.0.9/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'easy7zip',
+  packages = ['easy7zip'], # this must be the same as the name above
+  version = '0.0.9',
+  description = 'This is Windows 7zip module for Python 2.7',
+  #platform = 'Windows',
+  author = 'Sreejith KOVILAKATHUVEETTIL CHANDRAN',
+  author_email = 'sreeju_kc@hotmail.com',
+  license = 'Apache2',
+  #data_files = [("", ["LICENSE.txt"])],
+  url = 'https://github.com/sreejithkchandran/easy7zip/' # use the URL to the github repo
+)
