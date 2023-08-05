579ba5f67a5aa1765dcc95f43920322d09756778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:14 +0000

    Add package sandpaper/0.0.3/sandpaper-0.0.3.tar.gz

---
+++ packages/sandpaper/sandpaper-0.0.3.tar.gz/sandpaper-0.0.3/tests/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright (c) 2017 Stephen Bunn (stephen@bunn.io)
+# MIT License <https://opensource.org/licenses/MIT>
+
+from .test_sandpaper import (SandPaperTest,)
+from .test_utils import (UtilTest,)
+from .rules import *
