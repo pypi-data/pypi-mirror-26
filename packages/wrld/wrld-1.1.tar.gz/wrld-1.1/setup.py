0468490a9308d0c19d31fc0c98afc1eafeb4c037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:16 +0000

    Add package wrld/1.1/wrld-1.1.tar.gz

---
+++ packages/wrld/wrld-1.1.tar.gz/wrld-1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import fastentrypoints
+
+setup(
+    name='wrld',
+    version='1.1',
+    author='Aaron Christianson',
+    license='BSD',
+    author_email='ninjaaron@gmail.com',
+    url='https://github.com/ninjaaron/wrld',
+    description='simplified bash loops (or, xargs -I on steroids)',
+    long_description=open('README.rst').read(),
+    keywords='evaluate',
+    py_modules=['wrld'],
+    entry_points={'console_scripts': ['wrld=wrld:main']},
+)
