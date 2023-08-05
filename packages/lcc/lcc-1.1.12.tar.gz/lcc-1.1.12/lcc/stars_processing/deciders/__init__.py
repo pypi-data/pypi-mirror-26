46e5da8a869b7a09dd66d5506208442845a597d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:19 +0000

    Add package lcc/1.1.12/lcc-1.1.12.tar.gz

---
+++ packages/lcc/lcc-1.1.12.tar.gz/lcc-1.1.12/lcc/stars_processing/deciders/__init__.py
@@ -0,0 +1,4 @@
+from neuron_decider import NeuronDecider
+from supervised_deciders import (QDADec, LDADec, TreeDec, GaussianNBDec,
+                                 SVCDec, AdaBoostDec, ExtraTreesDec, RandomForestDec)
+from custom_decider import CustomDecider
