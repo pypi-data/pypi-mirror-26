f81cc57d1670b29f5ae91736fba27bec9c6bbce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:35 +0000

    Add package ls_coach3/1.0.4/ls_coach3-1.0.4.tar.gz

---
+++ packages/ls_coach3/ls_coach3-1.0.4.tar.gz/ls_coach3-1.0.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name       = 'ls_coach3',
+    version    = '1.0.4',
+    py_modules = ['ls_coach3'],
+    author     = 'lecsim',
+    author_email = 'lechler@gmx.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+)
