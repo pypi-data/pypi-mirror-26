be080a3cf4044e82d4b4813a881e7f6e17fb0e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:51 +0000

    Add package python-opendata-transport/0.0.3/python_opendata_transport-0.0.3.tar.gz

---
+++ packages/python-opendata-transport/python_opendata_transport-0.0.3.tar.gz/python_opendata_transport-0.0.3/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='python_opendata_transport',
+    version='0.0.3',
+    description='Python API for interacting with transport.opendata.ch.',
+    url='https://github.com/fabaff/python-opendata-transport',
+    download_url='https://github.com/fabaff/python-opendata-transport/releases',
+    author='Fabian Affolter',
+    author_email='fabian@affolter-engineering.ch',
+    license='MIT',
+    install_requires=['aiohttp'],
+    packages=['opendata_transport'],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Utilities',
+    ],
+)
