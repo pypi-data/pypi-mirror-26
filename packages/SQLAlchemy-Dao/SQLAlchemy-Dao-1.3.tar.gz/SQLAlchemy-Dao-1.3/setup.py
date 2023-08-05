ea3edb4c2eff62eb92c087e06cf7e8d69aa77fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:17 +0000

    Add package SQLAlchemy-Dao/1.3/SQLAlchemy-Dao-1.3.tar.gz

---
+++ packages/SQLAlchemy-Dao/SQLAlchemy-Dao-1.3.tar.gz/SQLAlchemy-Dao-1.3/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='SQLAlchemy-Dao',
+    version='1.3',
+    author='Mengchen LEE',
+    author_email='CooledCoffee@gmail.com',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries',
+    ],
+    description='Simple wrapper for sqlalchemy.',
+    install_requires=[
+        'decorated',
+        'fixtures2',
+        'inflection',
+        'sqlalchemy',
+    ],
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    url='https://github.com/cooledcoffee/sqlalchemy-dao'
+)
