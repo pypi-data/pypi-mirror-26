66c7c794a13e8c1c297180d32bffca06a5388078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:17 +0000

    Add package flake8-deprecated/1.2.2.dev0/flake8-deprecated-1.2.2.dev0.tar.gz

---
+++ packages/flake8-deprecated/flake8-deprecated-1.2.2.dev0.tar.gz/flake8-deprecated-1.2.2.dev0/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+short_description = 'Warns about deprecated method calls.'
+
+
+long_description = '{0}\n{1}'.format(
+    open('README.rst').read(),
+    open('CHANGES.rst').read()
+)
+
+
+setup(
+    name='flake8-deprecated',
+    version='1.2.2.dev0',
+    description=short_description,
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+    keywords='pep8 flake8 deprecations',
+    author='Gil Forcada',
+    author_email='gil.gnome@gmail.com',
+    url='https://github.com/gforcada/flake8-deprecated',
+    license='GPL version 2',
+    py_modules=['flake8_deprecated', ],
+    include_package_data=True,
+    test_suite = 'run_tests',
+    zip_safe=False,
+    install_requires=[
+        'flake8 >= 3.0.0',
+    ],
+    entry_points={
+        'flake8.extension': ['D001 = flake8_deprecated:Flake8Deprecated'],
+    },
+)
