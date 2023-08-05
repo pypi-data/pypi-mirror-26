8bce57a6b6aff0c44dfae2fb8a637db0fb30363a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:59 +0000

    Add package altapay/1.3.0/altapay-1.3.0-py2.py3-none-any.whl

---
+++ packages/altapay/altapay-1.3.0-py2.py3-none-any.whl/altapay/__init__.py
@@ -0,0 +1,16 @@
+__title__ = 'altapay'
+__version__ = '1.3.0'
+__author__ = 'Coolshop.com'
+__license__ = 'MIT'
+__github_url__ = 'https://github.com/coolshop-com/AltaPay'
+__api_base_url__ = {
+    'test': 'https://testgateway.altapaysecure.com/merchant/',
+    'production': 'https://{shop_name}.altapaysecure.com/merchant/'
+}
+
+from altapay.api import API  # NOQA
+from altapay.callback import Callback  # NOQA
+from altapay.funding import CustomReport, Funding, FundingList  # NOQA
+from altapay.payment import Payment  # NOQA
+from altapay.resource import Resource  # NOQA
+from altapay.transaction import Transaction  # NOQA
