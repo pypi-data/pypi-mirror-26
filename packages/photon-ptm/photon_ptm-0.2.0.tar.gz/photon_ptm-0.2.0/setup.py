0a14917b15ad2cc1e530ca7a4c7cdd8583d41ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:22 +0000

    Add package photon-ptm/0.2.0/photon_ptm-0.2.0.tar.gz

---
+++ packages/photon-ptm/photon_ptm-0.2.0.tar.gz/photon_ptm-0.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+import os
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='photon_ptm',
+        version='0.2.0',
+        description='PHOsphoproteomic dissecTiOn via Networks',
+        long_description=read('README.md'),
+        url='http://www.github.com/jdrudolph/photon',
+        author='Jan Rudolph',
+        author_email='jan.daniel.rudolph@gmail.com',
+        license='MIT',
+        packages=find_packages(),
+        install_requires=[
+            'numpy',
+            'pandas >= 0.21',
+            'scipy',
+            'matplotlib',
+            'statsmodels',
+            'networkx',
+            'perseuspy',
+            'flask',
+            'celery',
+            'redis',
+            'goenrich']
+) 
