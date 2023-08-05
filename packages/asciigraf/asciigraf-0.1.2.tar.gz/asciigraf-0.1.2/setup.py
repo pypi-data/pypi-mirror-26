f07df0c0091f5aff8d55c3018717c845b11b6d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:26 +0000

    Add package asciigraf/0.1.2/asciigraf-0.1.2.tar.gz

---
+++ packages/asciigraf/asciigraf-0.1.2.tar.gz/asciigraf-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.rst", 'r') as f:
+        return f.read()
+
+
+setup(
+    name="asciigraf",
+    version="0.1.2",
+    packages=["asciigraf"],
+    description="A python library for making ascii-art into network graphs.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description=readme(),
+    author="Anton Lodder",
+    author_email="antonlodder@gmail.com",
+    url="https://github.com/AnjoMan/asciigraf",
+    keywords=["graph", "network", "testing", "parser"],
+    license="MIT",
+    install_requires=[
+        'networkx==1.11',
+    ],
+    extras_require={
+        "test": ["pytest"],
+    },
+)
