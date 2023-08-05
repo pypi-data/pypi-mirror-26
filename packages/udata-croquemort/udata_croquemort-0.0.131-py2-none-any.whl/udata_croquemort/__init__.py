0810bccd85fe66378920d5414dd4784e68b26950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:54 +0000

    Add package udata-croquemort/0.0.131/udata_croquemort-0.0.131-py2-none-any.whl

---
+++ packages/udata-croquemort/udata_croquemort-0.0.131-py2-none-any.whl/udata_croquemort/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+uData
+'''
+from __future__ import unicode_literals
+
+__version__ = '0.0.2.dev'
+__description__ = 'uData Croquemort integration'
