42ce49cb67a8177ce9495a2ef9463263de9f35f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:43 +0000

    Add package txrwlock/1.1.4/txrwlock-1.1.4.tar.gz

---
+++ packages/txrwlock/txrwlock-1.1.4.tar.gz/txrwlock-1.1.4/txrwlock/txtestcase.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from twisted.internet import defer
+from twisted.trial.unittest import TestCase
+
+__all__ = ['TxTestCase']
+
+
+class TxTestCase(TestCase):
+    '''
+    Unit test helper class for Twisted.
+
+    Provides useful methods to test exception cases, such as ``assertRaisesWithMessage`` and
+    ``assertInlineCallbacksRaisesWithMessage`` in addition to ``twisted.trial.unittest.TestCase``.
+    '''
+
+    def __assertExceptionMessageIs(self, err, expectedMessage):
+        if hasattr(err, "message"):
+            self.assertSubstring(expectedMessage, err.message)
+        else:
+            self.assertSubstring(expectedMessage, str(err))
+
+    def assertRaisesWithMessage(self, exceptionClass, expectedMessage, func, *args, **kw):
+        '''
+        Check if a given function call (synchronous or deferred) raised with a given message.
+
+        Note: You cannot use an ``inlineCallbacks`` as func. Please use
+        ``assertInlineCallbacksRaisesWithMessage``.
+        '''
+        try:
+            defer.maybeDeferred(func(*args, **kw))
+        except exceptionClass as err:
+            self.__assertExceptionMessageIs(err, expectedMessage)
+            return
+        raise Exception("{0} not raised".format(exceptionClass, ))
+
+    @defer.inlineCallbacks
+    def assertInlineCbRaisesWithMsg(self,
+                                    exceptionClass,
+                                    expectedMessage,
+                                    inlineCallbacksFunc,
+                                    *args,
+                                    **kw):
+        '''
+        Assert a given inlineCallbacks decorated method raises with a given message.
+
+        This replaces ``assertRaisesWithMessage`` for ``inlineCallbacks``.
+
+        Note: this method is an ``inlineCallbacks`` and need to be yielded.
+        '''
+        try:
+            yield inlineCallbacksFunc(*args, **kw)
+        except exceptionClass as err:
+            self.__assertExceptionMessageIs(err, expectedMessage)
+            return
+        raise Exception("{0} not raised".format(exceptionClass, ))
+
+    @defer.inlineCallbacks
+    def assertInlineCbRaises(self, exceptionClass, inlineCallbacksFunc, *args, **kw):
+        '''
+        Assert a given inlineCallbacks decorated method raises.
+
+        This replaces ``assertRaisesWithMessage`` for ``inlineCallbacks``.
+
+        Note: this method is an ``inlineCallbacks`` and need to be yielded.
+        '''
+        try:
+            yield inlineCallbacksFunc(*args, **kw)
+        except exceptionClass:
+            return
+        raise Exception("{0} not raised".format(exceptionClass))
