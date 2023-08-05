2c1dd7c9201a6a239fab4e1a45353d72f1a7209c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:47 +0000

    Add package masonry/0.1.1/masonry-0.1.1.tar.gz

---
+++ packages/masonry/masonry-0.1.1.tar.gz/masonry-0.1.1/setup.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'masonry'
+DESCRIPTION = 'A command line tool for composable project templating.'
+URL = 'https://github.com/MrKriss/masonry'
+EMAIL = 'chris.j.musselle@gmail.com'
+AUTHOR = 'Chris Musselle'
+
+REQUIRED = [
+    'cookiecutter>=1.6',
+    'docopt',
+    'schema',
+    'inquirer',
+    'ruamel.yaml>=0.15',
+    'gitpython',
+    'clint'
+]
+
+repo_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+with io.open(os.path.join(repo_root, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(repo_root, 'package', 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
+setup(
+    # Metadata
+    name=NAME,
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    # Use version control to define version tag
+    use_scm_version={"root": "..", "relative_to": __file__},
+    # Package Locations
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    # Dependencies
+    install_requires=REQUIRED,
+    include_package_data=True,
+
+    # Entry Point for the command line application
+    entry_points={
+        'console_scripts': ['mason=masonry.main:main']
+    },
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+    ],
+    # Command for setup.py publishing support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
---
+++ packages/masonry/masonry-0.1.1.tar.gz/masonry-0.1.1/src/masonry/cli.py
@@ -0,0 +1,111 @@
+"""Masonry
+
+A tool for working with composable project templates. 
+
+Commands:
+    init  - initialise a new project 
+    add   - add a template to an existing project
+    check - check a particular project or template layer renders correctly
+
+Usage:
+    mason init [-v] [-o DIR] [PROJECT]
+    mason add [-v] [-o DIR] [TEMPLATE ...]
+    mason check [-v] [PROJECT]
+    mason (-h | --help)
+    mason --version
+
+Arguments:
+  PROJECT      URL/Filepath to directory for project type. Should contain
+               subdiectories for all templates and a metadata.json file.
+               Can also specify a template subdirectory, and if so, this
+               is used as the template instead of the default for the project.
+
+  TEMPLATE     Name of one or more templates to add to an existing project.
+
+Options:
+  -o DIR --output=DIR  Project directory to create/add to [default: .]
+  -h --help            Show this screen.
+  --version            Show version.
+  -v                   Increase verbosity of output
+"""
+import os
+from sys import exit
+from pathlib import Path
+
+from docopt import docopt
+
+from . import __version__
+
+try:
+    from schema import Schema, And, Or, Use, SchemaError
+except ImportError:
+    exit('This example requires that `schema` data-validation library'
+         ' is installed: \n    pip install schema\n'
+         'https://github.com/halst/schema')
+
+
+def parse_args(argv=None):
+    args = docopt(__doc__, version=__version__, argv=argv)
+    return args
+
+
+def validate_args(args):
+    def template_path_exists(template):
+        if template:
+            template_path = os.path.join(args['PROJECT'], template)
+            return os.path.exists(template_path)
+        else:
+            return True
+
+    def mason_file_exists(output):
+        mason_path = os.path.join(output, '.mason')
+        return os.path.exists(mason_path)
+
+    if args['init']:
+        schema = Schema({
+            # 'TEMPLATE': And(template_path_exists, error='TEMPLATE should be subdirectory of PROJECT'),
+            # 'PROJECT': And(os.path.exists, error='PROJECT should exist'),
+            str: object})
+
+    elif args['add']:
+        schema = Schema({
+            # 'TEMPLATE': And(str, error='TEMPLATE should be specified'),
+            # '--output': And(mason_file_exists, error='The .mason was not detected for project.'
+            #                                          ' Specofy location with --output option.'),
+            str: object})
+
+    elif args['check']:
+        schema = Schema({
+            # 'TEMPLATE': And(str, error='TEMPLATE should be specified'),
+            # '--output': And(mason_file_exists, error='The .mason was not detected for project.'
+            #                                          ' Specofy location with --output option.'),
+            str: object})
+    try:
+        args = schema.validate(args)
+    except SchemaError as e:
+        exit(e)
+
+
+def parse_and_validate_args(argv):
+    parsed_args = parse_args(argv)
+    validate_args(parsed_args)
+    return parsed_args
+
+
+def parse_project_argument(arg):
+
+    template = None
+
+    # See if template directroy or project directory was specified
+    project_argument = Path(arg).resolve()
+    project_metadata = project_argument / 'metadata.json'
+
+    if project_metadata.exists():
+        project_dir = arg
+
+    elif (project_argument.parent / 'metadata.json').exists():
+        # Check parent directory and use sub directory as template
+        project_dir = project_argument.parent.as_posix()
+        template = project_argument.name
+
+    return project_dir, template
