f055b8755e807996d231b1214763d3a5f9b71bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:28 +0000

    Add package Products.ExternalMethod/4.0/Products.ExternalMethod-4.0.tar.gz

---
+++ packages/Products.ExternalMethod/Products.ExternalMethod-4.0.tar.gz/Products.ExternalMethod-4.0/CHANGES.rst
@@ -0,0 +1,27 @@
+Changelog
+=========
+
+4.0 (2017-10-18)
+----------------
+
+- Add support for Python 3.4, 3.5 and 3.6.
+
+- Add compatibility with Zope 4.
+
+3.0 (2016-07-18)
+----------------
+
+- Remove HelpSys support.
+
+- Remove ZODB3 as direct dependency. Now we are able to use ZODB 4.0
+  and it is a dependency of Zope2 anyways
+
+2.13.1 (2014-11-02)
+-------------------
+
+- Handle both `func_code` / `__code__` and `func_defaults` / `__defaults__`.
+
+2.13.0 (2010-07-10)
+-------------------
+
+- Released as separate package.
---
+++ packages/Products.ExternalMethod/Products.ExternalMethod-4.0.tar.gz/Products.ExternalMethod-4.0/setup.py
@@ -0,0 +1,56 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+setup(name='Products.ExternalMethod',
+      version='4.0',
+      url='http://pypi.python.org/pypi/Products.ExternalMethod',
+      license='ZPL 2.1',
+      description="This package provides support for external Python methods "
+                  "within a Zope 2 environment.",
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=(open('README.rst').read() + '\n' +
+                        open('CHANGES.rst').read()),
+      packages=find_packages('src'),
+      namespace_packages=['Products'],
+      package_dir={'': 'src'},
+      classifiers=[
+          "Development Status :: 6 - Mature",
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: Zope Public License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: Implementation :: CPython",
+      ],
+      install_requires=[
+          'setuptools',
+          'AccessControl',
+          'Acquisition',
+          'ExtensionClass>=4.1a1',
+          'Persistence',
+          'ZODB',
+          'Zope2 >= 4.0a1'
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
