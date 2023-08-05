698fd56b606c198bd164bbe86fb5672f61a33cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:15 +0000

    Add package cplusnew/0.1/cplusnew-0.1.tar.gz

---
+++ packages/cplusnew/cplusnew-0.1.tar.gz/cplusnew-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='cplusnew',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='marksei',
+      author_email='mark.seidenschnur@gmail.com',
+      license='MIT',
+      packages=['cplusnew'],
+      zip_safe=False)
