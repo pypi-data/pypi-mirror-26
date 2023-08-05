29890cb686ed55cda98974c7ad9b17454528a8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:24 +0000

    Add package vsscli/0.2.1/vsscli-0.2.1.tar.gz

---
+++ packages/vsscli/vsscli-0.2.1.tar.gz/vsscli-0.2.1/vsscli/__init__.py
@@ -0,0 +1,11 @@
+import os
+__version__ = '0.2.1'
+__default_endpoint__ = 'https://vss-api.eis.utoronto.ca'
+__history_file_path__ = os.path.join('~', '.vss', 'history')
+__config_file_path__ = os.path.join('~', '.vss', 'config.json')
+__env_vars__ = {'user': 'VSS_API_USER',
+                'pass': 'VSS_API_USER_PASS',
+                'token': 'VSS_API_TOKEN',
+                'endpoint': 'VSS_API_ENDPOINT',
+                'config': 'VSS_CONFIG_FILE',
+                'output': 'VSS_DEFAULT_OUTPUT'}
