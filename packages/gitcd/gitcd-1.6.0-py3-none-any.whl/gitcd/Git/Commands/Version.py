485461ea0ee7213e8a6faded375e29f3237852f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package gitcd/1.6.0/gitcd-1.6.0-py3-none-any.whl

---
+++ packages/gitcd/gitcd-1.6.0-py3-none-any.whl/gitcd/Git/Commands/Version.py
@@ -0,0 +1,54 @@
+from gitcd.Git.Command import Command
+import pkg_resources
+import requests
+from packaging import version
+from gitcd.Exceptions import GitcdPyPiApiException
+
+
+class Version(Command):
+
+    packageUrl = 'https://pypi.org/pypi/gitcd/json'
+
+    def update(self):
+        pass
+
+    def execute(self, branch: str):
+        checkUpgrade = True
+        local = self.getLocalVersion()
+        try:
+            pypi = self.getPypiVersion()
+        except GitcdPyPiApiException as e:
+            pypi = 'unknown'
+            message = str(e)
+            checkUpgrade = False
+
+        self.interface.info('Local %s' % local)
+        self.interface.info('PyPi %s' % pypi)
+
+        if checkUpgrade is False:
+            self.interface.error(message)
+            return
+
+        if version.parse(local) < version.parse(pypi):
+            self.interface.error('You need to upgrade')
+        else:
+            self.interface.ok(
+                'You seem to be on the most recent version, congrats'
+            )
+
+    def getLocalVersion(self):
+        return pkg_resources.get_distribution("gitcd").version
+
+    def getPypiVersion(self):
+        response = requests.get(
+            self.packageUrl
+        )
+
+        if response.status_code != 200:
+            raise GitcdPyPiApiException(
+                "Could not fetch version info on PyPi site." +
+                "You need to check manually, sorry for that."
+            )
+
+        result = response.json()
+        return result['info']['version']
