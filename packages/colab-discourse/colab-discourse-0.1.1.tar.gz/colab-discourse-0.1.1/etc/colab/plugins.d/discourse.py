421716694ef1bd79670c287c59faf15263b33785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:36 +0000

    Add package colab-discourse/0.1.1/colab-discourse-0.1.1.tar.gz

---
+++ packages/colab-discourse/colab-discourse-0.1.1.tar.gz/colab-discourse-0.1.1/etc/colab/plugins.d/discourse.py
@@ -0,0 +1,13 @@
+name = 'colab_discourse'
+verbose_name = 'Colab Discourse Plugin Plugin'
+
+upstream = 'http://127.0.0.1:8080/expressao/'
+api_key = 'api_key'
+api_username = 'system'
+sso_secret = 'sso_secret'
+
+urls = {
+    'include': 'colab_discourse.urls',
+    'prefix': '^expressao/',
+    'login': '/expressao/accounts/login/',
+}
