f1e269c5c95712c47003e1815b3b21c74fa7f39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:08 +0000

    Add package cri/1.8.0/cri-1.8.0.tar.gz

---
+++ packages/cri/cri-1.8.0.tar.gz/cri-1.8.0/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+    name="cri",
+    packages=['cri'],
+    scripts=[],
+    version='1.8.0',
+    description='Python client for kubernetes container runtime interface (CRI)',
+    author='Pengfei Ni',
+    author_email="feiskyer@gmail.com",
+    license="Apache License Version 2.0",
+    url="https://github.com/feiskyer/python-cri",
+    keywords=['kubernetes', 'kubelet-cri', 'containers'],
+    install_requires=["grpcio"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+)
