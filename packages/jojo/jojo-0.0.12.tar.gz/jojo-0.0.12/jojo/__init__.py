180c54ab93ee6caa37b1505e0ec6afaeaae1a52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:06 +0000

    Add package jojo/0.0.12/jojo-0.0.12.tar.gz

---
+++ packages/jojo/jojo-0.0.12.tar.gz/jojo-0.0.12/jojo/__init__.py
@@ -0,0 +1,19 @@
+from jojo.jojo import (
+    VM, RP,
+    GET, SET,
+    JOJO, MSG, CLO,
+    APPLY, IFTE,
+    NEW,
+    CALL,
+    VALUES,
+
+    code_scan,
+    parse_sexp_vect,
+    sexp_print,
+
+    compile_module,
+
+    module_repl,
+
+    load, run,
+)
