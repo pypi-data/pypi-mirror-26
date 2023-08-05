545cf9738d1e3b9f4ca9c50f23aeb9faba8c114f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:09 +0000

    Add package cobtc/0.1/cobtc-0.1.tar.gz

---
+++ packages/cobtc/cobtc-0.1.tar.gz/cobtc-0.1/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='cobtc',
+      version='0.1',
+      description='a test script quick',
+      author='Alvixeon',
+      author_email='remusername@gmail.com',
+      license='MIT',
+      packages=['cobtc'],
+      zip_safe=False
+)
