c31dc7338ed4b63e6dca96bc77ab59f23b6293b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package QuakeCL/0.1.dev0/QuakeCL-0.1.dev0.tar.gz

---
+++ packages/QuakeCL/QuakeCL-0.1.dev0.tar.gz/QuakeCL-0.1.dev0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='QuakeCL',
+    version='0.1.dev0',
+    license='MIT',
+    description='A command line tool that displays the recent earthquakes and aftershocks in Nepal.',
+    long_description=open('README.md').read(),
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    author='Amit Chaudhary',
+    author_email='meamitkc@gmail.com',
+    url='https://github.com/amitness/QuakeCL',
+    download_url = 'https://github.com/amitness/QuakeCL/tarball/0.1.0',
+    packages=['quakecl', ],
+    test_suite='quakecl.tests',
+    entry_points="""
+    [console_scripts]
+    earthquake=quakecl.earthquake:main
+    """,
+)
