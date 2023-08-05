be0f6a212592c96215b869317c8ef5bd312ac53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:24 +0000

    Add package tethys-dataset-services/1.6.3/tethys_dataset_services-1.6.3.tar.gz

---
+++ packages/tethys-dataset-services/tethys_dataset_services-1.6.3.tar.gz/tethys_dataset_services-1.6.3/tethys_dataset_services/tests/test_config.py
@@ -0,0 +1,24 @@
+# Needed for testing
+
+TEST_CKAN_DATASET_SERVICE = {
+    'ENGINE': 'tethys_dataset_services.engines.CkanDatasetEngine',
+    'ENDPOINT': '',
+    'APIKEY': '',
+    'USERNAME': '',
+    'PASSWORD': ''
+}
+
+TEST_HYDRO_SHARE_DATASET_SERVICE = {
+    'ENGINE': '',
+    'ENDPOINT': '',
+    'APIKEY': '',
+    'USERNAME': '',
+    'PASSWORD': ''
+}
+
+TEST_GEOSERVER_DATASET_SERVICE = {
+    'ENGINE': 'tethys_dataset_services.engines.GeoServerSpatialDatasetEngine',
+    'ENDPOINT': '',
+    'USERNAME': '',
+    'PASSWORD': ''
+}
