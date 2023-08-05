827276a94379e63b69477a1c3332505813ebdb4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:13 +0000

    Add package nanowire-plugin/0.6.0/nanowire_plugin-0.6.0.tar.gz

---
+++ packages/nanowire-plugin/nanowire_plugin-0.6.0.tar.gz/nanowire_plugin-0.6.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+VERSION = None
+with open("VERSION") as f:
+    VERSION = f.read()
+
+setup(
+    name='nanowire_plugin',
+    version=VERSION,
+    packages=['nanowire_plugin'],
+    requires=['pika', 'minio'],
+    url="https://github.com/SpotlightData/nanowire-plugin-py",
+    author='Barnaby "Southclaws" Keene',
+    author_email='southclaws@gmail.com',
+    license='MIT',
+    include_package_data=True,
+    data_files=[
+        ('.', ['VERSION'])
+    ]
+)
