5e3e7e65618efed6b15dc0c979a98c443e95998e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:40 +0000

    Add package python-language-server/0.9.0/python-language-server-0.9.0.tar.gz

---
+++ packages/python-language-server/python-language-server-0.9.0.tar.gz/python-language-server-0.9.0/pyls/_utils.py
@@ -0,0 +1,29 @@
+# Copyright 2017 Palantir Technologies, Inc.
+import functools
+import re
+import threading
+
+FIRST_CAP_RE = re.compile('(.)([A-Z][a-z]+)')
+ALL_CAP_RE = re.compile('([a-z0-9])([A-Z])')
+
+
+def debounce(interval_s):
+    """Debounce calls to this function until interval_s seconds have passed."""
+    def wrapper(func):
+        @functools.wraps(func)
+        def debounced(*args, **kwargs):
+            if hasattr(debounced, '_timer'):
+                debounced._timer.cancel()
+            debounced._timer = threading.Timer(interval_s, func, args, kwargs)
+            debounced._timer.start()
+        return debounced
+    return wrapper
+
+
+def camel_to_underscore(string):
+    s1 = FIRST_CAP_RE.sub(r'\1_\2', string)
+    return ALL_CAP_RE.sub(r'\1_\2', s1).lower()
+
+
+def list_to_string(value):
+    return ",".join(value) if type(value) == list else value
---
+++ packages/python-language-server/python-language-server-0.9.0.tar.gz/python-language-server-0.9.0/test/test_utils.py
@@ -0,0 +1,36 @@
+# Copyright 2017 Palantir Technologies, Inc.
+import time
+from pyls import _utils
+
+
+def test_debounce():
+    interval = 0.1
+
+    @_utils.debounce(0.1)
+    def call_m():
+        call_m._count += 1
+
+    call_m._count = 0
+
+    call_m()
+    call_m()
+    call_m()
+    assert call_m._count == 0
+
+    time.sleep(interval * 2)
+    call_m()
+    assert call_m._count == 1
+
+    time.sleep(interval * 2)
+    call_m()
+    assert call_m._count == 2
+
+
+def test_list_to_string():
+    assert _utils.list_to_string("string") == "string"
+    assert _utils.list_to_string(["a", "r", "r", "a", "y"]) == "a,r,r,a,y"
+
+
+def test_camel_to_underscore():
+    assert _utils.camel_to_underscore("camelCase") == "camel_case"
+    assert _utils.camel_to_underscore("under_score") == "under_score"
