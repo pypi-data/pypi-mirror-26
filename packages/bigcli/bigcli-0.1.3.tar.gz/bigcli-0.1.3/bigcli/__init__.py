7c7e8589623d6da68de7114bc03094f67f07ba1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:02 +0000

    Add package bigcli/0.1.3/bigcli-0.1.3.tar.gz

---
+++ packages/bigcli/bigcli-0.1.3.tar.gz/bigcli-0.1.3/bigcli/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for bigcli."""
+
+__author__ = """Duda Dornelles"""
+__email__ = 'duda@thoughtworks.com'
+__version__ = '0.1.0'
+
+from bigcli import BigCli, arg
