30cded24897d98dc2efe4ae2a2e2480ce6e8b541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:39 +0000

    Add package PyMeasure/0.5/PyMeasure-0.5-py3-none-any.whl

---
+++ packages/PyMeasure/PyMeasure-0.5-py3-none-any.whl/pymeasure/display/plotter.py
@@ -0,0 +1,57 @@
+#
+# This file is part of the PyMeasure package.
+#
+# Copyright (c) 2013-2017 PyMeasure Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import logging
+
+import sys
+import time
+
+from .Qt import QtGui
+from .windows import PlotterWindow
+from ..process import StoppableProcess
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+
+class Plotter(StoppableProcess):
+    """ Plotter dynamically plots data from a file through the Results
+    object and supports error bars.
+    """
+
+    def __init__(self, results, refresh_time=0.1):
+        super(Plotter, self).__init__()
+        self.results = results
+        self.refresh_time = refresh_time
+
+    def run(self):
+        app = QtGui.QApplication(sys.argv)
+        window = PlotterWindow(self, refresh_time=self.refresh_time)
+        app.aboutToQuit.connect(window.quit)
+        window.show()
+        app.exec_()
+
+    def wait_for_close(self, check_time=0.1):
+        while not self.should_stop():
+            time.sleep(check_time)
