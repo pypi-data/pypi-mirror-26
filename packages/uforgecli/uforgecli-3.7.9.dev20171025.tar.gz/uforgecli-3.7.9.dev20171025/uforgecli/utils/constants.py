c9d58d01042889555acdb0b74b3b8a86c8f69107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:35 +0000

    Add package uforgecli/3.7.9.dev20171025/uforgecli-3.7.9.dev20171025.tar.gz

---
+++ packages/uforgecli/uforgecli-3.7.9.dev20171025.tar.gz/uforgecli-3.7.9.dev20171025/uforgecli/utils/constants.py
@@ -0,0 +1,17 @@
+__author__="UShareSoft"
+
+PROJECT_VERSION='3.7.9-SNAPSHOT'
+PROJECT_NAME='uforgecli'
+
+PROJECT_NIGHTLY_VERSION_PREFIX='3.7.9.dev'
+
+PYTHON_SDK='uforge_python_sdk'
+
+HTTP_TIMEOUT=30
+
+QUOTAS_SCAN="scan"
+QUOTAS_TEMPLATE="appliance"
+QUOTAS_GENERATION="generation"
+QUOTAS_DISK_USAGE="diskusage"
+
+DEFAULT_URL="http://localhost:8080/ufws"
