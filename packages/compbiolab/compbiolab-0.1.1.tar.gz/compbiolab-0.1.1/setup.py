282e54050fa8935c01145ce70932dc2503c0db0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:55 +0000

    Add package compbiolab/0.1.1/compbiolab-0.1.1.tar.gz

---
+++ packages/compbiolab/compbiolab-0.1.1.tar.gz/compbiolab-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='compbiolab',
+      version='0.1.1',
+      description='The CompBioLab python module is a python library that has several functions that allow saving work time and headaches to CompBioLab researchers. This module also helps to make things cool, canonic and Osuna-friendly.',
+      url='https://github.com/miqueleg/compbiolab',
+      author='Miquel Estévez',
+      author_email='miquel.estevez@udg.edu',
+      license='MIT',
+      packages=['compbiolab'],
+      zip_safe=False)
