f3ba36b3e5d990465c0eda76a2d78c0c9731f32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:50 +0000

    Add package tmdbv3api/0.5/tmdbv3api-0.5-py2.py3-none-any.whl

---
+++ packages/tmdbv3api/tmdbv3api-0.5-py2.py3-none-any.whl/tmdbv3api/__init__.py
@@ -0,0 +1,6 @@
+from .tmdb import TMDb
+from .objs.movie import Movie
+from .objs.discover import Discover
+from .objs.person import Person
+from .objs.tv import TV
+from .objs.collection import Collection
