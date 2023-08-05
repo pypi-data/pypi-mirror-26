98a9687090e581a4a02ffdabb23af4fd2a3d6ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:50 +0000

    Add package MongoFrames/1.2.8/MongoFrames-1.2.8.tar.gz

---
+++ packages/MongoFrames/MongoFrames-1.2.8.tar.gz/MongoFrames-1.2.8/mongoframes/__init__.py
@@ -0,0 +1,8 @@
+from mongoframes.frames import *
+from mongoframes.pagination import *
+from mongoframes.queries import *
+from pymongo import (
+    IndexModel,
+    ASCENDING as ASC,
+    DESCENDING as DESC
+)
