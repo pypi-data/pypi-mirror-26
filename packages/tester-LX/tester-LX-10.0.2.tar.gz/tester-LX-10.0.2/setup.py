c0a1ce7127d2f7b086c5a473d5af1f2e1d2abac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:32 +0000

    Add package tester-LX/10.0.2/tester-LX-10.0.2.tar.gz

---
+++ packages/tester-LX/tester-LX-10.0.2.tar.gz/tester-LX-10.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'tester-LX',
+    version     = '10.0.2',
+    py_modules  = ['tester-LX'],
+    author      = 'hfpython',
+    author_email = 'hfpython@headfirstlabs.com',
+    url         = 'http://www.headfirstlabs.com',
+    description = 'a simple printer of nested lists',
+    )
