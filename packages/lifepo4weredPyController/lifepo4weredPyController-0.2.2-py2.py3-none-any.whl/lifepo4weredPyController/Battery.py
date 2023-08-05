59e2312d06f9544f774cc6005cfefe66415e82cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:35 +0000

    Add package lifepo4weredPyController/0.2.2/lifepo4weredPyController-0.2.2-py2.py3-none-any.whl

---
+++ packages/lifepo4weredPyController/lifepo4weredPyController-0.2.2-py2.py3-none-any.whl/lifepo4weredPyController/Battery.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from observablePy import Observable, observable_property
+import lifepo4weredPy
+import copy
+
+BATTERY_FULL = 3392  # mVolt
+
+
+class Battery(Observable):
+    def __init__(self):
+        super(Battery, self).__init__()
+        self.instanceState = {
+            "voltage": 0,
+            "rate": 0.0,
+        }
+
+        self.addObservableElement("voltage")
+        self.addObservableElement("rate")
+
+    @property
+    def voltage(self):
+        return lifepo4weredPy.read(lifepo4weredPy.variablesEnum.VBAT)
+
+    @property
+    def rate(self):
+        return self._computeRate(self.voltage)
+
+    def _computeRate(self, actualVoltage):
+        shutdownVoltage = lifepo4weredPy.read(
+                                lifepo4weredPy.variablesEnum.VBAT_SHDN)
+
+        batteryNomalizedVolt = (actualVoltage
+                                if actualVoltage < BATTERY_FULL
+                                else BATTERY_FULL) - shutdownVoltage
+
+        if (batteryNomalizedVolt <= 0):
+            return 0
+        else:
+            return batteryNomalizedVolt / (BATTERY_FULL - shutdownVoltage)
+
+    def _diffuseChanges(self):
+        voltage = self.voltage
+
+        if (voltage != self.instanceState["voltage"]):
+            previousState = copy.deepcopy(self.instanceState)
+
+            self.instanceState["voltage"] = voltage
+            self.instanceState["rate"] = self._computeRate(voltage)
+
+            self.diffuse(previousState, self.instanceState)
---
+++ packages/lifepo4weredPyController/lifepo4weredPyController-0.2.2-py2.py3-none-any.whl/lifepo4weredPyController/Lifepo4wered.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from observablePy import Observable, observable_property
+import copy
+
+
+class Lifepo4wered(Observable):
+    def __init__(self):
+        super(Lifepo4wered, self).__init__()
+        self.instanceState = {
+            "batteryVoltage": 0,
+            "batteryRate": 0.0,
+            "usbPowerSourceVoltage": 0,
+            "usbPowerSourcePluggedIn": False,
+            "touchState": -1
+        }
+        self._previousInstanceState = {}
+
+        self.addObservableElement("state")
+
+    def _diffuseChanges(self):
+        if self.hasObservers():
+            self.diffuse(self._previousInstanceState, self.instanceState)
+            self._previousInstanceState = copy.deepcopy(self.instanceState)
