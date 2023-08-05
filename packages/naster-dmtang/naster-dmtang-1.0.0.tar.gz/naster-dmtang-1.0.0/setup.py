9c2cad2c1579c69b592242d39ffb3f1944907a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:27 +0000

    Add package naster-dmtang/1.0.0/naster-dmtang-1.0.0.tar.gz

---
+++ packages/naster-dmtang/naster-dmtang-1.0.0.tar.gz/naster-dmtang-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'naster-dmtang',
+        version = '1.0.0',
+        py_modles = ['naster'],
+        author = 'dmtang',
+        author_email = 'dmtang@gmail.com',
+        url = 'http://bbs.nju.edu.cn',
+        description = 'A simple printer of nasted lists',
+     )
