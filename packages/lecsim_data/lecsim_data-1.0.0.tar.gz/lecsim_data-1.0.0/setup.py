521e33d0607684efc22b3889526eba53192d9ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:56 +0000

    Add package lecsim_data/1.0.0/lecsim_data-1.0.0.tar.gz

---
+++ packages/lecsim_data/lecsim_data-1.0.0.tar.gz/lecsim_data-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name       = 'lecsim_data',
+    version    = '1.0.0',
+    py_modules = ['lecsim_data'],
+    author     = 'lecsim',
+    author_email = 'lechler@gmx.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'Data-Example',
+)
