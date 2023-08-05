b8f7390dd314a4f82b125744300d64f405cd45ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:13 +0000

    Add package plonecli/0.1.0a3/plonecli-0.1.0a3.tar.gz

---
+++ packages/plonecli/plonecli-0.1.0a3.tar.gz/plonecli-0.1.0a3/HISTORY.rst
@@ -0,0 +1,24 @@
+History
+=======
+
+
+
+0.1.0a3 (2017-10-24)
+--------------------
+
+- Update README to use easy_install instead of pip for now
+  [MrTango]
+
+
+0.1.0a2 (2017-10-24)
+--------------------
+
+- fix setup.py to use the github version of click, until click >6.7 is released
+  [MrTango]
+
+
+0.1.0a1 (2017-10-24)
+--------------------
+
+- initital version with list templates support and bobtemplates.plone integration
+  [MrTango, tmassman, Gomez]
---
+++ packages/plonecli/plonecli-0.1.0a3.tar.gz/plonecli-0.1.0a3/tests/test_registry.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `plonecli` package."""
+import os
+
+from plonecli.registry import TemplateRegistry
+from plonecli.registry import get_package_root
+
+
+def test_list_templates(tmpdir):
+    os.chdir(tmpdir.strpath)
+    reg = TemplateRegistry()
+    template_str = reg.list_templates()
+    assert '- buildout' in template_str
+    assert '- addon' in template_str
+    assert '- theme' in template_str
+    assert '- content_type' in template_str
+
+
+def test_get_package_root(tmpdir):
+    target_dir = tmpdir.strpath + '/collective.foo'
+    os.mkdir(target_dir)
+
+    package_root = get_package_root()
+    assert package_root is None
+
+    os.chdir(target_dir)
+    template = """[check-manifest]
+check=True
+
+"""
+    with open(os.path.join(target_dir + '/setup.cfg'), 'w') as f:
+        f.write(template)
+
+    package_root = get_package_root()
+    assert package_root is None
+
+    # test with correct setup.cfg:
+    template = """[check-manifest]
+check=True
+
+[tool:bobtemplates.plone]
+template=plone_addon
+"""
+    with open(os.path.join(target_dir + '/setup.cfg'), 'w') as f:
+        f.write(template)
+
+    package_root = get_package_root()
+    assert package_root is not None
+
+
+def test_get_subtemplates(tmpdir):
+    """ test get_templates inside of a package
+    """
+    template = """[check-manifest]
+check=True
+
+[tool:bobtemplates.plone]
+template=plone_addon
+"""
+    target_dir = tmpdir.strpath + '/collective.foo'
+    os.mkdir(target_dir)
+    with open(os.path.join(target_dir + '/setup.cfg'), 'w') as f:
+        f.write(template)
+    os.chdir(target_dir)
+    reg = TemplateRegistry()
+    templates = reg.get_templates()
+    assert 'content_type' in templates
+    assert 'theme' in templates
+    assert 'vocabulary' in templates
+
+
+def test_get_templates(tmpdir):
+    """ test get_templates outside of a package
+    """
+    os.chdir(tmpdir.strpath)
+    reg = TemplateRegistry()
+    templates = reg.get_templates()
+    assert 'addon' in templates
+    assert 'theme_package' in templates
+    assert 'buildout' in templates
+
+
+def test_resolve_template_name(tmpdir):
+    """ test resolving template names from plonecli alias
+    """
+    plonecli_alias = 'addon'
+    reg = TemplateRegistry()
+    template_name = reg.resolve_template_name(plonecli_alias)
+    assert 'bobtemplates.plone:addon' == template_name
