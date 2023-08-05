8c35a6cdf9efe7463fe783d5ee995b6084cf1d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:09 +0000

    Add package lecsim-nester/1.4.0/lecsim_nester-1.4.0.tar.gz

---
+++ packages/lecsim-nester/lecsim_nester-1.4.0.tar.gz/lecsim_nester-1.4.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name       = 'lecsim_nester',
+    version    = '1.4.0',
+    py_modules = ['lecsim_nester'],
+    author     = 'lecsim',
+    author_email = 'lechler@gmx.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+)
