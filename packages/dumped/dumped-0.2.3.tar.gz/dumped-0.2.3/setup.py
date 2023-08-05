96f085f7c80e83985cad55b0a3db5800796660d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:14 +0000

    Add package dumped/0.2.3/dumped-0.2.3.tar.gz

---
+++ packages/dumped/dumped-0.2.3.tar.gz/dumped-0.2.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name = 'dumped',
+    packages = ['dumped'],
+    version = '0.2.3',
+    description = 'Easy & lightweight objects persistence',
+    long_description = 'See GitHub repo for detailed description (sorry, but PyPI don\'t support Markdown).',
+    author = 'GoldsteinE',
+    author_email = 'mouse-art@ya.ru',
+    license='MIT',
+    url = 'https://github.com/GoldsteinE/dumped',
+    download_url = 'https://github.com/GoldsteinE/dumped/archive/0.2.3.tar.gz',
+    keywords = 'dumping json persistence lightweight',
+    classifiers = ['Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'License :: OSI Approved :: MIT License'],
+)
