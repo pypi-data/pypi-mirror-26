0d9496a211bbedb9e03d76d94509814fc1d290c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:42 +0000

    Add package vpicwrapper/0.1/vpicwrapper-0.1.tar.gz

---
+++ packages/vpicwrapper/vpicwrapper-0.1.tar.gz/vpicwrapper-0.1/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='vpicwrapper', version='0.1', description="A minimal wrapper around the the NHTSA's VIN decoder API. https://vpic.nhtsa.dot.gov/",url="https://github.com/lxpollara/pyvpic", license="GNU", pakages=['vpicwrapper'], zip_safe=False)
