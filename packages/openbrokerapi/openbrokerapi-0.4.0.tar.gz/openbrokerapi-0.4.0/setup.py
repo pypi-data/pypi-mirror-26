1a8d18d9caac236d19451f81cd8c13389bbd6c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:43 +0000

    Add package openbrokerapi/0.4.0/openbrokerapi-0.4.0.tar.gz

---
+++ packages/openbrokerapi/openbrokerapi-0.4.0.tar.gz/openbrokerapi-0.4.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name='openbrokerapi',
+    version='0.4.0',
+    packages=find_packages(),
+    install_requires=['flask'],
+    test_suite="test",
+
+    # Metadata
+    author="Maic Siemering",
+    author_email="maic@siemering.tech",
+    license="MIT",
+    description="A python package for the V2 CF Service Broker API and Open Broker API (version 2.10+)",
+    long_description=open('README.rst').read(),
+    url="https://github.com/eruvanos/openbrokerapi",
+    keywords=["cloudfoundry", "cfbrokerapi", "openbrokerapi", "openservicebrokerapi", "servicebroker", "flask"],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+    ],
+    extras_require={
+        'docs': [
+            'sphinx >= 1.4',
+            'sphinx_rtd_theme']}
+
+)
---
+++ packages/openbrokerapi/openbrokerapi-0.4.0.tar.gz/openbrokerapi-0.4.0/test/__init__.py
@@ -0,0 +1,28 @@
+import base64
+from unittest.mock import Mock
+
+import logging
+from flask.app import Flask
+from flask_testing import TestCase
+
+from openbrokerapi.log_util import basic_config
+from openbrokerapi.api import BrokerCredentials
+from openbrokerapi.service_broker import ServiceBroker
+
+
+class BrokerTestCase(TestCase):
+    auth_header = 'Basic ' + base64.b64encode(b":").decode("ascii")
+
+    def create_app(self):
+        from openbrokerapi.api import get_blueprint
+
+        app = Flask(__name__)
+        self.broker: ServiceBroker = Mock()
+
+        app.register_blueprint(
+            get_blueprint(self.broker,
+                          BrokerCredentials("", ""),
+                          basic_config(level=logging.WARN)
+                          )
+        )
+        return app
