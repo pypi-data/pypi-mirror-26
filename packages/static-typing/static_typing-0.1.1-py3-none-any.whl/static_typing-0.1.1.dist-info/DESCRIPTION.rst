fd7445c598102dee68228a9ec33d6e285afe771b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:11 +0000

    Add package static-typing/0.1.1/static_typing-0.1.1-py3-none-any.whl

---
+++ packages/static-typing/static_typing-0.1.1-py3-none-any.whl/static_typing-0.1.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,47 @@
+.. role:: python(code)
+    :language: python
+
+
+static-typing
+=============
+
+.. image:: https://img.shields.io/pypi/v/static-typing.svg
+    :target: https://pypi.python.org/pypi/static-typing
+    :alt: package version from PyPI
+
+.. image:: https://travis-ci.org/mbdevpl/static-typing.svg?branch=master
+    :target: https://travis-ci.org/mbdevpl/static-typing
+    :alt: build status from Travis CI
+
+.. image:: https://ci.appveyor.com/api/projects/status/github/mbdevpl/static-typing?branch=master&svg=true
+    :target: https://ci.appveyor.com/project/mbdevpl/static-typing
+    :alt: build status from AppVeyor
+
+.. image:: https://api.codacy.com/project/badge/Grade/c10705787cbf4ebeafa95d18459fd690
+    :target: https://www.codacy.com/app/mbdevpl/static-typing
+    :alt: grade from Codacy
+
+.. image:: https://codecov.io/gh/mbdevpl/static-typing/branch/master/graph/badge.svg
+    :target: https://codecov.io/gh/mbdevpl/static-typing
+    :alt: test coverage from Codecov
+
+.. image:: https://img.shields.io/pypi/l/static-typing.svg
+    :target: https://github.com/mbdevpl/static-typing/blob/master/NOTICE
+    :alt: license
+
+Attempt to add static type information to Python abstract syntax trees.
+
+Extends parser from ``typed_ast`` module.
+
+requirements
+------------
+
+Python version >= 3.4.
+
+Python libraries as specified in `<requirements.txt>`_.
+
+Building and running tests additionally requires packages listed in `<test_requirements.txt>`_.
+
+Tested on Linux and Windows.
+
+
---
+++ packages/static-typing/static_typing-0.1.1-py3-none-any.whl/static_typing/static_typer.py
@@ -0,0 +1,46 @@
+
+import ast
+import sys
+
+import typed_ast.ast3
+
+from .ast_manipulation import RecursiveAstTransformer
+from .nodes import \
+    StaticallyTypedModule, StaticallyTypedFunctionDef, StaticallyTypedClassDef, \
+    StaticallyTypedAssign, StaticallyTypedAnnAssign, StaticallyTypedFor, StaticallyTypedWith
+# , StaticallyTypedWhile, StaticallyTypedIf
+
+def create_static_typer(ast_module):
+
+    class StaticTyperClass(RecursiveAstTransformer[ast_module]):
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, fields_first=True, **kwargs)
+
+        nodes_to_be_typed = {
+            ast_module.Module: StaticallyTypedModule,
+            ast_module.FunctionDef: StaticallyTypedFunctionDef,
+            ast_module.ClassDef: StaticallyTypedClassDef,
+            ast_module.Assign: StaticallyTypedAssign,
+            ast_module.For: StaticallyTypedFor,
+            # ast_module.While: StaticallyTypedWhile,
+            # ast_module.If: StaticallyTypedIf,
+            ast_module.With: StaticallyTypedWith
+            }
+
+        if ast_module is not ast or sys.version_info[:2] >= (3, 6):
+            nodes_to_be_typed[ast_module.AnnAssign] = StaticallyTypedAnnAssign
+
+        def visit_node(self, node):
+            """Introduce static typing information to compatible nodes of the AST."""
+            node_type = type(node)
+            if node_type in self.nodes_to_be_typed:
+                return self.nodes_to_be_typed[node_type][ast_module].from_other(node)
+            return node
+
+    return StaticTyperClass
+
+
+StaticTyper = {
+    ast_module: create_static_typer(ast_module)
+    for ast_module in (ast, typed_ast.ast3)}
