f09dd9783bdb4f6375f3864ca4b28d8ab8a0d5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:33 +0000

    Add package zuoyeji/0.0.0/zuoyeji-0.0.0.tar.gz

---
+++ packages/zuoyeji/zuoyeji-0.0.0.tar.gz/zuoyeji-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='zuoyeji',
+    version='0.0.0',
+    description='homework machine',
+    url='https://github.com/zhihuijin/zuoyeji',
+    packages=['zuoyeji'],
+)
