5d7e4e1670acac946e5634b8341b7936ebf5b68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:47 +0000

    Add package dothub/0.16.1/dothub-0.16.1-py2.py3-none-any.whl

---
+++ packages/dothub/dothub-0.16.1-py2.py3-none-any.whl/dothub/__main__.py
@@ -0,0 +1,11 @@
+import logging
+from . import config, cli
+
+
+def main():
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
+    conf = config.load_config()
+    cli.dothub(obj={}, default_map=conf)
+
+if __name__ == '__main__':  # pragma: no cover
+    exit(main())
