66d1ecd3aae95523938ae2052186974cf4333dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:41 +0000

    Add package onegov.core/0.51.1/onegov.core-0.51.1.tar.gz

---
+++ packages/onegov.core/onegov.core-0.51.1.tar.gz/onegov.core-0.51.1/onegov/core/security/rules.py
@@ -0,0 +1,38 @@
+from onegov.core.framework import Framework
+
+
+@Framework.permission_rule(model=object, permission=object, identity=None)
+def has_permission_not_logged_in(app, identity, model, permission):
+    """ This catch-all rule returns the default permission rule. It says
+    that the permission must be part of the anonymous rule.
+
+    Models with a 'is_hidden_from_public' property are not viewable by
+    anonymous users, if said property is set to True. If it is set to False,
+    the usual permission check kicks in.
+
+    """
+
+    if getattr(model, 'is_hidden_from_public', False):
+        return False
+
+    return permission in app.settings.roles.anonymous
+
+
+@Framework.permission_rule(model=object, permission=object)
+def has_permission_logged_in(app, identity, model, permission):
+    """ This permission rule matches all logged in identities. It requires
+    the identity to have a 'role' attribute. Said role attribute is used
+    to determine if the given permission is part of the given role.
+
+    """
+
+    assert hasattr(identity, 'role'), """
+        the identity needs to implement a role property
+    """
+
+    # basic members do not get access to hidden from public
+    if identity.role == 'member':
+        if getattr(model, 'is_hidden_from_public', False):
+            return False
+
+    return permission in getattr(app.settings.roles, identity.role)
