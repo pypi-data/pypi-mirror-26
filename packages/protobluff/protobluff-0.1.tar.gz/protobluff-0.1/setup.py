24301ad2a41aad9b4ec7923b28996eb31b00f2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:25 +0000

    Add package protobluff/0.1/protobluff-0.1.tar.gz

---
+++ packages/protobluff/protobluff-0.1.tar.gz/protobluff-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'protobluff',
+  packages = ['protobluff'],
+  version = '0.1',
+  description = 'A random lib',
+  author = 'Mark Nemec',
+  author_email = 'mrknmc@me.com',
+  url = 'https://github.com/mrknmc/protobluff',
+  download_url = 'https://github.com/mrknmc/protobluff/archive/0.1.tar.gz',
+  keywords = ['example'],
+  classifiers = [],
+)
