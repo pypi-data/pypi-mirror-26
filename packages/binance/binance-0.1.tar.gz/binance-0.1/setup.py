f15af92bc53b106cccde41ebce3c042d0a854082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:00 +0000

    Add package binance/0.1/binance-0.1.tar.gz

---
+++ packages/binance/binance-0.1.tar.gz/binance-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='binance',
+      version='0.1',
+      py_packages=['binance'],
+      description='Binance API wrapper',
+      url='https://github.com/toshima/binance',
+      author='Takaki Oshima')
+
