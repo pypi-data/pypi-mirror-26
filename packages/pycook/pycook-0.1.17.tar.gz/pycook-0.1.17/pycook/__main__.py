d76e13be689a21a03855c22b48349a0854bb01fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package pycook/0.1.17/pycook-0.1.17.tar.gz

---
+++ packages/pycook/pycook-0.1.17.tar.gz/pycook-0.1.17/pycook/__main__.py
@@ -0,0 +1,33 @@
+#* Imports
+import sys
+import os
+import subprocess
+import pycook as pc
+from pycook import elisp as el
+
+#* Functions
+def script_describe(book):
+    return "usage: cook <recipe>\n\nAvailable recipes:\n" + \
+        pc.cook.recipe_names(book)
+
+def script_get_book():
+    dd = el.default_directory()
+    d1 = el.locate_dominating_file(dd, "Cookbook.py")
+    d2 = el.locate_dominating_file(dd, "cook/Cookbook.py")
+    if d1:
+        return d1
+    elif d2:
+        return d2
+    else:
+        raise RuntimeError("No Cookbook.py or cook/Cookbook.py found")
+
+def main(argv = None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        book = script_get_book()
+        os.chdir(el.file_name_directory(book))
+        pc.main(argv, book)
+    except subprocess.CalledProcessError as e:
+        print(e)
+        sys.exit(e.returncode)
