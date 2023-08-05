3856dda68468b151ffd721d429768738389ca990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:10 +0000

    Add package pidan/1.0.60/pidan-1.0.60.zip

---
+++ packages/pidan/pidan-1.0.60.zip/pidan-1.0.60/setup.py
@@ -0,0 +1,16 @@
+# install:python setup.py sdist upload
+# -*- coding:utf-8 -*-
+from distutils.core import setup
+ 
+setup (
+    name = 'pidan',
+    version = '1.0.60',
+    packages=["pidan"],
+    platforms=['any'],
+    author = 'chengang',
+    author_email = 'chengang.net@gmail.com',
+    description = u'修改http模块',
+    package_data = {
+        '': ['*.data'],
+    },
+)
