1d2adb47aa18edcb3306105efe311fc60bb14185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:00 +0000

    Add package aiohttp-json-rpc/0.8.5/aiohttp-json-rpc-0.8.5.tar.gz

---
+++ packages/aiohttp-json-rpc/aiohttp-json-rpc-0.8.5.tar.gz/aiohttp-json-rpc-0.8.5/aiohttp_json_rpc/exceptions.py
@@ -0,0 +1,34 @@
+class RpcError(Exception):
+    MESSAGE = ''
+
+    def __init__(self, msg_id=None, data=None):
+        self.data = data
+        self.msg_id = msg_id
+
+    def __str__(self):
+        return self.MESSAGE
+
+
+class RpcInvalidRequestError(RpcError):
+    ERROR_CODE = -32600
+    MESSAGE = 'Invalid request'
+
+
+class RpcMethodNotFoundError(RpcError):
+    ERROR_CODE = -32601
+    MESSAGE = 'Method not found'
+
+
+class RpcInternalError(RpcError):
+    ERROR_CODE = -32603
+    MESSAGE = 'Internal Error'
+
+
+class RpcInvalidParamsError(RpcError):
+    ERROR_CODE = -32602
+    MESSAGE = 'Invalid params'
+
+
+class RpcParseError(RpcError):
+    ERROR_CODE = -32700
+    MESSAGE = 'Invalid JSON was received'
