908de41b4b7301fe98f9b0e41473baac9e98fd39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:23 +0000

    Add package pybugger/0.5/pybugger-0.5.tar.gz

---
+++ packages/pybugger/pybugger-0.5.tar.gz/pybugger-0.5/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pybugger',
+    version='0.5',
+    packages=['pybugger'],
+    url='https://github.com/fareskalaboud/pybugger',
+    license='GNU General Public License v3.0',
+    author='fareskalaboud',
+    author_email='fares.alaboud@gmail.com',
+    description='Pybugger is an easy to use color-supported CLI debugging tool for python.'
+)
