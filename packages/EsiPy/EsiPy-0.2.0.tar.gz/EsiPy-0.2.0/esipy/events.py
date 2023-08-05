c11dcc63da9438a32e4d528ed2fa4a76ce7b617d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:33 +0000

    Add package EsiPy/0.2.0/EsiPy-0.2.0.tar.gz

---
+++ packages/EsiPy/EsiPy-0.2.0.tar.gz/EsiPy-0.2.0/esipy/events.py
@@ -0,0 +1,59 @@
+# -*- encoding: utf-8 -*-
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+class Signal(object):
+    def __init__(self):
+        """ Alarm constructor. """
+        self.event_receivers = []
+
+    def add_receiver(self, receiver):
+        """ Add a receiver to the list of receivers.
+
+        :param receiver: a callable variable
+        """
+        if not callable(receiver):
+            raise TypeError("receiver must be callable")
+        self.event_receivers.append(receiver)
+
+    def remove_receiver(self, receiver):
+        """ Remove a receiver to the list of receivers.
+
+        :param receiver: a callable variable
+        """
+        if receiver in self.event_receivers:
+            self.event_receivers.remove(receiver)
+
+    def send(self, **kwargs):
+        """ Trigger all receiver and pass them the parameters
+        If an exception is raised, it will stop the process and all receivers
+        may not be triggered at this moment.
+
+        :param kwargs: all arguments from the event.
+        """
+        for receiver in self.event_receivers:
+            receiver(**kwargs)
+
+    def send_robust(self, **kwargs):
+        """ Trigger all receiver and pass them the parameters
+        If an exception is raised it will be catched and displayed as error
+        in the logger (if defined).
+
+        :param kwargs: all arguments from the event.
+        """
+        for receiver in self.event_receivers:
+            try:
+                receiver(**kwargs)
+            except Exception as err:
+                if not hasattr(err, '__traceback__'):
+                    logger.error(sys.exc_info()[2])
+                else:
+                    logger.error(err.__traceback__)
+
+
+# define required alarms
+after_token_refresh = Signal()
+api_call_stats = Signal()
---
+++ packages/EsiPy/EsiPy-0.2.0.tar.gz/EsiPy-0.2.0/esipy/exceptions.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+
+
+class APIException(Exception):
+    def __init__(self, url, code, json_response):
+        self.url = url
+        self.status_code = code
+        self.response = json_response
+        super(APIException, self).__init__(str(self))
+
+    def __str__(self):
+        if 'error' in self.response:
+            return 'HTTP Error %s: %s' % (self.status_code,
+                                          self.response['error'])
+        elif 'message' in self.response:
+            return 'HTTP Error %s: %s' % (self.status_code,
+                                          self.response['message'])
+        else:
+            return 'HTTP Error %s' % (self.status_code)
