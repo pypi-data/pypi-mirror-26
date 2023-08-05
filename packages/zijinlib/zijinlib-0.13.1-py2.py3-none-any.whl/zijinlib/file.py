1bd11623368ad9b4de78ee508b5e8ba100d8f20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:46 +0000

    Add package zijinlib/0.13.1/zijinlib-0.13.1-py2.py3-none-any.whl

---
+++ packages/zijinlib/zijinlib-0.13.1-py2.py3-none-any.whl/zijinlib/file.py
@@ -0,0 +1,43 @@
+import os
+import imghdr
+
+def sort(path, suffix):
+    filelist = []
+    for f in os.listdir(path):
+        if f.endswith(suffix):
+            filelist.append(path+f)
+    return filelist
+
+def detect_damage(filelist):
+    dellist = []
+    for f in filelist:
+        if not imghdr.what(f):
+            dellist.append(f)
+    return dellist
+
+def delete_files(dellist):
+    delnumsuccess = 0
+    delnumfail = 0
+    for delpath in dellist:
+        try:
+            os.remove(delpath)
+            delnumsuccess += 1
+            print('已删除'+delpath)
+        except Exception as e:
+            delnumfail += 1
+            print(delpath+'删除失败')
+    return delnumsuccess, delnumfail
+
+def compare_suffixes(sourcelist, targetlist):
+    targetlist_new = []
+    dellist = []
+    for t in targetlist:
+        target = t.split('.')[:-1]
+        targetlist_new.append(target)
+        
+    for s in sourcelist:
+        source = s.split('.')[:-1]
+        # 不存在对应的target文件则删除source文件
+        if source not in targetlist_new:
+            dellist.append(s)
+    return dellist
