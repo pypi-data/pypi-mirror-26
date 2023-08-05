a7b67f8b90ba7343e2b229f918ba8f10053eb810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:51 +0000

    Add package generators/2017.10.17/generators-2017.10.17.tar.gz

---
+++ packages/generators/generators-2017.10.17.tar.gz/generators-2017.10.17/generators/tee.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Author: Cody Kochmann
+# @Date:   2017-05-05 16:25:49
+# @Last Modified 2017-10-05e>
+# @Last Modified time: 2017-05-05 16:33:53
+
+from __future__ import print_function
+del print_function
+
+def tee(pipeline, name, output_function=print):
+    for i in pipeline:
+        output_function('{} - {}'.format(name,i))
+        yield i
+
+if __name__ == '__main__':
+    gen = (i for i in range(100))
+    gen = tee(gen,'step1')
+
+    gen = (i*2 for i in gen)
+    gen = tee(gen,'step2')
+
+    gen = (i**2 for i in gen)
+    gen = tee(gen,'step3')
+
+    gen = (i/2 for i in gen)
+    gen = tee(gen,'step4')
+
+    # run the generator
+    print(list(i for i in gen))
