04bc6e9812d30193d23d3e488dddb60dc2a93cf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:37 +0000

    Add package pynetworktables2js/2017.0.4/pynetworktables2js-2017.0.4.tar.gz

---
+++ packages/pynetworktables2js/pynetworktables2js-2017.0.4.tar.gz/pynetworktables2js-2017.0.4/pynetworktables2js/js/magicbot.js
@@ -0,0 +1,25 @@
+"use strict";
+
+if (d3 === undefined) {
+    alert("d3.js must be downloaded+included to use magicbot.js!");
+
+/**
+	This will setup all of the magicbot tunable variables in a visually
+
+
+	For best results, include magicbot.css in your page.
+*/
+function setupTunables(html_id) {
+
+	// state machines
+
+	function update(key, value, isNew) {
+
+	}
+
+	
+
+
+	// use d3 to do this
+
+}
