8bb4fbdf383255b0dba8d5aedf4c6697b65fce27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:16 +0000

    Add package biostructmap/0.2.2/biostructmap-0.2.2.tar.gz

---
+++ packages/biostructmap/biostructmap-0.2.2.tar.gz/biostructmap-0.2.2/biostructmap/__init__.py
@@ -0,0 +1,11 @@
+"""biostructmap is a package for performing analyses on Protein Data Bank
+(PDB) structure files. In particular, it allows for the spatial averaging
+of sequence-aligned data over a given protein structure, and also allows the
+incorporation of structural information into genetic tests of selection pressure
+such as Tajima's D.
+"""
+
+from .biostructmap import Structure, SequenceAlignment
+
+__version__ = '0.2.2'
+__all__ = ["biostructmap", "seqtools", "pdbtools", "gentests", "map_functions"]
