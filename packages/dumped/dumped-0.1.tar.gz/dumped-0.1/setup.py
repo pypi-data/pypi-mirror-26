41b99504e17660d5be813ab9d6fa45d992c07ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package dumped/0.1/dumped-0.1.tar.gz

---
+++ packages/dumped/dumped-0.1.tar.gz/dumped-0.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'dumped',
+    packages = ['dumped'],
+    version = '0.1',
+    description = 'Easy & lightweight objects persistence',
+    author = 'GoldsteinE',
+    author_email = 'mouse-art@ya.ru',
+    license='MIT',
+    url = 'https://github.com/GoldsteinE/dumped',
+    download_url = 'https://github.com/GoldsteinE/dumped/archive/0.1.tar.gz',
+    keywords = 'dumping json persistence lightweight',
+    classifiers = ['Programming Language :: Python :: 3',
+                   'License :: OSI Approved :: MIT License'],
+)
