ab0be9dc1040dd1578528b6f014ff73c873da165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:59 +0000

    Add package clifford/0.81/clifford-0.81.zip

---
+++ packages/clifford/clifford-0.81.zip/clifford-0.81/clifford/conv.py
@@ -0,0 +1,18 @@
+
+"""
+Conversion utitlities to/from other systems
+
+
+
+
+.. autosummary::
+    :toctree: generated/
+    
+    c2v
+    v2c
+"""
+
+
+def c2v(z, :
+    lambda x: (x.real*e1) + (x.imag*e2) # complex2vector
+v2c = lambda x: float(x|e1)+ float(x|e2)*1j
