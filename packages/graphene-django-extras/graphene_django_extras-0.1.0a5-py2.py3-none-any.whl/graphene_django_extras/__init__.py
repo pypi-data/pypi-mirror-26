81e2b7c877097b5669835c95f639e6feabb46596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:45 +0000

    Add package graphene-django-extras/0.1.0a5/graphene_django_extras-0.1.0a5-py2.py3-none-any.whl

---
+++ packages/graphene-django-extras/graphene_django_extras-0.1.0a5-py2.py3-none-any.whl/graphene_django_extras/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from graphene import get_version
+
+from .fields import DjangoObjectField, DjangoFilterListField, DjangoFilterPaginateListField, \
+    DjangoListObjectField
+from .mutation import DjangoSerializerMutation
+from .paginations import LimitOffsetGraphqlPagination, PageGraphqlPagination, CursorGraphqlPagination
+from .types import DjangoObjectType, DjangoInputObjectType, DjangoListObjectType
+from .subscriptions import *
+
+VERSION = (0, 1, 0, 'alpha', '5')
+
+__version__ = get_version(VERSION)
+
+__all__ = (
+    '__version__',
+    
+    # FIELDS
+    'DjangoObjectField',
+    'DjangoFilterListField',
+    'DjangoFilterPaginateListField',
+    'DjangoListObjectField',
+
+    # MUTATIONS
+    'DjangoSerializerMutation',
+
+    # PAGINATIONS
+    'LimitOffsetGraphqlPagination',
+    'PageGraphqlPagination',
+    # 'CursorGraphqlPagination',  # Not implemented yet
+
+    # TYPES
+    'DjangoObjectType',
+    'DjangoListObjectType',
+    'DjangoInputObjectType',
+
+    # SUBSCRIPTIONS
+    'Subscription',
+    'GraphqlAPIDemultiplexer'
+)
