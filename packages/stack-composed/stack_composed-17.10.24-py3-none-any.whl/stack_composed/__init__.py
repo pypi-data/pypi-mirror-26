5705ce8a3c3b34d946138d2320fcbc8751c82046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:52 +0000

    Add package stack-composed/17.10.24/stack_composed-17.10.24-py3-none-any.whl

---
+++ packages/stack-composed/stack_composed-17.10.24-py3-none-any.whl/stack_composed/__init__.py
@@ -0,0 +1,30 @@
+
+
+__version__ = '17.10.24'  # year.month.day
+
+
+header = \
+'''====================================================================
+
+StackComposed v{}
+
+  Compute and generate the composed of a raster images stack
+
+Run with '-h' for more options or visit the documentation:
+https://smbyc.bitbucket.io/stackcomposed/
+
+Copyright (C) 2017 Xavier Corredor Llano
+Sistema de Monitoreo de Bosques y Carbono - SMByC and FAO
+General Public License - GPLv3
+
+===================================================================='''.format(__version__)
+
+epilog = \
+'''
+For more information visit:
+https://smbyc.bitbucket.io/stackcomposed/
+
+StackComposed v{}
+Copyright (C) Xavier Corredor Llano <xcorredorl@ideam.gov.co>
+Sistema de Monitoreo de Bosques y Carbono - SMByC and FAO
+General Public License - GPLv3'''.format(__version__)
