084abcb30146c63e7cbd8ffb553ea7bcdb8d8244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:53 +0000

    Add package blunix-toolkit/0.1.2/blunix_toolkit-0.1.2.tar.gz

---
+++ packages/blunix-toolkit/blunix_toolkit-0.1.2.tar.gz/blunix_toolkit-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='blunix_toolkit',
+    version='0.1.2',
+    author='Brian Wiborg',
+    author_email='b.wiborg@blunix.org',
+    packages=['blunix_toolkit'],
+    scripts=[
+        'bin/blunix-config-bash',
+        'bin/blunix-config-cat',
+        'bin/blunix-config-edit',
+        'bin/blunix-gh-repos',
+        'bin/blunix-gh-roles',
+        'bin/blunix-gh-playbooks',
+        'bin/blunix-role-glance',
+    ],
+    url='http://github.com/blunix/blunix-toolkit',
+    license='LICENCE.txt',
+    long_description=open('README.md').read(),
+    keywords='tool development',
+    python_requires='>=2.7, <=3.0',
+    install_requires=[
+        'PyGithub >= 1.35'
+    ],
+)
