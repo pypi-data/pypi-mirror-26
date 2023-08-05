108993ec27f5b88d88befe93b62a06bf93fb64d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:26 +0000

    Add package mintlabs/0.2.582/mintlabs-0.2.582.tar.gz

---
+++ packages/mintlabs/mintlabs-0.2.582.tar.gz/mintlabs-0.2.582/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='mintlabs',
+    version='0.2.582',
+    url='http://www.mint-labs.com',
+    license='MIT',
+    author='Mint Labs',
+    author_email='tim@mint-labs.com',
+    description='This is a Python API to interact with the ' +
+                'Mint-Labs platform.',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3.4'
+                 ],
+    packages=find_packages(exclude=['docs', 'test*']),
+    install_requires=['requests==2.10.0']
+)
