5e9b853fe28f7a205a65ec15b5e138367a4d5eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:21 +0000

    Add package p3_bitbucket/0.1.8/p3_bitbucket-0.1.8.tar.gz

---
+++ packages/p3_bitbucket/p3_bitbucket-0.1.8.tar.gz/p3_bitbucket-0.1.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name="p3_bitbucket",
+    packages=['p3_bitbucket'],
+    modules=['p3_bitbucket'],
+    version="0.1.8",
+    description="Bitbucket API",
+    author="Andy Hsieh",
+    author_email="andy.hsieh@hotmail.com",
+    license='LICENSE.txt',
+    scripts=['bin/p3_bitbucket'],
+    url='https://github.com/bealox/p3-bitbucket',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests >= 2.18.3",
+        'setuptools'
+    ]
+
+)
