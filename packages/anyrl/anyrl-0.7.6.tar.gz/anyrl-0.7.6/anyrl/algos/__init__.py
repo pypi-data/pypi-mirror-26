530982a1f191ea03296a7d2257a19fdf5f574e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:43 +0000

    Add package anyrl/0.7.6/anyrl-0.7.6.tar.gz

---
+++ packages/anyrl/anyrl-0.7.6.tar.gz/anyrl-0.7.6/anyrl/algos/__init__.py
@@ -0,0 +1,12 @@
+"""
+Learning algorithms for RL.
+"""
+
+from .advantages import AdvantageEstimator, GAE
+from .a2c import A2C
+from .ppo import PPO
+
+# Don't import mpi by default since that breaks when
+# you don't run your program with `mpirun`.
+
+__all__ = dir()
