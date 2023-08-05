b193a934614b0291fd9fdfffd7981fbd274dd406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:36 +0000

    Add package darkslide/4.0.1/darkslide-4.0.1.tar.gz

---
+++ packages/darkslide/darkslide-4.0.1.tar.gz/darkslide-4.0.1/examples/restructuredtext/slides.rst
@@ -0,0 +1,149 @@
+Title Slide
+===========
+
+----
+
+Paragraphs
+==========
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies
+tempus ultricies. Ut porta scelerisque viverra. Pellentesque aliquam metus
+scelerisque dui ultricies, auctor dictum erat aliquet. Integer vehicula nunc
+elit, vel iaculis lorem facilisis non.
+
+Vivamus ante mauris, pellentesque et rhoncus ut, condimentum sed ipsum.
+Nullam eu molestie sapien. Curabitur imperdiet, ligula id blandit sagittis,
+libero quam consectetur felis, ac pharetra metus lectus vitae leo. Mauris
+lacinia fermentum augue.
+
+----
+
+Blockquotes
+===========
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies
+    tempus ultricies. Ut porta scelerisque viverra. Pellentesque aliquam metus
+    scelerisque dui ultricies, auctor dictum erat aliquet. Integer vehicula
+    nunc elit, vel iaculis lorem facilisis non.
+
+----
+
+Subheadings and Emphasis
+========================
+
+Italic
+------
+
+*Lorem ipsum dolor sit amet*
+
+Bold
+----
+
+**Lorem ipsum dolor sit amet**
+
+----
+
+First heading: *lorem ipsum* dolor sit
+======================================
+
+Second: *lorem ipsum* dolor sfi amet, consecte
+----------------------------------------------
+
+Third: *lorem ipsum* dolor sit amet, consectetur adipiscing
+```````````````````````````````````````````````````````````
+
+* 1
+* 2
+* 3
+* 4
+* 5
+* 6
+* 7
+
+----
+
+Lists
+=====
+
+Unordered List
+--------------
+
+- Markdown
+- ReStructured Text
+- Textile
+
+Ordered List
+------------
+
+1. Python
+2. JavaScript
+3. HTML5
+
+----
+
+Second Title Slide
+==================
+
+----
+
+Code
+====
+
+Single Word
+-----------
+
+Hello ``World``
+
+Python
+------
+
+.. code-block:: python
+
+    def multiply (x, y):
+        return x * y
+
+JavaScript
+----------
+
+.. code-block:: javascript
+
+    multiply: function (x, y) {
+      return x * y;
+    }
+
+HTML
+----
+
+.. code-block:: html
+
+    <!doctype html>
+    <html>
+      <head></head>
+      <body></body>
+    </html>
+
+----
+
+Images
+======
+
+.. image:: ../_assets/landscape.jpg
+
+----
+
+View Presenter Notes
+====================
+
+This slide has presenter notes. Press ``p`` to view them.
+
+Presenter Notes
+===============
+
+Hello from presenter notes
+
+----
+
+Other features
+==============
+
+View other features in the help sidebar by pressing ``h``
---
+++ packages/darkslide/darkslide-4.0.1.tar.gz/darkslide-4.0.1/setup.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+setup(
+    name='darkslide',
+    version='4.0.1',
+    license='Apache Software License 2.0',
+    description='Lightweight markup language-based html5 slideshow generator. Forked from Landslide.',
+    long_description='%s\n%s' % (
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
+        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    ),
+    author='Adam Zapletal',
+    author_email='adamzap@gmail.com',
+    maintainer='Ionel Cristian Mărieș',
+    maintainer_email='contact@ionelmc.ro',
+    url='https://github.com/ionelmc/python-darkslide',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    platforms=['any'],
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+        'Topic :: Text Processing :: Markup'
+    ],
+    keywords=[
+        'markdown',
+        'slideshow',
+        'presentation',
+        'rst',
+        'restructuredtext',
+        'textile',
+        'landslide',
+    ],
+    install_requires=[
+        'Jinja2',
+        'Markdown',
+        'Pygments',
+        'docutils',
+        'six',
+        'qrcode',
+    ],
+    extras_require={
+        # eg: 'rst': ['docutils>=0.11'],
+    },
+    entry_points={
+        'console_scripts': [
+            'darkslide = darkslide.cli:main',
+        ]
+    },
+)
