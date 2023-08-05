cb21c050c012947e41df99c743a6d9c541ab4590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:06 +0000

    Add package django-bootstrap-select/0.1.3/django-bootstrap-select-0.1.3.tar.gz

---
+++ packages/django-bootstrap-select/django-bootstrap-select-0.1.3.tar.gz/django-bootstrap-select-0.1.3/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+version = '0.1.3'
+
+
+if sys.argv[-1] == 'publish':
+    try:
+        import wheel
+        print("Wheel version: ", wheel.__version__)
+    except ImportError:
+        print('Wheel library missing. Please run "pip install wheel"')
+        sys.exit()
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on git:")
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='django-bootstrap-select',
+    version=version,
+    description="""A Django wrapper for the Bootstrap Select library""",
+    long_description=readme + '\n\n' + history,
+    author='Joshua Massover',
+    author_email='joshua.massover@gmail.com',
+    url='https://github.com/voxy/django-bootstrap-select',
+    packages=[
+        'bootstrap_select',
+    ],
+    include_package_data=True,
+    install_requires=[],
+    license="MIT",
+    zip_safe=False,
+    keywords='django-bootstrap-select',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
