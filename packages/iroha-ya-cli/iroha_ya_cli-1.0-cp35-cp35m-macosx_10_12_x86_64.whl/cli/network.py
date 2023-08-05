07f25be8233fd0b0cf5f8c93eb5cd40b98ac09a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:55 +0000

    Add package iroha-ya-cli/1.0/iroha_ya_cli-1.0-cp35-cp35m-macosx_10_12_x86_64.whl

---
+++ packages/iroha-ya-cli/iroha_ya_cli-1.0-cp35-cp35m-macosx_10_12_x86_64.whl/cli/network.py
@@ -0,0 +1,43 @@
+import base64
+import datetime
+
+import binascii
+import grpc
+
+from cli import crypto
+from endpoint_pb2_grpc import CommandServiceStub
+from primitive_pb2 import Signature
+from block_pb2 import Transaction
+
+
+def generateTransaction(account_id, commands, key_pair):
+    payload = Transaction.Payload(
+        commands=commands,
+        creator_account_id=account_id,
+        tx_counter=1,
+        created_time=int(datetime.datetime.now().timestamp() * 1000)
+    )
+    payload_hash = crypto.sha3_256(payload.SerializeToString())
+    sign = crypto.sign(key_pair, payload_hash)
+    tx = Transaction(
+        payload=payload,
+        signature=[
+            Signature(pubkey=key_pair.raw_public_key,signature=sign)
+        ]
+    )
+    return tx
+
+
+def sendTx(location, tx):
+    try:
+        channel = grpc.insecure_channel(location)
+        stub = CommandServiceStub(channel)
+        stub.Torii(tx, timeout=1)  # wait 1s
+        return True
+    except grpc.RpcError as e:
+        print("== Grpc happens error ==\n"
+              "- Server is active?: {} \n"
+              "- What's happen?   : {} \n"
+              .format(e.is_active(),e.details())
+        )
+        return False
