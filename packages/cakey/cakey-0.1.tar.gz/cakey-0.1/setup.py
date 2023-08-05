063a0c7fc9f65943dd55903027c32064386571dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:12 +0000

    Add package cakey/0.1/cakey-0.1.tar.gz

---
+++ packages/cakey/cakey-0.1.tar.gz/cakey-0.1/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name='cakey',
+      version='0.1',
+      description='scraping bitcoin values',
+	  url = 'https://www.github.com/Alvixeon/btc-py',
+      author='Alvixeon',
+      author_email='remusername@gmail.com',
+      license='MIT',
+      packages=['cobtc'],
+      zip_safe=False
+)
