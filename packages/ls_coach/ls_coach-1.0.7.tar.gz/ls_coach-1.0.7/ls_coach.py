4675b11cd4b71a9c5bab736241c3e1e54d9c56c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:17 +0000

    Add package ls_coach/1.0.7/ls_coach-1.0.7.tar.gz

---
+++ packages/ls_coach/ls_coach-1.0.7.tar.gz/ls_coach-1.0.7/ls_coach.py
@@ -0,0 +1,17 @@
+import sanitize
+"Prints the unique 3 best values of a list of data from a bunch of files"
+def coach (files):
+        try:
+                for every_file in files:
+                    with open (every_file) as file_athlet:
+                        data = file_athlet.readline()
+                        athlet_time=data.strip().split(',')
+                        data_clean=sorted(set([float(sanitize.sanitize(t)) for t in athlet_time]))
+                    print (every_file+': ')
+                    print (data_clean[0:3])         
+        except IOError as err:
+                print ('Fehler:' +str(err))
+        except ValueError as verr:
+                print ('Fehler: ' +str(verr))
+
+
