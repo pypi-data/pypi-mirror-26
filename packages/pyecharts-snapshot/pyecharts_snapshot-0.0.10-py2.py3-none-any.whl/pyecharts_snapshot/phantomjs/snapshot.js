be7dc89024d01ef18a4fae9e7d3c9ceaeb600ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:02 +0000

    Add package pyecharts-snapshot/0.0.10/pyecharts_snapshot-0.0.10-py2.py3-none-any.whl

---
+++ packages/pyecharts-snapshot/pyecharts_snapshot-0.0.10-py2.py3-none-any.whl/pyecharts_snapshot/phantomjs/snapshot.js
@@ -0,0 +1,21 @@
+var page = require('webpage').create();
+var system = require('system');
+
+var file = system.args[1];
+var file_type = system.args[2];
+var delay = system.args[3];
+
+var snapshot = "" + 
+"    function(){"+
+"        var ele = document.querySelector('div[_echarts_instance_]');"+
+"        var mychart = echarts.getInstanceByDom(ele);"+
+"        return mychart.getDataURL({type:'"+file_type+"', excludeComponents: ['toolbox']});"+
+"    }";
+
+page.open(file, function(){
+	window.setTimeout(function () {
+		var content = page.evaluateJavaScript(snapshot);
+		console.log(content);
+        phantom.exit();
+    }, delay);
+});
