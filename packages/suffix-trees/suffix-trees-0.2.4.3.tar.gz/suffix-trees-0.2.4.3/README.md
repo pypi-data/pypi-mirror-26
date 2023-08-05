a38ef1d83abb603c32da2c4b53088bbc7e945291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:22 +0000

    Add package suffix-trees/0.2.4.3/suffix-trees-0.2.4.3.tar.gz

---
+++ packages/suffix-trees/suffix-trees-0.2.4.3.tar.gz/suffix-trees-0.2.4.3/README.md
@@ -0,0 +1,22 @@
+# suffix_trees
+Python implementation of Suffix Trees and Generalized Suffix Trees. Provided also methods with typcal aplications of STrees and GSTrees. 
+
+### Installation
+pip install suffix-trees
+
+### Usage
+
+```python
+from suffix_trees import STree
+
+# Suffix-Tree example.
+st = STree.STree("abcdefghab")
+print(st.find("abc")) # 0
+print(st.find_all("ab")) # [0, 8]
+
+# Generalized Suffix-Tree example.
+a = ["xxxabcxxx", "adsaabc", "ytysabcrew", "qqqabcqw", "aaabc"]
+st = STree.STree(a)
+print(st.lcs()) # "abc"
+```
+
---
+++ packages/suffix-trees/suffix-trees-0.2.4.3.tar.gz/suffix-trees-0.2.4.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+import pypandoc
+long_description = pypandoc.convert('README.md', 'rst')
+
+setup(
+    name='suffix-trees',
+    packages=['suffix_trees'],
+    version='0.2.4.3',
+    description='Suffix trees, generalized suffix trees and string processing methods',
+    author='Peter Us',
+    author_email='ptrusr@gmail.com',
+    url='https://github.com/ptrus/suffix-trees',
+    long_description=long_description,
+    package_data={},
+    include_package_data=True,
+    install_requires=[
+        'pypandoc',    
+    ],
+    license='MIT',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+)
