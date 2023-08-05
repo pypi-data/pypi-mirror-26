05d8ba80be146830053e73faa8246d48268e9ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:06 +0000

    Add package muapi/0.1.3/muapi-0.1.3.tar.gz

---
+++ packages/muapi/muapi-0.1.3.tar.gz/muapi-0.1.3/muapi/Response.py
@@ -0,0 +1,17 @@
+from flask import Response
+import json
+import pickle
+
+class ResponseHandler(Response):
+    def __init__(self, content = None, *args, **kwargs):
+        """
+        Serialize content of the response
+        """
+        super(Response, self).__init__(content, *args, **kwargs)
+
+    @classmethod
+    def force_type(cls, rv, environ=None):
+        if isinstance(rv, (dict, list)):
+            return cls(rv)
+
+        return super(ResponseHandler, cls).force_type(rv, environ)
