c747d7fd48ae6263d05bdb90ca4e9fe74d7f0e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:43 +0000

    Add package function_timer/0.6/function_timer-0.6.tar.gz

---
+++ packages/function_timer/function_timer-0.6.tar.gz/function_timer-0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='function_timer',
+      version='0.6',
+      description='Timer for counting function execution time.',
+      url='https://github.com/wuling31715/module/blob/master/function_timer/demo.ipynb',
+      author='蕭擎軒',
+      author_email='wuling31715@gmail.com',
+      license='MIT',
+      packages=['timer'],
+      zip_safe=False)
