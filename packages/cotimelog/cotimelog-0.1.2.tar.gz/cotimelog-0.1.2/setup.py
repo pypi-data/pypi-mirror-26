8bba319b2f74aadc881833afc0c316ad577f2857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:12 +0000

    Add package cotimelog/0.1.2/cotimelog-0.1.2.tar.gz

---
+++ packages/cotimelog/cotimelog-0.1.2.tar.gz/cotimelog-0.1.2/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+from cotimelog import __author__, __version__
+
+
+requires = ['portalocker']
+
+setup(
+    name="cotimelog",
+    version=__version__,
+    author=__author__,
+    author_email="yaleidu93@gmail.com",
+    description="mixins for tornado",
+    license="Apache License, Version 2",
+    keywords=["log", 'concurrent'],
+    url="https://github.com/badbye/timelog.git",
+    packages=['cotimelog'],
+    install_requires=requires,
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
+
+
