b324847028c97282cd47b388d9a0d8ddab51da4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:41 +0000

    Add package asgi_ipc/1.4.2/asgi_ipc-1.4.2.tar.gz

---
+++ packages/asgi_ipc/asgi_ipc-1.4.2.tar.gz/asgi_ipc-1.4.2/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+__version__ = '1.4.2'
+
+# We use the README as the long_description
+readme_path = os.path.join(os.path.dirname(__file__), "README.rst")
+
+
+setup(
+    name='asgi_ipc',
+    version=__version__,
+    url='http://github.com/django/asgi_ipc/',
+    author='Django Software Foundation',
+    author_email='foundation@djangoproject.com',
+    description='Posix IPC-backed ASGI channel layer implementation',
+    long_description=open(readme_path).read(),
+    license='BSD',
+    zip_safe=False,
+    packages=["asgi_ipc"],
+    include_package_data=True,
+    install_requires=[
+        'six',
+        'posix_ipc>=1.0.0',
+        'msgpack-python',
+        'asgiref~=1.1.2',
+    ]
+)
