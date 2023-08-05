33103230b1c0dbe5132e0b82cd1bc1e4021ca6e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:31 +0000

    Add package unicorm/0.0.1/unicorm-0.0.1.tar.gz

---
+++ packages/unicorm/unicorm-0.0.1.tar.gz/unicorm-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='unicorm',
+    packages=['unicorm'],
+    version='0.0.1',
+    description='A simple asynchronous ORM',
+    author='Enrique Piña Monserrat',
+    author_email='lord.feratum@gmail.com',
+    url='https://github.com/LordFeratum/UnicORM',
+    download_url='https://github.com/LordFeratum/UnicORM/archive/0.0.1.tar.gz',
+    keywords=['async', 'orm', 'mysql'],
+    classifiers=[],
+)
---
+++ packages/unicorm/unicorm-0.0.1.tar.gz/unicorm-0.0.1/unicorm/types.py
@@ -0,0 +1,66 @@
+class AbstractType:
+    def __init__(self, *args, **kwargs):
+        self._value = None
+        self._args = args
+        self._kwargs = kwargs
+
+    def sql_cast(self, value):
+        return value
+
+    def get_sql_value(self):
+        return self._sql_cast(self._value)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+
+class ForeignKey:
+    def __init__(self, column):
+        self._column = column
+
+    @property
+    def parent_column(self):
+        return self._column
+
+
+class Int(AbstractType):
+    def sql_type(self):
+        return 'INTEGER'
+
+
+class Float(AbstractType):
+    def sql_type(self):
+        max_len = self._kwargs.get('max_length')
+        if max_len is None:
+            return 'FLOAT'
+
+        return 'FLOAT({})'.format(max_len)
+
+
+class String(AbstractType):
+    def sql_type(self):
+        max_len = self._kwargs.get('max_length', 50)
+        return 'VARCHAR({})'.format(max_len)
+
+
+class Boolean(AbstractType):
+    def sql_cast(self, value):
+        return 'TRUE' if value else 'FALSE'
+
+    def sql_type(self):
+        return 'BOOLEAN'
+
+
+class DateTime(AbstractType):
+    def sql_type(self):
+        return 'DATETIME'
+
+
+class Date(AbstractType):
+    def sql_type(self):
+        return 'DATE'
