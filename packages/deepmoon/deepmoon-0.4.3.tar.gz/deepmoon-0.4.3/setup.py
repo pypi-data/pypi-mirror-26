df147edfdd6d7f943519c6b0c3b132c241f79250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:14 +0000

    Add package deepmoon/0.4.3/deepmoon-0.4.3.tar.gz

---
+++ packages/deepmoon/deepmoon-0.4.3.tar.gz/deepmoon-0.4.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+install_requires = [
+    'colorama',
+    'dataset',
+    'tqdm'
+]
+
+setup(name="deepmoon",
+      version="0.4.3",
+      author="Paul Fitzpatrick",
+      author_email="paulfitz@alum.mit.edu",
+      description="The deep learning framework from beyond the moon",
+      packages=['deepmoon'],
+      entry_points={
+          "console_scripts": [
+              "deepmoon=deepmoon.main:main"
+          ]
+      },
+      install_requires=install_requires,
+      extras_require={
+      },
+      tests_require=[
+      ],
+      test_suite="nose.collector",
+      url="https://github.com/paulfitz/deepmoon"
+)
