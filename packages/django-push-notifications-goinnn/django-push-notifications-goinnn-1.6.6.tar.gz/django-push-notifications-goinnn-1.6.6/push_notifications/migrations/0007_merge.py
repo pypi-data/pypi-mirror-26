ec19f192a289e3e55c10b27314976a2c38cafff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:20 +0000

    Add package django-push-notifications-goinnn/1.6.6/django-push-notifications-goinnn-1.6.6.tar.gz

---
+++ packages/django-push-notifications-goinnn/django-push-notifications-goinnn-1.6.6.tar.gz/django-push-notifications-goinnn-1.6.6/push_notifications/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('push_notifications', '0006_auto_20161220_1024'),
+        ('push_notifications', '0006_webpushdevice'),
+    ]
+
+    operations = [
+    ]
