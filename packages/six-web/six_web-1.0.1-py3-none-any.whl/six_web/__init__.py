6c9bac3ea83670a03989c5c2ac6b312f19c369c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:42 +0000

    Add package six-web/1.0.1/six_web-1.0.1-py3-none-any.whl

---
+++ packages/six-web/six_web-1.0.1-py3-none-any.whl/six_web/__init__.py
@@ -0,0 +1,9 @@
+# 这句话的意义, 能在发布后, 直接 from six_web import Six
+from .six import Six
+
+__version__ = '1.0.1'
+__license__ = 'MIT'
+__author__ = 'fish267'
+__description__ = 'Micro python web framework '
+__email__ = 'fsh267@gmail.com',
+__url__ = 'https://github.com/fish267/six-web',
