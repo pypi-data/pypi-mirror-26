1f66c88dd01ba1be4ee67d3ae1e9a925a50ec7e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:38 +0000

    Add package wellcome-lambda-utils/2017.10.23/wellcome_lambda_utils-2017.10.23.tar.gz

---
+++ packages/wellcome-lambda-utils/wellcome_lambda_utils-2017.10.23.tar.gz/wellcome_lambda_utils-2017.10.23/setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def local_file(name):
+    return os.path.relpath(os.path.join(os.path.dirname(__file__), name))
+
+
+SOURCE = local_file('src')
+
+setup(
+    name='wellcome_lambda_utils',
+    packages=find_packages(SOURCE),
+    package_dir={'': SOURCE},
+    version='2017.10.23',
+    install_requires=[
+        'attrs>=17.2.0',
+        'boto3',
+        'daiquiri>=1.3.0',
+        'python-dateutil',
+        'requests>=2.18.4',
+    ],
+    python_requires='>=3.6',
+    description='Common lib for lambdas',
+    author='Wellcome digital platform',
+    author_email='wellcomedigitalplatform@wellcome.ac.uk',
+    url='https://github.com/wellcometrust/platform',
+    keywords=['lambda', 'utils'],
+    classifiers=[],
+)
