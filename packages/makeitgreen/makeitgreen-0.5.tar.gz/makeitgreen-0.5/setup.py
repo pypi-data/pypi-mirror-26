7abe3b2ed70d5cef1083873b4f1fbf6465e54701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:28 +0000

    Add package makeitgreen/0.5/makeitgreen-0.5.tar.gz

---
+++ packages/makeitgreen/makeitgreen-0.5.tar.gz/makeitgreen-0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='makeitgreen',
+      version='0.5',
+      description='Makes your Git Green every time you run this package',
+      url='https://github.com/arjun921/MakeItGreen',
+      author='Arjun S',
+      author_email='arjun.mi1@iiitmk.ac.in',
+      packages=['mig'],
+      zip_safe=False)
