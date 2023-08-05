58e1fd675ced9f1512715ae445cb9808f8f85c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:59 +0000

    Add package interatorArr/1.3.0/interatorArr-1.3.0.tar.gz

---
+++ packages/interatorArr/interatorArr-1.3.0.tar.gz/interatorArr-1.3.0/myOS.py
@@ -0,0 +1,9 @@
+#test 1.3.0
+def interatorArr (obj,level=0):
+    for unit in obj:
+        if isinstance(unit,list):
+            interatorArr(unit,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end="")
+            print(unit)
