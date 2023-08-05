c064d1b7a1d155d40cecd2d73906bbcd67002fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:59 +0000

    Add package liveboxplaytv/2.0.2/liveboxplaytv-2.0.2.tar.gz

---
+++ packages/liveboxplaytv/liveboxplaytv-2.0.2.tar.gz/liveboxplaytv-2.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='liveboxplaytv',
+    version='2.0.2',
+    license='GPL3',
+    description='Python bindings for the Orange Livebox Play TV appliance',
+    long_description=open('README.rst').read(),
+    author='Philipp Schmitt',
+    author_email='philipp@schmitt.co',
+    url='https://github.com/pschmitt/python-liveboxplaytv',
+    packages=find_packages(),
+    install_requires=['fuzzywuzzy', 'python-Levenshtein', 'pyteleloisirs>=3.3',
+                      'requests', 'wikipedia'],
+    entry_points={
+        'console_scripts': ['liveboxplaytv=liveboxplaytv.cli:main']
+    }
+)
