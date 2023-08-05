b543606961a31d4d2ee96f05e1f5faf564d7a2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:42 +0000

    Add package stratify/0.1a2/stratify-0.1a2.tar.gz

---
+++ packages/stratify/stratify-0.1a2.tar.gz/stratify-0.1a2/stratify/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+from ._vinterp import (interpolate, interp_schemes, extrap_schemes,
+                       INTERPOLATE_LINEAR, INTERPOLATE_NEAREST,
+                       EXTRAPOLATE_NAN, EXTRAPOLATE_NEAREST,
+                       EXTRAPOLATE_LINEAR, PyFuncExtrapolator, 
+                       PyFuncInterpolator)
+
+__version__ = '0.1a2'
