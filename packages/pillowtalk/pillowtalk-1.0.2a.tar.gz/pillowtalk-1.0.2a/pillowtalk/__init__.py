27be82349c9e90e050700ee763f60a10a58caa23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:31 +0000

    Add package pillowtalk/1.0.2a/pillowtalk-1.0.2a.tar.gz

---
+++ packages/pillowtalk/pillowtalk-1.0.2a.tar.gz/pillowtalk-1.0.2a/pillowtalk/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.0.2a"
+
+from .base import *
+from .schemas import *
+from .relationship import *
+from .session import SessionManager
+from .exceptions import *
+from marshmallow import pprint
