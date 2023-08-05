f34ec7cf58cc90c91cd1887b16f12edb315a08d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package mamba-paw/0.2.3/mamba-paw-0.2.3.tar.gz

---
+++ packages/mamba-paw/mamba-paw-0.2.3.tar.gz/mamba-paw-0.2.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# Basics of pushing a new version:
+# - Change version in setup()
+# - Clean dist/ folder
+# >> python3 setup.py sdist
+# >> python3 setup.py bdist_wheel
+# >> twine upload dist/*
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mamba-paw',
+    version='0.2.3',
+    packages=['paw'],
+    url='https://github.com/ayudasystems/mamba-paw',
+    license='Apache 2.0',
+    author='Maxime Lapointe',
+    author_email='maxime@ayudasystems.com',
+    description='Uses Azure storage queue/table for a simple worker',
+    long_description=long_description,
+    install_requires=['azure-storage>==0.36.0'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    python_requires='>=3.5',
+)
