bf8d1717b5387f683c46b29b7b5798dc183b1f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:23 +0000

    Add package devml/0.3/devml-0.3.tar.gz

---
+++ packages/devml/devml-0.3.tar.gz/devml-0.3/setup.py
@@ -0,0 +1,48 @@
+import sys
+if sys.version_info < (3,6):
+    sys.exit('Sorry, Python < 3.6 is not supported')
+import os
+
+from setuptools import setup
+
+from devml import __version__
+
+if os.path.exists('README.rst'):
+    LONG = open('README.rst').read()
+
+setup(
+    name='devml',
+    version=__version__,
+    url='https://github.com/noahgift/devml',
+    license='MIT',
+    author='Noah Gift',
+    author_email='consulting@noahgift.com',
+    description="""Machine Learning, Statistics and Utilities around Developer Productivity, 
+        Company Productivity and Project Productivity""",
+    long_description=LONG,
+    packages=['devml'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'pandas',
+        'click',
+        'PyGithub',
+        'gitpython',
+        'sensible',
+        'scipy',
+        'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    entry_points='''
+        [console_scripts]
+        dml=dml:cli
+    '''
+)
