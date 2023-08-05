e41abc427ec3e05e05fd3535a25bea5e038f0773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package aliyun-ecs/0.0.1/aliyun_ecs-0.0.1.tar.gz

---
+++ packages/aliyun-ecs/aliyun_ecs-0.0.1.tar.gz/aliyun_ecs-0.0.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "aliyun_ecs",
+    version = "0.0.1",
+    keywords = ("aliyun", "ecs", "metadata", "vpc", "teachmyself"),
+    description = "A set of simple tools for aliyun ecs.",
+    long_description = open("README.txt").read(),
+    license = "MIT LICENSE",
+
+    url = "https://github.com/teachmyself/aliyun/ecs",
+    author = "teachmyself",
+    author_email = "teachmyself@126.com",
+
+    packages = find_packages(),
+    include_packages_data = True,
+    platform = "any",
+    install_requires = ["requests"],
+    test_suite = "",
+
+    scripts = [],
+    entry_points = {
+        "console_scripts": [
+            "ecs-metadata4vpc = metadata.vpc:main",
+        ],
+    }
+)
