22bf7266485d96421820dc350f4aefb8c9f625b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:29 +0000

    Add package e13tools/0.1.2a3/e13Tools-0.1.2a3-py3-none-any.whl

---
+++ packages/e13tools/e13Tools-0.1.2a3-py3-none-any.whl/e13tools/core.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+Core
+====
+Provides a collection of functions that are not specialized. Imported
+automatically with e13Tools.
+
+"""
+
+# %% IMPORTS
+import numpy as np
+
+
+# %% FUNCTIONS
+def product(*iterables):
+    """
+    Generates a list containing all possible permutations
+
+    """
+
+    # Create empty result list
+    result = [[]]
+
+    # Fill the list with all possible combinations
+    for iterable in iterables:
+        results = list(result)
+        result = [[]]
+        for x in results:
+            for y in iterable:
+                z = list(x)
+                if(result == [[]]):
+                    z.append(y)
+                    result = [z]
+                else:
+                    z.append(y)
+                    result.append(z)
+
+    # Return it
+    return(result)
