4d9643a513b5364aab0950c8ff57029cd73bc09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:43 +0000

    Add package pytorchbp/0.0.12/pytorchbp-0.0.12.tar.gz

---
+++ packages/pytorchbp/pytorchbp-0.0.12.tar.gz/pytorchbp-0.0.12/pytorchbp/bin/utils/log.py
@@ -0,0 +1,13 @@
+from inspect import currentframe, getframeinfo
+import sys
+
+class log(suppress_warnings = False):
+    def warning(message = "No message provided."):
+        if not suppress_warnings:
+            print("WARNING:", message)
+            print(frameinfo.filename, frameinfo.lineno)
+
+    def error(message = "No message provided."):
+        print("ERROR:", message)
+        print(frameinfo.filename, frameinfo.lineno)
+        sys.exit(1)
---
+++ packages/pytorchbp/pytorchbp-0.0.12.tar.gz/pytorchbp-0.0.12/setup.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(join(dirname(__file__), fname)).read()
+
+setup(
+    name = "pytorchbp",
+    version = "0.0.12",
+    author = "John Prothero",
+    author_email = "john.s.prothero@gmail.com",
+    description = "Pytorch Boilerplate Code",
+    license = "MIT",
+    keywords = "pytorch boiler plate boilerplate",
+    url = "https://github.com/jprothero/pytorchbp",
+    packages = find_packages(),
+    scripts = ['bin/learn.py', 'bin/get_dataset.py', 'bin/load_dataset.py', 'bin/utils.log.py'],
+    long_description = read('README'),
+    classifiers = [],
+)
+
