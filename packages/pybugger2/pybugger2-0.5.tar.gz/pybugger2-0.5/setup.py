826a0eb3994a8941d826d1ca55ea375297a05ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:24 +0000

    Add package pybugger2/0.5/pybugger2-0.5.tar.gz

---
+++ packages/pybugger2/pybugger2-0.5.tar.gz/pybugger2-0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pybugger2',
+    version='0.5',
+    packages=['pybugger2'],
+    package_data={'pybugger2': ['*.txt']},
+    url='https://github.com/fareskalaboud/pybugger2',
+    license='GNU General Public License v3.0',
+    author='fareskalaboud',
+    author_email='fares.alaboud@gmail.com',
+    description='Pybugger is an easy to use color-supported CLI debugging tool for python.'
+)
