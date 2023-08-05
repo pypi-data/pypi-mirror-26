651b0994781157ffd89c3bda504ded9e1a9b459e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:20 +0000

    Add package autonomie_base/1.3/autonomie_base-1.3.tar.gz

---
+++ packages/autonomie_base/autonomie_base-1.3.tar.gz/autonomie_base-1.3/setup.py
@@ -0,0 +1,52 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    ]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',  # includes virtualenv
+    'pytest-cov',
+    ]
+
+
+entry_points = {
+    "paste.app_factory": "main = autonomie_base:main",
+}
+
+setup(name='autonomie_base',
+      version='1.3',
+      description='autonomie_base',
+      long_description=README,
+      license='GPLv3',
+      classifiers=[
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='Majerti',
+      author_email='equipe@majerti.fr',
+      url='https://github.com/CroissanceCommune/autonomie_celery',
+      keywords='web wsgi bfg pylons pyramid autonomie',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'testing': tests_require,
+      },
+      install_requires=requires,
+      entry_points=entry_points,
+      )
