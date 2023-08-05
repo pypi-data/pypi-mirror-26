9563939b6595e58444394da57ad8673625af2f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:28 +0000

    Add package followthemoney/0.2.1/followthemoney-0.2.1.tar.gz

---
+++ packages/followthemoney/followthemoney-0.2.1.tar.gz/followthemoney-0.2.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='followthemoney',
+    version='0.2.1',
+    long_description="Data model and validator for investigative graph data.",
+    keywords='',
+    author='Organized Crime and Corruption Reporting Project',
+    author_email='pudo@occrp.org',
+    url='https://occrp.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'six',
+        'pyyaml',
+        'requests',
+        'normality',
+        'sqlalchemy',
+        'backports.csv',
+        'dalet>=1.3'
+    ],
+    test_suite='nose.collector',
+    entry_points={
+        'console_scripts': [],
+    },
+    tests_require=['coverage', 'nose']
+)
