2ac95243a7f688179640678ebf12f354317cd0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:43 +0000

    Add package dig-indicator-incall/1.9/dig_indicator_incall-1.9.tar.gz

---
+++ packages/dig-indicator-incall/dig_indicator_incall-1.9.tar.gz/dig_indicator_incall-1.9/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dig_indicator_incall',
+  packages = ['dig_indicator_incall','dig_indicator_incall.model'], # this must be the same as the name above
+  version = '1.9',
+  description = 'Domain: indicator',
+  author = 'Jiayuan Ding',
+  author_email = 'jiayuand@usc.edu',
+  url = 'https://github.com/usc-isi-i2/dig-indicators-data/tree/master/Code', #github 
+  download_url = 'https://github.com/usc-isi-i2/dig-indicators-data/tree/master/Code', # I'll explain this in a second
+  keywords = ['incall', 'domain', 'dig'], # arbitrary keywords
+  classifiers = [],
+)
