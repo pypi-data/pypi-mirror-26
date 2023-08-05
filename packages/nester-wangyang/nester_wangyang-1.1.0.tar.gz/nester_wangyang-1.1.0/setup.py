ab273e066340abedf02648184707888c12e6bfca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:43 +0000

    Add package nester-wangyang/1.1.0/nester_wangyang-1.1.0.tar.gz

---
+++ packages/nester-wangyang/nester_wangyang-1.1.0.tar.gz/nester_wangyang-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+			name='nester_wangyang',
+			version='1.1.0',
+			py_modules=['nester_wangyang'],
+			author='wangyang',
+			author_email='wangyang_1980@hotmail.com',
+			utrl='',
+			description='A simple printer of nested lists,with tab level control'
+)
