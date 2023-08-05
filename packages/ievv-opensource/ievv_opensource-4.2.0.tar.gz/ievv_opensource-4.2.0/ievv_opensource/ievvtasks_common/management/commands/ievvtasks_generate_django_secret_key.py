67c3262b7312bd2c7524fd281e3be668f3527dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:48 +0000

    Add package ievv-opensource/4.2.0/ievv_opensource-4.2.0.tar.gz

---
+++ packages/ievv-opensource/ievv_opensource-4.2.0.tar.gz/ievv_opensource-4.2.0/ievv_opensource/ievvtasks_common/management/commands/ievvtasks_generate_django_secret_key.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+
+from ievv_opensource.utils import secret_key
+
+
+class Command(BaseCommand):
+    help = 'Generate a Django SECRET_KEY.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--plain', dest='plain',
+                            required=False, action='store_true',
+                            default=False,
+                            help='Do not include SECRET_KEY= in output.')
+
+    def handle(self, *args, **options):
+        key = secret_key.generate_django_secret_key()
+        if not options['plain']:
+            key = 'SECRET_KEY="{}"'.format(key)
+        self.stdout.write(key)
