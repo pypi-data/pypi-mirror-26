ac4078d16e2914f4fdad8d3f8ca47013b95f492c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:25 +0000

    Add package iceman/0.0.3/iceman-0.0.3-py2-none-any.whl

---
+++ packages/iceman/iceman-0.0.3-py2-none-any.whl/iceman/crypto_strategies/strategy.py
@@ -0,0 +1,15 @@
+"""strategy module
+"""
+class Strategy(object):
+    """Abstracted Strategy class
+    """
+
+    def encrypt(self, payload):
+        """encrypt interface
+        """
+        raise NotImplementedError()
+
+    def decrypt(self, token):
+        """decrypt interface
+        """
+        raise NotImplementedError()
