6a750428e877ef0e23ce92f22ee129b0d95b34fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:21 +0000

    Add package avimigrationtools/17.2.2.post1/avimigrationtools-17.2.2.post1.tar.gz

---
+++ packages/avimigrationtools/avimigrationtools-17.2.2.post1.tar.gz/avimigrationtools-17.2.2.post1/avi/migrationtools/f5_converter/scp_util.py
@@ -0,0 +1,44 @@
+import logging
+import os
+
+from avi.migrationtools.scp_util import SCPUtil
+
+
+LOG = logging.getLogger(__name__)
+
+
+def get_files_from_f5(local_path, host, username, pw=None, key=None):
+    local_path = local_path + os.path.sep
+    scp = SCPUtil(host, username, pw, key)
+    scp.get_all_files('/config/ssl/ssl.crl/', local_path)
+    scp.get_all_files('/config/ssl/ssl.crt/', local_path)
+    scp.get_all_files('/config/ssl/ssl.csr/', local_path)
+    scp.get_all_files('/config/ssl/ssl.key/', local_path)
+    scp.get_all_files('/config/monitors/', local_path)
+    # Added support to get cert and key for V13.
+    try:
+        scp.get_all_files('/config/filestore/files_d/Common_d/certificate_d/',
+                          local_path)
+        scp.get_all_files('/config/filestore/files_d/Common_d/key_d/',
+                          local_path)
+    except:
+        pass
+    scp.get('/config/bigip.conf', local_path + 'bigip.conf')
+    try:
+        scp.get('/config/profile_base.conf', local_path + 'profile_base.conf')
+    except:
+        pass
+    try:
+        scp.get('/usr/share/monitors/base_monitors.conf', local_path +
+                'base_monitors.conf')
+    except:
+        pass
+    try:
+        scp.get('/config/bigip_gtm.conf', local_path + 'bigip_gtm.conf')
+    except:
+        pass
+    try:
+        scp.get_all_partition_config('/config/partitions/', local_path)
+    except:
+        pass
+    scp.close()
