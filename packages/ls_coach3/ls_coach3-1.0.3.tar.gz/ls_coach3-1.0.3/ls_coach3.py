a2ff420a18ff6fb7b7be9b0ad7043f9c2e62c838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:33 +0000

    Add package ls_coach3/1.0.3/ls_coach3-1.0.3.tar.gz

---
+++ packages/ls_coach3/ls_coach3-1.0.3.tar.gz/ls_coach3-1.0.3/ls_coach3.py
@@ -0,0 +1,29 @@
+
+import sanitize
+
+class Athlete:
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        self.name = a_name
+        self.dob = a_dob
+        self.times = a_times
+        print(self.name+ "'s fastest times are: " + str(self.top3()))
+    def top3(self):
+        return(sorted(set([sanitize.sanitize(t) for t in self.times]))[0:3])
+        
+def get_coach_data(filename):
+    try:
+        with open(filename) as f:
+            data = f.readline()
+        templ = data.strip().split(',')
+        return(Athlete(templ.pop(0), templ.pop(0), templ))
+    except IOError as ioerr:
+        print('File error: ' + str(ioerr))
+        return(None)
+
+
+
+    
+james = get_coach_data('james2.txt')
+julie = get_coach_data('julie2.txt')
+mikey = get_coach_data('mikey2.txt')
+sarah = get_coach_data('sarah2.txt')
