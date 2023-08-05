ec95826f06d36dbbbcce37a0e8ef911a396f149a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:57 +0000

    Add package skitai/0.26.15.23/skitai-0.26.15.23.tar.gz

---
+++ packages/skitai/skitai-0.26.15.23.tar.gz/skitai-0.26.15.23/skitai/saddle/config.py
@@ -0,0 +1,10 @@
+﻿from aquests.lib import attrdict
+
+def Config (preset = False):
+	cf = attrdict.AttrDict ()
+	if preset:
+		cf.max_post_body_size = 5 * 1024 * 1024
+		cf.max_cache_size = 5 * 1024 * 1024
+		cf.max_multipart_body_size = 20 * 1024 * 1024
+		cf.max_upload_file_size = 20000000
+	return cf
