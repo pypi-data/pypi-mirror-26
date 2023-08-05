8790ac97db27cc44c4bef52246abaf2e4ce0f8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:57 +0000

    Add package sspyrs/0.3/sspyrs-0.3.tar.gz

---
+++ packages/sspyrs/sspyrs-0.3.tar.gz/sspyrs-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='sspyrs',
+      version='0.3',
+      description='Lightweight interface for SSRS reports to python',
+      long_description=open('README.rst').read(),
+      url='https://pypi.python.org/pypi/sspyrs',
+      author='James Nix',
+      author_email='james@nixanalytics.com',
+      license='MIT',
+      packages=['sspyrs'],
+      install_requires=[
+           'pandas>=0.18.1',
+           'openpyxl>=2.4.7',
+           'xmltodict>=0.10.2',
+           'requests_ntlm>=1.0.0'
+      ],
+      keywords=['ssrs report reporting sql'],
+      zip_safe=False)
