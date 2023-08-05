2e44091b62dfe436d0b3b1cdc776e03e8b9bf757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:21 +0000

    Add package scikit-umfpack/0.3.1/scikit-umfpack-0.3.1.tar.gz

---
+++ packages/scikit-umfpack/scikit-umfpack-0.3.1.tar.gz/scikit-umfpack-0.3.1/scikits/umfpack/version.py
@@ -0,0 +1,9 @@
+# THIS FILE IS GENERATED FROM scikit-umfpack SETUP.PY
+short_version = '0.3.1'
+version = '0.3.1'
+full_version = '0.3.1.dev0+6332ea2'
+git_revision = '6332ea2aa5ba701da37b67f7a158608126250aa7'
+release = False
+
+if not release:
+    version = full_version
