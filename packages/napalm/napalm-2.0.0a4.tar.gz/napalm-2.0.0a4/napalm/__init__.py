448ba9bddfc1cd199ae3acac55dbc47bec0dc2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:59 +0000

    Add package napalm/2.0.0a4/napalm-2.0.0a4.tar.gz

---
+++ packages/napalm/napalm-2.0.0a4.tar.gz/napalm-2.0.0a4/napalm/__init__.py
@@ -0,0 +1,24 @@
+import pkg_resources
+import sys
+
+# Verify Python Version that is running
+try:
+    if not(sys.version_info.major == 2 and sys.version_info.minor == 7) and \
+            not(sys.version_info.major == 3):
+        raise RuntimeError('NAPALM requires Python 2.7 or Python3')
+except AttributeError:
+    raise RuntimeError('NAPALM requires Python 2.7 or Python3')
+
+try:
+    __version__ = pkg_resources.get_distribution('napalm').version
+except pkg_resources.DistributionNotFound:
+    __version__ = "Not installed"
+
+
+SUPPORTED_DRIVERS = [
+    "eos",
+    "ios",
+    "junos",
+    "nxos",
+    "nxos_ssh",
+]
