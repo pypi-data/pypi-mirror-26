85f398e5b7aa73891647b1b63ee5d2b174d5f84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:37 +0000

    Add package pytest-testrail/1.0.0/pytest-testrail-1.0.0.tar.gz

---
+++ packages/pytest-testrail/pytest-testrail-1.0.0.tar.gz/pytest-testrail-1.0.0/pytest_testrail/plugin.py
@@ -0,0 +1,188 @@
+from datetime import datetime
+import pytest
+import re
+import warnings
+
+
+PYTEST_TO_TESTRAIL_STATUS = {
+    "passed": 1,
+    "failed": 5,
+    "skipped": 2,
+}
+
+DT_FORMAT = '%d-%m-%Y %H:%M:%S'
+
+TESTRAIL_PREFIX = 'testrail'
+
+ADD_RESULTS_URL = 'add_results_for_cases/{}/'
+ADD_TESTRUN_URL = 'add_run/{}'
+
+
+class DeprecatedTestDecorator(DeprecationWarning):
+    pass
+
+
+warnings.simplefilter(action='once', category=DeprecatedTestDecorator, lineno=0)
+
+
+class pytestrail(object):
+    '''
+    An alternative to using the testrail function as a decorator for test cases, since py.test may confuse it as a test
+    function since it has the 'test' prefix
+    '''
+    @staticmethod
+    def case(*ids):
+        """
+        Decorator to mark tests with testcase ids.
+
+        ie. @pytestrail.case('C123', 'C12345')
+
+        :return pytest.mark:
+        """
+        return pytest.mark.testrail(ids=ids)
+
+
+def testrail(*ids):
+    """
+    Decorator to mark tests with testcase ids.
+
+    ie. @testrail('C123', 'C12345')
+
+    :return pytest.mark:
+    """
+    deprecation_msg = ('pytest_testrail: the @testrail decorator is deprecated and will be removed. Please use the '
+            '@pytestrail.case decorator instead.')
+    warnings.warn(deprecation_msg, DeprecatedTestDecorator)
+    return pytestrail.case(*ids)
+
+
+def get_test_outcome(outcome):
+    """
+    Return numerical value of test outcome.
+
+    :param str outcome: pytest reported test outcome value.
+    :returns: int relating to test outcome.
+    """
+    return PYTEST_TO_TESTRAIL_STATUS[outcome]
+
+
+def testrun_name():
+    """Returns testrun name with timestamp"""
+    now = datetime.utcnow()
+    return 'Automated Run {}'.format(now.strftime(DT_FORMAT))
+
+
+def clean_test_ids(test_ids):
+    """
+    Clean pytest marker containing testrail testcase ids.
+
+    :param list test_ids: list of test_ids.
+    :return list ints: contains list of test_ids as ints.
+    """
+    return map(int, [re.search('(?P<test_id>[0-9]+$)', test_id).groupdict().get('test_id') for test_id in test_ids])
+
+
+def get_testrail_keys(items):
+    """Return TestRail ids from pytests markers"""
+    testcaseids = []
+    for item in items:
+        if item.get_marker(TESTRAIL_PREFIX):
+            testcaseids.extend(
+                clean_test_ids(
+                    item.get_marker(TESTRAIL_PREFIX).kwargs.get('ids')
+                )
+            )
+    return testcaseids
+
+
+class PyTestRailPlugin(object):
+    def __init__(
+            self, client, assign_user_id, project_id, suite_id, cert_check, tr_name):
+        self.assign_user_id = assign_user_id
+        self.cert_check = cert_check
+        self.client = client
+        self.project_id = project_id
+        self.results = []
+        self.suite_id = suite_id
+        self.testrun_id = 0
+        self.testrun_name = tr_name
+
+    # pytest hooks
+
+    @pytest.hookimpl(trylast=True)
+    def pytest_collection_modifyitems(self, session, config, items):
+        tr_keys = get_testrail_keys(items)
+        if self.testrun_name is None:
+            self.testrun_name = testrun_name()
+
+        self.create_test_run(
+            self.assign_user_id,
+            self.project_id,
+            self.suite_id,
+            self.testrun_name,
+            tr_keys
+        )
+
+    @pytest.hookimpl(tryfirst=True, hookwrapper=True)
+    def pytest_runtest_makereport(self, item, call):
+        outcome = yield
+        rep = outcome.get_result()
+        if item.get_marker(TESTRAIL_PREFIX):
+            testcaseids = item.get_marker(TESTRAIL_PREFIX).kwargs.get('ids')
+
+            if rep.when == 'call' and testcaseids:
+                self.add_result(
+                    clean_test_ids(testcaseids),
+                    get_test_outcome(outcome.result.outcome)
+                )
+
+    def pytest_sessionfinish(self, session, exitstatus):
+        data = {'results': self.results}
+        if data['results']:
+            self.client.send_post(
+                ADD_RESULTS_URL.format(self.testrun_id),
+                data,
+                cert_check=self.cert_check
+            )
+
+    # plugin
+
+    def add_result(self, test_ids, status):
+        """
+        Add a new result to results dict to be submitted at the end.
+
+        :param list test_id: list of test_ids.
+        :param int status: status code of test (pass or fail).
+        """
+        for test_id in test_ids:
+            data = {
+                'case_id': test_id,
+                'status_id': status,
+            }
+            self.results.append(data)
+
+    def create_test_run(
+            self, assign_user_id, project_id, suite_id, testrun_name, tr_keys):
+        """
+        Create testrun with ids collected from markers.
+
+        :param list items: collected testrail ids.
+        """
+        data = {
+            'suite_id': suite_id,
+            'name': testrun_name,
+            'assignedto_id': assign_user_id,
+            'include_all': False,
+            'case_ids': tr_keys,
+        }
+
+        response = self.client.send_post(
+            ADD_TESTRUN_URL.format(project_id),
+            data,
+            cert_check=self.cert_check
+        )
+        for key, _ in response.items():
+            if key == 'error':
+                print('Failed to create testrun: {}'.format(response))
+            else:
+                self.testrun_id = response['id']
