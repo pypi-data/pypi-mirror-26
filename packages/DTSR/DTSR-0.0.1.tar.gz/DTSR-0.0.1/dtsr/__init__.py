73b9fe017cdfde22e7035da5dfb554b78c355fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:37 +0000

    Add package DTSR/0.0.1/DTSR-0.0.1.tar.gz

---
+++ packages/DTSR/DTSR-0.0.1.tar.gz/DTSR-0.0.1/dtsr/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+from .config import *
+from .data import *
+from .formula import *
+from .io import *
+from .plot import *
+from .signif import *
+from .util import *
+
