d5b7e074042ad885d0da491a0469b077b9f9846d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:28 +0000

    Add package Syh123/1.0.0/Syh123-1.0.0.zip

---
+++ packages/Syh123/Syh123-1.0.0.zip/Syh123-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Syh123',
+    version='1.0.0',
+    py_modules=['Syh123'],
+    author='syh',
+    author_email='284914425@qq.com',
+    url='http://www.test.com',
+    description='this is a test',
+)
