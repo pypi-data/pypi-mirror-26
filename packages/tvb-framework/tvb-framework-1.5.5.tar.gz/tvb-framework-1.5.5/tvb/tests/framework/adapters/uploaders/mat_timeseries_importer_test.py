c52828960d4e1ab8dfe82d48f188b46f633dc50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:40 +0000

    Add package tvb-framework/1.5.5/tvb-framework-1.5.5.tar.gz

---
+++ packages/tvb-framework/tvb-framework-1.5.5.tar.gz/tvb-framework-1.5.5/tvb/tests/framework/adapters/uploaders/mat_timeseries_importer_test.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+#
+#
+# TheVirtualBrain-Framework Package. This package holds all Data Management, and 
+# Web-UI helpful to run brain-simulations. To use it, you also need do download
+# TheVirtualBrain-Scientific Package (for simulators). See content of the
+# documentation-folder for more details. See also http://www.thevirtualbrain.org
+#
+# (c) 2012-2017, Baycrest Centre for Geriatric Care ("Baycrest") and others
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#   CITATION:
+# When using The Virtual Brain for scientific publications, please cite it as follows:
+#
+#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
+#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
+#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
+#
+
+"""
+Unit-test for mat_timeseries_importer and mat_parser.
+
+.. moduleauthor:: Mihai Andrei <mihai.andrei@codemart.ro>
+"""
+
+import unittest
+import os
+import tvb_data
+from tvb.datatypes.connectivity import Connectivity
+from tvb.datatypes.time_series import TimeSeriesRegion
+from tvb.core.entities.file.files_helper import FilesHelper
+from tvb.core.services.flow_service import FlowService
+from tvb.tests.framework.core.test_factory import TestFactory
+from tvb.tests.framework.datatypes.datatypes_factory import DatatypesFactory
+from tvb.tests.framework.core.base_testcase import TransactionalTestCase
+
+
+class MatTimeSeriesImporterTest(TransactionalTestCase):
+
+    base_pth = os.path.join(os.path.dirname(tvb_data.__file__), 'berlinSubjects', 'QL_20120814')
+    bold_path = os.path.join(base_pth, 'QL_BOLD_regiontimecourse.mat')
+    connectivity_path = os.path.join(base_pth, 'QL_20120814_Connectivity.zip')
+
+    def setUp(self):
+        self.datatypeFactory = DatatypesFactory()
+        self.test_project = self.datatypeFactory.get_project()
+        self.test_user = self.datatypeFactory.get_user()
+        self._import_connectivity()
+
+
+    def tearDown(self):
+        FilesHelper().remove_project_structure(self.test_project.name)
+
+
+    def _import_connectivity(self):
+        importer = TestFactory.create_adapter('tvb.adapters.uploaders.zip_connectivity_importer',
+                                              'ZIPConnectivityImporter')
+
+        ### Launch Operation
+        FlowService().fire_operation(importer, self.test_user, self.test_project.id,
+                                     uploaded=self.connectivity_path, Data_Subject='QL')
+
+        self.connectivity = TestFactory.get_entity(self.test_project, Connectivity())
+
+
+    def test_import_bold(self):
+        ### Retrieve Adapter instance
+        importer = TestFactory.create_adapter('tvb.adapters.uploaders.mat_timeseries_importer', 'MatTimeSeriesImporter')
+
+        args = dict(data_file=self.bold_path, dataset_name='QL_20120824_DK_BOLD_timecourse', structure_path='',
+                    transpose=False, slice=None, sampling_rate=1000, start_time=0,
+                    tstype='region',
+                    tstype_parameters_option_region_connectivity=self.connectivity.gid,
+                    Data_Subject="QL")
+
+        ### Launch import Operation
+        FlowService().fire_operation(importer, self.test_user, self.test_project.id, **args)
+
+        tsr = TestFactory.get_entity(self.test_project, TimeSeriesRegion())
+
+        self.assertEqual((661, 1, 68, 1), tsr.read_data_shape())
+
+
+
+def suite():
+    """
+    Gather all the tests in a test suite.
+    """
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.makeSuite(MatTimeSeriesImporterTest))
+    return test_suite
+
+
+if __name__ == "__main__":
+    #So you can run tests from this package individually.
+    TEST_RUNNER = unittest.TextTestRunner()
+    TEST_SUITE = suite()
+    TEST_RUNNER.run(TEST_SUITE)
---
+++ packages/tvb-framework/tvb-framework-1.5.5.tar.gz/tvb-framework-1.5.5/tvb/tests/framework/core/services/settings_service_test.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+#
+#
+# TheVirtualBrain-Framework Package. This package holds all Data Management, and 
+# Web-UI helpful to run brain-simulations. To use it, you also need do download
+# TheVirtualBrain-Scientific Package (for simulators). See content of the
+# documentation-folder for more details. See also http://www.thevirtualbrain.org
+#
+# (c) 2012-2017, Baycrest Centre for Geriatric Care ("Baycrest") and others
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#   CITATION:
+# When using The Virtual Brain for scientific publications, please cite it as follows:
+#
+#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
+#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
+#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
+#
+
+"""
+.. moduleauthor:: Ionel Ortelecan <ionel.ortelecan@codemart.ro>
+.. moduleauthor:: Bogdan Neacsa <bogdan.neacsa@codemart.ro>
+"""
+
+import os
+import shutil
+import unittest
+from tvb.tests.framework.core.base_testcase import BaseTestCase
+from tvb.basic.profile import TvbProfile
+from tvb.core.services.settings_service import SettingsService, InvalidSettingsException
+
+
+TEST_CONFIG_FILE = os.path.expanduser(os.path.join("~", 'tvb.tests.framework.configuration'))
+
+
+class SettingsServiceTest(BaseTestCase):
+    """
+    This class contains tests for the tvb.core.services.settings_service module.
+    """
+    TEST_SETTINGS = {SettingsService.KEY_ADMIN_NAME: 'test_name',
+                     SettingsService.KEY_ADMIN_EMAIL: 'my@yahoo.com',
+                     SettingsService.KEY_PORT: 8081,
+                     SettingsService.KEY_URL_WEB: "http://192.168.123.11:8081/",
+                     SettingsService.KEY_MAX_DISK_SPACE_USR: 2 ** 8}
+
+
+    def setUp(self):
+        """
+        Prepare the usage of a different config file for this class only.
+        """
+        if os.path.exists(TEST_CONFIG_FILE):
+            os.remove(TEST_CONFIG_FILE)
+
+        self.old_config_file = TvbProfile.current.TVB_CONFIG_FILE
+        TvbProfile.current.__class__.TVB_CONFIG_FILE = TEST_CONFIG_FILE
+        TvbProfile._build_profile_class(TvbProfile.CURRENT_PROFILE_NAME)
+        self.settings_service = SettingsService()
+        
+            
+    def tearDown(self):
+        """
+        Restore configuration file
+        """
+        if os.path.exists(TEST_CONFIG_FILE):
+            os.remove(TEST_CONFIG_FILE)
+
+        TvbProfile.current.__class__.TVB_CONFIG_FILE = self.old_config_file
+        TvbProfile._build_profile_class(TvbProfile.CURRENT_PROFILE_NAME)
+    
+
+
+    def test_check_db_url_invalid(self):
+        """
+        Make sure a proper exception is raised in case an invalid database url is passed.
+        """
+        self.assertRaises(InvalidSettingsException, self.settings_service.check_db_url, "this-url-should-be-invalid")
+        
+    
+    def test_get_free_disk_space(self):
+        """
+        Check that no exception is raised during the query for free disk space.
+        Also do a check that returned value is greater than 0.
+        Not most precise check but other does not seem possible so far.
+        """
+        disk_space = self.settings_service.get_disk_free_space(TvbProfile.current.TVB_STORAGE)
+        self.assertTrue(disk_space > 0, "Disk space should never be negative.")
+    
+            
+    def test_first_run_save(self):
+        """
+        Check that before setting something, all flags are pointing towards empty.
+        After storing some configurations, check that flags are changed.
+        """
+        initial_configurations = self.settings_service.configurable_keys
+        first_run = TvbProfile.is_first_run()
+        self.assertTrue(first_run, "Invalid First_Run flag!!")
+        self.assertFalse(os.path.exists(TEST_CONFIG_FILE))
+        self.assertTrue(len(TvbProfile.current.manager.stored_settings) == 0)
+
+        to_store_data = {key: value['value'] for key, value in initial_configurations.iteritems()}
+        for key, value in self.TEST_SETTINGS.iteritems():
+            to_store_data[key] = value
+        _, shoud_reset = self.settings_service.save_settings(**to_store_data)
+
+        self.assertTrue(shoud_reset)
+        first_run = TvbProfile.is_first_run()
+        self.assertFalse(first_run, "Invalid First_Run flag!!")
+        self.assertTrue(os.path.exists(TEST_CONFIG_FILE))
+        self.assertFalse(len(TvbProfile.current.manager.stored_settings) == 0)
+        
+        
+    def test_read_stored_settings(self):
+        """
+        Test to see that keys from the configuration dict is updated with
+        the value from the configuration file after store.
+        """
+        initial_configurations = self.settings_service.configurable_keys
+        to_store_data = {key: value['value'] for key, value in initial_configurations.iteritems()}
+        for key, value in self.TEST_SETTINGS.iteritems():
+            to_store_data[key] = value
+
+        is_changed, shoud_reset = self.settings_service.save_settings(**to_store_data)
+        self.assertTrue(shoud_reset and is_changed)
+
+        # enforce keys to get repopulated:
+        TvbProfile._build_profile_class(TvbProfile.CURRENT_PROFILE_NAME)
+        self.settings_service = SettingsService()
+
+        updated_configurations = self.settings_service.configurable_keys
+        for key, value in updated_configurations.iteritems():
+            if key in self.TEST_SETTINGS:
+                self.assertEqual(self.TEST_SETTINGS[key], value['value'])
+            elif key == SettingsService.KEY_ADMIN_PWD:
+                self.assertEqual(TvbProfile.current.web.admin.ADMINISTRATOR_PASSWORD, value['value'])
+                self.assertEqual(TvbProfile.current.web.admin.ADMINISTRATOR_BLANK_PWD,
+                                 initial_configurations[key]['value'])
+            else:
+                self.assertEqual(initial_configurations[key]['value'], value['value'])
+
+                    
+    def test_update_settings(self):
+        """
+        Test update of settings: correct flags should be returned, and check storage folder renamed
+        """
+        # 1. save on empty config-file:
+        to_store_data = {key: value['value'] for key, value in self.settings_service.configurable_keys.iteritems()}
+        for key, value in self.TEST_SETTINGS.iteritems():
+            to_store_data[key] = value
+
+        is_changed, shoud_reset = self.settings_service.save_settings(**to_store_data)
+        self.assertTrue(shoud_reset and is_changed)
+
+        # 2. Reload and save with the same values (is_changed expected to be False)
+        TvbProfile._build_profile_class(TvbProfile.CURRENT_PROFILE_NAME)
+        self.settings_service = SettingsService()
+        to_store_data = {key: value['value'] for key, value in self.settings_service.configurable_keys.iteritems()}
+
+        is_changed, shoud_reset = self.settings_service.save_settings(**to_store_data)
+        self.assertFalse(is_changed)
+        self.assertFalse(shoud_reset)
+
+        # 3. Reload and check that changing TVB_STORAGE is done correctly
+        TvbProfile._build_profile_class(TvbProfile.CURRENT_PROFILE_NAME)
+        self.settings_service = SettingsService()
+        to_store_data = {key: value['value'] for key, value in self.settings_service.configurable_keys.iteritems()}
+        to_store_data[SettingsService.KEY_STORAGE] = os.path.join(TvbProfile.current.TVB_STORAGE, 'RENAMED')
+
+        # Write a test-file and check that it is moved
+        file_writer = open(os.path.join(TvbProfile.current.TVB_STORAGE, "test_rename-xxx43"), 'w')
+        file_writer.write('test-content')
+        file_writer.close()
+
+        is_changed, shoud_reset = self.settings_service.save_settings(**to_store_data)
+        self.assertTrue(is_changed)
+        self.assertFalse(shoud_reset)
+        # Check that the file was correctly moved:
+        data = open(os.path.join(TvbProfile.current.TVB_STORAGE, 'RENAMED', "test_rename-xxx43"), 'r').read()
+        self.assertEqual(data, 'test-content')
+
+        shutil.rmtree(os.path.join(TvbProfile.current.TVB_STORAGE, 'RENAMED'))
+        os.remove(os.path.join(TvbProfile.current.TVB_STORAGE, "test_rename-xxx43"))
+
+
+
+def suite():
+    """
+    Gather all the tests in a test suite.
+    """
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.makeSuite(SettingsServiceTest))
+    return test_suite
+
+
+if __name__ == "__main__":
+    #So you can run tests individually.
+    TEST_RUNNER = unittest.TextTestRunner()
+    TEST_SUITE = suite()
+    TEST_RUNNER.run(TEST_SUITE)
+    
+    
+    
