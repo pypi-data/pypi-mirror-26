24ff8fd759cd459174afebea4ed75dcc76726c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:25 +0000

    Add package fjcommon/0.1.25/fjcommon-0.1.25.tar.gz

---
+++ packages/fjcommon/fjcommon-0.1.25.tar.gz/fjcommon-0.1.25/fjcommon/functools_ext.py
@@ -0,0 +1,22 @@
+""" Extends functools """
+
+
+import functools
+
+
+def compose(*args):
+    """
+    :param args: a list of functions
+    :return: composition of the functions
+    """
+    def compose2(f1, f2):
+        def composed(*args_c, **kwargs_c):
+            return f1(f2(*args_c, **kwargs_c))
+        return composed
+
+    return functools.reduce(compose2, args)
+
+
+def identity(*args):
+    return args
+
