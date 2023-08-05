56b46a1590079e0a8667c826c13aa135f9dc3d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:22 +0000

    Add package simpleflow/0.18.15/simpleflow-0.18.15.tar.gz

---
+++ packages/simpleflow/simpleflow-0.18.15.tar.gz/simpleflow-0.18.15/simpleflow/settings/__init__.py
@@ -0,0 +1,8 @@
+import sys
+
+from future.utils import iteritems
+
+from . import base
+
+for k, v in iteritems(base.load()):
+    setattr(sys.modules[__name__], k, v)
---
+++ packages/simpleflow/simpleflow-0.18.15.tar.gz/simpleflow-0.18.15/simpleflow/swf/process/worker/command.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+
+import swf.models
+
+from .base import (
+    Worker,
+    ActivityPoller,
+)
+
+
+def make_worker_poller(domain, task_list, heartbeat):
+    """
+    Make a worker poller for the domain and task list.
+    :param domain:
+    :type domain: str
+    :param task_list:
+    :type task_list: str
+    :param heartbeat:
+    :type heartbeat: int
+    :return:
+    :rtype: ActivityPoller
+    """
+    domain = swf.models.Domain(domain)
+    return ActivityPoller(domain, task_list, heartbeat)
+
+
+def start(domain, task_list, nb_processes=None, heartbeat=60):
+    """
+    Start a worker for the given domain and task_list.
+    :param domain:
+    :type domain: str
+    :param task_list:
+    :type task_list: str
+    :param nb_processes: Number of processes. Default: number of CPUs
+    :type nb_processes: Optional[int]
+    :param heartbeat: heartbeat frequency in seconds
+    :type heartbeat: int
+    """
+    poller = make_worker_poller(domain, task_list, heartbeat)
+    worker = Worker(poller, nb_processes)
+    worker.is_alive = True
+    worker.start()
