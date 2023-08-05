45831e2d560b02fffade1b7c9384d1a54b1a5a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:58 +0000

    Add package easyapiio/0.1.18/easyapiio-0.1.18.tar.gz

---
+++ packages/easyapiio/easyapiio-0.1.18.tar.gz/easyapiio-0.1.18/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='easyapiio',
+      version='0.1.18',
+      description='A library to simplify the use of EasyAPI.io JSON Objects.',
+      url='https://github.com/easyapiio/easyapi-objects-python-sdk',
+      author='keithweaver',
+      author_email='me@keithweaver.ca',
+      license='MIT License',
+      packages=['easyapiio'],
+      zip_safe=False)
