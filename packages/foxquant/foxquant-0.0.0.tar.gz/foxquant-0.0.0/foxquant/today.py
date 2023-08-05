3534932378193f97a2ad7e40e2b8f3e2b4ebdd4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:52 +0000

    Add package foxquant/0.0.0/foxquant-0.0.0.tar.gz

---
+++ packages/foxquant/foxquant-0.0.0.tar.gz/foxquant-0.0.0/foxquant/today.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+
+def get_day_today_quote(dropna=False):
+    """
+    得到今日个股实时股价数据（包括停牌的股）
+    :param dropna: 是否删除空行（即是否包括停牌的股）
+    :return: DataFrame
+    """
+    pass
+
+
+def get_day_today_quote(dropna=False):
+    """
+    得到今日个股实时股价数据（包括停牌的股）
+    :param dropna: 是否删除空行（即是否包括停牌的股）
+    :return: DataFrame
+    """
+    pass
