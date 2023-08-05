bfde432124e9995acba24d8b15c219c059add71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:32 +0000

    Add package superdice-by-lexa/0.4/superdice-by-lexa-0.4.tar.gz

---
+++ packages/superdice-by-lexa/superdice-by-lexa-0.4.tar.gz/superdice-by-lexa-0.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='superdice-by-lexa',
+    version='0.4',
+    packages=[''],
+    url='http://alexeyis.me',
+    license='MIT',
+    author='alexeyisme',
+    author_email='a.kislitsin@yandex.ru',
+    description='Dice'
+)
---
+++ packages/superdice-by-lexa/superdice-by-lexa-0.4.tar.gz/superdice-by-lexa-0.4/superdice_by_lexa.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from random import randint
+
+
+def dice():
+    return randint(1, 6)
+
