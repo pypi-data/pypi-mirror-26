288fe93c5128e0d3ee7e8fd66e18d305d8f80a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:23 +0000

    Add package pyseeyou/0.3.0/pyseeyou-0.3.0.tar.gz

---
+++ packages/pyseeyou/pyseeyou-0.3.0.tar.gz/pyseeyou-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyseeyou',
+    version='0.3.0',
+    description='A Python Parser for the ICU MessageFormat.',
+    author='Siame Rafiq',
+    author_email='mail@sia.me.uk',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['parsimonious', 'toolz'],
+    tests_require=['pytest'],
+    url='https://github.com/rolepoint/pyseeyou',
+    download_url='https://github.com/rolepoint/pyseeyou/archive/v0.3.0.tar.gz',
+    keywords='python i18n messageformat python3 python2'
+)
