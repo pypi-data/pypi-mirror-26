38ff4932f0ea7bb4c412db9e3ff5f627cfbabdcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:50 +0000

    Add package pythreshold/0.1.6/pythreshold-0.1.6.tar.gz

---
+++ packages/pythreshold/pythreshold-0.1.6.tar.gz/pythreshold-0.1.6/setup.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    # Package name
+    name='pythreshold',
+
+    # Package version
+    version='0.1.6',
+
+    # Included packages
+    packages=find_packages(),
+
+    # Package author information
+    author=u'BSc. Manuel Aguado Martínez',
+    author_email='manuelaguadomtz@gmail.com',
+    url='https://www.researchgate.net/profile/Manuel_Aguado_Martinez2',
+
+    # Package requirements
+    install_requires=['numpy',
+                      'scipy',
+                      'scikit-image',
+                      'matplotlib'],
+
+    # Package description
+    description='Numpy/Scipy implementations of state-of-the-art image thresholding algorithms',
+    keywords='thresholding entropy',
+
+)
