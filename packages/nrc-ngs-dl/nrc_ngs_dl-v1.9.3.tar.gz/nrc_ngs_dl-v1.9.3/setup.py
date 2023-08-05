f697c8af73a367622d5362b57bf560b19c5b78b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:45 +0000

    Add package nrc-ngs-dl/v1.9.3/nrc_ngs_dl-v1.9.3.tar.gz

---
+++ packages/nrc-ngs-dl/nrc_ngs_dl-v1.9.3.tar.gz/nrc_ngs_dl-v1.9.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+ 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('requirements.txt') as f:
+    requires = f.read()
+
+setup(
+    name='nrc_ngs_dl',
+    description='software for downloading and handling sequence data from NRC-LIMS website',
+    long_description=readme,
+    version='v1.9.3',
+    author='Chunfang Zheng',
+    author_email='chunfang.zheng@canada.ca',
+    license=license,
+    packages = find_packages(exclude=('test')),
+    install_requires = requires,
+    scripts = ['config.ini.sample'],
+    entry_points={
+        'console_scripts': [
+            'lims_downloader = nrc_ngs_dl.lims_downloader:main',
+            ],
+    }
+)
