33461e502cca64abd5bf118c63326edf20c9a49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:00 +0000

    Add package invenio-userprofiles/1.0.0b2/invenio_userprofiles-1.0.0b2-py2.py3-none-any.whl

---
+++ packages/invenio-userprofiles/invenio_userprofiles-1.0.0b2-py2.py3-none-any.whl/invenio_userprofiles/admin.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2016, 2017 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Admin views for invenio-userprofiles."""
+
+from __future__ import absolute_import, print_function
+
+from flask_admin.contrib.sqla import ModelView
+
+from .models import UserProfile
+
+
+def _(x):
+    """Identity."""
+    return x
+
+
+class UserProfileView(ModelView):
+    """Userprofiles view. Links User ID to user/full/display name."""
+
+    can_view_details = True
+    can_create = False
+    can_delete = False
+
+    column_list = (
+        'user_id',
+        '_displayname',
+        'full_name',
+    )
+
+    column_searchable_list = \
+        column_filters = \
+        column_details_list = \
+        columns_sortable_list = \
+        column_list
+
+    form_columns = ('username', 'full_name')
+
+    column_labels = {
+        '_displayname': _('Username'),
+    }
+
+
+user_profile_adminview = {
+    'model': UserProfile,
+    'modelview': UserProfileView,
+    'category': _('User Management'),
+}
