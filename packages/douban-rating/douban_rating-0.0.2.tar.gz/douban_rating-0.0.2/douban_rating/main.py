a74895ebe831988943bf1bbc09c092f0142bf677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:29 +0000

    Add package douban-rating/0.0.2/douban_rating-0.0.2.tar.gz

---
+++ packages/douban-rating/douban_rating-0.0.2.tar.gz/douban_rating-0.0.2/douban_rating/main.py
@@ -0,0 +1,32 @@
+import sys
+from douban_rating.query import query
+from simple_table import SimpleTable
+
+
+def print_ratings(ratings):
+    table = SimpleTable()
+    table.set_headers(['Title', 'Rating'])
+    table.add_rows([[rating.title, rating.rating] for rating in ratings])
+
+    print(table)
+
+def main():
+    if len(sys.argv) != 3:
+        print('Usage: douban-rating -book book-name')
+
+        sys.exit(1)
+
+    query_type = sys.argv[1]
+
+    if query_type not in ['-book', '-movie']:
+        print('Invalid type')
+
+        sys.exit(1)
+
+    title = sys.argv[2]
+    ratings = query(query_type.replace('-', ''), title)
+
+    print_ratings(ratings)
+
+if __name__ == '__main__':
+    main()
