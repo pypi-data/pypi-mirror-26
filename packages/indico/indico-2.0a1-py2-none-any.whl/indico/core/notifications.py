7df14a6c2022ee3d9ffeeac2d29b9531acecb678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package indico/2.0a1/indico-2.0a1-py2-none-any.whl

---
+++ packages/indico/indico-2.0a1-py2-none-any.whl/indico/core/notifications.py
@@ -0,0 +1,103 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2017 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from functools import wraps
+from types import GeneratorType
+
+from indico.core.config import config
+from indico.legacy.common.mail import GenericMailer
+
+
+def email_sender(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        mails = fn(*args, **kwargs)
+        if mails is None:
+            return
+        if isinstance(mails, GeneratorType):
+            mails = list(mails)
+        elif not isinstance(mails, list):
+            mails = [mails]
+        for mail in filter(None, mails):
+            send_email(mail)
+    return wrapper
+
+
+def send_email(email, event=None, module='', user=None, skip_queue=False):
+    """Sends an email created by :func:`make_email`.
+
+    :param email: The email object returned by :func:`make_email`
+    :param event: If specified, the email will be saved in that
+                  event's log
+    :param module: The module name to show in the email log
+    :param user: The user to show in the email log
+    :param skip_queue: If true, the email will be sent immediately
+                       instead of being queued until after commit even
+                       while inside a RH context
+    """
+    if event is not None:
+        GenericMailer.sendAndLog(email, event, module=module, user=user, skipQueue=skip_queue)
+    else:
+        GenericMailer.send(email, skipQueue=skip_queue)
+
+
+def make_email(to_list=None, cc_list=None, bcc_list=None, from_address=None, reply_address=None, attachments=None,
+               subject=None, body=None, template=None, html=False):
+    """Creates an email.
+
+    The preferred way to specify the email content is using the
+    `template` argument. To do so, use :func:`.get_template_module` on
+    a template inheriting from ``emails/base.txt`` for test emails or
+    ``emails/base.html`` for HTML emails.
+
+    :param to_list: The recipient email or a collection of emails
+    :param cc_list: The CC email or a collection of emails
+    :param bcc_list: The BCC email or a collection of emails
+    :param from_address: The sender address. Defaults to noreply.
+    :param reply_address: The reply-to address. Defaults to empty.
+    :param attachments: A list of attachments, consisting of dicts
+                        containing ``name`` and ``binary`` keys.
+    :param subject: The subject of the email.
+    :param body: The body of the email:
+    :param template: A template module containing ``get_subject`` and
+                     ``get_body`` macros.
+    :param html: ``True`` if the email body is HTML
+    """
+    if template is not None and (subject is not None or body is not None):
+        raise ValueError("Only subject/body or template can be passed")
+    if template:
+        subject = template.get_subject()
+        body = template.get_body()
+    if to_list is None:
+        to_list = set()
+    if cc_list is None:
+        cc_list = set()
+    if bcc_list is None:
+        bcc_list = set()
+    to_list = {to_list} if isinstance(to_list, basestring) else to_list
+    cc_list = {cc_list} if isinstance(cc_list, basestring) else cc_list
+    bcc_list = {bcc_list} if isinstance(bcc_list, basestring) else bcc_list
+    return {
+        'toList': set(to_list),
+        'ccList': set(cc_list),
+        'bccList': set(bcc_list),
+        'fromAddr': from_address or config.NO_REPLY_EMAIL,
+        'replyAddr': reply_address,
+        'attachments': attachments,
+        'subject': subject,
+        'body': body,
+        'content-type': 'text/html' if html else 'text/plain'
+    }
---
+++ packages/indico/indico-2.0a1-py2-none-any.whl/indico/legacy/common/mail.py
@@ -0,0 +1,169 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2017 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+import smtplib
+from email import charset
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+from flask import g
+
+from indico.core.config import config
+from indico.core.logger import Logger
+from indico.util.string import to_unicode
+
+
+# Prevent base64 encoding of utf-8 e-mails
+charset.add_charset('utf-8', charset.SHORTEST)
+
+
+class GenericMailer:
+    @classmethod
+    def send(cls, notification, skipQueue=False):
+        if isinstance(notification, dict):
+            # Wrap a raw dictionary in a notification class
+            from indico.legacy.webinterface.mail import GenericNotification
+            notification = GenericNotification(notification)
+        # enqueue emails if we have a rh and do not skip queuing, otherwise send immediately
+        rh = g.get('rh')
+        mailData = cls._prepare(notification)
+
+        if mailData:
+            if skipQueue or not rh:
+                cls._send(mailData)
+            else:
+                g.setdefault('email_queue', []).append(mailData)
+
+    @classmethod
+    def has_queue(cls):
+        return bool(g.get('email_queue'))
+
+    @classmethod
+    def flushQueue(cls, send):
+        queue = g.get('email_queue', None)
+        if not queue:
+            return
+        if send:
+            # send all emails
+            for mail in queue:
+                cls._send(mail)
+        # clear the queue no matter if emails were sent or not
+        del queue[:]
+
+    @staticmethod
+    def _prepare(notification):
+        fromAddr = notification.getFromAddr()
+        replyAddr = getattr(notification, '_replyAddr', None)
+        toList = set(filter(None, notification.getToList()))
+        ccList = set(filter(None, notification.getCCList()))
+        if hasattr(notification, "getBCCList"):
+            bccList = set(notification.getBCCList())
+        else:
+            bccList = set()
+
+        if not toList and not ccList and not bccList:
+            return
+
+        msg = MIMEMultipart()
+        msg["Subject"] = to_unicode(notification.getSubject()).strip()
+        msg["From"] = fromAddr
+        msg["To"] = ', '.join(toList) if toList else 'Undisclosed-recipients:;'
+        if ccList:
+            msg["Cc"] = ', '.join(ccList)
+        if replyAddr:
+            msg['Reply-to'] = replyAddr
+        msg["Date"] = formatdate()
+
+        try:
+            ct = notification.getContentType()
+        except Exception:
+            ct = "text/plain"
+
+        body = notification.getBody()
+        if ct == "text/plain":
+            part1 = MIMEText(body, "plain", "utf-8")
+        elif ct == "text/html":
+            part1 = MIMEText(body, "html", "utf-8")
+        else:
+            raise ValueError('Unknown MIME type: {}'.format(ct))
+        msg.attach(part1)
+
+        if hasattr(notification, 'getAttachments'):
+            attachments = notification.getAttachments() or []
+            for attachment in attachments:
+                part2 = MIMEApplication(attachment["binary"])
+                part2.add_header("Content-Disposition",
+                                 'attachment;filename="%s"' % (attachment["name"]))
+                msg.attach(part2)
+
+        return {
+            'msg': msg.as_string(),
+            'toList': toList,
+            'ccList': ccList,
+            'bccList': bccList,
+            'fromAddr': fromAddr,
+        }
+
+    @staticmethod
+    def _send(msgData):
+        server = smtplib.SMTP(*config.SMTP_SERVER)
+        if config.SMTP_USE_TLS:
+            server.ehlo()
+            (code, errormsg) = server.starttls()
+            if code != 220:
+                raise Exception('Cannot start secure connection to SMTP server: {}, {}'.format(code, errormsg))
+        if config.SMTP_LOGIN:
+            login = config.SMTP_LOGIN
+            password = config.SMTP_PASSWORD
+            (code, errormsg) = server.login(login, password)
+            if code != 235:
+                raise Exception('Cannot login on SMTP server: {}, {}'.format(code, errormsg))
+
+        to_addrs = msgData['toList'] | msgData['ccList'] | msgData['bccList']
+        try:
+            Logger.get('mail').info('Sending email: To: {} / CC: {} / BCC: {}'.format(
+                ', '.join(msgData['toList']) or 'None',
+                ', '.join(msgData['ccList']) or 'None',
+                ', '.join(msgData['bccList']) or 'None'))
+            server.sendmail(msgData['fromAddr'], to_addrs, msgData['msg'])
+        except smtplib.SMTPRecipientsRefused as e:
+            raise ValueError('Email address is not valid: {}'.format(e.recipients))
+        finally:
+            server.quit()
+        Logger.get('mail').info('Mail sent to {}'.format(', '.join(to_addrs)))
+
+    @classmethod
+    def sendAndLog(cls, notification, event, module=None, user=None, skipQueue=False):
+        from indico.modules.events.logs import EventLogRealm, EventLogKind
+        if isinstance(notification, dict):
+            # Wrap a raw dictionary in a notification class
+            from indico.legacy.webinterface.mail import GenericNotification
+            notification = GenericNotification(notification)
+        cls.send(notification, skipQueue=skipQueue)
+        log_data = {
+            u'content_type': to_unicode(notification.getContentType()),
+            u'from': to_unicode(notification.getFromAddr()),
+            u'to': map(to_unicode, notification.getToList()),
+            u'cc': map(to_unicode, notification.getCCList()),
+            u'bcc': map(to_unicode, notification.getBCCList()),
+            u'subject': to_unicode(notification.getSubject()).strip(),
+            u'body': to_unicode(notification.getBody()).strip()
+        }
+        summary = u'Sent email: {}'.format(log_data[u'subject'])
+        event.log(EventLogRealm.emails, EventLogKind.other, to_unicode(module or u'Unknown'), summary, user,
+                  type_=u'email', data=log_data)
