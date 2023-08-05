8d9b5338cfa9173530d16d680e7bef004732a660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:14 +0000

    Add package coralillo/0.6.6/coralillo-0.6.6-py3-none-any.whl

---
+++ packages/coralillo/coralillo-0.6.6-py3-none-any.whl/coralillo/tests/test_all.py
@@ -0,0 +1,13 @@
+from coralillo.tests.test_engine import EngineTestCase
+from coralillo.tests.test_events import EventTestCase
+from coralillo.tests.test_fields import FieldTestCase
+from coralillo.tests.test_models import ModelTestCase
+from coralillo.tests.test_permissions import PermissionTestCase
+from coralillo.tests.test_relations import RelationTestCase
+from coralillo.tests.test_presentation import PresentationTestCase
+from coralillo.tests.test_utils import UtilsTestCase
+import unittest
+
+
+if __name__ == '__main__':
+    unittest.main()
