7178614919cf9cb96da6248dd9b1edab89d8de32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:16 +0000

    Add package scattertext/0.0.2.9.12/scattertext-0.0.2.9.12-py3-none-any.whl

---
+++ packages/scattertext/scattertext-0.0.2.9.12-py3-none-any.whl/scattertext/CorpusFromPandas.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from scattertext.Corpus import Corpus
+from scattertext.TermDocMatrixFromPandas import TermDocMatrixFromPandas, build_sparse_matrices
+
+
+class CorpusFromPandas(TermDocMatrixFromPandas):
+	'''Creates a Corpus from a pandas data frame.  A Corpus is a Term Document Matrix
+	preserves the original texts.
+
+	Parameters
+	----------
+	data_frame : pd.DataFrame
+		The data frame that contains columns for the category of interest
+		and the document text.
+	text_col : str
+		The name of the column which contains the document text.
+	category_col : str
+		The name of the column which contains the category of interest.
+	clean_function : function, optional
+	nlp : function, optional
+	verbose : boolean, optional
+		If true, prints a message every time a document index % 100 is 0.
+
+	See Also
+	--------
+	TermDocMatrixFromPandas
+	'''
+
+	def _apply_pipeline_and_get_build_instance(self,
+	                                           X_factory,
+	                                           mX_factory,
+	                                           category_idx_store,
+	                                           df,
+	                                           parse_pipeline,
+	                                           term_idx_store,
+	                                           metadata_idx_store,
+	                                           y):
+		df.apply(parse_pipeline.parse, axis=1)
+		y = np.array(y)
+		X, mX = build_sparse_matrices(y, X_factory, mX_factory)
+		raw_texts = df[self._text_col]
+		return Corpus(X, mX, y, term_idx_store, category_idx_store,
+		              metadata_idx_store, raw_texts)
+
