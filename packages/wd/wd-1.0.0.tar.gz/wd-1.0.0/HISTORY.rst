aca2704e382891ed4513f3d6ff0e306d6fcce876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:50 +0000

    Add package wd/1.0.0/wd-1.0.0.tar.gz

---
+++ packages/wd/wd-1.0.0.tar.gz/wd-1.0.0/HISTORY.rst
@@ -0,0 +1,24 @@
+.. :changelog:
+
+Release History
+---------------
+
+0.1.7 (2017-05-16)
+++++++++++++++++++
+
+**Change**
+
+- Change the output of `send_keys` function, from ['1', '2', '3'] to ['123']
+
+0.1.1 (2016-08-15)
+++++++++++++++++++
+
+**Improvements**
+
+- Export class name in macaca module `#127cde7 <https://github.com/macacajs/wd.py/commit/127cde7c239f144fdfc8d3e137e299d4112fdfc4>`_.
+- Added docs and sample.
+
+0.1.0 (2016-08-10)
+++++++++++++++++++
+
+* Birth!
---
+++ packages/wd/wd-1.0.0.tar.gz/wd-1.0.0/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='wd',
+
+    version='1.0.0',
+
+    description='Macaca Python Client',
+    long_description=long_description,
+
+    url='https://github.com/macacajs/wd.py',
+
+    author='Zichen Zhu',
+    author_email='zic.zhu@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords=[
+        'macaca',
+        'webdriver',
+        'uitest',
+        'mobile automation',
+        'e2e'
+    ],
+
+    packages=find_packages(exclude=['tests*', 'docs']),
+
+    install_requires=[
+        'enum34',
+        'requests',
+        'retrying'
+    ],
+
+    extras_require={
+        'test': ['pytest', 'tox', 'pytest-xdist', 'pytest-cov', 'coverage', 'responses']
+    }
+)
