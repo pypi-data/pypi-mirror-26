4faabdb916ff1e5eaa498ad6c2e3fe6f016d7315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:52 +0000

    Add package razorpay/1.1.0/razorpay-1.1.0.tar.gz

---
+++ packages/razorpay/razorpay-1.1.0.tar.gz/razorpay-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="razorpay",
+    version="1.1.0",
+    description="Razorpay Python Client",
+    url="https://github.com/razorpay/razorpay-python",
+    author="Team Razorpay",
+    author_email="support@razorpay.com",
+    license="MIT",
+    install_requires=["requests"],
+    include_package_data=True,
+    package_dir={'razorpay': 'razorpay', 'razorpay.resources': 'razorpay/resources'},
+    packages=['razorpay', 'razorpay.resources'],
+    keywords='razorpay payment gateway india',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
