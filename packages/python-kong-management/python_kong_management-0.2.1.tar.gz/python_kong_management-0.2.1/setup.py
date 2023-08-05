6ef909df1f616166d1df5edf5c2ef28ebdb20ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:42 +0000

    Add package python-kong-management/0.2.1/python_kong_management-0.2.1.tar.gz

---
+++ packages/python-kong-management/python_kong_management-0.2.1.tar.gz/python_kong_management-0.2.1/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'requests',
+]
+
+setup_requirements = [
+    'pytest-runner',
+]
+
+test_requirements = [
+    'pytest',
+    'requests',
+]
+
+setup(
+    name='python_kong_management',
+    version='0.2.1',
+    description="Python api wrapper for Kong API Gatway",
+    long_description=readme + '\n\n' + history,
+    author="Matt Wisner",
+    author_email='mwisner@fourthlab.com',
+    url='https://github.com/mwisner/python-kong-management',
+    packages=find_packages(include=['kong']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='python-kong-management',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
