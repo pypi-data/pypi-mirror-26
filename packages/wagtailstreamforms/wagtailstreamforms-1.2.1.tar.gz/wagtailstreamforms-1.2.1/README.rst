f722d67f47ef4f66efaf4544b4cb1fe8b24bf42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:52 +0000

    Add package wagtailstreamforms/1.2.1/wagtailstreamforms-1.2.1.tar.gz

---
+++ packages/wagtailstreamforms/wagtailstreamforms-1.2.1.tar.gz/wagtailstreamforms-1.2.1/README.rst
@@ -0,0 +1,125 @@
+Wagtail StreamForms
+===================
+
+|CircleCI| |Codecov|
+
+This package is currently a concept but allows you to add add forms that
+are built in the cms admin area to any streamfield. You can also create
+your own form templates which will then appear as a template choice when
+you build your form. This allows you to decide how the form is submitted
+and to where.
+
+Documentation is currently being worked on but the basics are below
+
+Whats included?
+---------------
+
+-  Forms can be built in the cms admin and used site wide in any streamfield.
+-  You can create your own form templates to display/submit how ever you wish to do it.
+-  There is a ``before_page_serve`` hook that will handle the form ``POST`` submission. Which can easily be disabled to provide your own.
+-  Forms are catagorised by their class in the cms admin for easier navigation.
+-  Form submissions are also listed by their form which you can filter by date and are ordered by newest first.
+-  Recaptcha can be added to a form.
+-  You can also add site wide regex validators fo use in regex fields.
+
+Documentation
+-------------
+
+Can be found on `readthedocs <http://wagtailstreamforms.readthedocs.io/>`_.
+
+Screen shots
+------------
+
+.. figure:: https://github.com/AccentDesign/wagtailstreamforms/raw/master/images/screen5.png
+   :alt: screen5
+
+   Example Front End
+
+.. figure:: https://github.com/AccentDesign/wagtailstreamforms/raw/master/images/screen1.png
+   :alt: Screen1
+
+   Menu
+
+.. figure:: https://github.com/AccentDesign/wagtailstreamforms/raw/master/images/screen2.png
+   :alt: Screen2
+
+   Form Listing
+
+.. figure:: https://github.com/AccentDesign/wagtailstreamforms/raw/master/images/screen3.png
+   :alt: Screen3
+
+   Submission Listing
+
+.. figure:: https://github.com/AccentDesign/wagtailstreamforms/raw/master/images/screen4.png
+   :alt: Screen4
+
+   Form Editing
+
+Example site with docker
+------------------------
+
+Clone the repo
+
+.. code:: bash
+
+    $ git clone https://github.com/AccentDesign/wagtailstreamforms.git
+
+Run the docker container
+
+.. code:: bash
+
+    $ cd wagtailstreamforms
+    $ docker-compose up
+
+Create yourself a superuser
+
+.. code:: bash
+
+    $ docker exec -it <container_name> bash
+    $ python manage.py createsuperuser
+
+Go to http://127.0.0.1:8000
+
+Testing
+-------
+
+Install dependencies
+
+You will need pyenv installed see https://github.com/pyenv/pyenv
+
+Also tox needs to be installed
+
+.. code:: bash
+
+    $ pip install tox
+
+Install python versions in pyenv
+
+.. code:: bash
+
+    $ pyenv install 3.4.4
+    $ pyenv install 3.5.3
+    $ pyenv install 3.6.2
+
+Set local project versions
+
+.. code:: bash
+
+    $ pyenv local 3.4.4 3.5.3 3.6.2
+
+Run the tests
+
+.. code:: bash
+
+    $ tox
+
+or run for a single environment
+
+.. code:: bash
+
+    $ tox -e py36-dj111-wt112
+
+.. |CircleCI| image:: https://circleci.com/gh/AccentDesign/wagtailstreamforms/tree/master.svg?style=svg
+   :target: https://circleci.com/gh/AccentDesign/wagtailstreamforms/tree/master
+.. |Codecov| image:: https://codecov.io/gh/AccentDesign/wagtailstreamforms/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/AccentDesign/wagtailstreamforms
---
+++ packages/wagtailstreamforms/wagtailstreamforms-1.2.1.tar.gz/wagtailstreamforms-1.2.1/wagtailstreamforms/models/form.py
@@ -0,0 +1,208 @@
+import json
+import six
+
+from django.core.mail import send_mail
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from model_utils.managers import InheritanceManager
+from modelcluster.models import ClusterableModel
+from multi_email_field.fields import MultiEmailField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, TabbedInterface, ObjectList, PageChooserPanel
+from wagtailstreamforms.conf import settings
+from wagtailstreamforms.forms import FormBuilder
+from wagtailstreamforms.utils import recaptcha_enabled
+
+from .submission import FormSubmission
+
+
+class BaseForm(ClusterableModel):
+    """ A form base class, any form should inherit from this. """
+
+    name = models.CharField(
+        max_length=255
+    )
+    template_name = models.CharField(
+        verbose_name='template',
+        max_length=255,
+        choices=settings.WAGTAILSTREAMFORMS_FORM_TEMPLATES
+    )
+    submit_button_text = models.CharField(
+        max_length=100,
+        default='Submit'
+    )
+    store_submission = models.BooleanField(
+        default=False
+    )
+    add_recaptcha = models.BooleanField(
+        default=False,
+        help_text=_('Add a reCapcha field to the form.')
+    )
+    success_message = models.CharField(
+        blank=True,
+        max_length=255,
+        help_text=_('An optional success message to show when the form has been successfully submitted')
+    )
+    post_redirect_page = models.ForeignKey(
+        'wagtailcore.Page',
+        models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='+',
+        help_text=_('The page to redirect to after a successful submission')
+    )
+
+    settings_panels = [
+        FieldPanel('name', classname='full'),
+        FieldPanel('template_name'),
+        FieldPanel('submit_button_text'),
+        FieldPanel('success_message', classname='full'),
+        FieldPanel('store_submission'),
+        PageChooserPanel('post_redirect_page')
+    ]
+
+    field_panels = [
+        InlinePanel('form_fields', label='Fields'),
+    ]
+
+    edit_handler = TabbedInterface([
+        ObjectList(settings_panels, heading='General'),
+        ObjectList(field_panels, heading='Fields'),
+    ])
+
+    objects = InheritanceManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name', ]
+        verbose_name = _('form')
+
+    def get_form_fields(self):
+        """
+        Form expects `form_fields` to be declared.
+        If you want to change backwards relation name,
+        you need to override this method.
+        """
+
+        return self.form_fields.all()
+
+    def get_data_fields(self):
+        """
+        Returns a list of tuples with (field_name, field_label).
+        """
+
+        data_fields = [
+            ('submit_time', _('Submission date')),
+        ]
+        data_fields += [
+            (field.clean_name, field.label)
+            for field in self.get_form_fields()
+        ]
+
+        return data_fields
+
+    def get_form_class(self):
+        fb = FormBuilder(self.get_form_fields(), add_recaptcha=self.add_recaptcha)
+        return fb.get_form_class()
+
+    def get_form_parameters(self):
+        return {}
+
+    def get_form(self, *args, **kwargs):
+        form_class = self.get_form_class()
+        form_params = self.get_form_parameters()
+        form_params.update(kwargs)
+
+        return form_class(*args, **form_params)
+
+    def get_submission_class(self):
+        """
+        Returns submission class.
+
+        You can override this method to provide custom submission class.
+        Your class must be inherited from AbstractFormSubmission.
+        """
+
+        return FormSubmission
+
+    def process_form_submission(self, form):
+        """
+        Accepts form instance with submitted data.
+        Creates submission instance if self.store_submission = True.
+
+        You can override this method if you want to have custom creation logic.
+        For example, you want to additionally send an email.
+        """
+
+        if self.store_submission:
+            return self.get_submission_class().objects.create(
+                form_data=json.dumps(form.cleaned_data, cls=DjangoJSONEncoder),
+                form=self
+            )
+
+
+if recaptcha_enabled():  # pragma: no cover
+    BaseForm.field_panels.insert(0, FieldPanel('add_recaptcha'))
+
+
+class BasicForm(BaseForm):
+    """ A basic form. """
+
+
+class EmailForm(BaseForm):
+    """ A form that sends and email. """
+
+    # do not add these fields to the email
+    ignored_fields = ['recaptcha', 'form_id', 'form_reference']
+
+    subject = models.CharField(
+        max_length=255
+    )
+    from_address = models.EmailField()
+    to_addresses = MultiEmailField()
+    message = models.TextField()
+    fail_silently = models.BooleanField(
+        default=True
+    )
+
+    email_panels = [
+        FieldPanel('subject', classname="full"),
+        FieldPanel('from_address', classname="full"),
+        FieldPanel('to_addresses', classname="full"),
+        FieldPanel('message', classname="full"),
+        FieldPanel('fail_silently'),
+    ]
+
+    edit_handler = TabbedInterface([
+        ObjectList(BaseForm.settings_panels, heading='General'),
+        ObjectList(BaseForm.field_panels, heading='Fields'),
+        ObjectList(email_panels, heading='Email Submission'),
+    ])
+
+    def process_form_submission(self, form):
+        super(EmailForm, self).process_form_submission(form)
+        self.send_form_mail(form)
+
+    def send_form_mail(self, form):
+        content = [self.message + '\n\nSubmission\n', ]
+
+        for name, field in form.fields.items():
+            data = form.cleaned_data.get(name)
+
+            if name in self.ignored_fields or not data:
+                continue  # pragma: no cover
+
+            label = field.label or name
+
+            content.append(label + ': ' + six.text_type(data))
+
+        send_mail(
+            self.subject,
+            '\n'.join(content),
+            self.from_address,
+            self.to_addresses,
+            self.fail_silently
+        )
