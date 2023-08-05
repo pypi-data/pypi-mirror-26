b75913fb2de449bb5d6560ca999d79fed7c5b87f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:30 +0000

    Add package deepmachine/0.1.6/deepmachine-0.1.6.tar.gz

---
+++ packages/deepmachine/deepmachine-0.1.6.tar.gz/deepmachine-0.1.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='deepmachine',
+      version='0.1.6',
+      description='Deep Learning Framework',
+      packages=find_packages(),
+      author='Yuxiang ZHOU',
+      author_email='mr.yuxiang.zhou@googlemail.com',
+      url='https://github.com/yuxiang-zhou/deepmachine'
+      )
