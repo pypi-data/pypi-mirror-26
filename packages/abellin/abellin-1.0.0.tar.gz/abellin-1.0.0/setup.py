6c894bb111a88e35dfaef26fc8d19c97b12aeacc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:13 +0000

    Add package abellin/1.0.0/abellin-1.0.0.tar.gz

---
+++ packages/abellin/abellin-1.0.0.tar.gz/abellin-1.0.0/setup.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+from distutils.core import setup
+
+setup(
+       name          = 'abellin',
+       version       = '1.0.0',
+       py_modules    = ['abellin'],
+       author        = 'abel',
+       author_email  = 'abel.lin@mail.com',
+       
+url           = 'http://www.headfirstlabs.com',
+       description   = 'A simple printer of nested lists',
+       )
