9c9e730e688e6a22e0f5446733528d8262edf997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:45 +0000

    Add package IGitt/0.4.1.dev20171023115925/IGitt-0.4.1.dev20171023115925-py3-none-any.whl

---
+++ packages/IGitt/IGitt-0.4.1.dev20171023115925-py3-none-any.whl/IGitt/Interfaces/Hoster.py
@@ -0,0 +1,44 @@
+"""
+Contains the git Hoster abstraction.
+"""
+from typing import Set
+
+from IGitt.Interfaces import IGittObject
+from IGitt.Interfaces.Repository import Repository
+
+
+class Hoster(IGittObject):
+    """
+    Abstracts a service like GitHub and allows e.g. to query for available
+    repositories and stuff like that.
+    """
+    @property
+    def master_repositories(self) -> Set[Repository]:
+        """
+        Retrieves the repositories the user has administrative access to.
+        """
+        raise NotImplementedError
+
+    @property
+    def owned_repositories(self) -> Set[Repository]:
+        """
+        Retrieves the full names of the owned repositories as strings.
+
+        :return: A set of strings.
+        """
+        raise NotImplementedError
+
+    @property
+    def write_repositories(self) -> Set[Repository]:
+        """
+        Retrieves the full names of repositories this user can write to.
+
+        :return: A set of strings.
+        """
+        raise NotImplementedError
+
+    def get_repo(self, repository) -> Repository:
+        """
+        Return a repository object.
+        """
+        raise NotImplementedError
