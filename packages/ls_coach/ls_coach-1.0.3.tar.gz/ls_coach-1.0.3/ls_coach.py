ca08b8e0708ba36a2baec94c6ac1bdf88779fa61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:40 +0000

    Add package ls_coach/1.0.3/ls_coach-1.0.3.tar.gz

---
+++ packages/ls_coach/ls_coach-1.0.3.tar.gz/ls_coach-1.0.3/ls_coach.py
@@ -0,0 +1,16 @@
+
+def coach (files,n):
+        try:
+                if n > 0:
+                    with open (files[n-1]) as james:
+                        data = james.readline()
+                        james_line=data.strip().split(',')
+                    print (files[n-1]+': ')
+                    print(sorted(james_line))
+                    n=n-1
+                    coach (files,n)          
+        except IOError as err:
+                print ('Fehler:' +str(err))
+        except ValueError as verr:
+                print ('Fehler: ' +str(verr))
+
