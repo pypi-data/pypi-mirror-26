3b6334c35846ec6a4e0542a3b826c71488f59259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:33 +0000

    Add package hsync/1.0.1/hsync-1.0.1.tar.gz

---
+++ packages/hsync/hsync-1.0.1.tar.gz/hsync-1.0.1/hsync/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+@author: banixcyan
+
+@license: (C) Copyright 2013-2017, Tencent Corporation Limited.
+
+@contact: banixcyan@gmail.com
+
+@time: 2017/10/26 13:54
+
+@desc:
+
+"""
---
+++ packages/hsync/hsync-1.0.1.tar.gz/hsync-1.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='hsync',
+      version='1.0.1',
+      description='a file sync tools based http',
+      author='banixc',
+      author_email='banixc@qq.com',
+      url=' ',
+      py_modules=['hsync'],
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'hsync = hsync.local:run',
+              'hsyncd = hsync.server:run',
+          ],
+      }
+      )
