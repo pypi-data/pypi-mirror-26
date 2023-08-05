a5b7eba2027bac108b863f248cf67864349e3188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package nbssh/0.5.1/nbssh-0.5.1.tar.gz

---
+++ packages/nbssh/nbssh-0.5.1.tar.gz/nbssh-0.5.1/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+#from kdnb import __version__
+
+config = {
+    'description': 'Quickly SSH into a Netbox device..',
+    'author': 'Jon Purdy',
+    'url': 'https://github.com/jonpurdy/nbssh/',
+    'author_email': 'jon+nbssh@jonpurdy.com',
+    'version': '0.5.1',
+    'install_requires': [
+    'requests',
+    'docopt',
+    'inquirer',
+    'blessings',
+    'configparser'
+    ],
+    'packages': ['nbssh'],
+    'scripts': [],
+    'license': 'GNU General Public License v3 (GPLv3)',
+    'entry_points': {'console_scripts': ['nbssh=nbssh.__main__:main']},
+    'name': 'nbssh'
+}
+
+setup(**config)
