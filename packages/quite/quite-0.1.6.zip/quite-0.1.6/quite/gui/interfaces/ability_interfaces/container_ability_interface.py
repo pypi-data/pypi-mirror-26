b35303a23c4b2cf3dca437dc7a63b4f5be66202a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:38 +0000

    Add package quite/0.1.6/quite-0.1.6.zip

---
+++ packages/quite/quite-0.1.6.zip/quite-0.1.6/quite/gui/interfaces/ability_interfaces/container_ability_interface.py
@@ -0,0 +1,25 @@
+from .. import BaseInterface
+from ... import run_deferred_function
+
+
+class ContainerAbilityInterface(BaseInterface):
+    def set_central_widget(self, w):
+        return run_deferred_function('set_central_widget', self, w)
+
+    def set_square_widget(self, w, spacing=0):
+        return run_deferred_function('set_square_widget', self, w, spacing)
+
+    def set_layout_spacing(self, spacing):
+        return run_deferred_function('set_layout_spacing', self, spacing)
+
+    def export_to_image(self, filename: str):
+        if filename.lower().endswith('.pdf'):
+            return self.export_to_pdf(filename)
+        else:
+            return self.export_to_bitmap(filename)
+
+    def export_to_pdf(self, filename):
+        return run_deferred_function('export_to_pdf', self, filename)
+
+    def export_to_bitmap(self, filename):
+        return run_deferred_function('export_to_bitmap', self, filename)
