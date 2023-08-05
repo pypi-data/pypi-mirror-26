6e52566b684b9ef7966c58004a1a9eb4cc112e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:50 +0000

    Add package lifepo4weredPyController/0.1.0/lifepo4weredPyController-0.1.0-py2.py3-none-any.whl

---
+++ packages/lifepo4weredPyController/lifepo4weredPyController-0.1.0-py2.py3-none-any.whl/lifepo4weredPyController/USBPowerSource.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import copy
+from observablePy import Observable, observable_property
+import lifepo4weredPy
+
+
+class USBPowerSource(Observable):
+    def __init__(self):
+        super(USBPowerSource, self).__init__()
+        self.instanceState = {
+          "voltage": 0,
+          "pluggedIn": False,
+        }
+        self.addObservableElement("voltage")
+        self.addObservableElement("pluggedIn")
+
+    @property
+    def voltage(self):
+        return lifepo4weredPy.read(lifepo4weredPy.variablesEnum.VIN)
+
+    @property
+    def pluggedIn(self):
+        return lifepo4weredPy.read(lifepo4weredPy.variablesEnum.VIN) > 0
+
+    def _diffuseChanges(self):
+        if self.hasObservers():
+            voltage = self.voltage
+
+            if (voltage != self.instanceState["voltage"]):
+                previousState = copy.deepcopy(self.instanceState)
+
+                self.instanceState["voltage"] = voltage
+                self.instanceState["pluggedIn"] = voltage > 0
+
+                self.diffuse(previousState, self.instanceState)
---
+++ packages/lifepo4weredPyController/lifepo4weredPyController-0.1.0-py2.py3-none-any.whl/lifepo4weredPyController/__init__.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from .Battery import Battery
+from .LedStateEnum import ledStateEnum
+from .Led import Led
+from .USBPowerSource import USBPowerSource
+from .TouchStateEnum import touchStateEnum
+from .WakeTimer import WakeTimer
+from .Touch import Touch
+from .Reader import Reader
+
+__author__ = "Frederick Lussier <frederick.lussier@hotmail.com>"
+__status__ = "dev"
+__version__ = "0.1.0"
+__date__ = "september 10th 2017"
+
+__all__ = [
+    "Battery",
+    "USBPowerSource",
+    "Led", "ledStateEnum",
+    "Touch", "touchStateEnum"
+]
+
+battery = Battery()
+led = Led()
+touch = Touch()
+wakeTimer = WakeTimer()
+usbPowerSource = USBPowerSource()
+
+__reader = Reader()
+__reader.add(battery._diffuseChanges)
+__reader.add(usbPowerSource._diffuseChanges)
+__reader.add(touch._diffuseChanges)
+__reader.startPeriodicallyReading()
+
+
+def getPeriodicInterval():
+    """
+    Get the interval between read data from power supply board
+    """
+    return __reader.interval
+
+
+def setPeriodicInterval(interval):
+    """
+    Set the interval between read data from power supply board
+
+    :param number interval: delay in second
+    """
+    __reader.interval = interval
+
+
+def ceaseReading():
+    """
+    Cease reading periodically the data from power supply board.
+
+    This is important when you close your application.
+    """
+    __reader.stop()
+
+
+def restartReading():
+    """
+    Restart reading periodically the data from power supply board.
+
+    """
+    __reader.restart()
