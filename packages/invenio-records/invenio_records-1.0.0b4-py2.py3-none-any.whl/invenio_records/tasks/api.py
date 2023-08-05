660346621e2e60a2b23ed0fc701a6fba52e8aeb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:11 +0000

    Add package invenio-records/1.0.0b4/invenio_records-1.0.0b4-py2.py3-none-any.whl

---
+++ packages/invenio-records/invenio_records-1.0.0b4-py2.py3-none-any.whl/invenio_records/tasks/api.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015, 2016, 2017 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Celery tasks for Invenio-Records."""
+
+from __future__ import absolute_import
+
+import warnings
+
+from celery import shared_task
+from celery.utils.log import get_task_logger
+from flask import current_app
+from sqlalchemy import exc
+
+from ..api import Record
+
+logger = get_task_logger(__name__)
+
+warnings.warn(
+    "Invenio-Records Celery tasks module will be removed.", DeprecationWarning)
+
+
+@shared_task
+def create_record(data=None, id_=None, force=False):
+    """Create a record using a Celery task."""
+    from invenio_db import db
+    try:
+        return str(Record.create(data, id_=id_).id)
+    except exc.IntegrityError:
+        if force:
+            current_app.logger.warning(
+                "Trying to force insert: {0}".format(id_))
+            record = Record.get_record(id_)
+            return str(Record(data, model=record.model).commit().id)
+    finally:
+        db.session.commit()
