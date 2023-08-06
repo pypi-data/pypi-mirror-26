b6f22008b2c904e59e5fc3661bbb34aab5d9ad1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:48 +0000

    Add package dcm2bids/1.0.1/dcm2bids-1.0.1.tar.gz

---
+++ packages/dcm2bids/dcm2bids-1.0.1.tar.gz/dcm2bids-1.0.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+description = """Reorganising NIfTI files from dcm2niix into the Brain Imaging Data Structure"""
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+from setuptools import setup
+
+
+DISTNAME = "dcm2bids"
+DESCRIPTION = description
+VERSION = "1.0.1"
+AUTHOR = "Christophe Bedetti"
+AUTHOR_EMAIL = "christophe.bedetti@umontreal.ca"
+URL = "https://github.com/cbedetti/Dcm2Bids"
+DOWNLOAD_URL = URL + "/archive/" + VERSION + ".tar.gz"
+
+
+if __name__ == "__main__":
+    setup(
+            name=DISTNAME,
+            version=VERSION,
+            description=description,
+            long_description=long_description,
+            author=AUTHOR,
+            author_email=AUTHOR_EMAIL,
+            url=URL,
+            download_url=DOWNLOAD_URL,
+            packages=['dcm2bids'],
+            scripts=['scripts/dcm2bids', 'scripts/dcm2bids_helper'],
+            install_requires=['future'],
+            )
