b7336ced926e4b4faba15d2d8a324952f1a2d273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:15 +0000

    Add package nester-sytang/1.0.0/nester-sytang-1.0.0.tar.gz

---
+++ packages/nester-sytang/nester-sytang-1.0.0.tar.gz/nester-sytang-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester-sytang',
+        version = '1.0.0',
+        py_modles = ['nester-sytang'],
+        author = 'sytang',
+        author_email = '3048608732@qq.com',
+        url = 'https://en.mail.qq.com',
+        description = 'A simple printer of nasted lists',
+     )
