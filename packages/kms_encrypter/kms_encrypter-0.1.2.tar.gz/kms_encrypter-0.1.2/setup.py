e525d176955d613092d7ee988816beee6ac6787e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:39 +0000

    Add package kms_encrypter/0.1.2/kms_encrypter-0.1.2.tar.gz

---
+++ packages/kms_encrypter/kms_encrypter-0.1.2.tar.gz/kms_encrypter-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kms_encrypter',
+      version='0.1.2',
+      description='Encrypt using AWS KMS',
+      url='https://github.com/haradashinya/kms_encrypter',
+      author='furodrive',
+      author_email='haradashinya@gmail.com',
+      license='MIT',
+      packages=['kms_encrypter'],
+      zip_safe=False)
