31c788616ee64d40396e5c21609f600a3114b48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:56 +0000

    Add package tfr/0.2.3/tfr-0.2.3.tar.gz

---
+++ packages/tfr/tfr-0.2.3.tar.gz/tfr-0.2.3/tfr/__init__.py
@@ -0,0 +1,5 @@
+from .reassignment import Spectrogram, LinearTransform, PitchTransform, \
+    reassigned_spectrogram, pitchgram
+from .signal import SignalFrames
+from .sklearn import PitchgramTransformer
+from .tuning import Tuning, PitchQuantizer
