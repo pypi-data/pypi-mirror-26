092038b8b58655ddba568a8f61533b0ed30b08d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:25 +0000

    Add package validatexmls/0.1.1/validatexmls-0.1.1.tar.gz

---
+++ packages/validatexmls/validatexmls-0.1.1.tar.gz/validatexmls-0.1.1/validatexmls/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+def get_data(path):
+    return os.path.join(_ROOT, 'data', path)
+
+s = get_data('testfile.txt')
+print(s, os.path.exists(s))
+
+s = get_data('xsd/BC9.xsd')
+print(s, os.path.exists(s))
+
+s = get_data('input/35104616_49_30527.XML')
+print(s, os.path.exists(s))
+pass
