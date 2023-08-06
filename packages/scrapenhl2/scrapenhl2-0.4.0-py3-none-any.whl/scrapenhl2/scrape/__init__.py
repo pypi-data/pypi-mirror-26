402477ae42e6a27facf05f7bc00e78a39ef23dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:15 +0000

    Add package scrapenhl2/0.4.0/scrapenhl2-0.4.0-py3-none-any.whl

---
+++ packages/scrapenhl2/scrapenhl2-0.4.0-py3-none-any.whl/scrapenhl2/scrape/__init__.py
@@ -0,0 +1,17 @@
+"""
+Indicates to import all .py files
+"""
+__all__ = ['autoupdate',
+           'events',
+           'games'
+           'general_helpers',
+           'manipulate_schedules',
+           'organization',
+           'parse_pbp',
+           'parse_toi',
+           'players',
+           'schedules',
+           'scrape_pbp',
+           'scrape_toi',
+           'team_info',
+           'teams']
