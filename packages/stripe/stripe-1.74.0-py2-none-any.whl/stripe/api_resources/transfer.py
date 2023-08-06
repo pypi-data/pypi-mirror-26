9bf5b52d1d2f18716a30af4f092ae13ecf9b4292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:45 +0000

    Add package stripe/1.74.0/stripe-1.74.0-py2-none-any.whl

---
+++ packages/stripe/stripe-1.74.0-py2-none-any.whl/stripe/api_resources/transfer.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function
+
+from stripe.api_resources.abstract import CreateableAPIResource
+from stripe.api_resources.abstract import UpdateableAPIResource
+from stripe.api_resources.abstract import ListableAPIResource
+from stripe.api_resources.abstract import nested_resource_class_methods
+
+
+@nested_resource_class_methods('reversal', operations=['create', 'retrieve',
+                                                       'update', 'list'])
+class Transfer(CreateableAPIResource, UpdateableAPIResource,
+               ListableAPIResource):
+    OBJECT_NAME = 'transfer'
+
+    def cancel(self):
+        self.refresh_from(self.request('post',
+                          self.instance_url() + '/cancel'))
