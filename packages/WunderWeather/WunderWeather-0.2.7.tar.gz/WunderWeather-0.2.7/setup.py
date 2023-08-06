0377d5b6f1dd77dfaea40342055ffc7e592f98f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:17 +0000

    Add package WunderWeather/0.2.7/WunderWeather-0.2.7.tar.gz

---
+++ packages/WunderWeather/WunderWeather-0.2.7.tar.gz/WunderWeather-0.2.7/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+# this is only necessary when not using setuptools/distribute
+from sphinx.setup_command import BuildDoc
+cmdclass = {'build_sphinx': BuildDoc}
+
+# To use a consistent encoding
+from codecs import open
+from os import path, pardir
+
+# Get the long description from the README file
+# with open('README.rst', encoding='utf-8') as f:
+#     long_description = f.read()
+
+setup(
+  name = 'WunderWeather',
+  packages = ['WunderWeather'],
+  install_requires = ['easydict','requests'],
+  version = '0.2.7',
+  description = 'Wrapper for Weather Underground API',
+  #long_description = long_description,
+  author = 'Tyler Santos',
+  author_email = '1tsantos7+wunderweather@gmail.com',
+  url = 'https://github.com/T-Santos/WunderWeather',
+  keywords = ['weather', 'wunderground'],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.5'],
+  cmdclass=cmdclass,
+)
