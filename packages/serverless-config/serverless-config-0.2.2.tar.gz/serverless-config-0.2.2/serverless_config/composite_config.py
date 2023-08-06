4e70905718a8f884030ac0cfce2157d72cb5c57c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:36 +0000

    Add package serverless-config/0.2.2/serverless-config-0.2.2.tar.gz

---
+++ packages/serverless-config/serverless-config-0.2.2.tar.gz/serverless-config-0.2.2/serverless_config/composite_config.py
@@ -0,0 +1,37 @@
+''' Composite Config.
+
+    Falls back to other configs if a parameter is not found.
+
+    The order of precedence is provided by the constructor.
+'''
+
+from .config_base import ConfigBase
+
+
+class CompositeConfig(ConfigBase):
+    '''Composite Config'''
+
+    def __init__(self, *configs):
+        '''Pass in as many configs as you want, in order of precedence'''
+        self.configs = configs
+
+    def get_str(self, prop_name, default_value=None, **kwargs):
+        '''Get the string property by name'''
+        for config in self.configs:
+            try:
+                return config.get_str(prop_name, **kwargs)
+            except ValueError:
+                pass
+
+        if default_value:
+            return default_value
+
+        raise ValueError('Property not found: ' + prop_name)
+
+    def get_int(self, prop_name, default_value=None, **kwargs):
+        ''' Get an int property by name.
+
+            Raises ValueError if not found.
+            Raises ValueError if not an int.
+        '''
+        return int(self.get_str(prop_name, default_value, **kwargs))
