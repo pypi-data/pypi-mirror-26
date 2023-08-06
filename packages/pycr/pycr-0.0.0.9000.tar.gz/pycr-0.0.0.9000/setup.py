7bd16862bc57c39bdffcdf81d834dbd884101a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:37 +0000

    Add package pycr/0.0.0.9000/pycr-0.0.0.9000.tar.gz

---
+++ packages/pycr/pycr-0.0.0.9000.tar.gz/pycr-0.0.0.9000/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycr',
+      version='0.0.0.9000',
+      description='Analyzing Real-Time Quantitative PCR Data',
+      url='https://github.com/MahShaaban/pycr',
+      author='Mahmoud Ahmed',
+      author_email='mahmoud.s.fahmy@students.kasralainy.edu.eg',
+      license='GPL-3',
+      packages=['pycr'],
+      zip_safe=False)
