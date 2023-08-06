73b3219c70a83131b019043385ea4a758c4c0759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:24 +0000

    Add package dopplerr/0.6.1/dopplerr-0.6.1.tar.gz

---
+++ packages/dopplerr/dopplerr-0.6.1.tar.gz/dopplerr-0.6.1/dopplerr/api/v1/medias.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+# Standard Libraries
+import logging
+
+# Third Party Libraries
+from sanic import Blueprint
+from sanic_transmute import add_route
+from sanic_transmute import describe
+from schematics.models import Model
+
+# Dopplerr
+from dopplerr.downloader import DopplerrDownloader
+
+log = logging.getLogger(__name__)
+
+
+class RequestAnswer(Model):
+    pass
+
+
+bp = Blueprint('medias', url_prefix="/api/v1")
+
+
+@describe(paths="/medias/", methods="GET")
+async def fullscan(request) -> RequestAnswer:
+    content = request.json
+    logging.debug("Fullscan request: %r", content)
+    res = DopplerrDownloader().process_fullscan(content)
+    res = "Unimplemented"
+    return res
+
+
+add_route(bp, fullscan)
