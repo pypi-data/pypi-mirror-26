4a61605c8bb0f50ec8fc3dbf7528d00ab7c9545f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:12 +0000

    Add package pylinda/0.1.1/pylinda-0.1.1.tar.gz

---
+++ packages/pylinda/pylinda-0.1.1.tar.gz/pylinda-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='pylinda',
+    version='0.1.1',
+    description='',
+    author_email='appa@engineer.com',
+    license='MIT',
+    packages=['pylinda'],
+    scripts=['bin/pylinda-svr'],
+    install_requires=[
+    '',
+    ],
+    zip_safe=False
+    )
