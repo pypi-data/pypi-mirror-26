db0d4ae49431c2b9f5c1569af39062ecbc47c9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:15 +0000

    Add package scitokens/0.3.0/scitokens-0.3.0.tar.gz

---
+++ packages/scitokens/scitokens-0.3.0.tar.gz/scitokens-0.3.0/setup.py
@@ -0,0 +1,20 @@
+
+"""
+Install file for SciTokens project.
+"""
+
+import setuptools
+
+setuptools.setup(name="scitokens",
+                 version="0.3.0",
+                 description="SciToken reference implementation library",
+                 author_email="team@scitokens.org",
+                 author="Brian Bockelman",
+                 url="https://scitokens.org",
+                 package_dir={"": "src"},
+                 packages=["scitokens"],
+                 scripts=['tools/scitokens-admin-create-token',
+                          'tools/scitokens-admin-create-key'],
+                 install_requires=['cryptography',
+                                   'PyJWT'],
+                )
