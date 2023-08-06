e599b45b7090fbc78e8178c0d1325f0e1fffe7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:05 +0000

    Add package ofxparse/0.17/ofxparse-0.17.tar.gz

---
+++ packages/ofxparse/ofxparse-0.17.tar.gz/ofxparse-0.17/tests/test_write.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+from ofxparse import OfxParser, OfxPrinter
+from unittest import TestCase
+from os import close, remove
+from tempfile import mkstemp
+import sys
+sys.path.append('..')
+from .support import open_file
+
+
+class TestOfxWrite(TestCase):
+    def test_write(self):
+        with open_file('fidelity.ofx') as f:
+            ofx = OfxParser.parse(f)
+        self.assertEqual(str(ofx), "")
+
+    def test_using_ofx_printer(self):
+        with open_file('checking.ofx') as f:
+            ofx = OfxParser.parse(f)
+        fd, name = mkstemp()
+        close(fd)
+        printer = OfxPrinter(ofx=ofx, filename=name)
+        printer.write(tabs=1)
+
+if __name__ == "__main__":
+    import unittest
+    unittest.main()
