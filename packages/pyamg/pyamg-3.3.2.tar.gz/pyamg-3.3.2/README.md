2daaa3269b45653bc981eb8da11f58342e033a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:00 +0000

    Add package pyamg/3.3.2/pyamg-3.3.2.tar.gz

---
+++ packages/pyamg/pyamg-3.3.2.tar.gz/pyamg-3.3.2/README.md
@@ -0,0 +1,99 @@
+[![Build Status](https://img.shields.io/travis/pyamg/pyamg/master.svg?style=flat-square)](https://travis-ci.org/pyamg/pyamg)
+[![Coverage Status](https://img.shields.io/codecov/c/github/pyamg/pyamg/master.svg?style=flat-square)](https://codecov.io/gh/pyamg/pyamg)
+[![PyPi](https://img.shields.io/pypi/pyversions/pyamg.svg?style=flat-square)](https://pypi.python.org/pypi/pyamg/)
+
+# Installation
+PyAMG requires `numpy` and `scipy`
+
+      pip install pyamg
+
+or
+
+      python setup.py install
+
+
+# Introduction
+
+PyAMG is a library of **Algebraic Multigrid (AMG)** solvers with a convenient Python interface.
+
+![](Docs/logo/PyAMG_logo.png)
+![](Docs/logo/CS_logo.png)
+
+PyAMG is developed by **[Nathan Bell](http://graphics.cs.uiuc.edu/~wnbell/)**, **[Luke Olson](http://lukeo.cs.illinois.edu)**, and **[Jacob Schroder](http://people.llnl.gov/schroder2)**, in the **[Deparment of Computer Science](http://www.cs.uiuc.edu)** at the **[University of Illinois at Urbana-Champaign](http://www.illinois.edu)**.  Portions of the project were partially supported by the [NSF](http://www.nsf.gov) under award DMS-0612448.
+
+
+
+# Citing
+
+<pre>
+@MISC{BeOlSc2011,
+      author = "Bell, W. N. and Olson, L. N. and Schroder, J. B.",
+      title = "{PyAMG}: Algebraic Multigrid Solvers in {Python} v3.0",
+      year = "2015",
+      url = "https://github.com/pyamg/pyamg",
+      note = "Release 3.2"
+      }
+</pre>
+
+# Getting Help
+
+For documentation see [http://pyamg.readthedocs.io/en/latest/](http://pyamg.readthedocs.io/en/latest/).
+
+Creat an [issue](https://github.com/pyamg/pyamg/issues).
+
+Look at the [Tutorial](https://github.com/pyamg/pyamg/wiki/Tutorial) or the [Examples](https://github.com/pyamg/pyamg/wiki/Examples) (for instance  the [0STARTHERE](https://github.com/pyamg/pyamg-examples/blob/master/0STARTHERE/demo.py) example).
+
+# What is AMG?
+
+ AMG is a multilevel technique for solving large-scale linear systems with optimal or near-optimal efficiency.  Unlike geometric multigrid, AMG requires little or no geometric information about the underlying problem and develops a sequence of coarser grids directly from the input matrix.  This feature is especially important for problems discretized on unstructured meshes and irregular grids.
+
+# PyAMG Features
+
+PyAMG features implementations of:
+
+- **Ruge-Stuben (RS)** or *Classical AMG*
+- AMG based on **Smoothed Aggregation (SA)**
+
+and experimental support for:
+
+- **Adaptive Smoothed Aggregation (αSA)**
+- **Compatible Relaxation (CR)**
+
+The predominant portion of PyAMG is written in Python with a smaller amount of supporting C++ code for performance critical operations.
+
+# Example Usage
+
+PyAMG is easy to use!  The following code constructs a two-dimensional Poisson problem and solves the resulting linear system with Classical AMG.
+
+````python
+import pyamg
+import numpy as np
+A = pyamg.gallery.poisson((500,500), format='csr')  # 2D Poisson problem on 500x500 grid
+ml = pyamg.ruge_stuben_solver(A)                    # construct the multigrid hierarchy
+print(ml)                                           # print hierarchy information
+b = np.random.rand(A.shape[0])                      # pick a random right hand side
+x = ml.solve(b, tol=1e-10)                          # solve Ax=b to a tolerance of 1e-8
+print("residual: ", np.linalg.norm(b-A*x))          # compute norm of residual vector
+````
+
+Program output:
+
+<pre>
+multilevel_solver
+Number of Levels:     9
+Operator Complexity:  2.199
+Grid Complexity:      1.667
+Coarse Solver:        'pinv2'
+  level   unknowns     nonzeros
+    0       250000      1248000 [45.47%]
+    1       125000      1121002 [40.84%]
+    2        31252       280662 [10.23%]
+    3         7825        70657 [ 2.57%]
+    4         1937        17971 [ 0.65%]
+    5          483         4725 [ 0.17%]
+    6          124         1352 [ 0.05%]
+    7           29          293 [ 0.01%]
+    8            7           41 [ 0.00%]
+
+residual:  1.24748994988e-08
+</pre>
---
+++ packages/pyamg/pyamg-3.3.2.tar.gz/pyamg-3.3.2/setup.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+PyAMG: Algebraic Multigrid Solvers in Python
+
+PyAMG is a library of Algebraic Multigrid (AMG)
+solvers with a convenient Python interface.
+
+PyAMG features implementations of:
+
+- Ruge-Stuben (RS) or Classical AMG
+- AMG based on Smoothed Aggregation (SA)
+- Adaptive Smoothed Aggregation (αSA)
+- Compatible Relaxation (CR)
+- Krylov methods such as CG, GMRES, FGMRES, BiCGStab, MINRES, etc
+
+PyAMG is primarily written in Python with
+supporting C++ code for performance critical operations.
+"""
+
+import io
+import os
+import subprocess
+
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.test import test as TestCommand
+
+version = '3.3.2'
+isreleased = True
+
+install_requires = (
+    'numpy>=1.7.0',
+    'scipy>=0.12.0',
+    'pytest>=2'
+)
+
+
+# set the version information
+# https://github.com/numpy/numpy/commits/master/setup.py
+# Return the git revision as a string
+
+
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                               env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+        out = _minimal_ext_cmd(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
+        GIT_BRANCH = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = 'Unknown'
+        GIT_BRANCH = ''
+
+    return GIT_REVISION
+
+
+def set_version_info(VERSION, ISRELEASED):
+    if os.path.exists('.git'):
+        GIT_REVISION = git_version()
+    elif os.path.exists('pyamg/version.py'):
+        try:
+            import imp
+            version = imp.load_source("pyamg.version", "pyamg/version.py")
+            GIT_REVISION = version.git_revision
+        except ImportError:
+            raise ImportError('Unable to read version information.')
+    else:
+        GIT_REVISION = 'Unknown'
+        GIT_BRANCH = ''
+
+    FULLVERSION = VERSION
+    if not ISRELEASED:
+        FULLVERSION += '.dev0' + '+' + GIT_REVISION[:7]
+
+    print(GIT_REVISION)
+    print(FULLVERSION)
+    return FULLVERSION, GIT_REVISION
+
+
+def write_version_py(VERSION,
+                     FULLVERSION,
+                     GIT_REVISION,
+                     ISRELEASED,
+                     filename='pyamg/version.py'):
+    cnt = """
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '%(version)s'
+version = '%(version)s'
+full_version = '%(full_version)s'
+git_revision = '%(git_revision)s'
+release = %(isrelease)s
+if not release:
+    version = full_version
+"""
+
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': VERSION,
+                       'full_version': FULLVERSION,
+                       'git_revision': GIT_REVISION,
+                       'isrelease': str(ISRELEASED)})
+    finally:
+        a.close()
+
+
+fullversion, git_revision = set_version_info(version, isreleased)
+write_version_py(version, fullversion, git_revision, isreleased,
+                 filename='pyamg/version.py')
+
+
+# identify extension modules
+# since numpy is needed (for the path), need to bootstrap the setup
+# http://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
+class build_ext(_build_ext):
+    'to install numpy'
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        pytest.main(self.test_args)
+
+
+ext_modules = [Extension('pyamg.amg_core._amg_core',
+                         sources=['pyamg/amg_core/amg_core_wrap.cxx'],
+                         define_macros=[('__STDC_FORMAT_MACROS', 1)])]
+
+setup(
+    name='pyamg',
+    version=fullversion,
+    keywords=['algebraic multigrid AMG sparse matrix preconditioning'],
+    author='Nathan Bell, Luke OLson, and Jacob Schroder',
+    author_email='luke.olson@gmail.com',
+    maintainer='Luke Olson',
+    maintainer_email='luke.olson@gmail.com',
+    url='https://github.com/pyamg/pyamg',
+    download_url='https://github.com/pyamg/pyamg/releases',
+    license='MIT',
+    platforms=['Windows', 'Linux', 'Solaris', 'Mac OS-X', 'Unix'],
+    description=__doc__.split('\n')[0],
+    long_description=__doc__,
+    #
+    packages=find_packages(exclude=['doc']),
+    package_data={'pyamg': ['gallery/example_data/*.mat']},
+    include_package_data=False,
+    install_requires=install_requires,
+    zip_safe=False,
+    #
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': build_ext, 'test': PyTest},
+    setup_requires=['numpy'],
+    #
+    tests_require=['pytest'],
+    #
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: X11 Applications',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: C++',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
