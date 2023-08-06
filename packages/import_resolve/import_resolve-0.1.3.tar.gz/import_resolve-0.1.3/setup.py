0d76853809f9287946b3979971a7887a338dbb6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:52 +0000

    Add package import_resolve/0.1.3/import_resolve-0.1.3.tar.gz

---
+++ packages/import_resolve/import_resolve-0.1.3.tar.gz/import_resolve-0.1.3/setup.py
@@ -0,0 +1,52 @@
+"""
+Resolves pip imports in a python project
+"""
+from setuptools import setup
+
+dependencies = ['argparse']
+readme = ""
+with open("README.txt", "r") as f:
+    readme += f.read()
+setup(
+    name='import_resolve',
+    version='0.1.3',
+    url='https://github.com/PandaWhoCodes/import_resolve',
+    license='MIT',
+    author='Thomas Ashish Cherian',
+    author_email='ufoundashish@gmail.com',
+    description='Resolves pip imports in a python project',
+    long_description=readme,
+    packages=['import_resolve'],
+    include_package_data=True,
+    zip_safe=True,
+    platforms='any',
+    scripts=['import_resolve/check.py'],
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'import_resolve = import_resolve.check:argParser',
+        ],
+    },
+
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
