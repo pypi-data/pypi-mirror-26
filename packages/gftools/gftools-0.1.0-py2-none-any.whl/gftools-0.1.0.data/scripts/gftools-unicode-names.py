9b9a8f050fc5e327b94133b7727bbbd2c1c016c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:24 +0000

    Add package gftools/0.1.0/gftools-0.1.0-py2-none-any.whl

---
+++ packages/gftools/gftools-0.1.0-py2-none-any.whl/gftools-0.1.0.data/scripts/gftools-unicode-names.py
@@ -0,0 +1,43 @@
+#!python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 The Google Font Tools Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import unicodedata
+from google.apputils import app
+import gflags as flags
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string('nam_file', None, 'Location of nam file')
+
+
+def main(_):
+  with open(FLAGS.nam_file, 'r') as f:
+    for line in f:
+      print _ReformatLine(line)
+
+
+def _ReformatLine(line):
+  if line.startswith('0x'):
+    codepoint = int(line[2:6], 16)
+    out = unichr(codepoint) + ' ' + unicodedata.name(unichr(codepoint), '')
+    return '0x%04X  %s' % (codepoint, out)
+  else:
+    return line
+
+
+if __name__ == '__main__':
+  flags.MarkFlagAsRequired('nam_file')
+  app.run()
