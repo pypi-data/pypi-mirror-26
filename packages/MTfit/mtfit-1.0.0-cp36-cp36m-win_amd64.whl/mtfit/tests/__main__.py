883b383ef9dcf80627768c15ee651b0a8873abca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:41 +0000

    Add package MTfit/1.0.0/mtfit-1.0.0-cp36-cp36m-win_amd64.whl

---
+++ packages/MTfit/mtfit-1.0.0-cp36-cp36m-win_amd64.whl/mtfit/tests/__main__.py
@@ -0,0 +1,7 @@
+from mtfit.tests.unit import run_tests as run_unit_tests
+# from mtfit.tests.functional import run_tests as run_functional_tests
+
+if __name__ == "__main__":
+    results = run_unit_tests(1)
+    if not results.wasSuccessful():
+        raise ValueError('Tests failed')
