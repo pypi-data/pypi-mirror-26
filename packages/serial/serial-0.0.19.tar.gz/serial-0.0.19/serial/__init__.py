a6fde5d0f655dc1efb9b379f9014760c73bd82de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:55 +0000

    Add package serial/0.0.19/serial-0.0.19.tar.gz

---
+++ packages/serial/serial-0.0.19.tar.gz/serial-0.0.19/serial/__init__.py
@@ -0,0 +1,16 @@
+"""
+``serial`` is an object serialization/deserialization library intended to facilitate authoring of API models which are
+readable and introspective, and to expedite code and data validation and testing. ``serial`` supports JSON, YAML, and
+XML.
+"""
+# Backwards Compatibility ->
+from __future__ import nested_scopes, generators, division, absolute_import, with_statement,\
+print_function, unicode_literals
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
+# <- Backwards Compatibility
+
+from serial import errors, utilities, properties, meta, hooks, test, model
+
+
