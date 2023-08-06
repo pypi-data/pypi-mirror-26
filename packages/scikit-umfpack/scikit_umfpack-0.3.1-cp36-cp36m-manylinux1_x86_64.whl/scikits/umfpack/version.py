56b93dafa73f4c0c8c8d1b02fcbe7cc0958c7d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:56 +0000

    Add package scikit-umfpack/0.3.1/scikit_umfpack-0.3.1-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/scikit-umfpack/scikit_umfpack-0.3.1-cp36-cp36m-manylinux1_x86_64.whl/scikits/umfpack/version.py
@@ -0,0 +1,9 @@
+# THIS FILE IS GENERATED FROM scikit-umfpack SETUP.PY
+short_version = '0.3.1'
+version = '0.3.1'
+full_version = '0.3.1.dev0+'
+git_revision = ''
+release = False
+
+if not release:
+    version = full_version
