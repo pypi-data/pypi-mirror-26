cf4b5283da4ce5737b0e17a761c767afce3cc492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:37 +0000

    Add package ocrmypdf/5.4.2/ocrmypdf-5.4.2.tar.gz

---
+++ packages/ocrmypdf/ocrmypdf-5.4.2.tar.gz/ocrmypdf-5.4.2/ocrmypdf/__init__.py
@@ -0,0 +1,9 @@
+# © 2017 James R. Barlow: github.com/jbarlow83
+
+import pkg_resources
+
+PROGRAM_NAME = 'ocrmypdf'
+
+VERSION = pkg_resources.get_distribution('ocrmypdf').version
+
+
---
+++ packages/ocrmypdf/ocrmypdf-5.4.2.tar.gz/ocrmypdf-5.4.2/tests/spoof/qpdf_dummy_return2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# © 2016 James R. Barlow: github.com/jbarlow83
+
+import sys
+
+
+def main():
+    print('qpdf dummy')
+    sys.exit(2)
+
+
+if __name__ == '__main__':
+    main()
