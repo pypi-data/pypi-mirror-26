6f88ab05cfa846f2b12fe93f879cbc6490e6afa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:19 +0000

    Add package futoin-cid/0.7.99995/futoin-cid-0.7.99995.tar.gz

---
+++ packages/futoin-cid/futoin-cid-0.7.99995.tar.gz/futoin-cid-0.7.99995/futoin/cid/tool/maventool.py
@@ -0,0 +1,53 @@
+
+from ..buildtool import BuildTool
+from ..testtool import TestTool
+from .sdkmantoolmixin import SdkmanToolMixIn
+
+
+class mavenTool(SdkmanToolMixIn, BuildTool, TestTool):
+    """Apache Maven is a software project management and comprehension tool.
+
+Home: https://maven.apache.org/
+
+Expects clean, compile, package and test targets.
+
+Build targets:
+    prepare -> clean
+    build -> compile
+    package -> package
+    check -> test
+Override targets with .config.toolTune.
+
+Requires Java >= 7.
+"""
+    __slots__ = ()
+
+    def _minJava(self):
+        return '7'
+
+    def autoDetectFiles(self):
+        return 'pom.xml'
+
+    def _binName(self):
+        return 'mvn'
+
+    def _callMaven(self, config, target):
+        cmd = [config['env']['mavenBin'], target]
+        self._executil.callMeaningful(cmd)
+
+    def onPrepare(self, config):
+        target = self._getTune(config, 'prepare', 'clean')
+        self._callMaven(config, target)
+
+    def onBuild(self, config):
+        target = self._getTune(config, 'build', 'compile')
+        self._callMaven(config, target)
+
+    def onPackage(self, config):
+        target = self._getTune(config, 'package', 'package')
+        self._callMaven(config, target)
+        self._pathutil.addPackageFiles(config, 'target/*.jar')
+
+    def onCheck(self, config):
+        target = self._getTune(config, 'check', 'test')
+        self._callMaven(config, target)
