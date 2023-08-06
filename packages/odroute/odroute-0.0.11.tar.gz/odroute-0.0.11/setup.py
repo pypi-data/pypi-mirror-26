3c8056252873e4d897033b1c4117d955ec165c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:48 +0000

    Add package odroute/0.0.11/odroute-0.0.11.tar.gz

---
+++ packages/odroute/odroute-0.0.11.tar.gz/odroute-0.0.11/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+INSTALL_REQUIREMENTS = [
+    'Click>=6.0,<7.0',
+    'pyzmq>=16.0,<17.0',
+    'tornado>=4.0,<5.0',
+]
+
+setup(
+    author='Jonas Ohrstrom',
+    author_email='ohrstrom@gmail.com',
+    url='https://github.com/digris/odr-stream-router',
+    name='odroute',
+    version='0.0.11',
+    description='A primitive tool to route streams from ord-dabmod',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIREMENTS,
+    entry_points='''
+        [console_scripts]
+        odroute=odroute:cli
+    ''',
+)
