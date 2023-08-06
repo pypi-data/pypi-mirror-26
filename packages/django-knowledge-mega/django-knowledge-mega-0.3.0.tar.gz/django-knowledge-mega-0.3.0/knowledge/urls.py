ca9d16dd2889c1e26d883d904817fe9d575dde4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:37 +0000

    Add package django-knowledge-mega/0.3.0/django-knowledge-mega-0.3.0.tar.gz

---
+++ packages/django-knowledge-mega/django-knowledge-mega-0.3.0.tar.gz/django-knowledge-mega-0.3.0/knowledge/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+
+from knowledge import views
+
+urlpatterns = [
+    url(r'^$', views.knowledge_index, name='knowledge_index'),
+
+    url(r'^questions/$', views.knowledge_list, name='knowledge_list'),
+
+    url(r'^questions/(?P<question_id>\d+)/$', views.knowledge_thread, name='knowledge_thread_no_slug'),
+
+    url(r'^questions/(?P<category_slug>[a-z0-9-_]+)/$', views.knowledge_list, name='knowledge_list_category'),
+
+    url(r'^questions/(?P<question_id>\d+)/(?P<slug>[a-z0-9-_]+)/$', views.knowledge_thread, name='knowledge_thread'),
+
+    url(r'^moderate/(?P<model>[a-z]+)/(?P<lookup_id>\d+)/(?P<mod>[a-zA-Z0-9_]+)/$', views.knowledge_moderate, name='knowledge_moderate'),
+
+    url(r'^ask/$', views.knowledge_ask, name='knowledge_ask'),
+]
