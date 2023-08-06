2ebdb0dc636d01497f77abf44b29157d2b6640e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:30 +0000

    Add package namesgenerator/0.1/namesgenerator-0.1.tar.gz

---
+++ packages/namesgenerator/namesgenerator-0.1.tar.gz/namesgenerator-0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='namesgenerator',
+    version='0.1',
+    scripts=['namesgenerator'],
+    url='https://github.com/jznf/namesgenerator',
+    maintainer='jznf',
+    maintainer_email='bofh@zvsx.net',
+)
