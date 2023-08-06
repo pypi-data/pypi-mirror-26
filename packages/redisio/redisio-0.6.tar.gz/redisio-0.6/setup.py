b6835da3b7609703bbc156c3c2d6fd7c49b9ed8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:54 +0000

    Add package redisio/0.6/redisio-0.6.tar.gz

---
+++ packages/redisio/redisio-0.6.tar.gz/redisio-0.6/setup.py
@@ -0,0 +1,30 @@
+import commands
+from distutils.core import setup
+
+from redisio import __version__
+
+me = 'Roy'
+email = 'cf020031308@163.com'
+
+setup(
+    name='redisio',
+    version=__version__,
+    author=me,
+    author_email=email,
+    maintainer=me,
+    maintainer_email=email,
+    packages=['redisio'],
+    url='https://github.com/cf020031308/redisio',
+    keywords=['Redis', 'key-value store'],
+    license='MIT',
+    description='A tiny and fast redis client for script boys.',
+    long_description=commands.getoutput('mdv -A README.md'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Database'
+    ]
+)
