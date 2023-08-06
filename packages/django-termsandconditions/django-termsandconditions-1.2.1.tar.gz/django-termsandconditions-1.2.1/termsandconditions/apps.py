4e7d010b8cdd1a76cdafe7d344bd8920e798908b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:33 +0000

    Add package django-termsandconditions/1.2.1/django-termsandconditions-1.2.1.tar.gz

---
+++ packages/django-termsandconditions/django-termsandconditions-1.2.1.tar.gz/django-termsandconditions-1.2.1/termsandconditions/apps.py
@@ -0,0 +1,9 @@
+"""Django Apps Config"""
+
+from django.apps import AppConfig
+
+
+class TermsAndConditionsConfig(AppConfig):
+    """App config for TermsandConditions"""
+    name = 'termsandconditions'
+    verbose_name = "Terms and Conditions"
