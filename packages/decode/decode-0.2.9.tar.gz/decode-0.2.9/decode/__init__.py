60d416b5a0a8290009f59ab8cd14ce6a63b829d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:43 +0000

    Add package decode/0.2.9/decode-0.2.9.tar.gz

---
+++ packages/decode/decode-0.2.9.tar.gz/decode-0.2.9/decode/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+from . import utils
+from .core import *
+from . import io
+from . import plot
+from .logging import *
+del core
+del logging
---
+++ packages/decode/decode-0.2.9.tar.gz/decode-0.2.9/setup.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+"DESHIMA Code for data analysis"
+
+# standard library
+from setuptools import setup
+
+# module constants
+INSTALL_REQUIRES = [
+    'astropy',
+    'numpy',
+    'scipy',
+    'pyyaml',
+    'xarray',
+    'matplotlib',
+]
+
+PACKAGES = [
+    'decode',
+    'decode.core',
+    'decode.core.array',
+    'decode.core.cube',
+    'decode.io',
+    'decode.logging',
+    'decode.plot',
+    'decode.utils',
+    'decode.utils.misc',
+]
+
+
+# main
+setup(
+    name = 'decode',
+    description = __doc__,
+    version = '0.2.9',
+    author = 'snoopython',
+    author_email = 'taniguchi@ioa.s.u-tokyo.ac.jp',
+    url = 'https://github.com/deshima-dev/decode',
+    # install_requires = INSTALL_REQUIRES,
+    packages = PACKAGES,
+)
