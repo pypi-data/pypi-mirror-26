dec90610dfe58e2d085704771180f5b5a3802178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:37 +0000

    Add package nesterxia/1.0.0/nesterxia-1.0.0.tar.gz

---
+++ packages/nesterxia/nesterxia-1.0.0.tar.gz/nesterxia-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name  ='nesterxia',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='daniel',
+    author_email='fda@fdsa.com',
+    url='http://www.dsfk.com',
+    description='a simple printer of nested lists',
+    )
