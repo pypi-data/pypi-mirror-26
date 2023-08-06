da295a04c98f82c2c4631ee9ab119711ea7f0554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:23 +0000

    Add package test-page/0.1/test-page-0.1.tar.gz

---
+++ packages/test-page/test-page-0.1.tar.gz/test-page-0.1/setup.py
@@ -0,0 +1,17 @@
+from __future__ import print_function  
+from setuptools import setup, find_packages  
+import sys  
+  
+setup(  
+    name="test-page",
+    version="0.1",
+    author="wjolin",
+    author_email="792994035@qq.com",
+    description="this just a test for python pypi .",
+    long_description=open("README").read(),
+    license="MIT",
+    url="",
+    packages=["tidypage"],
+    platforms=["any"],
+    classifiers=[]
+)
---
+++ packages/test-page/test-page-0.1.tar.gz/test-page-0.1/tidypage/test.py
@@ -0,0 +1,3 @@
+import os
+
+print("this is a test for pypi, print current path : %s"%os.getcwd())
