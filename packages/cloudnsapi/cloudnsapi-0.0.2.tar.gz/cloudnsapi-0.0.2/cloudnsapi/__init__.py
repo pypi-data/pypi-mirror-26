4813213a66c08e57ebc57642999576b9dd28e0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:08 +0000

    Add package cloudnsapi/0.0.2/cloudnsapi-0.0.2.tar.gz

---
+++ packages/cloudnsapi/cloudnsapi-0.0.2.tar.gz/cloudnsapi-0.0.2/cloudnsapi/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+cloudns API library
+~~~~~~~~~~~~~~~~~~~
+
+This is a library that allows simple access to the ClouDNS HTTP API.
+
+:copyright: (c) 2016, Richard Franks
+:license: MIT, see LICENSE for more information
+
+"""
+
+__title__ = 'cloudns'
+__version__ = '0.2a'
+__author__ = 'Richard Franks'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Richard Franks'
+
+from .api import Api
+api = Api
