5a589a25fea1003dd3024b23dd5b73c108eb9abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:35 +0000

    Add package kqueen/0.8/kqueen-0.8.tar.gz

---
+++ packages/kqueen/kqueen-0.8.tar.gz/kqueen-0.8/kqueen/server.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_jwt import JWT
+from kqueen.auth import authenticate, identity
+from kqueen.blueprints.api.views import api
+from kqueen.serializers import KqueenJSONEncoder
+from werkzeug.contrib.cache import SimpleCache
+
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+cache = SimpleCache()
+
+config_file = os.environ.get('KQUEEN_CONFIG_FILE', 'config/dev.py')
+
+
+def create_app(config_file=config_file):
+    app = Flask(__name__, static_folder='./asset/static')
+    app.json_encoder = KqueenJSONEncoder
+
+    app.register_blueprint(api, url_prefix='/api/v1')
+
+    # load configuration
+    if app.config.from_pyfile(config_file):
+        logger.info('Loading configuration from {}'.format(config_file))
+    else:
+        raise Exception('Config file {} could not be loaded.'.format(config_file))
+
+    return app
+
+
+app = create_app()
+app.logger.setLevel(logging.INFO)
+jwt = JWT(app, authenticate, identity)
+
+
+def run():
+    logger.debug('kqueen starting')
+    app.run()
