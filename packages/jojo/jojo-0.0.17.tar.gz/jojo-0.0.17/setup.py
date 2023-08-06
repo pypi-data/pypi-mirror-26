1751e3fce20d3811dfb96371a204103c46bfff6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:38 +0000

    Add package jojo/0.0.17/jojo-0.0.17.tar.gz

---
+++ packages/jojo/jojo-0.0.17.tar.gz/jojo-0.0.17/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README') as f:
+    readme = f.read()
+
+setup(
+    name='jojo',
+    version='0.0.17',
+    description="jojo's programming adventure in python",
+    url='http://github.com/xieyuheng/jojo-on-python',
+
+    author='xieyuheng',
+    author_email='xyheme@gmail.com',
+
+
+    packages=['jojo'],
+    package_data={'jojo': ['*.jo']},
+    scripts=['bin/jojo'],
+
+    long_description=readme,
+    license="I dedicate all my works here to all human beings.",
+)
