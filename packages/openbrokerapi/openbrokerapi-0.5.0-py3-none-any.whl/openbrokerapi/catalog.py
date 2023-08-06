de34e0d3cb2eadfe10b7892e18eb82ccbda31fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:04 +0000

    Add package openbrokerapi/0.5.0/openbrokerapi-0.5.0-py3-none-any.whl

---
+++ packages/openbrokerapi/openbrokerapi-0.5.0-py3-none-any.whl/openbrokerapi/catalog.py
@@ -0,0 +1,102 @@
+from typing import List, Dict
+
+
+class ServiceMetadata:
+    def __init__(self,
+                 displayName: str,
+                 imageUrl: str,
+                 longDescription: str,
+                 providerDisplayName: str,
+                 documentationUrl: str,
+                 supportUrl: str):
+        self.displayName = displayName
+        self.imageUrl = imageUrl
+        self.longDescription = longDescription
+        self.providerDisplayName = providerDisplayName
+        self.documentationUrl = documentationUrl
+        self.supportUrl = supportUrl
+
+
+class ServiceDashboardClient:
+    def __init__(self,
+                 redirect_uri: str,
+                 id: str = None,
+                 secret: str = None
+                 ):
+        self.id = id
+        self.secret = secret
+        self.redirect_uri = redirect_uri
+
+
+class ServicePlanCost:
+    def __init__(self,
+                 amount: Dict[str, int],
+                 unit: str):
+        self.amount = amount
+        self.unit = unit
+
+
+class ServicePlanMetaData:
+    def __init__(self,
+                 displayName: str = None,
+                 bullets: List[str] = None,
+                 costs: List[ServicePlanCost] = None
+                 ):
+        self.displayName = displayName
+        self.bullets = bullets
+        self.costs = costs
+
+
+class Schemas:
+    def __init__(self,
+                 service_instance: Dict = None,
+                 service_binding: Dict = None):
+        self.service_instance = service_instance
+        self.service_binding = service_binding
+
+
+class ServicePlan:
+    def __init__(self,
+                 id: str,
+                 name: str,
+                 description: str,
+                 metadata: ServicePlanMetaData = None,
+                 free: bool = None,
+                 bindable: bool = None,
+                 schemas: Schemas = None
+                 ):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.metadata = metadata
+        self.free = free
+        self.bindable = bindable
+        self.schemas = schemas
+
+
+class Service:
+    def __init__(self,
+                 id: str,
+                 name: str,
+                 description: str,
+                 bindable: bool,
+                 plans: List[ServicePlan],
+                 tags: List[str] = None,
+                 requires: List[str] = None,
+                 metadata: ServiceMetadata = None,
+                 dashboard_client: ServiceDashboardClient = None,
+                 plan_updateable: bool = False
+                 ):
+        """
+        :param requires:  syslog_drain, route_forwarding or volume_mount
+        """
+        self.id = id
+        self.name = name
+        self.description = description
+        self.bindable = bindable
+        self.plans = plans
+        self.tags = tags
+        self.requires = requires
+        self.metadata = metadata
+        self.dashboard_client = dashboard_client
+        self.plan_updateable = plan_updateable
---
+++ packages/openbrokerapi/openbrokerapi-0.5.0-py3-none-any.whl/openbrokerapi/response.py
@@ -0,0 +1,61 @@
+from typing import List
+
+from openbrokerapi.catalog import Service
+from openbrokerapi.service_broker import OperationState, VolumeMount
+
+
+class EmptyResponse:
+    pass
+
+
+class ErrorResponse:
+    def __init__(self,
+                 error: str = None,
+                 description: str = None):
+        self.error = error
+        self.description = description
+
+
+class CatalogResponse:
+    def __init__(self, services: List[Service]):
+        self.services = services
+
+
+class ProvisioningResponse:
+    def __init__(self,
+                 dashboard_url: str,
+                 operation: str):
+        self.dashboard_url = dashboard_url
+        self.operation = operation
+
+
+class BindResponse:
+    def __init__(self,
+                 credentials: dict = None,
+                 syslog_drain_url: str = None,
+                 route_service_url: str = None,
+                 volume_mounts: List[VolumeMount] = None
+                 ):
+        self.credentials = credentials
+        self.syslog_drain_url = syslog_drain_url
+        self.route_service_url = route_service_url
+        self.volume_mounts = volume_mounts
+
+
+class UpdateResponse:
+    def __init__(self, operation: str):
+        self.operation = operation
+
+
+class DeprovisionResponse:
+    def __init__(self, operation: str):
+        self.operation = operation
+
+
+class LastOperationResponse:
+    def __init__(self,
+                 state: OperationState,
+                 description: str
+                 ):
+        self.state = state.value
+        self.description = description
