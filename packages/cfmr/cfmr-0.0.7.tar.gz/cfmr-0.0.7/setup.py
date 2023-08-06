3fa51b54c10374eeb1e011b698c25e510564229a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:52 +0000

    Add package cfmr/0.0.7/cfmr-0.0.7.tar.gz

---
+++ packages/cfmr/cfmr-0.0.7.tar.gz/cfmr-0.0.7/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cfmr',
+  packages = ['cfmr'], 
+  version = '0.0.7',
+  description = 'Python support library for CFMR',
+  author = 'Donnie Flood',
+  author_email = 'pypi@floodfx.com',
+  url = 'https://github.com/floodfx/cfmr-python',
+  download_url = 'https://github.com/floodfx/cfmr-python/archive/0.0.7.tar.gz',
+  keywords = ['map', 'reduce', 'cloud', 'function', 'aws', 'lambda', 'azure', 'google', 'cloud'], 
+  classifiers = [],
+)
