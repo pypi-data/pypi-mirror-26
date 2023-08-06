1324567dce27f2b41a13fb4cdcc042509b448351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:05 +0000

    Add package explay-testr/0.3.2/explay_testr-0.3.2.tar.gz

---
+++ packages/explay-testr/explay_testr-0.3.2.tar.gz/explay_testr-0.3.2/setup.py
@@ -0,0 +1,26 @@
+#  from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name = 'explay_testr',
+    packages = ['explay'],
+    scripts = ['bin/exp'],
+    version = '0.3.2',
+    description = 'make excel jobs playful again',
+    author = 'zealzel',
+    author_email = 'zealzel@gmail.com',
+    url = 'https://github.com/zealzel/explay',
+    download_url = 'https://github.com/zealzel/explay/tarball/v0.3.2',
+    #  keywords = ['pandas', 'openpyxl', 'fire'],
+    #  classifiers = [],
+    install_requires=[
+        'pandas',
+        'openpyxl',
+        'regex',
+        'fire',
+        'XlsxWriter',
+        'numpy',
+        'xlrd',
+        'pyyaml',
+    ]
+)
