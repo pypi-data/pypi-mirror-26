1a75bd54e4e51ba14542f490c2d6f34a70471c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:34 +0000

    Add package arivo.om/0.1.8/arivo.om-0.1.8-py2-none-any.whl

---
+++ packages/arivo.om/arivo.om-0.1.8-py2-none-any.whl/om/message/__init__.py
@@ -0,0 +1,7 @@
+# noinspection PyUnresolvedReferences
+from om.message.message import Message, Topic
+
+__version__ = '0.1.8'
+__git_branch__ = 'master'
+__git_commit__ = '2626614426d64dcaf5cd7afd10867f4640d00e49'
+__git_commit_short__ = '26266144'
---
+++ packages/arivo.om/arivo.om-0.1.8-py2-none-any.whl/om/utils/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1.8'
+__git_branch__ = 'master'
+__git_commit__ = '3bf305b1d730885351e25eca7815eb72db04dd69'
+__git_commit_short__ = '3bf305b1'
