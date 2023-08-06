eb5f3539b63af7f680387e8361d90d80d8019f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:14 +0000

    Add package dopplerr/0.4.9/dopplerr-0.4.9-py2.py3-none-any.whl

---
+++ packages/dopplerr/dopplerr-0.4.9-py2.py3-none-any.whl/dopplerr/main.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import logging
+import os
+import sys
+from pathlib import Path
+
+from babelfish import Language
+
+from dopplerr import DOPPLERR_VERSION
+from dopplerr.config import DopplerrConfig
+from dopplerr.db import DopplerrDb
+from dopplerr.downloader import DopplerrDownloader
+from dopplerr.logging import OutputType
+from dopplerr.logging import setup_logging
+from dopplerr.routes import listen
+from dopplerr.status import DopplerrStatus
+
+# from dopplerr.logging import setup_logger
+
+log = logging.getLogger(__name__)
+
+
+def print_list(alist):
+    return ",".join(alist)
+
+
+def list_of_languages(lang_list):
+    langs = [s.lower() for s in lang_list.split(',')]
+    failed = False
+    for l in langs:
+        try:
+            Language(l)
+        except ValueError:
+            failed = True
+            logging.critical("Invalid language: %r", l)
+    if failed:
+        sys.exit(2)
+    return langs
+
+
+def main():
+    if "--debug-config" in sys.argv:
+        default_level = logging.DEBUG
+    else:
+        default_level = logging.ERROR
+    debug = default_level is logging.DEBUG
+    setup_logging(debug=debug)
+    log.debug("Initializing Dopplerr version %s...", DOPPLERR_VERSION)
+
+    DopplerrConfig().find_configuration_values()
+    debug = DopplerrConfig().get_cfg_value("general.verbose")
+    output_type = DopplerrConfig().get_cfg_value("general.output_type")
+    if output_type == 'dev':
+        outputtype = OutputType.DEV
+    elif output_type == 'plain':
+        outputtype = OutputType.PLAIN
+    else:
+        raise NotImplementedError("Invalid output type: {!r}".format(output_type))
+
+    log.debug("Applying configuration")
+    setup_logging(
+        outputtype=outputtype,
+        debug=debug,
+        logfile=DopplerrConfig().get_cfg_value("general.logfile"))
+    log.info("Logging is set to %s", "verbose"
+             if DopplerrConfig().get_cfg_value("general.verbose") else "not verbose")
+    DopplerrStatus().refresh_from_cfg()
+
+    log.info("Initializing Subtitle DopplerrDownloader Service")
+
+    DopplerrDownloader().initialize_subliminal()
+    DopplerrStatus().sqlite_db_path = (
+        Path(DopplerrConfig().get_cfg_value("general.configdir")) / "sqlite.db")
+    log.debug("SQLite DB: %s", DopplerrStatus().sqlite_db_path.as_posix())
+    DopplerrDb().init(DopplerrStatus().sqlite_db_path)
+    DopplerrDb().create_tables()
+
+    # change current work dir for subliminal work files
+    os.chdir(DopplerrConfig().get_cfg_value("general.configdir"))
+
+    DopplerrDb().insert_event("start", "dopplerr started")
+
+    # main event loop (Asyncio behind)
+    listen()
+
+    logging.info("Clean stopping")
+    DopplerrDb().insert_event("stop", "dopplerr stopped")
---
+++ packages/dopplerr/dopplerr-0.4.9-py2.py3-none-any.whl/dopplerr/status.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import logging
+
+from dopplerr.config import DopplerrConfig
+from dopplerr.singleton import singleton
+
+log = logging.getLogger(__name__)
+
+
+@singleton
+class DopplerrStatus(object):
+    """
+    Contains current status of the application and derived values from DopplerrConfig
+    """
+
+    def __init__(self):
+        self.healthy = False
+        self.sqlite_db_path = None
+        self.subliminal_provider_configs = None
+
+    def refresh_from_cfg(self):
+        """
+        Refresh derived values from cfg
+        """
+        cfg = DopplerrConfig()
+        if not cfg.get_cfg_value("general.port"):
+            log.fatal("No port defined !")
+            raise Exception("No port defined")
+        if not cfg.get_cfg_value("general.frontenddir"):
+            log.fatal("No frontend dir defined")
+            raise Exception("No frontend dir defined")
+        self.subliminal_provider_configs = self._build_subliminal_provider_cfgs()
+
+        languages = cfg.get_cfg_value("subliminal.languages")
+        if not languages:
+            raise Exception("No languages defined")
+        if any(not x for x in languages):
+            raise Exception("Bad languages: {!r}".format(languages))
+
+    def _build_subliminal_provider_cfgs(self):
+        cfg = DopplerrConfig()
+        provider_configs = {}
+        provider_names = [
+            "addic7ed",
+            "legendastv",
+            "opensubtitles",
+            "subscenter",
+        ]
+        for provider_name in provider_names:
+            if cfg.get_cfg_value("subliminal.{}.enabled".format(provider_name)):
+                provider_configs[provider_name] = {
+                    'username': cfg.get_cfg_value("subliminal.{}.user".format(provider_name)),
+                    'password': cfg.get_cfg_value("subliminal.{}.password".format(provider_name)),
+                }
+                log.debug("Using %s username: %s", provider_name,
+                          provider_configs[provider_name]['username'])
+        return provider_configs
