2b1bd099e596169525035c111a8874373b45d7b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:21 +0000

    Add package nester-demo/1.1.1/nester-demo-1.1.1.tar.gz

---
+++ packages/nester-demo/nester-demo-1.1.1.tar.gz/nester-demo-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name          = 'nester-demo',
+    version       = '1.1.1',
+    py_modules    = ['nester'],
+    author        = 'hfpython',
+    author_email  = 'test@headfirstlabs.com',
+    url           = 'http://wwww.test.com',
+    description   = 'a simple test',
+    )
