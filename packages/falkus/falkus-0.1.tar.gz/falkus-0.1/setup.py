22c2251d3e3081c2e9ac7a73b0890bc46160167e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:35 +0000

    Add package falkus/0.1/falkus-0.1.tar.gz

---
+++ packages/falkus/falkus-0.1.tar.gz/falkus-0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+# sudo python setup.py install
+
+setup(
+    name='falkus',
+    version='0.1',
+    description='Automation for quick setup of AWS SAM application with Python Lambda',
+    url='https://gitlab.com/daniele.rigato.amz/falkus',
+    author='Daniele Rigato',
+    author_email='daniele.rigato@gmail.com',
+    license='GNU GPL v3',
+    classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 3 - Alpha',
+
+            # Indicate who your project is intended for
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+
+            # Specify the Python versions you support here. In particular, ensure
+            # that you indicate whether you support Python 2, Python 3 or both.
+            'Programming Language :: Python :: 2.7',
+    ],
+    keywords='aws lambda SAM serverless',
+    scripts=['falkus'],
+    packages=['falkuslib', 'falkustest'],
+    install_requires=['jsonschema', 'requests']
+)
