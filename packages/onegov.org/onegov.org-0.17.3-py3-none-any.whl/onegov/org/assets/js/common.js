4589d9cafcb7729ddba52393a61a5558acee86c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:54 +0000

    Add package onegov.org/0.17.3/onegov.org-0.17.3-py3-none-any.whl

---
+++ packages/onegov.org/onegov.org-0.17.3-py3-none-any.whl/onegov/org/assets/js/common.js
@@ -0,0 +1,108 @@
+// intercooler has the abilty to redirect depending on response headers
+// we'd like for it to do the same with our own 'redirect-after' attribute
+var setupRedirectAfter = function(elements) {
+    elements.on('success.ic', function(_event, el) {
+        var redirect_after = $(el).attr('redirect-after');
+        if (!_.isUndefined(redirect_after)) {
+            window.location = redirect_after;
+        }
+
+        return true;
+    });
+};
+
+setupRedirectAfter($('a'));
+
+// show the new content placeholder when hovering over the add content dropdown
+$('.show-new-content-placeholder')
+    .on('mouseenter', function() {
+        var placeholder = $('<li>')
+            .text($(this).text())
+            .addClass('new-content-placeholder');
+
+        $('.children').append(placeholder);
+        placeholder.show();
+    })
+    .on('mouseleave', function() {
+        $('.new-content-placeholder').remove();
+    });
+
+// initialize all foundation functions
+$(document).foundation();
+
+// get the footer height and write it to the footer_height setting if possible
+$(document).find('#footer_height').val($('footer > div').height() + 'px');
+
+// Make sure files open in another window
+$('.page-text a[href*="/datei/"]').attr('target', '_blank');
+
+// Turn video links into clickable thumbnails
+$('.page-text a.has-video').videoframe();
+
+// Disable scroll on elements which wish it disabled
+$('.disable-scroll').on('mouseover', function() {
+    var el = $(this);
+    var height = el.height();
+    var scrollHeight = el.get(0).scrollHeight;
+
+    $(this).on('mousewheel', function(event) {
+        var block = this.scrollTop === scrollHeight - height && event.deltaY < 0 || this.scrollTop === 0 && event.deltaY > 0;
+        return !block;
+    });
+});
+
+$('.disable-scroll').on('mouseout', function() {
+    $(this).off('mousewheel');
+});
+
+// Toggle the selected state in image selection views when clicking the checkbox
+$('.image-select input[type="checkbox"]').on('click', function(e) {
+    var target = $(e.target);
+    var checked = target.is(':checked');
+
+    target.closest('.image-box').toggleClass('selected', checked);
+});
+
+// A generic error messages handler
+$(document).on('show-alert', function(_, data) {
+    var alert = $('<div />')
+        .attr('data-alert', '')
+        .attr('class', 'alert-box ' + data.type)
+        .text(data.message)
+        .append($('<a href="#" class="close">&times;</a>'));
+
+    var target = $(data.target || '#alert-boxes');
+    target.append(alert);
+
+    $(document).foundation();
+});
+
+$('button[data-toggle]').toggleButton();
+
+// show the slider once everything has loaded
+$(document).ready(function() {
+    $('.slider').css('opacity', 1);
+});
+
+// support some extraordinary styling
+$(document).ready(function() {
+    $('.requires-children').each(function() {
+        var el = $(this);
+
+        if ($(el.data('required-unless')).length === 0) {
+            var children = el.find(el.data('required-children'));
+            var required = parseInt(el.data('required-count'), 10);
+
+            if (children.length < required) {
+                el.hide();
+            }
+        }
+    });
+});
+
+// send clicks from certain blocks down to the first link
+$(document).ready(function() {
+    $('.click-through').click(function() {
+        window.location = $(this).find('a:first').attr('href');
+    });
+});
