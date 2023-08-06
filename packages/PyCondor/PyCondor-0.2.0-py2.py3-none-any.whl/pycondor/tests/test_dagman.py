2c09abaf9295409d20bae4161bb7036d1048efd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:56 +0000

    Add package PyCondor/0.2.0/PyCondor-0.2.0-py2.py3-none-any.whl

---
+++ packages/PyCondor/PyCondor-0.2.0-py2.py3-none-any.whl/pycondor/tests/test_dagman.py
@@ -0,0 +1,194 @@
+
+import os
+from collections import Counter
+import filecmp
+import pytest
+from pycondor import Job, Dagman
+from pycondor.dagman import (_iter_job_args, _get_subdag_string,
+                             _get_job_arg_lines)
+from pycondor.utils import clear_pycondor_environment_variables
+
+clear_pycondor_environment_variables()
+
+example_script = os.path.join('examples/savelist.py')
+
+
+def test_add_job_int_fail():
+    with pytest.raises(TypeError) as excinfo:
+        dag = Dagman('dagname')
+        dag.add_job(50)
+    error = 'Expecting a Job or Dagman. ' + \
+            'Got an object of type {}'.format(type(50))
+    assert error == str(excinfo.value)
+
+
+def test_job_dag_submit_file_same(tmpdir):
+    # Test to check that the submit file for a Job with no arguments is the
+    # same whether built from a Dagman or not. See issue #38.
+
+    example_script = os.path.join('examples/savelist.py')
+    submit_dir = str(tmpdir.mkdir('submit'))
+    # Build Job object that will be built outside of a Dagman
+    job_outside_dag = Job('test_job', example_script, submit=submit_dir,
+                          queue=5)
+    job_outside_dag.build(fancyname=False)
+
+    # Build Job object that will be built inside of a Dagman
+    job_inside_dag = Job('test_job', example_script, submit=submit_dir,
+                         queue=5)
+    dagman = Dagman('exampledagman', submit=submit_dir)
+    dagman.add_job(job_inside_dag)
+    dagman.build(fancyname=False)
+
+    # Check that the contents of the two Job submit files are the same
+    assert filecmp.cmp(job_outside_dag.submit_file, job_inside_dag.submit_file,
+                       shallow=False)
+
+
+def test_job_arg_name_files(tmpdir):
+    # Test to check that when a named argument is added to a Job, and the Job
+    # is built with fancyname=True, the Job submit file and the
+    # error/log/output files for the argument start with the same index.
+    # E.g. job_(date)_01.submit, job_(date)_01.error, etc.
+    # Regression test for issue #47
+    example_script = os.path.join('examples/savelist.py')
+    submit_dir = str(tmpdir.mkdir('submit'))
+
+    for fancyname in [True, False]:
+        job = Job('testjob', example_script, submit=submit_dir)
+        job.add_arg('arg', name='argname')
+        dagman = Dagman('testdagman', submit=submit_dir)
+        dagman.add_job(job)
+        dagman.build(fancyname=fancyname)
+
+        with open(dagman.submit_file, 'r') as dagman_submit_file:
+            dagman_submit_lines = dagman_submit_file.readlines()
+
+        # Get root of the dagman submit file (submit file basename w/o .submit)
+        submit_file_line = dagman_submit_lines[0]
+        submit_file_basename = submit_file_line.split('/')[-1].rstrip()
+        submit_file_root = os.path.splitext(submit_file_basename)[0]
+        # Get job_name variable (used to built error/log/output file basenames)
+        jobname_line = dagman_submit_lines[2]
+        jobname = jobname_line.split('"')[-2]
+        other_file_root = '_'.join(jobname.split('_')[:-1])
+
+        assert submit_file_root == other_file_root
+
+
+def test_get_subdag_string_fail():
+    with pytest.raises(TypeError) as excinfo:
+        not_dagman = 'thisisastring'
+        _get_subdag_string(not_dagman)
+    error = 'Expecting a Dagman object, got {}'.format(type(not_dagman))
+    assert error == str(excinfo.value)
+
+
+def test_iter_job_args(tmpdir):
+    # Check node names yielded by _iter_job_args
+    example_script = os.path.join('examples/savelist.py')
+    submit_dir = str(tmpdir.mkdir('submit'))
+
+    job = Job('testjob', example_script, submit=submit_dir)
+    job.add_arg('argument1', name='arg1')
+    job.add_arg('argument2')
+    job.build()
+    for idx, (node_name, jobarg) in enumerate(_iter_job_args(job)):
+        if jobarg.name is not None:
+            assert node_name == '{}_{}'.format(job.submit_name, jobarg.name)
+        else:
+            assert node_name == '{}_arg_{}'.format(job.submit_name, idx)
+
+
+def test_iter_job_args_fail(tmpdir):
+    example_script = os.path.join('examples/savelist.py')
+    submit_dir = str(tmpdir.mkdir('submit'))
+
+    # Check _iter_job_args raises a ValueError if input Job is not built
+    job = Job('testjob', example_script, submit=submit_dir)
+    with pytest.raises(ValueError) as excinfo:
+        i = _iter_job_args(job)
+        node_name, arg = next(i)
+    error = ('Job {} must be built before adding it to a '
+             'Dagman'.format(job.name))
+    assert error == str(excinfo.value)
+
+    # Check _iter_job_args raises a StopIteration exception on a Job w/o args
+    job.build()
+    with pytest.raises(StopIteration):
+        i = _iter_job_args(job)
+        node_name, arg = next(i)
+
+    # Check _iter_job_args raises a TypeError when input is not a Job
+    with pytest.raises(TypeError) as excinfo:
+        not_job = 'thisisastring'
+        i = _iter_job_args(not_job)
+        node_name, arg = next(i)
+    error = 'Expecting a Job object, got {}'.format(type(not_job))
+    assert error == str(excinfo.value)
+
+
+def test_dagman_job_order(tmpdir):
+    # Test to check that the order in which Jobs are added to a Dagman doesn't
+    # change the Dagman submit file that is built. See issue #57.
+    example_script = os.path.join('examples/savelist.py')
+    submit_dir = str(tmpdir.mkdir('submit'))
+
+    dag_submit_lines = []
+    for order_idx in range(2):
+        dagman = Dagman('testdagman', submit=submit_dir)
+        job_child = Job('childjob', example_script, submit=submit_dir)
+        job_child.add_arg('--length 200', name='200jobname')
+        job_child.add_arg('--length 400', retry=3)
+
+        job_parent = Job('parentjob', example_script, submit=submit_dir)
+        job_parent.add_arg('--length 100')
+        job_parent.add_child(job_child)
+
+        if order_idx == 0:
+            # Add job_parent to dagman first
+            dagman.add_job(job_parent)
+            dagman.add_job(job_child)
+        else:
+            # Add job_child to dagman first
+            dagman.add_job(job_child)
+            dagman.add_job(job_parent)
+
+        dagman.build(fancyname=False)
+        # Append submit file lines to dag_submit_lines
+        with open(dagman.submit_file, 'r') as dag_submit_file:
+            dag_submit_lines.append(dag_submit_file.readlines())
+
+    # Test that the same lines occur in the Dagman submit file for
+    # adding the parent/child jobs in either order
+    assert Counter(dag_submit_lines[0]) == Counter(dag_submit_lines[1])
+
+
+def test_repr():
+    default_dagman = Dagman('dagname')
+    dag_repr = repr(default_dagman)
+    expected_repr = 'Dagman(name=dagname, n_nodes=0)'
+    assert dag_repr == expected_repr
+
+    dag_non_default = Dagman('dagname', submit='/submit_dir')
+    dag_non_default.add_subdag(default_dagman)
+    dag_repr = repr(dag_non_default)
+    expected_repr = 'Dagman(name=dagname, n_nodes=1, submit=/submit_dir)'
+    assert dag_repr == expected_repr
+
+
+def test_get_job_arg_lines_non_job_raises():
+    not_job = 'not a job'
+    with pytest.raises(TypeError) as excinfo:
+        _get_job_arg_lines(not_job, fancyname=True)
+    error = 'Expecting a Job object, got {}'.format(type(not_job))
+    assert error == str(excinfo.value)
+
+
+def test_get_job_arg_lines_not_built_raises():
+    job = Job('testjob', example_script)
+    with pytest.raises(ValueError) as excinfo:
+        _get_job_arg_lines(job, fancyname=True)
+    error = ('Job {} must be built before adding it to a '
+             'Dagman'.format(job.name))
+    assert error == str(excinfo.value)
---
+++ packages/PyCondor/PyCondor-0.2.0-py2.py3-none-any.whl/pycondor/tests/test_job.py
@@ -0,0 +1,187 @@
+
+import os
+import pytest
+from pycondor import Job
+from pycondor.utils import clear_pycondor_environment_variables
+
+clear_pycondor_environment_variables()
+
+example_script = os.path.join('examples/savelist.py')
+
+
+def test_add_arg_type_fail():
+    with pytest.raises(TypeError) as excinfo:
+        job = Job('jobname', 'jobex')
+        job.add_arg(50)
+    error = 'arg must be a string'
+    assert error == str(excinfo.value)
+
+
+def test_add_arg_name_type_fail():
+    with pytest.raises(TypeError) as excinfo:
+        job = Job('jobname', 'jobex')
+        job.add_arg('arg', name=23.12)
+    error = 'name must be a string'
+    assert error == str(excinfo.value)
+
+
+def test_add_arg_retry_type_fail():
+    with pytest.raises(TypeError) as excinfo:
+        job = Job('jobname', 'jobex')
+        job.add_arg('arg', retry='10')
+    error = 'retry must be an int'
+    assert error == str(excinfo.value)
+
+
+def test_add_child_type_fail():
+    with pytest.raises(TypeError) as excinfo:
+        job = Job('jobname', 'jobex')
+        job.add_child('childjob')
+    error = 'add_child() is expecting a Job or Dagman instance. ' + \
+            'Got an object of type {}'.format(type('childjob'))
+    assert error == str(excinfo.value)
+
+
+def test_add_children_type_fail():
+    with pytest.raises(TypeError):
+        job = Job('jobname', 'jobex')
+        job.add_children([1, 2, 3, 4])
+
+
+def test_add_parent_type_fail():
+    with pytest.raises(TypeError) as excinfo:
+        job = Job('jobname', 'jobex')
+        job.add_parent('parentjob')
+    error = 'add_parent() is expecting a Job or Dagman instance. ' + \
+            'Got an object of type {}'.format(type('parentjob'))
+    assert error == str(excinfo.value)
+
+
+def test_add_parents_type_fail():
+    with pytest.raises(TypeError):
+        job = Job('jobname', 'jobex')
+        job.add_parents([1, 2, 3, 4])
+
+
+def test_build_executeable_not_found_fail():
+    with pytest.raises(IOError) as excinfo:
+        ex = '/path/to/executable'
+        job = Job('jobname', ex)
+        job.build(makedirs=False)
+    error = 'The executable {} does not exist'.format(ex)
+    assert error == str(excinfo.value)
+
+
+def test_queue_written_to_submit_file(tmpdir):
+    # Test to check that the queue parameter is properly written
+    # to submit file when Job is created. See issue #38.
+
+    submit_dir = str(tmpdir.mkdir('submit'))
+
+    # Build Job object with queue=5
+    job = Job('jobname', example_script, submit=submit_dir, queue=5)
+    job.build(fancyname=False)
+
+    # Read the built submit file and check that the 'queue 5' is
+    # contained in the file.
+    with open(job.submit_file, 'r') as f:
+        lines = f.readlines()
+    assert 'queue 5' in lines
+
+
+def test_job_env_variable_dir(tmpdir):
+    # Set pycondor environment variables
+    for dir_name in ['submit', 'output', 'error', 'log']:
+        dir_path = str(tmpdir.mkdir(dir_name))
+        os.environ['PYCONDOR_{}_DIR'.format(dir_name.upper())] = dir_path
+
+    job = Job('jobname', example_script)
+    job.build()
+    for dir_name in ['submit', 'output', 'error', 'log']:
+        tmpdir_path = os.path.join(str(tmpdir), dir_name)
+        job_path = os.path.dirname(getattr(job, '{}_file'.format(dir_name)))
+        assert tmpdir_path == job_path
+
+    clear_pycondor_environment_variables()
+
+
+def test_repr():
+    default_job = Job('jobname', example_script)
+    job_repr = repr(default_job)
+    expected_repr = ('Job(name=jobname, executable=savelist.py, '
+                     'getenv=True, notification=never, universe=vanilla)')
+    assert job_repr == expected_repr
+
+    job_non_default = Job('jobname', example_script, queue=2)
+    job_repr = repr(job_non_default)
+    expected_repr = ('Job(name=jobname, executable=savelist.py, getenv=True, '
+                     'notification=never, queue=2, universe=vanilla)')
+    assert job_repr == expected_repr
+
+
+def test_submit_job_not_built_raises():
+    job = Job('jobname', example_script)
+    with pytest.raises(ValueError) as excinfo:
+        job.submit_job()
+    error = 'build() must be called before submit()'
+    assert error == str(excinfo.value)
+
+
+def test_submit_job_parents_raises(tmpdir):
+    # Test submitting a Job with parents (not in a Dagman) raises an error
+    submit = str(tmpdir)
+    job = Job('jobname', example_script, submit=submit)
+    parent_job = Job('parent_jobname', example_script)
+    job.add_parent(parent_job)
+    job.build()
+    with pytest.raises(ValueError) as excinfo:
+        job.submit_job()
+    error = ('Attempting to submit a Job with parents. '
+             'Interjob relationships requires Dagman.')
+    assert error == str(excinfo.value)
+
+
+def test_submit_job_children_raises(tmpdir):
+    # Test submitting a Job with children (not in a Dagman) raises an error
+    submit = str(tmpdir)
+    job = Job('jobname', example_script, submit=submit)
+    child_job = Job('child_jobname', example_script)
+    job.add_child(child_job)
+    job.build()
+    with pytest.raises(ValueError) as excinfo:
+        job.submit_job()
+    error = ('Attempting to submit a Job with children. '
+             'Interjob relationships requires Dagman.')
+    assert error == str(excinfo.value)
+
+
+def test_add_args_raises():
+    # Test that add_args won't accept single argument inputs
+    job = Job('jobname', example_script)
+    with pytest.raises(TypeError) as excinfo:
+        job.add_args('single argument')
+    error = 'add_args() is expecting an iterable of argument strings'
+    assert error == str(excinfo.value)
+
+
+def test_add_args():
+    # Test that add_args is equivalent to multiple add_arg
+    job_1 = Job('job1', example_script)
+    for i in range(10):
+        job_1.add_arg('file_{}.hdf'.format(i))
+
+    job_2 = Job('job2', example_script)
+    job_2.add_args(['file_{}.hdf'.format(i) for i in range(10)])
+
+    assert job_1.args == job_2.args
+
+
+def test_retry_job_raises():
+    # Test that building a Job (not in a Dagman) with a retry raises an error
+    job = Job('jobname', example_script)
+    job.add_arg('argument', retry=2)
+    with pytest.raises(NotImplementedError) as excinfo:
+        job.build()
+    error = ('Retrying failed Jobs is only available when submitting '
+             'from a Dagman.')
+    assert error == str(excinfo.value)
