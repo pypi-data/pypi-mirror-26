0bdc9ed5f1d8f69c18eec0e0467860a222dcb870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:31 +0000

    Add package django-field-editor/0.0.2/django-field-editor-0.0.2.tar.gz

---
+++ packages/django-field-editor/django-field-editor-0.0.2.tar.gz/django-field-editor-0.0.2/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='django-field-editor',
+    version='0.0.2',
+    url='https://github.com/kycool/django-json-editor',
+    author='kycool',
+    author_email='kycoolcool@gmail.com',
+    description='Django field editor.',
+    license='GPL',
+    packages=['django_field_editor'],
+    include_package_data=True,
+    zip_safe=False,
+)
