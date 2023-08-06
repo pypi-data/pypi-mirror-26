bde2407e18d7f030d8ceb9ab5d996523e658582c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:45 +0000

    Add package knackpy/0.0.5/knackpy-0.0.5.tar.gz

---
+++ packages/knackpy/knackpy-0.0.5.tar.gz/knackpy-0.0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    author='John Clary',
+    author_email='john.clary@austintexas.gov',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        # Pick your license as you wish (should match "license" above)
+        'License :: Public Domain',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+    description='Python API wrapper for interacting with Knack applications.',
+    install_requires=[
+      'arrow',
+      'requests'
+    ],
+    keywords='knack api api-client integration',
+    license='Public Domain',
+    name='knackpy',
+    packages=['knackpy'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    url='http://github.com/cityofaustin/knack-py',
+    version='0.0.5',
+)
