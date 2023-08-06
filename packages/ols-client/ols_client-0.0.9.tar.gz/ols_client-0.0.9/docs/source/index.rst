fd219e62d8afb9a218b250c1140a91418d7631d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:57 +0000

    Add package ols-client/0.0.9/ols_client-0.0.9.tar.gz

---
+++ packages/ols-client/ols_client-0.0.9.tar.gz/ols_client-0.0.9/docs/source/index.rst
@@ -0,0 +1,24 @@
+.. OLS Client documentation master file, created by
+   sphinx-quickstart on Fri Sep 29 15:28:49 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to OLS Client's documentation!
+======================================
+
+This is a Python software package for interfacing with the RESTful API of the EBI OLS.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api
+   client
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/ols-client/ols_client-0.0.9.tar.gz/ols_client-0.0.9/tests/test_ols_client.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+
+from ols_client import get_labels, get_metadata, get_hierarchy
+
+
+class TestClient(unittest.TestCase):
+    def test_terms(self):
+        """Tests that the right terms are acquired.
+
+        Uses the ``ancestro`` ontology because it's pretty short"""
+        labels = set(get_labels('ancestro'))
+        self.assertIn('Irish', labels)
+
+    def test_hierarchy(self):
+        pairs = set(get_hierarchy('ancestro'))
+        self.assertIn(('country', 'Algeria'), pairs)
+
+    def test_metadata(self):
+        """Tests that the right metadata are acquired."""
+        metadata = get_metadata('ancestro')
+
+        self.assertIn('numberOfTerms', metadata)
+        self.assertEqual(540, metadata['numberOfTerms'])
+
+        self.assertIn('config', metadata)
+        self.assertIn('title', metadata['config'])
+        self.assertIn('Ancestry Ontology', metadata['config']['title'])
