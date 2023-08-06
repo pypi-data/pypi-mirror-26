1c5862fb8c37e345cb2eeb51d31320e09da9f176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:40 +0000

    Add package elabtui/0.1.0/elabtui-0.1.0.tar.gz

---
+++ packages/elabtui/elabtui-0.1.0.tar.gz/elabtui-0.1.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+long_description = """This is a text user interface to eLabFTW."""
+
+setup(
+    name='elabtui',
+    version='0.1.0',
+    description='elabftw text user interface to manage experiments',
+    author='Nicolas CARPi',
+    author_email='nicolas.carpi@curie.fr',
+    url='https://github.com/elabftw/elabtui',
+    install_requires=['asciimatics', 'elabapy'],
+    license='GPL v3',
+    long_description=long_description
+)
