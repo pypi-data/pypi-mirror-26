5b41b8da573d434fe7852bfa1f1bfa01683ea3f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:03 +0000

    Add package pycydemo/0.3.5/pycydemo-0.3.5.tar.gz

---
+++ packages/pycydemo/pycydemo-0.3.5.tar.gz/pycydemo-0.3.5/pycydemo/tests/test_extension.py
@@ -0,0 +1,11 @@
+"""Unit tests for pycydemo."""
+
+from pycydemo.extension import some_function
+from nose.tools import assert_equal
+
+
+def test_some_function():
+    assert_equal(some_function(0, 0), 0)
+    assert_equal(some_function(0, 42), 0)
+    assert_equal(some_function(41, 2), 1)
+    assert_equal(some_function(1, 2), 3)
