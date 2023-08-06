821a9f0fd3f32b3495f69a149035a08ade252576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:43 +0000

    Add package spoonproxy/0.2.4/spoonproxy-0.2.4-py2.py3-none-any.whl

---
+++ packages/spoonproxy/spoonproxy-0.2.4-py2.py3-none-any.whl/spoon_server/proxy/fetcher.py
@@ -0,0 +1,53 @@
+from spoon_server.proxy.gou_provider import GouProvider
+from spoon_server.proxy.kuai_provider import KuaiProvider
+from spoon_server.proxy.xici_provider import XiciProvider
+from spoon_server.proxy.kuai_pay_provider import KuaiPayProvider
+from spoon_server.proxy.file_provider import FileProvider
+from spoon_server.proxy.wuyou_provider import WuyouProvider
+from spoon_server.proxy.us_provider import UsProvider
+from spoon_server.proxy.ip181_provider import IP181Provider
+from spoon_server.proxy.six_provider import SixProvider
+
+
+class Fetcher(object):
+    def __init__(self, use_default=True):
+        if use_default:
+            self.provider_list = self._generate_provider_list()
+        else:
+            self.provider_list = []
+
+    @staticmethod
+    def _generate_provider_list():
+        ip181 = IP181Provider()
+        gp = GouProvider()  # Maybe IP Block
+        kp = KuaiProvider()  # Maybe malfunction
+        kpp = KuaiPayProvider()
+        xp = XiciProvider()
+        fp = FileProvider()
+        wp = WuyouProvider()  # Maybe IP Block
+        up = UsProvider()
+        six = SixProvider()
+        return [ip181, up, gp, kp, kpp, xp, fp, wp, six]
+
+    def clear(self):
+        self.provider_list = []
+        return self
+
+    def set_provider(self, providers):
+        self.provider_list = providers
+        return self
+
+    def add_provider(self, providers):
+        self.provider_list.extend(providers)
+        return self
+
+    def __len__(self):
+        return len(self.provider_list)
+
+
+if __name__ == '__main__':
+    fetch = Fetcher()
+
+    for k in fetch.provider_list:
+        for px in k.getter():
+            print(px)
