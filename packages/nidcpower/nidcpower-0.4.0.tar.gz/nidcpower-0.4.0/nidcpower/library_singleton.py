6e53c2aff9bbc0c538199de298cd3f2cfec1ed3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:12 +0000

    Add package nidcpower/0.4.0/nidcpower-0.4.0.tar.gz

---
+++ packages/nidcpower/nidcpower-0.4.0.tar.gz/nidcpower-0.4.0/nidcpower/library_singleton.py
@@ -0,0 +1,54 @@
+# This file was generated
+
+import platform
+
+import ctypes
+from nidcpower import errors
+from nidcpower import library
+import threading
+
+
+_instance = None
+_instance_lock = threading.Lock()
+
+
+def _get_library_name():
+    try:
+        return {'Linux': {'64bit': {'name': 'libnidcpower.so', 'type': 'cdll'}},
+                'Windows': {'32bit': {'name': 'nidcpower_32.dll', 'type': 'windll'},
+                            '64bit': {'name': 'nidcpower_64.dll', 'type': 'cdll'}}}[platform.system()][platform.architecture()[0]]['name']
+    except KeyError:  # pragma: no cover
+        raise errors.UnsupportedConfigurationError
+
+
+def _get_library_type():
+    try:
+        return {'Linux': {'64bit': {'name': 'libnidcpower.so', 'type': 'cdll'}},
+                'Windows': {'32bit': {'name': 'nidcpower_32.dll', 'type': 'windll'},
+                            '64bit': {'name': 'nidcpower_64.dll', 'type': 'cdll'}}}[platform.system()][platform.architecture()[0]]['type']
+    except KeyError:  # pragma: no cover
+        raise errors.UnsupportedConfigurationError
+
+
+def get():
+    '''get
+
+    Returns the library.Library singleton for nidcpower.
+    '''
+    global _instance
+    global _instance_lock
+
+    with _instance_lock:
+        if _instance is None:
+            try:
+                library_type = _get_library_type()
+                if library_type == 'windll':
+                    ctypes_library = ctypes.WinDLL(_get_library_name())
+                else:  # pragma: no cover
+                    assert library_type == 'cdll'
+                    ctypes_library = ctypes.CDLL(_get_library_name())
+            except OSError:  # pragma: no cover
+                raise errors.DriverNotInstalledError()
+            _instance = library.Library(ctypes_library)
+        return _instance
+
