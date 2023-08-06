67348aadba9efcab75e2fd800cf17860f64d3252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:23 +0000

    Add package octoprint-dashboard/0.3.1.dev1/octoprint_dashboard-0.3.1.dev1-py2.py3-none-any.whl

---
+++ packages/octoprint-dashboard/octoprint_dashboard-0.3.1.dev1-py2.py3-none-any.whl/octoprint_dashboard/model/User.py
@@ -0,0 +1,153 @@
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from octoprint_dashboard.app import db
+
+
+class User(db.Model):
+    """
+    Instance of this class equals single record of config in database
+    Class behaves like repository of Config records
+
+    User represents human being, logged into app at least once
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    access_token = db.Column(db.String(80))
+    refresh_token = db.Column(db.String(80))
+    superadmin = db.Column(db.Boolean, default=False)
+
+    group = association_proxy("group_user", "group")
+
+    def __init__(self, username=None, access_token=None, refresh_token=None, superadmin=False):
+        self.username = username
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+        self.superadmin = superadmin
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    @staticmethod
+    def upsert(username, access_token, refresh_token):
+        """
+        Creates user with parameters in database or updates access and refresh token of user
+        """
+        user = User.query.filter_by(username=username).scalar()
+        if user is None:
+            user = User(username, access_token, refresh_token)
+            db.session.add(user)
+        else:
+            user.access_token = access_token
+            user.refresh_token = refresh_token
+        db.session.commit()
+        return user
+
+    @staticmethod
+    def upsert_superadmin(username):
+        """
+        Makes user superadmin
+        """
+        user = User.query.filter_by(username=username).scalar()
+        if user is None:
+            user = User(username, None, None)
+            user.superadmin = True
+            db.session.add(user)
+        else:
+            user.superadmin = True
+        db.session.commit()
+
+    def get_accessible_printers(self):
+        """
+        Returns all printers accessible to user with admin role
+        """
+        from octoprint_dashboard.model import Printer, Group, GroupUser
+        if self.superadmin:
+            printers = Printer.query.all()
+        else:
+            printers = Printer.query.join(Printer.group).join(Group.group_user).filter(User.id == self.id,
+                                                                                       GroupUser.role == "admin").all()
+
+        return printers
+
+    def get_accessible_printers_id(self, printer_ids):
+        """
+        Returns printers of given ids accessible to user with admin role
+        """
+        from octoprint_dashboard.model import Printer, Group, GroupUser
+        if self.superadmin:
+            printers = Printer.query.filter(Printer.id.in_(printer_ids)).all()
+        else:
+            printers = Printer.query.filter(Printer.id.in_(printer_ids)) \
+                .join(Printer.group).join(Group.group_user) \
+                .filter(User.id == self.id, GroupUser.role == "admin").all()
+
+        return printers
+
+    def get_accessible_printer_id(self, printer_id):
+        """
+        Returns printer of given id accessible to user with admin role or None
+        """
+        from octoprint_dashboard.model import Printer, Group, GroupUser
+        if self.superadmin:
+            printer = Printer.query.get(printer_id)
+        else:
+            printer = Printer.query.filter(Printer.id == printer_id) \
+                .join(Printer.group).join(Group.group_user) \
+                .filter(User.id == self.id, GroupUser.role == "admin").scalar()
+
+        return printer
+
+    def get_printer_id(self, printer_id):
+        """
+        Returns printer of given id accessible to user or None
+        """
+        from octoprint_dashboard.model import Printer, Group
+        if self.superadmin:
+            printer = Printer.query.get(printer_id)
+        else:
+            printer = Printer.query.filter(Printer.id == printer_id) \
+                .join(Printer.group).join(Group.group_user) \
+                .filter(User.id == self.id).scalar()
+
+        return printer
+
+    def get_editable_groups(self):
+        """
+        Returns groups accessible to user with admin role
+        """
+        from octoprint_dashboard.model import Group, GroupUser
+
+        if self.superadmin:
+            groups = Group.query.all()
+        else:
+            groups = Group.query.join(Group.group_user).join(GroupUser.user).filter(User.id == self.id).filter(
+                GroupUser.role == "admin").all()
+
+        return groups
+
+    def get_editable_group_id(self, id):
+        """
+        Returns groups of given id accessible to user with admin role
+        """
+        from octoprint_dashboard.model import Group, GroupUser
+
+        if self.superadmin:
+            group = Group.query.get(id)
+        else:
+            group = Group.query.join(Group.group_user).join(GroupUser.user).filter(User.id == self.id).filter(
+                GroupUser.role == "admin").filter(Group.id == id).scalar()
+
+        return group
+
+    def get_groups(self):
+        """
+        Returns groups accessible to user
+        """
+        from octoprint_dashboard.model import Group, GroupUser
+
+        if self.superadmin:
+            groups = Group.query.all()
+        else:
+            groups = Group.query.join(Group.group_user).join(GroupUser.user).filter(User.id == self.id).all()
+
+        return groups
---
+++ packages/octoprint-dashboard/octoprint_dashboard-0.3.1.dev1-py2.py3-none-any.whl/octoprint_dashboard/services/loginService.py
@@ -0,0 +1,104 @@
+import base64
+from datetime import datetime, timedelta
+
+import jwt
+import requests
+
+
+class LoginService:
+    """
+    This class is meant to be whole static to simulate singleton
+
+    It is collection of functions for login handling
+    """
+    access_route = "https://auth.fit.cvut.cz/oauth/oauth/token"
+    check_route = "https://auth.fit.cvut.cz/oauth/oauth/check_token"
+    secret = None
+    client_id = None
+    client_secret = None
+    redirect_uri = None
+    authorization = None
+
+    @classmethod
+    def init(cls):
+        from octoprint_dashboard.model import Config
+        config = Config.query.first()
+        if config:
+            cls.secret = config.secret
+            cls.client_id = config.oauth_client_id
+            cls.client_secret = config.oauth_client_secret
+            cls.redirect_uri = config.oauth_redirect_uri
+            cls.authorization = "Basic " + base64.b64encode(
+                "{0}:{1}".format(cls.client_id, cls.client_secret).encode('ascii')).decode('utf-8')
+
+    @staticmethod
+    def create_api_token(username, role):
+        """
+        Returns token for access to API
+        """
+        payload = {
+            'username': username,
+            'role': role,
+            'iat': datetime.utcnow(),
+            'exp': datetime.utcnow() + timedelta(days=14)
+        }
+        token = jwt.encode(payload, LoginService.secret, algorithm="HS256")
+        return token.decode('unicode_escape')
+
+    @staticmethod
+    def parse_api_token(request):
+        """
+        Returns parsed and decoded token from authorization header
+        """
+        token = request.headers.get('Authorization').split()[1]
+        return jwt.decode(token, LoginService.secret)
+
+    @staticmethod
+    def parse_api_token_direct(token):
+        """
+        Returns parsed and decoded token from authorization header
+        """
+        return jwt.decode(token, LoginService.secret)
+
+    @staticmethod
+    def get_access_code(code):
+        """
+        Makes request to OAuth to exchange code for access_token and returns response
+        """
+        response = requests.post(LoginService.access_route, headers={
+            "Authorization": LoginService.authorization
+        }, data={
+            "grant_type": "authorization_code",
+            "code": code,
+            "scope": "urn:zuul:oauth",
+            "redirect_uri": LoginService.redirect_uri
+        })
+        response.raise_for_status()
+        return response.json()
+
+    @staticmethod
+    def check_token(access_token):
+        """
+        Makes request to OAuth check token route and returns response
+        Returns information about token (user)
+        """
+        response = requests.post(LoginService.check_route, data={
+            "token": access_token
+        })
+        response.raise_for_status()
+        return response.json()
+
+    @staticmethod
+    def refresh_token(refresh_token):
+        """
+        Makes request to OAuth to refresh access token and returns response
+        """
+        response = requests.post(LoginService.access_route, headers={
+            "Authorization": LoginService.authorization
+        }, data={
+            "grant_type": "refresh_token",
+            "scope": "urn:zuul:oauth",
+            "refresh_token": refresh_token
+        })
+        response.raise_for_status()
+        return response.json()
