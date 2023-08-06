e2c717c65b2a5b0eb4a14de998d2241e67d792f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:32 +0000

    Add package repotool/0.2/repotool-0.2.tar.gz

---
+++ packages/repotool/repotool-0.2.tar.gz/repotool-0.2/repotool/__init__.py
@@ -0,0 +1,7 @@
+##################################################
+# Copyright (C) 2017, All rights reserved.
+##################################################
+
+__project_name__ = "repotool"
+__version__ = "0.2"
+__description__ = "Python wrappers around GitHub, GitLab and Bitbucket REST APIs"
