62e94c1c92992b86902a3857ef2cb9881501898e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:52 +0000

    Add package safetyculture-sdk-python/3.1.3/safetyculture-sdk-python-3.1.3.tar.gz

---
+++ packages/safetyculture-sdk-python/safetyculture-sdk-python-3.1.3.tar.gz/safetyculture-sdk-python-3.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(name = 'safetyculture-sdk-python',
+      version = '3.1.3',
+      description = 'iAuditor Python SDK and integration tools',
+      url = 'https://github.com/SafetyCulture/safetyculture-sdk-python',
+      author = 'SafetyCulture',
+      author_email = 'integrations@safetyculture.io',
+      include_package_data=True,
+      packages = ['safetypy', 'tools', 'tools/exporter', 'tools/import_grs'],
+      entry_points = {
+            'console_scripts': [
+                  'iauditor_exporter = tools.exporter.exporter:main',
+                  'import_grs = tools.import_grs.import_grs:main'
+            ],
+      },
+      long_description=open('README.md', 'r').read(),
+      install_requires = [
+            'python-dateutil>=2.5.0',
+            'pytz>=2015.7',
+            'tzlocal>=1.3',
+            'unicodecsv>=0.14.1',
+            'requests>=2.10.0',
+            'pyyaml>=3.11',
+            'future>=0.16.0',
+            'xlrd==1.1.0'
+      ],
+      )
