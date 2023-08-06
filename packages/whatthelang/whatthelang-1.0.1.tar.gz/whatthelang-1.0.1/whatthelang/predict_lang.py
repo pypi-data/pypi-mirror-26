7e50d3d3914fcdd984d82993891f1c7017898c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:38 +0000

    Add package whatthelang/1.0.1/whatthelang-1.0.1.tar.gz

---
+++ packages/whatthelang/whatthelang-1.0.1.tar.gz/whatthelang-1.0.1/whatthelang/predict_lang.py
@@ -0,0 +1,49 @@
+from pyfasttext import FastText
+from os import path
+import re
+
+MODEL_FILE = path.join(path.dirname(__file__), 'model', 'lid.176.ftz')
+
+class WhatTheLang(object):
+    def __init__(self):
+        self.model_file = MODEL_FILE
+        self.model = self.load_model()
+        self.unknown = "CANT_PREDICT"
+
+    def load_model(self):
+        return FastText(self.model_file)
+
+    def _clean_up(self,txt):
+        txt = re.sub(r"\b\d+\b", "", txt)
+        return txt
+
+    def _flatten(self,pred):
+        return [item[0] if len(item)!=0 else self.unknown for item in pred]
+
+
+    def _get_langs(self):
+        return self.model.labels
+
+
+    def predict_lang(self,inp):
+        if type(inp) != list:
+            cleaned_txt = self._clean_up(inp)
+            if cleaned_txt == "":
+                raise ValueError("Not enough text to predict language")
+            pred = self.model.predict([cleaned_txt])[0]
+            if len(pred) == 0:
+                return self.unknown
+            return pred[0]
+        else:
+            batch = [self._clean_up(i) for i in inp]
+            return self._flatten(self.model.predict(batch))
+
+
+    def pred_prob(self,inp):
+        if type(inp) != list:
+            inp = self._clean_up(inp)
+            return self.model.predict_proba([inp])
+        return self.model.predict_proba(inp)
+
+
+
---
+++ packages/whatthelang/whatthelang-1.0.1.tar.gz/whatthelang-1.0.1/whatthelang/test_predict_lang.py
@@ -0,0 +1,50 @@
+#coding=utf-8
+
+from predict_lang import WhatTheLang
+from os import path
+
+class WhatTheLangTest():
+
+    def __init__(self):
+        self.model_file = path.join(path.dirname(__file__),'model','lid.176.ftz')
+        self.wtl = WhatTheLang()
+
+    def test_model_present(self):
+        assert path.isfile(self.model_file) == True
+
+    def test_english(self):
+        pred = self.wtl.predict_lang("This is an English sentence")
+        assert pred == 'en'
+
+    def test_batch(self):
+        pred = self.wtl.predict_lang(["English sentence","അമ്മ"])
+        assert pred == ['en','ml']
+
+    def test_num_langs(self):
+        assert len(self.wtl._get_langs()) ==176
+
+    def test_unknown(self):
+        pred = self.wtl.predict_lang("asdfs")
+        assert pred == "CANT_PREDICT"
+
+    def test_batch_with_unknown(self):
+        pred = self.wtl.predict_lang(["English sentence", "അമ്മ","asdfs"])
+        assert pred == ['en', 'ml', "CANT_PREDICT"]
+
+    def test_unicode_input(self):
+        pred = self.wtl.predict_lang(u"nike running shoes")
+        assert pred == 'en'
+
+    def test_all(self):
+
+        self.test_model_present()
+        self.test_num_langs()
+        self.test_english()
+        self.test_batch()
+        self.test_unknown()
+        self.test_batch_with_unknown()
+        self.test_unicode_input()
+
+if __name__ == "__main__":
+    wtlt = WhatTheLangTest()
+    wtlt.test_all()
