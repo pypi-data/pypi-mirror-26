e1e0e67015f09457a02337b4be7813ed0d05c489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:59 +0000

    Add package djangofloor/1.0.27/djangofloor-1.0.27.tar.gz

---
+++ packages/djangofloor/djangofloor-1.0.27.tar.gz/djangofloor-1.0.27/djangofloor/views/legacy.py
@@ -0,0 +1,24 @@
+import warnings
+
+from djangofloor import views
+from djangofloor.utils import RemovedInDjangoFloor110Warning
+
+__author__ = 'Matthieu Gallet'
+
+
+def signals(request):
+    warnings.warn('"df/signals.js" URL is deprecated, replaced by the "df:signals" view.',
+                  RemovedInDjangoFloor110Warning)
+    return views.signals(request)
+
+
+def signal_call(request, signal):
+    warnings.warn('"df_signal_call" view is deprecated, replaced by the websocket-based signals.',
+                  RemovedInDjangoFloor110Warning)
+    return views.signal_call(request, signal)
+
+
+def get_signal_calls(request):
+    warnings.warn('"df_get_signal_calls" view is deprecated, replaced by the websocket-based signals.',
+                  RemovedInDjangoFloor110Warning)
+    return views.get_signal_calls(request)
