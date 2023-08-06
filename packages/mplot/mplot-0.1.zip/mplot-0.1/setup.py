779d862045e98f77be0162dff9e5533927160b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:40 +0000

    Add package mplot/0.1/mplot-0.1.zip

---
+++ packages/mplot/mplot-0.1.zip/mplot-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mplot',
+      version='0.1',
+      description='This is a plotting library designed to be similar in usage to maple',
+      url='',
+      author='Steve Mohr',
+      author_email='steve.h.mohr@gmail.com',
+      python_requires='>=2.6',
+      packages=['mplot'],
+      zip_safe=False)
