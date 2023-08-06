de68214d532f285ae33f6f43e491de879de85755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:26 +0000

    Add package hbr_client/0.1.1/hbr_client-0.1.1.tar.gz

---
+++ packages/hbr_client/hbr_client-0.1.1.tar.gz/hbr_client-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from __version__ import version
+setup(
+    name = 'hbr_client',
+    packages = ['hbr_client'], # this must be the same as the name above
+    version = version,
+    description = 'Python Library to consume HBR Cloud API',
+    author = 'HotBlack Robotics',
+    author_email = 'info@hotblackrobotics.com',
+    url = 'https://github.com/HotBlackRobotics/hbr_client_python', # use the URL to the github repo
+    download_url = 'https://github.com/HotBlackRobotics/hbr_client_python/archive/{}.tar.gz'.format(version), # I'll explain this in a second
+    keywords = ['HBR', 'logging'], # arbitrary keywords
+    classifiers = [],
+    install_requires=['requests']
+)
