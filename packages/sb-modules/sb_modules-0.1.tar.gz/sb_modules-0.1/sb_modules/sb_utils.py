8ef36d6af8a03d67510123b43cf6dba68a3bfb9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:23 +0000

    Add package sb-modules/0.1/sb_modules-0.1.tar.gz

---
+++ packages/sb-modules/sb_modules-0.1.tar.gz/sb_modules-0.1/sb_modules/sb_utils.py
@@ -0,0 +1,43 @@
+""" Some utilities
+"""
+import json
+from pygments import highlight
+from pygments.lexers import JsonLexer
+from pygments.formatters import TerminalFormatter
+import jsonpickle
+from . backports.shutil_get_terminal_size import get_terminal_size
+from . import constants as C
+
+def ppc(obj):
+    """ Pretty print an obj in color
+
+    Args:
+        obj (object): An object
+
+    """
+    serialized = jsonpickle.encode(obj, unpicklable=False)
+    json_str = json.dumps(json.loads(serialized), indent=4, sort_keys=True)
+    print(highlight(json_str, JsonLexer(), TerminalFormatter()))
+
+def status(message, style):
+    """ Prints to screen and log file
+
+    Args:
+        message (str): The message to print
+        style (str): A style with which to print
+
+    """
+    header = '\033[%s;%sm' % (C.BOLD, C.FG_WHITE)
+    okgreen = '\033[%sm' % C.FG_GREEN
+    warning = '\033[%s;%sm' % (C.BOLD, C.FG_YELLOW)
+    fail = '\033[%s;%sm' % (C.BOLD, C.FG_RED)
+    endc = '\033[0m'
+    if style == 'header':
+        columns, _lines = get_terminal_size()
+        print("%s[%s] %s%s" % (header, message, ("*" * (columns - len(message) - 3)), C.RESET_SEQ))
+    elif style == 'ok':
+        print(u"%s%-60s%s%s" % (okgreen, message, u'[\u2714]', endc))
+    elif style == 'warning':
+        print(u"%s%-60s%s%s" % (warning, message, u'[!]', endc))
+    elif style == 'fail':
+        print(u"%s%-60s%s%s" % (fail, message, u'[\u2715]', endc))
---
+++ packages/sb-modules/sb_modules-0.1.tar.gz/sb_modules-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sb_modules',
+      version='0.1',
+      description='Simple helpers',
+      url='http://github.com/cidrblock/sb_helpers',
+      author='Bradley A. Thornton',
+      author_email='brad@thethorntons.net',
+      license='MIT',
+      packages=['sb_modules'],
+      zip_safe=False)
