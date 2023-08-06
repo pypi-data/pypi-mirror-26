a518872ce85a588d156b703158e050ae9a71aa94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:57 +0000

    Add package qpagent/248/qpagent-248-py2.py3-none-any.whl

---
+++ packages/qpagent/qpagent-248-py2.py3-none-any.whl/agent/queue/__init__.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import Queue
+import logging
+from threading import Thread
+
+from agent.consts import AgentWorkerType
+from agent.worker.android_monkey import AndroidMonkey
+# from agent.worker.appcrawler import Appcrawler
+# from agent.worker.appstore import AppStoreCrawler
+# from agent.worker.compatibility import Compatibility
+# from agent.worker.ios_monkey import iosmonkey
+# from agent.worker.macaca import Macaca
+# from agent.worker.xiaomi import XiaomiCrawler
+
+logger = logging.getLogger(__name__)
+
+
+class TaskQueue(Queue.Queue):
+    def __init__(self, num_workers=1):
+        Queue.Queue.__init__(self)
+        self.num_workers = num_workers
+        self.start_workers()
+
+    def add_task(self, worker_type, task_id):
+        self.put((worker_type, task_id))
+
+    def start_workers(self):
+        for i in range(self.num_workers):
+            t = Thread(target=self.worker)
+            t.daemon = True
+            t.start()
+
+    def worker(self):
+        while True:
+            worker_type, task_id = self.get(True)
+            if worker_type is AgentWorkerType.androidmonkey.value:
+                logger.info('androidmonkey')
+                AndroidMonkey(task_id).run()
