6066bb8b0616aa52847a414d8ce637748596f9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:15 +0000

    Add package naster-sytang/1.1.0/naster-sytang-1.1.0.tar.gz

---
+++ packages/naster-sytang/naster-sytang-1.1.0.tar.gz/naster-sytang-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'naster-sytang',
+        version = '1.1.0',
+        py_modles = ['naster-sytang'],
+        author = 'sytang',
+        author_email = '3048608732@qq.com',
+        url = 'https://en.mail.qq.com',
+        description = 'A simple printer of nested lists',
+     )
