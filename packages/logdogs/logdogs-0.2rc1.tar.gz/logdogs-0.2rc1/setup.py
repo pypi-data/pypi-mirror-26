e1ce492c37f4709457ffbe9543abc1f1f5882877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:56 +0000

    Add package logdogs/0.2rc1/logdogs-0.2rc1.tar.gz

---
+++ packages/logdogs/logdogs-0.2rc1.tar.gz/logdogs-0.2rc1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name="logdogs",
+    version="0.2rc1",
+    author="Xurui Yan",
+    author_email="yxr1993@gmail.com",
+    description='A daemon to monitor keywords in any log files specified by glob pattern',
+    long_description=open('README.rst').read(),
+    license="MIT License",
+    keywords="log monitor",
+    url="https://github.com/yanxurui/logdogs",
+    package_dir = {'': 'src'},
+    py_modules=['logdogs', 'pyconfig'],
+    platforms=['Linux'],
+    install_requires=[
+        'glob2>=0.6',
+        'python-daemon>=2.1.2'
+    ],
+    entry_points={
+        'console_scripts': [
+            'logdogs=logdogs:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Environment :: Console',
+        'Topic :: Utilities'
+    ]
+)
