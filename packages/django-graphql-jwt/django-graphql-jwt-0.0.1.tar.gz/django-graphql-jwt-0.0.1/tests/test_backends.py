199298c6f961d5686c1ef05d5518db9340161b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:00 +0000

    Add package django-graphql-jwt/0.0.1/django-graphql-jwt-0.0.1.tar.gz

---
+++ packages/django-graphql-jwt/django-graphql-jwt-0.0.1.tar.gz/django-graphql-jwt-0.0.1/tests/test_backends.py
@@ -0,0 +1,35 @@
+from graphql_jwt.backends import JWTBackend
+from graphql_jwt.exceptions import GraphQLJWTError
+
+from .base import GraphQLJWTTestCase
+
+
+class BackendsTests(GraphQLJWTTestCase):
+
+    def test_authenticate(self):
+        headers = {
+            'HTTP_AUTHORIZATION': 'JWT ' + self.token,
+        }
+
+        request = self.factory.get('/', **headers)
+        user = JWTBackend().authenticate(request=request)
+
+        self.assertEqual(user, self.user)
+
+    def test_authenticate_fail(self):
+        headers = {
+            'HTTP_AUTHORIZATION': 'JWT invalid',
+        }
+
+        request = self.factory.get('/', **headers)
+
+        with self.assertRaises(GraphQLJWTError):
+            JWTBackend().authenticate(request=request)
+
+    def test_authenticate_null_request(self):
+        user = JWTBackend().authenticate(request=None)
+        self.assertIsNone(user)
+
+    def test_get_user(self):
+        user = JWTBackend().get_user(self.user.username)
+        self.assertEqual(user, self.user)
