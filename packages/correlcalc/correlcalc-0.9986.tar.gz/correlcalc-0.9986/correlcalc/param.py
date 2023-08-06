8f40a31975abf9e0badb74bf2c316d260c5096c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:14 +0000

    Add package correlcalc/0.9986/correlcalc-0.9986.tar.gz

---
+++ packages/correlcalc/correlcalc-0.9986.tar.gz/correlcalc-0.9986/correlcalc/param.py
@@ -0,0 +1,12 @@
+# Set global parameters here related to Cosmology (more to be added)
+# Om= matter density Ol=dark energy density at the present epoch
+# Planck 2015
+# Om = 0.3121
+# Ol = 0.6879
+# WMAP9
+# Om = 0.279
+# Ol = 0.721
+# Default
+Om = 0.3
+Ol = 0.7
+cbyH0 = 2997.92458 # to multiply bins to plot in Mpc h^{-1}
