c58124c3a0dee3edc5116cbdee64d55fa6f3442c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:36 +0000

    Add package pyetcd/1.8.0/pyetcd-1.8.0.tar.gz

---
+++ packages/pyetcd/pyetcd-1.8.0.tar.gz/pyetcd-1.8.0/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pip.req import parse_requirements
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [str(ir.req) for ir in
+                parse_requirements('requirements.txt', session=False)]
+
+test_requirements = [str(ir.req) for ir in
+                     parse_requirements('requirements_dev.txt', session=False)]
+
+
+setup(
+    name='pyetcd',
+    version='1.8.0',
+    description="Python library to work with Etcd",
+    long_description=readme + '\n\n' + history,
+    author="TwinDB Development Team",
+    author_email='dev@twindb.com',
+    url='https://github.com/twindb/pyetcd',
+    packages=[
+        'pyetcd',
+    ],
+    package_dir={'pyetcd':
+                 'pyetcd'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    zip_safe=False,
+    keywords='pyetcd',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
