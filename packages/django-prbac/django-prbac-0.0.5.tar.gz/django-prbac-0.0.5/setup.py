8c966e201456f964f0e7ddd2fd246127603437f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:30 +0000

    Add package django-prbac/0.0.5/django-prbac-0.0.5.tar.gz

---
+++ packages/django-prbac/django-prbac-0.0.5.tar.gz/django-prbac-0.0.5/setup.py
@@ -0,0 +1,27 @@
+import os
+import re
+from setuptools import setup, find_packages
+
+def get_version():
+    version_re = re.compile(r"""^__version__ = (['"])(.*?)\1$""", re.M)
+    thisdir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(thisdir, "django_prbac/__init__.py")
+    with open(path) as fh:
+        return version_re.search(fh.read()).group(2)
+
+setup(
+    name='django-prbac',
+    version=get_version(),
+    description='Parameterized Role-Based Access Control for Django',
+    author='Dimagi',
+    author_email='dev@dimagi.com',
+    url='http://github.com/dimagi/django-prbac',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'django>=1.8,<2.0',
+        'jsonfield>=1.0.3',
+        'simplejson',
+        'six',
+    ],
+)
