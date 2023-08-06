0db7c74786089d4a48ec094cfe885fa9a94487b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:09 +0000

    Add package crawlib/0.0.8/crawlib-0.0.8.tar.gz

---
+++ packages/crawlib/crawlib-0.0.8.tar.gz/crawlib-0.0.8/crawlib/exc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Exceptions.
+"""
+
+from requests import Timeout as TimeoutError
+try:
+    from .spider.requests_spider import DownloadOversizeError
+    from .html_parser.parser import (
+        SoupError, CaptchaError, WrongHtmlError, ParseError, ServerSideError,
+    )
+except:  # pragma: no cover
+    from crawlib.spider.requests_spider import DownloadOversizeError
+    from crawlib.html_parser.parser import (
+        SoupError, CaptchaError, WrongHtmlError, ParseError, ServerSideError,
+    )
