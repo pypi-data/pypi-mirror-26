3a7f5ddfdd0fb4cd103eca7387c32e6dc3ea5c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:25 +0000

    Add package shorttext/0.5.7/shorttext-0.5.7.tar.gz

---
+++ packages/shorttext/shorttext-0.5.7.tar.gz/shorttext-0.5.7/shorttext/__init__.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+thisdir, _ = os.path.split(__file__)
+sys.path.append(thisdir)
+
+from . import utils
+from . import data
+from . import classifiers
+from . import generators
+from . import stack
+from .smartload import smartload_compact_model
+from . import metrics
