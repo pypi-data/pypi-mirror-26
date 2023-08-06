eaddbc36ddf612c01254e036d0cca841e9bdc00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:22 +0000

    Add package substance/0.15.0/substance-0.15.0.tar.gz

---
+++ packages/substance/substance-0.15.0.tar.gz/substance-0.15.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import platform
+
+with open('README.rst') as f:
+    readme = f.read()
+
+execfile('substance/_version.py')
+
+install_requires = ['setuptools>=1.1.3', 'PyYAML', 'tabulate', 'paramiko>=2.3.1', 'netaddr', 'requests', 'tinydb', 'python_hosts==0.3.3', 'jinja2']
+
+if 'Darwin' in platform.system():
+    install_requires.append('macfsevents')
+
+setup(name='substance',
+      version=__version__,
+      author='Turbulent',
+      author_email='oss@turbulent.ca',
+      url='https://substance.readthedocs.io/',
+      license='Apache License 2.0',
+      long_description=readme,
+      description='Substance - Local dockerized development environment',
+      install_requires=install_requires,
+      packages=find_packages(),
+      package_data={'substance': ['support/*']},
+      test_suite='tests',
+      zip_safe=False,
+      include_package_data=True,
+      entry_points={
+          'console_scripts': [
+              'substance = substance.cli:cli',
+              'subenv = substance.subenv.cli:cli'
+          ],
+      })
