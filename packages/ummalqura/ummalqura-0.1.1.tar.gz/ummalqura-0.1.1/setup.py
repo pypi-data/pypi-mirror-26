2d45d7e23d0da594a47e5f01fe76c85d762efc4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:51 +0000

    Add package ummalqura/0.1.1/ummalqura-0.1.1.tar.gz

---
+++ packages/ummalqura/ummalqura-0.1.1.tar.gz/ummalqura-0.1.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='ummalqura',
+      version='0.1.1',
+      description='Python Hijri Umalqurra',
+      url='https://github.com/borni-dhifi/ummalqura',
+      author='Borni DHIFI',
+      author_email='dhifi.borni@gmail.com',
+      keywords=['ummalqura'],
+      license='Waqef',
+      packages=['ummalqura'],
+      zip_safe=False)
