39355030147702b1615f15c918d0227a9c87ea71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:14 +0000

    Add package TBWW/0.1.5/TBWW-0.1.5.tar.gz

---
+++ packages/TBWW/TBWW-0.1.5.tar.gz/TBWW-0.1.5/tbww.py
@@ -0,0 +1,35 @@
+##TBWW, based on python-telegram-bot
+
+import os
+from telegram.ext import Updater, CommandHandler
+
+class Bot(object):
+    def __init__(self,TOKEN,IP="0.0.0.0",PORT=None):
+        """Leave PORT as None to autodetect for Heroku"""
+        #set up basics and webserver config
+        self.TOKEN = TOKEN
+        self.IP = IP
+        if PORT == None:
+            PORT = int(os.environ.get("PORT","5000"))
+        self.PORT = PORT
+
+        #Most important bit!
+        self.updater = Updater(token=TOKEN)
+        self.dispatcher = self.updater.dispatcher # Shortcut
+
+    def start_webhook(self,host):
+        """Host should be heroku app domain if on heroku"""
+        self.updater.start_webhook(listen=self.IP,
+                                   port=self.PORT,
+                                   url_path=self.TOKEN)
+        self.updater.bot.set_webhook(host+self.TOKEN)
+        self.updater.idle()
+
+    def command(self,name,pass_args=False):
+        def decorator(function):
+            def wrapper():
+                return function
+            self.dispatcher.add_handler(CommandHandler(name,function,pass_args=pass_args))
+            
+            return wrapper()
+        return decorator
