324aeba7a670efbe46277a033b433d8187ec061a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:37 +0000

    Add package ahttp/1.0.1/ahttp-1.0.1.tar.gz

---
+++ packages/ahttp/ahttp-1.0.1.tar.gz/ahttp-1.0.1/setup.py
@@ -0,0 +1,24 @@
+﻿#/usr/bin/env python3
+#-*- coding: utf-8 -*-
+from setuptools import setup, find_packages, os
+
+setup(
+	name='ahttp',
+	version='1.0.1',
+	description='Based on aiohttp and asyncio requests',
+    long_description='The Instructions of ahttp at https://github.com/web-trump/ahttp.git',
+	install_requires=['aiohttp>=2.0.0', 'cchardet>=2.0.0', ('uvloop' if os.name is not 'nt' else '')  ] ,
+	author='LiShenGang',
+	author_email='cszy2013@163.com',
+	license='BSD License',
+	py_modules= ['ahttp'] ,
+	platforms=["python 3.5+"],
+	url='https://github.com/web-trump/ahttp.git',
+	classifiers=[
+		'Development Status :: 6 - Mature',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python :: 3.5',
+		],
+)
