99d7e9339c23db44da21ee7e85b6efd1fef7b94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:29 +0000

    Add package wellcome-aws-utils/0.0.1/wellcome_aws_utils-0.0.1.tar.gz

---
+++ packages/wellcome-aws-utils/wellcome_aws_utils-0.0.1.tar.gz/wellcome_aws_utils-0.0.1/src/wellcome_aws_utils/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+
+from wellcome_aws_utils.version import __version_info__, __version__
+
+
+def hello():
+    print('Hello world!')
+
+
+__all__ = ['__version_info__', '__version__', 'hello']
