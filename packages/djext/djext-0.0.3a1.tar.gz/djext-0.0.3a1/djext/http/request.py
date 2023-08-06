737443d8dded7e28a1c23f5bc4ecef93289e117f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:18 +0000

    Add package djext/0.0.3a1/djext-0.0.3a1.tar.gz

---
+++ packages/djext/djext-0.0.3a1.tar.gz/djext-0.0.3a1/djext/http/request.py
@@ -0,0 +1,8 @@
+from django.core.handlers.wsgi import WSGIRequest
+
+
+class ExtendedWSGIRequest(WSGIRequest):
+    """
+    This is a placeholder for an extended Request object
+    """
+    pass
---
+++ packages/djext/djext-0.0.3a1.tar.gz/djext-0.0.3a1/setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='djext',
+    version='0.0.3-a1',
+    description='Quick tools for Django',
+    author='Yehuda Deutsch',
+    author_email='yeh@uda.co.il',
+
+    license='MIT',
+    url='https://gitlab.com/uda/djext',
+    keywords='django http wsgi quick tools models forms views templates templatetags',
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+    ],
+    extras_require={
+        'dev': ['django'],
+        'test': ['coverage'],
+    },
+    python_requires='>=3.5',
+)
