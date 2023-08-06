37c561046f38b663218f54e9a0118f1ec5b97a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:48 +0000

    Add package xlsxcompare/0.0.1/xlsxcompare-0.0.1.tar.gz

---
+++ packages/xlsxcompare/xlsxcompare-0.0.1.tar.gz/xlsxcompare-0.0.1/README.rst
@@ -0,0 +1,4 @@
+# xlsx_compare
+Forked from XLSXwriter
+
+A package that extracts some test helper functions from XLSXwriter for external use. The script provies the ability to compare the equivalency of two Excel documents.
---
+++ packages/xlsxcompare/xlsxcompare-0.0.1.tar.gz/xlsxcompare-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xlsxcompare',
+    version='0.0.1',
+    author='Bella Woo',
+    author_email='bella@dharmaplatform.com',
+    license='BSD',
+    description='A Python script to compare the equivalency of two Excel documents',
+    long_description=open('README.rst').read(),
+    py_modules=['xlsx_compare']
+)
