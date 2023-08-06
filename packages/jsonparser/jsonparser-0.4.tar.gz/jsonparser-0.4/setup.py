9af4f3544e258a7b5be1e9c23150e806447a4992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:39 +0000

    Add package jsonparser/0.4/jsonparser-0.4.tar.gz

---
+++ packages/jsonparser/jsonparser-0.4.tar.gz/jsonparser-0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'jsonparser',
+  packages = find_packages(),
+  version = '0.4',
+  description = 'A simple JSON file parser',
+  author = 'David Curran',
+  author_email = 'david.curran3@gmail.com',
+  url = 'https://github.com/schizoid90/jsonparser',
+  keywords = ['json', 'parser', 'jsonparser'],
+  classifiers = [],
+  license = 'MIT',
+)
