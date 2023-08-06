abaa2525fdacf1628ffdf0db9e9d486c3e483439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:14 +0000

    Add package gitcd/1.6.12/gitcd-1.6.12-py3-none-any.whl

---
+++ packages/gitcd/gitcd-1.6.12-py3-none-any.whl/gitcd/Cli/Command.py
@@ -0,0 +1,46 @@
+import subprocess
+import string
+from gitcd.Cli.Interface import Interface
+from gitcd.Exceptions import GitcdCliExecutionException
+
+
+class Command(object):
+
+    raiseException = False
+    verbose = False
+    interface = Interface()
+
+    def setVerbose(self, verbose: bool):
+        self.verbose = verbose
+
+    def getVerbose(self):
+        return self.verbose
+
+    def setRaiseException(self, raiseException: bool):
+        self.raiseException = raiseException
+
+    def getRaiseException(self):
+        return self.raiseException
+
+    def execute(self, command: str):
+        if self.verbose is True:
+            self.interface.warning("Executing: %s" % command)
+
+        process = subprocess.Popen(
+            command,
+            shell=True,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        output, err = process.communicate()
+        if process.returncode != 0:
+            if self.raiseException is True:
+                raise GitcdCliExecutionException(err.decode("utf-8").strip())
+            return False
+
+        output = output.decode("utf-8").strip()
+        if self.verbose is True:
+            self.interface.writeOut(output)
+
+        return output
---
+++ packages/gitcd/gitcd-1.6.12-py3-none-any.whl/gitcd/Config/FilePersonal.py
@@ -0,0 +1,53 @@
+import os
+import yaml
+from gitcd.Config.Parser import Parser
+from gitcd.Config.DefaultsPersonal import DefaultsPersonal
+
+
+class FilePersonal:
+
+    loaded = False
+    filename = ".gitcd-personal"
+    parser = Parser()
+    defaults = DefaultsPersonal()
+    config = {}
+
+    def setFilename(self, configFilename: str):
+        self.filename = configFilename
+
+    def load(self):
+        defaultConfig = self.defaults.load()
+        if not os.path.isfile(self.filename):
+            self.config = defaultConfig
+        else:
+            config = self.parser.load(self.filename)
+            for key in defaultConfig.keys():
+                if key in config:
+                    self.config[key] = config[key]
+                else:
+                    self.config[key] = defaultConfig[key]
+
+    def write(self):
+        self.parser.write(self.filename, self.config)
+
+        # add .gitcd-personal to .gitignore
+        gitignore = ".gitignore"
+        if not os.path.isfile(gitignore):
+            gitignoreContent = self.filename
+        else:
+            with open(gitignore, "r") as gitignoreFile:
+                gitignoreContent = gitignoreFile.read()
+            # if not yet in gitignore
+            if "%s" % (self.filename) not in gitignoreContent:
+                # add it
+                gitignoreContent = "%s\n%s\n" % (
+                    gitignoreContent, self.filename)
+
+        with open(gitignore, "w") as gitignoreFile:
+            gitignoreFile.write(gitignoreContent)
+
+    def getToken(self):
+        return self.config['token']
+
+    def setToken(self, token):
+        self.config['token'] = token
