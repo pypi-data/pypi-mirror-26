1219f32f9f0c3fd2f76c29200b7e5ce569040d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:01 +0000

    Add package info-generators/0.0.6/info-generators-0.0.6.tar.gz

---
+++ packages/info-generators/info-generators-0.0.6.tar.gz/info-generators-0.0.6/info_generators/area_generator.py
@@ -0,0 +1,29 @@
+import random
+import os
+
+from .gabc_generator import Generator
+
+
+class AreaGenerator(Generator):
+    @staticmethod
+    def get():
+        codes_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'consts/codes.txt')
+        cities_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'consts/city.txt')
+
+        codes = open(codes_path, encoding='utf-8').readlines()
+        cities = open(cities_path, encoding='utf-8').readlines()
+        city = random.choice(cities)
+        city = city.split(',')
+
+        def get_code():
+            for item in codes:
+                code = item.split(',')[0]
+                name = item.split(',')[1]
+
+                if city[0].startswith(code):
+                    return (code, name)
+            return None
+
+        code = get_code()
+
+        return (city[0], '{}{}'.format(code[1], city[1]).strip())
