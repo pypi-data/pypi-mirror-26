0827dad3b6b2a5a5e9745b3d03eb82f4141e2b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:31 +0000

    Add package catacomb/0.1.2/catacomb-0.1.2.tar.gz

---
+++ packages/catacomb/catacomb-0.1.2.tar.gz/catacomb-0.1.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "catacomb",
+    version = "0.1.2",
+
+    py_modules = [
+        "catacomb",
+    ],
+
+    packages = find_packages(),
+
+    # Dependencies
+    install_requires = [
+        "click",
+        "terminaltables",
+    ],
+
+    # Script execution
+    entry_points = {
+        "console_scripts": [
+            "tomb = catacomb.catacomb:tomb",
+        ]
+    },
+
+    # Metadata
+    author = "Mitchell Jeitani",
+    author_email = "mitchelljeitani@hotmail.com",
+    description = "A minimalistic CLI tool for storing shell commands.",
+    license = "MIT",
+    keywords = "command-line shell productivity storage",
+    url = "https://github.com/mitchjeitani/catacomb"
+)
