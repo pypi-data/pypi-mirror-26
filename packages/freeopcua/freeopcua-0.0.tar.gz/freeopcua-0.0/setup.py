828cccf9f4e528776ceabae82792cbdc4638fa39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:41 +0000

    Add package freeopcua/0.0/freeopcua-0.0.tar.gz

---
+++ packages/freeopcua/freeopcua-0.0.tar.gz/freeopcua-0.0/setup.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Olivier Roulet-Dubonnet',
+    author_email='olivier.roulet@gmail.com',
+    description='Pure Python OPC-UA client and server library',
+    install_requires=['opcua'],
+    long_description='''Use `opcua <https://pypi.python.org/pypi/opcua/>`_ instead.''',
+    maintainer='Olivier Roulet-Dubonnet',
+    maintainer_email='olivier.roulet@gmail.com',
+    name='freeopcua',
+    platforms=['all'],
+    py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+    url='https://pypi.python.org/pypi/opcua/',
+    version="0.0",
+    zip_safe=False,
+)
