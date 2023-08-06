482d91c858cd1d5c444d8d1426b89b20e2baa73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:17 +0000

    Add package CodeChat/1.5.8/CodeChat-1.5.8.tar.gz

---
+++ packages/CodeChat/CodeChat-1.5.8.tar.gz/CodeChat-1.5.8/CodeChat/__init__.py
@@ -0,0 +1,27 @@
+# .. Copyright (C) 2012-2017 Bryan A. Jones.
+#
+#    This file is part of CodeChat.
+#
+#    CodeChat is free software: you can redistribute it and/or modify it under
+#    the terms of the GNU General Public License as published by the Free
+#    Software Foundation, either version 3 of the License, or (at your option)
+#    any later version.
+#
+#    CodeChat is distributed in the hope that it will be useful, but WITHOUT ANY
+#    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#    FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+#    details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with CodeChat.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ******************************
+# __init__.py - CodeChat Package
+# ******************************
+# This file defines this directory as a Python package, which contains the
+# CodeChat application.
+#
+# Give the version number, which is read by `../setup.py` during packaging.
+# This is chosen following the `version convention
+# <http://packaging.python.org/en/latest/tutorial.html#version>`_.
+__version__ = '1.5.8'
