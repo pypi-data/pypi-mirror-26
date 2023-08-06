024fc0153e4d3f55a6a8e83bdbed75a9778e3454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:07 +0000

    Add package stem/1.6.0/stem-1.6.0.tar.gz

---
+++ packages/stem/stem-1.6.0.tar.gz/stem-1.6.0/setup.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# Copyright 2012-2016, Damian Johnson and The Tor Project
+# See LICENSE for licensing information
+#
+# Release Checklist
+# =================
+#
+# * Recache latest information (cache_manual.py and cache_fallback_directories.py)
+#
+# * Tag the release
+#   |- Bump stem's version (in stem/__init__.py and docs/index.rst).
+#   |- git commit -a -m "Stem release 1.0.0"
+#   |- git tag -u 9ABBEEC6 -m "stem release 1.0.0" 1.0.0 d0bb81a
+#   +- git push --tags
+#
+# * Dry-run release on https://pypi.python.org/pypi/stem/
+#   |- python setup.py sdist --dryrun
+#   |- gpg --detach-sig --armor dist/stem-dry-run-1.0.0.tar.gz
+#   |- twine upload dist/*
+#   +- Check that https://pypi.python.org/pypi/stem-dry-run/ looks correct, comparing it to https://pypi.python.org/pypi/stem/
+#
+# * Final release
+#   |- rm dist/*
+#   |- python setup.py sdist
+#   |- gpg --detach-sig --armor dist/stem-1.0.0.tar.gz
+#   +- twine upload dist/*
+#
+# * Contact package maintainers
+# * Announce the release (example: https://blog.torproject.org/blog/stem-release-11)
+
+import distutils.core
+import os
+import sys
+import stem
+
+if '--dryrun' in sys.argv:
+  DRY_RUN = True
+  sys.argv.remove('--dryrun')
+else:
+  DRY_RUN = False
+
+SUMMARY = 'Stem is a Python controller library that allows applications to interact with Tor (https://www.torproject.org/).'
+DRY_RUN_SUMMARY = 'Ignore this package. This is dry-run release creation to work around PyPI limitations (https://github.com/pypa/packaging-problems/issues/74#issuecomment-260716129).'
+
+DESCRIPTION = """
+For tutorials and API documentation see `Stem's homepage <https://stem.torproject.org/>`_.
+
+Quick Start
+-----------
+
+To install you can either use...
+
+::
+
+  pip install stem
+
+... or install from the source tarball. Stem supports both the python 2.x and 3.x series. To use its python3 counterpart you simply need to install using that version of python.
+
+::
+
+  python3 setup.py install
+
+After that, give some `tutorials <https://stem.torproject.org/tutorials.html>`_ a try! For questions or to discuss project ideas we're available on `irc <https://www.torproject.org/about/contact.html.en#irc>`_ and the `tor-dev@ email list <https://lists.torproject.org/cgi-bin/mailman/listinfo/tor-dev>`_.
+""".strip()
+
+MANIFEST = """
+include cache_fallback_directories.py
+include cache_manual.py
+include LICENSE
+include MANIFEST.in
+include requirements.txt
+include run_tests.py
+include tox.ini
+graft docs
+graft test
+global-exclude __pycache__
+global-exclude *.orig
+global-exclude *.pyc
+global-exclude *.swp
+global-exclude *.swo
+global-exclude .tox
+global-exclude *~
+recursive-exclude test/data *
+recursive-exclude docs/_build *
+""".strip()
+
+# installation requires us to be in our setup.py's directory
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+with open('MANIFEST.in', 'w') as manifest_file:
+  manifest_file.write(MANIFEST)
+
+try:
+  distutils.core.setup(
+    name = 'stem-dry-run' if DRY_RUN else 'stem',
+    version = stem.__version__,
+    description = DRY_RUN_SUMMARY if DRY_RUN else SUMMARY,
+    long_description = DESCRIPTION,
+    license = stem.__license__,
+    author = stem.__author__,
+    author_email = stem.__contact__,
+    url = stem.__url__,
+    packages = ['stem', 'stem.descriptor', 'stem.interpreter', 'stem.response', 'stem.util'],
+    keywords = 'tor onion controller',
+    scripts = ['tor-prompt'],
+    package_data = {
+      'stem': ['cached_tor_manual.sqlite', 'settings.cfg'],
+      'stem.descriptor': ['fallback_directories.cfg'],
+      'stem.interpreter': ['settings.cfg'],
+      'stem.util': ['ports.cfg'],
+    }, classifiers = [
+      'Development Status :: 5 - Production/Stable',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+      'Topic :: Security',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+  )
+finally:
+  if os.path.exists('MANIFEST.in'):
+    os.remove('MANIFEST.in')
+
+  if os.path.exists('MANIFEST'):
+    os.remove('MANIFEST')
---
+++ packages/stem/stem-1.6.0.tar.gz/stem-1.6.0/test/integ/descriptor/remote.py
@@ -0,0 +1,264 @@
+"""
+Integration tests for stem.descriptor.remote.
+"""
+
+import unittest
+
+import stem.descriptor
+import stem.descriptor.extrainfo_descriptor
+import stem.descriptor.networkstatus
+import stem.descriptor.remote
+import stem.descriptor.router_status_entry
+import stem.descriptor.server_descriptor
+import test.require
+
+
+class TestDescriptorDownloader(unittest.TestCase):
+  @test.require.only_run_once
+  @test.require.online
+  def test_shorthand_aliases(self):
+    """
+    Quick sanity test that we can call our shorthand aliases for getting
+    descriptors.
+    """
+
+    desc = list(stem.descriptor.remote.get_server_descriptors('9695DFC35FFEB861329B9F1AB04C46397020CE31').run())[0]
+    self.assertEqual('moria1', desc.nickname)
+
+    desc = list(stem.descriptor.remote.get_extrainfo_descriptors('9695DFC35FFEB861329B9F1AB04C46397020CE31').run())[0]
+    self.assertEqual('moria1', desc.nickname)
+
+    consensus = list(stem.descriptor.remote.get_consensus())
+    self.assertTrue(len(consensus) > 50)
+
+  @test.require.only_run_once
+  @test.require.online
+  def test_authorities_are_up_to_date(self):
+    """
+    Check that our hardcoded directory authority data matches the present
+    consensus.
+    """
+
+    downloader = stem.descriptor.remote.DescriptorDownloader()
+    consensus = downloader.get_consensus(document_handler = stem.descriptor.DocumentHandler.BARE_DOCUMENT).run()[0]
+
+    for auth in consensus.directory_authorities:
+      if auth.nickname == 'dannenberg-legacy':
+        continue  # skip due to https://trac.torproject.org/projects/tor/ticket/17906
+
+      stem_auth = stem.descriptor.remote.get_authorities().get(auth.nickname)
+
+      if not stem_auth:
+        self.fail("%s isn't a recognized directory authority in stem" % auth.nickname)
+
+      for attr in ('v3ident', 'or_port', 'dir_port'):
+        if auth.nickname == 'moria1' and attr == 'address':
+          continue  # skip due to https://trac.torproject.org/projects/tor/ticket/14955
+
+        if getattr(auth, attr) != getattr(stem_auth, attr):
+          self.fail('%s has %s %s, but we expected %s' % (auth.nickname, attr, getattr(auth, attr), getattr(stem_auth, attr)))
+
+  @test.require.only_run_once
+  @test.require.online
+  def test_using_authorities(self):
+    """
+    Fetches a descriptor from each of the directory authorities. This is
+    intended to check that DIRECTORY_AUTHORITIES is still up to date (that
+    addresses and ports haven't changed).
+
+    This is hardcoded to fetch moria1's descriptor. If its fingerprint changes
+    then this test will need to be updated.
+    """
+
+    queries = []
+
+    for nickname, authority in stem.descriptor.remote.get_authorities().items():
+      queries.append((stem.descriptor.remote.Query(
+        '/tor/server/fp/9695DFC35FFEB861329B9F1AB04C46397020CE31',
+        'server-descriptor 1.0',
+        endpoints = [(authority.address, authority.dir_port)],
+        timeout = 30,
+        validate = True,
+      ), authority))
+
+    for query, authority in queries:
+      try:
+        descriptors = list(query.run())
+      except Exception as exc:
+        self.fail('Unable to use %s (%s:%s, %s): %s' % (authority.nickname, authority.address, authority.dir_port, type(exc), exc))
+
+      self.assertEqual(1, len(descriptors))
+      self.assertEqual('moria1', descriptors[0].nickname)
+
+  @test.require.only_run_once
+  @test.require.online
+  def test_use_directory_mirrors(self):
+    """
+    Checks that we can fetch and use a list of directory mirrors.
+    """
+
+    downloader = stem.descriptor.remote.DescriptorDownloader()
+    downloader.use_directory_mirrors()
+    self.assertTrue(len(downloader._endpoints) > 50)
+
+  @test.require.only_run_once
+  @test.require.online
+  def test_get_server_descriptors(self):
+    """
+    Exercises the downloader's get_server_descriptors() method.
+    """
+
+    downloader = stem.descriptor.remote.DescriptorDownloader(validate = True)
+
+    # Fetch a single descriptor and a batch. I'd love to also exercise
+    # retrieving all descriptors, but that adds roughly a minute to the runtime
+    # of this test and adds an appreciable load to directory authorities.
+
+    single_query = downloader.get_server_descriptors('9695DFC35FFEB861329B9F1AB04C46397020CE31')
+
+    multiple_query = downloader.get_server_descriptors([
+      '9695DFC35FFEB861329B9F1AB04C46397020CE31',
+      '847B1F850344D7876491A54892F904934E4EB85D',
+    ])
+
+    # Explicitly running the queries so they'll provide a useful error if
+    # unsuccessful.
+
+    single_query.run()
+    multiple_query.run()
+
+    single_query_results = list(single_query)
+    self.assertEqual(1, len(single_query_results))
+    self.assertEqual('moria1', single_query_results[0].nickname)
+    self.assertTrue(isinstance(single_query_results[0], stem.descriptor.stem.descriptor.server_descriptor.ServerDescriptor))
+
+    self.assertEqual(2, len(list(multiple_query)))
+
+  @test.require.only_run_once
+  @test.require.online
+  def test_get_extrainfo_descriptors(self):
+    """
+    Exercises the downloader's get_extrainfo_descriptors() method.
+    """
+
+    downloader = stem.descriptor.remote.DescriptorDownloader(validate = True)
+
+    single_query = downloader.get_extrainfo_descriptors('9695DFC35FFEB861329B9F1AB04C46397020CE31')
+
+    multiple_query = downloader.get_extrainfo_descriptors([
+      '9695DFC35FFEB861329B9F1AB04C46397020CE31',
+      '847B1F850344D7876491A54892F904934E4EB85D',
+    ])
+
+    single_query.run()
+    multiple_query.run()
+
+    single_query_results = list(single_query)
+    self.assertEqual(1, len(single_query_results))
+    self.assertEqual('moria1', single_query_results[0].nickname)
+    self.assertTrue(isinstance(single_query_results[0], stem.descriptor.extrainfo_descriptor.ExtraInfoDescriptor))
+
+    self.assertEqual(2, len(list(multiple_query)))
+
+  @test.require.only_run_once
+  @test.require.online
+  def test_get_consensus(self):
+    """
+    Exercises the downloader's get_consensus() method.
+    """
+
+    downloader = stem.descriptor.remote.DescriptorDownloader(validate = True)
+
+    consensus_query = downloader.get_consensus()
+    consensus_query.run()
+
+    consensus = list(consensus_query)
+    self.assertTrue(len(consensus) > 50)
+    self.assertTrue(isinstance(consensus[0], stem.descriptor.router_status_entry.RouterStatusEntryV3))
+
+  @test.require.only_run_once
+  @test.require.online
+  def test_get_microdescriptor_consensus(self):
+    """
+    Exercises the downloader's get_consensus() method for fetching a
+    microdescriptor consensus.
+    """
+
+    downloader = stem.descriptor.remote.DescriptorDownloader(validate = True)
+
+    consensus_query = downloader.get_consensus(microdescriptor = True)
+    consensus_query.run()
+
+    consensus = list(consensus_query)
+    self.assertTrue(len(consensus) > 50)
+    self.assertTrue(isinstance(consensus[0], stem.descriptor.router_status_entry.RouterStatusEntryMicroV3))
+
+  @test.require.only_run_once
+  @test.require.online
+  def test_get_key_certificates(self):
+    """
+    Exercises the downloader's get_key_certificates() method.
+    """
+
+    downloader = stem.descriptor.remote.DescriptorDownloader(validate = True)
+
+    single_query = downloader.get_key_certificates('D586D18309DED4CD6D57C18FDB97EFA96D330566')
+
+    multiple_query = downloader.get_key_certificates([
+      'D586D18309DED4CD6D57C18FDB97EFA96D330566',
+      '14C131DFC5C6F93646BE72FA1401C02A8DF2E8B4',
+    ])
+
+    single_query.run()
+    multiple_query.run()
+
+    single_query_results = list(single_query)
+    self.assertEqual(1, len(single_query_results))
+    self.assertEqual('D586D18309DED4CD6D57C18FDB97EFA96D330566', single_query_results[0].fingerprint)
+    self.assertTrue(isinstance(single_query_results[0], stem.descriptor.networkstatus.KeyCertificate))
+
+    self.assertEqual(2, len(list(multiple_query)))
+
+  @test.require.online
+  def test_that_cache_is_up_to_date(self):
+    """
+    Check if the cached fallback directories bundled with Stem are up to date
+    or not.
+    """
+
+    cached_fallback_directories = stem.descriptor.remote.FallbackDirectory.from_cache()
+    latest_fallback_directories = stem.descriptor.remote.FallbackDirectory.from_remote()
+
+    if cached_fallback_directories != latest_fallback_directories:
+      self.fail("Stem's cached fallback directories are out of date. Please run 'cache_fallback_directories.py'...\n\n%s" % stem.descriptor.remote._fallback_directory_differences(cached_fallback_directories, latest_fallback_directories))
+
+  @test.require.online
+  def test_fallback_directory_reachability(self):
+    """
+    Fetch information from each fallback directory to confirm that it's
+    available.
+    """
+
+    # Don't run this test by default. Once upon a time it was fine, but tor has
+    # added so many fallbacks now that this takes a looong time. :(
+
+    self.skipTest('(skipped by default)')
+    return
+
+    unsuccessful = {}
+    downloader = stem.descriptor.remote.DescriptorDownloader()
+    moria1_v3ident = stem.descriptor.remote.get_authorities()['moria1'].v3ident
+
+    for fallback_directory in stem.descriptor.remote.FallbackDirectory.from_cache().values():
+      try:
+        downloader.get_key_certificates(authority_v3idents = moria1_v3ident, endpoints = [(fallback_directory.address, fallback_directory.dir_port)]).run()
+      except Exception as exc:
+        unsuccessful[fallback_directory] = exc
+
+    if unsuccessful:
+      lines = ['We were unable to contact the following fallback directories...\n']
+
+      for fallback_directory, exc in unsuccessful.items():
+        lines.append('* %s:%s (%s): %s' % (fallback_directory.address, fallback_directory.dir_port, fallback_directory.fingerprint, exc))
+
+      self.fail('\n'.join(lines))
