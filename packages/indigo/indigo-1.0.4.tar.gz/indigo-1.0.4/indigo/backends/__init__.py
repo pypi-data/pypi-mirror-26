20907f17686d2080a23b579a74d377af3ea71b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:44 +0000

    Add package indigo/1.0.4/indigo-1.0.4.tar.gz

---
+++ packages/indigo/indigo-1.0.4.tar.gz/indigo-1.0.4/indigo/backends/__init__.py
@@ -0,0 +1,60 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+def available_backends():
+    backends = []
+
+    try:
+        from indigo.backends.np import NumpyBackend
+        backends.append( NumpyBackend )
+    except Exception as e:
+        log.warn("couldn't find NUMPY backend")
+
+    try:
+        from indigo.backends.mkl import MklBackend
+        backends.append( MklBackend )
+    except Exception as e:
+        log.warn("couldn't find MKL backend")
+
+    try:
+        from indigo.backends.cuda import CudaBackend
+        backends.append( CudaBackend )
+    except Exception as e:
+        log.warn("couldn't find CUDA backend")
+
+    try:
+        from indigo.backends.customcpu import CustomCpuBackend
+        backends.append( CustomCpuBackend )
+    except Exception as e:
+        log.warn("couldn't find CustomCpu backend")
+
+    try:
+        from indigo.backends.customgpu import CustomGpuBackend
+        backends.append( CustomGpuBackend )
+    except Exception as e:
+        log.warn("couldn't find CustomGpu backend")
+
+    return backends
+
+def get_backend(name, **init):
+    """
+    Instantiates the requested backend.
+    """
+    if name == 'mkl':
+        from indigo.backends.mkl  import MklBackend
+        return MklBackend(**init)
+    elif name == 'cuda':
+        from indigo.backends.cuda import CudaBackend
+        return CudaBackend(**init)
+    elif name == 'numpy':
+        from indigo.backends.np   import NumpyBackend
+        return NumpyBackend(**init)
+    elif name == 'customcpu':
+        from indigo.backends.customcpu import CustomCpuBackend
+        return CustomCpuBackend(**init)
+    elif name == 'customgpu':
+        from indigo.backends.customgpu import CustomGpuBackend
+        return CustomGpuBackend(**init)
+    else:
+        raise ValueError("unrecognized backend: %s" % name)
