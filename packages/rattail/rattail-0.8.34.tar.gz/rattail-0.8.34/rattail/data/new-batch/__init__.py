eac21988ff18f1f0fe16284a9a0e74d7ef3c423c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:50 +0000

    Add package rattail/0.8.34/rattail-0.8.34.tar.gz

---
+++ packages/rattail/rattail-0.8.34.tar.gz/rattail-0.8.34/rattail/data/new-batch/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8; -*-
+# -*- coding: utf-8; -*-
+"""
+${model_title} batches
+"""
+
+from __future__ import unicode_literals, absolute_import
+
+from .model import ${model_name}, ${model_name}Row
