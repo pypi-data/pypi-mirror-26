705d77121c18d325e392b6f067a0fe04302e47c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:10 +0000

    Add package LMSQuery/0.4.0/LMSQuery-0.4.0.tar.gz

---
+++ packages/LMSQuery/LMSQuery-0.4.0.tar.gz/LMSQuery-0.4.0/setup.py
@@ -0,0 +1,31 @@
+from lmsquery import const
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="LMSQuery",
+    version=const.LMSQUERY_VERSION,
+    author="Robert Einhaus",
+    author_email="robert@einhaus.info",
+    description=("Query library for Logitech Media Server"),
+    license="MIT",
+    keywords="logitech media server lms",
+    url="https://github.com/roberteinhaus/lmsquery",
+    packages=['lmsquery'],
+    long_description="This library provides easy to use functions to send \
+                      queries to a Logitech Media Server \
+                      (https://github.com/Logitech/slimserver)",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
