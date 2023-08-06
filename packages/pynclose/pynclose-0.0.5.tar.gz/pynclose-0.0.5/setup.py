086155e132d07337edf48825e97de299c40e60ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:57 +0000

    Add package pynclose/0.0.5/pynclose-0.0.5.tar.gz

---
+++ packages/pynclose/pynclose-0.0.5.tar.gz/pynclose-0.0.5/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+import glob
+from setuptools import setup, Extension
+# from pynclose import gen_finclose, gen_inclose
+from inclose_package import gen_inclose
+
+
+inclose = Extension('inclose',
+                    sources=[*glob.glob('inclose_package/*.cpp')],
+                    include_dirs=['inclose_package/'],
+                    extra_compile_args=['-fopenmp', '-O2'],
+                    extra_link_args=['-fopenmp', '-O2', '-Wl,-z,defs', '-flto'],
+                    language='c++')
+
+setup(ext_modules=[inclose], packages=['pynclose'])
