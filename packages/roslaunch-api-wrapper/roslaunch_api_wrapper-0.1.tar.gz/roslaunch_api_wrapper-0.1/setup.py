f74335f671262493ec17c7219329cac785c5712e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:08 +0000

    Add package roslaunch-api-wrapper/0.1/roslaunch_api_wrapper-0.1.tar.gz

---
+++ packages/roslaunch-api-wrapper/roslaunch_api_wrapper-0.1.tar.gz/roslaunch_api_wrapper-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='roslaunch_api_wrapper',
+      version='0.1',
+      description='wrapper for rosalunch python api',
+      url='https://github.com/ACarfi/roslaunch_api_wrapper',
+      author='Alessandro Carfi',
+      author_email='carfi.alessandro@gmail.com',
+      license='MIT',
+      packages=['roslaunch_api_wrapper'],
+      zip_safe=False)
