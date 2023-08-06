b6567182d9d8ca5c2211a88573d1a886ebff4880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:08 +0000

    Add package followthemoney/0.2.3/followthemoney-0.2.3-py2-none-any.whl

---
+++ packages/followthemoney/followthemoney-0.2.3-py2-none-any.whl/followthemoney/util.py
@@ -0,0 +1,31 @@
+import six
+from normality import stringify
+from normality.cleaning import remove_control_chars
+from banal import clean_dict, unique_list, is_sequence
+
+
+def key_bytes(key):
+    """Convert the given data to a value appropriate for hashing."""
+    key = stringify(key)
+    if key is None:
+        return six.binary_type()
+    key = remove_control_chars(key)
+    return key.encode('utf-8')
+
+
+def merge_data(old, new):
+    """Exend the values of the new doc with extra values from the old."""
+    if old is None or new is None:
+        return old or new
+    old = dict(clean_dict(old))
+    new = dict(clean_dict(new))
+    for k, v in new.items():
+        if k in old:
+            if is_sequence(v):
+                v = old[k] + v
+                old[k] = unique_list(v)
+            elif isinstance(v, dict):
+                old[k] = merge_data(v, old[k])
+        else:
+            old[k] = v
+    return old
