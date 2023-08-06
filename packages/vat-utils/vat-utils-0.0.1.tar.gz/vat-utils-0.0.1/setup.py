deaf41b450edbf75d1ddd1a12abc7a87ee964b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:13 +0000

    Add package vat-utils/0.0.1/vat-utils-0.0.1.tar.gz

---
+++ packages/vat-utils/vat-utils-0.0.1.tar.gz/vat-utils-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='vat-utils',
+    version='0.0.1',
+    packages=['vat_utils'],
+    install_requires=['boto3', 'jsonpointer'],
+    author='VAT Dev',
+    author_email='vatdev@invalid.in'
+)
