6862b15f50a36d6a3aa5a38c415a000707bcb2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:38 +0000

    Add package dopplerr/0.5.9/dopplerr-0.5.9-py2.py3-none-any.whl

---
+++ packages/dopplerr/dopplerr-0.5.9-py2.py3-none-any.whl/dopplerr/notifications/_base.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+# Standard Libraries
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class _NotificationBase(object):
+
+    NOTIFICATION_TYPES = ["fetched"]
+
+    def __init__(self, registered_notification):
+        self._registered_notification = registered_notification
+
+    def can_emit_notification_type(self, requested_notif_type):
+        return requested_notif_type in self._registered_notification
