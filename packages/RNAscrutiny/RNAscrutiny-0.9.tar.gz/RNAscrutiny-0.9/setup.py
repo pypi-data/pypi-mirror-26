4d864001a745b8779878d4bcc635399c5cbeea64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:55 +0000

    Add package RNAscrutiny/0.9/RNAscrutiny-0.9.tar.gz

---
+++ packages/RNAscrutiny/RNAscrutiny-0.9.tar.gz/RNAscrutiny-0.9/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='RNAscrutiny',
+    version='0.9',
+    description='Generative models from scRNA-seq data.',
+    keywords='generative single cell RNA sequencing transcriptomics genomics',
+    url='http://lbm.niddk.nih.gov/mckennajp/RNAscrutiny',
+    author='Steffen K Cromwell, Joseph P McKena, Vipul Periwal',
+    author_email='joepatmckenna@gmail.com',
+    license='MIT',
+    packages=['RNAscrutiny'],
+    install_requires=['numpy', 'matplotlib', 'sklearn'],
+    include_package_data=True,
+    zip_safe=False)
