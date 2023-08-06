f433057c70e742ecbe8c5113ee6e71c6e8a3f126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:19 +0000

    Add package pybob/0.13.post1/pybob-0.13.post1.tar.gz

---
+++ packages/pybob/pybob-0.13.post1.tar.gz/pybob-0.13.post1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='pybob',
+      version='0.13-1',
+      description='Collection of geospatial and other tools I find useful.',
+      url='http://github.com/iamdonovan/pybob',
+      author='Bob McNabb',
+      author_email='robertmcnabb@gmail.com',
+      license='MIT',
+      packages=['pybob'],
+      install_requires=[
+                        'numpy', 'scipy', 'matplotlib', 'fiona',
+                        'shapely', 'opencv-python', 'pandas', 'geopandas',
+                        'scikit-image',
+                         ],
+      scripts=['bin/dem_coregistration.py', 'bin/generate_panchromatic.py',
+               'bin/print_reverb_browse_urls.py', 'bin/print_reverb_granule_names.py',
+               'bin/find_aster_dem_pairs.py', 'bin/image_footprint.py'],
+      zip_safe=False)
