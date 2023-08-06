3cd2a08c701e8f3516f0c56175d2b3a22657b857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:39 +0000

    Add package chemkin8/0.4/chemkin8-0.4.tar.gz

---
+++ packages/chemkin8/chemkin8-0.4.tar.gz/chemkin8-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='chemkin8',
+      version='0.4',
+      description='A Chemical Kinetics Library',
+      url='https://github.com/G8-CS207F17/cs207-FinalProject',
+      author='Mehul Smriti Raje, Ruiqi Chen, Ziqi Guo,',
+      author_email='zguo@g.harvard.edu',
+      license='Harvard',
+      packages=['chemkin8'],
+      zip_safe=False)
