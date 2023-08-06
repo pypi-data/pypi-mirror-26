d7217ab6ce2639c4a11e142aaebea57bd30b5bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:25 +0000

    Add package dopplerr/0.4.11/dopplerr-0.4.11.tar.gz

---
+++ packages/dopplerr/dopplerr-0.4.11.tar.gz/dopplerr-0.4.11/dopplerr/__init__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+import pbr.version
+
+__version__ = pbr.version.VersionInfo('dopplerr').release_string()
+VERSION = __version__
+DOPPLERR_VERSION = __version__
+
+__all__ = [
+    '__version__',
+    'DOPPLERR_VERSION',
+    'VERSION',
+]
