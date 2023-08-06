4a080ce3534598300518378ee2d4e9d912c4fd1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:51 +0000

    Add package pyplotz/0.18/pyplotz-0.18.tar.gz

---
+++ packages/pyplotz/pyplotz-0.18.tar.gz/pyplotz-0.18/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyplotz',
+  packages = ['pyplotz'], # this must be the same as the name above
+  version = '0.18',
+  description = 'A light weight wrapper for matplotlib users with Chinese characters supported',
+  author = 'Ray Williams',
+  author_email = 'rock307977586@gmail.com',
+  url = 'https://github.com/201528015329004/pyplotz', # use the URL to the github repo
+  download_url = 'https://github.com/201528015329004/pyplotz/archive/0.18.tar.gz', # I'll explain this in a second
+  keywords = ['pyplot', 'chinese', 'matplotlib','python','plt','pltz'], # arbitrary keywords
+  classifiers = [],
+)
