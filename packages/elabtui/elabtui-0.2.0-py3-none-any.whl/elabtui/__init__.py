d800f32392dcd7136f8a1541bf46073420c204ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:01 +0000

    Add package elabtui/0.2.0/elabtui-0.2.0-py3-none-any.whl

---
+++ packages/elabtui/elabtui-0.2.0-py3-none-any.whl/elabtui/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""A text user interface to eLabFTW"""
+
+__version__ = "0.2.0"
+__author__ = "Nicolas CARPi"
+__author_email__ = "nicolas.carpi@curie.fr"
+__license__ = "GPL v3"
+__copyright__ = "Copyright (©) 2017 Nicolas CARPi"
