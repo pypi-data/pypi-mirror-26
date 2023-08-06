0fae10ec87dd968af0241669421cfc8b6e1de598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:26 +0000

    Add package cacofonisk/0.2.2/cacofonisk-0.2.2.tar.gz

---
+++ packages/cacofonisk/cacofonisk-0.2.2.tar.gz/cacofonisk-0.2.2/tests/test_origattnxfer.py
@@ -0,0 +1,138 @@
+from cacofonisk.callerid import CallerId
+from .replaytest import ChannelEventsTestCase
+
+
+class TestAttnXferOrig(ChannelEventsTestCase):
+    def test_xfer_abacbc(self):
+        """
+        Test an ABACBC attended transfer.
+
+        First of all, we need to get notifications that calls are being
+        made:
+        - 201 (126680001) calls 202
+        - 201 calls 203 (126680003)
+
+        Secondly, we need notifications that an (attended) transfer has
+        happened:
+        - 201 joins the other channels (202 <--> 203)
+        """
+        events = self.run_and_get_events('examples/orig/xfer_abacbc.json')
+
+        expected_events = self.events_from_tuples((
+            # 201 calls 202
+            ('on_b_dial', {
+                'call_id': 'vgua0-dev-1442387090.552',
+                'caller': CallerId(code=126680001, number='201', is_public=True),
+                'callee': CallerId(code=126680002, number='202', is_public=True),
+            }),
+            ('on_up', {
+                'call_id': 'vgua0-dev-1442387090.552',
+                'caller': CallerId(code=126680001, number='201', is_public=True),
+                'callee': CallerId(code=126680002, number='202', is_public=True),
+            }),
+
+            # 201 calls 203
+            ('on_b_dial', {
+                'call_id': 'vgua0-dev-1442387091.556',
+                'caller': CallerId(code=126680001, number='201', is_public=True),
+                'callee': CallerId(code=126680003, number='203', is_public=True),
+            }),
+            ('on_up', {
+                'call_id': 'vgua0-dev-1442387091.556',
+                'caller': CallerId(code=126680001, number='201', is_public=True),
+                'callee': CallerId(code=126680003, number='203', is_public=True),
+            }),
+
+            # 201 transfers 202 <-> 203
+            ('on_transfer', {
+                'redirector': CallerId(code=126680001, number='201', is_public=True),
+                'party1': CallerId(code=126680002, number='202', is_public=True),
+                'party2': CallerId(code=126680003, number='203', is_public=True),
+                'new_id': 'vgua0-dev-1442387091.556',
+                'merged_id': 'vgua0-dev-1442387090.552',
+            }),
+
+            # The old call of 201 <-> 202 is hung up
+            ('on_hangup', {
+                'call_id': 'vgua0-dev-1442387090.552',
+                'caller': CallerId(code=126680001, number='201', is_public=True),
+                'callee': CallerId(code=126680002, number='202', is_public=True),
+                'reason': 'transferred',
+            }),
+
+            # 202 and 203 are done
+            ('on_hangup', {
+                'call_id': 'vgua0-dev-1442387091.556',
+                'caller': CallerId(code=126680002, number='202', is_public=True),
+                'callee': CallerId(code=126680003, number='203', is_public=True),
+                'reason': 'completed',
+            })
+        ))
+
+        self.assertEqual(expected_events, events)
+
+    def test_xfer_abbcac(self):
+        """
+        First of all, we need to get notifications that calls are being
+        made:
+        - +31501234567 calls 201 (126680001)
+        - 201 calls 202
+
+        Secondly, we need notifications that an (attended) transfer has
+        happened:
+        - 201 joins the other channels (+31501234567 <--> 202)
+        """
+        events = self.run_and_get_events('examples/orig/xfer_abbcac.json')
+
+        expected_events = self.events_from_tuples((
+            # +31501234567 calls 201
+            ('on_b_dial', {
+                'call_id': 'vgua0-dev-1442387041.544',
+                'caller': CallerId(code=0, name='Foo bar', number='+31501234567', is_public=True),
+                'callee': CallerId(code=126680001, number='+31508009000', is_public=True),
+            }),
+            ('on_up', {
+                'call_id': 'vgua0-dev-1442387041.544',
+                'caller': CallerId(code=0, name='Foo bar', number='+31501234567', is_public=True),
+                'callee': CallerId(code=126680001, number='+31508009000', is_public=True),
+            }),
+
+            # 201 calls 202
+            ('on_b_dial', {
+                'call_id': 'vgua0-dev-1442387044.548',
+                'caller': CallerId(code=126680001, number='201', is_public=True),
+                'callee': CallerId(code=126680002, number='202', is_public=True),
+            }),
+            ('on_up', {
+                'call_id': 'vgua0-dev-1442387044.548',
+                'caller': CallerId(code=126680001, number='201', is_public=True),
+                'callee': CallerId(code=126680002, number='202', is_public=True),
+            }),
+
+            # 201 transfers +31501234567 <-> 202
+            ('on_transfer', {
+                'redirector': CallerId(code=126680001, number='201', is_public=True),
+                'party1': CallerId(code=0, name='Foo bar', number='+31501234567', is_public=True),
+                'party2': CallerId(code=126680002, number='202', is_public=True),
+                'new_id': 'vgua0-dev-1442387044.548',
+                'merged_id': 'vgua0-dev-1442387041.544',
+            }),
+
+            # The original call of +31501234567 <-> 201 is hung up
+            ('on_hangup', {
+                'call_id': 'vgua0-dev-1442387041.544',
+                'caller': CallerId(code=0, name='Foo bar', number='+31501234567', is_public=True),
+                'callee': CallerId(code=126680001, number='+31508009000', is_public=True),
+                'reason': 'transferred',
+            }),
+
+            # +31501234567 and 202 are done
+            ('on_hangup', {
+                'call_id': 'vgua0-dev-1442387044.548',
+                'caller': CallerId(code=0, name='Foo bar', number='+31501234567', is_public=True),
+                'callee': CallerId(code=126680002, number='202', is_public=True),
+                'reason': 'completed',
+            }),
+        ))
+
+        self.assertEqual(expected_events, events)
---
+++ packages/cacofonisk/cacofonisk-0.2.2.tar.gz/cacofonisk-0.2.2/tests/test_origsimple.py
@@ -0,0 +1,191 @@
+from cacofonisk.callerid import CallerId
+from .replaytest import ChannelEventsTestCase
+
+
+class TestSimpleOrig(ChannelEventsTestCase):
+
+    def test_ab_success(self):
+        """Test a simple, successful call.
+
+        202 calls 203, 203 picks up and later the call is disconnected.
+        """
+        events = self.run_and_get_events('examples/orig/ab_success.json')
+
+        expected_events = self.events_from_tuples((
+            ('on_b_dial', {
+                'call_id': '63f2f9ce924a-1501851189.231',
+                'caller': CallerId(code=150010002, name='Robert Murray', number='202', is_public=True),
+                'callee': CallerId(code=150010003, number='203', is_public=True),
+            }),
+            ('on_up', {
+                'call_id': '63f2f9ce924a-1501851189.231',
+                'caller': CallerId(code=150010002, name='Robert Murray', number='202', is_public=True),
+                'callee': CallerId(code=150010003, number='203', is_public=True),
+            }),
+            ('on_hangup', {
+                'call_id': '63f2f9ce924a-1501851189.231',
+                'caller': CallerId(code=150010002, name='Robert Murray', number='202', is_public=True),
+                'callee': CallerId(code=150010003, number='203', is_public=True),
+                'reason': 'completed',
+            }),
+        ))
+
+        self.assertEqual(expected_events, events)
+
+    def test_ab_busy(self):
+        """Test a simple call where B is busy.
+        """
+        events = self.run_and_get_events('examples/orig/ab_busy.json')
+
+        expected_events = self.events_from_tuples((
+            ('on_b_dial', {
+                'call_id': '63f2f9ce924a-1501851519.239',
+                'caller': CallerId(code=150010002, name='Robert Murray', number='202', is_public=True),
+                'callee': CallerId(code=150010001, number='201', is_public=True),
+            }),
+            ('on_hangup', {
+                'call_id': '63f2f9ce924a-1501851519.239',
+                'caller': CallerId(code=150010002, name='Robert Murray', number='202', is_public=True),
+                'callee': CallerId(code=150010001, number='201', is_public=True),
+                'reason': 'busy'
+            }),
+        ))
+
+        self.assertEqual(expected_events, events)
+
+    def test_ab_acceptance(self):
+        """Test a simple, successful via callgroup with call acceptance.
+
+        +315080xxxxx calls +31612345678 picks up, accepts the call by pressing 1, and later the call is disconnected.
+        """
+        events = self.run_and_get_events('examples/orig/ab_acceptance.json')
+
+        expected_events = self.events_from_tuples((
+            ('on_b_dial', {
+                'call_id': 'ua0-acc-1506952916.1769',
+                'caller': CallerId(code=0, name='', number='+315080xxxxx', is_public=True),
+                'callee': CallerId(code=0, name='', number='+31612345678', is_public=True),
+            }),
+            ('on_up', {
+                'call_id': 'ua0-acc-1506952916.1769',
+                'caller': CallerId(code=0, name='', number='+315080xxxxx', is_public=True),
+                'callee': CallerId(code=0, name='', number='+31612345678', is_public=True),
+            }),
+            ('on_hangup', {
+                'call_id': 'ua0-acc-1506952916.1769',
+                'caller': CallerId(code=0, name='', number='+315080xxxxx', is_public=True),
+                'callee': CallerId(code=0, name='', number='+31853xxxxxx', is_public=True),
+                'reason': 'completed',
+            }),
+        ))
+
+        self.assertEqual(events, expected_events)
+
+    def test_ab_noacceptance(self):
+        """Test a simple, successful via callgroup with call acceptance.
+
+        +315080xxxxx calls +31613925xxx picks up, does NOT accept the call, and later the call is disconnected.
+        """
+        events = self.run_and_get_events('examples/orig/ab_noacceptance.json')
+
+        expected_events = self.events_from_tuples((
+            ('on_b_dial', {
+                'call_id': 'ua0-acc-1507621393.1940',
+                'caller': CallerId(code=0, name='', number='+315080xxxxx', is_public=True),
+                'callee': CallerId(code=0, name='', number='+31613925xxx', is_public=True),
+            }),
+            ('on_hangup', {
+                'call_id': 'ua0-acc-1507621393.1940',
+                'caller': CallerId(code=0, name='', number='+315080xxxxx', is_public=True),
+                'callee': CallerId(code=0, name='', number='+31613925xxx', is_public=True),
+                'reason': 'completed',
+            }),
+        ))
+
+        self.assertEqual(events, expected_events)
+
+    def test_ab_callgroup(self):
+        """Test a simple call to a group where one phone is picked up.
+        """
+        events = self.run_and_get_events('examples/orig/ab_callgroup.json')
+
+        expected_events = self.events_from_tuples((
+            ('on_b_dial', {
+                'call_id': '63f2f9ce924a-1501852169.254',
+                'caller': CallerId(code=150010002, name='Robert Murray', number='202', is_public=True),
+                'callee': CallerId(code=150010001, number='401', is_public=True),
+            }),
+            ('on_b_dial', {
+                'call_id': '63f2f9ce924a-1501852169.254',
+                'caller': CallerId(code=150010002, name='Robert Murray', number='202', is_public=True),
+                'callee': CallerId(code=150010003, number='401', is_public=True),
+            }),
+            ('on_hangup', {
+                'call_id': '63f2f9ce924a-1501852169.254',
+                'caller': CallerId(code=150010002, name='Robert Murray', number='202', is_public=True),
+                'callee': CallerId(code=150010003, name='', number='401', is_public=True),
+                'reason': 'answered-elsewhere'
+            }),
+            ('on_up', {
+                'call_id': '63f2f9ce924a-1501852169.254',
+                'caller': CallerId(code=150010002, name='Robert Murray', number='202', is_public=True),
+                'callee': CallerId(code=150010001, number='401', is_public=True),
+            }),
+            ('on_hangup', {
+                'call_id': '63f2f9ce924a-1501852169.254',
+                'caller': CallerId(code=150010002, name='Robert Murray', number='202', is_public=True),
+                'callee': CallerId(code=150010001, number='401', is_public=True),
+                'reason': 'completed'
+            }),
+        ))
+
+        self.assertEqual(expected_events, events)
+
+    def test_a_cancel_hangup(self):
+        """
+        Test a call where A hangs up before B can pick up.
+        """
+        events = self.run_and_get_events('examples/orig/ab_a_cancel_hangup.json')
+
+        expected_events = self.events_from_tuples((
+            ('on_b_dial', {
+                'call_id': '0f00dcaa884f-1508490698.34',
+                'caller': CallerId(code=150010002, name='David Meadows', number='202', is_public=True),
+                'callee': CallerId(code=150010004, number='204', is_public=True),
+            }),
+            ('on_hangup', {
+                'call_id': '0f00dcaa884f-1508490698.34',
+                'caller': CallerId(code=150010002, name='David Meadows', number='202', is_public=True),
+                'callee': CallerId(code=150010004, number='204', is_public=True),
+                'reason': 'cancelled'
+            }),
+        ))
+
+        self.assertEqual(expected_events, events)
+
+    def test_a_success_hangup(self):
+        """
+        Test a call where A hangs up after being connected to B.
+        """
+        events = self.run_and_get_events('examples/orig/ab_a_success_hangup.json')
+
+        expected_events = self.events_from_tuples((
+            ('on_b_dial', {
+                'call_id': '0f00dcaa884f-1508490669.30',
+                'caller': CallerId(code=150010002, name='David Meadows', number='202', is_public=True),
+                'callee': CallerId(code=150010004, number='204', is_public=True),
+            }),
+            ('on_up', {
+                'call_id': '0f00dcaa884f-1508490669.30',
+                'caller': CallerId(code=150010002, name='David Meadows', number='202', is_public=True),
+                'callee': CallerId(code=150010004, number='204', is_public=True),
+            }),
+            ('on_hangup', {
+                'call_id': '0f00dcaa884f-1508490669.30',
+                'caller': CallerId(code=150010002, name='David Meadows', number='202', is_public=True),
+                'callee': CallerId(code=150010004, number='204', is_public=True),
+                'reason': 'completed',
+            }),
+        ))
+
+        self.assertEqual(expected_events, events)
