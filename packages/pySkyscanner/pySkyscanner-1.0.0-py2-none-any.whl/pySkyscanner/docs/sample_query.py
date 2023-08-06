8ca9d25c686f6642e867f73ae70e2457def2d73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:39 +0000

    Add package pySkyscanner/1.0.0/pySkyscanner-1.0.0-py2-none-any.whl

---
+++ packages/pySkyscanner/pySkyscanner-1.0.0-py2-none-any.whl/pySkyscanner/docs/sample_query.py
@@ -0,0 +1,22 @@
+from pySkyscanner.pyskyscanner import *
+from secrets import API_KEY
+
+__author__ = "Nick Lee"
+__copyright__ = "Copyright (C) 2015 Nick Lee"
+__license__ = "MIT License"
+
+# Test code
+def demo():
+    # Fun fact: If the airport code doesn't exist, this app crashes
+
+    client = SkyscannerClient(API_KEY)
+
+    res = client.init_search_session("DE", "EUR", "HAM", "PHL", "2018-03-07", "2018-03-14", "Economy")
+    itins = client.get_itineraries(res)
+
+    for i in itins:
+        price = client.get_itinerary_cost(i)
+        if price is not None and client.print_flight_segments(i):
+            print "Trip Cost: " + u"\u20AC" + str(client.get_itinerary_cost(i)) + "\n"
+
+demo()
