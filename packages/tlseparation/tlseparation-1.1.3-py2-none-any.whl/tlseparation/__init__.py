6ccd8b6bf4280b53e06ca73ec850098a4dcd8985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:31 +0000

    Add package tlseparation/1.1.3/tlseparation-1.1.3-py2-none-any.whl

---
+++ packages/tlseparation/tlseparation-1.1.3-py2-none-any.whl/tlseparation/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Matheus Boni Vicari (matheus.boni.vicari@gmail.com)
+"""
+
+from auto_separation_1 import main as auto_separation_1
+from auto_separation_2 import main as auto_separation_2
