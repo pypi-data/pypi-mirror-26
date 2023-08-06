0394ced69f3b030be959effb50c6bd9a582b62c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:22 +0000

    Add package EinsteinVision/0.6/EinsteinVision-0.6.tar.gz

---
+++ packages/EinsteinVision/EinsteinVision-0.6.tar.gz/EinsteinVision-0.6/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+  name = 'EinsteinVision',
+  packages = ['EinsteinVision'], # this must be the same as the name above
+  version = '0.6',
+  description = 'Wrapper for Salesforce\'s Einstein Predictive Vision API',
+  author = 'Felipe Ryan',
+  author_email = 'feliperyan@gmail.com',
+  url = 'https://github.com/feliperyan/EinsteinVisionPython', # use the URL to the github repo
+  download_url = 'https://github.com/feliperyan/EinsteinVisionPython/archive/0.6.tar.gz', # I'll explain this in a second
+  keywords = ['api', 'einsteinpredictivevision', 'metamind', 'salesforce'], # arbitrary keywords
+  classifiers = [],
+  install_requires=[
+          'requests',
+          'requests_toolbelt',
+          'cryptography',
+          'PyJWT'
+      ],
+)
