7fa31d1657bfc8deac2082a7171f9c9fa6314cf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:25 +0000

    Add package fmc-rest-client/0.2/fmc_rest_client-0.2.tar.gz

---
+++ packages/fmc-rest-client/fmc_rest_client-0.2.tar.gz/fmc_rest_client-0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='fmc_rest_client',
+      version='0.2',
+      description='FMC REST API Client',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Security',
+          'Topic :: System :: Networking :: Firewalls',
+      ],
+      keywords='firepower management rest-api',
+      url='https://github.com/macvjuhu/fmc_rest_client',
+      author='Vikas Sharma',
+      author_email='macvjuhu@yahoo.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'requests'
+      ],
+      include_package_data=True,
+      zip_safe=False)
