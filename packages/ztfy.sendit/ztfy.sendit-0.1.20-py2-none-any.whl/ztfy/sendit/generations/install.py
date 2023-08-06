47a5822436da65cda904bed3813de643e9b4ecda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:02 +0000

    Add package ztfy.sendit/0.1.20/ztfy.sendit-0.1.20-py2-none-any.whl

---
+++ packages/ztfy.sendit/ztfy.sendit-0.1.20-py2-none-any.whl/ztfy/sendit/generations/install.py
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2013 Thierry Florac <tflorac AT onf.fr>
+# All Rights Reserved.
+#
+
+
+# import standard packages
+
+# import Zope3 interfaces
+
+# import local interfaces
+
+# import Zope3 packages
+
+# import local packages
+
+
+def evolve(context):
+    """Change text indexes splitter"""
+    pass
