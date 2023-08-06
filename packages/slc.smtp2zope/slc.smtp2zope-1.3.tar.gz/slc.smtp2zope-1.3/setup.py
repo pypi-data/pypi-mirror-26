7caa13ec61b462c4170149a9fae12115ce7621f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:28 +0000

    Add package slc.smtp2zope/1.3/slc.smtp2zope-1.3.tar.gz

---
+++ packages/slc.smtp2zope/slc.smtp2zope-1.3.tar.gz/slc.smtp2zope-1.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '1.3'
+
+setup(
+    name='slc.smtp2zope',
+    version=version,
+    description="Provides smtp2zope integration script for slc.mailrouter.",
+    long_description=open("README.txt").read(),
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Plone",
+        ],
+    keywords='smtp2zope',
+    author='Izak Burger, Syslab.com GmbH',
+    author_email='isburger@gmail.com',
+    url='https://github.com/syslabcom/slc.smtp2zope',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir = {'' : 'src'},
+    namespace_packages=['slc'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points="""
+        [console_scripts]
+        smtp2zope = slc.smtp2zope:main
+    """,
+    setup_requires=[],
+    paster_plugins = ["ZopeSkel"],
+    )
---
+++ packages/slc.smtp2zope/slc.smtp2zope-1.3.tar.gz/slc.smtp2zope-1.3/src/slc/smtp2zope/__init__.py
@@ -0,0 +1,77 @@
+# > POST /Plone/mailrouter-inject HTTP/1.1
+# > Authorization: Basic YWRtaW46YWRtaW4=
+# > User-Agent: curl/7.19.7 (i486-pc-linux-gnu) libcurl/7.19.7 OpenSSL/0.9.8k zlib/1.2.3.3 libidn/1.15
+# > Host: localhost:8080
+# > Accept: */*
+# > Content-Length: 197364
+# > Content-Type: application/x-www-form-urlencoded
+# > Expect: 100-continue
+
+import sys
+import logging
+import httplib
+import socket
+from optparse import OptionParser
+
+def main():
+
+    parser = OptionParser()
+    parser.add_option("-z", "--zope",
+        help="Zope host name and port to submit email to, eg: localhost:8080")
+    parser.add_option("-u", "--url",
+        help="URL to submit email to, eg: /Plone/@@mailrouter-inject")
+    parser.add_option("-v", "--verbose", default=0,
+        action="count", help="Increase verbosity")
+
+    (options, args) = parser.parse_args()
+
+    # Check required options
+    for option in (options.zope, options.url):
+        if option is None:
+            parser.print_help()
+            sys.exit(1)
+
+    # Configure logging
+    # level works out at 30 (WARNING) if no -v given. For one -v, it drops
+    # to 20 (INFO).
+    logging.basicConfig(level=max(3-options.verbose,1)*10,
+        format='%(asctime)s %(levelname)s %(message)s')
+
+    message = sys.stdin.read()
+    try:
+        h = httplib.HTTPConnection(options.zope)
+        h.request('POST', options.url, message)
+        response = h.getresponse()
+    except httplib.BadStatusLine as bsl:
+        logging.info('BadStatusLine: %s' % bsl)
+        sys.exit(78) # EX_CONFIG
+    except (socket.error, socket.timeout, httplib.HTTPException), e:
+        logging.error(e)
+        sys.exit(75) # EX_TEMPFAIL
+    status = response.status
+    response = response.read()
+
+
+    # By default, exim will treat EX_TEMPFAIL (75) as a temporary error.
+    # Postfix also treats 75 as temporary.
+    # All other exit status' are permanent errors. This can be changed of
+    # course. We shall therefore exit with status 75 if we want the MTA to
+    # defer.
+
+    # If a 2xx is returned, we exit status 0, delivery done.
+    if status / 100 == 2:
+        logging.info(response)
+        sys.exit(0)
+
+    # If status is not 200, the response should hold an error message
+    logging.error(response)
+    # If not found, permanent error
+    if status == 401:
+        sys.exit(77) # EX_NOPERM
+    if status == 404:
+        sys.exit(67) # EX_NOUSER
+    if status / 100 == 4:
+        sys.exit(78) # EX_CONFIG
+
+    # Otherwise, defer it, try again later.
+    sys.exit(75) # EX_TEMPFAIL
