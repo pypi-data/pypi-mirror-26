0cfbd1563c9b85b16ff2d7de08c1c3c639943931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:57 +0000

    Add package spookify-py/0.1/spookify_py-0.1.tar.gz

---
+++ packages/spookify-py/spookify_py-0.1.tar.gz/spookify_py-0.1/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name='spookify_py',
+      version='0.1',
+      description='scraping bitcoin values',
+	  url = 'https://www.github.com/Alvixeon/btc-py',
+      author='Alvixeon',
+      author_email='remusername@gmail.com',
+      license='MIT',
+      packages=['spookify'],
+      zip_safe=False
+)
