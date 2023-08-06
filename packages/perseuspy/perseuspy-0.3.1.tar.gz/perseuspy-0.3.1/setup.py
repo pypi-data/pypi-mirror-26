006c9ca38030429ef9eb6a22c4d42a7e3051fff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:05 +0000

    Add package perseuspy/0.3.1/perseuspy-0.3.1.tar.gz

---
+++ packages/perseuspy/perseuspy-0.3.1.tar.gz/perseuspy-0.3.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+from perseuspy import __version__
+
+import os
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='perseuspy',
+        version=__version__,
+        description='Utilities for integrating python scripts into Perseus workflows',
+        long_description=read('README.rst'),
+        url='http://www.github.com/jdrudolph/perseuspy',
+        author='Jan Rudolph',
+        author_email='jan.daniel.rudolph@gmail.com',
+        license='MIT',
+        packages=find_packages(),
+        install_requires=['pandas >= 0.19', 'networkx'],
+        test_suite = 'nose.collector',
+        tests_require= ['nose']
+) 
+
