bb1c5076a40ad4fd757ce01f30b18a9f51f345e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:21 +0000

    Add package home-assistant-frontend/20171111.0/home-assistant-frontend-20171111.0.tar.gz

---
+++ packages/home-assistant-frontend/home-assistant-frontend-20171111.0.tar.gz/home-assistant-frontend-20171111.0/hass_frontend_es5/__init__.py
@@ -0,0 +1,27 @@
+"""AUTO-GENERATED. DO NOT MODIFY"""
+import os
+
+
+def where():
+    """Return path to the frontend."""
+    return os.path.dirname(__file__)
+
+VERSION = '1f9fc46576abfb5a644e86930f85698071dcdec5'
+CREATED_AT = 1510436108
+FINGERPRINTS = {
+    "config": "e679488ca47d2c631edf20c3bf9fbb05",
+    "dev-event": "5336e675a747a97441f6a2e61ea5a1fa",
+    "dev-info": "3cc20673652406718eb41573ab1a494e",
+    "dev-mqtt": "ebf4d71b0871b23655e68e125d9aca05",
+    "dev-service": "6edae62d47e663eefa35326000de3507",
+    "dev-state": "14bdcafbbf44c6a841303a2f2a2c60a1",
+    "dev-template": "18743488268ed3378c29f664783b3889",
+    "hassio": "978575253f5133e469310097db34ad62",
+    "history": "c6d64596879bad7ea0373eecce651a69",
+    "iframe": "185480826c02fec59d9ad045abee15f4",
+    "kiosk": "4d15ef5a5be516d512bb8a5ba879c3b0",
+    "logbook": "05584fd801df5f5755db3399aac22baf",
+    "mailbox": "bdaf8ed7f889813e8d90569a456116bc",
+    "map": "e487400ba15d9e326d9c7655b4d53685",
+    "shopping-list": "ffa7af8d0506d6baffee33783da08fc1"
+}
