80a955b28cdac603527ba1d25f5e8d304c0c26e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:16 +0000

    Add package chemkin/0.9/chemkin-0.9.tar.gz

---
+++ packages/chemkin/chemkin-0.9.tar.gz/chemkin-0.9/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup,find_packages
+
+setup(name='chemkin',
+      version='0.9',
+      description='The best chemkin package',
+      url='https://github.com/G12-cs207-FinalProject/cs207-FinalProject.git',
+      author='Parser',
+      license='Harvard',
+      packages=find_packages()
+)
