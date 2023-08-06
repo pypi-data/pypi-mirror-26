e18918b0129ffec0888026598e2bad3e88c2e942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:10 +0000

    Add package p_stone_labs/0.1/p_stone_labs-0.1.tar.gz

---
+++ packages/p_stone_labs/p_stone_labs-0.1.tar.gz/p_stone_labs-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='p_stone_labs',
+      version='0.1',
+      description='P-Stone Labs',
+      url='http://github.com/gravitt8460',
+      author='P-Stone',
+      author_email='max.gravitt@gmail.com',
+      license='MIT',
+      packages=['p_stone_labs'],
+      zip_safe=False)
