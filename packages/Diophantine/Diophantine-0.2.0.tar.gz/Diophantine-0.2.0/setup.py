bc2605fd6f672f080f40c50dc9b4378e72f97fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:13 +0000

    Add package Diophantine/0.2.0/Diophantine-0.2.0.tar.gz

---
+++ packages/Diophantine/Diophantine-0.2.0.tar.gz/Diophantine-0.2.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Diophantine",
+    version="0.2.0",
+    py_modules=['diophantine'],
+    author="Thomas G. Close",
+    author_email="tom.g.close@gmail.com",
+    description=(
+        "A python package for finding small solutions of systems of "
+        "diophantine (integer algebra) equations"),
+    long_description=open("README.rst").read(),
+    license="MIT",
+    keywords=["mathematics", "diophantine", "algebra", "integer",
+              "systems"],
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Console',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: MIT License',
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Topic :: Scientific/Engineering'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    install_requires=['sympy>=1.1',
+                      'future>=0.16'],
+    tests_require=['nose']
+)
