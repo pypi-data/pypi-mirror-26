e71fa74fc5d49a29d40dffc91af0b784d079e820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:37 +0000

    Add package dvach.api/0.0.4.2/dvach.api-0.0.4.2.tar.gz

---
+++ packages/dvach.api/dvach.api-0.0.4.2.tar.gz/dvach.api-0.0.4.2/setup.py
@@ -0,0 +1,41 @@
+from os import path
+
+from setuptools import setup
+
+from api2ch import api2ch
+
+here = path.abspath(path.dirname(__file__))
+
+install_requires = [
+    'requests'
+]
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='dvach.api',
+    version=api2ch.__version__,
+    python_requires='>=3.5',
+    packages=['api2ch'],
+    url='https://github.com/anonim-legivon/dvach.api',
+    license='Apache License 2.0',
+    author=api2ch.__author__,
+    author_email='fadedDexofan@protonmail.com',
+    py_modules=['api2ch'],
+    install_requires=['requests'],
+    description='Python 2ch.hk API wrapper',
+    long_description=long_description,
+    keywords='dvach 2ch 2ch.hk api wrapper',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Internet'
+
+    ]
+)
