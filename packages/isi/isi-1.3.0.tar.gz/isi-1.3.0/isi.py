b5579e98f56e62edc0d91300c96a70e3521aa421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:34 +0000

    Add package isi/1.3.0/isi-1.3.0.tar.gz

---
+++ packages/isi/isi-1.3.0.tar.gz/isi-1.3.0/isi.py
@@ -0,0 +1,12 @@
+'''This is a test function'''
+def print_lol(the_list,indent=False,level=0):
+    '''This function print list,会根据需要缩进列表'''
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(each_item)
+            
