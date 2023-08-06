58097fc9b9981a66dbf0ad08f429362d806afd7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:53 +0000

    Add package Red-Trivia/0.0.1/Red-Trivia-0.0.1.tar.gz

---
+++ packages/Red-Trivia/Red-Trivia-0.0.1.tar.gz/Red-Trivia-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name='Red-Trivia',
+    version='0.0.1',
+    namespace_packages=['redbot'],
+    packages=['redbot.trivia'],
+    license='GPLv3',
+    author='Cog-Creators',
+    description='A collection of trivia lists curated by Red-DiscordBot developers',
+    include_package_data=True
+)
