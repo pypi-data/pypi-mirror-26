baba50914b4894098cd1b6bebbe68b662d5ef60c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:49 +0000

    Add package amen/0.0.3/amen-0.0.3.tar.gz

---
+++ packages/amen/amen-0.0.3.tar.gz/amen-0.0.3/amen/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+AMEN
+
+A toolbox for algorithmic remixing, after Echo Nest Remix.
+'''
+from .feature import Feature
+from .timing import TimeSlice
+from .version import version as __version__
+
+__all__ = ['audio', 'Feature', 'utils']
