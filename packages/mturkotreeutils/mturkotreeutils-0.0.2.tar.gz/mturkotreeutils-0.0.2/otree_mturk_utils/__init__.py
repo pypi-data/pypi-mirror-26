c19fdf9e84f33dc19228f47e3007f5f5cf7c86a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:46 +0000

    Add package mturkotreeutils/0.0.2/mturkotreeutils-0.0.2.tar.gz

---
+++ packages/mturkotreeutils/mturkotreeutils-0.0.2.tar.gz/mturkotreeutils-0.0.2/otree_mturk_utils/__init__.py
@@ -0,0 +1,6 @@
+# default_app_config = 'otree_mturk_utils.apps.Config'
+# from .models import *
+# from .views import *
+
+__version__ = '0.0.2'
+
---
+++ packages/mturkotreeutils/mturkotreeutils-0.0.2.tar.gz/mturkotreeutils-0.0.2/otree_mturk_utils/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Config(AppConfig):
+    name = 'mturkotreeutils'
+    verbose_name = 'set of utilities for oTree and mTurk'
