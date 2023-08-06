8033cb6be5babd3d69aba353ae6d4b39d093a8ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:32 +0000

    Add package pyphihue/0.2.3/pyphihue-0.2.3.tar.gz

---
+++ packages/pyphihue/pyphihue-0.2.3.tar.gz/pyphihue-0.2.3/pyphihue/__init__.py
@@ -0,0 +1,7 @@
+from pyphihue.bridge import Bridge
+from pyphihue.light import Light
+
+__all__ = ['Bridge', 'Light']
+
+__version__ = (0, 2, 3)
+__author__ = 'Patrick Scholten'
---
+++ packages/pyphihue/pyphihue-0.2.3.tar.gz/pyphihue-0.2.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from pyphihue import __author__
+from pyphihue import __version__
+
+
+VER = "{}.{}.{}".format(*__version__)
+URL = 'https://gitlab.com/pyphihue/pyphihue'
+
+setup(name='pyphihue',
+      packages=['pyphihue'],
+      version=VER,
+      description='A library to interface with Philips Hue',
+      author=__author__,
+      author_email='pydevpat@gmail.com',
+      url=URL,
+      download_url='{}/repository/archive.tar.gz?ref=master'.format(URL),
+      license='MIT',
+      keywords='Philips Hue API-wrapper',
+      install_requires=[
+          'requests',
+      ],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Home Automation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+      ],
+      )
