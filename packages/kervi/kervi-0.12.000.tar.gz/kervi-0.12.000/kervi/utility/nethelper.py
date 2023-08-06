05b7b6db5ad6d6192869513aded187806cdbd300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:07 +0000

    Add package kervi/0.12.000/kervi-0.12.000.tar.gz

---
+++ packages/kervi/kervi-0.12.000.tar.gz/kervi-0.12.000/kervi/utility/nethelper.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2016, Tim Wentzlau
+# Licensed under MIT
+
+""" helper function for getting the ip address of host device """
+
+import socket
+
+def get_ip_address():
+    """ returns ip addres of this device bound to primary net adapter """
+    
+    try:
+        _socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        _socket.connect(('google.com', 0))
+        return _socket.getsockname()[0]
+    except:
+        return "localhost"
+def get_free_port(preferred_ports = None):
+    """ returns a free port """
+
+    port = 0
+    if preferred_ports:
+        for preferred_port in preferred_ports:
+            if is_port_free(preferred_port):
+                port = preferred_port
+                break
+
+    if port == 0:
+        _socket = socket.socket()
+        _socket.bind(('', 0))
+        port = _socket.getsockname()[1]
+        _socket.close()
+    return port
+
+def is_port_free(port):
+    """ return True if port is free """
+    _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        _socket.bind(("127.0.0.1", port))
+    except socket.error:
+        return False
+
+    _socket.close()
+    return True
+
---
+++ packages/kervi/kervi-0.12.000.tar.gz/kervi-0.12.000/kervi/utility/process.py
@@ -0,0 +1,79 @@
+# Copyright (c) 2016, Tim Wentzlau
+# Licensed under MIT
+
+""" Handles multiprocessing functionality in Kervi """
+
+from multiprocessing import Process #, Array, Value, Manager, freeze_support
+import time
+import kervi.spine as spine
+from kervi.utility.process_spine import _ProcessSpine
+#import sys
+import kervi.kervi_logging as k_logging
+
+MAIN_SPINE = None
+def _start_root_spine(settings, reset_log=False):
+    global MAIN_SPINE
+    k_logging.init_process_logging("kervi-main", settings["log"])
+    k_logging.KerviLog("kervi main")
+    spine._init_spine("kervi-main")
+    MAIN_SPINE = _ProcessSpine(settings["network"]["IPCRootPort"], settings, is_root=True)
+
+def _stop_root_spine():
+    MAIN_SPINE.close_all_connections()
+
+
+class _KerviProcess(object):
+    def __init__(self, name, settings, ipcPort):
+        self.name = name
+        self.do_terminate = False
+        self.port = ipcPort
+        self.settings = settings
+        spine._init_spine(name)
+        self.spine = spine.Spine()
+        self.process_spine = _ProcessSpine(ipcPort, settings)
+        self.init_process()
+        self.spine.register_command_handler("terminateProcess", self.terminate)
+
+    def terminate(self):
+        self.spine.log.debug("do terminate:{0}", self.port)
+        #print("terminate:", self.port)
+        self.do_terminate = True
+
+    def init_process(self):
+        self.spine.log.error("abstract init_process called in KerviProcess")
+
+    def _terminate_process(self):
+        self.terminate_process()
+        self.spine.trigger_event("processTerminating", None, scope="process")
+        time.sleep(5)
+        self.process_spine.close_all_connections()
+        self.spine.log.info("process terminated:{0}", self.port)
+        self.spine.stop()
+
+    def terminate_process(self):
+        pass
+
+def _launch(name, process_class, settings, ipc_port):
+    k_logging.init_process_logging(name, settings["log"])
+    log = k_logging.KerviLog(name)
+    log.info('create process:{0} ipc port:{1}:', process_class.__name__, ipc_port)
+    process = process_class(name, settings, ipc_port)
+    try:
+        while not process.do_terminate:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+    except:
+        log.exception("error in process loop")
+        pass
+
+    process._terminate_process()
+
+
+def _start_process(name, settings, port_idx, process_class):
+    process = Process(target=_launch, args=(name, process_class, settings, port_idx))
+    process.start()
+    return process
+
+def _stop_processes():
+    spine.Spine().send_command("terminateProcess")
