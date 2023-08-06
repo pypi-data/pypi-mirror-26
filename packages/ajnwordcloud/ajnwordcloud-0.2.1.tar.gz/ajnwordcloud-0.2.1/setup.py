5124a58211b360c2ab4c83fb89fd28bcfc23abaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:15 +0000

    Add package ajnwordcloud/0.2.1/ajnwordcloud-0.2.1.tar.gz

---
+++ packages/ajnwordcloud/ajnwordcloud-0.2.1.tar.gz/ajnwordcloud-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+
+setup(name='ajnwordcloud',
+      version='0.2.1',
+      description='simple wordcloud generator',
+      url='http://github.com/voiceoftreason/ajnwordcloud',
+      author='Andy Newton',
+      author_email='voiceoftreason@me.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['numpy>=1.6.1', 'pillow'],
+      ext_modules=[Extension("ajnwordcloud.ii_search",
+                           ["ajnwordcloud/ii_search.c"],include_dirs=['.'])],
+      include_package_data=True,
+      zip_safe=False)
