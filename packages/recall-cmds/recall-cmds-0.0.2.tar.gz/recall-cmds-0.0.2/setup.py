2985b40dd6aec325c87597e819ad7d81ef916b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:42 +0000

    Add package recall-cmds/0.0.2/recall-cmds-0.0.2.tar.gz

---
+++ packages/recall-cmds/recall-cmds-0.0.2.tar.gz/recall-cmds-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+	name='recall-cmds',
+	version='0.0.2',
+	url='https://github.com/mavcook/recall',
+	author='Maverick Cook',
+	author_email='mav@mavcook.com',
+	description='A tool to view and edit templated commands that you forget.',
+
+	py_modules=['recall'],
+	entry_points={
+		'console_scripts': [
+			'recall=recall:main',
+		],
+	},
+
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Environment :: Console',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+	],
+
+	python_requires='>=3',
+)
