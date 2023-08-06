4b9dbebdd8bc02aef62167cc900752475e1af1ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:15 +0000

    Add package Direct-Downloader/0.7/Direct%20Downloader-0.7.tar.gz

---
+++ packages/Direct-Downloader/Direct%20Downloader-0.7.tar.gz/Direct Downloader-0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='Direct Downloader',
+      version='0.7',
+      description='Download a list of direct links using multi-threading.',
+      author='Derek Santos',
+      author_email='derek@dyrenex.com',
+      license='The MIT License (MIT)',
+      url='https://github.com/santosderek/Direct-Downloader',
+      packages=find_packages(),
+      scripts=['directdownloader/__main__.py',
+               'directdownloader/direct_downloader.py'],
+      entry_points={
+          'console_scripts': [
+              'dDownloader = directdownloader.__main__:main'
+
+          ]
+      },
+      install_requires=[
+          'requests',
+          'bs4'
+      ],
+      keywords=['testing', 'logging', 'example']
+      )
