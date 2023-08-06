1deaf5b51f75e0589b2b42579d47d9b1a5140e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:49 +0000

    Add package ecs-deployer/0.1.2/ecs-deployer-0.1.2.tar.gz

---
+++ packages/ecs-deployer/ecs-deployer-0.1.2.tar.gz/ecs-deployer-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name="ecs-deployer",
+    version="0.1.2",
+    packages=["ecs_deployer"],
+    author="Andy Sun",
+    author_email="andy_sun_sha@hotmail.com",
+    license='MIT',
+    url='https://github.com/AndySun25/ecs-deployer',
+    description="Simple Python ECS deployment script.",
+    scripts=["ecs_deployer/bin/ecs-deployer.py"],
+    install_requires=[
+        'boto3>=1.4.6',
+        'awscli==1.11.106',
+    ],
+    python_requires='>3.5',
+)
