4277228cad576c34bd4534d0e7b392ce8b4f2370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:37 +0000

    Add package fast-histogram/0.3/fast-histogram-0.3.tar.gz

---
+++ packages/fast-histogram/fast-histogram-0.3.tar.gz/fast-histogram-0.3/setup.py
@@ -0,0 +1,21 @@
+import os
+
+import numpy as np
+from setuptools import setup
+from setuptools.extension import Extension
+
+extensions = [Extension("fast_histogram._histogram_core",
+                       [os.path.join('fast_histogram', '_histogram_core.c')],
+                       include_dirs=[np.get_include()])]
+
+setup(name='fast-histogram',
+      version='0.3',
+      description='Fast simple 1D and 2D histograms',
+      long_description=open('README.rst').read(),
+      install_requires=['numpy'],
+      author='Thomas Robitaille',
+      author_email='thomas.robitaille@gmail.com',
+      license='BSD',
+      url='https://github.com/astrofrog/fast-histogram',
+      packages=['fast_histogram', 'fast_histogram.tests'],
+      ext_modules=extensions)
