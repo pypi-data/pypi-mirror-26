704598387cca6d6120ef5a0c316216c3f636429b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:16 +0000

    Add package nltools/0.3.2/nltools-0.3.2.tar.gz

---
+++ packages/nltools/nltools-0.3.2.tar.gz/nltools-0.3.2/setup.py
@@ -0,0 +1,43 @@
+# from nltools.version import __version__
+from setuptools import setup, find_packages
+
+__version__ = '0.3.2'
+
+# try:
+#     from setuptools.core import setup
+# except ImportError:
+#     from distutils.core import setup
+
+extra_setuptools_args = dict(
+    tests_require=['pytest']
+)
+
+setup(
+    name='nltools',
+    version=__version__,
+    author='Luke Chang',
+    author_email='luke.j.chang@dartmouth.edu',
+    url='http://neurolearn.readthedocs.org/en/latest/',
+    install_requires=['numpy>=1.9', 'scipy > ', 'nilearn>=0.3.0', 'nibabel>=2.0.1',
+                    'pandas>=0.20', 'six', 'seaborn', 'matplotlib',
+                    'scikit-learn>=0.18.1','pynv','joblib'],
+    extras_require={
+    'ibrainViewer':['ipywidgets>=5.2.2']
+    },
+    packages=find_packages(exclude=['nltools/tests']),
+    package_data={'nltools': ['resources/*']},
+    license='LICENSE.txt',
+    description='A Python package to analyze neuroimaging data',
+    long_description='nltools is a collection of python tools to perform '
+                     'preprocessing, univariate GLMs, and predictive '
+                     'multivariate modeling of neuroimaging data. It is the '
+                     'analysis engine powering www.neuro-learn.org.',
+    keywords = ['neuroimaging', 'preprocessing', 'analysis','machine-learning'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License"
+    ],
+    **extra_setuptools_args
+)
