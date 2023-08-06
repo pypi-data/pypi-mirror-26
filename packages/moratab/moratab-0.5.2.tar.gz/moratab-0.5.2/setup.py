054d2359753c29b2e0733c1e0cb9572d2f7b7866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:54 +0000

    Add package moratab/0.5.2/moratab-0.5.2.tar.gz

---
+++ packages/moratab/moratab-0.5.2.tar.gz/moratab-0.5.2/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+
+setup(name='moratab',
+	version='0.5.2',
+	description='Persian markdown convertor.',
+	author='Alireza Nourian',
+	author_email='az.nourian@gmail.com',
+	url='http://www.sobhe.ir/moratab/',
+	packages=['moratab'],
+	classifiers=[
+		'Natural Language :: Persian',
+		'Programming Language :: Python :: 2.7',
+		'License :: OSI Approved :: MIT License',
+	],
+	install_requires=['mistune==0.7.1']
+)
