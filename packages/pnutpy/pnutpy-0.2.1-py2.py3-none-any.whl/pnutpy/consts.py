d4061c04f389c0cce481e63928a372d0c4308740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:48 +0000

    Add package pnutpy/0.2.1/pnutpy-0.2.1-py2.py3-none-any.whl

---
+++ packages/pnutpy/pnutpy-0.2.1-py2.py3-none-any.whl/pnutpy/consts.py
@@ -0,0 +1,143 @@
+PAGINATION_PARAMS = [
+    'since_id',
+    'before_id',
+    'count',
+]
+
+
+POST_PARAMS = [
+    'include_deleted',
+    'include_client',
+    'include_counts',
+    'include_html',
+    'include_post_html',
+    'include_bookmarked_by',
+    'include_reposted_by',
+    'include_directed_posts',
+    'include_copy_mentions',
+    'include_muted',
+    'include_raw',
+    'include_post_raw',
+    'include_user',
+    'include_user_html',
+    'include_presence',
+    'include_user_raw',
+]
+
+
+USER_PARAMS = [
+    'include_html',
+    'include_user_html',
+    'include_counts',
+    'include_user',
+    'include_presence',
+    'include_raw',
+    'include_user_raw',
+]
+
+
+# USER_SEARCH_PARAMS = [
+    # 'q',
+    # 'count',
+# ]
+
+
+# POST_SEARCH_PARAMS = [
+    # 'index',
+    # 'order',
+    # 'query',
+    # 'text',
+    # 'hashtags',
+    # 'links',
+    # 'link_domains',
+    # 'mentions',
+    # 'leading_mentions',
+    # 'annotation_types',
+    # 'attachment_types',
+    # 'crosspost_url',
+    # 'crosspost_domain',
+    # 'place_id',
+    # 'is_reply',
+    # 'is_directed',
+    # 'has_location',
+    # 'has_checkin',
+    # 'is_crosspost',
+    # 'has_attachment',
+    # 'has_oembed_photo',
+    # 'has_oembed_video',
+    # 'has_oembed_html5video',
+    # 'has_oembed_rich',
+    # 'language',
+    # 'client_id',
+    # 'creator_id',
+    # 'reply_to',
+    # 'thread_id',
+# ]
+
+
+CHANNEL_PARAMS = [
+    'include_read',
+    'channel_types',
+    'exclude_channel_types',
+    'include_marker',
+    'include_inactive',
+    'include_raw',
+    'include_channel_raw',
+    'include_recent_message',
+    'include_limited_users',
+    'include_message_raw',
+    'include_html',
+    'include_user_html',
+    'include_counts',
+    'include_user',
+    'include_presence',
+    'include_user_raw',
+    
+]
+
+
+MESSAGE_PARAMS = [
+    'include_deleted',
+    'include_html',
+    'include_message_html',
+    'include_raw',
+    'include_message_raw',
+    'include_client',
+    'include_user_html',
+    'include_counts',
+    'include_user',
+    'include_presence',
+    'include_user_raw',
+]
+
+
+FILE_PARAMS = [
+    'file_types',
+    'include_incomplete',
+    'include_private',
+    'include_raw',
+    'include_file_raw',
+    'include_html',
+    'include_user_html',
+    'include_counts',
+    'include_user',
+    'include_presence',
+    'include_user_raw',
+]
+
+
+# PLACE_SEARCH_PARAMS = [
+    # 'latitude',
+    # 'longitude',
+    # 'q',
+    # 'radius',
+    # 'count',
+    # 'remove_closed',
+    # 'altitude',
+    # 'horizontal_accuracy',
+    # 'vertical_accuracy',
+# ]
+
+# APP_STREAM_PARAMS = [
+    # 'key',
+# ]
