57c4ccba9270055827ffea9a4f405dca21892923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:42 +0000

    Add package pokrok/0.0.1/pokrok-0.0.1.tar.gz

---
+++ packages/pokrok/pokrok-0.0.1.tar.gz/pokrok-0.0.1/pokrok/styles.py
@@ -0,0 +1,30 @@
+import enum
+
+
+class Widget(enum.Enum):
+    BAR = 'bar'
+    ETA = 'eta'
+    ELAPSED = 'elapsed'
+    SPINNER = 'spinner'
+
+
+class StyleManager(dict):
+    def __init__(self, default_style=None):
+        super().__init__()
+        self['default'] = default_style or Style()
+
+    def set_style_options(self, config, **kwargs):
+        pass
+
+
+class Style:
+    def __init__(self, widgets=None, sized=None, unsized=None):
+        if widgets:
+            self.sized = sized or widgets
+            self.unsized = unsized or widgets
+        else:
+            self.sized = sized
+            self.unsized = unsized
+
+    def get_widgets(self, sized):
+        return self.sized if sized else self.unsized
