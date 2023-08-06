97bd8729f5ed2a8dd7cb72cc1f586de3bfcd9486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:15 +0000

    Add package mycelery/1.0.5/mycelery-1.0.5.tar.gz

---
+++ packages/mycelery/mycelery-1.0.5.tar.gz/mycelery-1.0.5/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+PACKAGE = "mycelery"
+NAME = "mycelery"
+DESCRIPTION = "For mycelery! For Tim"
+AUTHOR = "Lei wenzheng"
+AUTHOR_EMAIL = "leiwz@kaisagroup.com"
+URL = "https://github.com/leiwenzheng/mycelery.git"
+VERSION = '1.0.5'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="Apache License, Version 2.0",
+    url=URL,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    entry_points={
+        'console_scripts': []
+    },
+    zip_safe=False,
+    install_requires=['pymongo >= 3.4.0']
+)
+
