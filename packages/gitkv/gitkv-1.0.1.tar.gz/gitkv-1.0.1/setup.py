1b8597d217b22a0322f91aa748d53992f0c16223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:04 +0000

    Add package gitkv/1.0.1/gitkv-1.0.1.tar.gz

---
+++ packages/gitkv/gitkv-1.0.1.tar.gz/gitkv-1.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from os import path
+
+__version__ = '1.0.1'
+
+here = path.abspath(path.dirname(__file__))
+
+install_requires = ['sphinx', 'flake8', 'coverage']
+dependency_links = []
+
+setup(
+    name='gitkv',
+    version=__version__,
+    description='Use a git repo as a key-value store.',
+    url='https://github.com/edouardklein/gitkv',
+    download_url='https://github.com/edouardklein/gitkv/tarball/' +
+    __version__,
+    license='AGPLv3',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='gitkv use a git repo as a key-value store.',
+    packages=['gitkv'],
+    include_package_data=True,
+    author='HaiLuan Nguyen, Edouard Klein',
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    author_email='edouardklein@gmailnospam.com'
+)
