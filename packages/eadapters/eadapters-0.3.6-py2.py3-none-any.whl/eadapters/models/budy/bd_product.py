058305bb3be6fdf9cc56171e071f3939c1f6f5b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:11 +0000

    Add package eadapters/0.3.6/eadapters-0.3.6-py2.py3-none-any.whl

---
+++ packages/eadapters/eadapters-0.3.6-py2.py3-none-any.whl/eadapters/models/budy/bd_product.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import commons
+
+from . import bd_common
+
+from .. import product
+
+class BDProduct(product.EProduct, bd_common.BDCommon):
+
+    @classmethod
+    def wrap(cls, models, build = True, handler = None, **kwargs):
+        def handler(model):
+            model.update(
+                name = model["short_description"],
+                code = model["product_id"],
+                currency = model.get("currency", "GBP")
+            )
+
+        return super(BDProduct, cls).wrap(
+            models,
+            build = build,
+            handler = handler,
+            **kwargs
+        )
+
+    def unwrap(self, **kwargs):
+        result = product.EProduct.unwrap(self, **kwargs)
+        result.update(
+            short_description = self.name
+        )
+        return result
+
+    def related(self, *args, **kwargs):
+        api = self._get_api()
+        product = api.related_product(self.id, *args, **kwargs)
+        return self.wrap(product)
+
+    def share(self, *args, **kwargs):
+        api = self._get_api()
+        share = api.share_product(self.id, *args, **kwargs)
+        return share
+
+    @property
+    def discount(self):
+        if not self.price: return commons.Decimal(0.0)
+        if not self.price_compare: return commons.Decimal(0.0)
+        return self.price_compare - self.price
+
+    @property
+    def discount_percent(self):
+        if not self.discount: return commons.Decimal(0.0)
+        return self.discount / self.price_compare * commons.Decimal(100.0)
+
+    @property
+    def is_discounted(self):
+        return self.discount > 0.0
