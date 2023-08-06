8303aa9265e7e483b58f84a2e551f73965d39fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:24 +0000

    Add package mothur-py/0.2.5/mothur_py-0.2.5.tar.gz

---
+++ packages/mothur-py/mothur_py-0.2.5.tar.gz/mothur_py-0.2.5/mothur_py/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2017, Richard Campen
+All rights reserved.
+
+Licensed under the Modified BSD License.
+For full license terms see LICENSE.txt
+
+"""
+
+# mothur-py v0.2.5
+
+from .core import Mothur, MothurCommand
