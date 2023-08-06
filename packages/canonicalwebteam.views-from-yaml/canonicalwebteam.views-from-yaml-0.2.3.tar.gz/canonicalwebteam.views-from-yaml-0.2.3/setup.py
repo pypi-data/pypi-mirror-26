9be3a4a6531958fdac8d35eb1c6da4c2525bb543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:41 +0000

    Add package canonicalwebteam.views-from-yaml/0.2.3/canonicalwebteam.views-from-yaml-0.2.3.tar.gz

---
+++ packages/canonicalwebteam.views-from-yaml/canonicalwebteam.views-from-yaml-0.2.3.tar.gz/canonicalwebteam.views-from-yaml-0.2.3/setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='canonicalwebteam.views-from-yaml',
+    version='0.2.3',
+    author='Canonical webteam',
+    author_email='robin+pypi@canonical.com',
+    url='https://github.com/canonical-webteam/views-from-yaml',
+    packages=[
+        'canonicalwebteam.views_from_yaml',
+    ],
+    description=(
+        'A helper function for creating Django views from a YAML file of URL paths'
+    ),
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'Django >= 1.3',
+        'PyYaml >= 3.0'
+    ],
+)
+
