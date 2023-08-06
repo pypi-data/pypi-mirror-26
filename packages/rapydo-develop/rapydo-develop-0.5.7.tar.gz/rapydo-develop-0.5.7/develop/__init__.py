faaec219d5382bc92e34169a2577117620609508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:08 +0000

    Add package rapydo-develop/0.5.7/rapydo-develop-0.5.7.tar.gz

---
+++ packages/rapydo-develop/rapydo-develop-0.5.7.tar.gz/rapydo-develop-0.5.7/develop/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.5.7'
+
+# ORDER: utilities > core > controller > develop > builds > http-api
+TOOLS = [
+    'utilities',
+    # 'core',
+    'controller',
+    'develop',
+    'builds',
+    'http',
+]
