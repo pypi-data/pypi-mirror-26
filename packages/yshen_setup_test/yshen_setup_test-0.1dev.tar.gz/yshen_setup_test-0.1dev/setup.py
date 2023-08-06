cb448d11a1ae9bb3aa134351c1964828eefeb651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:30 +0000

    Add package yshen_setup_test/0.1dev/yshen_setup_test-0.1dev.tar.gz

---
+++ packages/yshen_setup_test/yshen_setup_test-0.1dev.tar.gz/yshen_setup_test-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="yshen_setup_test",
+    version="0.1dev",
+    packages=['src',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read()
+)
