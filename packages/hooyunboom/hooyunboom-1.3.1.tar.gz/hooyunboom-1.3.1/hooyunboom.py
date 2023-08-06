807dbd836c6f9b46f10efafd4314f8198223c0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:49 +0000

    Add package hooyunboom/1.3.1/hooyunboom-1.3.1.tar.gz

---
+++ packages/hooyunboom/hooyunboom-1.3.1.tar.gz/hooyunboom-1.3.1/hooyunboom.py
@@ -0,0 +1,15 @@
+"""
+This is hooyunboom's first python module,it's great~
+"""
+
+import sys
+
+def print_lol(the_list,indent=False,level=0,fh=sys.stdout):
+    """This module reading everyone in a list"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1,fh)
+        else:
+            if indent:
+                print("\t"*level,end='',file=fh)
+            print(each_item,file=fh)
---
+++ packages/hooyunboom/hooyunboom-1.3.1.tar.gz/hooyunboom-1.3.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name         ='hooyunboom',
+    version      ='1.3.1',
+    py_modules   =['hooyunboom'],
+    author       ='hooyunboom',
+    author_email ='hooyunboom@gmail.com',
+    description  ='This module is from hooyunboom'
+)
