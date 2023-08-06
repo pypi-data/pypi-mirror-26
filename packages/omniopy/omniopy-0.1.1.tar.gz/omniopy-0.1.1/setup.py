ad005806de1f054076ba1576f8693a82fcaa6fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:41 +0000

    Add package omniopy/0.1.1/omniopy-0.1.1.tar.gz

---
+++ packages/omniopy/omniopy-0.1.1.tar.gz/omniopy-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'omniopy',
+    packages = ['omniopy'],
+    version = '0.1.1',
+    description = 'A general purpose library containing useful functions for most scenarios.',
+    author = 'Omnio Creative Limited',
+    author_email = 'lukec@omnio.xyz',
+    license = 'Unlicense',
+    url = 'https://omnio.xyz',
+    keywords = ['library', 'general', 'purpose', 'api', 'string', 'number', 'functions', 'development'],
+    classifiers = []
+)
