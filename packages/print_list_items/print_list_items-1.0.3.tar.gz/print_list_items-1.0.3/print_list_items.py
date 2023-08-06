15c6753cc5aff69d1307293c046aefd25d010038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:47 +0000

    Add package print_list_items/1.0.3/print_list_items-1.0.3.tar.gz

---
+++ packages/print_list_items/print_list_items-1.0.3.tar.gz/print_list_items-1.0.3/print_list_items.py
@@ -0,0 +1,10 @@
+'''this function is to print all list value which including or not including list'''
+def print_lol(the_list,indent=False,level=0):
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item,indent,level+1)
+		else:
+			if indent:
+				for tab_stop in range(level):
+					print("\t",end='')
+			print(each_item)
