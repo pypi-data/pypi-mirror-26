c3da6d17df67f4dc0a200b7560ef9e5d73a5b638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:35 +0000

    Add package dxl-pygate/0.8.1/dxl-pygate-0.8.1.tar.gz

---
+++ packages/dxl-pygate/dxl-pygate-0.8.1.tar.gz/dxl-pygate-0.8.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='dxl-pygate',
+      version='0.8.1',
+      description='A simplified python interface for Gate.',
+      url='https://github.com/Hong-Xiang/pygate',
+      author='Hong Xiang',
+      author_email='hx.hongxiang@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['fs', 'click', 'pyyaml',
+                        'rx', 'jinja2', 'dxl-dxpy>=0.5'],
+      scripts=['bin/pygate'],
+      zip_safe=False)
