d02f03c64e2d2fde8bd394e2431b1eb524722501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:22 +0000

    Add package spoilerfy/1.1/spoilerfy-1.1.tar.gz

---
+++ packages/spoilerfy/spoilerfy-1.1.tar.gz/spoilerfy-1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='spoilerfy',
+      version='1.01',
+      description='Format your code for /r/dailyprogrammer',
+      url='https://www.github.com/rodenmonte/spoilerfy',
+      author='Monte Roden',
+      author_email='rodenmonte@gmail.com',
+      license='MIT',
+      packages=['spoilerfy'],
+      install_requires=[
+        'pyperclip'
+      ],
+      zip_safe=False,
+      entry_points = {
+        'console_scripts': ['spoilerfy=spoilerfy.command_line:main']
+      })
