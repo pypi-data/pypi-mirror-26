47f6ccb838a96178cf49d93f2c4745c853b6540e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:29 +0000

    Add package cloud-training/0.1.0/cloud_training-0.1.0.tar.gz

---
+++ packages/cloud-training/cloud_training-0.1.0.tar.gz/cloud_training-0.1.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='cloud_training',
+      version='0.1.0',
+      description='Train models on AWS Spot Instances',
+      url='http://github.com/apls777/cloud-training',
+      author='Oleg Polosin',
+      author_email='apls777@gmail.com',
+      license='MIT',
+      packages=['cloud_training', 'cloud_training.commands'],
+      package_data={'cloud_training': ['data/launch-specification.json', 'data/unzip.py', 'data/user_data.sh']},
+      scripts=['bin/cloud-training'],
+      install_requires=['awscli>=1.11.0'],
+      classifiers=(
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Intended Audience :: System Administrators',
+            'Natural Language :: English',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+      ))
