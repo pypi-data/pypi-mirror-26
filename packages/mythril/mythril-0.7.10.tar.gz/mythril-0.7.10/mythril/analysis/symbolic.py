1ac7e3938540b8a5c3e87db11beee18aff4bc0d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:36 +0000

    Add package mythril/0.7.10/mythril-0.7.10.tar.gz

---
+++ packages/mythril/mythril-0.7.10.tar.gz/mythril-0.7.10/mythril/analysis/symbolic.py
@@ -0,0 +1,31 @@
+from laser.ethereum import svm
+from .modules import unchecked_suicide, ether_send
+import logging
+
+
+class StateSpace:
+
+    '''
+    Symbolic EVM wrapper
+    '''
+    
+    def __init__(self, contracts, dynloader = None, simplified=True):
+
+        # Convert ETHContract objects to LASER SVM "modules"
+
+        modules = {}
+
+        for contract in contracts:
+            modules[contract.address] = contract.as_dict()
+
+        _svm = svm.SVM(modules, simplified=True, dynamic_loader=dynloader)
+
+        _svm.sym_exec(contracts[0].address)
+
+        self.modules = modules
+        self.nodes = _svm.nodes
+        self.edges = _svm.edges
+
+        # Analysis
+
+
