da268c6a78be59ada6e96fdb689772541110f37a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:46 +0000

    Add package marabunta/0.7.3/marabunta-0.7.3-py2.py3-none-any.whl

---
+++ packages/marabunta/marabunta-0.7.3-py2.py3-none-any.whl/marabunta/model.py
@@ -0,0 +1,266 @@
+# -*- coding: utf-8 -*-
+# © 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+import shlex
+import sys
+
+from distutils.version import StrictVersion
+try:  # Python 2.x
+    from cStringIO import StringIO
+except ImportError:  # Python 3.x
+    from io import StringIO
+
+import pexpect
+
+from .exception import ConfigurationError, OperationError
+from .helpers import string_types
+
+
+class Migration(object):
+
+    def __init__(self, versions):
+        self._versions = versions
+
+    @property
+    def versions(self):
+        return sorted(self._versions, key=lambda v: StrictVersion(v.number))
+
+
+class MigrationOption(object):
+
+    def __init__(self, install_command=None, install_args=None):
+        self.install_command = install_command or u'odoo'
+        self.install_args = install_args or u''
+
+
+class Version(object):
+
+    def __init__(self, number, options):
+        try:
+            StrictVersion().parse(number)
+        except ValueError:
+            raise ConfigurationError(
+                u'{} is not a valid version'.format(number)
+            )
+        self.number = number
+        self._version_modes = {}
+        self.options = options
+
+    def is_processed(self, db_versions):
+        return self.number in (v.number for v in db_versions if v.date_done)
+
+    def is_noop(self):
+        has_operations = [mode.pre_operations or mode.post_operations
+                          for mode in self._version_modes.values()]
+        has_upgrade_addons = [mode.upgrade_addons or mode.remove_addons
+                              for mode in self._version_modes.values()]
+        noop = (not has_operations and not has_upgrade_addons)
+        return noop
+
+    def skip(self, db_versions):
+        """ Version is either noop either it has been processed already """
+        return self.is_noop() or self.is_processed(db_versions)
+
+    def _get_version_mode(self, mode=None):
+        """ Return a VersionMode for a mode name.
+
+        When the mode is None, we are working with the 'base' mode.
+
+        """
+        version_mode = self._version_modes.get(mode)
+        if not version_mode:
+            version_mode = self._version_modes[mode] = VersionMode(name=mode)
+        return version_mode
+
+    def add_operation(self, operation_type, operation, mode=None):
+        """ Add an operation to the version
+
+        :param mode: Name of the mode in which the operation is executed
+        :type mode: str
+        :param operation_type: one of 'pre', 'post'
+        :type operation_type: str
+        :param operation: the operation to add
+        :type operation: :class:`marabunta.model.Operation`
+        """
+        version_mode = self._get_version_mode(mode=mode)
+        if operation_type == 'pre':
+            version_mode.add_pre(operation)
+        elif operation_type == 'post':
+            version_mode.add_post(operation)
+        else:
+            raise ConfigurationError(
+                u"Type of operation must be 'pre' or 'post', got %s" %
+                (operation_type,)
+            )
+
+    def add_upgrade_addons(self, addons, mode=None):
+        version_mode = self._get_version_mode(mode=mode)
+        version_mode.add_upgrade_addons(addons)
+
+    def add_remove_addons(self, addons, mode=None):
+        version_mode = self._get_version_mode(mode=mode)
+        version_mode.add_remove_addons(addons)
+        raise ConfigurationError(
+            u'Removing addons is not yet supported because it cannot be done '
+            u'using the command line. You have to uninstall addons using '
+            u'an Odoo (\'import openerp\') script'
+        )
+
+    def pre_operations(self, mode=None):
+        """ Return pre-operations only for the mode asked """
+        version_mode = self._get_version_mode(mode=mode)
+        return version_mode.pre_operations
+
+    def post_operations(self, mode=None):
+        """ Return post-operations only for the mode asked """
+        version_mode = self._get_version_mode(mode=mode)
+        return version_mode.post_operations
+
+    def upgrade_addons_operation(self, addons_state, mode=None):
+        """ Return merged set of main addons and mode's addons """
+        installed = set(a.name for a in addons_state
+                        if a.state in ('installed', 'to upgrade'))
+
+        base_mode = self._get_version_mode()
+        addons_list = base_mode.upgrade_addons.copy()
+        if mode:
+            add_mode = self._get_version_mode(mode=mode)
+            addons_list |= add_mode.upgrade_addons
+
+        to_install = addons_list - installed
+        to_upgrade = installed & addons_list
+
+        return UpgradeAddonsOperation(self.options, to_install, to_upgrade)
+
+    def remove_addons_operation(self):
+        raise NotImplementedError
+
+    def __repr__(self):
+        return u'Version<{}>'.format(self.number)
+
+
+class VersionMode(object):
+
+    def __init__(self, name=None):
+        self.name = name
+        self.pre_operations = []
+        self.post_operations = []
+        self.upgrade_addons = set()
+        self.remove_addons = set()
+
+    def add_pre(self, operation):
+        self.pre_operations.append(operation)
+
+    def add_post(self, operation):
+        self.post_operations.append(operation)
+
+    def __repr__(self):
+        name = self.name if self.name else 'base'
+        return u'VersionMode<{}>'.format(name)
+
+    def add_upgrade_addons(self, addons):
+        self.upgrade_addons.update(addons)
+
+    def add_remove_addons(self, addons):
+        self.remove_addons.update(addons)
+        raise ConfigurationError(
+            u'Removing addons is not yet supported because it cannot be done '
+            u'using the command line. You have to uninstall addons using '
+            u'an Odoo (\'import openerp\') script'
+        )
+
+
+class UpgradeAddonsOperation(object):
+
+    def __init__(self, options, to_install, to_upgrade):
+        self.options = options
+        self.to_install = set(to_install)
+        self.to_upgrade = set(to_upgrade)
+
+    def operation(self, exclude_addons=None):
+        if exclude_addons is None:
+            exclude_addons = set()
+        install_command = self.options.install_command
+        install_args = self.options.install_args[:] or []
+        install_args += [u'--workers=0', u'--stop-after-init', u'--no-xmlrpc']
+
+        to_install = self.to_install - exclude_addons
+        if to_install:
+            install_args += [u'-i', u','.join(to_install)]
+
+        to_upgrade = self.to_upgrade - exclude_addons
+        if to_upgrade:
+            install_args += [u'-u', u','.join(to_upgrade)]
+
+        if to_install or to_upgrade:
+            return Operation([install_command] + install_args)
+        else:
+            return Operation('')
+
+
+class Operation(object):
+
+    def __init__(self, command):
+        if isinstance(command, string_types):
+            command = self._shlex_split_unicode(command)
+        self.command = command
+
+    @staticmethod
+    def _shlex_split_unicode(command):
+        return [l.decode('utf8') for l in shlex.split(command.encode('utf8'))]
+
+    def __nonzero__(self):
+        return bool(self.command)
+
+    def _execute(self, log, interactive=True):
+        child = pexpect.spawn(self.command[0].encode('utf8'),
+                              [l.encode('utf8') for l in self.command[1:]],
+                              timeout=None,
+                              )
+        # interact() will transfer the child's stdout to
+        # stdout, but we also copy the output in a buffer
+        # so we can save the logs in the database
+        log_buffer = StringIO()
+        if interactive:
+            child.logfile = log_buffer
+            # use the interactive mode so we can use pdb in the
+            # migration scripts
+            child.interact()
+        else:
+            # set the logfile to stdout so we have an unbuffered
+            # output
+            child.logfile = sys.stdout
+            child.expect(pexpect.EOF)
+            # child.before contains all the the output of the child program
+            # before the EOF
+            log_buffer.write(child.before)
+        child.close()
+        if child.signalstatus is not None:
+            raise OperationError(
+                u"command '{}' has been interrupted by signal {}".format(
+                    ' '.join(self.command),
+                    child.signalstatus
+                )
+            )
+        elif child.exitstatus != 0:
+            raise OperationError(
+                u"command '{}' returned {}".format(
+                    ' '.join(self.command),
+                    child.exitstatus
+                )
+            )
+        log_buffer.seek(0)
+        # the pseudo-tty used for the child process returns
+        # lines with \r\n endings
+        log('\n'.join(log_buffer.read().splitlines())
+                .decode('utf-8', errors='replace'),
+            decorated=False,
+            stdout=False)
+
+    def execute(self, log):
+        log(u'{}'.format(u' '.join(self.command)))
+        self._execute(log, interactive=sys.stdout.isatty())
+
+    def __repr__(self):
+        return u'Operation<{}>'.format(' '.join(self.command))
---
+++ packages/marabunta/marabunta-0.7.3-py2.py3-none-any.whl/marabunta/runner.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+# © 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+import traceback
+
+from datetime import datetime
+from distutils.version import StrictVersion
+
+from .database import IrModuleModule
+from .exception import MigrationError
+from .output import print_decorated, safe_print
+
+LOG_DECORATION = u'|> '
+
+
+class Runner(object):
+
+    def __init__(self, config, migration, database, table):
+        self.config = config
+        self.migration = migration
+        self.database = database
+        self.table = table
+        # we keep the addons upgrading during a run in this set,
+        # this is only useful when using 'allow_serie',
+        # if an addon has just been installed or updated,
+        # we don't want to do it again for another version
+        self.upgraded_addons = set()
+
+    def log(self, message):
+        print_decorated(u'migration: {}'.format(message))
+
+    def perform(self):
+        self.table.create_if_not_exists()
+
+        db_versions = self.table.versions()
+
+        if not self.config.force_version:
+            unfinished = [db_version for db_version
+                          in db_versions
+                          if not db_version.date_done]
+            if unfinished:
+                raise MigrationError(
+                    u'Upgrade of version {} has been attempted and failed. '
+                    u'You may want to restore the backup or to run again the '
+                    u'migration with the MARABUNTA_FORCE_VERSION '
+                    u'environment variable '
+                    u'or to fix it manually (in that case, you will have to '
+                    u'update the  \'marabunta_version\' table yourself.'
+                    .format(u','.join(v.number for v in unfinished))
+                )
+
+        unprocessed = [version for version in self.migration.versions
+                       if not version.skip(db_versions)]
+
+        if not self.config.allow_serie:
+            if len(unprocessed) > 1:
+                raise MigrationError(
+                    u'Only one version can be upgraded at a time.\n'
+                    u'The following versions need to be applied: {}.\n'.format(
+                        [v.number for v in unprocessed]
+                        )
+                )
+
+        if not self.config.force_version and db_versions and unprocessed:
+            installed = max(StrictVersion(v.number) for v in db_versions)
+            next_unprocess = min(StrictVersion(v.number) for v in unprocessed)
+            if installed > next_unprocess:
+                raise MigrationError(
+                    u'The version you are trying to install ({}) is below '
+                    u'the current database version.'.format(
+                        next_unprocess, installed
+                    )
+                )
+
+        for version in self.migration.versions:
+            # when we force-execute one version, we skip all the others
+            if self.config.force_version:
+                if self.config.force_version != version.number:
+                    continue
+                else:
+                    self.log(
+                        u'force-execute version {}'.format(version.number)
+                    )
+
+            self.log(u'processing version {}'.format(version.number))
+            VersionRunner(self, version).perform()
+
+
+class VersionRunner(object):
+
+    def __init__(self, runner, version):
+        self.runner = runner
+        self.table = runner.table
+        self.migration = runner.migration
+        self.config = runner.config
+        self.database = runner.database
+        self.version = version
+        self.logs = []
+
+    def log(self, message, decorated=True, stdout=True):
+        self.logs.append(message)
+        if not stdout:
+            return
+        if decorated:
+            app_message = u'version {}: {}'.format(
+                self.version.number,
+                message,
+            )
+            print_decorated(app_message)
+        else:
+            safe_print(message)
+
+    def start(self):
+        self.log(u'start')
+        self.table.start_version(self.version.number, datetime.now())
+
+    def finish(self):
+        self.log(u'done')
+        module_table = IrModuleModule(self.database)
+        addons_state = module_table.read_state()
+        self.table.finish_version(self.version.number, datetime.now(),
+                                  u'\n'.join(self.logs),
+                                  [state._asdict() for state in addons_state])
+
+    def perform(self):
+        db_versions = self.table.versions()
+
+        version = self.version
+        if (version.is_processed(db_versions) and
+                not self.config.force_version == self.version.number):
+            self.log(
+                u'version {} is already installed'.format(version.number)
+            )
+            return
+
+        self.start()
+        try:
+            self.perform_version(version)
+        except Exception:
+            error = u'\n'.join(
+                self.logs +
+                [u'\n', traceback.format_exc().decode('utf8', errors='ignore')]
+            )
+            self.table.record_log(version.number, error)
+            raise
+        self.finish()
+
+    def perform_version(self, version):
+        if version.is_noop():
+            self.log(u'version {} is a noop'.format(version.number))
+
+        else:
+            self.log(u'execute base pre-operations')
+            for operation in version.pre_operations():
+                operation.execute(self.log)
+            if self.config.mode:
+                self.log(u'execute %s pre-operations' % self.config.mode)
+                for operation in version.pre_operations(mode=self.config.mode):
+                    operation.execute(self.log)
+
+            self.perform_addons()
+
+            self.log(u'execute base post-operations')
+            for operation in version.post_operations():
+                operation.execute(self.log)
+            if self.config.mode:
+                self.log(u'execute %s post-operations' % self.config.mode)
+                for operation in version.post_operations(self.config.mode):
+                    operation.execute(self.log)
+
+    def perform_addons(self):
+        version = self.version
+
+        module_table = IrModuleModule(self.database)
+        addons_state = module_table.read_state()
+
+        upgrade_operation = version.upgrade_addons_operation(
+            addons_state,
+            mode=self.config.mode
+        )
+        # exclude the addons already installed or updated during this run
+        # when 'allow_serie' is active
+        exclude = self.runner.upgraded_addons
+        self.log(u'installation / upgrade of addons')
+        operation = upgrade_operation.operation(exclude_addons=exclude)
+        if operation:
+            operation.execute(self.log)
+        self.runner.upgraded_addons |= (upgrade_operation.to_install |
+                                        upgrade_operation.to_upgrade)
