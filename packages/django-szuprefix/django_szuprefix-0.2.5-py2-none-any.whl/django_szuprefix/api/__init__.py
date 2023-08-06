c293dd4aef7e6136e0eca041bfa186e53751a846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:22 +0000

    Add package django-szuprefix/0.2.5/django_szuprefix-0.2.5-py2-none-any.whl

---
+++ packages/django-szuprefix/django_szuprefix-0.2.5-py2-none-any.whl/django_szuprefix/api/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+from django.conf.urls import url
+from django.utils.module_loading import autodiscover_modules
+
+from django_szuprefix.utils import modelutils
+from django_szuprefix.utils.views import csrf_token
+
+__author__ = 'denishuang'
+from rest_framework import serializers, routers
+
+
+class newRouter(routers.DefaultRouter):
+    def get_urls(self):
+        urls = super(newRouter, self).get_urls()
+        urls.append(url(r'^csrf_token/$', csrf_token, name='csrf_token'), )
+        return urls
+
+router = newRouter()
+
+
+def autodiscover():
+    # print "autodiscover"
+    autodiscover_modules('apis')
+
+
+def register(package, resource, viewset):
+    # print "%s/%s" % (package, resource)
+    router.register("%s/%s" % (package, resource), viewset)
+
+
+serializers.ModelSerializer.serializer_field_mapping.update({modelutils.JSONField: serializers.JSONField})
+autodiscover()
