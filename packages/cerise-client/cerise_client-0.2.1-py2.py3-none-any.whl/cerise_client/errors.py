f6e33df475e51ec6fbd6980af38576ea6eebb447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:41 +0000

    Add package cerise-client/0.2.1/cerise_client-0.2.1-py2.py3-none-any.whl

---
+++ packages/cerise-client/cerise_client-0.2.1-py2.py3-none-any.whl/cerise_client/errors.py
@@ -0,0 +1,63 @@
+class ServiceNotFound(Exception):
+    """
+    The given service does not exist on this machine. Either it was
+    never created, or it was destroyed, or the Docker container running
+    it was destroyed by an outside influence.
+    """
+    pass
+
+class ServiceAlreadyExists(Exception):
+    """
+    A service with the given name already exists on this machine, so
+    another one cannot be created.
+    """
+    pass
+
+class PortNotAvailable(Exception):
+    """
+    The requested port is occupied by another service, or by some
+    other program that has bound it on localhost. Either stop the
+    blocking service or program, or try again with a different
+    port number.
+    """
+    pass
+
+class JobNotFound(Exception):
+    """
+    The given job does not exist on this service. Either it never
+    existed because it hasn't been submitted yet (did you call
+    job.run()?), or it was deleted.
+    """
+    pass
+
+class JobAlreadyExists(Exception):
+    """
+    You tried to create a job with a name that already existed on this
+    service. Jobs must have a unique name.
+    """
+    pass
+
+class InvalidJob(Exception):
+    """
+    You submitted an invalid job. Did you forget to add a workflow?
+    """
+    pass
+
+class UnknownInput(Exception):
+    """
+    You tried to set the value for an input that is not in the workflow
+    for the job you are creating.
+    """
+    pass
+
+class MissingOutput(Exception):
+    """
+    The output returned by the service does not refer to an existing
+    file. Maybe the job was deleted?
+    """
+
+class CommunicationError(Exception):
+    """
+    There was an error communicating with the service.
+    """
+    pass
