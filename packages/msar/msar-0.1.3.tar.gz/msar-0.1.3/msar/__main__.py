9ef9123f08832d8cb2a13673f5ffd46d68220f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:07 +0000

    Add package msar/0.1.3/msar-0.1.3.tar.gz

---
+++ packages/msar/msar-0.1.3.tar.gz/msar-0.1.3/msar/__main__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 10/11/2017
+
+@author: seven
+"""
+
+from .command_line import main
+
+main()
---
+++ packages/msar/msar-0.1.3.tar.gz/msar-0.1.3/msar/command_line.py
@@ -0,0 +1,10 @@
+"""
+Created on 10/11/2017
+
+@author: seven
+"""
+import msar
+
+
+def main():
+    msar().run()
