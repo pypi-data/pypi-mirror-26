9d2a5dd851d098f753cec3dae962a6004c783777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:26 +0000

    Add package loop1/1.0/loop1-1.0.tar.gz

---
+++ packages/loop1/loop1-1.0.tar.gz/loop1-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "loop1",
+    version = "1.0",
+    py_modules = ["loop1"],
+    author = "yuejing08",
+    author_email = "568623642@qq.com",
+    url = "https://github.com/yuejing08/hello",
+    description = "just a test."
+    )
