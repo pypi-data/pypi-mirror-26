5af790a96d257cd88bd954ac4a0cd2f640e5273d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:37 +0000

    Add package bleson/0.0.2/bleson-0.0.2.tar.gz

---
+++ packages/bleson/bleson-0.0.2.tar.gz/bleson-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bleson',
+    version='0.0.2',
+    packages= find_packages(),
+    url='https://github.com/TheCellule/python-bleson',
+    license='MIT',
+    author='TheCellule',
+    author_email='thecellule@gmail.com',
+    description='Bluetooth LE Library'
+)
