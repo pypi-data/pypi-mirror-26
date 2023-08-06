e13843d47a714a59d3ca307d8ca8353bb33a3eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:06 +0000

    Add package breach-buster-redux/0.1/breach-buster-redux-0.1.tar.gz

---
+++ packages/breach-buster-redux/breach-buster-redux-0.1.tar.gz/breach-buster-redux-0.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+breach_buster
+=================
+
+Replacement for Django's gzip middleware.  Protects against BREACH.
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='breach-buster-redux',
+    version='0.1',
+    author='Adam DePrince',
+    author_email='adeprince@nypublicradio.org',
+    description='BREACH resistant gzip middleware for Django',
+    url="https://github.com/miki725/breach_buster",
+    long_description=__doc__,
+    py_modules=[
+        'breach_buster/__init__',
+        'breach_buster/middleware/__init__',
+        'breach_buster/middleware/gzip',
+        'breach_buster/examples/__init__',
+        'breach_buster/examples/demo_server',
+    ],
+    packages=['breach_buster',],
+    zip_safe=True,
+    license='GPLv3',
+    include_package_data=True,
+    classifiers=[],
+    install_requires=[
+        'django',
+        'six',
+    ],
+    tests_require=[
+        'web.py',
+    ]
+)
