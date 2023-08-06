968ef2ee50fecee0ceb7981e1035fa39a9ac3728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:53 +0000

    Add package django-opt-out/0.1.0/django-opt-out-0.1.0.tar.gz

---
+++ packages/django-opt-out/django-opt-out-0.1.0.tar.gz/django-opt-out-0.1.0/src/django_opt_out/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+"""Top-level package for Django Opt-out application."""
+
+__author__ = """Janusz Skonieczny"""
+__email__ = 'js+pypi@bravelabs.pl'
+__version__ = '0.1.0'
+
+from django.conf import settings
+
+
+
+
+
