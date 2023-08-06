323cc7904531f104de90e186e1803b6d27eea702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:10 +0000

    Add package pyexpect/1.0.17/pyexpect-1.0.17.tar.gz

---
+++ packages/pyexpect/pyexpect-1.0.17.tar.gz/pyexpect-1.0.17/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf8
+
+from setuptools import setup, find_packages
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+def readme():
+    "Falls back to just file().read() on any error, because the conversion to rst is only really relevant when uploading the package to pypi"
+    from subprocess import CalledProcessError
+    try:
+        from subprocess import check_output
+        return str(check_output(['pandoc', '--from', 'markdown', '--to', 'rst', os.path.join(here, 'README.md')]))
+    except (ImportError, OSError, CalledProcessError) as error:
+        print('python2.6 and pandoc is required to get the description as rst (as required to get nice rendering in pypi) - using the original markdown instead.',
+              'See http://johnmacfarlane.net/pandoc/')
+    return str(open(os.path.join(here, 'README.md')).read())
+
+setup(
+    name='pyexpect',
+    version='1.0.17',
+    description='Python expectaton library',
+    long_description=readme(),
+    author='Martin Häcker',
+    author_email='spamfaenger@gmx.de',
+    license='ISC',
+    url='https://bitbucket.org/dwt/pyexpect',
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite="tests",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+)
