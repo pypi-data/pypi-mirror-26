405381f08cdf6242fe0f705f1b86ec506f4cd644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:02 +0000

    Add package wedoca-hello/0.1/wedoca_hello-0.1.tar.gz

---
+++ packages/wedoca-hello/wedoca_hello-0.1.tar.gz/wedoca_hello-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='wedoca_hello',
+    version='0.1',
+    packages=['wedoca_hello'],
+    url='https://github.com/wedoca',
+    license='Apache 2.0',
+    author='wedoca',
+    author_email='wedoca@gmail.com',
+    description='A random test lib',
+    keywords = ['testing', 'logging', 'example'],
+    classifiers = [],
+)
