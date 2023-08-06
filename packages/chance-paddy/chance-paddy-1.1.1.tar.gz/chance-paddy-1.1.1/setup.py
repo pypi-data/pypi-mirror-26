dd04b18d7c1c1f8e5115be2bb444ce95b6d76fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:19 +0000

    Add package chance-paddy/1.1.1/chance-paddy-1.1.1.tar.gz

---
+++ packages/chance-paddy/chance-paddy-1.1.1.tar.gz/chance-paddy-1.1.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File: setup.py
+# Author: Jimin Huang <huangjimin@whu.edu.cn>
+# Date: 19.10.2017
+from setuptools import setup
+
+from paddy import __version__
+
+
+setup(
+    name='chance-paddy',
+    version=__version__,
+    description='The base trader for chancefocus',
+    url='https://gitee.com/QianFuFinancial/paddy.git',
+    author='Jimin Huang',
+    author_email='huangjimin@whu.edu.cn',
+    license='MIT',
+    packages=['paddy'],
+    install_requires=[
+        'nose>=1.3.7',
+        'coverage>=4.1',
+        'requests>=2.13.0',
+        'chance-mock-logger>=0.0.1',
+    ],
+    zip_safe=False,
+)
