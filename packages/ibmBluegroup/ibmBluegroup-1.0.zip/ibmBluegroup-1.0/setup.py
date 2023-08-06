5985087417f9e35e3a09a435ce6fe295ffec6e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:46 +0000

    Add package ibmBluegroup/1.0/ibmBluegroup-1.0.zip

---
+++ packages/ibmBluegroup/ibmBluegroup-1.0.zip/ibmBluegroup-1.0/setup.py
@@ -0,0 +1,17 @@
+ #!/usr/bin/env python3
+ # -*- coding: utf-8 -*- 
+from setuptools import setup
+
+setup(name='ibmBluegroup',      
+      version='1.0',
+      description="IBM Bluegroup API",
+      keywords='Bluegroup',
+      author='ThomasIBM', 
+      author_email='guojial@cn.ibm.com',
+      url='https://github.com/ThomasIBM',
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'httplib2',
+      ],
+)
