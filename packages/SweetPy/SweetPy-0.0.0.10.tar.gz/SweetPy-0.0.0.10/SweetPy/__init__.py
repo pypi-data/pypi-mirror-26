8f080a65682ad460ca1ac36eb424055852387211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:06 +0000

    Add package SweetPy/0.0.0.10/SweetPy-0.0.0.10.tar.gz

---
+++ packages/SweetPy/SweetPy-0.0.0.10.tar.gz/SweetPy-0.0.0.10/SweetPy/__init__.py
@@ -0,0 +1,17 @@
+from rest_framework import response
+import SweetPy.response_plus
+
+response.Response = SweetPy.response_plus.Response
+from rest_framework import mixins
+import SweetPy.mixins_plus
+
+mixins.ListModelMixin = SweetPy.mixins_plus.ListModelMixin
+mixins.RetrieveModelMixin = SweetPy.mixins_plus.RetrieveModelMixin
+mixins.DestroyModelMixin = SweetPy.mixins_plus.DestroyModelMixin
+mixins.CreateModelMixin = SweetPy.mixins_plus.CreateModelMixin
+from rest_framework import views
+import SweetPy.view_plus
+
+views.exception_handler = SweetPy.view_plus.exception_handler
+import SweetPy.swagger_plus
+import SweetPy.sweet_setting
