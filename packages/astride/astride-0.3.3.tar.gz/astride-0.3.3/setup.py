b0c3d20189d11185a50fd00198f524c0456a551c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:33 +0000

    Add package astride/0.3.3/astride-0.3.3.tar.gz

---
+++ packages/astride/astride-0.3.3.tar.gz/astride-0.3.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='astride',
+    version='0.3.3',
+    description='Automated Streak Detection for High Velocity Objects',
+    long_description=readme(),
+    platforms=['any'],
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/dwkim78/astride',
+    license='MIT',
+    author='Dae-Won Kim',
+    author_email='dwkim78@gmail.com',
+    install_requires=['numpy>=1.11.2', 'photutils>=0.4', 'astropy>=1.2',
+                      'matplotlib>=1.5.3', 'scipy>=0.18.1',
+                      'scikit-image>=0.12.3'],
+    keywords=['astronomy', 'image', 'streak', 'satellite', 'meteor', 'NEO',
+              'fast-moving objects', 'boundary-tracing', 'contour-tracing'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Astronomy'
+    ]
+)
