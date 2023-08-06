a610c180bffca3635561c1a45f8ba4768fabd84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:14 +0000

    Add package nester-example/1.3.1/nester_example-1.3.1.zip

---
+++ packages/nester-example/nester_example-1.3.1.zip/nester_example-1.3.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester_example',
+        version = '1.3.1',
+        py_modules = ['nester'],
+        author = 'ccoffman5984',
+        author_email = 'hfpython@headfirstlabs.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+      )
