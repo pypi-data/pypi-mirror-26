782fcbc0bd0f89dbc9d89994209c5c0c9c02f0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:48 +0000

    Add package resnet-tensorflow/0.0.1/resnet-tensorflow-0.0.1.tar.gz

---
+++ packages/resnet-tensorflow/resnet-tensorflow-0.0.1.tar.gz/resnet-tensorflow-0.0.1/resnet/model.py
@@ -0,0 +1,212 @@
+from typing import List, Tuple
+
+import tensorflow as tf
+from tensorflow.contrib.layers import xavier_initializer
+from tensorflow.contrib.layers import l1_regularizer, l2_regularizer
+
+
+class ResNet(object):
+    """
+    Deep Residual Network for CIFAR-10 image classification
+    """
+    regularizers = {
+        'l1': l1_regularizer,
+        'l2': l2_regularizer
+    }
+    EPSILON = 1e-12
+
+    def __init__(self, input: tf.Tensor = None, output: tf.Tensor = None, batch: int = 256, n_label: int = 10):
+        """
+        :param input: Input Tensor. Use tf.placeholder. If not provided input layer for CIFAR-10 is used
+        :param output: Output Tensor. Use tf.placeholder. If not provided output layer for CIFAR-10 is used
+        :param batch: Batch Size
+        :param n_label: The number of labels for classification
+        """
+
+        self.batch = batch
+        self.n_label = n_label
+        self.x_ts = tf.placeholder('float', [None, 32, 32, 3]) if input is None else input
+        self.y_ts = tf.placeholder('int64', [None]) if output is None else output
+
+        self.sess = None
+        self._names = dict()
+        self.layers = list()
+        self.layers.append(self.x_ts)
+
+        self.saver = None
+
+    def create_variable(self, name: str, shape: tuple, dtype=tf.float32,
+                        initializer=xavier_initializer(), regularizer: str = None):
+        if regularizer is not None:
+            regularizer = regularizer.lower()
+            regularizer = self.regularizers[regularizer]()
+
+        v = tf.get_variable(self._naming(name), shape=shape, dtype=dtype,
+                            initializer=initializer, regularizer=regularizer)
+        return v
+
+    def conv(self, input_layer, filter: List[int], channel: List[int],
+             stride: int, padding: str = 'SAME') -> Tuple[tf.Tensor, tf.Tensor]:
+        """
+        :param input_layer: Previous layer or tensor
+        :param filter: [filter_height, filter_width]
+        :param channel: [in_channels, out_channels]
+        :param stride:
+        :param padding:
+        :return: [conv_layer, filter]
+        """
+
+        filter_ts = self.create_variable('filter', shape=(*filter, *channel))
+        conv = tf.nn.conv2d(input_layer, filter=filter_ts, strides=[1, stride, stride, 1], padding=padding)
+        return conv, filter_ts
+
+    def batch_norm(self, input_layer, dimension):
+        mean, variance = tf.nn.moments(input_layer, [0, 1, 2], keep_dims=False)
+        beta = self.create_variable('batch_beta', dimension, dtype=tf.float32,
+                                    initializer=tf.constant_initializer(0.0, tf.float32))
+        gamma = self.create_variable('batch_gamma', dimension, dtype=tf.float32,
+                                     initializer=tf.constant_initializer(1.0, tf.float32))
+        bn_layer = tf.nn.batch_normalization(input_layer, mean, variance, beta, gamma, self.EPSILON)
+        return bn_layer
+
+    def conv_bn(self, input_layer, filter: List[int], channel: List[int], stride: int):
+        """
+        ResNet에서는 Convolution 다음에는 항상 Batch Normalization을 넣는다.
+           "We adopt batch normalization (BN) right after each convolution and before activation"
+
+        filter: [filter_height, filter_width]
+        channel: [in_channels, out_channels]
+        """
+        out_channel = channel[1]
+        h, _filter = self.conv(input_layer, filter=filter, channel=channel, stride=stride, padding='SAME')
+        h = self.batch_norm(h, out_channel)
+        return h
+
+    def init_block(self, filter: List[int] = (7, 7), channel: List[int] = (3, 16),
+                   stride: int = 1, max_pool: bool = True) -> tf.Tensor:
+        """
+        input -> Conv -> ReLU -> output
+
+        :param filter: [filter_height, filter_width]
+        :param channel: [in_channels, out_channels]
+        :param stride:
+        """
+        init_conv, _filter = self.conv(self.x_ts, filter=filter, channel=channel, stride=stride)
+        init_conv = tf.nn.relu(init_conv)
+        if max_pool:
+            # MaxPooling
+            # ksize: The size of the window for each dimension of the input tensor
+            # strides: The stride of the sliding window for each dimension of the input tensor
+            output = tf.nn.max_pool(init_conv, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        else:
+            output = init_conv
+
+        self.layers.append(output)
+        return output
+
+    def max_pool(self, input_layer, kernel: List[int], stride: List[int], padding: str = 'SAME') -> tf.Tensor:
+        """
+        :param input_layer:
+        :param kernel: [width, height] Kernel Size
+        :param stride: [width, height] Stirde Size
+        :param padding:
+        :return:
+        """
+        k_height, k_width = kernel
+        stride_width, stride_height = stride
+        output = tf.nn.max_pool(input_layer,
+                                ksize=[1, k_height, k_width, 1],
+                                strides=[1, stride_width, stride_height, 1], padding=padding)
+        self.layers.append(output)
+        return output
+
+    def avg_pool(self, input_layer, kernel: List[int], stride: List[int], padding: str = 'SAME') -> tf.Tensor:
+        """
+        :param input_layer:
+        :param kernel: [width, height] Kernel Size
+        :param stride: [width, height] Stirde Size
+        :param padding:
+        :return:
+        """
+        k_height, k_width = kernel
+        stride_width, stride_height = stride
+        output = tf.nn.avg_pool(input_layer,
+                                ksize=[1, k_height, k_width, 1],
+                                strides=[1, stride_width, stride_height, 1], padding=padding)
+        self.layers.append(output)
+        return output
+
+    def residual_block(self, input_layer, filter: List[int], channel: List[int], stride: int = 1) -> tf.Tensor:
+        """
+        input -> Conv -> BN -> ReLU -> Conv -> BN -> Addition -> ReLU -> output
+
+        :param input_layer: Usually previous layer
+        :param filter: (width<int>, height<int>) The size of the filter
+        :param channel: [in_channels, out_channels]
+        :param stride: The size of the s
+        :return:
+        """
+        input_channel, output_channel = channel
+
+        h = self.conv_bn(input_layer, filter=filter, channel=[input_channel, output_channel], stride=stride)
+        h = tf.nn.relu(h)
+        h = self.conv_bn(h, filter=filter, channel=[output_channel, output_channel], stride=stride)
+
+        if input_channel != output_channel:
+            # Input channel 과 output channel이 dimension이 다르기 때문에 projection 을 통해서 dimension을 맞춰준다.
+            inp, _filter = self.conv(input_layer, filter=[1, 1], channel=[input_channel, output_channel], stride=stride)
+        else:
+            inp = input_layer
+
+        h = tf.add(h, inp)
+        h = tf.nn.relu(h)
+        self.layers.append(h)
+        return h
+
+    def fc(self, input_layer):
+        global_pool = tf.reduce_mean(input_layer, axis=[1, 2])
+        fc_w = self.create_variable(name='fc_w', shape=[global_pool.shape[-1], self.n_label])
+        fc_b = self.create_variable(name='fc_b', shape=[self.n_label])
+
+        output = tf.matmul(global_pool, fc_w) + fc_b
+        self.layers.append(output)
+        return output
+
+    def loss(self):
+        loss_f = tf.nn.sparse_softmax_cross_entropy_with_logits
+        cross_entropy = loss_f(logits=self.last_layer, labels=self.y_ts, name='cross_entropy')
+        cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy_mean')
+        return cross_entropy_mean
+
+    def compile(self, target=None) -> tf.Session:
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5, allow_growth=True)
+        sess = tf.Session(target, config=tf.ConfigProto(gpu_options=gpu_options,
+                                                        allow_soft_placement=False,
+                                                        log_device_placement=False))
+        sess.run(tf.global_variables_initializer())
+        self.sess = sess
+        return sess
+
+    def save(self, path='/tmp/resnet_anderson.ckpt'):
+        if self.saver is None:
+            self.saver = tf.train.Saver()
+        self.saver.save(self.sess, path)
+
+    def restore(self, path='/tmp/resnet_anderson.ckpt'):
+        if self.saver is None:
+            self.saver = tf.train.Saver()
+        print(f'Restoring "{path}" model')
+        self.saver.restore(self.sess, path)
+
+    @property
+    def last_layer(self) -> tf.Tensor:
+        return self.layers[-1]
+
+    def _naming(self, name=None):
+        if name is None or not name:
+            name = 'variable'
+        name = name.lower()
+        self._names.setdefault(name, 0)
+        self._names[name] += 1
+        count = self._names[name]
+        return f'{name}_{count:02}'
---
+++ packages/resnet-tensorflow/resnet-tensorflow-0.0.1.tar.gz/resnet-tensorflow-0.0.1/resnet/tool.py
@@ -0,0 +1,137 @@
+import os
+import pickle
+import tarfile
+from urllib.request import urlopen
+
+import numpy as np
+from tensorflow.python.client import device_lib
+
+# Constants
+CIFAR_URL = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+
+
+def _download(dest_dir):
+    """
+    데이터가 없으면 CIFAR-10 데이터셋을 인터넷에서 다운로드한다.
+    """
+    tar_path = os.path.abspath(os.path.join(dest_dir, 'cifar-10-python.tar.gz'))
+
+    # 저장할 디렉토리 생성
+    if not os.path.exists(dest_dir):
+        os.mkdir(dest_dir)
+
+    # 파일이 존재하지 않으면 다운로드
+    if not os.path.exists(tar_path):
+        print('Start downloading CIFAR-10 dataset from internet')
+        print(f'the path to save: {tar_path}')
+        raw = urlopen(CIFAR_URL).read()
+        with open(tar_path, 'wb') as f:
+            f.write(raw)
+
+    return tar_path
+
+
+def _download_from_hdfs(dest_dir):
+    import hdfs
+    tar_path = os.path.abspath(os.path.join(dest_dir, 'cifar-10-python.tar.gz'))
+
+    # 저장할 디렉토리 생성
+    if not os.path.exists(dest_dir):
+        os.mkdir(dest_dir)
+    # 파일이 존재하지 않으면 다운로드
+    if not os.path.exists(tar_path):
+        print('Start downloading CIFAR-10 dataset from HDFS')
+        print(f'the path to save: {tar_path}')
+        client = hdfs.InsecureClient(url='http://node1.samsung.com:50070/', user='ocrbg1')
+        client.download('/user/user001/cifar-10-python.tar.gz', tar_path)
+
+
+def _uncompress(data_path):
+    """
+    압축된 cifar-10-python.tar.gz 파일을 읽어서 data_path에 압축을 해제한다.
+    """
+    data_path = os.path.abspath(data_path)
+    tar_path = os.path.join(data_path, 'cifar-10-python.tar.gz')
+    uncompressed_path = os.path.join(data_path, 'cifar-10-batches-py')
+
+    if not os.path.exists(uncompressed_path):
+        print(f'Extracting {tar_path}')
+        archive = tarfile.open(tar_path, 'r')
+        archive.extractall(data_path)
+
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+
+def _to_onehot(data):
+    N = data.shape[0]
+    n_label = len(np.unique(data))
+
+    onehot = np.zeros((N, n_label), dtype=np.int)
+    onehot[np.arange(N), data] = 1
+    return onehot
+
+
+def _preprocessing1(data_path, force=False):
+    """
+    1차 과정으로 압축을 풀은 데이터 파일로 부터 읽어드린후, 각각의 파일을 합쳐서 새로운 파일로 저장을 한다.
+    """
+    abs_path = os.path.abspath(data_path)
+    preprocessed_path = os.path.join(abs_path, 'cifar-10-preprocessed.pkl')
+    uncompressed_dir = os.path.join(abs_path, 'cifar-10-batches-py')
+
+    if os.path.exists(preprocessed_path) and not force:
+        return preprocessed_path
+
+    data_x = list()
+    data_y = list()
+    for i in range(1, 6):
+        rawdata = unpickle(os.path.join(uncompressed_dir, f'data_batch_{i}'))
+        data_x.append(rawdata[b'data'])
+        data_y.append(rawdata[b'labels'])
+    rawdata = unpickle(os.path.join(uncompressed_dir, 'test_batch'))
+
+    train_x = np.array(data_x).reshape(-1, 3, 32, 32)
+    train_x = train_x.transpose([0, 2, 3, 1])
+    train_y = np.array(data_y).reshape(-1)
+
+    test_x = np.array(rawdata[b'data']).reshape(-1, 3, 32, 32)
+    test_x = test_x.transpose([0, 2, 3, 1])
+    test_y = np.array(rawdata[b'labels']).reshape(-1)
+
+    dataset = dict()
+    dataset['train_x'] = train_x.astype('float32')
+    dataset['train_y'] = train_y
+    dataset['test_x'] = test_x.astype('float32')
+    dataset['test_y'] = test_y
+
+    f = open(preprocessed_path, 'wb')
+    pickle.dump(dataset, f)
+
+    return preprocessed_path
+
+
+def load_data(data_path):
+    _download(data_path)
+    # _download_from_hdfs(data_path)
+    _uncompress(data_path)
+    prep_path = _preprocessing1(data_path)
+
+    dataset = pickle.load(open(prep_path, 'rb'))
+    return dataset['train_x'], dataset['train_y'], dataset['test_x'], dataset['test_y']
+
+
+def numpy_to_image(matrix: np.array, path: str):
+    import scipy.misc
+    scipy.misc.imsave(path, matrix)
+
+
+if __name__ == '__main__':
+    train_x, train_y, test_x, test_y = load_data('_dataset')
+    print('train_x:', train_x.shape)
+    print('train_y:', train_y.shape)
+    print('test_x :', test_x.shape)
+    print('test_y :', test_y.shape)
