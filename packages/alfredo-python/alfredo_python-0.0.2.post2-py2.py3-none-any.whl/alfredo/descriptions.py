da2830bfa5e40b8290ecb4e4812a6adc140c4216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:19 +0000

    Add package alfredo-python/0.0.2.post2/alfredo_python-0.0.2.post2-py2.py3-none-any.whl

---
+++ packages/alfredo-python/alfredo_python-0.0.2.post2-py2.py3-none-any.whl/alfredo/descriptions.py
@@ -0,0 +1,41 @@
+ruote = {
+    'users': {
+        'me': {},
+        ':id': {}
+    },
+    'sso': {
+        'token_by_email': {},
+        'providers': {},
+    },
+    'clusters': {
+        ':id': {}
+    },
+    'AWSclusters': {
+        ':id': {}
+    },
+    'queues': {
+        ':id': {}
+    },
+    'files': {
+        '__attrs_files': ['file'],
+        ':id': {}
+    },
+    'jobs': {
+        ':id': {
+            'stdout': {},
+            'stderr': {},
+            'log': {},
+        }
+    },
+    'apps': {
+        ':id': {}
+    }
+}
+virgo = {
+    'build': {
+        '__attrs_files': ['file'],
+        ':id': {
+            'log': {},
+        },
+    },
+}
