27a4d1e05848363e113d16170e1ab9e11870e287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:25 +0000

    Add package incuna-feincms/0.16.1/incuna-feincms-0.16.1.tar.gz

---
+++ packages/incuna-feincms/incuna-feincms-0.16.1.tar.gz/incuna-feincms-0.16.1/incunafein/admin/__init__.py
@@ -0,0 +1,132 @@
+from feincms.module.page.models import Page, PageAdmin as PageAdminOld
+try:
+    from feincms.module.page.forms import PageAdminForm as PageAdminFormOld
+except ImportError:
+    # this is for FeinCMS<=1.5 compat when PageAdminFrom was located elsewhere
+    from feincms.module.page.models import PageAdminForm as PageAdminFormOld
+from django.utils.safestring import mark_safe
+from django.contrib.admin.widgets import ForeignKeyRawIdWidget
+from django.conf.urls.defaults import patterns
+from django.http import HttpRequest, HttpResponse
+from django.utils import simplejson
+from django.shortcuts import get_object_or_404
+from django.conf import settings
+
+class RefreshingParentForeignKeyRawIdWidget(ForeignKeyRawIdWidget):
+
+    """Adds the javascript to force-refresh the template field when the parent field loses focus"""
+
+    class Media:
+        js = (settings.STATIC_URL + 'incunafein/scripts/get_templates.js',)
+
+
+def get_valid_templates(instance=None, parent=None):
+    """
+        Looks up the list of valid templates for this instance, and its parent.
+
+        This is achivined by looking up through the parent tree for a page where the template enforces child templates,
+        or the root template list if none is found
+
+        @return dict: dict containing all the templates valid for this instance
+    """
+
+    # copy of all the available templates
+    templates = Page._feincms_templates.copy()
+    check_pages = None
+
+    if parent and not isinstance(parent, Page):
+        parent = Page.objects.get(pk=parent)
+
+    if parent:
+        check_pages = (parent.get_ancestors(ascending=True) | Page.objects.filter(pk = parent.pk)) or [parent,]
+    elif instance:
+        check_pages = instance.get_ancestors(ascending=True)
+
+    if check_pages:
+        for page in check_pages:
+            if hasattr(page.template, 'children'):
+                templates = dict([(templates[child].key, templates[child]) for child in page.template.children])
+
+    # remove templates that are defined as children within the current set
+    for t in templates.values():
+        if hasattr(t, 'children') and t.children:
+            for c in t.children:
+                if c in templates:
+                    del(templates[c])
+                
+    return templates
+
+
+class PageAdminForm(PageAdminFormOld):
+
+    """
+
+    Adds hooks into the admin form to allow the munging of the template list.
+
+    cf http://www.marcofucci.com/tumblelog/17/jun/2010/customizing-feincms-part-2/
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(PageAdminForm, self).__init__(*args, **kwargs)
+
+        instance = kwargs.get('instance')
+        parent = kwargs.get('initial', {}).get('parent')
+        if not parent and instance:
+            parent = instance.parent
+
+        templates = get_valid_templates(instance, parent)
+
+        choices = []
+        for key, template in templates.items():
+            if template.preview_image:
+                choices.append((template.key,
+                    mark_safe(u'<img src="%s" alt="%s" /> %s' % (
+                        template.preview_image, template.key, template.title))))
+            else:
+                choices.append((template.key, template.title))
+
+        self.fields['template_key'].choices = choices
+        if choices:
+            self.fields['template_key'].default = choices[0][0]
+
+        self.fields['parent'].widget = RefreshingParentForeignKeyRawIdWidget(self.fields['parent'].widget.rel)
+
+
+
+class PageAdmin(PageAdminOld):
+    form = PageAdminForm
+    
+    def get_templates(self, request):
+        """Ajax view for getting a JSON list of the avaliable templates,
+        (using the get_valid_templates function above)
+        """
+
+        if 'page_id' in request.POST and request.POST['page_id']:
+            parent = get_object_or_404(Page, pk=request.POST['page_id'])
+        else:
+            parent = None
+        valid_templates = get_valid_templates(parent=parent)
+        if valid_templates:
+            result = [{'id':key, 'desc':unicode(valid_templates[key]),} for key in valid_templates.keys()]
+        else:
+            result = list()
+        
+        return HttpResponse(simplejson.dumps(result), mimetype="application/javascript")
+
+    def get_urls(self):
+        """URL hooks to add the above view at /admin/page/page/get_templates/"""
+
+        urls = super(PageAdmin, self).get_urls()
+        my_urls = patterns('',
+            (r'^get_templates/?$', self.admin_site.admin_view(self.get_templates))
+        )
+        return my_urls + urls
+
+# Change the field order so the parent is first
+try:
+    admin_fields = PageAdmin.fieldsets[0][1]['fields']
+    if 'parent' in admin_fields and 'template_key' in admin_fields:
+        admin_fields.remove('parent')
+        admin_fields.insert(admin_fields.index('template_key'), 'parent')
+except IndexError:
+    pass
---
+++ packages/incuna-feincms/incuna-feincms-0.16.1.tar.gz/incuna-feincms-0.16.1/incunafein/module/navigation/templatetags/navigationtags.py
@@ -0,0 +1,137 @@
+from django.db.models import Q
+from django import template
+from incunafein.module.navigation.models import Navigation
+
+register = template.Library()
+class IncunaFeinNavigationNode(template.Node):
+    """
+    Render a navigation.
+    arguments: 
+        navigate: The root item (instance or dom_id) of the navigation to render.
+        depth: The depth of sub navigation to include.
+        show_all_subnav: Whether to show all sub navigation items (or just the ones in the currently selected branch).
+
+    example usage:
+        {% incunafein_navigation 'footer' 1 0 %}
+    """
+    def __init__(self,  navigate=None, depth=1, show_all_subnav=False):
+        self.navigate = navigate
+        self.depth = depth
+        self.show_all_subnav = show_all_subnav
+
+    def render(self, context):
+        navigate = self.navigate and self.navigate.resolve(context)
+        depth = int(self.depth.resolve(context) if isinstance(self.depth, template.FilterExpression) else self.depth)
+        show_all_subnav = self.show_all_subnav.resolve(context) if isinstance(self.show_all_subnav, template.FilterExpression) else self.show_all_subnav
+
+        instance = None
+        if isinstance(navigate, Navigation):
+            instance = navigate
+        elif isinstance(navigate, (str, unicode)) and navigate:
+            try:
+                instance = Navigation.objects.get(dom_id=navigate)
+            except Navigation.DoesNotExist, er:
+                return ''
+
+        if not 'request' in context:
+            raise ValueError("No request in the context. Try using RequestContext in the view.")
+        path = context['request'].path
+
+        try:
+            current = Navigation.objects.filter(Q(url=path) | Q(page___cached_url=path))[0]
+        except IndexError:
+            current = None
+
+        entries = self.entries(instance, current, depth, show_all_subnav)
+
+        if not entries:
+            return ''
+
+        def get_item(item, next_level, extra_context=None):
+            context.push()
+
+            if extra_context:
+                context.update(extra_context)
+
+            context['item'] = item
+            context['url'] = item.get_absolute_url()
+            context['is_current'] = context['url'] == path
+            context['title'] = unicode(item)
+
+            css_class = item.css_class or (item.page and item.page.slug) or ''
+            if css_class:
+                if 'css_class' in context:
+                    context['css_class'] += ' ' + css_class
+                else:
+                    context['css_class'] = css_class
+
+            context['is_ancestor'] = False
+            if context['is_current']:
+                if 'css_class' in context:
+                    context['css_class'] += ' selected'
+                else:
+                    context['css_class'] = 'selected'
+            else:
+                context['is_ancestor'] = path.startswith(context['url'])
+
+            if next_level > item.level:
+                context['down'] = next_level - item.level
+            elif next_level < item.level:
+                context['up'] = item.level - next_level
+
+            html = template.loader.get_template('navigation/navitem.html').render(context)
+            context.pop()
+
+            return html
+
+        output = ''
+        item = entries[0]
+        for i, next in enumerate(entries[1:]):
+            output += get_item(item, next.level, {'css_class': i==0 and 'first' or ''})
+            item = next
+        
+        output += get_item(item, entries[0].level, {'css_class': len(entries)==1 and 'first last' or 'last'})
+
+        if instance:
+            return '<ul id="%s" class="%s">%s</ul>' % (instance.dom_id, instance.css_class, output)
+        else:
+            return '<ul>%s</ul>' % (output,)
+
+
+    def entries(self, instance, current, depth=1, show_all_subnav=False):
+
+        if depth == 1:
+            if instance is None:
+                return Navigation.objects.filter(parent__isnull=True)
+            else:
+                return instance.children.all()
+        elif show_all_subnav:
+            if instance is None:
+                return Navigation.objects.filter(level__lt=depth)
+            else:
+                return instance.get_descendants().filter(level__lt=depth)
+        else:
+            if instance is None:
+                qs = Navigation.objects.filter(parent__isnull=True) 
+                if current:
+                    qs = qs | current.get_ancestors() \
+                            | current.get_siblings(include_self=True).filter(level__lt=depth) \
+                            | current.children.filter(level__lt=depth)
+            else:
+                relative_depth = instance.level + depth
+                qs = instance.children.all()
+                if current:
+                    qs = qs | current.get_ancestors().filter(level__gt=instance.level) \
+                            | current.get_siblings(include_self=True).filter(level__gt=instance.level, level__lt=relative_depth) \
+                            | current.children.filter(level__gt=instance.level, level__lt=relative_depth)
+            return qs
+
+def do_incunafein_navigation(parser, token):
+    args = token.split_contents()
+    if len(args) > 4:
+        raise template.TemplateSyntaxError("'%s tag accepts no more than 3 argument." % args[0])
+    return IncunaFeinNavigationNode(*map(parser.compile_filter, args[1:]))
+
+register.tag('incunafein_navigation', do_incunafein_navigation)
+
+
