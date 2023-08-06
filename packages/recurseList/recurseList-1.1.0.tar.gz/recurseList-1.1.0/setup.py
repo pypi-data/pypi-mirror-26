0f40a7e289433c6389c3b7eadd13fa4717bec6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:51 +0000

    Add package recurseList/1.1.0/recurseList-1.1.0.tar.gz

---
+++ packages/recurseList/recurseList-1.1.0.tar.gz/recurseList-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'recurseList',
+    version = '1.1.0',
+    py_modules = ['recurseList'],
+    author = 'Jacky',
+    author_email = 'pheonix_jk@hotmail.com',
+    url = 'http://www.baidu.com',
+    description = 'simple orinter of nested lists')
