a2b7299c33777e0ef45ad7c83c9315478077d42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:48 +0000

    Add package elasticsearch_helper/0.1.2/elasticsearch_helper-0.1.2.tar.gz

---
+++ packages/elasticsearch_helper/elasticsearch_helper-0.1.2.tar.gz/elasticsearch_helper-0.1.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+with open('README.rst') as f:
+    description = f.read()
+
+
+setuptools.setup(
+    name='elasticsearch_helper',
+    version='0.1.2',
+    description=description,
+    url='http://github.com/yuuichi-fujioka/elasticsearch_helper.git',  # noqa
+    author='Yuuichi Fujioka',
+    author_email='fujioka.yuuichi@gmail.com',
+    entry_points={
+        'console_scripts': [
+            "es_stream=elasticsearch_helper.cmd:stream",
+            "es_del_index=elasticsearch_helper.cmd:del_index",
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=['elasticsearch_helper'],
+    install_requires=requirements
+)
