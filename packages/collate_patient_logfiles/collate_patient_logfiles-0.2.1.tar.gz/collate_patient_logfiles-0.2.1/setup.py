5a6d7dd3e7e9913836c56ce71d46e77fa85362cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:06 +0000

    Add package collate_patient_logfiles/0.2.1/collate_patient_logfiles-0.2.1.tar.gz

---
+++ packages/collate_patient_logfiles/collate_patient_logfiles-0.2.1.tar.gz/collate_patient_logfiles-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="collate_patient_logfiles",
+    version="0.2.1",
+    author="Simon Biggs",
+    author_email="mail@simonbiggs.net",
+    description="Collate logfile data.",
+    packages=[
+        "collate_patient_logfiles"
+    ],
+    entry_points={
+        'console_scripts': [
+            'collate_patient_logfiles=collate_patient_logfiles:main',
+        ],
+    },
+    install_requires=[
+        'attrs',
+        'pyyaml',
+        'numpy',
+        'pandas'
+    ],
+    license='AGPL3+'
+)
