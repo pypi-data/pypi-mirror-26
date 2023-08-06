8c9589aaffa02e66907a056e107a6aa0584db668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:57 +0000

    Add package Keg/0.6.1/Keg-0.6.1-py2.py3-none-any.whl

---
+++ packages/Keg/Keg-0.6.1-py2.py3-none-any.whl/keg_apps/db/app.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+from keg.app import Keg
+
+
+class DBApp(Keg):
+    import_name = 'keg_apps.db'
+    db_enabled = True
+
+
+if __name__ == '__main__':
+    DBApp.cli.main()
