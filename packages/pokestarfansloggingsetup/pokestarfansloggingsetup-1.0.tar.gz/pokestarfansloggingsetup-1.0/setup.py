74abed5acd3a2496e717dce3f63bb3b9ad671e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:09 +0000

    Add package pokestarfansloggingsetup/1.0/pokestarfansloggingsetup-1.0.tar.gz

---
+++ packages/pokestarfansloggingsetup/pokestarfansloggingsetup-1.0.tar.gz/pokestarfansloggingsetup-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='pokestarfansloggingsetup',
+      version='1.0',
+      description='My logging setup. Dependency',
+      url='https://pypi.python.org/pypi/pokestarfansloggingsetup/1.0',
+      author='PokestarFan',
+      author_email='pokestarfan@yahoo.com',
+      license='MIT',
+      packages=['Setup'],
+      zip_safe=True)
