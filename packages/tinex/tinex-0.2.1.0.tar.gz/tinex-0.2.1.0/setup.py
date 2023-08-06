95eaa0eb115211fc125e983c7d075e1936c1dd60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:55 +0000

    Add package tinex/0.2.1.0/tinex-0.2.1.0.tar.gz

---
+++ packages/tinex/tinex-0.2.1.0.tar.gz/tinex-0.2.1.0/setup.py
@@ -0,0 +1,55 @@
+import os
+from setuptools import setup
+from distutils.extension import Extension
+from distutils.command.sdist import sdist as _sdist
+
+USE_CYTHON = os.environ.get('TINEX_USE_CYTHON', False)
+
+
+class sdist(_sdist):
+
+    def run(self):
+        # make sure the cython-built files are up-to-date
+        from Cython.Build import cythonize
+        cythonize(Extension('tinex', ['tinex.pyx'],
+                            extra_objects=['tinyexpr/tinyexpr.c']))
+        _sdist.run(self)
+
+
+cmdclass = {'sdist': sdist}
+
+if USE_CYTHON:
+    from Cython.Build import build_ext
+    src = 'tinex.pyx'
+    cmdclass['build_ext'] = build_ext
+else:
+    src = 'tinex.c'
+
+
+setup(
+    name='tinex',
+    version='0.2.1.0',
+    description='Python wrapper for Tinyexpr',
+    author='Arie Bovenberg',
+    author_email='a.c.bovenberg@gmail.com',
+    url='https://github.com/ariebovenberg/tinex',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+
+    ],
+    ext_modules=[
+        Extension('tinex', [src],
+                  extra_objects=['tinyexpr/tinyexpr.c'])
+    ],
+    cmdclass=cmdclass
+)
