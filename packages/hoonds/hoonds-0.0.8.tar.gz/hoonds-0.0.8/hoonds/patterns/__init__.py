d8127ef790066dd2598d8d2f5fb866e562d22cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:53 +0000

    Add package hoonds/0.0.8/hoonds-0.0.8.tar.gz

---
+++ packages/hoonds/hoonds-0.0.8.tar.gz/hoonds-0.0.8/hoonds/patterns/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. currentmodule:: __init__.py
+.. moduleauthor:: Pat Daburu <pat@daburu.net>
+
+python implementations of common design patterns.
+"""
