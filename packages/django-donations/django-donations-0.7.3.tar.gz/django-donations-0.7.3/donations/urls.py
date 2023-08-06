1260824039de965c8a26e7e6531f2aa14b33aa2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:01 +0000

    Add package django-donations/0.7.3/django-donations-0.7.3.tar.gz

---
+++ packages/django-donations/django-donations-0.7.3.tar.gz/django-donations-0.7.3/donations/urls.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8
+from __future__ import unicode_literals, absolute_import
+
+from django import VERSION
+from django.conf.urls import include, url
+
+from donations.views import DonateAPI, VerifyAPI
+
+app_name = 'donations'
+
+api_urls = [
+    url(r'^donate/$', DonateAPI.as_view(), name="donate"),
+    url(r'^verify/(?P<pk>[0-9]+)$', VerifyAPI.as_view(), name="verify"),
+]
+
+donations = [
+    url(r'^api/', include(api_urls, namespace="api")),
+]
+
+urlpatterns = [
+    url(r'^', include(donations, namespace="donations"))
+]
