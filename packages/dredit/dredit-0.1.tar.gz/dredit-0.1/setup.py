0d4d8cd21319e38d94079297050e80cbf4a2e6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:21 +0000

    Add package dredit/0.1/dredit-0.1.tar.gz

---
+++ packages/dredit/dredit-0.1.tar.gz/dredit-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'dredit',
+    packages = ['dredit'],
+    version = '0.1',
+    description = 'Edit directories',
+    author = 'Misha Wagner',
+    author_email = 'mishajw@gmail.com',
+    url = 'https://github.com/mishajw/dredit',
+    download_url = 'https://github.com/mishajw/dredit/archive/0.1.tar.gz',
+    keywords = ['directory', 'linux', 'utility'],
+    classifiers = [])
