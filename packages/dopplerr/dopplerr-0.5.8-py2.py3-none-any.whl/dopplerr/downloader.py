3705dfd1a9a1698a84385f0a82542628caab8208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:34 +0000

    Add package dopplerr/0.5.8/dopplerr-0.5.8-py2.py3-none-any.whl

---
+++ packages/dopplerr/dopplerr-0.5.8-py2.py3-none-any.whl/dopplerr/downloader.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+# Standard Libraries
+import logging
+
+# Dopplerr
+from dopplerr.singleton import singleton
+
+log = logging.getLogger(__name__)
+
+
+@singleton
+class DopplerrDownloader(object):
+
+    def process_fullscan(self, _request):
+        log.debug("Processing full scan of missing subtitle files...")
+        res = {
+            'status': 'unprocessed',
+            'message': 'not implemented yet!',
+        }
+        # TODO: inspiration
+        #   https://gist.github.com/alexsavio/9299716
+        return res
