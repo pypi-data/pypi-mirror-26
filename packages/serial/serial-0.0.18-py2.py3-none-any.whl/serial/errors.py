5bab9936b11006c340aa2b0219be6f7112206ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:54 +0000

    Add package serial/0.0.18/serial-0.0.18-py2.py3-none-any.whl

---
+++ packages/serial/serial-0.0.18-py2.py3-none-any.whl/serial/errors.py
@@ -0,0 +1,27 @@
+# Backwards Compatibility ->
+from __future__ import nested_scopes, generators, division, absolute_import, with_statement, print_function,\
+    unicode_literals
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
+# <- Backwards Compatibility
+
+try:
+    import typing
+except ImportError as e:
+    typing = None
+
+
+class ValidationError(Exception):
+
+    pass
+
+
+class VersionError(AttributeError):
+
+    pass
+
+
+class DefinitionExistsError(Exception):
+
+    pass
