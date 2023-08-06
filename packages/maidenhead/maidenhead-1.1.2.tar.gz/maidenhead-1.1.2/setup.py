410ce1f548448e079a09cb4301f2cdaee25029c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:01 +0000

    Add package maidenhead/1.1.2/maidenhead-1.1.2.tar.gz

---
+++ packages/maidenhead/maidenhead-1.1.2.tar.gz/maidenhead-1.1.2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name = "maidenhead",
+    packages = ["maidenhead"],
+    version = "1.1.2",
+    description = "Maidenhead Locator",
+    author = ['Michael Hirsch',"Henri Kuiper"],
+	url = "https://github.com/scivision/maidenhead",
+    keywords = ["location", "maidenhead"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        ],
+)
