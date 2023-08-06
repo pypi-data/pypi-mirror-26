0f65ec93f425f3d837b76d58954c113e94874f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:48 +0000

    Add package django-unused-media/0.1.10/django-unused-media-0.1.10.tar.gz

---
+++ packages/django-unused-media/django-unused-media-0.1.10.tar.gz/django-unused-media-0.1.10/tests/runtests.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+from django_nose import NoseTestSuiteRunner
+
+
+if __name__ == '__main__':
+    if NoseTestSuiteRunner(verbosity=1).run_tests(['tests']) > 0:
+        exit(1)
+    else:
+        exit(0)
