1816e8e4d27f2f44ca050bb755f559640c0231ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:59 +0000

    Add package sklearn-features/0.0.1.dev0/sklearn-features-0.0.1.dev0.tar.gz

---
+++ packages/sklearn-features/sklearn-features-0.0.1.dev0.tar.gz/sklearn-features-0.0.1.dev0/README.md
@@ -0,0 +1,12 @@
+# sklearn-features
+
+[![Build Status](https://travis-ci.org/sixtwoeight-tech/sklearn-features.svg?branch=master)](https://travis-ci.org/sixtwoeight-tech/sklearn-features) [![Docs Status](https://readthedocs.org/projects/sklearn-features/badge/?version=latest)](http://sklearn-features.readthedocs.io/en/latest/?badge=latest)
+
+
+## Tests
+
+In the root directory run
+
+```
+ptest --pyargs src
+```
---
+++ packages/sklearn-features/sklearn-features-0.0.1.dev0.tar.gz/sklearn-features-0.0.1.dev0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='sklearn-features',
+    version='0.0.1.dev0',
+    description='Helpful tools for building feature extraction pipelines with scikit-learn',
+    url='https://github.com/sixtwoeight-tech/sklearn-features',
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+    ],
+
+    install_requires=[
+        'scikit-learn',
+        'pandas',
+    ],
+
+    python_requires='>=3.3',
+
+    packages=find_packages(),
+)
