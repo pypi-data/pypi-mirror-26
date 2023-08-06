be639cc951debe931aa04aff55c89d463bc704b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:12 +0000

    Add package djangocarrot/1.1/djangocarrot-1.1.tar.gz

---
+++ packages/djangocarrot/djangocarrot-1.1.tar.gz/djangocarrot-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'djangocarrot',
+  packages = find_packages(),
+  version = '1.1',
+  description = 'Carrot -- A simple task Queue for Django.',
+  author = 'John Bowen',
+  author_email = 'jbowen7@gmail.com',
+  url = 'https://github.com/jbowen7/djangocarrot',
+  keywords = ['django', 'task', 'queue'],
+  classifiers = [],
+)
