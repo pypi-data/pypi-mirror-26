2095d4163189b658793fc60b63c55ca9dfc2b7f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:42 +0000

    Add package grow/0.3.12/grow-0.3.12.tar.gz

---
+++ packages/grow/grow-0.3.12.tar.gz/grow-0.3.12/grow/commands/subcommands/build.py
@@ -0,0 +1,86 @@
+"""Command for building pods into static deployments."""
+
+import os
+import click
+from grow.commands import shared
+from grow.common import rc_config
+from grow.common import utils
+from grow.deployments import stats
+from grow.deployments.destinations import local as local_destination
+from grow.pods import pods
+from grow.pods import storage
+from grow.rendering import renderer
+
+
+CFG = rc_config.RC_CONFIG.prefixed('grow.build')
+
+
+# pylint: disable=too-many-locals
+@click.command()
+@shared.pod_path_argument
+@click.option('--out_dir', '--out-dir', default=CFG.get('out-dir', None),
+              help='Where to output built files.')
+@click.option('--preprocess/--no-preprocess', '-p/-np',
+              default=CFG.get('preprocess', True), is_flag=True,
+              help='Whether to run preprocessors.')
+@click.option('--clear-cache',
+              default=CFG.get('clear-cache', False), is_flag=True,
+              help='Clear the pod cache before building.')
+@click.option('--file', '--pod-path', 'pod_paths',
+              help='Build only pages affected by content files.', multiple=True)
+@click.option('--locate-untranslated',
+              default=CFG.get('locate-untranslated', False), is_flag=True,
+              help='Shows untranslated message information.')
+@shared.deployment_option(CFG)
+@shared.reroute_option(CFG)
+def build(pod_path, out_dir, preprocess, clear_cache, pod_paths,
+          locate_untranslated, deployment, use_reroute):
+    """Generates static files and dumps them to a local destination."""
+    root = os.path.abspath(os.path.join(os.getcwd(), pod_path))
+    out_dir = out_dir or os.path.join(root, 'build')
+
+    pod = pods.Pod(root, storage=storage.FileStorage, use_reroute=use_reroute)
+    if not pod_paths or clear_cache:
+        # Clear the cache when building all, only force if the flag is used.
+        pod.podcache.reset(force=clear_cache)
+    if deployment:
+        deployment_obj = pod.get_deployment(deployment)
+        pod.set_env(deployment_obj.config.env)
+    if preprocess:
+        with pod.profile.timer('grow_preprocess'):
+            pod.preprocess()
+    if locate_untranslated:
+        pod.enable(pod.FEATURE_TRANSLATION_STATS)
+    try:
+        with pod.profile.timer('grow_build'):
+            config = local_destination.Config(out_dir=out_dir)
+            destination = local_destination.LocalDestination(config)
+            destination.pod = pod
+            repo = utils.get_git_repo(pod.root)
+            if use_reroute:
+                pod.router.use_simple()
+                if pod_paths:
+                    pod.router.add_pod_paths(pod_paths)
+                else:
+                    pod.router.add_all()
+                routes = pod.router.routes
+                stats_obj = stats.Stats(pod, paths=routes.paths)
+                rendered_docs = renderer.Renderer.rendered_docs(pod, routes)
+                destination.deploy(
+                    rendered_docs, stats=stats_obj, repo=repo,
+                    confirm=False, test=False, is_partial=bool(pod_paths))
+            else:
+                paths, _ = pod.determine_paths_to_build(pod_paths=pod_paths)
+                stats_obj = stats.Stats(pod, paths=paths)
+                content_generator = destination.dump(pod, pod_paths=pod_paths)
+                destination.deploy(
+                    content_generator, stats=stats_obj, repo=repo, confirm=False,
+                    test=False, is_partial=bool(pod_paths))
+
+            pod.podcache.write()
+    except pods.Error as err:
+        raise click.ClickException(str(err))
+    if locate_untranslated:
+        pod.translation_stats.pretty_print()
+        destination.export_untranslated_catalogs()
+    return pod
---
+++ packages/grow/grow-0.3.12.tar.gz/grow-0.3.12/grow/commands/subcommands/filter.py
@@ -0,0 +1,64 @@
+"""Subcommand for filtering untranslated messages."""
+
+import os
+import click
+from grow.commands import shared
+from grow.common import rc_config
+from grow.pods import pods
+from grow.pods import storage
+
+
+CFG = rc_config.RC_CONFIG.prefixed('grow.filter')
+
+
+@click.command()
+@shared.pod_path_argument
+@click.option('--locale', type=str, multiple=True,
+              help='Which locale(s) to analyze when creating template catalogs'
+                   ' that contain only untranslated messages. This option is'
+                   ' only applicable when using --untranslated.')
+@click.option('-o', type=str, default=None,
+              help='Where to write the extracted translation catalog. The path'
+                   ' must be relative to the pod\'s root.')
+@click.option('--include-obsolete/--no-include-obsolete',
+              default=CFG.get('include-obsolete', False), is_flag=True,
+              help='Whether to include obsolete messages. If false, obsolete'
+                   ' messages will be removed from the catalog template. By'
+                   ' default, Grow cleans obsolete messages from the catalog'
+                   ' template.')
+@click.option('--localized/--no-localized', default=CFG.get('localized', False), is_flag=True,
+              help='Whether to create localized message catalogs. Use this'
+                   ' option if content varies by locale.')
+@click.option('--path', type=str, multiple=True,
+              help='Which paths to extract strings from. By default, all paths'
+                   ' are extracted. This option is useful if you\'d like to'
+                   ' generate a partial messages file representing just a'
+                   ' specific set of files.')
+@click.option('--include-header', default=CFG.get('include-header', False), is_flag=True,
+              help='Whether to preserve headers at the beginning of catalogs.')
+@click.option('--out-dir', '--out_dir', type=str, default=CFG.get('out-dir', None),
+              help='Where to write extracted localized translation catalogs.'
+                   ' The path must be relative to the pod\'s root. This option'
+                   ' is only applicable when using --localized.')
+@click.option('-f', default=CFG.get('force', False), is_flag=True,
+              help='Whether to force an update when writing localized message'
+                   ' catalogs.')
+def filter(pod_path, locale, o, include_obsolete, localized, path,
+           include_header, out_dir, f):
+    """Filters untranslated messages from catalogs into new catalogs."""
+    root = os.path.abspath(os.path.join(os.getcwd(), pod_path))
+    pod = pods.Pod(root, storage=storage.FileStorage)
+    with pod.profile.timer('grow_filter'):
+        catalogs = pod.get_catalogs()
+        if not locale:
+            locale = catalogs.list_locales()
+        if out_dir and pod.file_exists(out_dir) and not f:
+            raise click.UsageError(
+                '{} exists. You must specify a directory that does not exist, or '
+                'use the "-f" flag, which will force update catalogs within the '
+                'specified directory.'.format(out_dir))
+        catalogs.filter(out_path=o, out_dir=out_dir,
+                        include_obsolete=include_obsolete,
+                        localized=localized, paths=path,
+                        include_header=include_header, locales=locale)
+    return pod
