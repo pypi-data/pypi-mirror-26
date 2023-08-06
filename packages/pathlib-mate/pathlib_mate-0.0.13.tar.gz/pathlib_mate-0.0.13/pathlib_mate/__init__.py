32a8fc7c2dd7a2ae31a88b817d065ef3ef6c5bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:19 +0000

    Add package pathlib-mate/0.0.13/pathlib_mate-0.0.13.tar.gz

---
+++ packages/pathlib-mate/pathlib_mate-0.0.13.tar.gz/pathlib_mate-0.0.13/pathlib_mate/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+pathlib_mate provide extensive methods, attributes for pathlib.
+"""
+
+__version__ = "0.0.13"
+__short_description__ = "An extended and more powerful pathlib."
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__maintainer__ = "Sanhe Hu"
+__maintainer_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
+
+from .pathlib import Path
