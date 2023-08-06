df383e5d1ab48221b07658674cb8e4164cc88aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:50 +0000

    Add package mlens/0.2.0/mlens-0.2.0-py2.py3-none-any.whl

---
+++ packages/mlens/mlens-0.2.0-py2.py3-none-any.whl/mlens/base/__init__.py
@@ -0,0 +1,32 @@
+"""ML-ENSEMBLE
+
+:author: Sebastian Flennerhag
+:copyright: 2017
+:licence: MIT
+
+Base modules
+"""
+
+from .id_train import IdTrain
+from .indexer import (FoldIndex,
+                      BlendIndex,
+                      SubsetIndex,
+                      FullIndex,
+                      ClusteredSubsetIndex)
+
+
+INDEXERS = {'stack': FoldIndex,
+            'blend': BlendIndex,
+            'subsemble': SubsetIndex,
+            'subsemble': ClusteredSubsetIndex,
+            'full': FullIndex
+            }
+
+
+__all__ = ['IdTrain',
+           'BlendIndex',
+           'FoldIndex',
+           'SubsetIndex',
+           'FullIndex',
+           'ClusteredSubsetIndex'
+           ]
