f3acc261575e0a4f44036d3b5021223eeb83df16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:14 +0000

    Add package pysam/0.13/pysam-0.13-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/pysam/pysam-0.13-cp36-cp36m-manylinux1_x86_64.whl/pysam/version.py
@@ -0,0 +1,10 @@
+# pysam versioning information
+__version__ = "0.13"
+
+# TODO: upgrade number
+__samtools_version__ = "1.6"
+
+# TODO: upgrade code and number
+__bcftools_version__ = "1.6"
+
+__htslib_version__ = "1.6"
