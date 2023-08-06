f96534ed4c3c9f567f242e40ea5c07f17a7649b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:06 +0000

    Add package djangofloor/1.0.29/djangofloor-1.0.29.tar.gz

---
+++ packages/djangofloor/djangofloor-1.0.29.tar.gz/djangofloor-1.0.29/djangofloor/checks.py
@@ -0,0 +1,17 @@
+"""Define checks integrated to the Django check framework
+=====================================================
+
+Currently, the check is build at settings loading time: any config value can add messages to `settings_check_results`.
+"""
+
+from django.core.checks import register
+
+__author__ = 'Matthieu Gallet'
+
+settings_check_results = []
+
+
+# noinspection PyUnusedLocal
+@register()
+def settings_check(app_configs, **kwargs):
+    return settings_check_results
