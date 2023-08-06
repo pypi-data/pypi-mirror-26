5c0dccdff5dc9c10feb51070b857ec08295cf010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:11 +0000

    Add package pyramid-default-cors/0.2.1/pyramid_default_cors-0.2.1.tar.gz

---
+++ packages/pyramid-default-cors/pyramid_default_cors-0.2.1.tar.gz/pyramid_default_cors-0.2.1/setup.py
@@ -0,0 +1,20 @@
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+setup(
+    name='pyramid_default_cors',
+    version='0.2.1',
+    description='Default wiring for CORS XHR',
+    long_description=README,
+    author='Mark Floyd',
+    author_email='mark@goodrx.com',
+    url='https://github.com/GoodRx/pyramid_default_cors',
+    keywords='config web wsgi pyramid',
+    packages=find_packages(),
+    zip_safe=False,
+)
