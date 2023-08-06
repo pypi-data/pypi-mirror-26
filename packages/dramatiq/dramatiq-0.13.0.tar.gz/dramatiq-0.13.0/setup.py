94a8e436f551dbd3e72ad3f660127e38cd7285e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:30 +0000

    Add package dramatiq/0.13.0/dramatiq-0.13.0.tar.gz

---
+++ packages/dramatiq/dramatiq-0.13.0.tar.gz/dramatiq-0.13.0/setup.py
@@ -0,0 +1,61 @@
+import os
+
+from setuptools import setup
+
+
+def rel(*xs):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), *xs)
+
+
+with open(rel("dramatiq", "__init__.py"), "r") as f:
+    version_marker = "__version__ = "
+    for line in f:
+        if line.startswith(version_marker):
+            _, version = line.split(version_marker)
+            version = version.strip().strip('"')
+            break
+    else:
+        raise RuntimeError("Version marker not found.")
+
+
+def parse_dependencies(filename):
+    with open(rel("requirements", filename)) as reqs:
+        for line in reqs:
+            line = line.strip()
+            if line.startswith("#"):
+                continue
+
+            elif line.startswith("-r"):
+                yield from parse_dependencies(line[len("-r "):])
+
+            else:
+                yield line
+
+
+dependencies = list(parse_dependencies("common.txt"))
+
+extras = ("memcached", "rabbitmq", "redis", "watch")
+extra_dependencies = {}
+for extra in extras:
+    filename = "{}.txt".format(extra)
+    extra_dependencies[extra] = list(parse_dependencies(filename))
+
+
+setup(
+    name="dramatiq",
+    version=version,
+    description="A distributed task processing library.",
+    long_description="Visit http://dramatiq.io for more information.",
+    packages=[
+        "dramatiq",
+        "dramatiq.brokers",
+        "dramatiq.middleware",
+        "dramatiq.rate_limits",
+        "dramatiq.rate_limits.backends"
+    ],
+    include_package_data=True,
+    install_requires=dependencies,
+    extras_require=extra_dependencies,
+    entry_points={"console_scripts": ["dramatiq = dramatiq.__main__:main"]},
+    scripts=["bin/dramatiq-gevent"],
+)
