a4bcfa7791cf6e905129a7e5057089cd9524db8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:35 +0000

    Add package tribology/0.2.0/tribology-0.2.0.tar.gz

---
+++ packages/tribology/tribology-0.2.0.tar.gz/tribology-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+   name='tribology',
+   version='0.2.0',
+   setup_requires=['setuptools-git-version'],
+   description='methods and classes for tribology research',
+   long_description='collection of methods and classes for tribology research '
+                    'and education, including contact mechanics and '
+                    'lubrication science,',
+   author='Moritz Ploss',
+   author_email='moritz.ploss@gmail.com',
+   packages=['tribology'],
+   license='MIT',
+   keywords='tribology machine design research',
+   install_requires=['numpy', 'numexpr', 'scipy'],
+   python_requires='>=3.5',
+   url='https://github.com/moritzploss/tribology'
+)
