1515f85410f784b70bc76d64e28f69cbb54bd186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:02 +0000

    Add package gym-link/0.0.2/gym_link-0.0.2.tar.gz

---
+++ packages/gym-link/gym_link-0.0.2.tar.gz/gym_link-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='gym_link',
+    version='0.0.2',
+    install_requires=['gym'],
+    author='Ilija Vukotic',
+    author_email='ivukotic@cern.ch',
+    url='http://vukotic.me'
+)
