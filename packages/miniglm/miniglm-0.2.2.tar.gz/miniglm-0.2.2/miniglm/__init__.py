0fb57908bdd739082682fa36d654601814b948bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:55 +0000

    Add package miniglm/0.2.2/miniglm-0.2.2.tar.gz

---
+++ packages/miniglm/miniglm-0.2.2.tar.gz/miniglm-0.2.2/miniglm/__init__.py
@@ -0,0 +1,28 @@
+'''
+    Minimalist pythonic matrix, vector, quaternion math.
+
+    Matrices are stored in column-major order.
+
+    This module is using glm.
+'''
+
+import os
+
+if os.environ.get('READTHEDOCS') == 'True':
+    from .mock import (
+        Vec2, Vec3, Vec4, Mat2, Mat3, Mat4, Quat,
+        mat4_perspective, mat4_ortho, mat4_look_at,
+    )
+
+else:
+    from .glm import (
+        Vec2, Vec3, Vec4, Mat2, Mat3, Mat4, Quat,
+        mat4_perspective, mat4_ortho, mat4_look_at,
+    )
+
+__all__ = [
+    'Vec2', 'Vec3', 'Vec4', 'Mat2', 'Mat3', 'Mat4', 'Quat',
+    'mat4_perspective', 'mat4_ortho', 'mat4_look_at',
+]
+
+__version__ = '0.2.2'
