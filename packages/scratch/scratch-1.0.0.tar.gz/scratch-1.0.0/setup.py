4f00affcba1f5ba05f0fbe469951a3763bfef2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:37 +0000

    Add package scratch/1.0.0/scratch-1.0.0.tar.gz

---
+++ packages/scratch/scratch-1.0.0.tar.gz/scratch-1.0.0/setup.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2012-2017 Ben Croston
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from setuptools import setup, find_packages
+
+classifiers = ['Development Status :: 5 - Production/Stable',
+               'Operating System :: OS Independent',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Programming Language :: Other',
+               'Topic :: Software Development',
+               'Intended Audience :: Education']
+
+setup(name             = 'scratch',
+      version          = '1.0.0',
+      author           = 'Ben Croston',
+      author_email     = 'ben@croston.org',
+      description      = 'Interface Python with MIT Scratch 1.3+ (not 2.x)',
+      long_description = open('README.rst').read() + open('CHANGELOG.txt').read(),
+      license          = 'MIT',
+      keywords         = 'Scratch education',
+      url              = 'http://www.wyre-it.co.uk/py-scratch/',
+      classifiers      = classifiers,
+      packages         = find_packages())
+
