61d14ee937ecdb66116bfffe6624f78959d06fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:10 +0000

    Add package logfilenamer/0.3.3/logfilenamer-0.3.3.tar.gz

---
+++ packages/logfilenamer/logfilenamer-0.3.3.tar.gz/logfilenamer-0.3.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="logfilenamer",
+    version="0.3.3",
+    author="Simon Biggs",
+    author_email="mail@simonbiggs.net",
+    description="Name log files using Mosaiq exports.",
+    packages=[
+        "logfilenamer"
+    ],
+    license='AGPL3+',
+    entry_points={
+        'console_scripts': [
+            'logfilenamer=logfilenamer:main',
+        ],
+    },
+    install_requires=[
+        'trf2csv>=0.1.4',
+        'mosaiq_connection>=0.1.2',
+        'centre_config',
+        'attrs',
+        'pyyaml',
+        'python-dateutil'
+    ]
+)
