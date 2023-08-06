0349d5fd9e9fb4764c010ca4d68eb64a11ed4d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:39 +0000

    Add package labelord/0.4/labelord-0.4.tar.gz

---
+++ packages/labelord/labelord-0.4.tar.gz/labelord-0.4/setup.py
@@ -0,0 +1,66 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    long_description = ''.join(f.readlines())
+
+setup(
+    name='labelord',
+    version='0.4',
+    keywords='github labels management replication',
+    description='Simple CLI and WEB tools for managing GitHub labels',
+    long_description=long_description,
+    author='Marek Suchánek',
+    author_email='suchama4@fit.cvut.cz',
+    license='MIT',
+    url='https://github.com/MarekSuchanek/labelord',
+    zip_safe=False,
+    packages=find_packages(),
+    package_data={
+        'labelord': [
+            'static/*.js',
+            'static/*.css',
+            'templates/*.html',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'labelord = labelord:main',
+        ]
+    },
+    install_requires=[
+        'configparser',
+        'click',
+        'Flask',
+        'requests',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'betamax',
+        'betamax-serializers',
+        'flexmock',
+        'pytest',
+        'pytest-flake8',
+        'pytest-sugar',
+        'pytest-cov',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
+)
