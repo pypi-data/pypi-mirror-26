261dda7edd9e606ce9387939149b38e5ff3d916b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:43 +0000

    Add package vsts-cli-work/0.1.0b0/vsts_cli_work-0.1.0b0-py2.py3-none-any.whl

---
+++ packages/vsts-cli-work/vsts_cli_work-0.1.0b0-py2.py3-none-any.whl/vsts/cli/work/commands.py
@@ -0,0 +1,21 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from ._format import (transform_work_item_table_output,
+                      transform_work_item_query_result_table_output)
+from knack.commands import CommandSuperGroup
+
+
+def load_work_commands(cli_command_loader):
+    with CommandSuperGroup(__name__, cli_command_loader, 'vsts.cli.work.common.custom#{}') as sg:
+        with sg.group('work') as g:
+            g.command('item show', 'show_work_item',
+                      table_transformer=transform_work_item_table_output)
+            g.command('item create', 'create_work_item',
+                      table_transformer=transform_work_item_table_output)
+            g.command('item query', 'query_work_items',
+                      table_transformer=transform_work_item_query_result_table_output)
+            g.command('item update', 'update_work_item',
+                      table_transformer=transform_work_item_table_output)
