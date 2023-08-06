a36c2775be409d74583df122a1b7cbdd853f6a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:34 +0000

    Add package dxl-pygate/0.8.0/dxl-pygate-0.8.0.tar.gz

---
+++ packages/dxl-pygate/dxl-pygate-0.8.0.tar.gz/dxl-pygate-0.8.0/pygate/scripts/map_bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -o %J.out
+#SBATCH -e %J.err
+source ~/.bashrc
+echo 'Run on:' `hostname`
+echo 'Start at: ' `date`
+if [ ! -d {{local_work_directory}} ]; then
+    mkdir {{local_work_directory}}
+fi
+cp {{server_work_directory}}/* {{local_work_directory}}
+cd {{local_work_directory}}
+{{commands}}
+cp {{local_work_directory}}/* {{server_work_directory}}
+echo 'Finish at: ' `date`
