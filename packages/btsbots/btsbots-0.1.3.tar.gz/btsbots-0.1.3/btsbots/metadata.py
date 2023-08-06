cfea09da30b47dc7d822400d5c62dfdacfd5d4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:56 +0000

    Add package btsbots/0.1.3/btsbots-0.1.3.tar.gz

---
+++ packages/btsbots/btsbots-0.1.3.tar.gz/btsbots-0.1.3/btsbots/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project btsbots
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'btsbots'
+project = "btsbots"
+project_no_spaces = project.replace(' ', '')
+version = '0.1.3'
+description = 'trade bots client for https://btsbots.com'
+authors = ['Alt']
+authors_string = ', '.join(authors)
+emails = ['pch957@gmail.com']
+license = 'MIT'
+copyright = '2015 ' + authors_string
+url = 'https://github.com/pch957/python-btsbots'
