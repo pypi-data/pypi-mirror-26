7939a731b77650b2b4533e264d5600068aa1d546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:33 +0000

    Add package django-performance-testing/0.7.2/django-performance-testing-0.7.2.tar.gz

---
+++ packages/django-performance-testing/django-performance-testing-0.7.2.tar.gz/django-performance-testing-0.7.2/django_performance_testing/queries.py
@@ -0,0 +1,121 @@
+import random
+from django.db import connection
+from django.utils import six
+from django_performance_testing.signals import before_clearing_queries_log
+from django_performance_testing.core import \
+    BaseLimit, BaseCollector, NameValueResult
+from django_performance_testing.utils import DelegatingProxy
+
+
+def setup_sending_before_clearing_queries_log_signal():
+    class SignalSendingBeforeClearingQueriesProxy(DelegatingProxy):
+        def clear(self):
+            before_clearing_queries_log.send(sender=None, queries=tuple(self))
+            self.wrapped.clear()
+
+    connection.queries_log = SignalSendingBeforeClearingQueriesProxy(
+        connection.queries_log)
+
+
+class QueryCountResult(NameValueResult):
+
+    def __init__(self, queries, name):
+        self.queries = queries
+        super(QueryCountResult, self).__init__(
+            name=name, value=self.number_of_queries)
+
+    @property
+    def number_of_queries(self):
+        return len(self.queries)
+
+
+class QueryCollector(BaseCollector):
+
+    type_name = 'queries'
+
+    def __enter__(self):
+        self.queries = []
+        self.nr_of_queries_when_entering = len(connection.queries)
+        self.orig_force_debug_cursor = connection.force_debug_cursor
+        connection.force_debug_cursor = True
+        before_clearing_queries_log.connect(
+            self.queries_about_to_be_reset_handler)
+        return self
+
+    def queries_about_to_be_reset_handler(self,
+                                          signal, sender, queries, **kwargs):
+        self.store_queries()
+        self.nr_of_queries_when_entering = 0
+
+    def before_exit(self):
+        before_clearing_queries_log.disconnect(
+            self.queries_about_to_be_reset_handler)
+        connection.force_debug_cursor = self.orig_force_debug_cursor
+        self.store_queries()
+
+    def get_results_to_send(self):
+        by_type = dict(read=[], write=[], other=[])
+        for result in self.queries:
+            tp = classify_query(result['sql'])
+            by_type[tp].append(result)
+        by_type['total'] = self.queries
+        return list(
+            QueryCountResult(name=tp, queries=q)
+            for (tp, q) in six.iteritems(by_type))
+
+    @classmethod
+    def get_sample_results(cls):
+        read = """QUERY - 'SELECT "auth_user"."id" FROM "auth_group"'""" \
+            """- PARAMS = ()"""
+        write = """QUERY - 'UPDATE "auth_group" SET "name" = %s'""" \
+            """- PARAMS = ('bar',)"""
+        other = """QUERY - 'BEGIN TRANSACTION' - PARAMS = ()"""
+
+        def to_query(sql):
+            return {'sql': sql, 'time': '%.3f' % random.random()}
+
+        def to_single_result(*sqls):
+            qc = cls()
+            qc.queries = [to_query(sql) for sql in sqls]
+            return qc.get_results_to_send()
+
+        return [
+            to_single_result(*sqls)
+            for sqls in [
+                [read], [read, write], [read, read], [write, write],
+                [other, other], [read, write, other]
+            ]
+        ]
+
+    def store_queries(self):
+        self.queries += connection.queries[self.nr_of_queries_when_entering:]
+
+
+_query_token_to_classification = {
+    'SELECT': 'read',
+    'INSERT': 'write',
+    'UPDATE': 'write',
+    'DELETE': 'write',
+}
+
+
+def classify_query(sql):
+    if sql.startswith('QUERY ='):  # django 1.8
+        without_query_prefix = sql.split(' = ')[1]
+    else:
+        without_query_prefix = sql
+
+    first_token = without_query_prefix.split(' ')[0]
+    pattern = '\''
+    if pattern in first_token:
+        query_type_token = first_token.split(pattern)[1]
+    else:
+        query_type_token = first_token
+    return _query_token_to_classification.get(query_type_token, 'other')
+
+
+class QueryBatchLimit(BaseLimit):
+    collector_cls = QueryCollector
+
+    quantifier = 'many'
+    items_name = 'queries'
---
+++ packages/django-performance-testing/django-performance-testing-0.7.2.tar.gz/django-performance-testing-0.7.2/django_performance_testing/reports.py
@@ -0,0 +1,96 @@
+import copy
+from django.utils import six
+from django_performance_testing.signals import results_read
+import pprint
+
+
+class Result(object):
+    def __init__(self, value, context):
+        self.value = value
+        self.context = context
+
+    def __repr__(self):
+        return '{} {}'.format(self.value, pprint.pformat(self.context))
+
+
+class WorstReport(object):
+
+    def __init__(self):
+        results_read.connect(self.handle_results_collected)
+        self.data = {}
+
+    def handle_results_collected(self, signal, sender, results, context, **kw):
+        name_value_pairs = list(map(self.to_name_value_pair, results))
+        self.ensure_unique_names(name_value_pairs)
+
+        def get_data(*parts):
+            d = self.data
+            for p in parts:
+                d.setdefault(p, {})
+                d = d[p]
+            return d
+
+        def handle_result(name, result):
+            d = get_data(sender.id_, sender.type_name)
+            current = d.get(name, None)
+            if current is None or current.value < result:
+                # TODO: once serialization, no need to deepcopy
+                d[name] = Result(value=result, context=copy.deepcopy(context))
+
+        for name, result in name_value_pairs:
+            handle_result(name, result)
+
+    def to_name_value_pair(self, value):
+        return (
+            getattr(value, 'name', ''),
+            getattr(value, 'value', value)
+        )
+
+    def ensure_unique_names(self, name_value_pairs):
+        dupes = set()
+        last = None
+        for (name, value) in sorted(name_value_pairs):
+            if name == last:
+                dupes.add(name)
+            last = name
+        if dupes:
+            dupes_as_str = ', '.join(map(repr, dupes))
+            raise TypeError(
+                'Duplicate result name(s): {}'.format(dupes_as_str))
+
+    def render(self, stream):
+        if not self.data:
+            return
+        stream.write('\nWorst Performing Items\n\n')
+        self.render_dict(stream=stream, d=self.data, indent=0, underline='=')
+
+    def render_dict(self, stream, d, indent, underline=None):
+        prefix = ' '*indent
+
+        for k in sorted(d.keys()):
+            v = d[k]
+            val, nextdict = self._to_val_nextdict(v)
+
+            if val:
+                val = ' {}'.format(val)
+            else:
+                val = ''
+            line = '{}{}:{}'.format(prefix, k, val)
+            stream.write('{}\n'.format(line))
+            if underline:
+                stream.write('{}\n'.format(underline*len(line)))
+            if nextdict:
+                self.render_dict(stream=stream, d=nextdict, indent=indent + 2)
+
+    def _to_val_nextdict(self, v):
+        if isinstance(v, Result):
+            return (v.value, v.context)
+        elif isinstance(v, dict):
+            return (None, v)
+        else:
+            return (v, None)
+
+    def rendered(self):
+        out = six.StringIO()
+        self.render(out)
+        return out.getvalue()
