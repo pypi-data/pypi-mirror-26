c8637a92e0bb318fc2f9d053bc1086260fd83919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:51 +0000

    Add package ilbcj/1.0.2/ilbcj-1.0.2.tar.gz

---
+++ packages/ilbcj/ilbcj-1.0.2.tar.gz/ilbcj-1.0.2/ilbcj/nester.py
@@ -0,0 +1,8 @@
+﻿def printLOL(lists, indent=False, level=0):
+	for item in lists:
+		if (isinstance(item,list)):
+			printLOL(item, indent, level+1)
+		else:
+			if(indent):
+				print("\t"*level,end="")
+			print(item)
---
+++ packages/ilbcj/ilbcj-1.0.2.tar.gz/ilbcj-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'ilbcj',
+    version = '1.0.2',
+    py_modules = ['ilbcj.nester'],
+    author = 'ilbcj',
+    author_email = '16274457@qq.com',
+    url = 'http://ilbcj.com',
+    description = 'a simple printer of nested lists',
+    license='MIT',
+    )
