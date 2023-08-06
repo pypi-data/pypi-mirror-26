7e015513213b9e508ba18419f9dcf227610275ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:32 +0000

    Add package tweet-parser/1.13.0/tweet_parser-1.13.0.tar.gz

---
+++ packages/tweet-parser/tweet_parser-1.13.0.tar.gz/tweet_parser-1.13.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='tweet_parser',
+      description="Tools for Tweet parsing",
+      url='https://github.com/tw-ddis/tweet_parser',
+      author='Fiona Pigott, Jeff Kolb, Josh Montague, Aaron Gonzales',
+      long_description=open('README.rst', 'r').read(),
+      author_email='fpigott@twitter.com',
+      license='MIT',
+      version='1.13.0',
+      packages=find_packages(),
+      scripts=["tools/parse_tweets.py"],
+      install_requires=[],
+      )
