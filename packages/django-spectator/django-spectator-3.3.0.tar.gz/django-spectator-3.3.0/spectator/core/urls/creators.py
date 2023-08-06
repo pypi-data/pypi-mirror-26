600e02a00fbf55f1588192dcda86cf229539fee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:38 +0000

    Add package django-spectator/3.3.0/django-spectator-3.3.0.tar.gz

---
+++ packages/django-spectator/django-spectator-3.3.0.tar.gz/django-spectator-3.3.0/spectator/core/urls/creators.py
@@ -0,0 +1,29 @@
+from django.conf.urls import url
+
+from .. import views
+
+# All Creator-specific URLs, so they can be included under whatever path is
+# required.
+# This should be under the namespace 'spectator:creators'.
+
+urlpatterns = [
+    # Individuals:
+    url(
+        regex=r"^$",
+        view=views.CreatorListView.as_view(),
+        name='creator_list'
+    ),
+    #  Groups:
+    url(
+        regex=r"^groups/$",
+        view=views.CreatorListView.as_view(),
+        name='creator_list_group',
+        kwargs={'kind': 'group',}
+    ),
+    url(
+        regex=r"^(?P<slug>[\w-]+)/$",
+        view=views.CreatorDetailView.as_view(),
+        name='creator_detail'
+    ),
+]
+
