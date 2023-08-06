5b076f9963dbcabfba6c55b5ba4e2325d728d050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:50 +0000

    Add package Mastodon.py/1.1.2/Mastodon.py-1.1.2.tar.gz

---
+++ packages/Mastodon.py/Mastodon.py-1.1.2.tar.gz/Mastodon.py-1.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='Mastodon.py',
+      version='1.1.2',
+      description='Python wrapper for the Mastodon API',
+      packages=['mastodon'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      install_requires=['requests', 'python-dateutil', 'six', 'pytz'],
+      url='https://github.com/halcy/Mastodon.py',
+      author='Lorenz Diener',
+      author_email='lorenzd+mastodonpypypi@gmail.com',
+      license='MIT',
+      keywords='mastodon api microblogging',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Communications',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ])
