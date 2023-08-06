83a035874a1e6fef77ebb54cf1678ac7983bec82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:42 +0000

    Add package coach-sytang/1.0.0/coach-sytang-1.0.0.tar.gz

---
+++ packages/coach-sytang/coach-sytang-1.0.0.tar.gz/coach-sytang-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'coach-sytang',
+        version = '1.0.0',
+        py_modles = ['coach-sytang'],
+        author = 'sytang',
+        author_email = '3048608732@qq.com',
+        url = 'https://en.mail.qq.com',
+        description = "A list of people's fast times",
+     )
