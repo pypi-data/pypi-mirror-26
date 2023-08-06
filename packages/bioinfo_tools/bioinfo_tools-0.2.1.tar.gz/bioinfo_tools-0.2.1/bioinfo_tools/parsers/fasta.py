ae5c4f197a29f13ace09eca9fb4840b1a9e1a01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:01 +0000

    Add package bioinfo_tools/0.2.1/bioinfo_tools-0.2.1.tar.gz

---
+++ packages/bioinfo_tools/bioinfo_tools-0.2.1.tar.gz/bioinfo_tools-0.2.1/bioinfo_tools/parsers/fasta.py
@@ -0,0 +1,19 @@
+from typing import Tuple, Iterable
+
+import re
+from Bio import SeqIO
+
+
+class FastaParser(object):
+    def __init__(self):
+        pass
+    
+    def read(self, fasta_file: str, id_separator = "\||\:") -> Iterable[Tuple[str,str]]:
+        with open(fasta_file) as fh:
+            for record in SeqIO.parse(fh, "fasta"):
+                try:
+                    seqid = re.split(id_separator, record.id, 1)[1]
+                except IndexError:
+                    seqid = record.id
+                seq = str(record.seq)
+                yield (seqid, seq)
