1fbf209d88d5e96e3448cfe354000fcc4a966403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:08 +0000

    Add package nuheat/0.1.1/nuheat-0.1.1.tar.gz

---
+++ packages/nuheat/nuheat-0.1.1.tar.gz/nuheat-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+  name = 'nuheat',
+  packages = ['nuheat'],
+  version = '0.1.1',
+  description = 'A Python library that allows control of connected radiant floor thermostats.',
+  author = 'Derek Brooks',
+  author_email = 'derek@broox.com',
+  url = 'https://github.com/broox/python-nuheat',
+  download_url = 'https://github.com/broox/python-nuheat/archive/0.1.1.tar.gz',
+  license = 'MIT',
+  keywords = ['nuheat', 'thermostat', 'home automation', 'python'],
+  classifiers = [],
+)
