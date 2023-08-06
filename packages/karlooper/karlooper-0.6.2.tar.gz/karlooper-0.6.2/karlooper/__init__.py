6ce272cc689256994e2fba23df1fd0b28ae7bd40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:20 +0000

    Add package karlooper/0.6.2/karlooper-0.6.2.tar.gz

---
+++ packages/karlooper/karlooper-0.6.2.tar.gz/karlooper-0.6.2/karlooper/__init__.py
@@ -0,0 +1,20 @@
+# -*-coding:utf-8-*-
+
+"""
+
+"""
+
+__author__ = 'karlvorndoenitz@gmail.com'
+__version__ = '0.6.2'
+__packages__ = [
+    "autoreload",
+    "config",
+    "coroutine",
+    "escape",
+    "http_parser",
+    "logger",
+    "router",
+    "template",
+    "utils",
+    "web"
+]
