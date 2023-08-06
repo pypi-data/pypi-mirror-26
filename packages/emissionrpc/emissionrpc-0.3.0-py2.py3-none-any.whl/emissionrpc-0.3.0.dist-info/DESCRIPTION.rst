3ebb28b570104b69c65403156aacd7f2d6f58413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:07 +0000

    Add package emissionrpc/0.3.0/emissionrpc-0.3.0-py2.py3-none-any.whl

---
+++ packages/emissionrpc/emissionrpc-0.3.0-py2.py3-none-any.whl/emissionrpc-0.3.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,13 @@
+EmissionRPC
+========
+
+EmissionRPC, another wrapper JSON-RPC to Transmission
+
+
+Usage
+-----
+
+Still in development
+
+
+
---
+++ packages/emissionrpc/emissionrpc-0.3.0-py2.py3-none-any.whl/emissionrpc/__version__.py
@@ -0,0 +1,12 @@
+version = '0.3.0'
+
+name = 'emissionrpc'
+
+description = 'Emission, Another Wrapper JSON-RPC to Transmission'
+
+url = 'https://github.com/Germione/emissionrpc'
+
+author = 'Jerome Pradier'
+author_email = 'jerome.pradier@gmail.com'
+
+license = 'MIT'
