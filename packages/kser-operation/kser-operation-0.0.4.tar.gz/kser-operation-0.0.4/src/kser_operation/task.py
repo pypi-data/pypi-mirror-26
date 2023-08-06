ab52b4dc4f32443bbe22db0b0bbdb29307babcdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:01 +0000

    Add package kser-operation/0.0.4/kser-operation-0.0.4.tar.gz

---
+++ packages/kser-operation/kser-operation-0.0.4.tar.gz/kser-operation-0.0.4/src/kser_operation/task.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. codeauthor:: Cédric Dumay <cedric.dumay@gmail.com>
+
+
+"""
+from kser.entry import Entrypoint
+
+
+class Task(Entrypoint):
+    """"""
+    def __init__(self, uuid=None, params=None, status="PENDING", **kwargs):
+        Entrypoint.__init__(self, uuid=uuid, params=params, result=None)
+        self.status = status
+        self.metadata = kwargs
+
+    def get_attr(self, item):
+        attr = "{}Id".format(item)
+        return attr, getattr(getattr(self, item, self), attr, None)
+
+    def __repr__(self):
+        return "Task [{}](uuid={},status={}, params={})".format(
+            self.__class__.path, self.uuid, self.status, self.params
+        )
