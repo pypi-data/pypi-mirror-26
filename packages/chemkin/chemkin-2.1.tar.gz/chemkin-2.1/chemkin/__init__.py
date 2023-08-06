684255e9b3694a7ef51456c5db8f9902bdcb711c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:50 +0000

    Add package chemkin/2.1/chemkin-2.1.tar.gz

---
+++ packages/chemkin/chemkin-2.1.tar.gz/chemkin-2.1/chemkin/__init__.py
@@ -0,0 +1,8 @@
+# Import all module attributes into chemkin namespace.
+from chemkin.preprocessing.parse_xml import *
+from chemkin.reaction.base_rxn import *
+from chemkin.reaction.elementary_rxn import *
+from chemkin.reaction.non_elementary_rxn import *
+from chemkin.reaction.reaction_coefficients import *
+from chemkin.thermodynamics.thermo import *
+from chemkin.chemkin_errors import *
