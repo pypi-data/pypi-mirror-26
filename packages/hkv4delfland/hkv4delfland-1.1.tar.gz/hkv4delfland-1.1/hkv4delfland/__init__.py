1b948d523bba1df64f6bd5ddf982891c6f1517fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:07 +0000

    Add package hkv4delfland/1.1/hkv4delfland-1.1.tar.gz

---
+++ packages/hkv4delfland/hkv4delfland-1.1.tar.gz/hkv4delfland-1.1/hkv4delfland/__init__.py
@@ -0,0 +1,21 @@
+from hkv4delfland.io.bui import __bui_class
+from hkv4delfland.io.his import __his_class
+from hkv4delfland.core.waterlevelstat import __waterlevelstat_class
+from hkv4delfland.core.plausibility import __plausibility_class
+
+__doc__ = """package for water-statistics and plausibility checker using his- and bui-file"""
+
+__version__ = '1.1'
+
+# initiate class for .bui-files
+__bui = __bui_class()
+read_bui = __bui.read_bui
+
+# initiate class for .his-files
+read_his = __his_class()
+
+# initiate class for waterlevelstats
+waterlevelstat = __waterlevelstat_class()
+
+# initiate class for plausibilitychecker
+plausibility = __plausibility_class()
