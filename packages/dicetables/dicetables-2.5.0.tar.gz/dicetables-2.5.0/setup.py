4fd03d12d649c197ef395c7a8fb9672f3e9d6f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:57 +0000

    Add package dicetables/2.5.0/dicetables-2.5.0.tar.gz

---
+++ packages/dicetables/dicetables-2.5.0.tar.gz/dicetables-2.5.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='dicetables',
+      version='2.5.0',
+      description='get all combinations for any set of dice',
+      long_description=readme(),
+      keywords='dice, die, statistics, table, probability, combinations',
+      url='http://github.com/eric-s-s/dice-tables',
+      author='Eric Shaw',
+      author_email='shaweric01@gmail.com',
+      license='MIT',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Games/Entertainment :: Role-Playing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+      ],
+      packages=['dicetables', 'dicetables.tools', 'dicetables.factory', 'dicetables.eventsbases'],
+      install_requires=[],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data=True,
+      zip_safe=False)
