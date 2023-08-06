3c5a02c6f9d55b5b105b853e42f861f9a8af8939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:27 +0000

    Add package django-logging-json/1.12/django-logging-json-1.12.tar.gz

---
+++ packages/django-logging-json/django-logging-json-1.12.tar.gz/django-logging-json-1.12/django_logging/__init__.py
@@ -0,0 +1,10 @@
+from . import handlers
+from . import logger
+from .log_object import LogObject, ErrorLogObject, SqlLogObject
+
+
+__version__ = '1.12'
+__author__ = 'Ciprian Tarta'
+
+log = logger.get_logger()
+
