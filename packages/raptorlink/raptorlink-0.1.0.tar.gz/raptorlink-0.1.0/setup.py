b116049b2289ed4656990a598bb9fae76201082e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:07 +0000

    Add package raptorlink/0.1.0/raptorlink-0.1.0.tar.gz

---
+++ packages/raptorlink/raptorlink-0.1.0.tar.gz/raptorlink-0.1.0/setup.py
@@ -0,0 +1,31 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='raptorlink',
+    version="0.1.0",
+    description = 'test plugins for submit test result to raptor server based on nosetests',
+    author = 'liuchengtao',
+    author_email = 'chengtao.liu@yahoo.com',
+    license = 'MIT',
+    long_description = """\
+
+Extra plugins for the nose testing framework to submit test result to raptor server\n
+
+usage:\n
+>>> nosetests --with-plan-loader --plan-file <test_plan_file> --loop <loop_num>\n
+
+start from command-line:
+    nosetests --with-plan-loader --plan-file plan1 --loop 100 --report-to-raptor --
+
+""",
+    packages = ['raptorlink'],
+    entry_points = {
+        'nose.plugins': [
+            'plan-loader = raptorlink.raptorlink:RaptorPlugin'
+            ],
+         },
+)
+
