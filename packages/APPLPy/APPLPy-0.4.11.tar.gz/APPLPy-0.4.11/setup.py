68be6a96db6727e3e8bbb1e752055a5a2ce2b082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:09 +0000

    Add package APPLPy/0.4.11/APPLPy-0.4.11.tar.gz

---
+++ packages/APPLPy/APPLPy-0.4.11.tar.gz/APPLPy-0.4.11/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+# To upload to PyPi run python setup.py sdist upload -r pypi
+setup(
+    name='APPLPy',
+    version='0.4.11',
+    author='Matthew Robinson',
+    author_email='mthw.wm.robinson@gmail.com',
+    packages=['applpy','applpy.test'],
+    scripts=[],
+    url='https://pypi.python.org/pypi/APPLPy/',
+    license='LICENSE.txt',
+    description='open source computational probability software',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "sympy",
+        "scipy",
+        "matplotlib",
+        "seaborn",
+        "numpy",
+        "mpmath",
+        "pandas"
+        ]
+    )
