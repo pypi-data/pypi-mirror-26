efe6adb2cf78f86cf4e43357e101d21c4fe92a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:54 +0000

    Add package tangerine/0.1.1/tangerine-0.1.1.tar.gz

---
+++ packages/tangerine/tangerine-0.1.1.tar.gz/tangerine-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="tangerine",
+    version="0.1.1",
+    url="https://github.com/kevinjqiu/tangerine",
+
+    author="Kevin J. Qiu",
+    author_email="kevin@idempotent.ca",
+
+    description="API for the Tangerine Bank",
+    long_description=open('README.md').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=['requests'],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
