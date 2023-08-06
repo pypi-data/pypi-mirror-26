452e8dc1cbf2a92d8ba3e86a24df3789666a19a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:16 +0000

    Add package validatexmls/0.1.7/validatexmls-0.1.7.tar.gz

---
+++ packages/validatexmls/validatexmls-0.1.7.tar.gz/validatexmls-0.1.7/validatexmls/__main__.py
@@ -0,0 +1,14 @@
+import os
+from validatexmls import main
+#import validatexmls
+#import click
+
+__main__dir = os.path.abspath(os.path.dirname(__file__))
+print('Running __main__ in dir:', __main__dir)
+
+#def main():
+#   main()
+
+
+if __name__ == '__main__':
+    main()
