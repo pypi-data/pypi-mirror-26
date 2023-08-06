5e76da0ac2bee48c56a4e7c8223dc054779349cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:25 +0000

    Add package test_postgre_pd/0.5/test_postgre_pd-0.5.tar.gz

---
+++ packages/test_postgre_pd/test_postgre_pd-0.5.tar.gz/test_postgre_pd-0.5/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='test_postgre_pd',
+	version='0.5',
+	description='test3 postgpd upload. package for better postgresql-pandas cooperation',
+	author= 'Mateusz Janczak',
+	author_email='mateusz.janczak@op.pl'
+)
