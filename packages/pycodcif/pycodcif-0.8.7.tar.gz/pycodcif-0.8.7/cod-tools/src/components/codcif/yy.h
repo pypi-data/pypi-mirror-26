0b8b124c13dba35c337885eea2a63e38112c4755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:57 +0000

    Add package pycodcif/0.8.7/pycodcif-0.8.7.tar.gz

---
+++ packages/pycodcif/pycodcif-0.8.7.tar.gz/pycodcif-0.8.7/cod-tools/src/components/codcif/yy.h
@@ -0,0 +1,25 @@
+
+/* external declarations specific to yacc */
+
+#ifndef __YY_H
+#define __YY_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <cexceptions.h>
+
+extern FILE *yyin;
+extern FILE *yy2in;
+
+extern int yyparse( void );
+
+/* For testing of lexical analysers: */
+extern int yylex( void );
+extern char *yytext;
+
+#ifdef YYDEBUG   
+   extern int yydebug;
+   extern int yy_flex_debug;
+#endif
+
+#endif
