6c9ae493662a0c3b283c5c99f8318dc60df3e10d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:30 +0000

    Add package sdp-test-package/0.1/sdp_test_package-0.1-py3-none-any.whl

---
+++ packages/sdp-test-package/sdp_test_package-0.1-py3-none-any.whl/sdp_test_package/main.py
@@ -0,0 +1,5 @@
+import mod1
+from mod1 import *
+
+sum = add_int(10,20)
+print ("Sum of 10 and 20 is : " , sum)
---
+++ packages/sdp-test-package/sdp_test_package-0.1-py3-none-any.whl/sdp_test_package/mod1.py
@@ -0,0 +1,7 @@
+'''
+    This is a sample module. This is just to test.
+'''
+
+def add_int(a,b):
+    c = a+b
+    return c
