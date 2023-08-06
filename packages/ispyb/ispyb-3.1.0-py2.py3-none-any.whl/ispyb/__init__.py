73bb26d7fbd90621695f5e23c75818cc9b003591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:16 +0000

    Add package ispyb/3.1.0/ispyb-3.1.0-py2.py3-none-any.whl

---
+++ packages/ispyb/ispyb-3.1.0-py2.py3-none-any.whl/ispyb/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division, print_function
+
+__version__ = '3.1.0'
+
+# add legacy imports to top level name space
+import ispyb.legacy.common.driver
+legacy_get_driver = ispyb.legacy.common.driver.get_driver
+legacy_Backend = ispyb.legacy.common.driver.Backend
---
+++ packages/ispyb/ispyb-3.1.0-py2.py3-none-any.whl/ispyb/exception.py
@@ -0,0 +1,17 @@
+class ISPyBException(Exception):
+  '''Base class for all exceptions'''
+
+class ISPyBNoResultException(ISPyBException):
+  '''Query returned no result.'''
+
+class ISPyBWriteFailed(ISPyBException):
+  '''Record could not be inserted, updated or deleted. This could be due to
+  illegal values, the wrong number of parameters, a violation of table or
+  index constraints, or a database failure.'''
+
+class ISPyBRetrieveFailed(ISPyBException):
+  '''Record(s) could not be retrieved. This could be due to invalid argument
+  values, or a database failure. '''
+
+class ISPyBKeyProblem(ISPyBException):
+  '''A mandatory key is missing or its value is None.'''
