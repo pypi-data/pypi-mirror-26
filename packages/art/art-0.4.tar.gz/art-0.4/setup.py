b6aa56c313b45a9fad1ae85091a9295b5630ea95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:11 +0000

    Add package art/0.4/art-0.4.tar.gz

---
+++ packages/art/art-0.4.tar.gz/art-0.4/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'art',
+  packages = ['art'],
+  version = '0.4',
+  description = 'ASCII Art Collection In Python',
+  long_description='ASCII Art Collection In Python',
+  author = 'Sepand Haghighi',
+  author_email = 'sepand@qpage.ir',
+  url = 'https://github.com/sepandhaghighi/art',
+  download_url = 'https://github.com/sepandhaghighi/art/tarball/v0.4',
+  keywords = ['ascii', 'art', 'python3','python','text'],
+  install_requires=[
+	  'codecov',
+      ],
+  classifiers = [],
+  license='MIT',
+)
