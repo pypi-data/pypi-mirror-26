76969114d1a1a2c8f7edf824f5ccfd771ec2957c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:23 +0000

    Add package slovar/0.0.22/slovar-0.0.22-py2-none-any.whl

---
+++ packages/slovar/slovar-0.0.22-py2-none-any.whl/slovar/__init__.py
@@ -0,0 +1,5 @@
+# Export all the things at top level!
+from slovar.main import *
+from slovar.operations.dictionaries import *
+from slovar.operations.lists import *
+from slovar.operations.strings import *
