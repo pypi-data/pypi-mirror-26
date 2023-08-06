df7ff35177209002e0bd040ff0f8fa0b7de4ec01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:42 +0000

    Add package goose3/3.0.7/goose3-3.0.7-py3-none-any.whl

---
+++ packages/goose3/goose3-3.0.7-py3-none-any.whl/goose3/network.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""\
+This is a python port of "Goose" orignialy licensed to Gravity.com
+under one or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.
+
+Python port was written by Xavier Grangier for Recrutae
+
+Gravity.com licenses this file
+to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import weakref
+import requests
+
+
+class NetworkError(RuntimeError):
+    def __init__(self, status_code, reason):
+        self.reason = reason
+        self.status_code = status_code
+
+
+class NetworkFetcher(object):
+
+    def __init__(self, config):
+        self.config = config
+        self._connection = requests.Session()
+        self._connection.headers['User-agent'] = self.config.browser_user_agent
+        self._finalizer = weakref.finalize(self, self.close)
+
+        self._url = None
+        self.response = None
+        self.headers = None
+
+    def close(self):
+        if self._connection is not None:
+            self._connection.close()
+            self._connection = None
+
+    def get_url(self):
+        return self._url
+
+    def fetch(self, url):
+        response = self._connection.get(url, timeout=self.config.http_timeout, headers=self.headers)
+        if response.ok:
+            self._url = response.url
+            text = response.content
+        else:
+            self._url = None
+            text = None
+            if self.config.strict:
+                raise NetworkError(response.status_code, response.reason)
+
+        return text
