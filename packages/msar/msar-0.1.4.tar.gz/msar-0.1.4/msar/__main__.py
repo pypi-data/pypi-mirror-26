ae5bdd64cf3531bb06ebd64202ccc3e746dd0636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:09 +0000

    Add package msar/0.1.4/msar-0.1.4.tar.gz

---
+++ packages/msar/msar-0.1.4.tar.gz/msar-0.1.4/msar/__main__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 10/11/2017
+
+@author: seven
+"""
+from msar import msar
+
+
+def main():
+    msar().run()
+
+
+if __name__ == '__main__':
+    main()
