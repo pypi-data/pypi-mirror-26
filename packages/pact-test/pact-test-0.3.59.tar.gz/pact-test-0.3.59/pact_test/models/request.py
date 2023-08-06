4031417c0f677035f4b0b6c62c516f381b497e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:55 +0000

    Add package pact-test/0.3.59/pact-test-0.3.59.tar.gz

---
+++ packages/pact-test/pact-test-0.3.59.tar.gz/pact-test-0.3.59/pact_test/models/request.py
@@ -0,0 +1,12 @@
+class PactRequest(object):
+    def __init__(self, method='', body={}, headers=[], path='', query=''):
+        print('BODY??? >>> ' + str(body))
+        self.body = body
+        print('BODY??? >>> ' + str(self.body))
+        self.path = path
+        self.query = query
+        self.method = method
+        self.headers = headers
+
+    def __str__(self):
+        return "$$$ BODY: " + str(self.body) + ", HEADERS: " + str(self.headers)
