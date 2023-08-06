563420488f0ed7727efe547a076ad349ac46f007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:10 +0000

    Add package dstoolbox/0.6.1/dstoolbox-0.6.1.tar.gz

---
+++ packages/dstoolbox/dstoolbox-0.6.1.tar.gz/dstoolbox-0.6.1/dstoolbox/transformers/__init__.py
@@ -0,0 +1,30 @@
+"""Use this to allow imports, e.g.
+
+```
+from dstoolbox.transformers import ItemSelector
+```
+
+instead of
+
+```
+from dstoolbox.transformers.slicing import ItemSelector
+```
+
+"""
+
+from dstoolbox.transformers.casting import ToDataFrame
+from dstoolbox.transformers.padding import Padder2d
+from dstoolbox.transformers.padding import Padder3d
+from dstoolbox.transformers.preprocessing import ParallelFunctionTransformer
+from dstoolbox.transformers.preprocessing import XLabelEncoder
+from dstoolbox.transformers.slicing import ItemSelector
+
+
+__all__ = [
+    'ItemSelector',
+    'ParallelFunctionTransformer',
+    'Padder2d',
+    'Padder3d',
+    'ToDataFrame',
+    'XLabelEncoder',
+]
