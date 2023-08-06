bd6260a5837d1b6745435e76ff477db30fd60a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:11 +0000

    Add package transifex-client/0.12.4/transifex_client-0.12.4-py2.7.egg

---
+++ packages/transifex-client/transifex_client-0.12.4-py2.7.egg/txclib/exceptions.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+Exception classes for the tx client.
+"""
+
+
+class UnInitializedError(Exception):
+    """The project directory has not been initialized."""
+
+
+class UnknownCommandError(Exception):
+    """The provided command is not supported."""
+
+class MalformedConfigFile(Exception):
+    pass
+
+# HTTP exceptions
+
+
+class HttpNotFound(Exception):
+    pass
+
+
+class HttpNotAuthorized(Exception):
+    pass
