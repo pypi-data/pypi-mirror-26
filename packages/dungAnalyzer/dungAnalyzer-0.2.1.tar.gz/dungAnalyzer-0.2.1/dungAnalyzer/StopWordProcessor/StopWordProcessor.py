392fed8aba819d96f7924afb19b8bb188ba000eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:59 +0000

    Add package dungAnalyzer/0.2.1/dungAnalyzer-0.2.1.tar.gz

---
+++ packages/dungAnalyzer/dungAnalyzer-0.2.1.tar.gz/dungAnalyzer-0.2.1/dungAnalyzer/StopWordProcessor/StopWordProcessor.py
@@ -0,0 +1,35 @@
+import os
+import re
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+
+
+class StopWordProcessor:
+    def __init__(self):
+        self.stopWordList = list(stopwords.words('english'))
+        self.re_machine = re.compile(r'[ `~!@#$%^&*()\-_=+[{\]}\\|;:\'",<.>/?\n।\u200c‘’—]')
+        with open(os.path.join(os.path.dirname(__file__), '../resources/stopwords-bn.txt')) as f:
+            self.stopWordList += [word.strip() for word in f.readlines()]
+            pass
+        pass
+
+    def tokenizeString(self, inputString):
+        words = self.re_machine.split(inputString)
+        words = [word.strip().lower() for word in words if len(word.strip()) != 0]
+        return words
+        pass
+
+    def removeStopWords(self, inputString):
+        words = self.tokenizeString(inputString)
+        return ' '.join([word for word in words if self.stopWordList.count(word) == 0])
+        pass
+    pass
+
+
+if __name__ == '__main__':
+    sw_proc = StopWordProcessor()
+    print(sw_proc.removeStopWords('test in testing and what?'))
+    print(sw_proc.removeStopWords('test and in আমি বাংলার গান গাই আর খেলি'))
+
