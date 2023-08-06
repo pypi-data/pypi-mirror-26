9071ff7ce36d6ddcda57f39e0356b84ac15be30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:46 +0000

    Add package framewalker/0.8/framewalker-0.8.tar.gz

---
+++ packages/framewalker/framewalker-0.8.tar.gz/framewalker-0.8/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='framewalker',
+	packages=['framewalker'],
+    version='0.8',
+    url='https://github.com/stefanbie/FrameWalker',
+    install_requires=['peewee','PyMySQL']
+)
