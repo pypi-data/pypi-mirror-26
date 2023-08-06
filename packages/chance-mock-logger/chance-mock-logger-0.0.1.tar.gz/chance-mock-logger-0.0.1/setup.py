4fc31867d835c4b8af78ce1be6d2e5e26e01ecf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:08 +0000

    Add package chance-mock-logger/0.0.1/chance-mock-logger-0.0.1.tar.gz

---
+++ packages/chance-mock-logger/chance-mock-logger-0.0.1.tar.gz/chance-mock-logger-0.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File: setup.py
+# Author: Jimin Huang <huangjimin@whu.edu.cn>
+# Date: 19.10.2017
+from setuptools import setup
+
+from mock_logger import __version__
+
+
+setup(
+    name='chance-mock-logger',
+    version=__version__,
+    description='The mock logging handler for chancefocus',
+    url='https://gitee.com/QianFuFinancial/chance-mock-logger.git',
+    author='Jimin Huang',
+    author_email='huangjimin@whu.edu.cn',
+    license='MIT',
+    packages=['mock_logger'],
+    install_requires=[
+        'nose>=1.3.7',
+        'coverage>=4.1',
+    ],
+    zip_safe=False,
+)
