38bd0c471de6a2aef3900ff362d15eac29d01b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:49 +0000

    Add package lineTool/1.0.3/lineTool-1.0.3.tar.gz

---
+++ packages/lineTool/lineTool-1.0.3.tar.gz/lineTool-1.0.3/lineTool.py
@@ -0,0 +1,16 @@
+import requests
+
+"""
+發送 Line Notify 訊息
+"""
+def lineNotify(token, msg):
+
+    url = "https://notify-api.line.me/api/notify"
+    headers = {
+        "Authorization": "Bearer " + token, 
+        "Content-Type" : "application/x-www-form-urlencoded"
+    }
+    
+    payload = {'message': msg}
+    r = requests.post(url, headers = headers, params = payload)
+    return r.status_code
---
+++ packages/lineTool/lineTool-1.0.3.tar.gz/lineTool-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup (
+    name            = "lineTool", 
+    version         = "1.0.3",
+    py_modules      = ["lineTool"],
+    author          = "rocky.wang",
+    author_email    = "rockywang101@gmail.com",
+    url             = "https://rockywang101.github.io",
+    description     = "A Simple Line Notify Tool"
+)
+
+
+
