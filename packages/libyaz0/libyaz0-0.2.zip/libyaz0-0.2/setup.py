b176f67fa4d5b28ea0c0b551880ddced42775225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:37 +0000

    Add package libyaz0/0.2/libyaz0-0.2.zip

---
+++ packages/libyaz0/libyaz0-0.2.zip/libyaz0-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'libyaz0',
+  packages = ['libyaz0'],
+  version = '0.2',
+  description = 'A library for compressing/decompressing Yaz0/1 compression formats',
+  author = 'AboodXD',
+  author_email = 'aboood40091@gmail.com',
+  url = 'https://github.com/aboood40091/libyaz0',
+  download_url = 'https://github.com/aboood40091/libyaz0/archive/v0.2.tar.gz',
+  keywords = ['compress', 'decompress', 'yaz0', 'szs', 'yaz1'],
+  classifiers = [],
+)
