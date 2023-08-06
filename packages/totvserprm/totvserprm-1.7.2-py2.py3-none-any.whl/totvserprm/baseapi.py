4c03ff68cd77c05ec53f073905392df44ace63b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:17 +0000

    Add package totvserprm/1.7.2/totvserprm-1.7.2-py2.py3-none-any.whl

---
+++ packages/totvserprm/totvserprm-1.7.2-py2.py3-none-any.whl/totvserprm/baseapi.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from auth import create_service
+from dicttoxml import dicttoxml
+from lxml import objectify
+from totvserprm.utils import normalize_xml
+
+
+class BaseApi(object):
+    dataservername = ''
+
+    def __init__(self, server, username, password):
+        self.service = create_service(server, username, password)
+
+    def create(self, dict, context):
+        xml = dicttoxml(dict, attr_type=False)
+        return self.service.SaveRecord(DataServerName=self.dataservername, XML=xml, Contexto=context)
+
+    def get(self, codcoligada, id):
+        primary_key = '{};{}'.format(codcoligada, id)
+        return_from_api = self.service.ReadRecord(
+            DataServerName=self.dataservername, PrimaryKey=primary_key, Contexto='CODCOLIGADA={}'.format(codcoligada))
+        return_from_api = normalize_xml(return_from_api)
+        return objectify.fromstring(return_from_api)
+
+    def all(self, codcoligada):
+        return_from_api = self.service.ReadView(
+            DataServerName=self.dataservername, Filtro='CODCOLIGADA={}'.format(
+            codcoligada), Contexto='CODCOLIGADA={}'.format(codcoligada))
+        return_from_api = normalize_xml(return_from_api)
+        return objectify.fromstring(return_from_api)
