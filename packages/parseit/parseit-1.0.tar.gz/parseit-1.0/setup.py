55f51ce65f0695d143427270da2252d0e869a06d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:19 +0000

    Add package parseit/1.0/parseit-1.0.tar.gz

---
+++ packages/parseit/parseit-1.0.tar.gz/parseit-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='parseit',
+    version='1.0',
+    packages=['parseit'],
+    url='https://github.com/',
+    license='MIT',
+    author='mincrmatt12',
+    author_email='matthewmirvish@hotmail.com',
+    description='Simple parser that creates ASTs',
+    install_requires=["future"],
+    python_requires=">=2.7"
+)
