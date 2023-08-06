3da5a510b4bbfeaec5005e08d82fdcac470ef812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:15 +0000

    Add package python-language-server/0.10.0/python-language-server-0.10.0.tar.gz

---
+++ packages/python-language-server/python-language-server-0.10.0.tar.gz/python-language-server-0.10.0/pyls/plugins/pycodestyle_lint.py
@@ -0,0 +1,79 @@
+# Copyright 2017 Palantir Technologies, Inc.
+import logging
+import pycodestyle
+from pyls import config as pyls_config, hookimpl, lsp, _utils
+
+log = logging.getLogger(__name__)
+
+# Potential config files in reverse order of preference
+CONFIG_FILES = ['tox.ini', 'pep8.cfg', 'setup.cfg', 'pycodestyle.cfg']
+
+
+@hookimpl
+def pyls_lint(config, document):
+    # Start with empty config, and update with each level of settings
+    conf = {}
+
+    def _config_from_files(config_files):
+        pycodestyle_conf = pyls_config.build_config('pycodestyle', config_files)
+        pep8_conf = pyls_config.build_config('pep8', config_files)
+        conf_to_use = pycodestyle_conf or pep8_conf or {}
+        return {k.replace("-", "_"): v for k, v in conf_to_use.items()}
+
+    # First, read the user configuration (in user's home directory)
+    if pycodestyle.USER_CONFIG:
+        conf.update(_config_from_files([pycodestyle.USER_CONFIG]))
+
+    # Then, read the PYLS configuration
+    ide_conf = config.plugin_settings('pycodestyle')
+    conf.update({_utils.camel_to_underscore(k): _utils.list_to_string(v) for k, v in ide_conf.items()})
+
+    # Finally, read the project configuration
+    conf.update(_config_from_files(config.find_parents(document.path, CONFIG_FILES)))
+
+    # Grab the pycodestyle parser and set the defaults based on the config we found
+    parser = pycodestyle.get_parser()
+    parser.set_defaults(**conf)
+
+    opts, _args = parser.parse_args([])
+    log.debug("Got pycodestyle config: %s", opts)
+    styleguide = pycodestyle.StyleGuide(vars(opts))
+
+    c = pycodestyle.Checker(
+        filename=document.uri, lines=document.lines, options=styleguide.options,
+        report=PyCodeStyleDiagnosticReport(styleguide.options)
+    )
+    c.check_all()
+    diagnostics = c.report.diagnostics
+    return diagnostics
+
+
+class PyCodeStyleDiagnosticReport(pycodestyle.BaseReport):
+
+    def __init__(self, options=None, **kwargs):
+        self.diagnostics = []
+        super(PyCodeStyleDiagnosticReport, self).__init__(options=options, **kwargs)
+
+    def error(self, lineno, offset, text, check):
+        # PyCodeStyle will sometimes give you an error the line after the end of the file
+        #   e.g. no newline at end of file
+        # In that case, the end offset should just be some number ~100
+        # (because why not? There's nothing to underline anyways)
+        log.info("Got pycodestyle error at %d:%d %s", lineno, offset, text)
+        range = {
+            'start': {'line': lineno - 1, 'character': offset},
+            'end': {
+                # FIXME: It's a little naiive to mark until the end of the line, can we not easily do better?
+                'line': lineno - 1, 'character': 100 if lineno > len(self.lines) else len(self.lines[lineno - 1])
+            },
+        }
+        code, _message = text.split(" ", 1)
+
+        self.diagnostics.append({
+            'source': 'pycodestyle',
+            'range': range,
+            'message': text,
+            'code': code,
+            # Are style errors really ever errors?
+            'severity': lsp.DiagnosticSeverity.Warning
+        })
---
+++ packages/python-language-server/python-language-server-0.10.0.tar.gz/python-language-server-0.10.0/test/plugins/test_lint.py
@@ -0,0 +1,135 @@
+# Copyright 2017 Palantir Technologies, Inc.
+import os
+from pyls import lsp, uris
+from pyls.config import Config
+from pyls.workspace import Document
+from pyls.plugins import mccabe_lint, pycodestyle_lint, pydocstyle_lint, pyflakes_lint
+
+DOC_URI = uris.from_fs_path(__file__)
+DOC = """import sys
+
+def hello():
+\tpass
+
+import json
+"""
+
+DOC_SYNTAX_ERR = """def hello()
+    pass
+"""
+
+
+def test_mccabe(config):
+    old_settings = config.settings
+    try:
+        config.update({'plugins': {'mccabe': {'threshold': 1}}})
+        doc = Document(DOC_URI, DOC)
+        diags = mccabe_lint.pyls_lint(config, doc)
+
+        assert all([d['source'] == 'mccabe' for d in diags])
+
+        # One we're expecting is:
+        msg = 'Cyclomatic complexity too high: 1 (threshold 1)'
+        mod_import = [d for d in diags if d['message'] == msg][0]
+
+        assert mod_import['severity'] == lsp.DiagnosticSeverity.Warning
+        assert mod_import['range']['start'] == {'line': 3, 'character': 0}
+        assert mod_import['range']['end'] == {'line': 3, 'character': 6}
+    finally:
+        config._settings = old_settings
+
+
+def test_pycodestyle(config):
+    doc = Document(DOC_URI, DOC)
+    diags = pycodestyle_lint.pyls_lint(config, doc)
+
+    assert all([d['source'] == 'pycodestyle' for d in diags])
+
+    # One we're expecting is:
+    msg = 'W191 indentation contains tabs'
+    mod_import = [d for d in diags if d['message'] == msg][0]
+
+    assert mod_import['code'] == 'W191'
+    assert mod_import['severity'] == lsp.DiagnosticSeverity.Warning
+    assert mod_import['range']['start'] == {'line': 3, 'character': 0}
+    assert mod_import['range']['end'] == {'line': 3, 'character': 6}
+
+
+def test_pycodestyle_config(workspace):
+    """ Test that we load config files properly.
+
+    Config files are loaded in the following order:
+        tox.ini pep8.cfg setup.cfg pycodestyle.cfg
+
+    Each overriding the values in the last.
+
+    These files are first looked for in the current document's
+    directory and then each parent directory until any one is found
+    terminating at the workspace root.
+
+    If any section called 'pycodestyle' exists that will be solely used
+    and any config in a 'pep8' section will be ignored
+    """
+    doc_uri = uris.from_fs_path(os.path.join(workspace.root_path, 'test.py'))
+    workspace.put_document(doc_uri, DOC)
+    doc = workspace.get_document(doc_uri)
+    config = Config(workspace.root_uri, {})
+
+    # Make sure we get a warning for 'indentation contains tabs'
+    diags = pycodestyle_lint.pyls_lint(config, doc)
+    assert [d for d in diags if d['code'] == 'W191']
+
+    content = {
+        'setup.cfg': ('[pycodestyle]\nignore = W191', True),
+        'pep8.cfg': ('[pep8]\nignore = W191', True),
+        'tox.ini': ('', False)
+    }
+
+    for conf_file, (content, working) in list(content.items()):
+        # Now we'll add config file to ignore it
+        with open(os.path.join(workspace.root_path, conf_file), 'w+') as f:
+            f.write(content)
+
+        # And make sure we don't get any warnings
+        diags = pycodestyle_lint.pyls_lint(config, doc)
+        assert len([d for d in diags if d['code'] == 'W191']) == 0 if working else 1
+
+        os.unlink(os.path.join(workspace.root_path, conf_file))
+
+    # Make sure we can ignore via the PYLS config as well
+    config.update({'plugins': {'pycodestyle': {'ignore': ['W191']}}})
+    # And make sure we don't get any warnings
+    diags = pycodestyle_lint.pyls_lint(config, doc)
+    assert not [d for d in diags if d['code'] == 'W191']
+
+
+def test_pydocstyle():
+    doc = Document(DOC_URI, DOC)
+    diags = pydocstyle_lint.pyls_lint(doc)
+
+    assert all([d['source'] == 'pydocstyle' for d in diags])
+
+    # One we're expecting is:
+    msg = 'D100: Missing docstring in public module'
+    unused_import = [d for d in diags if d['message'] == msg][0]
+
+    assert unused_import['range']['start'] == {'line': 0, 'character': 0}
+
+
+def test_pyflakes():
+    doc = Document(DOC_URI, DOC)
+    diags = pyflakes_lint.pyls_lint(doc)
+
+    # One we're expecting is:
+    msg = '\'sys\' imported but unused'
+    unused_import = [d for d in diags if d['message'] == msg][0]
+
+    assert unused_import['range']['start'] == {'line': 0, 'character': 0}
+
+
+def test_syntax_error_pyflakes():
+    doc = Document(DOC_URI, DOC_SYNTAX_ERR)
+    diag = pyflakes_lint.pyls_lint(doc)[0]
+
+    assert diag['message'] == 'invalid syntax'
+    assert diag['range']['start'] == {'line': 0, 'character': 12}
