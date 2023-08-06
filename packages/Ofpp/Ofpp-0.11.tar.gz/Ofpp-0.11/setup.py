d512a7a1c305c3ed3aeebea1539e27a9de39b2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:07 +0000

    Add package Ofpp/0.11/Ofpp-0.11.tar.gz

---
+++ packages/Ofpp/Ofpp-0.11.tar.gz/Ofpp-0.11/setup.py
@@ -0,0 +1,25 @@
+
+
+from setuptools import setup
+
+package_name = 'Ofpp'
+
+packages = [
+    'numpy',
+    ]
+
+config = {
+    'author': 'XU Xianghua',
+    'author_email': 'dayigu@gmail.com',
+    'url': 'https://github.com/dayigu/ofpp',
+    'description': 'A simple Python code to parse OpenFOAM data to Numpy.array',
+    'license': 'MIT',
+    'version': '0.11',
+    'packages': [package_name],
+    'install_requires': packages,
+    'name': package_name,
+    'zip_safe': False
+}
+
+
+setup(**config)
