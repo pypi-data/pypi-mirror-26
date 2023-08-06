fa012a8df20677130849711e6fdbc14a2318fa66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:57 +0000

    Add package djdb/0.1.0/djdb-0.1.0.tar.gz

---
+++ packages/djdb/djdb-0.1.0.tar.gz/djdb-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='djdb',
+    version='0.1.0',
+    packages=['pydb'],
+    description='a python databases api',
+    url='https://github.com/HuangHongkai/pydb',
+    install_requires=["Django >= 1.1.1",],
+    author='dfsss',
+    author_email='2523272490@qq.com',
+)
