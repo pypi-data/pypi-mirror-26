3ecee8ed845555506f00e202e154071de56e31e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:16 +0000

    Add package oskui/0.13.5/oskui-0.13.5.tar.gz

---
+++ packages/oskui/oskui-0.13.5.tar.gz/oskui-0.13.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='oskui',
+      version='0.13.5',
+      description='Set of command line user interface tools',
+      author='Oskari Vinko',
+      author_email='oskari.vinko@gmail.com',
+      license='MIT',
+      packages=['oskui'],
+      url="https://github.com/Wing0/oskui",
+      download_utl="https://github.com/Wing0/oskui/archive/0.13.5.tar.gz",
+      keywords=['user', 'interface', 'ui', 'cmd'],
+      zip_safe=False)
