a16f2341b3033a34fcdd32aa80f745288678f9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:01 +0000

    Add package info-generators/0.0.7/info-generators-0.0.7.tar.gz

---
+++ packages/info-generators/info-generators-0.0.7.tar.gz/info-generators-0.0.7/info_generators/birthday_generator.py
@@ -0,0 +1,15 @@
+import random
+
+from .gabc_generator import Generator
+
+
+class BirthdayGenerator(Generator):
+    @staticmethod
+    def get():
+        consts_months = ('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12')
+        consts_days = ('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12',
+                       '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28',
+                       '29', '30')
+        return '19{}{}{}'.format(random.randrange(80, 95),
+                                 random.choice(consts_months),
+                                 random.choice(consts_days))
