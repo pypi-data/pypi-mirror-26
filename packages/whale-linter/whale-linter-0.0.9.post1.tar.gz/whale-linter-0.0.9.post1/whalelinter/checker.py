a663b387bc4a6fc65da11af5b0bee010ebe35d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:07 +0000

    Add package whale-linter/0.0.9.post1/whale-linter-0.0.9.post1.tar.gz

---
+++ packages/whale-linter/whale-linter-0.0.9.post1.tar.gz/whale-linter-0.0.9.post1/whalelinter/checker.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+from whalelinter.app import App
+from whalelinter.dispatcher import Dispatcher
+from whalelinter.token import Maintainer
+from whalelinter.commands.apt import Apt
+
+import whalelinter.commands.common
+
+
+class Checker(object):
+    def __init__(self, lst):
+        self.lines = lst
+        self.line_number = 0
+        self.consecutive_run = 0
+
+    def check(self):
+        dispatcher = Dispatcher()
+
+        for line in self.lines:
+            dispatcher.react(line)
+            self.must_be_present(line.instruction)
+            self.must_be_unique(line.instruction)
+            self.should_be_present(line.instruction)
+
+        if Apt._has_been_used:
+            App._collecter.throw(3000, keys={'line': Apt._has_been_used})
+
+        for mandatory_token in App._mandatory_tokens:
+            App._collecter.throw(1000, keys={'instruction': mandatory_token})
+
+        for recommended_token in App._recommended_tokens:
+            App._collecter.throw(3001, keys={'instruction': recommended_token})
+
+    def must_be_present(self, instruction):
+        if instruction in App._mandatory_tokens:
+            App._mandatory_tokens.remove(instruction)
+
+    def should_be_present(self, instruction):
+        if instruction in App._recommended_tokens:
+            App._recommended_tokens.remove(instruction)
+
+    def must_be_unique(self, instruction):
+        if instruction in App._unique_tokens:
+            App._unique_tokens[instruction] += 1
+            if App._unique_tokens[instruction] > 1:
+                App._collecter.throw(
+                    1001,
+                    line=self.line_number,
+                    keys={
+                        'instruction': instruction
+                    })
---
+++ packages/whale-linter/whale-linter-0.0.9.post1.tar.gz/whale-linter-0.0.9.post1/whalelinter/commands/common.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import re
+
+from whalelinter.app import App
+from whalelinter.dispatcher import Dispatcher
+from whalelinter.commands.command import ShellCommand
+from whalelinter.commands.apt import Apt
+
+
+@Dispatcher.register(token='run', command='cd')
+class Cd(ShellCommand):
+    def __init__(self, **kwargs):
+        App._collecter.throw(2002, lineno=kwargs.get('lineno'))
+
+
+@Dispatcher.register(token='run', command='rm')
+class Rm(ShellCommand):
+    def __init__(self, **kwargs):
+        rf_flags_regex = re.compile("(-.*[rRf].+-?[rRf]|-[rR]f|-f[rR])")
+        rf_flags = True if [
+            i for i in kwargs.get('args') if rf_flags_regex.search(i)
+        ] else False
+        cache_path_regex = re.compile("/var/lib/apt/lists(\/\*?)?")
+        cache_path = True if [
+            i for i in kwargs.get('args') if cache_path_regex.search(i)
+        ] else False
+
+        if rf_flags and cache_path:
+            if (int(Apt._has_been_used) < int(kwargs.get('lineno'))):
+                Apt._has_been_used = 0
