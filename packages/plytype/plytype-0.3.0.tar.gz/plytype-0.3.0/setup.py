1d4846461f03023e01afb42b21d7504aef87c749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:45 +0000

    Add package plytype/0.3.0/plytype-0.3.0.tar.gz

---
+++ packages/plytype/plytype-0.3.0.tar.gz/plytype-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+setup(
+    name = 'plytype',
+    version = '0.3.0',
+    keywords = ('python', 'lexer', 'parser'),
+    description = 'PLY with get_type and set_type',
+    license = 'MIT License',
+
+    author = 'Jian Wang',
+    author_email = 'yt4766269@126.com',
+
+    packages = find_packages(),
+    platforms = 'linux',
+)
