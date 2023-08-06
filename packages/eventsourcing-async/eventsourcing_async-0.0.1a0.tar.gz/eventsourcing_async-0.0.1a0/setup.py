b7909cd18a37ac1c6e8943ba1609036d4410e5de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:17 +0000

    Add package eventsourcing-async/0.0.1a0/eventsourcing_async-0.0.1a0.tar.gz

---
+++ packages/eventsourcing-async/eventsourcing_async-0.0.1a0.tar.gz/eventsourcing_async-0.0.1a0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_packages, setup
+from eventsource import __version__
+
+long_description = """
+This package provides generic support for async event sourcing in Python.
+
+`Please raise issues on GitHub <https://github.com/laskoviymishka/cqrs-eventsource/issues>`_.
+"""
+
+packages = find_packages(
+    exclude=[
+    ]
+)
+
+setup(
+    name='eventsourcing_async',
+    version=__version__,
+    description='Event sourcing async in Python',
+    author='John Bywater',
+    author_email='andrei_tserakhau@epam.com',
+    url='https://github.com/laskoviymishka/cqrs-eventsource',
+    packages=packages,
+    install_requires=[
+        'six<=1.10.99999',
+        'python-dateutil',
+        'peewee',
+        'peewee_async',
+        'aiounittest', 'aiopg',
+    ],
+    zip_safe=False,
+    long_description=long_description,
+    keywords=['event sourcing', 'event store', 'async', 'domain driven design', 'ddd', 'cqrs', 'cqs'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
