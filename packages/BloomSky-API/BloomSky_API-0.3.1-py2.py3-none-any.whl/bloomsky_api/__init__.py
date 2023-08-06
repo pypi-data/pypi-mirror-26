caa2db4358eae7dcf8ca69f023d4d57f914e07b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:37 +0000

    Add package BloomSky-API/0.3.1/BloomSky_API-0.3.1-py2.py3-none-any.whl

---
+++ packages/BloomSky-API/BloomSky_API-0.3.1-py2.py3-none-any.whl/bloomsky_api/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from ._version import get_versions
+
+__author__ = 'Dave Forgac'
+__email__ = 'tylerdave@tylerdave.com'
+__version__ = get_versions()['version']
+del get_versions
+
+from .bloomsky_api import BloomSkyAPIResponse, BloomSkyAPIClient
+
+__all__ = [
+        'BloomSkyAPIClient',
+        'BloomSkyAPIResponse',
+        ]
