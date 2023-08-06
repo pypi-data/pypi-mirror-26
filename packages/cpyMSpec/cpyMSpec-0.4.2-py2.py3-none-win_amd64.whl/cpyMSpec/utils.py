b764590ecd18d01bbabde847d473f8a833ad9dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:05 +0000

    Add package cpyMSpec/0.4.2/cpyMSpec-0.4.2-py2.py3-none-win_amd64.whl

---
+++ packages/cpyMSpec/cpyMSpec-0.4.2-py2.py3-none-win_amd64.whl/cpyMSpec/utils.py
@@ -0,0 +1,20 @@
+import platform
+import os.path
+import cffi
+
+def full_filename(fn):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), fn))
+
+def shared_lib(name):
+    suffixes = {'Windows': 'dll', 'Linux': 'so', 'Darwin': 'dylib'}
+    return 'lib' + name + '.' + suffixes[platform.system()]
+
+def init_ffi():
+    ffi = cffi.FFI()
+    ffi.cdef(open(full_filename("ims.h")).read())
+    return ffi
+
+def load_shared_lib(ffi):
+    return ffi.dlopen(full_filename(shared_lib("ms_cffi")))
+
+VERSION = "0.4.2"
