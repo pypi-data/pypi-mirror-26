2d4490b0765481364ae718c54ea936a32392cc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:18 +0000

    Add package irs/6.7.5/irs-6.7.5.tar.gz

---
+++ packages/irs/irs-6.7.5.tar.gz/irs-6.7.5/irs/setup_binaries.py
@@ -0,0 +1,15 @@
+import ydl_binaries
+from shutil import copyfile
+import os
+import inspect
+import irs
+
+
+def setup():
+    ydl_binaries.download_ffmpeg("~/.irs/bin/")
+    ydl_binaries.update_ydl("~/.irs/bin/")
+
+    config_file = os.path.expanduser("~/.irs/config_.py")
+    if not os.path.isfile(config_file):
+        copyfile(os.path.dirname(inspect.getfile(irs)) + "/config_preset.py",
+                 config_file)
