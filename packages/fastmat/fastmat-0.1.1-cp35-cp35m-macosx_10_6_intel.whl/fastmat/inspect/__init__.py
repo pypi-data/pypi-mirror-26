4fdd9a3f609cd06e6c8b41f4dc44ed8ac50d7132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:21 +0000

    Add package fastmat/0.1.1/fastmat-0.1.1-cp35-cp35m-macosx_10_6_intel.whl

---
+++ packages/fastmat/fastmat-0.1.1-cp35-cp35m-macosx_10_6_intel.whl/fastmat/inspect/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+'''
+  fastmat/inspect/__init__.py
+ -------------------------------------------------- part of the fastmat demos
+
+
+  Author      : wcw
+  Introduced  : 2017-07-19
+ ------------------------------------------------------------------------------
+
+   Copyright 2016 Sebastian Semper, Christoph Wagner
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ ------------------------------------------------------------------------------
+'''
+from .documentation import DOC, Documentation
+from .benchmark import BENCH, Benchmark
+from .test import TEST, Test
+from .common import arrTestDist, arrSparseTestDist, dynFormat
