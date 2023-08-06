756df8e8a144d355100673d6361ae1e827ec79a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:37 +0000

    Add package dask-ml/0.3.1/dask_ml-0.3.1-cp27-cp27m-win32.whl

---
+++ packages/dask-ml/dask_ml-0.3.1-cp27-cp27m-win32.whl/dask_ml/linear_model/__init__.py
@@ -0,0 +1,8 @@
+"""The ``dask_ml.linear_model`` module implements linear models for
+classification and regression.
+"""
+from .stochastic_gradient import PartialSGDClassifier, PartialSGDRegressor  # noqa
+from .perceptron import PartialPerceptron  # noqa
+from .passive_aggressive import PartialPassiveAggressiveClassifier, PartialPassiveAggressiveRegressor # noqa
+
+from dask_glm.estimators import LogisticRegression, LinearRegression, PoissonRegression  # noqa
