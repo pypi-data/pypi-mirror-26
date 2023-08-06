23a6e7e353a7ea9e589e84d3daa47266db992db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:24 +0000

    Add package progressivecsv/0.0.0.5/progressivecsv-0.0.0.5.tar.gz

---
+++ packages/progressivecsv/progressivecsv-0.0.0.5.tar.gz/progressivecsv-0.0.0.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup( name = "progressivecsv",
+       url="https://bitbucket.org/mjr129/progressive_csv",
+       version = "0.0.0.5",
+       description = "Package for writing a CSV with dynamic headers. CSV is completed at end.",
+       author = "Martin Rusilowicz",
+       license = "GNU AGPLv3",
+       packages = [ "progressivecsv" ],
+       )
