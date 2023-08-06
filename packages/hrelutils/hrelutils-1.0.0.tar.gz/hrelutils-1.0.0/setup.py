6a606280a83729dbef3707289b3c52eb37b921a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:10 +0000

    Add package hrelutils/1.0.0/hrelutils-1.0.0.tar.gz

---
+++ packages/hrelutils/hrelutils-1.0.0.tar.gz/hrelutils-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='hrelutils',
+    version='1.0.0',
+    description='Hrel utils',
+    url='https://gitlab.com/hrel3/hrelutils/',
+    author='Justin Barber',
+    author_email='justin@hrel.ca',
+    python_requires='>=3.6',
+)
