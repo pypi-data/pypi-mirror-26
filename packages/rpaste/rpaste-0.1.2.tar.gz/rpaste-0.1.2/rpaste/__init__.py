ccb3a4d49a215b20086de68dd3c51d409c6ac990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:14 +0000

    Add package rpaste/0.1.2/rpaste-0.1.2.tar.gz

---
+++ packages/rpaste/rpaste-0.1.2.tar.gz/rpaste-0.1.2/rpaste/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from .version import __version__, __VERSION__
+from .main import main
+from .rpaste import rpaste
---
+++ packages/rpaste/rpaste-0.1.2.tar.gz/rpaste-0.1.2/setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from rpaste.version import __version__
+
+setup(
+    name='rpaste',
+    packages=['rpaste'],
+    version=__version__,
+    description='Code Snippets Uploader',
+    author='Anup Pokhrel',
+    author_email='virtualanup@gmail.com',
+    install_requires=['pyperclip', 'requests'],
+    url='https://github.com/rpaste/rpaste',
+    keywords=['rpaste', 'paste', 'bin'],
+    classifiers=[],
+    entry_points={
+        "console_scripts": [
+            "rpaste=rpaste:main",
+        ]
+    },
+)
