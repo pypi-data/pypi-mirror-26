a76131dd622a023ae79117a3124ae4e2eaffa7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:03 +0000

    Add package nider/0.4.1/nider-0.4.1.tar.gz

---
+++ packages/nider/nider-0.4.1.tar.gz/nider-0.4.1/nider/colors/__init__.py
@@ -0,0 +1,10 @@
+from .colormap import BASIC_COLORS
+from .colormap import FLAT_UI_COLORS
+from .colormap import COLORS
+from .colormap import rgb
+
+from .utils import color_to_rgb
+from .utils import get_img_dominant_color
+from .utils import monochrome_color
+from .utils import generate_opposite_color
+from .utils import blend
