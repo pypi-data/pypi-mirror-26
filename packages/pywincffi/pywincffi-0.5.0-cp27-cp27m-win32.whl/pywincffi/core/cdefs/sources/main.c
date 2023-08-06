c77df2d0e12b5c50ec709709c976add2821ab03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:00 +0000

    Add package pywincffi/0.5.0/pywincffi-0.5.0-cp27-cp27m-win32.whl

---
+++ packages/pywincffi/pywincffi-0.5.0-cp27-cp27m-win32.whl/pywincffi/core/cdefs/sources/main.c
@@ -0,0 +1,51 @@
+#include <io.h>
+#include <winsock2.h>
+#include <winerror.h>
+#include <TlHelp32.h>
+#include <windows.h>
+
+// Extra constants which are not defined in all versions of the Windows
+// SDK.  If cffi fails to find the value, it ends up being picked up from
+// here.
+#if !defined(FILE_FLAG_SESSION_AWARE)
+    static const int FILE_FLAG_SESSION_AWARE = 0x00800000;
+#endif
+
+#if !defined(STARTF_UNTRUSTEDSOURCE)
+    static const int STARTF_UNTRUSTEDSOURCE = 0x00008000;
+#endif
+
+#if !defined(STARTF_PREVENTPINNING)
+    static const int STARTF_PREVENTPINNING = 0x00002000;
+#endif
+
+#if !defined(STARTF_TITLEISAPPID)
+    static const int STARTF_TITLEISAPPID = 0x00001000;
+#endif
+
+#if !defined(STARTF_TITLEISLINKNAME)
+    static const int STARTF_TITLEISLINKNAME = 0x00000800;
+#endif
+
+#if !defined(CREATE_PROTECTED_PROCESS)
+    static const int CREATE_PROTECTED_PROCESS 0x00040000;
+#endif
+
+#if !defined(EXTENDED_STARTUPINFO_PRESENT)
+    static const int EXTENDED_STARTUPINFO_PRESENT = 0x00080000;
+#endif
+
+#if !defined(INHERIT_PARENT_AFFINITY)
+    static const int INHERIT_PARENT_AFFINITY = 0x00010000;
+#endif
+
+HANDLE handle_from_fd(int fd) {
+    return (HANDLE)_get_osfhandle(fd);
+}
+
+// Checks to see if a given event is considered invalid.  We perform this
+// check in C because cffi itself has trouble creating a usable value for
+// WSA_INVALID_EVENT.
+BOOL wsa_invalid_event(WSAEVENT event) {
+    return event == WSA_INVALID_EVENT;
+}
