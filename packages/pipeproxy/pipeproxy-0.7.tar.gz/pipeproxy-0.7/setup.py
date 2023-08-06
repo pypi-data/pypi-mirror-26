105026eb4c25f07bc52a45218d81a6ba62d74b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:06 +0000

    Add package pipeproxy/0.7/pipeproxy-0.7.tar.gz

---
+++ packages/pipeproxy/pipeproxy-0.7.tar.gz/pipeproxy-0.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='pipeproxy',
+      version='0.7',
+      description='A sort-of-proxy solution for multiprocess communication using Pipe from mulitprocessing.',
+      url='https://github.com/dRoje/pipe-proxy.git',
+      author='Duje Roje',
+      author_email='rojeduje@gmail.com',
+      license='MIT',
+      packages=['pipeproxy',
+                'pipeproxy.lib',
+                'pipeproxy.lib.objectProxy',
+                'pipeproxy.lib.objectProxy.proxyMessenger',
+                'pipeproxy.lib.proxyListener',
+                'pipeproxy.lib.proxyMessages',
+
+                'test_pipeproxy',
+                'test_pipeproxy.test_lib',
+                'test_pipeproxy.test_lib.test_objectProxy',
+                'test_pipeproxy.test_lib.test_objectProxy.test_proxyMessenger',
+                'test_pipeproxy.test_lib.test_proxyListener',
+                'test_pipeproxy.test_lib.test_proxyMessages'],
+
+      # package_dir={'pipeproxy': 'lib'},
+      zip_safe=False)
