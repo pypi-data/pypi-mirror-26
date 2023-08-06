20a20a2f86f3c717ee61cd80271d3e0baef4ccac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:30 +0000

    Add package canceladebitos/1.1.3/canceladebitos-1.1.3.tar.gz

---
+++ packages/canceladebitos/canceladebitos-1.1.3.tar.gz/canceladebitos-1.1.3/canceladebitos/config.py
@@ -0,0 +1,7 @@
+# Dados para login no Siapa
+config_siapa = {'cpf': '',
+                'senha': ''}
+
+# Dados para login no banco de dados do SPUnet
+config_spunet = {'user': '',
+                 'password': ''}
