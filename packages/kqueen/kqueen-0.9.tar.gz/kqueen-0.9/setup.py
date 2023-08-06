0e712f08a3c3fe97e6e3041f2b667b6f7024e0f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:16 +0000

    Add package kqueen/0.9/kqueen-0.9.tar.gz

---
+++ packages/kqueen/kqueen-0.9.tar.gz/kqueen-0.9/setup.py
@@ -0,0 +1,45 @@
+"""Setup for kqueen package."""
+
+from setuptools import setup, find_packages
+
+version = '0.9'
+
+with open('README.rst') as f:
+    long_description = ''.join(f.readlines())
+
+setup(
+    name='kqueen',
+    version=version,
+    description='Kubernetes cluster orchestrator',
+    long_description=long_description,
+    author='Tomáš Kukrál',
+    author_email='tomas.kukral@6shore.net',
+    license='MIT',
+    url='https://github.com/Mirantis/kqueen/',
+    download_url='https://github.com/Mirantis/kqueen/archive/v{}.tar.gz'.format(version),
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'Flask==0.12.2',
+        'Flask-JWT==0.3.2',
+        'flask-swagger-ui',
+        'gunicorn',
+        'kubernetes',
+        'python-etcd',
+        'python-jenkins',
+        'pyyaml',
+        'requests',
+    ],
+    classifiers=[
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points={
+        'console_scripts': [
+            'kqueen = kqueen.server:run',
+        ],
+    },
+)
