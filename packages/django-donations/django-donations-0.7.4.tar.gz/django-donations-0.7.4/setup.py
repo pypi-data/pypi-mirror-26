0b74566958be697e4a52c18ab2aab6aa6383e7b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:46 +0000

    Add package django-donations/0.7.4/django-donations-0.7.4.tar.gz

---
+++ packages/django-donations/django-donations-0.7.4.tar.gz/django-donations-0.7.4/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import codecs
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = '0.7.4'
+
+readme = codecs.open('README.rst', 'r', 'utf-8').read()
+history = codecs.open('HISTORY.rst', 'r', 'utf-8').read().replace('.. :changelog:', '')
+
+install_requires = [
+    'django-money',
+    'djangorestframework<3.7',
+    'requests',
+    'py-moneyed',
+    'six',
+    'django-compat>=1.0.11',
+]
+
+setup(
+    name='django-donations',
+    version=version,
+    description="""Reusable django app to receive & track donations on charitable sites""",
+    long_description=readme + '\n\n' + history,
+    author='Andrew Miller',
+    author_email='dev@founders4schools.org.uk',
+    url='https://github.com/founders4schools/django-donations',
+    packages=[
+        'donations',
+    ],
+    include_package_data=True,
+    install_requires=install_requires,
+    license="BSD",
+    zip_safe=False,
+    keywords='django-donations',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
