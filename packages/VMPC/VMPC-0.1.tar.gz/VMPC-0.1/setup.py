852efb0cb9c42d3a0a34ace6360e4cabf3a08273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:53 +0000

    Add package VMPC/0.1/VMPC-0.1.tar.gz

---
+++ packages/VMPC/VMPC-0.1.tar.gz/VMPC-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'VMPC',
+  packages = ['VMPC'],
+  version = '0.1',
+  description = 'Python implementation of the VMPC stream cipher',
+  author = 'Karl Zander',
+  author_email = 'pvial@kryptomagik.com',
+  url = 'https://github.com/pvial00/VMPC',
+  keywords = ['cryptography', 'encryption', 'security'],
+  classifiers = [],
+)
