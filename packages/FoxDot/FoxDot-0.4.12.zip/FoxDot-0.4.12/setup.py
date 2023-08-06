1d52b92b65deaedc1f824d0a3bd4168ec99e6e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:56 +0000

    Add package FoxDot/0.4.12/FoxDot-0.4.12.zip

---
+++ packages/FoxDot/FoxDot-0.4.12.zip/FoxDot-0.4.12/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup   
+
+setup(name='FoxDot',
+      version='0.4.12',
+      description='Live Coding with SuperCollider',
+      author='Ryan Kirkbride',
+      author_email='ryan@foxdot.org',
+      url='http://foxdot.org/',
+      packages=['FoxDot',
+                'FoxDot.lib',
+                'FoxDot.lib.Code',
+                'FoxDot.lib.Custom',
+                'FoxDot.lib.Workspace',
+                'FoxDot.lib.Patterns',
+                'FoxDot.lib.SCLang',
+                'FoxDot.lib.Settings',
+                'FoxDot.lib.Utils'],
+      package_data = {'FoxDot': ['snd/*/*/*.*',
+                                 'snd/_loop_/foxdot.wav',
+                                 'snd/_loop_/drums130.wav',
+                                 'snd/_loop_/dirty120.wav',
+                                 'osc/*.scd',
+                                 'osc/sceffects/*.scd',
+                                 'osc/scsyndef/*.scd',],
+                      'FoxDot.lib.Workspace': ['img/*', 'tmp/*'],
+                      'FoxDot.lib.Settings' : ['conf.txt']})
