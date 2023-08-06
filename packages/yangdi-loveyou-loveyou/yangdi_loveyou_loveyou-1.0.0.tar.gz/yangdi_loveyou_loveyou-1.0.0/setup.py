e80c10b0205fa07208084a597ae3430be90ced62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:21 +0000

    Add package yangdi-loveyou-loveyou/1.0.0/yangdi_loveyou_loveyou-1.0.0.tar.gz

---
+++ packages/yangdi-loveyou-loveyou/yangdi_loveyou_loveyou-1.0.0.tar.gz/yangdi_loveyou_loveyou-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name            = 'yangdi_loveyou_loveyou',
+    version         = '1.0.0',
+    py_modules      = ['yangdi_loveyou_loveyou'],
+    author          = 'hfpython',
+    author_email    = 'hfpython@headfirstlabs.com',
+    url             = 'http://www.headfirstlabs.com',
+    description     = 'A simple printer of nested lists',)
