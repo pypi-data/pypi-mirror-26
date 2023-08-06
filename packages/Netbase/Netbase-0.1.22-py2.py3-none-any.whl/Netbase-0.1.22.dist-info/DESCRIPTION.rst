2dcda682c4b139c39153bec532369650ccf651a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:53 +0000

    Add package Netbase/0.1.22/Netbase-0.1.22-py2.py3-none-any.whl

---
+++ packages/Netbase/Netbase-0.1.22-py2.py3-none-any.whl/Netbase-0.1.22.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+# netbase-python
+
+
---
+++ packages/Netbase/Netbase-0.1.22-py2.py3-none-any.whl/netbase/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# import netbase
+version = "0.1.16"
+# from netbase import world, the, net, Netbase
+# from netbase import Netbase, the
+# from netbase import Netbase
+from .netbase import Netbase, All
+
+# from netbase import net, Netbase
+# if py3:
+# 	from alle import All
+#
+world = net = Netbase()
+#
+#
+# class Alle(type):
+# 	def __getattr__(self, name):
+# 		return net._all(name, False, False)
+#
+#
+# class All:
+# 	__metaclass__ = Alle
+
+# print(All.USA)
+# print(the.USA)
+
+# net = netbase.net
+# the = netbase.world
+# cache = netbase.cache
+# import netbase
+# import netbase
