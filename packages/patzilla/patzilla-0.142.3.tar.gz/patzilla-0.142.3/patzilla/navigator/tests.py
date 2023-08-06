7465dd0993220ff62433cea3b5e74098a13678b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:30 +0000

    Add package patzilla/0.142.3/patzilla-0.142.3.tar.gz

---
+++ packages/patzilla/patzilla-0.142.3.tar.gz/patzilla-0.142.3/patzilla/navigator/tests.py
@@ -0,0 +1,15 @@
+import unittest
+from pyramid import testing
+
+class TestMyView(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_it(self):
+        from patzilla.navigator.views import navigator_standalone
+        request = testing.DummyRequest()
+        info = navigator_standalone(request)
+        self.assertEqual(info['project'], 'PatZilla')
