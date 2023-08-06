abba73abd903b91ecfc62abccc10f4b94d45f2bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:56 +0000

    Add package swarms-sdk/0.3/swarms_sdk-0.3.tar.gz

---
+++ packages/swarms-sdk/swarms_sdk-0.3.tar.gz/swarms_sdk-0.3/swarms/sdk/jobs.py
@@ -0,0 +1,17 @@
+from .utils.hal import get_link
+from . import CrudService
+
+
+class Jobs(CrudService):
+    path = "jobs"
+
+    # copy a job
+    def copy(self, job):
+        return self.client.post(get_link(job, "copy"))
+
+    # manage job's tasks
+    def get_tasks(self, job):
+        return self.client.get(get_link(job, "tasks"))
+
+    def update_tasks(self, job, tasks):
+        return self.client.put(get_link(job, "tasks"), self.get_ids(tasks))
