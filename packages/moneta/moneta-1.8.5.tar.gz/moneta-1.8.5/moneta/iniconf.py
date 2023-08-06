c3df2bf5640f3f568d9ca7a6778df5a5fe5f696e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:56 +0000

    Add package moneta/1.8.5/moneta-1.8.5.tar.gz

---
+++ packages/moneta/moneta-1.8.5.tar.gz/moneta-1.8.5/moneta/iniconf.py
@@ -0,0 +1,23 @@
+from djangofloor.conf.fields import bool_setting, CharConfigField, ConfigField
+from djangofloor.conf.mapping import BASE_MAPPING, REDIS_MAPPING, AUTH_MAPPING
+
+__author__ = 'flanker'
+
+
+def x_accel_converter(value):
+    if bool_setting(value):
+        return [('{MEDIA_ROOT}/', '/p/get/'),
+                ('{MEDIA_ROOT}/', '/a/get/'), ]
+    return []
+
+
+INI_MAPPING = BASE_MAPPING + AUTH_MAPPING + REDIS_MAPPING + [
+    ConfigField('global.use_apache', 'USE_X_SEND_FILE', from_str=bool_setting,
+                help_str='Apache only. Set it to "true" or "false"'),
+    ConfigField('global.use_nginx', 'X_ACCEL_REDIRECT', from_str=x_accel_converter,
+                help_str='Nginx only. Set it to "true" or "false"', to_str=lambda x: str(bool(x))),
+    CharConfigField('gnupg.home', 'GNUPG_HOME', help_str='Path of the GnuPG secret data'),
+    CharConfigField('gnupg.keyid', 'GNUPG_KEYID', help_str='ID of the GnuPG key'),
+    CharConfigField('gnupg.path', 'GNUPG_PATH', help_str='Path of the gpg binary'),
+
+]
---
+++ packages/moneta/moneta-1.8.5.tar.gz/moneta-1.8.5/setup.py
@@ -0,0 +1,38 @@
+"""Setup file for the Moneta project.
+"""
+import re
+import codecs
+import os.path
+from setuptools import setup, find_packages
+
+__author__ = 'flanker'
+
+
+with codecs.open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as fd:
+    long_description = fd.read()
+version = None
+for line in codecs.open(os.path.join('moneta', '__init__.py'), 'r', encoding='utf-8'):
+    matcher = re.match(r"""^__version__\s*=\s*['"](.*)['"]\s*$""", line)
+    version = version or matcher and matcher.group(1)
+
+entry_points = {'console_scripts': ['moneta-manage = djangofloor.scripts:django',
+                                    'moneta-aiohttp = djangofloor.scripts:aiohttp',
+                                    'moneta-gunicorn = djangofloor.scripts:gunicorn']}
+
+setup(
+    name='moneta',
+    version=version,
+    description='Emulator for different kinds (Debian/Yum/Pypi/Maven/…) of package repositories on the same server.',
+    long_description=long_description,
+    author='flanker',
+    author_email='flanker@19pouces.net',
+    license='CeCILL-B',
+    url='https://moneta.readthedocs.io/',
+    entry_points=entry_points,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools>=1.0', 'djangofloor>=1.0.25', 'python-gnupg', 'rubymarshal', 'pyyaml', 'gunicorn'],
+    setup_requires=[],
+    classifiers=[],
+)
