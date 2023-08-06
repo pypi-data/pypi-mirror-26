8141f541a789db3b8f1c191cfd06a8776ace9e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:30 +0000

    Add package vmo/0.3.3b/vmo-0.3.3b.tar.gz

---
+++ packages/vmo/vmo-0.3.3b.tar.gz/vmo-0.3.3b/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name="vmo",
+      packages=['vmo', 'vmo.VMO', 'vmo.analysis', 'vmo.realtime', 'vmo.VMO.utility', 'vmo.VMO.utility.distances'],
+      version="0.3.3b",
+      description="vmo - Variable Markov Oracle in Python",
+      author="Cheng-i Wang",
+      author_email='chw160@ucsd.edu',
+      url='https://github.com/wangsix/vmo',
+      long_description='vmo is a Python library for time series and symbolic sequence analysis/synthesis in the family '
+                       'of software built around the Factor Oracle and Variable Markov Oracle algorithms. One of the '
+                       'main innovations in vmo is using functions related to Information Dynamics to determine oracle '
+                       'structure and query-matching algorithms.',
+      license='GNU GPL 3.0'
+      )
