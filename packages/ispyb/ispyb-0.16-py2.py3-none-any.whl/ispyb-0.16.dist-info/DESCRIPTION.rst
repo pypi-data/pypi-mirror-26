d2ca4e2621021a7a77bf7934f12c1e5e8259b9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:34 +0000

    Add package ispyb/0.16/ispyb-0.16-py2.py3-none-any.whl

---
+++ packages/ispyb/ispyb-0.16-py2.py3-none-any.whl/ispyb-0.16.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This package provides a python interface to ISPyB. It can run on top of the official ISPyB webservices API or access the ISPyB database directly.
+
+
---
+++ packages/ispyb/ispyb-0.16-py2.py3-none-any.whl/ispyb/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division, print_function
+
+__version__ = "0.16"
+
+import ispyb.legacy.common.driver
+
+legacy_get_driver = ispyb.legacy.common.driver.get_driver
+legacy_Backend = ispyb.legacy.common.driver.Backend
