a0e6330b10451ff7094c28f6313ba1c051edf855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:03 +0000

    Add package taskmap/0.0.3/taskmap-0.0.3.tar.gz

---
+++ packages/taskmap/taskmap-0.0.3.tar.gz/taskmap-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='taskmap',
+    version='0.0.3',
+    description='Dependency graph with parallel asyncronous task runner',
+    url='https://github.com/n-s-f/taskmap',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['multiprocess', 'multiprocessing-logging'],
+)
