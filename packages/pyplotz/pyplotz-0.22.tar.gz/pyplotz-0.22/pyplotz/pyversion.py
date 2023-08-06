49b7fd7e6feb05e74ff5e76f962f3c4b81d4ad02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:58 +0000

    Add package pyplotz/0.22/pyplotz-0.22.tar.gz

---
+++ packages/pyplotz/pyplotz-0.22.tar.gz/pyplotz-0.22/pyplotz/pyversion.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 17 17:09:19 2017
+
+@author: Ray
+"""
+from __future__ import print_function
+
+import sys
+
+major_version = sys.version_info[0]
+
+def cprint(content):
+    print(content)
