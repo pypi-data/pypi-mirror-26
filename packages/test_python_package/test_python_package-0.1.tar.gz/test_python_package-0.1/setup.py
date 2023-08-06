54d5300038f344d3a12a72ae0a2d8477083d288b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:14 +0000

    Add package test_python_package/0.1/test_python_package-0.1.tar.gz

---
+++ packages/test_python_package/test_python_package-0.1.tar.gz/test_python_package-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="test_python_package",
+      version='0.1',
+      description='works',
+      author='me',
+      license='MIT',
+      packages=['test_python_package'],
+      zip_safe=False)
+      
+
+
+
+
