d22ecb60065e62117b4fab8a10975299585dd0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:37 +0000

    Add package pybel/0.9.7/PyBEL-0.9.7.tar.gz

---
+++ packages/pybel/PyBEL-0.9.7.tar.gz/PyBEL-0.9.7/src/pybel/dsl/utils.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from ..constants import EFFECT, FROM_LOC, IDENTIFIER, MODIFIER, NAME, NAMESPACE, TO_LOC, TRANSLOCATION
+
+
+def entity(namespace, name=None, identifier=None):
+    """Creates a dictionary representing a reference to an entity
+
+    :param str namespace: The namespace to which the entity belongs
+    :param str name: The name of the entity
+    :param str identifier: The identifier of the entity in the namespace
+    :rtype: dict
+    """
+    if name is None and identifier is None:
+        raise ValueError('cannot create an entity without neither a name nor identifier')
+
+    rv = {
+        NAMESPACE: namespace,
+    }
+
+    if name is not None:
+        rv[NAME] = name
+
+    if identifier is not None:
+        rv[IDENTIFIER] = identifier
+
+    return rv
+
+
+def make_translocation_modifier_dict(from_loc, to_loc):
+    """Makes a translocation dict
+
+    :param dict from_loc:
+    :param dict to_loc:
+    :rtype: dict
+    """
+    return {
+        MODIFIER: TRANSLOCATION,
+        EFFECT: {
+            FROM_LOC: from_loc,
+            TO_LOC: to_loc
+        }
+    }
---
+++ packages/pybel/PyBEL-0.9.7.tar.gz/PyBEL-0.9.7/src/pybel/exceptions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""This module contains base exceptions that are shared through the package"""
+
+
+class PyBelWarning(Exception):
+    """The base class for warnings during compilation from which PyBEL can recover"""
