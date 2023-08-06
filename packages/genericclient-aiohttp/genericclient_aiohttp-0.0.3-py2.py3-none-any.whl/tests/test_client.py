e6fc600e5cee6326ddbfec55c80362b82b640b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:38 +0000

    Add package genericclient-aiohttp/0.0.3/genericclient_aiohttp-0.0.3-py2.py3-none-any.whl

---
+++ packages/genericclient-aiohttp/genericclient_aiohttp-0.0.3-py2.py3-none-any.whl/tests/test_client.py
@@ -0,0 +1,22 @@
+from aiohttp.test_utils import AioHTTPTestCase
+from aiohttp import web
+
+from genericclient_aiohttp import GenericClient
+
+
+class RequestClientTestCase(AioHTTPTestCase):
+    async def get_application(self):
+        return web.Application()
+
+    def test_host(self):
+        client = GenericClient(url='http://dummy.org')
+        self.assertEqual(client.host, 'dummy.org')
+
+        client = GenericClient(url='http://dummy.org:8000')
+        self.assertEqual(client.host, 'dummy.org:8000')
+
+        client = GenericClient(url='http://dummy.org:8000/api')
+        self.assertEqual(client.host, 'dummy.org:8000')
+
+        client = GenericClient(url='http://dummy.org/api')
+        self.assertEqual(client.host, 'dummy.org')
