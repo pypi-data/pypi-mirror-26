df868fc8eaeef48f950af058299c5f93b89cb9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:43 +0000

    Add package pvt/0.2/pvt-0.2.tar.gz

---
+++ packages/pvt/pvt-0.2.tar.gz/pvt-0.2/README.md
@@ -0,0 +1,29 @@
+# pvt by Richard Cook
+
+[![View on PyPI](https://img.shields.io/pypi/v/pvt.svg)](https://pypi.python.org/pypi/pvt)
+[![Licence](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/rcook/pvt/master/LICENSE)
+
+Python virtual environment tool
+
+## Clone repository
+
+```
+git clone https://github.com/rcook/pvt.git
+```
+
+## Developer notes
+
+Various package properties are defined in `pvt/__init__py`:
+
+* `__project_name__`
+* `__version__`
+* `__description__`
+
+When publishing a new build of the package, ensure that `__version__` is incremented as appropriate.
+
+## Licence
+
+Released under [MIT License][licence]
+
+[licence]: LICENSE
+[pypi]: https://pypi.python.org/pypi
---
+++ packages/pvt/pvt-0.2.tar.gz/pvt-0.2/pvt/project.py
@@ -0,0 +1,76 @@
+##################################################
+# Copyright (C) 2017, All rights reserved.
+##################################################
+
+import hashlib
+import os
+import shutil
+import virtualenv
+
+from pyprelude.file_system import make_path
+from pyprelude.process import execute
+from pyprelude.temp_util import temp_cwd
+from pyprelude.util import unpack_args
+from pysimplevcs.git import Git
+
+from pvt.exceptions import Informational
+
+class Project(object):
+    @staticmethod
+    def find(config, search_dir=None):
+        git = Git(search_dir)
+
+        setup_py_path = make_path(git.repo_dir, "setup.py")
+        if not os.path.isfile(setup_py_path):
+            raise Informational("Could not determine project directory from search location {}".format(search_dir))
+
+        project_dir = os.path.dirname(setup_py_path)
+        project_id = hashlib.md5(project_dir).hexdigest()
+        env_dir = make_path(config.dir, "envs", project_id)
+        return Project(project_dir, env_dir)
+
+    def __init__(self, project_dir, env_dir):
+        self._project_dir = project_dir
+        self._env_dir = env_dir
+
+        # TODO: Implement for non-Windows platforms!
+        self._bin_dir = make_path(self._env_dir, "Scripts")
+
+    @property
+    def project_dir(self): return self._project_dir
+
+    @property
+    def env_dir(self): return self._env_dir
+
+    @property
+    def bin_dir(self): return self._bin_dir
+
+    def initialize(self, force=False):
+        if os.path.isdir(self._env_dir):
+            if force:
+                shutil.rmtree(self._env_dir)
+            else:
+                raise Informational("Virtual environment directory {} already exists: use \"--force\" to overwrite".format(self._env_dir))
+
+        virtualenv.create_environment(
+            self._env_dir,
+            search_dirs=virtualenv.file_search_dirs(),
+            download=True)
+
+    def uninitialize(self):
+        if os.path.isdir(self._env_dir):
+            shutil.rmtree(self._env_dir)
+
+    def execute_script(self, script_name, args):
+        self._check_env_dir()
+        script_path = make_path(self._bin_dir, script_name)
+        os.system(" ".join([script_path] + args))
+
+    def execute_setup_actions(self, actions):
+        self._check_env_dir()
+        with temp_cwd(self._project_dir):
+            self.execute_script("python", ["setup.py"] + actions)
+
+    def _check_env_dir(self):
+        if not os.path.isdir(self._env_dir):
+            raise Informational("Virtual environment directory does not exist for project {}: create with \"init\" command".format(self._project_dir))
