eff7ec7e486bfc37bf4dccbccb6abf435a1a6de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:32 +0000

    Add package markus/1.1/markus-1.1-py2.py3-none-any.whl

---
+++ packages/markus/markus-1.1-py2.py3-none-any.whl/markus/__init__.py
@@ -0,0 +1,22 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from markus.main import configure, get_metrics  # noqa
+
+INCR = 'incr'
+GAUGE = 'gauge'
+TIMING = 'timing'
+HISTOGRAM = 'histogram'
+
+__all__ = [
+    'configure', 'get_metrics', 'INCR', 'GAUGE', 'TIMING', 'HISTOGRAM'
+]
+
+__author__ = 'Will Kahn-Greene'
+__email__ = 'willkg@mozilla.com'
+
+# yyyymmdd
+__releasedate__ = '20171113'
+# x.y or x.y.dev0
+__version__ = '1.1'
