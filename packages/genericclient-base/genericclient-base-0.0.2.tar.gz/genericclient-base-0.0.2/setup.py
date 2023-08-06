797516df4b0e84c2bcc3373822396807b79a1f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:05 +0000

    Add package genericclient-base/0.0.2/genericclient-base-0.0.2.tar.gz

---
+++ packages/genericclient-base/genericclient-base-0.0.2.tar.gz/genericclient-base-0.0.2/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    readme = f.read()
+
+setup(
+    name='genericclient-base',
+    version=VERSION,
+    description='',
+    long_description=readme,
+    author='Flavio Curella',
+    author_email='flavio.curella@gmail.com',
+    url='https://github.com/genericclient/genericclient-base',
+    license='MIT License',
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires=[
+    ],
+    test_suite='tests',
+    tests_require=[
+        "coveralls",
+        "mock",
+    ]
+)
