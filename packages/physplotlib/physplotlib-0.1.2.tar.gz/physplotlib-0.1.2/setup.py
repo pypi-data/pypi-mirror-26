41b4e04cc15ea486ebdf96e86e8e1720ddbf3b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:15 +0000

    Add package physplotlib/0.1.2/physplotlib-0.1.2.tar.gz

---
+++ packages/physplotlib/physplotlib-0.1.2.tar.gz/physplotlib-0.1.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+"""physplotlib: Library for plotting data, in particular from log files
+
+Usefull when handling with output files from numerical
+simulations in a quick way. Loads data from text files, makes treatments
+and plots the resulted data in a very simple manner.
+
+"""
+
+from setuptools import setup
+
+
+setup(name='physplotlib',
+      version='0.1.2',
+      description='Library for plotting data from log files',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Scientific/Engineering :: Physics',
+      ],
+      url='https://github.com/danieljosesilva/physplotlib',
+      author='Daniel Silva',
+      author_email='djsilva99@gmail.com',
+      license='MIT',
+      packages=['physplotlib'],
+      install_requires=[
+          'matplotlib',
+      ],
+      include_package_data=True,
+      zip_safe=False)
