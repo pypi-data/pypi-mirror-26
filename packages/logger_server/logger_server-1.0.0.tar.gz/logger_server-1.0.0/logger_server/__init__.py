ae864c7be76216ebc8110cfb3a8629a8732dfde4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:28 +0000

    Add package logger_server/1.0.0/logger_server-1.0.0.tar.gz

---
+++ packages/logger_server/logger_server-1.0.0.tar.gz/logger_server-1.0.0/logger_server/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#coding=utf-8
+"""
+@Project: LoggerServer
+@Filename: __init__.py
+@Author: Kehr <kehr.china@gmail.com>
+@Created Date:   2017-11-15T19:16:34+08:00
+@Last modified time: 2017-11-15T22:33:54+08:00
+@License: Apache License <http://www.apache.org/licenses/>
+"""
+from server import AttrDict
+from server import LoggerFormatter
+from server import LoggerStreamHandler
+from server import LoggerServer
+from server import version
