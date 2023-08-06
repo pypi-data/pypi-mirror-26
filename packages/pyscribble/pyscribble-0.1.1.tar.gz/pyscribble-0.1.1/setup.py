eb2d273b2c127126698c8d29927467291c972676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:43 +0000

    Add package pyscribble/0.1.1/pyscribble-0.1.1.tar.gz

---
+++ packages/pyscribble/pyscribble-0.1.1.tar.gz/pyscribble-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pyscribble',
+    version='0.1.1',
+    description='A scribbletune-like library for Python3',
+    url='https://github.com/blakeanedved/pyscribble',
+    author='Blake Nedved',
+    author_email='blakeanedved@gmail.com',
+    packages=['pyscribble'],
+    install_requires=[
+        'midiutil'
+    ],
+    zip_safe=False
+)
