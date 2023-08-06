80e6f6aa4fc6638ac1e83343094c33273b325b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:11 +0000

    Add package pg_requests/0.1.0/pg_requests-0.1.0.tar.gz

---
+++ packages/pg_requests/pg_requests-0.1.0.tar.gz/pg_requests-0.1.0/pg_requests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from pg_requests.query import QueryFacade
+
+
+query_facade = QueryFacade()
---
+++ packages/pg_requests/pg_requests-0.1.0.tar.gz/pg_requests-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pg_requests',
+    version='0.1.0',
+    packages=['pg_requests'],
+    url='https://github.com/prawn-cake/pg_requests',
+    license='MIT',
+    author='Maksim Ekimovskii',
+    author_email='ekimovsky.maksim@gmail.com',
+    description='PostgreSQL-specific query builder library'
+)
