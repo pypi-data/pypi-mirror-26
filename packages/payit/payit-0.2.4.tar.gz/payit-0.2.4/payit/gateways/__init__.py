03827a1769d331e8601521446165d316a10b2c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:25 +0000

    Add package payit/0.2.4/payit-0.2.4.tar.gz

---
+++ packages/payit/payit-0.2.4.tar.gz/payit-0.2.4/payit/gateways/__init__.py
@@ -0,0 +1,11 @@
+from .mellat import MellatGateway
+from .payir import PayIrGateway
+from .zarinpal import ZarinpalGateway
+from .irankish import IrankishGateway
+
+__all__ = (
+    MellatGateway,
+    PayIrGateway,
+    ZarinpalGateway,
+    IrankishGateway
+)
