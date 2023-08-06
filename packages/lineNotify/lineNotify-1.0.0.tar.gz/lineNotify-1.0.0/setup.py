ea3bdd89889265288c065fd4c3d0fa68b34c0176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:49 +0000

    Add package lineNotify/1.0.0/lineNotify-1.0.0.tar.gz

---
+++ packages/lineNotify/lineNotify-1.0.0.tar.gz/lineNotify-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup (
+    name            = "lineNotify", 
+    version         = "1.0.0",
+    py_modules      = ["lineNotify"],
+    author          = "rocky.wang",
+    author_email    = "rockywang101@gmail.com",
+    url             = "https://rockywang101.github.io",
+    description     = "A simple Line Notify program"
+)
+
+
+
