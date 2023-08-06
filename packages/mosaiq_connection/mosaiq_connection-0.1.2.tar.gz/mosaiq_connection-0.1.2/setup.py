3741fd4571908432a4eb9fd88e3b5822fe65dd14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:44 +0000

    Add package mosaiq_connection/0.1.2/mosaiq_connection-0.1.2.tar.gz

---
+++ packages/mosaiq_connection/mosaiq_connection-0.1.2.tar.gz/mosaiq_connection-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="mosaiq_connection",
+    version="0.1.2",
+    author="Simon Biggs",
+    author_email="mail@simonbiggs.net",
+    description="A toolbox for interacting with Mosaiq SQL.",
+    packages=[
+        "mosaiq_connection"
+    ],
+    license='AGPL3+',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'pymssql',
+        'keyring',
+        'IPython'
+    ]
+)
