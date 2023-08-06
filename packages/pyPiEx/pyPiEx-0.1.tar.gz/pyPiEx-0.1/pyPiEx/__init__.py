7ba36b940f79e326ec5c44919fec60a72cfe19e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:43 +0000

    Add package pyPiEx/0.1/pyPiEx-0.1.tar.gz

---
+++ packages/pyPiEx/pyPiEx-0.1.tar.gz/pyPiEx-0.1/pyPiEx/__init__.py
@@ -0,0 +1,3 @@
+def start():
+    return (u'stated\u00fcck ? Ja! ... '
+            u'cool.')
---
+++ packages/pyPiEx/pyPiEx-0.1.tar.gz/pyPiEx-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pyPiEx',
+      version='0.1',
+      description='This is demo for python package creation',
+      url='https://github.com/pearlselvan/PyPiEx.git',
+      author='Muthu Selvan SR',
+      author_email='learnwin2020@gmail.com',
+      license='MIT',
+      packages=['pyPiEx'],
+      zip_safe=False)
+
+      
