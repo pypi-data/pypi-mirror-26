380bee22ba667a743e9aecb0b998cf4ba2378790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:57 +0000

    Add package easyS3/v1/easyS3-v1.tar.gz

---
+++ packages/easyS3/easyS3-v1.tar.gz/easyS3-v1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='easyS3',
+    packages=['easyS3'],
+    version='v1',
+    description='A simple and extremely user friendly s3 client',
+    author='Fauzan Baig M',
+    author_email='fauzanbaig16@gmail.com',
+    url='https://github.com/fauzanbaigm/easyS3',
+    download_url='https://github.com/fauzanbaigm/easyS3/releases/tag/easyS3v1.tar.gz',
+    keywords=['s3', 'aws s3', 'sqs', "aws sqs", "ses", "aws ses", "sns", "aws sns", "amazon"],
+    classifiers=[],
+)
