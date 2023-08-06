c027a80db8d56d8513ae7002c2b270c3f6c6ebd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:21 +0000

    Add package pact-test/0.3.23/pact-test-0.3.23.tar.gz

---
+++ packages/pact-test/pact-test-0.3.23.tar.gz/pact-test-0.3.23/pact_test/runners/pact_tests_runner.py
@@ -0,0 +1,42 @@
+import os
+import json
+from pact_test.utils.logger import *
+from pact_test.config.config_builder import Config
+from pact_test.utils.logger import log_consumers_test_results
+from pact_test.utils.logger import log_providers_test_results
+from pact_test.runners.service_consumers.test_suite import ServiceConsumerTestSuiteRunner
+from pact_test.runners.service_providers.test_suite import ServiceProviderTestSuiteRunner
+
+
+def verify(verify_consumers=False, verify_providers=False):
+    config = Config()
+
+    if verify_consumers:
+        run_consumer_tests(config)
+    if verify_providers:
+        run_provider_tests(config)
+
+
+def run_consumer_tests(config):
+    test_results = ServiceConsumerTestSuiteRunner(config).verify()
+    log_consumers_test_results(test_results)
+
+
+def run_provider_tests(config):
+    test_results = ServiceProviderTestSuiteRunner(config).verify()
+    log_providers_test_results(test_results)
+    write_pact_files(test_results.value)
+
+
+def write_pact_files(pacts):
+    pacts_directory = os.path.join(os.getcwd(), config.pacts_path)
+    if not os.path.exists(pacts_directory):
+        info('Creating Pacts directory at: ' + str(pacts_directory))
+        os.makedirs(pacts_directory)
+
+    for pact in pacts:
+        filename = pact['consumer']['name'] + '_' + pact['provider']['name'] + '.json'
+        filename = filename.replace(' ', '_').lower()
+        info('Writing pact to: ' + filename)
+        with open(os.path.join(pacts_directory, filename), 'w+') as file:
+            file.write(json.dumps(pact, indent=2, sort_keys=True))
