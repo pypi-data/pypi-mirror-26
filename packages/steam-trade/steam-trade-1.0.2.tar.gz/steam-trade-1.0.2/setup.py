eb8477c3508ac4b42b469ff1e9f1cf0010b2196c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:10 +0000

    Add package steam-trade/1.0.2/steam-trade-1.0.2.tar.gz

---
+++ packages/steam-trade/steam-trade-1.0.2.tar.gz/steam-trade-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="steam-trade",
+    version="1.0.2",
+    description="An Asynchronous, event based steam trade library",
+    url="https://github.com/Zwork101/steam-trade",
+    author="Zwork101",
+    author_email="zwork101@gmail.com",
+    license='MIT',
+
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    packages=['pytrade'],
+
+    keywords="steam trade"
+)
