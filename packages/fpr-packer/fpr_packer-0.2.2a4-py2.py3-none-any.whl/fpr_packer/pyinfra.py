ad54268d28d16fa6b4d9210138163cdc8a462525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:11 +0000

    Add package fpr-packer/0.2.2a4/fpr_packer-0.2.2a4-py2.py3-none-any.whl

---
+++ packages/fpr-packer/fpr_packer-0.2.2a4-py2.py3-none-any.whl/fpr_packer/pyinfra.py
@@ -0,0 +1,50 @@
+from pyinfra.api import FactBase, operation
+from pyinfra.modules import files, server
+
+
+class PythonVersion(FactBase):
+    """get python version"""
+
+    def command(self):
+        return 'cat /usr/local/lib/pyenv/_pyenv/version'
+
+    def process(self, output):
+        return output[0].strip()
+
+
+class NginxVersion(FactBase):
+    '''
+    Returns the nginx version installed.
+    '''
+
+    def command(self):
+        return "nginx -v 2>&1"
+
+    def process(self, output):
+        return str(output[0].split('/')[1].split()[0]).strip()
+
+
+class UwsgiVersion(FactBase):
+    '''
+    Returns the nginx version installed.
+    '''
+
+    def command(self):
+        return "uwsgi --version"
+
+    def process(self, output):
+        return str(output[0]).strip()
+
+
+@operation
+def move(comment, src, dest, user=None, group=None, mode=None):
+    f""" {comment} """
+    server.shell(
+        f"mv ./packer/{src} {dest}",
+        sudo=True
+    )
+    files.file(
+        dest,
+        user=user, group=group, mode=mode,
+        sudo=True
+    )
