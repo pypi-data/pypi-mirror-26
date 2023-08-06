b8ae704b3470c96d47a3dac6359d6bafb3dc2c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:21 +0000

    Add package lala3/0.1/lala3-0.1.tar.gz

---
+++ packages/lala3/lala3-0.1.tar.gz/lala3-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lala3',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='must',
+      author_email='mustorbit@posteo.de',
+      license='MIT',
+      packages=['lala3'],
+      zip_safe=False)
+
