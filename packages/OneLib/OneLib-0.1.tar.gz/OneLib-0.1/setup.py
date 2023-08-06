8ef6c152f0e2c88f9f27b22b5865b5c7b6e8fb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:02 +0000

    Add package OneLib/0.1/OneLib-0.1.tar.gz

---
+++ packages/OneLib/OneLib-0.1.tar.gz/OneLib-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'OneLib',
+  packages = ['OneLib'], # this must be the same as the name above
+  version = '0.1',
+  description = 'my lib',
+  author = 'He Zhihao',
+  author_email = 'gaoyuanhezhihao@gmail.com',
+  url = 'https://github.com/gaoyuanhezhihao/pyLib', # use the URL to the github repo
+  download_url = 'https://codeload.github.com/gaoyuanhezhihao/pyLib/tar.gz/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
