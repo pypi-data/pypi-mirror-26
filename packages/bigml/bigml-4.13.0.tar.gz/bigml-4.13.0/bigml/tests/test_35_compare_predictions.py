583b835752e7565a9ab747792f5fcc6acf5d26a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:39 +0000

    Add package bigml/4.13.0/bigml-4.13.0.tar.gz

---
+++ packages/bigml/bigml-4.13.0.tar.gz/bigml-4.13.0/bigml/tests/test_35_compare_predictions.py
@@ -0,0 +1,214 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+#
+# Copyright 2017 BigML
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+""" Comparing remote and local predictions
+
+"""
+from world import world, setup_module, teardown_module, show_doc
+import create_source_steps as source_create
+import create_dataset_steps as dataset_create
+import create_model_steps as model_create
+import create_time_series_steps as time_series_create
+import create_forecast_steps as forecast_create
+import compare_forecasts_steps as forecast_compare
+
+
+class TestComparePrediction(object):
+
+    def setup(self):
+        """
+            Debug information
+        """
+        print "\n-------------------\nTests in: %s\n" % __name__
+
+    def teardown(self):
+        """
+            Debug information
+        """
+        print "\nEnd of tests in: %s\n-------------------\n" % __name__
+
+
+    def test_scenario1(self):
+        """
+            Scenario: Successfully comparing forecasts from time series:
+                Given I create a data source uploading a "<data>" file
+                And I wait until the source is ready less than <time_1> secs
+                And I create a dataset
+                And I wait until the dataset is ready less than <time_2> secs
+                And I create a time series with "<params>"
+                And I wait until the time series is ready less than <time_3> secs
+                And I create a local time series
+                When I create a forecast for "<input_data>"
+                Then the forecast is "<forecasts>"
+                And I create a local forecast for "<data_input>"
+                Then the local forecast is "<forecasts>"
+
+                Examples:
+                | data             | time_1  | time_2 | time_3 | input_data  | forecasts | params
+
+
+        """
+        examples = [
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5}}', '{"000005": [{"point_forecast": [68.50243, 68.50243, 68.50243, 68.50243, 68.50243], "model": "A,N,N"}]}', '{"objective_fields": ["000001", "000005"]}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["M,N,N"], "criterion": "aic", "limit": 3}}}', '{"000005": [{"point_forecast":  [68.47181, 68.47181, 68.47181, 68.47181, 68.47181], "model": "M,N,N"}]}', '{"objective_fields": ["000001", "000005"]}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["A,A,N"], "criterion": "aic", "limit": 3}}}', '{"000005": [{"point_forecast": [72.54131, 72.64131, 72.74131, 72.84131, 72.94131], "model": "A,A,N"}]}', '{"objective_fields": ["000001", "000005"]}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["A,Ad,N"], "criterion": "aic", "limit": 3}}}', '{"000005": [{"point_forecast":  [69.87368, 69.89291, 69.91175, 69.93022, 69.94831], "model": "A,Ad,N"}]}', '{"objective_fields": ["000001", "000005"]}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5}, "000001": {"horizon": 3, "ets_models": {"criterion": "aic", "limit": 2}}}', '{"000005": [{"point_forecast": [68.50243, 68.50243, 68.50243, 68.50243, 68.50243], "model": "A,N,N"}], "000001": [{"point_forecast": [54.73965, 89.70194, 82.0656], "model": "A,N,A"}, {"point_forecast": [56.84168, 91.91525, 84.8288], "model": "A,A,A"}]}', '{"objective_fields": ["000001", "000005"]}']]
+        show_doc(self.test_scenario1, examples)
+
+        for example in examples:
+            print "\nTesting with:\n", example
+            source_create.i_upload_a_file(self, example[0])
+            source_create.the_source_is_finished(self, example[1])
+            dataset_create.i_create_a_dataset(self)
+            dataset_create.the_dataset_is_finished_in_less_than(self, example[2])
+            time_series_create.i_create_a_time_series_with_params(self, example[6])
+            time_series_create.the_time_series_is_finished_in_less_than(self, example[3])
+            time_series_create.create_local_time_series(self)
+            forecast_create.i_create_a_forecast(self, example[4])
+            forecast_create.the_forecast_is(self, example[5])
+            forecast_compare.i_create_a_local_forecast(self, example[4])
+            forecast_compare.the_local_forecast_is(self, example[5])
+
+
+    def test_scenario2(self):
+        """
+            Scenario: Successfully comparing forecasts from time series with "A" seasonality
+                Given I create a data source uploading a "<data>" file
+                And I wait until the source is ready less than <time_1> secs
+                And I create a dataset
+                And I wait until the dataset is ready less than <time_2> secs
+                And I create a time series with "<params>"
+                And I wait until the time series is ready less than <time_3> secs
+                And I create a local time series
+                When I create a forecast for "<input_data>"
+                Then the forecast is "<forecasts>"
+                And I create a local forecast for "<data_input>"
+                Then the local forecast is "<forecasts>"
+
+                Examples:
+                | data             | time_1  | time_2 | time_3 | input_data  | forecasts | params
+
+        """
+        examples = [
+
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5}}', '{"000005": [{"point_forecast": [68.50243, 68.50243, 68.50243, 68.50243, 68.50243], "model": "A,N,N"}]}', '{"objective_fields": ["000001", "000005"], "period": 12}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["M,N,A"], "criterion": "aic", "limit": 3}}}', '{"000005": [{"point_forecast":  [72.09715, 72.11611, 66.58929, 71.71928, 69.29861], "model": "M,N,A"}]}', '{"objective_fields": ["000001", "000005"], "period": 12}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["A,A,A"], "criterion": "aic", "limit": 3}}}', '{"000005": [{"point_forecast": [68.56915, 73.55705, 68.07577, 74.99042, 72.51314], "model": "A,A,A"}]}', '{"objective_fields": ["000001", "000005"], "period": 12}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["A,Ad,A"], "criterion": "aic", "limit": 3}}}', '{"000005": [{"point_forecast":[66.16225, 72.17308, 66.65573, 73.09698, 70.51449], "model": "A,Ad,A"}]}', '{"objective_fields": ["000001", "000005"], "period": 12}']]
+        show_doc(self.test_scenario2, examples)
+
+        for example in examples:
+            print "\nTesting with:\n", example
+            source_create.i_upload_a_file(self, example[0])
+            source_create.the_source_is_finished(self, example[1])
+            dataset_create.i_create_a_dataset(self)
+            dataset_create.the_dataset_is_finished_in_less_than(self, example[2])
+            time_series_create.i_create_a_time_series_with_params(self, example[6])
+            time_series_create.the_time_series_is_finished_in_less_than(self, example[3])
+            time_series_create.create_local_time_series(self)
+            forecast_create.i_create_a_forecast(self, example[4])
+            forecast_create.the_forecast_is(self, example[5])
+            forecast_compare.i_create_a_local_forecast(self, example[4])
+            forecast_compare.the_local_forecast_is(self, example[5])
+
+    def test_scenario3(self):
+        """
+            Scenario: Successfully comparing forecasts from time series with "M" seasonality
+                Given I create a data source uploading a "<data>" file
+                And I wait until the source is ready less than <time_1> secs
+                And I create a dataset
+                And I wait until the dataset is ready less than <time_2> secs
+                And I create a time series with "<params>"
+                And I wait until the time series is ready less than <time_3> secs
+                And I create a local time series
+                When I create a forecast for "<input_data>"
+                Then the forecast is "<forecasts>"
+                And I create a local forecast for "<data_input>"
+                Then the local forecast is "<forecasts>"
+
+                Examples:
+                | data             | time_1  | time_2 | time_3 | input_data  | forecasts | params
+
+
+
+        """
+        examples = [
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["M,N,M"], "criterion": "aic", "limit": 3}}}', '{"000005": [{"point_forecast":  [72.39954, 71.72705, 66.45629, 69.92079, 68.9178], "model": "M,N,M"}]}', '{"objective_fields": ["000001", "000005"], "period": 12}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["M,A,M"], "criterion": "aic", "limit": 3}}}', '{"000005": [{"point_forecast": [75.8422, 76.16165, 68.78198, 74.59676, 73.37048], "model": "M,A,M"}]}', '{"objective_fields": ["000001", "000005"], "period": 12}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["M,M,M"], "criterion": "aic", "limit": 3}}}', '{"000005": [{"point_forecast": [76.18187, 77.95967, 70.45434, 75.35636, 74.66747], "model": "M,M,M"}]}', '{"objective_fields": ["000001", "000005"], "period": 12}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["M,Ad,M"], "criterion": "aic", "limit": 3}}}', '{"000005": [{"point_forecast": [73.75816, 74.60699, 66.71212, 72.49586, 71.76787], "model": "M,Ad,M"}]}', '{"objective_fields": ["000001", "000005"], "period": 12}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["M,Md,M"], "criterion": "aic", "limit": 3}}}', '{"000005": [{"point_forecast": [74.3725, 75.02963, 67.15826, 73.19628, 71.66919], "model": "M,Md,M"}]}', '{"objective_fields": ["000001", "000005"], "period": 12}']]
+        show_doc(self.test_scenario3, examples)
+
+        for example in examples:
+            print "\nTesting with:\n", example
+            source_create.i_upload_a_file(self, example[0])
+            source_create.the_source_is_finished(self, example[1])
+            dataset_create.i_create_a_dataset(self)
+            dataset_create.the_dataset_is_finished_in_less_than(self, example[2])
+            time_series_create.i_create_a_time_series_with_params(self, example[6])
+            time_series_create.the_time_series_is_finished_in_less_than(self, example[3])
+            time_series_create.create_local_time_series(self)
+            forecast_create.i_create_a_forecast(self, example[4])
+            forecast_create.the_forecast_is(self, example[5])
+            forecast_compare.i_create_a_local_forecast(self, example[4])
+            forecast_compare.the_local_forecast_is(self, example[5])
+
+
+    def test_scenario4(self):
+        """
+            Scenario: Successfully comparing forecasts from time series with trivial models
+                Given I create a data source uploading a "<data>" file
+                And I wait until the source is ready less than <time_1> secs
+                And I create a dataset
+                And I wait until the dataset is ready less than <time_2> secs
+                And I create a time series with "<params>"
+                And I wait until the time series is ready less than <time_3> secs
+                And I create a local time series
+                When I create a forecast for "<input_data>"
+                Then the forecast is "<forecasts>"
+                And I create a local forecast for "<data_input>"
+                Then the local forecast is "<forecasts>"
+
+                Examples:
+                | data             | time_1  | time_2 | time_3 | input_data  | forecasts | params
+
+        """
+        examples = [
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["naive"]}}}', '{"000005": [{"point_forecast": [61.39, 61.39, 61.39, 61.39, 61.39], "model": "naive"}]}', '{"objective_fields": ["000001", "000005"], "period": 1}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["naive"]}}}', '{"000005": [{"point_forecast": [78.89, 61.39, 78.89, 61.39, 78.89], "model": "naive"}]}', '{"objective_fields": ["000001", "000005"], "period": 2}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["mean"]}}}', '{"000005": [{"point_forecast": [68.48369, 68.48369, 68.48369, 68.48369, 68.48369], "model": "mean"}]}', '{"objective_fields": ["000001", "000005"], "period": 1}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["mean"]}}}', '{"000005": [{"point_forecast": [65.16813, 71.79925, 65.16813, 71.79925, 65.16813], "model": "mean"}]}', '{"objective_fields": ["000001", "000005"], "period": 2}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["drift"]}}}', '{"000005": [{"point_forecast": [61.50113, 61.61226, 61.72339, 61.83452, 61.94565], "model": "drift"}]}', '{"objective_fields": ["000001", "000005"], "period": 1}'],
+            ['data/grades.csv', '10', '10', '120', '{"000005": {"horizon": 5, "ets_models": {"names": ["drift"]}}}', '{"000005": [{"point_forecast": [61.50113, 61.61226, 61.72339, 61.83452, 61.94565], "model": "drift"}]}', '{"objective_fields": ["000001", "000005"], "period": 2}']]
+        show_doc(self.test_scenario4, examples)
+
+        for example in examples:
+            print "\nTesting with:\n", example
+            source_create.i_upload_a_file(self, example[0])
+            source_create.the_source_is_finished(self, example[1])
+            dataset_create.i_create_a_dataset(self)
+            dataset_create.the_dataset_is_finished_in_less_than(self, example[2])
+            time_series_create.i_create_a_time_series_with_params(self, example[6])
+            time_series_create.the_time_series_is_finished_in_less_than(self, example[3])
+            time_series_create.create_local_time_series(self)
+            forecast_create.i_create_a_forecast(self, example[4])
+            forecast_create.the_forecast_is(self, example[5])
+            forecast_compare.i_create_a_local_forecast(self, example[4])
+            forecast_compare.the_local_forecast_is(self, example[5])
