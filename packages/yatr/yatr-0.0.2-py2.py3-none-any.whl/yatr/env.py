c0409f6d81d5ffa0d0d4c0b10a3b64ef8e4aa271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:54 +0000

    Add package yatr/0.0.2/yatr-0.0.2-py2.py3-none-any.whl

---
+++ packages/yatr/yatr-0.0.2-py2.py3-none-any.whl/yatr/env.py
@@ -0,0 +1,78 @@
+from syn.base import Base, Attr, init_hook
+from syn.type import Dict
+from syn.five import STR
+
+from .base import resolve, ordered_macros
+from .context import Context, BUILTIN_CONTEXTS
+from .task import Task
+
+#-------------------------------------------------------------------------------
+
+INITIAL_MACROS = {}
+
+#-------------------------------------------------------------------------------
+# Env
+
+
+class Env(Base):
+    _attrs = dict(macros = Attr(Dict((STR, int)), init=lambda self: dict(),
+                                doc='Macro definitions'),
+                  contexts = Attr(Dict(Context), init=lambda self: dict(),
+                                  doc='Execution context definitions'),
+                  tasks = Attr(Dict(Task), init=lambda self: dict(),
+                               doc='Task definitions'),
+                  secret_values = Attr(Dict(STR), init=lambda self: dict(),
+                                       doc='Secret value store'),
+                  env = Attr(Dict(STR), init=lambda self: dict(),
+                             doc='Current name resolution environment'),
+                  default_context = Attr(Context, doc='Execution context to use '
+                                         'if none is specified in task definition'))
+    _opts = dict(init_validate = True)
+
+    @init_hook
+    def _init_populate(self):
+        self.macros.update(INITIAL_MACROS)
+        self.contexts.update(BUILTIN_CONTEXTS)
+
+        if not hasattr(self, 'default_context'):
+            self.default_context = self.contexts['null']
+
+    def macro_env(self, **kwargs):
+        dct = dict(self.macros)
+        dct.update(self.secret_values)
+        return dct
+
+    def resolve_macros(self, **kwargs):
+        env = self.macro_env(**kwargs)
+        # TODO: better error message if there is a cycle
+        for name, template in ordered_macros(self.macros):
+            env[name] = resolve(template, env)
+        self.env = env
+
+    def resolve(self, template):
+        return resolve(template, self.env)
+
+    def update(self, env, **kwargs):
+        self.secret_values.update(env.secret_values)
+        self.macros.update(env.macros)
+        self.contexts.update(env.contexts)
+        self.tasks.update(env.tasks)
+
+        self.default_context = env.default_context
+
+    def validate(self):
+        super(Env, self).validate()
+        
+        for name, ctx in self.contexts.items():
+            ctx.validate()
+
+        for name, task in self.tasks.items():
+            task.validate()
+
+
+#-------------------------------------------------------------------------------
+# __all__
+
+__all__ = ('Env',)
+
+#-------------------------------------------------------------------------------
---
+++ packages/yatr/yatr-0.0.2-py2.py3-none-any.whl/yatr/parse.py
@@ -0,0 +1,141 @@
+import os
+import imp
+import yaml
+from syn.base_utils import chdir
+from syn.base import Base, Attr, init_hook
+from syn.type import List, Dict
+from syn.five import STR
+
+from .base import ValidationError, resolve_url, resolve, ordered_macros
+from .context import Context
+from .task import Task
+from .env import Env
+
+STRList = List(STR)
+
+#-------------------------------------------------------------------------------
+# Utilities
+
+def get_delete(in_, out, key, default, outkey=None):
+    if outkey is None:
+        outkey = key
+
+    out[outkey] = in_.get(key, default)
+    
+    if key in in_:
+        del in_[key]
+
+#-------------------------------------------------------------------------------
+# Document
+
+class Document(Base):
+    _attrs = dict(imports = Attr(List(STR), init=lambda self:list()),
+                  includes = Attr(List(STR), init=lambda self: list()),
+                  secrets = Attr(List(STR), init=lambda self: list()),
+                  macros = Attr(Dict((STR, int)), init=lambda self: dict()),
+                  contexts = Attr(Dict(Context), init=lambda self: dict()),
+                  tasks = Attr(Dict(Task), init=lambda self: dict()),
+                  secret_values = Attr(Dict(STR), init=lambda self: dict()),
+                  env = Attr(Env, init=lambda self: Env(), internal=True),
+                  dirname = Attr(STR, doc='Relative path for includes'),
+                  pull = Attr(bool, False, 'Force-pull URLs'),
+                 )
+    _opts = dict(init_validate = True)
+
+    @classmethod
+    def from_path(cls, path, **kwargs):
+        with open(path, 'r') as f:
+            dct = yaml.load(f)
+            return cls.from_yaml(dct,
+                                 os.path.abspath(os.path.dirname(path)), 
+                                 **kwargs)
+
+    @classmethod
+    def from_yaml(cls, dct, dirname, **kwargs):
+        get_delete(dct, kwargs, 'import', [], 'imports')
+        get_delete(dct, kwargs, 'include', [], 'includes')
+        get_delete(dct, kwargs, 'secrets', [])
+        get_delete(dct, kwargs, 'macros', {})
+        get_delete(dct, kwargs, 'contexts', {})
+        get_delete(dct, kwargs, 'tasks', {})
+
+        if dct:
+            raise ValidationError('Invalid top-level keys: {}'
+                                  .format(','.join(dct.keys())))
+
+        for key in list(kwargs['contexts'].keys()):
+            kwargs['contexts'][key] = \
+                Context.from_yaml(key, kwargs['contexts'][key])
+
+        for key in list(kwargs['tasks'].keys()):
+            kwargs['tasks'][key] = \
+                Task.from_yaml(key, kwargs['tasks'][key])
+
+        kwargs['dirname'] = dirname
+        return cls(**kwargs)
+
+    @init_hook
+    def process(self, **kwargs):
+        pre_macros = dict(self.macros)
+        for name, macro in ordered_macros(pre_macros, lenient=True):
+            pre_macros[name] = resolve(macro, pre_macros)
+
+        def process(path):
+            return resolve_url(resolve(path, pre_macros), force=self.pull)
+
+        with chdir(self.dirname):
+            for path in map(process, self.imports):
+                if not os.path.exists(path):
+                    raise ValidationError("Module path does not exist: {}"
+                                          .format(path))
+                self.process_import(path, **kwargs)
+
+            for path in map(process, self.includes):
+                if not os.path.isfile(path):
+                    raise ValidationError("Include path does not exist: {}"
+                                          .format(path))
+                self.process_include(path, **kwargs)
+
+        for name in self.secrets:
+            self.process_secret(name, **kwargs)
+
+        env = Env(macros=self.macros, contexts=self.contexts, tasks=self.tasks,
+                  secret_values=self.secret_values)
+        self.env.update(env, **kwargs)
+
+    def process_import(self, path, **kwargs):
+        mod = imp.load_source('yatr_module_import', path)
+        
+        if not hasattr(mod, 'env'):
+            raise ImportError("yatr extension module '{}' has no env"
+                              .format(path))
+
+        self.env.update(mod.env, **kwargs)
+
+    def process_include(self, path, **kwargs):
+        # TODO: support for ':' in path to restrict include scope
+        # TODO: when implementing :-support, will need to modify path validation
+        doc = Document.from_path(path)
+        self.env.update(doc.env, **kwargs)
+
+    def process_secret(self, name, **kwargs):
+        raise NotImplementedError('Secrets currently unsupported')
+
+    def run(self, name, **kwargs):
+        try:
+            with chdir(self.dirname):
+                return self.env.tasks[name].run(self.env, **kwargs)
+        except KeyError:
+            raise RuntimeError('No such task: {}'.format(name))
+
+    def validate(self):
+        super(Document, self).validate()
+        self.env.validate()
+
+
+#-------------------------------------------------------------------------------
+# __all__
+
+__all__ = ('Document',)
+
+#-------------------------------------------------------------------------------
