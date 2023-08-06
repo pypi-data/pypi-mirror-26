4a5fc48d5a8965600022aa7a513a0769e919dd3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:23 +0000

    Add package stormbot-fortune/1.2.1/stormbot-fortune-1.2.1.tar.gz

---
+++ packages/stormbot-fortune/stormbot-fortune-1.2.1.tar.gz/stormbot-fortune-1.2.1/README.rst
@@ -0,0 +1,23 @@
+Stormbot fortune plugin
+=======================
+
+This is a Stormbot_ plugin that tell random fortune::
+
+    <master>   | stormbot: fortune
+    <stormbot> | Il faut pas respirer la compote, ça fait tousser.
+
+.. _Stormbot: https://pypi.org/project/stormbot
+
+Install
+=======
+
+::
+
+    pip install stormbot-fortune
+
+Example
+=======
+
+::
+
+    stormbot --plugins stormbot_fortune.fortune
---
+++ packages/stormbot-fortune/stormbot-fortune-1.2.1.tar.gz/stormbot-fortune-1.2.1/stormbot_fortune/__init__.py
@@ -0,0 +1,36 @@
+"""Fortune for stormbot"""
+import sys
+import random
+import argparse
+from pkg_resources import resource_string
+
+from stormbot.bot import Plugin
+
+class Fortune(Plugin):
+    def __init__(self, bot, args):
+        self._bot = bot
+        self._sentences = args.fortune_dict.decode().splitlines()
+
+    @classmethod
+    def argparser(cls, parser):
+        default_dict = resource_string(__name__, 'data/kaamelott.dic')
+        parser.add_argument("--fortune-dict", type=argparse.FileType('r'), default=default_dict,
+                            help="Dictionnary to use for fortune (default: kaamelott)")
+
+    def cmdparser(self, parser):
+        subparser = parser.add_parser('fortune', bot=self._bot)
+        subparser.set_defaults(command=self.run)
+        if 'stormbot_say' in sys.modules:
+            subparser.add_argument("--say", dest="say", action="store_true", help="Say the fortune quote")
+
+    def random(self):
+        return random.choice(self._sentences)
+
+    def run(self, msg, parser, args):
+        quote = self.random()
+        if getattr(args, 'say', False):
+            say_args = ["say", quote]
+            say_args = parser.parse_args(say_args)
+            say_args.command(msg, parser, say_args)
+        else:
+            self._bot.write(quote)
