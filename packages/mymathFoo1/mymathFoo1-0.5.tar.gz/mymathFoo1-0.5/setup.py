975afb35ab9f4e2a8f072ded190b623eadb9adfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:21 +0000

    Add package mymathFoo1/0.5/mymathFoo1-0.5.tar.gz

---
+++ packages/mymathFoo1/mymathFoo1-0.5.tar.gz/mymathFoo1-0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="mymathFoo1", 
+      version="0.5", 
+      url="",
+      maintainer='shubham katariya',
+      maintainer_email='shubham.katariya@newput.com',
+      py_modules=['add', 'subtract', 'mymathFoo1'],
+      description="Silly little math turd to add/subtract.") 
