4cdb1f800407e8df2236a6fc21b1f4c1fed917aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:47 +0000

    Add package readable-log-formatter/0.1.1/readable-log-formatter-0.1.1.tar.gz

---
+++ packages/readable-log-formatter/readable-log-formatter-0.1.1.tar.gz/readable-log-formatter-0.1.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import subprocess
+import sys
+
+from setuptools import setup
+
+version = '0.1.1'
+
+readme = open('README.rst').read()
+
+setup(
+    name='readable-log-formatter',
+    version=version,
+    description="""A human readable log formatter for Python""",
+    long_description=readme,
+    author='Peter Baumgartner',
+    author_email='pete@lincolnloop.com',
+    url='https://github.com/ipmb/readable-log-formatter',
+    py_modules=['readable_log_formatter'],
+    test_suite='tests',
+    license="BSD",
+    keywords='readable-log-formatter',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
