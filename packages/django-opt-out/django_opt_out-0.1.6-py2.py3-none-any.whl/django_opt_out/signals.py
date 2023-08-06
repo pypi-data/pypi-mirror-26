9fca399459830947254c57a1a50b8bc3fa567540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:32 +0000

    Add package django-opt-out/0.1.6/django_opt_out-0.1.6-py2.py3-none-any.whl

---
+++ packages/django-opt-out/django_opt_out-0.1.6-py2.py3-none-any.whl/django_opt_out/signals.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+import django.dispatch
+
+opt_out_visited = django.dispatch.Signal(providing_args=["view", "request", "context"])
+opt_out_submitted = django.dispatch.Signal(providing_args=["view", "request", "opt_out"])
+opt_out_deleted = django.dispatch.Signal(providing_args=["view", "request", "opt_out"])
