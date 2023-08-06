504e3e6e4a86ab31e650b88a71be3961e7d636c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:23 +0000

    Add package batch-retry/0.0.1/batch_retry-0.0.1.tar.gz

---
+++ packages/batch-retry/batch_retry-0.0.1.tar.gz/batch_retry-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='batch_retry',
+    version='0.0.1',
+    license='MIT',
+    author='probinson',
+    author_email='patrick.robinson@envato.com',
+    url='https://github.com/envato/batch_retry',
+    long_description="README.md",
+    packages=['batch_retry'],
+    include_package_data=True,
+    description="Batch send data with retries and exponential backoff",
+    install_requires=[
+        'boto3>=1.3.1,<2',
+    ],
+    suite='tests',
+)
