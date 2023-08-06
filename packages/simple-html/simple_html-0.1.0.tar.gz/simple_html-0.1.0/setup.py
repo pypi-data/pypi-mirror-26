f7b9431525c8e0619085b62e9a443293da88686b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:24 +0000

    Add package simple-html/0.1.0/simple_html-0.1.0.tar.gz

---
+++ packages/simple-html/simple_html-0.1.0.tar.gz/simple_html-0.1.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='simple_html',
+    version='0.1.0',
+    license='MIT',
+    description='Simple HTML generator',
+    author='Keith Philpott',
+    url='https://gitlab.com/keithasaurus/simple_html',
+    packages=['simple_html'],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
