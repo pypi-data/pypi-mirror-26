736d3c8f4ef01c576d737f021d896261ab21f27b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:15 +0000

    Add package messengerpy/0.0.1/messengerpy-0.0.1.tar.gz

---
+++ packages/messengerpy/messengerpy-0.0.1.tar.gz/messengerpy-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='messengerpy',
+    version='0.0.1',
+    description='',
+    long_description='Facebook Messenger API',
+    author='',
+    url='',
+    packages=['messengerpy'],
+)
