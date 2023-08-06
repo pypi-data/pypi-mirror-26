502f968632e48f25b70ea434df2749d7b8a9d970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:04 +0000

    Add package jsnapy/1.2.1/jsnapy-1.2.1.tar.gz

---
+++ packages/jsnapy/jsnapy-1.2.1.tar.gz/jsnapy-1.2.1/lib/jnpr/jsnapy/setup_logging.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+# Copyright (c) 1999-2016, Juniper Networks Inc.
+#
+# All rights reserved.
+#
+
+import os
+import yaml
+import logging.config
+from jnpr.jsnapy import get_config_location
+
+def setup_logging(
+        default_path='logging.yml', default_level=logging.INFO,
+        env_key='LOG_CFG'):
+    config_location = get_config_location('logging.yml')
+    path = os.path.join(config_location, default_path)
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.load(f.read())
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
---
+++ packages/jsnapy/jsnapy-1.2.1.tar.gz/jsnapy-1.2.1/lib/jnpr/jsnapy/version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# Copyright (c) 1999-2017, Juniper Networks Inc.
+#
+# All rights reserved.
+#
+
+__version__ = "1.2.1"
+DATE = "2017-Nov-09"
