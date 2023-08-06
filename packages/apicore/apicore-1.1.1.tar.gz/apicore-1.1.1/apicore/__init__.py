61fec63990198ddff8f1753a1d4065d8ca202db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:46 +0000

    Add package apicore/1.1.1/apicore-1.1.1.tar.gz

---
+++ packages/apicore/apicore-1.1.1.tar.gz/apicore-1.1.1/apicore/__init__.py
@@ -0,0 +1,7 @@
+from .logger import Logger
+from .exceptions import *
+from .config import config
+from .api import api
+from .jwt import Authorization
+from .cache import cache
+from .lang import Lang
