4d38ba1be5e7eb0ccfcd120edb4aa51eff0897dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:17 +0000

    Add package shankar-new/3.6.6/shankar_new-3.6.6.zip

---
+++ packages/shankar-new/shankar_new-3.6.6.zip/shankar_new-3.6.6/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="shankar_new",
+
+    # Version number (initial):
+    version="3.6.6",
+
+    # Application author details:
+    author="shankar ",
+    
+    # Packages
+    packages=["shankar_2017"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="http://pypi.python.org/pypi/shankar-0.1.2/",
+
+    # Dependent packages (distributions)
+   
+)
