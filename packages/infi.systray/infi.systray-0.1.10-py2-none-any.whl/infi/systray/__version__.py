530a980a949c390e4cd4065ed36c07b1d9d77a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:04 +0000

    Add package infi.systray/0.1.10/infi.systray-0.1.10-py2-none-any.whl

---
+++ packages/infi.systray/infi.systray-0.1.10-py2-none-any.whl/infi/systray/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.10"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.10)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'a384ff4294f0169244d5bd2f427fe247efd62f48'
+__git_head_subject__ = u'Finished Release v0.1.10'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2017-11-15 16:20:11'
