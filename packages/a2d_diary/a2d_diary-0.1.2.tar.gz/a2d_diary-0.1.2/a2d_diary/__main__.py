1ea310e5132670b322f383e7f5d924cfbb65cfe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:40 +0000

    Add package a2d_diary/0.1.2/a2d_diary-0.1.2.tar.gz

---
+++ packages/a2d_diary/a2d_diary-0.1.2.tar.gz/a2d_diary-0.1.2/a2d_diary/__main__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from __future__ import absolute_import 
+
+
+#from subprocess import call
+import threading
+import webbrowser
+import time
+from marking_server import app
+import waitress
+
+
+def launch_browser():
+    """Launches the app"""
+    time.sleep(3)
+    webbrowser.open("http://localhost:8000/static/index.html", new=0, autoraise=True)
+
+def main(argv=None):
+    """Main method for A2D_Diary"""
+    LAUNCH_THREAD = threading.Thread(target=launch_browser)
+    LAUNCH_THREAD.start()
+    waitress.serve(app, port=8000)
+    LAUNCH_THREAD.join()
+
+if __name__ == "__main__":
+    main()
