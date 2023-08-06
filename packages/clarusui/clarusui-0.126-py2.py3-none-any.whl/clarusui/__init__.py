504830e0bf0de7a07e2446efdb46374ebc0ce910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:05 +0000

    Add package clarusui/0.126/clarusui-0.126-py2.py3-none-any.whl

---
+++ packages/clarusui/clarusui-0.126-py2.py3-none-any.whl/clarusui/__init__.py
@@ -0,0 +1,4 @@
+from clarusui.chart import PieChart, BarChart, StackedBarChart, LineChart, AreaChart, DonutChart
+from clarusui.layout import Grid, Dashboard
+from clarusui.table import Table
+from clarusui.iframe import IFrame
