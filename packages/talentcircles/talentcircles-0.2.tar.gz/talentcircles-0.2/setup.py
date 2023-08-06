d342581857bf10c58b87401af69e72db712220ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:28 +0000

    Add package talentcircles/0.2/talentcircles-0.2.tar.gz

---
+++ packages/talentcircles/talentcircles-0.2.tar.gz/talentcircles-0.2/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+setup(
+  name = 'talentcircles',
+  packages = ['talentcircles'],
+  version = '0.2',
+  description = 'A module for accessing the TalentCircles REST API',
+  author = 'Tom Clowers',
+  author_email = 'tom@talentcircles.com',
+  url = 'https://github.com/talentcircles/python_sdk',
+  download_url = 'https://github.com/talentcircles/python_sdk/archive/0.2.tar.gz',
+  keywords = ['hr', 'talent', 'jobs'],
+  license='MIT',
+  classifiers = [
+    'Development Status :: 5 - Production/Stable',
+
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+  ],
+  install_requires=[
+    'requests',
+    'datetime',
+    'urllib3',
+    'json',
+  ],
+  python_requires='>=2.6, <3',
+  py_modules=["talentcircles"],
+)
