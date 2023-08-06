cfe24421df348478459248c836c033871d964937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:41 +0000

    Add package gci-validator/0.0.1/gci-validator-0.0.1.tar.gz

---
+++ packages/gci-validator/gci-validator-0.0.1.tar.gz/gci-validator-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(
+    name="gci-validator",
+    version="0.0.1",
+    long_description=readme(),
+    description="CSV validator for the Google Code-in web API",
+    author="Daniel Hsing",
+    author_email="hsing.daniel@gmail.com",
+    license="LICENSE",
+    scripts=["bin/gci-validator"]
+)
