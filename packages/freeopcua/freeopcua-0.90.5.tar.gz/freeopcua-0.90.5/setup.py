99e084aba344399a819d97f107f632c36f72282e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:44 +0000

    Add package freeopcua/0.90.5/freeopcua-0.90.5.tar.gz

---
+++ packages/freeopcua/freeopcua-0.90.5.tar.gz/freeopcua-0.90.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="freeopcua",
+      version="0.90.5",
+      description="Pure Python OPC-UA client and server library",
+      author="Olivier Roulet-Dubonnet",
+      author_email="olivier.roulet@gmail.com",
+      url='http://freeopcua.github.io/',
+      packages=[],
+      license="GNU Lesser General Public License v3 or later",
+      install_requires=['opcua'],
+      )
