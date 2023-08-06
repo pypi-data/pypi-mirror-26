08364dbc01c4cb1e274c4605df35e908def660a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:54 +0000

    Add package keyring-otp/0.2/keyring_otp-0.2-py3-none-any.whl

---
+++ packages/keyring-otp/keyring_otp-0.2-py3-none-any.whl/kotp/tests/test_kotp.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from kotp import kotp
+
+
+class TestKeyringOTP(TestCase):
+
+    def test_run(self):
+        kotp.KeyringOTP("SECRETS", console_output=True, run_duration=0.1).run()
