1e597df02bf345cbbfc0118ec0645e8aecd10a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:15 +0000

    Add package fbpageplc/0.5/fbpageplc-0.5.tar.gz

---
+++ packages/fbpageplc/fbpageplc-0.5.tar.gz/fbpageplc-0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fbpageplc',
+      version='0.5',
+      description='Fetch posts, likes, comments of a public facebook page.',
+      url='https://github.com/akash0824/fbpageplc',
+      author='akash0824',
+      author_email='akashkumar0824@gmail.com',
+      #license='MIT',
+      packages=['fbpageplc'],
+      zip_safe=False)
