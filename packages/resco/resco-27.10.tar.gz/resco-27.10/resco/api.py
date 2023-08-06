a4cbaad0114a7f655c3726752a15fca0de02981b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:46 +0000

    Add package resco/27.10/resco-27.10.tar.gz

---
+++ packages/resco/resco-27.10.tar.gz/resco-27.10/resco/api.py
@@ -0,0 +1,19 @@
+from fabric.api import local, put, run, cd
+
+from .rvenv import RemoteVirtualEnv
+
+
+def run_unit_tests():
+    local("python -m unittest discover -s unittests/ -p '*tests.py'")
+
+
+def run_script(script_name, venv, module, working_dir):
+    run_unit_tests()
+    put(module, working_dir)
+    put('scripts', working_dir)
+
+    with cd(working_dir):
+        with venv.activate():
+            if not script_name.startswith('scripts/'):
+                script_name = 'scripts/{}'.format(script_name)
+            run('python {}'.format(script_name))
