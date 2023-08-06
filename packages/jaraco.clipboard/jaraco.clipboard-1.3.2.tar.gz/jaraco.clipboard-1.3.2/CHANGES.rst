b282da7555e9e5e682cbcb0b6d49717f2279db22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:09 +0000

    Add package jaraco.clipboard/1.3.2/jaraco.clipboard-1.3.2.tar.gz

---
+++ packages/jaraco.clipboard/jaraco.clipboard-1.3.2.tar.gz/jaraco.clipboard-1.3.2/CHANGES.rst
@@ -0,0 +1,35 @@
+1.3.2
+=====
+
+Refresh packaging.
+
+Update dependency declaration on richxerox.
+
+Update dependency on pyperclip and fix usage.
+
+1.3.1
+=====
+
+Refresh packaging.
+
+1.3
+===
+
+Add support for copy/paste text on Linux via pyperclip.
+
+1.2
+===
+
+Added test suite and automated deployments for tagged
+commits.
+
+1.1
+===
+
+Move hosting to github.
+
+1.0
+===
+
+Initial implementation with text and HTML support on Windows
+and Mac OS X and image paste support on Windows.
---
+++ packages/jaraco.clipboard/jaraco.clipboard-1.3.2.tar.gz/jaraco.clipboard-1.3.2/setup.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+	long_description = readme.read()
+
+name = 'jaraco.clipboard'
+description = 'Multi-format, cross-platform clipboard library'
+nspkg_technique = 'managed'
+"""
+Does this package use "native" namespace packages or
+pkg_resources "managed" namespace packages?
+"""
+
+name = 'jaraco.clipboard'
+description = 'Multi-format, cross-platform clipboard library'
+
+params = dict(
+	name=name,
+	use_scm_version=True,
+	author="Jason R. Coombs",
+	author_email="jaraco@jaraco.com",
+	description=description or name,
+	long_description=long_description,
+	url="https://github.com/jaraco/" + name,
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	namespace_packages=(
+		name.split('.')[:-1] if nspkg_technique == 'managed'
+		else []
+	),
+	python_requires='>=2.7',
+	install_requires=[
+	],
+	extras_require={
+		'testing': [
+			'pytest>=2.8',
+			'pytest-sugar',
+			'collective.checkdocs',
+		],
+		'docs': [
+			'sphinx',
+			'jaraco.packaging>=3.2',
+			'rst.linker>=1.9',
+		],
+		':sys_platform=="win32"': [
+			'jaraco.windows>=3.4',
+		],
+		':sys_platform=="darwin"': [
+			'richxerox>=1',
+			# for now, declare the dependency as richxerox doesn't
+			# see https://bitbucket.org/jeunice/richxerox/issues/3
+			'pyobjc',
+		],
+		':sys_platform=="linux2" or sys_platform=="linux"': [
+			"pyperclip>=1.6",
+		],
+	},
+	setup_requires=[
+		'setuptools_scm>=1.15.0',
+	],
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+	],
+	entry_points={
+	},
+)
+if __name__ == '__main__':
+	setuptools.setup(**params)
