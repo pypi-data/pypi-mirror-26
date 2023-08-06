f1648ae0772a71b199ce761c57e4dfc00b609e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:10 +0000

    Add package diarypy/0.2/diarypy-0.2.tar.gz

---
+++ packages/diarypy/diarypy-0.2.tar.gz/diarypy-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'diarypy',
+  packages = ['diarypy'], # this must be the same as the name above
+  version = '0.2',
+  description = 'Diary to create notebooks and store intermediate results and figures',
+  author = 'Miquel Perello Nieto',
+  author_email = 'perello.nieto@gmail.com',
+  url = 'https://github.com/perellonieto/DiaryPy', # use the URL to the github repo
+  download_url = 'https://github.com/perellonieto/DiaryPy/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['diary', 'notebook', 'logging', 'figures', 'experiments'], # arbitrary keywords
+  classifiers = [],
+)
