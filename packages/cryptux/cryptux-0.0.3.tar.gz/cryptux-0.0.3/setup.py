2b294829a7c509970c44180ae67845afc8e609f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:08 +0000

    Add package cryptux/0.0.3/cryptux-0.0.3.tar.gz

---
+++ packages/cryptux/cryptux-0.0.3.tar.gz/cryptux-0.0.3/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Read https://github.com/django-extensions/django-extensions/issues/92
+# Read: http://setuptools.readthedocs.io/en/latest/setuptools.html
+# Look for find_packages, packages, package_dir
+from setuptools import setup, find_packages
+
+setup(name='cryptux',
+      version='0.0.3',
+      description='Simple wallet for Cryptocurrencies',
+      author='Viet Le',
+      author_email='vietlq85@gmail.com',
+      url='https://github.com/VISCHub/cryptux',
+      install_requires=['ecdsa>=0.13'],
+      packages=find_packages('src'),  # include all packages under src
+      package_dir={'' : 'src'},   # tell distutils packages are under src
+      scripts=['tools/cryptux'],
+      keywords=['crypto', 'hdw', 'wallet', 'bitcoin', 'ether'],
+      classifiers=[])
