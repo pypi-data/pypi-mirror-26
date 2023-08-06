82dd7406b0b2efdd675fb5ab5ce6a1c57439189a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:13 +0000

    Add package Sanic-Plugins-Framework/0.3.3.dev20171102/Sanic-Plugins-Framework-0.3.3.dev20171102.tar.gz

---
+++ packages/Sanic-Plugins-Framework/Sanic-Plugins-Framework-0.3.3.dev20171102.tar.gz/Sanic-Plugins-Framework-0.3.3.dev20171102/spf/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: latin-1 -*-
+from .plugin import SanicPlugin, SanicPluginsFramework
+__version__ = '0.3.3.dev20171102'
+__all__ = ["SanicPlugin", "SanicPluginsFramework", "__version__"]
