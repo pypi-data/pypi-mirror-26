28ea8c0d040a7e7c8ca20205bbe55736e1d4cdef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:42 +0000

    Add package canonicalwebteam.yaml-redirects/1.0.3/canonicalwebteam.yaml-redirects-1.0.3.tar.gz

---
+++ packages/canonicalwebteam.yaml-redirects/canonicalwebteam.yaml-redirects-1.0.3.tar.gz/canonicalwebteam.yaml-redirects-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='canonicalwebteam.yaml-redirects',
+    version='1.0.3',
+    author='Canonical Webteam',
+    url='https://github.com/canonical-webteam/yaml-redirects',
+    packages=[
+        'canonicalwebteam.yaml_redirects'
+    ],
+    description=(
+        'A simple redirects utility that uses '
+        'a YAML file for redirects definitions'
+    ),
+    install_requires=[
+        "Django >= 1.3",
+        "canonicalwebteam.views-from-yaml >= 0.2.2",
+    ],
+)
