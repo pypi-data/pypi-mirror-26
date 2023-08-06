0998b5c79a2c829e14e98d47b5e1d3468a365f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:39 +0000

    Add package redisearch/0.7.0/redisearch-0.7.0.tar.gz

---
+++ packages/redisearch/redisearch-0.7.0.tar.gz/redisearch-0.7.0/redisearch/document.py
@@ -0,0 +1,15 @@
+class Document(object):
+    """
+    Represents a single document in a result set 
+    """
+
+    def __init__(self, id, payload = None, **fields):
+
+        self.id = id
+        self.payload = payload
+        for k, v in fields.iteritems():
+            setattr(self, k, v)
+
+    def __repr__(self):
+
+        return 'Document %s' % self.__dict__
---
+++ packages/redisearch/redisearch-0.7.0.tar.gz/redisearch-0.7.0/redisearch/result.py
@@ -0,0 +1,44 @@
+import itertools
+from .document import Document
+
+class Result(object):
+    """
+    Represents the result of a search query, and has an array of Document objects
+    """
+
+    def __init__(self, res, hascontent, duration=0, has_payload = False):
+        """
+        - **snippets**: An optional dictionary of the form {field: snippet_size} for snippet formatting
+        """
+
+        self.total = res[0]
+        self.duration = duration
+        self.docs = []
+
+        step = 1
+        if hascontent:
+            step = 3 if has_payload else 2
+        else:
+            # we can't have nocontent and payloads in the same response
+            has_payload = False
+
+        for i in xrange(1, len(res), step):
+            id = res[i]
+            payload = res[i+1] if has_payload else None
+            fields_offset = 2 if has_payload else 1
+            
+            fields = {} 
+            if hascontent:
+                fields = dict(
+                    dict(itertools.izip(res[i + fields_offset][::2], res[i + fields_offset][1::2]))) if hascontent else {}
+            try:
+                del fields['id']
+            except KeyError:
+                pass
+
+            doc = Document(id, payload=payload, **fields)
+            self.docs.append(doc)
+
+    def __repr__(self):
+
+        return 'Result{%d total, docs: %s}' % (self.total, self.docs)
