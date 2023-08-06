b1cc0e83fb65b158dc4bcdf13057c51504cb1370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:54 +0000

    Add package trx/0.5.8/trx-0.5.8.tar.gz

---
+++ packages/trx/trx-0.5.8.tar.gz/trx-0.5.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='trx',
+      version='0.5.8',
+      description='tools for (T)ime (R)esolved (X)-ray techniques',
+      long_description=readme(),
+      url='https://github.com/marcocamma/trx',
+      author='marco cammarata',
+      author_email='marcocamma@gmail.com',
+      license='MIT',
+      packages=['trx'],
+      install_requires=[
+          'numpy',
+          'fabio',
+          'pyFAI',
+          'matplotlib',
+          'datastorage',
+          'statsmodels'
+      ],
+      zip_safe=False)
