f78251169e282b00efd1149803241ea598a3e9d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:36 +0000

    Add package nester.py-olszak/1.1.0/nester.py_olszak-1.1.0.tar.gz

---
+++ packages/nester.py-olszak/nester.py_olszak-1.1.0.tar.gz/nester.py_olszak-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nester.py_olszak',
+        version         = '1.1.0',
+        py_modules      = ['nester'],
+        author          = 'Michal Olszak',
+        author_email    = 'michal.olszak@outlook.com',
+        url             = 'http://elmao-firanki.com/Wordpress/',
+        description     = 'simple printer for nested lists',
+    )
