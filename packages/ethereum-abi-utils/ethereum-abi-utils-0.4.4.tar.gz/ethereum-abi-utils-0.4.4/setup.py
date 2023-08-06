760c385a7df280d6df7889a0a94380c3a8c60e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:24 +0000

    Add package ethereum-abi-utils/0.4.4/ethereum-abi-utils-0.4.4.tar.gz

---
+++ packages/ethereum-abi-utils/ethereum-abi-utils-0.4.4.tar.gz/ethereum-abi-utils-0.4.4/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+version = '0.4.4'
+
+readme = open(os.path.join(DIR, 'README.md')).read()
+
+
+setup(
+    name='ethereum-abi-utils',
+    version=version,
+    description="""Ethereum ABI Utils""",
+    long_description=readme,
+    author='Piper Merriam',
+    author_email='pipermerriam@gmail.com',
+    url='https://github.com/pipermerriam/ethereum-abi-utils',
+    include_package_data=True,
+    install_requires=[
+        'ethereum-utils>=0.2.0',
+    ],
+    py_modules=['eth_abi'],
+    license="MIT",
+    zip_safe=False,
+    keywords='ethereum',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
