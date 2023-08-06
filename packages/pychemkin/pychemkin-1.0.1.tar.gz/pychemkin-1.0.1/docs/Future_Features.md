e280171de091a8c07b82a5f9b4c7bc0ad9b473c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:23 +0000

    Add package pychemkin/1.0.1/pychemkin-1.0.1.tar.gz

---
+++ packages/pychemkin/pychemkin-1.0.1.tar.gz/pychemkin-1.0.1/docs/Future_Features.md
@@ -0,0 +1,3 @@
+# Feature proposal for milestone 2
+
+(The requirement is to put a section in the code documentation...)
---
+++ packages/pychemkin/pychemkin-1.0.1.tar.gz/pychemkin-1.0.1/pychemkin/__init__.py
@@ -0,0 +1,5 @@
+from .InputParser import InputParser
+from .SQLParser import SQLParser
+from .BackwardCoeffs import BackwardCoeffs
+from .ReactionCoeffs import ReactionCoeffs
+from .chemkin import chemkin
