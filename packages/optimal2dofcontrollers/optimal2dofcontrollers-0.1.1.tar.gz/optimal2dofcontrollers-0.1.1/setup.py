35905ce37cf7f3a3633b94b353cd0103e47a8a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:54 +0000

    Add package optimal2dofcontrollers/0.1.1/optimal2dofcontrollers-0.1.1.tar.gz

---
+++ packages/optimal2dofcontrollers/optimal2dofcontrollers-0.1.1.tar.gz/optimal2dofcontrollers-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='optimal2dofcontrollers',
+      version='0.1.1',
+      description='Calculation of 2 Degrees of Freedom Optimal Controllers',
+      url='https://github.com/Faggioni/optimal2dofcontrollers',
+      author='Miguel Faggioni',
+      author_email='miguelfaggioni@gmail.com',
+      license='MIT',
+      install_requires=['control','numpy'],
+      zip_safe=False)
