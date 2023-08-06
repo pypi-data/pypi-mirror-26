0e2dc6abd75d83e35fdd3d3a0a82ad663b63a259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:17 +0000

    Add package tracycui/1.0.0/tracycui-1.0.0.tar.gz

---
+++ packages/tracycui/tracycui-1.0.0.tar.gz/tracycui-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name            ='tracycui',
+    version         ='1.0.0',
+    py_modules      =['tracycui'],
+    author          ='hfpython',
+    author_email    ='hfpython@headfirstlabs.com',
+    url             ='http://www.headfirstlabs.com',
+    description     ='A simple printer of nested lists',
+    )
