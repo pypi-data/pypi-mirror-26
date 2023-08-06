38e9fb5a6056747f127ba1fc8445aa6176c5bda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:33 +0000

    Add package amazonstoreprice/0.1.4/amazonstoreprice-0.1.4.tar.gz

---
+++ packages/amazonstoreprice/amazonstoreprice-0.1.4.tar.gz/amazonstoreprice-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='amazonstoreprice',
+    packages=['amazonstoreprice'],
+    package_dir={'amazonestoreprice': 'amazonstoreprice'},
+    version='0.1.4',
+    install_requires=['requests', 'beautifulsoup4'],
+    description='Find the price on Amazon store starting from url',
+    author='Alessandro Sbarbati',
+    author_email='miriodev@gmail.com',
+    url='https://github.com/Mirio/amazonstoreprice',
+    download_url='https://github.com/Mirio/amazonstoreprice/tarball/0.1',
+    keywords=['Amazon', 'amazonprice', 'amazonstoreprice', "amazonstore"],
+    license='BSD',
+    classifiers=["License :: OSI Approved :: BSD License",
+                 "Programming Language :: Python :: 3",
+                 "Topic :: Software Development :: Libraries :: Python Modules",
+                 "Topic :: Utilities"],
+)
