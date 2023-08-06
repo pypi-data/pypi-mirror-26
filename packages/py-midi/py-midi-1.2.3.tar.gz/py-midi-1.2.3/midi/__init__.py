6cc8af4267a6d002adbdc9b90c187ec8fa495103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:28 +0000

    Add package py-midi/1.2.3/py-midi-1.2.3.tar.gz

---
+++ packages/py-midi/py-midi-1.2.3.tar.gz/py-midi-1.2.3/midi/__init__.py
@@ -0,0 +1,2 @@
+
+from .midi import *
---
+++ packages/py-midi/py-midi-1.2.3.tar.gz/py-midi-1.2.3/setup.py
@@ -0,0 +1,37 @@
+"""Setup tool for the package Py-midi
+Created by Edouard Theron
+Febuary 2017
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='py-midi',
+    version='1.2.3',
+    description='A package for sending and receiving MIDI messages easily.',
+    long_description=long_description,
+    url='https://github.com/edouardtheron/py-midi',
+    author='Edouard Theron',
+    author_email='edouard@edtheron.me',
+    license='GNU',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='package library midi music digital serial interface',
+    packages=find_packages(),
+    install_requires=['pyserial'],
+)
