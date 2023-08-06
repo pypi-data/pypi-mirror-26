33e0289e3316fa37d6007e6c026a79f4d09c240e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:24 +0000

    Add package Sanic-Plugins-Framework/0.4.5.dev20171113/Sanic-Plugins-Framework-0.4.5.dev20171113.tar.gz

---
+++ packages/Sanic-Plugins-Framework/Sanic-Plugins-Framework-0.4.5.dev20171113.tar.gz/Sanic-Plugins-Framework-0.4.5.dev20171113/spf/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: latin-1 -*-
+from .framework import SanicPluginsFramework
+from .plugin import SanicPlugin
+
+__version__ = '0.4.5.dev20171113'
+__all__ = ["SanicPlugin", "SanicPluginsFramework", "__version__"]
