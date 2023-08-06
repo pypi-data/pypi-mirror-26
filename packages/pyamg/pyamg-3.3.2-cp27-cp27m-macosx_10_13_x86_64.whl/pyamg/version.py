baf8da6048c68c427eac7ea936e05f5072faac91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:00 +0000

    Add package pyamg/3.3.2/pyamg-3.3.2-cp27-cp27m-macosx_10_13_x86_64.whl

---
+++ packages/pyamg/pyamg-3.3.2-cp27-cp27m-macosx_10_13_x86_64.whl/pyamg/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '3.3.2'
+version = '3.3.2'
+full_version = '3.3.2'
+git_revision = '683beda9dff76f0c5270b748431977f76686a76a'
+release = True
+if not release:
+    version = full_version
