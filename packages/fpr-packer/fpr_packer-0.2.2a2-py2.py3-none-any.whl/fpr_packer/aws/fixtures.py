5b7964b52267cdac3b4b7de259294f9f93a0048d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:42 +0000

    Add package fpr-packer/0.2.2a2/fpr_packer-0.2.2a2-py2.py3-none-any.whl

---
+++ packages/fpr-packer/fpr_packer-0.2.2a2-py2.py3-none-any.whl/fpr_packer/aws/fixtures.py
@@ -0,0 +1,40 @@
+import pytest
+import testinfra
+import time
+
+from paramiko.ssh_exception import NoValidConnectionsError
+
+from .aws import EC2
+
+MAX_RETRIES = 20
+
+
+def get_fixture_ec2(image, identity_file, termination=True, **kwargs):
+    def ec2(request):
+        print('Create EC2 instance', end='')
+        instance = EC2.create_instance(pattern=image, **kwargs)
+        instance.wait_until_running()
+
+        retry = 0
+        host = None
+        while retry < MAX_RETRIES:
+            try:
+                host_name = instance.public_dns_name
+                host = testinfra.get_host(f"paramiko://ubuntu@{host_name}?ssh_identity_file={identity_file}")
+                host.exists("pwd")
+                break
+            except NoValidConnectionsError:
+                time.sleep(5)
+                retry += 1
+                print('.', end='')
+
+        print('.. running')
+        yield host
+
+        def fin():
+            if termination:
+                instance.terminate()
+
+        request.addfinalizer(fin)
+
+    return pytest.fixture(scope="module")(ec2)
