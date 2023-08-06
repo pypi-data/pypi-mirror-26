41d1f42c1fbb5ed6184d571f28d4ccb62945df21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:05 +0000

    Add package psycopgr/1.0.1/psycopgr-1.0.1.tar.gz

---
+++ packages/psycopgr/psycopgr-1.0.1.tar.gz/psycopgr-1.0.1/test/testgpxpy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import gpxpy
+
+gpx = gpxpy.gpx.GPX()
+
+# Create first track in our GPX:
+gpx_track = gpxpy.gpx.GPXTrack()
+gpx.tracks.append(gpx_track)
+
+# Create first segment in our GPX track:
+gpx_segment = gpxpy.gpx.GPXTrackSegment()
+gpx_track.segments.append(gpx_segment)
+
+# Create points:
+gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1234, 5.1234, elevation=1234))
+gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1235, 5.1235, elevation=1235))
+gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1236, 5.1236, elevation=1236))
+
+# You can add routes and waypoints, too...
+
+print 'Created GPX:', gpx.to_xml()
