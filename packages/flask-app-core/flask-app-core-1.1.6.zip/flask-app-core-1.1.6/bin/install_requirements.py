3d0460349ac276e396d6606b9df549c9adb73e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:01 +0000

    Add package flask-app-core/1.1.6/flask-app-core-1.1.6.zip

---
+++ packages/flask-app-core/flask-app-core-1.1.6.zip/flask-app-core-1.1.6/bin/install_requirements.py
@@ -0,0 +1,13 @@
+def install_requirements():
+    """ install flask """
+    try:
+        from flask import Flask
+        from flask_blueprint import Core
+    except ImportError:
+        from subprocess import call
+        import sys
+
+        __pip_call = "pip"
+        if sys.version_info[0] >= 3:
+            __pip_call = "pip3"
+        call([__pip_call, 'install', '-r', 'requirements.txt'])
