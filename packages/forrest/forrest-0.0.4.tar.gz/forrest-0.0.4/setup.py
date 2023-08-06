42e646e6323fe77bc7605b9fbcbf3dbcd7e75986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:31 +0000

    Add package forrest/0.0.4/forrest-0.0.4.tar.gz

---
+++ packages/forrest/forrest-0.0.4.tar.gz/forrest-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='forrest',
+    version='0.0.4',
+    description='Utility for run ephemeral applications on cloud instances',
+    author='Hector Vergara',
+    author_email='hvergara@gmail.com',
+    license='MIT',
+    scripts = ['bin/forrest'],
+    packages=find_packages(),
+    install_requires = [
+        'awscli'
+    ]
+)
