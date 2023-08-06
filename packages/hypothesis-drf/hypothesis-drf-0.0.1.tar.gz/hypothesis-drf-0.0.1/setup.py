e5e9bee9935af0f76a7d9577e0e9534ccaacc78c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:25 +0000

    Add package hypothesis-drf/0.0.1/hypothesis-drf-0.0.1.tar.gz

---
+++ packages/hypothesis-drf/hypothesis-drf-0.0.1.tar.gz/hypothesis-drf-0.0.1/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup, find_packages
+
+# We use the README as the long_description
+# TODO md -> rst
+readme_path = os.path.join(os.path.dirname(__file__), "README.md")
+
+tests_require = ['pytest~=3.2', 'Django<2', 'flake8<4']
+
+
+setup(
+    name='hypothesis-drf',
+    use_scm_version=True,
+    url='https://gitlab.com/schmidt.simon/hypothesis-drf',
+    author='Simon Schmidt',
+    author_email='schmidt.simon@gmail.com',
+    description='Hypothesis DRF',
+    long_description=open(readme_path).read(),
+    long_description_content_type='text/markdown; charset=UTF-8',
+    license='BSD',
+    zip_safe=False,
+    packages=find_packages('src/'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[
+        'hypothesis[fakefactory]',
+        'hypothesis-fspaths',
+        'djangorestframework',
+    ],
+    setup_requires=['setuptools_scm'],
+    extras_require={
+        "tests": tests_require,
+    }
+)
