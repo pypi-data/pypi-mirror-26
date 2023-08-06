bd75abe7bf8fb1e3b7fa38968d7acd74036c8060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:30 +0000

    Add package password_gen/0.2.1/password_gen-0.2.1.tar.gz

---
+++ packages/password_gen/password_gen-0.2.1.tar.gz/password_gen-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name="password_gen",
+    packages=['password_gen'],
+    version="0.2.1",
+    description="Generates memorable strong passwords.",
+    author='Olu Gbadebo',
+    author_email='odgbadeb@asu.edu',
+    url='https://github.com/weirdestnerd/password-generator',
+    keywords=['password-generator', 'passwords', 'generator'],
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Testing',
+    ],
+   install_requires=['os','re','sys','random','warnings'],
+   package_data={'default_dict':['dictionary.txt']}
+)
