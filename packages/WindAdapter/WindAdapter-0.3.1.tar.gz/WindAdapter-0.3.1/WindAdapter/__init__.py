9617ac953293dc900cb8edc1785b343f6bb1f38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:07 +0000

    Add package WindAdapter/0.3.1/WindAdapter-0.3.1.tar.gz

---
+++ packages/WindAdapter/WindAdapter-0.3.1.tar.gz/WindAdapter-0.3.1/WindAdapter/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+from WindAdapter.api import get_universe
+from WindAdapter.api import factor_load
+from WindAdapter.api import factor_help
+from WindAdapter.api import factor_details_help
+from WindAdapter.api import reset_data_dict_path
+
+__all__ = ['version',
+           'get_universe',
+           'factor_load',
+           'factor_help',
+           'factor_details_help',
+           'reset_data_dict_path']
+
+
+def version():
+    return __version__
+
+
+__version__ = '0.3.1'
