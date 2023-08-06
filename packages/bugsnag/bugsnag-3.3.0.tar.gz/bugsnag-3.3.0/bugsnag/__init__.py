58c92616d35f1ad181bb993081f59767934a2814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:03 +0000

    Add package bugsnag/3.3.0/bugsnag-3.3.0.tar.gz

---
+++ packages/bugsnag/bugsnag-3.3.0.tar.gz/bugsnag-3.3.0/bugsnag/__init__.py
@@ -0,0 +1,22 @@
+import sys
+import logging
+
+from bugsnag.configuration import Configuration, RequestConfiguration
+from bugsnag.notification import Notification
+from bugsnag.client import Client
+from bugsnag.legacy import (configuration, configure, configure_request,
+                            add_metadata_tab, clear_request_config, notify,
+                            auto_notify, before_notify)
+
+__all__ = ('Client', 'Notification', 'Configuration', 'RequestConfiguration',
+           'configuration', 'configure', 'configure_request',
+           'add_metadata_tab', 'clear_request_config', 'notify',
+           'auto_notify', 'before_notify')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+handler = logging.StreamHandler(sys.stderr)
+formatter = logging.Formatter(
+    '%(asctime)s - [%(name)s] %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
