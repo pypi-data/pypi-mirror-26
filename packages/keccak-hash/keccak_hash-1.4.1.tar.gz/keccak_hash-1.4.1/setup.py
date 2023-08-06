dbbb8e8fa713251906e472b8bd3ccaf40ceb0c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:03 +0000

    Add package keccak-hash/1.4.1/keccak_hash-1.4.1.tar.gz

---
+++ packages/keccak-hash/keccak_hash-1.4.1.tar.gz/keccak_hash-1.4.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup, Extension
+
+keccak_hash_module = Extension('keccak_hash',
+                                 sources = ['keccakmodule.c',
+                                            'keccakhash.c',
+                                            'sha3/keccak.c'],
+                               include_dirs=['.', './sha3'])
+
+setup (name = 'keccak_hash',
+       version = '1.4.1',
+       maintainer = 'rc125',
+       maintainer_email = 'rc125@protonmail.com',
+       url = 'https://github.com/rc125/python-keccak_hash',
+       download_url = 'https://github.com/rc125/python-keccak_hash/raw/master/dist/keccak_hash-1.4.tar.gz',
+       description = 'Binding for keccak proof of work hashing.',
+       ext_modules = [keccak_hash_module])
