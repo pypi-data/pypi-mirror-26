7e4839880540cc431a353029403fb79c872f9fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:03 +0000

    Add package djongo/1.2.7/djongo-1.2.7.tar.gz

---
+++ packages/djongo/djongo-1.2.7.tar.gz/djongo-1.2.7/setup.py
@@ -0,0 +1,61 @@
+from distutils.core import setup
+
+LONG_DESCRIPTION = """
+Use Mongodb as a backend database for your django project, without changing a single django model!
+
+Usage::
+
+    pip install djongo
+     
+Into settings.py file of your project, add:: 
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'djongo',
+            'NAME': 'your-db-name',
+        }
+    }
+
+Run (ONLY the first time to create collections in mongoDB)::
+    
+    manage.py migrate
+     
+YOUR ARE SET! HAVE FUN! 
+
+Requirements:
+
+1. djongo requires python 3.5 or above.
+
+
+How it works:
+
+djongo is a SQL to mongodb query transpiler. It translates a SQL query string into a mongoDB query document. As a result, all Django features, models etc work as is.
+  
+Django contrib modules:: 
+ 
+    'django.contrib.admin',
+    'django.contrib.auth',    
+    'django.contrib.sessions',
+
+and others... fully supported.
+ 
+`Full Documentation <https://nesdis.github.io/djongo/>`_
+"""
+
+setup(
+    name='djongo',
+    version='1.2.7',
+    packages=['djongo'],
+    url='https://nesdis.github.io/djongo/',
+    license='BSD',
+    author='nesdis',
+    author_email='nesdis@gmail.com',
+    description='Driver for allowing Django to use MongoDB as the database backend.',
+    install_requires=[
+        'sqlparse>=0.2.3',
+        'pymongo>=3.2.0',
+        'django>=1.8'
+    ],
+	long_description=LONG_DESCRIPTION,
+    python_requires='>=3.6'
+)
