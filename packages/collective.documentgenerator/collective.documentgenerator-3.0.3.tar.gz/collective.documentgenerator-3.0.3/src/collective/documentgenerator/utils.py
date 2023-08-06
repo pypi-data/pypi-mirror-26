5321080156ded078d12203626af7cd77651ca5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:07 +0000

    Add package collective.documentgenerator/3.0.3/collective.documentgenerator-3.0.3.tar.gz

---
+++ packages/collective.documentgenerator/collective.documentgenerator-3.0.3.tar.gz/collective.documentgenerator-3.0.3/src/collective/documentgenerator/utils.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+import hashlib
+import logging
+import os
+
+from zope import i18n
+from zope.lifecycleevent import modified
+from plone import api
+
+from zope.interface import Invalid
+
+from . import _
+
+logger = logging.getLogger('collective.documentgenerator')
+
+
+def translate(msgid, domain='collective.documentgenerator'):
+    portal = api.portal.get()
+    translation = i18n.translate(
+        msgid,
+        domain=domain,
+        context=portal.REQUEST
+    )
+    return translation
+
+
+def compute_md5(data):
+    md5 = hashlib.md5(data).hexdigest()
+    return md5
+
+
+def update_templates(templates, profile='', force=False):
+    """
+        function to manage templates update.
+        # see http://trac.imio.be/trac/ticket/9383 for full implementation
+        :param list templates: list of tuples containing ('plone-template-path', 'os-template-path')
+        :param str profile: profile path stored on template (or various identification)
+        :param bool force: force overrides of templates
+    """
+    # Don't use profile now !
+    portal = api.portal.getSite()
+    ret = []
+    for (ppath, ospath) in templates:
+        ppath = ppath.strip('/ ')
+        obj = portal.unrestrictedTraverse(ppath, default=None)
+        if not obj:
+            logger.warn("The plone template '%s' was not found" % ppath)
+            ret.append((ppath, ospath, 'plone path error'))
+            continue
+        if not os.path.exists(ospath):
+            logger.warn("The template file '%s' doesn't exist" % ospath)
+            ret.append((ppath, ospath, 'os path error'))
+            continue
+        with open(ospath, 'rb') as f:
+            data = f.read()
+            new_md5 = compute_md5(data)
+            if obj.initial_md5 == new_md5:
+                ret.append((ppath, ospath, 'unchanged'))
+                continue
+            elif obj.has_been_modified() and not force:
+                ret.append((ppath, ospath, 'kept'))
+                continue
+            obj.initial_md5 = new_md5
+            obj.style_modification_md5 = new_md5
+            obj.odt_file.data = data
+            modified(obj)
+            ret.append((ppath, ospath, 'replaced'))
+    return ret
+
+
+def update_dict_with_validation(original_dict, update_dict, error_message=_("Dict update collision on key")):
+    for key in update_dict:
+        if key in original_dict:
+            raise Invalid(_("${error_message} for key = '${key}'",
+                            mapping={'error_message': error_message, 'key': key}))
+
+        original_dict[key] = update_dict[key]
+
+
+def safe_encode(value, encoding='utf-8'):
+    """
+        Converts a value to encoding, only when it is not already encoded.
+    """
+    if isinstance(value, unicode):
+        return value.encode(encoding)
+    return value
