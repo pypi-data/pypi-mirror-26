652c78f3865499d25473fccb069c5e657eebd60e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:46 +0000

    Add package ProxyAnonymityDetector/0.1.0/ProxyAnonymityDetector-0.1.0.tar.gz

---
+++ packages/ProxyAnonymityDetector/ProxyAnonymityDetector-0.1.0.tar.gz/ProxyAnonymityDetector-0.1.0/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    version='0.1.0',
+    name='ProxyAnonymityDetector',
+    description='detect proxy anonymity with headers information',
+    packages=['ProxyAnonymityDetector'],
+    url='https://github.com/ezirmusitua/proxy-anonymity-detector',
+    download_url='https://github.com/ezirmusitua/proxy-anonymity-detector/releases',
+    author='jferroal',
+    author_email='jferroal@gmail.com',
+    keywords=['proxy', 'request', 'http', 'anonymity'],
+    license='MIT'
+)
