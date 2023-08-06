825b7f78cac7c6d0232216c832b110e639a894a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:41 +0000

    Add package websharecli/1.1.0/websharecli-1.1.0.tar.gz

---
+++ packages/websharecli/websharecli-1.1.0.tar.gz/websharecli-1.1.0/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+
+setup(
+    name="websharecli",
+    version="1.1.0",
+    description="webshare.cz CLI downloader",
+    author="Tomas Krizek",
+    author_email="tomas.krizek@mailbox.org",
+    url="https://github.com/tomaskrizek/websharecli",
+    license="GPLv3",
+    packages=['websharecli'],
+    package_data={'websharecli': ['static/*']},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'webshare=websharecli.cli:main'
+        ]
+    },
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'PyYAML',
+        'requests',
+        'xmltodict',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Environment :: Console',
+        'Topic :: Communications :: File Sharing',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
