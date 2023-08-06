c0070cfefb12d05c57841fb1b9628693bc427555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:00 +0000

    Add package dvach.api/0.0.5.5/dvach.api-0.0.5.5.tar.gz

---
+++ packages/dvach.api/dvach.api-0.0.5.5.tar.gz/dvach.api-0.0.5.5/test.py
@@ -0,0 +1,9 @@
+import api2ch
+api = api2ch.Api('test')
+
+captcha = api.get_captcha()
+print(api.get_captcha_img(captcha))
+value = input('Captcha answer: ')
+api.set_captcha_answer(captcha, value)
+comment = '''Newfags cannot\n  T\nR E'''
+print(api.send_post(board='b', comment=comment, email='', thread=165433076, captcha=captcha))
