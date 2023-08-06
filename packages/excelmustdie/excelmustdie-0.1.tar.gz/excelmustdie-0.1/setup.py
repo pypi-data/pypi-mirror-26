541b9e115275e6f2ef0ee353bdd310951d977c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:03 +0000

    Add package excelmustdie/0.1/excelmustdie-0.1.tar.gz

---
+++ packages/excelmustdie/excelmustdie-0.1.tar.gz/excelmustdie-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages 
+
+setup(name='excelmustdie',
+version='0.1',
+description='A MIPT lab report creation tool',
+long_description='A workaround to use jupyter instead of excel + tex for MIPT laboratory reports creation',
+classifiers=[ 'License :: OSI Approved :: MIT License',
+'Programming Language :: Python :: 3.6',
+],
+keywords='MIPT tex excel',
+author='Andrei Pago',
+author_email='andpago@gmail.com',
+license='MIT',
+packages=find_packages(),
+install_requires=['jupyter'],
+include_package_data=True,
+zip_safe=False) 
