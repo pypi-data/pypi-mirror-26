de0a56b94aa680b2fdc50665e3eb39eb1e7b1c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:57 +0000

    Add package datamunger/0.1/datamunger-0.1.tar.gz

---
+++ packages/datamunger/datamunger-0.1.tar.gz/datamunger-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='datamunger',
+    version='0.1',
+    description = 'Set of tools for Nan and Outlier Imputation',
+    url = 'https://github.com/bacross/datamunger',
+    author = 'bacross',
+    author_email = 'pydatamunger@gmail.com',
+    license='MIT',
+    packages=['datamunger'],
+    zip_safe=False)
