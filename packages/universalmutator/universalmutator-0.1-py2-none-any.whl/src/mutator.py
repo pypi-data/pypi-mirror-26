d4bdfce6c87d98b69cdc760302072b3500565ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:53 +0000

    Add package universalmutator/0.1/universalmutator-0.1-py2-none-any.whl

---
+++ packages/universalmutator/universalmutator-0.1-py2-none-any.whl/src/mutator.py
@@ -0,0 +1,72 @@
+import sys
+import re
+import pkg_resources
+
+def mutants(source, rules = ["universal.rules"]):
+    rulesText = []
+
+    for ruleFile in rules:
+        try:
+            with pkg_resources.resource_stream('src', 'static/' + ruleFile) as builtInRule:
+                for l in builtInRule:
+                    rulesText.append((l,"builtin:"+ruleFile))
+        except Exception as e:
+            print e
+            print "FAILED TO FIND BUILT IN RULE"
+            with open(ruleFile,'r') as file:
+                for l in file:
+                    rulesText.append((l,ruleFile))
+
+    rules = []
+    
+    for (r,ruleSource) in rulesText:
+        if r == "\n":
+            continue
+        if " ==> " not in r:
+            if " ==>" in r:
+                s = r.split(" ==>")
+            else:
+                if r[0] == "#": # Don't warn about comments
+                    continue
+                print "*" * 60
+                print "WARNING:"
+                print "RULE:",r,"FROM",ruleSource
+                print "DOES NOT MATCH EXPECTED FORMAT, AND SO WAS IGNORED"
+                print "*" * 60                
+                continue # Allow blank lines and comments, just ignore lines without a transformation
+        else:
+            s = r.split(" ==> ")
+        lhs = re.compile(s[0])
+        if (len(s[1]) > 0) and (s[1][-1] == "\n"):
+            rhs = s[1][:-1]
+        else:
+            rhs = s[1]
+        rules.append((lhs,rhs))
+
+    mutants = []
+    lineno = 0
+    for l in source:
+        lineno += 1
+        for (lhs,rhs) in rules:
+            pos = 0
+            p = lhs.search(l,pos)
+            while p and (pos < len(l)):
+                pos = p.start()+1
+                mutant = l[:p.start()] + lhs.sub(rhs,l[p.start():],count=1)
+                if mutant[-1] != "\n":
+                    mutant += "\n"
+                mutants.append((lineno,mutant))
+                p = lhs.search(l,pos)    
+
+    return mutants
+
+def makeMutant(source, mutant, path):
+    (lineModified, newCode) = mutant
+    with open(path, 'w') as file:
+        lineno = 0
+        for l in source:
+            lineno += 1
+            if lineno != lineModified:
+                file.write(l)
+            else:
+                file.write(newCode)
