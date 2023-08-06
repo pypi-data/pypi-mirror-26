9015f619d1cb7d3e382e37ebbb77bcfa784beaff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:44 +0000

    Add package opus-api/0.4.0/opus_api-0.4.0-py2.py3-none-any.whl

---
+++ packages/opus-api/opus_api-0.4.0-py2.py3-none-any.whl/opus_api/cache.py
@@ -0,0 +1,39 @@
+import diskcache as dc
+from os.path import expanduser
+
+cache = dc.Cache(expanduser('~') + '/.opus_api')
+
+
+def jcache(function):
+    """
+    Decorator for caching API json results
+    """
+    def wrapper(*args, **kwargs):
+        key = None
+        if function.__name__ == 'get':
+            key = args
+        elif function.__name__ == 'langs':
+            key = 'langs'
+        if key and key in cache:
+            return cache[key]
+        result = function(*args, **kwargs)
+        cache[key] = result
+        return result
+    return wrapper
+
+
+def hcache(function):
+    """
+    Decorator for caching crawler html
+    """
+    def wrapper(*args, **kwargs):
+        src = args[0]
+        trg = args[1]
+        key = ('html', src, trg)
+        if key in cache:
+            html = cache[key]
+        else:
+            html = function(src, trg)
+            cache[key] = html
+        return html
+    return wrapper
