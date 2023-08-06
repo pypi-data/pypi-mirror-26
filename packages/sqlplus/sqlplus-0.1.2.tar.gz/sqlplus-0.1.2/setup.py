e09a5396ec614bc43469dd97a1d1286e3d90cbc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:49 +0000

    Add package sqlplus/0.1.2/sqlplus-0.1.2.tar.gz

---
+++ packages/sqlplus/sqlplus-0.1.2.tar.gz/sqlplus-0.1.2/setup.py
@@ -0,0 +1,24 @@
+# import os
+from setuptools import setup
+
+# dir = os.path.dirname(os.path.realpath(__file__))
+
+# with open(os.path.join(dir, 'requirements.txt')) as f:
+#     required = f.read().splitlines()
+
+setup(name='sqlplus',
+      version='0.1.2',
+      description='data work tools',
+      url='https://github.com/nalssee/sqlplus.git',
+      author='nalssee',
+      author_email='kenjin@sdf.org',
+      license='MIT',
+      packages=['sqlplus'],
+      install_requires=[
+          'pandas',
+          'matplotlib',
+          'pypred',
+          'sas7bdat'
+
+      ],
+      zip_safe=False)
