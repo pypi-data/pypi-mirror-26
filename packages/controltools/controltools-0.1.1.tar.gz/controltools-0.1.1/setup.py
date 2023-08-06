0ede12a04004a5144a916f6a99a680751dba2c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:01 +0000

    Add package controltools/0.1.1/controltools-0.1.1.tar.gz

---
+++ packages/controltools/controltools-0.1.1.tar.gz/controltools-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='controltools',
+      version='0.1.1',
+      description='Control System Wrapper',
+      url='https://github.com/Faggioni/controltools',
+      author='Miguel Faggioni',
+      author_email='miguelfaggioni@gmail.com',
+      license='MIT',
+      zip_safe=False)
