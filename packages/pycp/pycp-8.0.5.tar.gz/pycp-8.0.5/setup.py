485f6a6fa18495421d97d3552e11714c27cbde98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:42 +0000

    Add package pycp/8.0.5/pycp-8.0.5.tar.gz

---
+++ packages/pycp/pycp-8.0.5.tar.gz/pycp-8.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import os
+import sys
+import pycp
+
+
+setup(name='pycp',
+      version="8.0.5",
+      description='cp and mv with a progressbar',
+      author="Dimitri Merejkowsky",
+      author_email="d.merej@gmail.com",
+      url='http://github.com/dmerejkowsky/pycp',
+      packages=['pycp'],
+      license='COPYING',
+      install_requires=[
+          "attrs",
+          "python-cli-ui",
+      ],
+      entry_points={
+          "console_scripts": [
+              "pycp = pycp.main:main",
+              "pymv = pycp.main:main",
+            ]
+      },
+      classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Shells",
+      ],
+      )
