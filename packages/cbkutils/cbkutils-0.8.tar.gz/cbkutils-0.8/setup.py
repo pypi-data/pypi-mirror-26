71be2fc7e54433335a5f6e0335cd2df80c65124f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:26 +0000

    Add package cbkutils/0.8/cbkutils-0.8.tar.gz

---
+++ packages/cbkutils/cbkutils-0.8.tar.gz/cbkutils-0.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='cbkutils',
+      version='0.8',
+      description='Simple helpers',
+      url='http://github.com/cidrblock/cbkutils',
+      author='Bradley A. Thornton',
+      author_email='brad@thethorntons.net',
+      license='MIT',
+      packages=[
+        'cbkutils',
+        'cbkutils.backports',
+        'cbkutils.backports.shutil_get_terminal_size'
+      ],
+      install_requires=[
+        'Jinja2',
+        'jsonpickle',
+        'MarkupSafe',
+        'Pygments',
+        'PyYAML'
+      ],
+      zip_safe=False)
