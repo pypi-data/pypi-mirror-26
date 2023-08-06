699ed7ebc86431a5c762747387780127cd2567db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:28 +0000

    Add package pygametemplate/0.7.0/pygametemplate-0.7.0-py3-none-any.whl

---
+++ packages/pygametemplate/pygametemplate-0.7.0-py3-none-any.whl/pygametemplate/__init__.py
@@ -0,0 +1,5 @@
+"""pygametemplate module for making creating games with Pygame easier."""
+from pygametemplate.core import log, load_image, load_font
+from pygametemplate.image import Image
+from pygametemplate.game import Game
+from pygametemplate.view import View
