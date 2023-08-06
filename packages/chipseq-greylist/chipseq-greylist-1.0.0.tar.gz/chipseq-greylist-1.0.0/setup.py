b02381f2ad5ae530254cf05c9fede72d43d01273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:49 +0000

    Add package chipseq-greylist/1.0.0/chipseq-greylist-1.0.0.tar.gz

---
+++ packages/chipseq-greylist/chipseq-greylist-1.0.0.tar.gz/chipseq-greylist-1.0.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup
+version = "1.0.0"
+
+install_requires = ["pandas", "numpy", "scipy", "statsmodels"] 
+
+scripts = ['scripts/chipseq-greylist']
+
+setup(name="chipseq-greylist",
+      version=version,
+      author="Rory Kirchner",
+      author_email="roryk@alum.mit.edu",
+      description="python implementation of GreyListChIP",
+      license="MIT",
+      url="https://github.com/roryk/chipesq-greylist",
+      scripts=scripts,
+      install_requires=install_requires)
