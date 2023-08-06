eb59a0b821630decf448694e3e994b55ea6b4204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:33 +0000

    Add package meraki/0.22/meraki-0.22.tar.gz

---
+++ packages/meraki/meraki-0.22.tar.gz/meraki-0.22/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'meraki',
+  packages = ['meraki'],
+  version = '0.22',
+  description = 'Cisco Meraki Dashboard API Python 3.6 Module',
+  author = 'Shiyue Cheng',
+  author_email = 'shiychen@cisco.com',
+  url = 'https://github.com/meraki/dashboard-api-python',
+  download_url = 'https://github.com/meraki/dashboard-api-python/archive/0.22.tar.gz',
+  keywords = ['meraki', 'dashboard', 'cisco'],
+  classifiers = [],
+)
