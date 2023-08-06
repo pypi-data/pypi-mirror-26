0803e73c66c5dc6416866e480b7e3cf87d03ef48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:01 +0000

    Add package pymediainfo/2.2.0/pymediainfo-2.2.0.tar.gz

---
+++ packages/pymediainfo/pymediainfo-2.2.0.tar.gz/pymediainfo-2.2.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pymediainfo',
+    version='2.2.0',
+    author='Louis Sautier',
+    author_email='sautier.louis@gmail.com',
+    url='https://github.com/sbraz/pymediainfo',
+    description="""A Python wrapper for the mediainfo library.""",
+    packages=find_packages(),
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT',
+    tests_require=["pytest", "pytest-runner"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
