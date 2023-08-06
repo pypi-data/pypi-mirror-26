618c556535cc68d8e7973cb57bb3208b0252e823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:32 +0000

    Add package bpmn-dmn/0.1.5/bpmn_dmn-0.1.5.tar.gz

---
+++ packages/bpmn-dmn/bpmn_dmn-0.1.5.tar.gz/bpmn_dmn-0.1.5/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+  name              = 'bpmn_dmn',
+  packages          = find_packages(),
+  version           = '0.1.5',
+  description       = 'A library to execute BPMN Workflows and DMN Decision Tables',
+  author            = 'Denny Weinberg',
+  author_email      = 'denny.weinberg@labsolution.lu',
+  url               = 'https://github.com/labsolutionlu/bpmn_dmn',
+  download_url      = 'https://github.com/labsolutionlu/bpmn_dmn/archive/0.1.zip',
+  keywords          = ['bpmn', 'dmn', 'camunda'],
+  classifiers       = [],
+  install_requires  = ['SpiffWorkflow'],
+  zip_safe          = True,
+)
