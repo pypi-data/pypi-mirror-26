98b7df3fabb6a51250dc79c12d55cce484bdfcf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:36 +0000

    Add package table-enforcer/0.1.2/table_enforcer-0.1.2-py2.py3-none-any.whl

---
+++ packages/table-enforcer/table_enforcer-0.1.2-py2.py3-none-any.whl/table_enforcer/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Table Enforcer."""
+
+__author__ = """Gus Dunn"""
+__email__ = 'w.gus.dunn@gmail.com'
+__version__ = '0.1.2'
+
+from table_enforcer.table_enforcer import Enforcer, Column  # noqa: F401
