27abdbf65e098858452ad494970f47366371bd9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:56 +0000

    Add package quality-report/2.17.2/quality_report-2.17.2.tar.gz

---
+++ packages/quality-report/quality_report-2.17.2.tar.gz/quality_report-2.17.2/tests/unittests/metric_source/jira_tests.py
@@ -0,0 +1,172 @@
+"""
+Copyright 2012-2017 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+import urllib.error
+
+from hqlib.metric_source import Jira
+
+
+class JiraUnderTest(Jira):  # pylint: disable=too-few-public-methods
+    """ Override class to return a fixed JSON file. """
+
+    nr_query_results = 5
+    view_url = 'http://view'
+    issues = '[]'
+
+    def url_read(self, url):  # pylint: disable=unused-argument
+        """ Return the static content. """
+        if 'raise' in url:
+            raise urllib.error.HTTPError(None, None, None, None, None)
+        else:
+            return '{{"searchUrl": "http://search", "viewUrl": "{0}", "total": {1}, "issues": {2}}}'.format(
+                self.view_url, self.nr_query_results, self.issues)
+
+
+class JiraTestCase(unittest.TestCase):
+    """ Base class for Jira tests. """
+
+    url = 'http://jira/'
+
+    def jira(self, *args, **kwargs):
+        """ Factory function for creating Jira instances. """
+        return JiraUnderTest(self.url, 'username', 'password', *args, **kwargs)
+
+
+class JiraTest(JiraTestCase):
+    """ Unit tests for the Jira class. """
+
+    def test_url(self):
+        """ Test the Jira url. """
+        self.assertEqual(self.url, self.jira().url())
+
+
+class JiraManualTestCasesTest(JiraTestCase):
+    """ Unit tests for the Jira manual test cases queries. """
+
+    def setUp(self):
+        self.__jira = self.jira(manual_test_cases_query_id=654)
+
+    def test_nr_manual_tests(self):
+        """ Test that the correct number of manual test cases is returned. """
+        self.assertEqual(5, self.__jira.nr_manual_test_cases())
+
+    def test_nr_manual_tests_without_query(self):
+        """ Test that the number of manual tests is -1 when Jira has no query id. """
+        self.assertEqual(-1, self.jira().nr_manual_test_cases())
+
+    def test_manual_test_time(self):
+        """ Test that the total number of minutes spend on manual test cases is correct. """
+        self.__jira.issues = '[{"fields": {"customfield_11700": "20"}},' \
+                             ' {"fields": {"customfield_11700": 100}},' \
+                             ' {"fields": {"customfield_11700": null}}]'
+        self.assertEqual(120, self.__jira.manual_test_cases_time())
+
+    def test_manual_test_time_without_query(self):
+        """ Test that the manual test time is -1 when Jira has no query id. """
+        self.assertEqual(-1, self.jira().manual_test_cases_time())
+
+    def test_nr_manual_tests_not_measured(self):
+        """ Test that the number of manual test cases not measured is correct. """
+        self.__jira.issues = '[{"fields": {"customfield_11700": "20"}},' \
+                             ' {"fields": {"customfield_11700": 100}},' \
+                             ' {"fields": {"customfield_11700": null}}]'
+        self.assertEqual(1, self.__jira.nr_manual_test_cases_not_measured())
+
+    def test_nr_manual_tests_not_measured_without_query(self):
+        """ Test that the number of manual test cases not measured is -1 when Jira has no query id. """
+        self.assertEqual(-1, self.jira().nr_manual_test_cases_not_measured())
+
+    def test_manual_test_time_url(self):
+        """ Test that the url is correct. """
+        self.assertEqual(self.__jira.view_url, self.__jira.manual_test_cases_url())
+
+
+class JiraReadyUserStoriesTest(JiraTestCase):
+    """ Unit tests for the Jira ready user stories query. """
+
+    def setUp(self):
+        self.__jira = self.jira(user_stories_ready_query_id=555)
+
+    def test_points_ready(self):
+        """ Test that the total number points of ready user stories is correct. """
+        self.__jira.issues = '[{"fields": {"customfield_10002": "1.0"}},' \
+                             ' {"fields": {"customfield_10002": "8.0"}},' \
+                             ' {"fields": {"customfield_10002": null}}]'
+        self.assertEqual(9, self.__jira.nr_story_points_ready())
+
+    def test_points_ready_without_query(self):
+        """ Test that the number of points is -1 when Jira has no query id. """
+        self.assertEqual(-1, self.jira().nr_story_points_ready())
+
+    def test_user_stories_ready_url(self):
+        """ Test that the url is correct. """
+        self.assertEqual(self.__jira.view_url, self.__jira.user_stories_ready_url())
+
+
+class JiraUserStoryAssessmentTest(JiraTestCase):
+    """ Unit tests for the user stories without assessment queries of Jira. """
+
+    def setUp(self):
+        self.__jira = self.jira(user_stories_without_security_risk_query_id=567,
+                                user_stories_without_performance_risk_query_id=789)
+
+    def test_security_risk(self):
+        """ Test that the correct number of user stories without security risk assessment is returned. """
+        self.assertEqual(5, self.__jira.nr_user_stories_without_security_risk_assessment())
+
+    def test_security_risk_without_query(self):
+        """ Test that the number of user stories is -1 when Jira hasn't got the right query. """
+        self.assertEqual(-1, self.jira().nr_user_stories_without_security_risk_assessment())
+
+    def test_security_risk_url(self):
+        """ Test that the url is correct. """
+        self.assertEqual('http://view', self.__jira.user_stories_without_security_risk_assessment_url())
+
+    def test_performance_risk(self):
+        """ Test that the correct number of user stories without performance risk assessment is returned. """
+        self.assertEqual(5, self.__jira.nr_user_stories_without_performance_risk_assessment())
+
+    def test_performance_risk_without_query(self):
+        """ Test that the number of user stories is -1 when Jira hasn't got the right query. """
+        self.assertEqual(-1, self.jira().nr_user_stories_without_performance_risk_assessment())
+
+    def test_performance_risk_url(self):
+        """ Test that the url is correct. """
+        self.assertEqual('http://view', self.__jira.user_stories_without_performance_risk_assessment_url())
+
+
+class JiraWhenFailingTest(JiraTestCase):
+    """ Unit tests for a Jira that's unavailable. """
+
+    url = 'http://raise'
+
+    def setUp(self):
+        self.__jira = self.jira(manual_test_cases_query_id=654, user_stories_ready_query_id=555,
+                                user_stories_without_security_risk_query_id=567,
+                                user_stories_without_performance_risk_query_id=789)
+
+    def test_nr_stories(self):
+        """ Test that the number of user stories is -1 when Jira is not available. """
+        self.assertEqual(-1, self.__jira.nr_user_stories_without_performance_risk_assessment())
+
+    def test_nr_stories_url(self):
+        """ Test that the url is correct. """
+        self.assertEqual(None, self.__jira.user_stories_without_performance_risk_assessment_url())
+
+    def test_points_ready(self):
+        """ Test that the total number points of ready user stories is -1 when Jira is not available. """
+        self.assertEqual(-1, self.__jira.nr_story_points_ready())
---
+++ packages/quality-report/quality_report-2.17.2.tar.gz/quality_report-2.17.2/tests/unittests/metric_source/url_opener_tests.py
@@ -0,0 +1,107 @@
+"""
+Copyright 2012-2017 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+import urllib.request
+import urllib.error
+import io
+import sys
+
+from hqlib.metric_source import url_opener
+
+
+class FakeBuildOpener(object):  # pylint: disable=too-few-public-methods
+    """ Fake a url opener build method. """
+
+    raise_exception = None
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def open(cls, *args):  # pylint: disable=unused-argument
+        """ Fake opening a url and returning its contents. """
+        if cls.raise_exception:
+            raise cls.raise_exception(None, None, None, None, None)
+        else:
+            return 'url contents'
+
+
+class UrlOpenerTest(unittest.TestCase):
+    """ Unit tests for the URL opener class. """
+
+    def test_username_password(self):
+        """ Test that the username and password can be set. """
+        opener = url_opener.UrlOpener(username='user', password='pass')
+        self.assertEqual('user', opener.username())
+        self.assertEqual('pass', opener.password())
+
+    def test_opener_with_password_mgr(self):
+        """ Test that the opener can create a basic auth handler with password manager. """
+        opener = url_opener.UrlOpener('http://uri', username='user', password='pass', build_opener=FakeBuildOpener)
+        self.assertEqual('url contents', opener.url_open('http://bla'))
+
+    def test_basic_auth_handler(self):
+        """ Test that the opener can create a basic auth handler.  """
+        opener = url_opener.UrlOpener(username='user', password='pass', url_open=FakeBuildOpener.open)
+        self.assertEqual('url contents', opener.url_open('http://bla'))
+
+    def test_opener_without_auth(self):
+        """ Test that the opener can open urls without authentication. """
+        opener = url_opener.UrlOpener(url_open=FakeBuildOpener.open)
+        self.assertEqual('url contents', opener.url_open('http://bla'))
+
+    def test_exception_while_opening(self):
+        """ Test an exception during opening. """
+        FakeBuildOpener.raise_exception = urllib.error.HTTPError
+        opener = url_opener.UrlOpener(url_open=FakeBuildOpener.open)
+        self.assertRaises(urllib.error.HTTPError, opener.url_open, 'http://bla')
+        FakeBuildOpener.raise_exception = None
+
+    def test_timeout(self):
+        """ Test the timeout exception. """
+        FakeBuildOpener.raise_exception = TimeoutError
+        opener = url_opener.UrlOpener(url_open=FakeBuildOpener.open)
+        self.assertRaises(TimeoutError, opener.url_open, 'http://bla')
+        FakeBuildOpener.raise_exception = None
+
+    def test_url_read(self):
+        """ Test reading an url. """
+        opener = url_opener.UrlOpener(url_open=lambda url: io.StringIO('contents'))
+        self.assertEqual('contents', opener.url_read('http://bla'))
+
+
+class FakeSignalModule(object):
+    """ Fake the signal module for unit test purposes. """
+    SIGALRM = 0
+
+    @staticmethod
+    def signal(_, handler):
+        """ Register the signal. We call the handler immediately. """
+        handler()
+
+
+class TimeoutTest(unittest.TestCase):
+    """ Unit tests for the Timeout class. """
+
+    @unittest.skipIf(sys.platform.startswith("win"), "Timeout only works on Posix platforms.")
+    def test_with(self):
+        """ Test the with statement. """
+        try:
+            with url_opener.Timeout(4, signal=FakeSignalModule()):
+                self.fail("Expected TimeoutError")  # pragma: no cover
+        except TimeoutError as reason:
+            self.assertEqual("Operation timed out after 4 seconds.", str(reason))
