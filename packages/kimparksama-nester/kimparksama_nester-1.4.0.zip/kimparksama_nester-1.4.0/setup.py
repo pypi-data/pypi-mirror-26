5ed9c4d24644a0f5c71c2c7910d365138345f13b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:51 +0000

    Add package kimparksama-nester/1.4.0/kimparksama_nester-1.4.0.zip

---
+++ packages/kimparksama-nester/kimparksama_nester-1.4.0.zip/kimparksama_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'kimparksama_nester',
+	version      = '1.4.0',
+	py_modules   = ['kimparksama_nester'],
+	author       = 'kimparksama',
+	author_email = 'lovefuji.ok@163.com',
+	url          = '',
+	description  = 'A simple printer of nested lists',
+      )
