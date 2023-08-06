48f13cebebceb2967e85dd8bac78f28ba08f99c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:56 +0000

    Add package sumotools/0.6.1/sumotools-0.6.1.tar.gz

---
+++ packages/sumotools/sumotools-0.6.1.tar.gz/sumotools-0.6.1/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+# with open('requirements.txt') as f:
+#     required = f.read().splitlines()
+
+
+setup(
+    # install_requires=required,
+    name="sumotools",
+    version="0.6.1",
+    description="Redistribution of the SUMO tools modules for use in the TrafficSenseMSD project",
+    author="TrafficSenseMSD",
+    packages=find_packages(),
+    url='https://github.com/TrafficSenseMSD/SumoTools',
+    keywords=['sumo', 'traffic', 'simulation'],
+    python_requires='>=3.5'
+)
