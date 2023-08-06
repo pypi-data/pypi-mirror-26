573f2afb275c6887ad0d6f64428b60dc6158c79d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:55 +0000

    Add package tieba-to-epub/0.0.1/tieba-to-epub-0.0.1.tar.gz

---
+++ packages/tieba-to-epub/tieba-to-epub-0.0.1.tar.gz/tieba-to-epub-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+name = 'tieba-to-epub'
+main_module_name = name.replace('-', '_')
+
+setup(
+    name=name,
+    version='0.0.1',
+    description='convert baidu tieba post to epub',
+    long_description='',
+    author='Elephant Liu',
+    author_email='elephant_liu@mail.dlut.edu.cn',
+    url='https://github.com/lexdene/tieba-post-to-epub',
+    license='GPLv3',
+    packages=find_packages(exclude=['tests']),
+    package_data={main_module_name: ['templates/*']},
+    install_requires=[
+        'aiohttp',
+        'lxml',
+        'ebooklib',
+        'Jinja2>=2.9',  # Jinja2 supports async since 2.9
+    ],
+    entry_points={
+        'console_scripts': [
+            '%s = %s.main:main' % (name, main_module_name)
+        ]
+    },
+    platforms=['any'],
+)
