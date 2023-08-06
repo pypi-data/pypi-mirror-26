17f06319851a32a92579aa5a51187af271793657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:58 +0000

    Add package Nextcode/0.0.1/Nextcode-0.0.1-py2.py3-none-any.whl

---
+++ packages/Nextcode/Nextcode-0.0.1-py2.py3-none-any.whl/nextcode/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+''' nextcode.py - nextcode.'''
+
+# Fjalar Sigurðarson (fjalar@wuxinextcode.com)
+# Created: November 2017
+
+import sys
+import os
+import os.path
+from datetime import datetime
+import time
+import signal
+import argparse
+import base64
+import requests
+import hashlib
+import json
+#from pathlib import Path  # Python 3
+from os.path import expanduser
+from configparser import SafeConfigParser # https://docs.python.org/2/library/configparser.html
+
+if __name__ == '__main__':
+    pass
