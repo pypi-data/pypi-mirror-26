d31889a12c77f65d26ca7fd7cb5f6ecda87988ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:52 +0000

    Add package yfm/1.0/yfm-1.0.tar.gz

---
+++ packages/yfm/yfm-1.0.tar.gz/yfm-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'yfm',
+  packages = ['yfm'],
+  version = '1.0',
+  description = 'A command line tool to store and manage Yahoo Finance stock data in a MongoDb database',
+  author = 'Ruben Afonso',
+  author_email = 'rbfrancos@gmail.com',
+  url = 'https://github.com/rubenafo/yfMongo', 
+  download_url = 'https://github.com/rubenafo/yfMongo/archive/1.0.zip',
+  keywords = ['yahoo-finance', 'mongodb', 'finance','stock'],
+  classifiers = [],
+)
+
