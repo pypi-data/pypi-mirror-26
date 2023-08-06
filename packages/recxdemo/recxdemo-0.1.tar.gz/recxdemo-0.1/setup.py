e0a9f5ae29b6b7f430466d18196b7eb0aab6304a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:01 +0000

    Add package recxdemo/0.1/recxdemo-0.1.tar.gz

---
+++ packages/recxdemo/recxdemo-0.1.tar.gz/recxdemo-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='recxdemo',
+      version='0.1',
+      description='The funniest demo in the world',
+      url='http://github.com/sleprecx',
+      author='Markus Andresen',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['demo'],
+      zip_safe=False)
