2d764ef504401e8121008db3d46c65c245f4ae09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:44 +0000

    Add package freeopcua/0.90.6/freeopcua-0.90.6.tar.gz

---
+++ packages/freeopcua/freeopcua-0.90.6.tar.gz/freeopcua-0.90.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="freeopcua",
+      version="0.90.6",
+      description="Use opcua <https://pypi.python.org/pypi/opcua> instead.",
+      author="Olivier Roulet-Dubonnet",
+      author_email="olivier.roulet@gmail.com",
+      url='http://freeopcua.github.io/',
+      packages=[],
+      license="GNU Lesser General Public License v3 or later",
+      install_requires=['opcua'],
+      )
