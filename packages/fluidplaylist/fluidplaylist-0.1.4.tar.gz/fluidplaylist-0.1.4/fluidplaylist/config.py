a2e3e15eb848b6314db59cd77e818b96061636d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:43 +0000

    Add package fluidplaylist/0.1.4/fluidplaylist-0.1.4.tar.gz

---
+++ packages/fluidplaylist/fluidplaylist-0.1.4.tar.gz/fluidplaylist-0.1.4/fluidplaylist/config.py
@@ -0,0 +1,14 @@
+"""
+Fluid Playlist configs
+"""
+
+SPOTIFY = dict(
+    client_id='8b45a07691334f74a1d4513e0cb42dce',
+    client_secret='f0b6b40efbcc40d1908df4045b0f729f'
+)
+
+FLUIDCONFIG = dict(
+    details_threshold=0.1,
+    playlist_name="Your Fluid Playlist",
+    callback_url="https://localhost:8080"
+)
