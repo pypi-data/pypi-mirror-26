d61adb6eb8ff9a2443ac40db1bf562d09c7a4d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:51 +0000

    Add package nbgitpuller/0.3/nbgitpuller-0.3.tar.gz

---
+++ packages/nbgitpuller/nbgitpuller-0.3.tar.gz/nbgitpuller-0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='nbgitpuller',
+    version='0.3',
+    url='https://github.com/data-8/nbgitpuller',
+    license='BSD',
+    author='Peter Veerman',
+    author_email='peterkangveerman@gmail.com',
+    description='Notebook Extension to do one-way synchronization of git repositories',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=['notebook'],
+    entry_points={
+        'console_scripts': [
+            'gitpuller = nbgitpuller.pull:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
