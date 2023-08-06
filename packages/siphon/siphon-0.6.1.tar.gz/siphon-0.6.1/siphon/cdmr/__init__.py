e64e8615d5d6b4428c0825f660f04184f3f39b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:27 +0000

    Add package siphon/0.6.1/siphon-0.6.1.tar.gz

---
+++ packages/siphon/siphon-0.6.1.tar.gz/siphon-0.6.1/siphon/cdmr/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2013-2015 University Corporation for Atmospheric Research/Unidata.
+# Distributed under the terms of the MIT License.
+# SPDX-License-Identifier: MIT
+"""Support for using CDMRemote on a THREDDS Data Server (TDS)."""
+
+from .dataset import Dataset
+
+__all__ = ['Dataset']
