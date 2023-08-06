67d0dec76ce0da0998e38ba8cc0c5a766de7d091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:29 +0000

    Add package SimpleQIWI/0.7/SimpleQIWI-0.7.tar.gz

---
+++ packages/SimpleQIWI/SimpleQIWI-0.7.tar.gz/SimpleQIWI-0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='SimpleQIWI',
+    version='0.7',
+    author='Emberium',
+    description='QIWI API for Humans',
+    long_description='See https://github.com/Emberium/SimpleQIWI',
+    url='https://github.com/Emberium/SimpleQIWI',
+    install_requires=['requests', 'uuid'],
+    packages=['SimpleQIWI']
+)
