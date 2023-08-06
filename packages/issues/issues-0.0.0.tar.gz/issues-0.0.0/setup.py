0b387bc72733ec2629638d0563dca842ae5bcf3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:54 +0000

    Add package issues/0.0.0/issues-0.0.0.tar.gz

---
+++ packages/issues/issues-0.0.0.tar.gz/issues-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='issues',
+      version='0.0.0',
+      description='Issues',
+      url='https://github.com/nullelement/issues.git',
+      author='Thiago O. Dutra',
+      author_email='nullelement777@gmail.com',
+      license='GPL v.3',
+      packages=['issues'],
+      zip_safe=False)
