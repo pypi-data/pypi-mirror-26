d6ab26b1dfc6509a33d76bac016a0e29f2ba4d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:10 +0000

    Add package controltools/0.1.5/controltools-0.1.5.zip

---
+++ packages/controltools/controltools-0.1.5.zip/controltools-0.1.5/controltools/__init__.py
@@ -0,0 +1,3 @@
+__all__ = [
+    'simulation', 'synthesys','toolbox','representation'
+]
---
+++ packages/controltools/controltools-0.1.5.zip/controltools-0.1.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='controltools',
+      description='Set of Control System Tools, Based on Control Package - Not Complete yet - Development',
+      version='0.1.5',
+      url='https://github.com/Faggioni/controltools',
+      author='Miguel Faggioni',
+      author_email='miguelfaggioni@gmail.com',
+      license='MIT',
+      install_requires=['numpy', 'control'],
+      zip_safe=False)
