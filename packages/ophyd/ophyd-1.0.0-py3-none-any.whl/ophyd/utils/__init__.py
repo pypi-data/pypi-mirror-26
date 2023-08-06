84e9275ee0c80a3b1770f7c48968d1f3a58a7685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:42 +0000

    Add package ophyd/1.0.0/ophyd-1.0.0-py3-none-any.whl

---
+++ packages/ophyd/ophyd-1.0.0-py3-none-any.whl/ophyd/utils/__init__.py
@@ -0,0 +1,77 @@
+# vi: ts=4 sw=4 sts=4 expandtab
+import logging
+from collections import OrderedDict
+
+from .errors import *
+from .epics_pvs import *
+from .paths import makedirs, make_dir_tree
+
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
+def enum(**enums):
+    '''Create an enum from the keyword arguments'''
+    return type('Enum', (object,), enums)
+
+
+class OrderedDefaultDict(OrderedDict):
+    """
+    a combination of defaultdict and OrderedDict
+
+    source: http://stackoverflow.com/a/6190500/1221924
+    """
+    def __init__(self, default_factory=None, *a, **kw):
+        if (default_factory is not None and not callable(default_factory)):
+            raise TypeError('first argument must be callable')
+        super().__init__(*a, **kw)
+        self.default_factory = default_factory
+
+    def __getitem__(self, key):
+        try:
+            return super().__getitem__(key)
+        except KeyError:
+            return self.__missing__(key)
+
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError(key)
+        self[key] = value = self.default_factory()
+        return value
+
+    def __reduce__(self):
+        if self.default_factory is None:
+            args = tuple()
+        else:
+            args = self.default_factory,
+        return type(self), args, None, None, self.items()
+
+    def copy(self):
+        return self.__copy__()
+
+    def __copy__(self):
+        return type(self)(self.default_factory, self)
+
+    def __deepcopy__(self, memo):
+        import copy
+        return type(self)(self.default_factory,
+                          copy.deepcopy(self.items()))
+
+    def __repr__(self):
+        return '%s(%s, %s)' % (self.__class__.__name__, self.default_factory,
+                               super().__repr__())
+
+
+def doc_annotation_forwarder(base_klass):
+    def wrapper(f):
+        f_name = getattr(f, '__name__')
+        base_func = getattr(base_klass, f_name)
+        base_docs = getattr(base_func, '__doc__')
+        base_annotation = getattr(base_func, '__annotations__')
+        f.__doc__ = base_docs
+        f.__annotations__ = base_annotation
+
+        return f
+
+    return wrapper
