8fd0225d4103499c2caead6c2e60f101585ac4cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:20 +0000

    Add package trytond-company/4.2.2/trytond_company-4.2.2-py3-none-any.whl

---
+++ packages/trytond-company/trytond_company-4.2.2-py3-none-any.whl/trytond/modules/company/party.py
@@ -0,0 +1,30 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from trytond.pyson import Eval
+from trytond.pool import PoolMeta
+
+__all__ = ['PartyConfiguration', 'PartyReplace']
+
+
+class PartyConfiguration(metaclass=PoolMeta):
+    __name__ = 'party.configuration'
+
+    @classmethod
+    def __setup__(cls):
+        super(PartyConfiguration, cls).__setup__()
+
+        cls.party_sequence.domain = [
+            cls.party_sequence.domain,
+            ('company', 'in', [Eval('context', {}).get('company'), None]),
+            ]
+
+
+class PartyReplace(metaclass=PoolMeta):
+    __name__ = 'party.replace'
+
+    @classmethod
+    def fields_to_replace(cls):
+        return super(PartyReplace, cls).fields_to_replace() + [
+            ('company.company', 'party'),
+            ('company.employee', 'party'),
+            ]
