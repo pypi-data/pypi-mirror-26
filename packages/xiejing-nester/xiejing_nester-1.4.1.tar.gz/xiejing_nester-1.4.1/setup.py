8d6d217c9cd06a25237dce438605c43d1a90b931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:42 +0000

    Add package xiejing-nester/1.4.1/xiejing_nester-1.4.1.tar.gz

---
+++ packages/xiejing-nester/xiejing_nester-1.4.1.tar.gz/xiejing_nester-1.4.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = "xiejing_nester",
+        version = "1.4.1",
+        py_modules = ['xiejing_nester'],
+        author = 'xiejing',
+        author_email = '13906470@qq.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+    )
