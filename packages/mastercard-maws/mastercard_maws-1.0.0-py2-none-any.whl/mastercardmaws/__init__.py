0116c755819afbbc2b6cc087073817eb3b24a97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:54 +0000

    Add package mastercard-maws/1.0.0/mastercard_maws-1.0.0-py2-none-any.whl

---
+++ packages/mastercard-maws/mastercard_maws-1.0.0-py2-none-any.whl/mastercardmaws/__init__.py
@@ -0,0 +1,8 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardmaws.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardmaws.abu import *
