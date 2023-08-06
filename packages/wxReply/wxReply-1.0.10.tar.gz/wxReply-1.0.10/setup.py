7cf12d203e39ed788f6c27bcd8cab89c8c9108ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:10 +0000

    Add package wxReply/1.0.10/wxReply-1.0.10.tar.gz

---
+++ packages/wxReply/wxReply-1.0.10.tar.gz/wxReply-1.0.10/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+from wxReply import __version__
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+__author__ = 'tianshl'
+__date__ = '2017/01/26'
+
+
+setup(
+    name='wxReply',                                 # 名称
+    version=__version__,                            # 版本号
+    description='wxReply',                          # 简单描述
+    long_description=long_description,              # 详细描述
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords='wechat robot weixin wxReply',         # 关键字
+    author='tianshl',                               # 作者
+    author_email='xiyuan91@126.com',                # 邮箱
+    url='https://github.com/tianshl/wxReply',       # 包含包的项目地址
+    license='MIT',                                  # 授权方式
+    packages=find_packages(),                       # 包列表
+    install_requires=['requests', 'itchat'],
+    include_package_data=True,
+    zip_safe=True,
+    use_2to3=True,
+)
---
+++ packages/wxReply/wxReply-1.0.10.tar.gz/wxReply-1.0.10/wxReply/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from .wxReply import run
+
+__author__ = 'tianshl'
+__date__ = '2017/01/26'
+__version__ = '1.0.10'
+
+__all__ = ["wxReply"]
+
+run = run
