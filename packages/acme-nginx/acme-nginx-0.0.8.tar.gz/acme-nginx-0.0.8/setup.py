f8af93b2e600c7a26dcd1225009f3e56f56e9ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:28 +0000

    Add package acme-nginx/0.0.8/acme-nginx-0.0.8.tar.gz

---
+++ packages/acme-nginx/acme-nginx-0.0.8.tar.gz/acme-nginx-0.0.8/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+p_version = '0.0.8'
+
+setup(
+    name = 'acme-nginx',
+    version = p_version,
+    author = 'Konstantin Shcherban',
+    author_email = 'k.scherban@gmail.com',
+    packages = find_packages(),
+    url = 'https://github.com/kshcherban/acme-nginx',
+    download_url = 'https://github.com/kshcherban/acme-nginx/tarball/v{0}'.format(p_version),
+    license = 'GPL v3',
+    description = 'A simple client/tool for Let\'s Encrypt or any ACME server that issues SSL certificates.',
+    keywords = ["tls", "ssl", "certificate", "acme", "letsencrypt", "nginx"],
+    install_requires = [
+        "pyOpenSSL>=0.13",
+        "pycrypto>=2.6"
+    ],
+    entry_points = {
+        'console_scripts': [
+            'acme-nginx = acme_nginx.client:main',
+        ]
+    }
+)
