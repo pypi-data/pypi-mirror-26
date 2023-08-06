b0379cdecc901482d52eff2b83f418dad67f7749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:33 +0000

    Add package vizontele/2017.10.27/vizontele-2017.10.27.tar.gz

---
+++ packages/vizontele/vizontele-2017.10.27.tar.gz/vizontele-2017.10.27/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+
+version = '2017.10.27'
+setup(
+    name='vizontele',
+    version=version,
+    description='A tool to crawl and get sources of TV Shows from various Turkish web sites.',
+    author='Halil Ozercan',
+    author_email='halilozercan@gmail.com',
+    url='https://github.com/halilozercan/vizontele',
+    download_url='https://github.com/halilozercan/vizontele/tarball/' + version,
+    entry_points={
+        'console_scripts': [
+            'vizontele = vizontele.bin:main',
+            'mizontele = vizontele.bin_movie:main'
+        ],
+    },
+    install_requires=['requests', 'pyquery', 'demjson', 'pget', 'furl', 'PyExecJS'],
+    packages=find_packages(exclude=("tests", "tests.*")),
+)
