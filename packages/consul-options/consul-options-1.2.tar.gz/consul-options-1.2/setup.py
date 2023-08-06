8e73a1a54f4f2953447573b69607430798300e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:38 +0000

    Add package consul-options/1.2/consul-options-1.2.tar.gz

---
+++ packages/consul-options/consul-options-1.2.tar.gz/consul-options-1.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='consul-options',
+    version='1.2',
+    description='Framework for using Consul as your project options storage',
+    long_description=open('README.rst').read(),
+    platforms="all",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 5 - Production/Stable',
+    ],
+    license='Apache License, Version 2.0',
+    author='Alexander Tikhonov',
+    author_email='random.gauss@gmail.com',
+    url='https://github.com/Fatal1ty/consul-options',
+    py_modules=['consul_options'],
+    install_requires=[
+        'python-consul==0.7.2',
+        'six==1.11.0',
+    ]
+)
