cbf214b19c18729425a6774006fa4fff32ae1934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:23 +0000

    Add package sdcclient/0.6.3/sdcclient-0.6.3.tar.gz

---
+++ packages/sdcclient/sdcclient-0.6.3.tar.gz/sdcclient-0.6.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sdcclient',
+      version='0.6.3',
+      description='Python client for Sysdig Cloud',
+      url='http://github.com/draios/python-sdc-client',
+      author='sysdig Inc.',
+      author_email='info@sysdig.com',
+      license='MIT',
+      packages=['sdcclient'],
+      install_requires=['requests'],
+      zip_safe=False)
