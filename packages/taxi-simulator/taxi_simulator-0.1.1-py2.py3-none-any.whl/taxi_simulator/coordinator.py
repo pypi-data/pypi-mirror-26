55ff98bcbe3703a6a61720bf60ef6175ca6bbbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:16 +0000

    Add package taxi-simulator/0.1.1/taxi_simulator-0.1.1-py2.py3-none-any.whl

---
+++ packages/taxi-simulator/taxi_simulator-0.1.1-py2.py3-none-any.whl/taxi_simulator/coordinator.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+import json
+import logging
+import threading
+import os
+
+import faker
+from spade.ACLMessage import ACLMessage
+from spade.Agent import Agent
+from spade.Behaviour import Behaviour, ACLTemplate, MessageTemplate
+
+from passenger import PassengerAgent
+from taxi import TaxiAgent
+from utils import random_position, coordinator_aid, CREATE_PROTOCOL, REQUEST_PROTOCOL, REQUEST_PERFORMATIVE, load_class
+
+logger = logging.getLogger("CoordinatorAgent")
+
+
+class CoordinatorAgent(Agent):
+    def __init__(self, agentjid, password, debug, http_port, backend_port, debug_level):
+        self.simulation_running = False
+        self.kill_simulator = threading.Event()
+        self.kill_simulator.clear()
+        self.lock = threading.RLock()
+        self.taxi_agents = {}
+        self.passenger_agents = {}
+
+        self.coordinator_strategy = None
+        self.taxi_strategy = None
+        self.passenger_strategy = None
+
+        self.faker = faker.Factory.create()
+
+        self.http_port = http_port
+        self.backend_port = backend_port
+        self.debug_level = debug_level
+
+        Agent.__init__(self, agentjid=agentjid, password=password, debug=debug)
+
+    def _setup(self):
+        logger.info("Coordinator agent running")
+        self.wui.setPort(self.http_port)
+        self.wui.start()
+        logger.info("Web interface running at http://127.0.0.1:{}/app".format(self.wui.port))
+
+        self.template_path = os.path.dirname(__file__) + os.sep + "templates"
+        self.wui.setTemplatePath(self.template_path)
+
+        self.wui.registerController("app", self.index_controller)
+        self.wui.registerController("entities", self.entities_controller)
+        self.wui.registerController("generate", self.generate_controller)
+        self.wui.registerController("run", self.run_controller)
+        self.wui.registerController("clean", self.clean_controller)
+
+        # tpl = ACLTemplate()
+        # tpl.setProtocol(CREATE_PROTOCOL)
+        # template = MessageTemplate(tpl)
+        # self.addBehaviour(CreateAgentBehaviour(), template)
+
+    def set_strategies(self, coordinator_strategy, taxi_strategy, passenger_strategy):
+        self.coordinator_strategy = load_class(coordinator_strategy)
+        self.taxi_strategy = load_class(taxi_strategy)
+        self.passenger_strategy = load_class(passenger_strategy)
+
+    def run_simulation(self):
+        if not self.simulation_running:
+            self.add_strategy(self.coordinator_strategy)
+            for taxi in self.taxi_agents.values():
+                taxi.add_strategy(self.taxi_strategy)
+            for passenger in self.passenger_agents.values():
+                passenger.add_strategy(self.passenger_strategy)
+
+            self.simulation_running = True
+            logger.info("Simulation started.")
+
+    def add_strategy(self, strategy_class):
+        tpl = ACLTemplate()
+        tpl.setProtocol(REQUEST_PROTOCOL)
+        template = MessageTemplate(tpl)
+        self.addBehaviour(strategy_class(), template)
+
+    def index_controller(self):
+        return "index.html", {"port": self.backend_port}
+
+    def run_controller(self):
+        self.run_simulation()
+        return None, {}
+
+    def entities_controller(self):
+        result = {
+            "taxis": [taxi.to_json() for taxi in self.taxi_agents.values()],
+            "passengers": [passenger.to_json() for passenger in self.passenger_agents.values()],
+            "tree": self.generate_tree(),
+            "stats": self.get_stats()
+        }
+        return None, result
+
+    def generate_controller(self, taxis=1, passengers=1):
+        taxis = int(taxis) if taxis is not None else 0
+        passengers = int(passengers) if passengers is not None else 0
+        logger.info("Creating {} taxis and {} passengers.".format(taxis, passengers))
+        self.create_agent("taxi", number=taxis)
+        self.create_agent("passenger", number=passengers)
+        return None, {"status": "done"}
+
+    def clean_controller(self):
+        self.stop_agents()
+        self.taxi_agents = {}
+        self.passenger_agents = {}
+        return None, {"status": "done"}
+
+    def stop_agents(self):
+        self.kill_simulator.set()
+        with self.lock:
+            for name, agent in self.taxi_agents.items():
+                logger.info("Stopping taxi {}".format(name))
+                agent.stop()
+        with self.lock:
+            for name, agent in self.passenger_agents.items():
+                logger.info("Stopping passenger {}".format(name))
+                agent.stop()
+
+    def create_agent(self, type_, number=1):
+        msg = ACLMessage()
+        msg.addReceiver(coordinator_aid)
+        msg.setProtocol(CREATE_PROTOCOL)
+        msg.setPerformative(REQUEST_PERFORMATIVE)
+        content = {
+            "type": type_,
+            "number": number
+        }
+        msg.setContent(json.dumps(content))
+        self.send(msg)
+
+    def generate_tree(self):
+        tree = [
+            {
+                "text": "Taxis",
+                "tags": ["{}".format(len(self.taxi_agents))],
+                "nodes": [{
+                    "text": " {}".format(i),
+                    "icon": "fa fa-taxi"
+                } for i in self.taxi_agents.keys()]
+            },
+            {
+                "text": "Passengers",
+                "tags": ["{}".format(len(self.passenger_agents))],
+                "nodes": [{
+                    "text": " {}".format(i),
+                    "icon": "fa fa-user"
+                } for i in self.passenger_agents.keys()]
+            }
+        ]
+        return tree
+
+    def get_stats(self):
+        def avg(l):
+            fl = filter(None, l)
+            return (sum(fl, 0.0) / len(fl)) if len(fl) > 0 else 0.0
+
+        waiting = avg([passenger.get_waiting_time() for passenger in self.passenger_agents.values()])
+        total = avg([passenger.total_time() for passenger in self.passenger_agents.values()])
+        return {
+            "waiting": "{0:.2f}".format(waiting),
+            "totaltime": "{0:.2f}".format(total)
+        }
+
+
+class CreateAgentBehaviour(Behaviour):
+    def _process(self):
+        msg = self._receive(block=True)
+        content = json.loads(msg.content)
+        type_ = content["type"]
+        number = content["number"]
+        if type_ == "taxi":
+            cls = TaxiAgent
+            store = self.myAgent.taxi_agents
+            strategy = self.myAgent.taxi_strategy
+        else:  # type_ == "passenger":
+            cls = PassengerAgent
+            store = self.myAgent.passenger_agents
+            strategy = self.myAgent.passenger_strategy
+
+        for _ in range(number):
+            with self.myAgent.lock:
+                if self.myAgent.kill_simulator.isSet():
+                    break
+                position = random_position()
+                name = self.myAgent.faker.user_name()
+                password = self.myAgent.faker.password()
+                jid = name + "@127.0.0.1"
+                agent = cls(jid, password, debug=self.myAgent.debug_level)
+                agent.set_id(name)
+                agent.set_position(position)
+                store[name] = agent
+                agent.start()
+                if self.myAgent.simulation_running:
+                    agent.add_strategy(strategy)
+                logger.debug("Created {} {} at position {}".format(type_, name, position))
+
+
+class CoordinatorStrategyBehaviour(Behaviour):
+    def onStart(self):
+        self.logger = logging.getLogger("CoordinatorAgent")
+        self.logger.debug("Strategy {} started in coordinator".format(type(self).__name__))
+
+    def _process(self):
+        raise NotImplementedError
---
+++ packages/taxi-simulator/taxi_simulator-0.1.1-py2.py3-none-any.whl/taxi_simulator/taxi.py
@@ -0,0 +1,183 @@
+import json
+import logging
+
+from spade.ACLMessage import ACLMessage
+from spade.Agent import Agent
+from spade.Behaviour import Behaviour, ACLTemplate, MessageTemplate
+
+from utils import TAXI_WAITING, random_position, unused_port, request_path, PROPOSE_PERFORMATIVE, INFORM_PERFORMATIVE, \
+    TAXI_MOVING_TO_PASSENGER, TAXI_IN_PASSENGER_PLACE, TAXI_MOVING_TO_DESTINY, \
+    PASSENGER_IN_DEST, REQUEST_PROTOCOL, TRAVEL_PROTOCOL, PASSENGER_LOCATION, build_aid, CANCEL_PERFORMATIVE
+
+logger = logging.getLogger("TaxiAgent")
+
+
+class TaxiAgent(Agent):
+    def __init__(self, agentjid, password, debug):
+        Agent.__init__(self, agentjid, password, debug=debug)
+        self.agent_id = None
+        self.status = TAXI_WAITING
+        self.current_pos = None
+        self.dest = None
+        self.path = None
+        self.distances = []
+        self.durations = []
+        self.port = None
+        self.current_passenger = None
+        self.current_passenger_orig = None
+        self.current_passenger_dest = None
+        self.num_assignments = 0
+
+    def _setup(self):
+        self.port = unused_port("127.0.0.1")
+        self.wui.setPort(self.port)
+        self.wui.start()
+
+        self.wui.registerController("update_position", self.update_position_controller)
+        self.wui.registerController("arrived", self.arrived_to_dest_controller)
+
+    def add_strategy(self, strategy_class):
+        tpl = ACLTemplate()
+        tpl.setProtocol(REQUEST_PROTOCOL)
+        template = MessageTemplate(tpl)
+        self.addBehaviour(strategy_class(), template)
+
+    def update_position_controller(self, lat, lon):
+        coords = [float(lat), float(lon)]
+        self.set_position(coords)
+        return None, {}
+
+    def arrived_to_dest_controller(self):
+        if self.status == TAXI_MOVING_TO_PASSENGER:
+            self.inform_passenger(TAXI_IN_PASSENGER_PLACE)
+            self.status = TAXI_MOVING_TO_DESTINY
+            self.move_to(self.current_passenger_dest)
+            logger.info("Taxi {} has picked up the passenger {}.".format(self.agent_id, self.current_passenger.getName()))
+        elif self.status == TAXI_MOVING_TO_DESTINY:
+            self.inform_passenger(PASSENGER_IN_DEST)
+            self.status = TAXI_WAITING
+            logger.info("Taxi {} has taken the passenger {} to his destination.".format(self.agent_id,
+                                                                                        self.current_passenger.getName()))
+            self.current_passenger = None
+
+        return None, {}
+
+    def set_id(self, agent_id):
+        self.agent_id = agent_id
+
+    def set_position(self, coords=None):
+        if coords:
+            self.current_pos = coords
+        else:
+            self.current_pos = random_position()
+
+        logger.debug("Taxi {} position is {}".format(self.agent_id, self.current_pos))
+        if self.status == TAXI_MOVING_TO_DESTINY:
+            self.inform_passenger(PASSENGER_LOCATION, {"location": self.current_pos})
+
+    def get_position(self):
+        return self.current_pos
+
+    def move_to(self, dest):
+        counter = 5
+        path = None
+        while counter > 0 and path is None:
+            logger.debug("Requesting path from {} to {}".format(self.current_pos, dest))
+            path, distance, duration = request_path(self.current_pos, dest)
+            counter -= 1
+        if path is None:
+            logger.error("Taxi {} could not get a path to passenger {}.".format(self.agent_id,
+                                                                                self.current_passenger.getName()))
+            reply = ACLMessage()
+            reply.addReceiver(self.current_passenger)
+            reply.setProtocol(REQUEST_PROTOCOL)
+            reply.setPerformative(CANCEL_PERFORMATIVE)
+            reply.setContent("{}")
+            logger.debug("Taxi {} sent cancel proposal to passenger {}".format(self.agent_id,
+                                                                               self.current_passenger.getName()))
+            self.send(reply)
+        else:
+            self.path = path
+            self.dest = dest
+            self.distances.append(distance)
+            self.durations.append(duration)
+
+    def inform_passenger(self, status, data=None):
+        if data is None:
+            data = {}
+        msg = ACLMessage()
+        msg.addReceiver(self.current_passenger)
+        msg.setProtocol(TRAVEL_PROTOCOL)
+        msg.setPerformative(INFORM_PERFORMATIVE)
+        content = {
+            "status": status
+        }
+        for k, v in data.items():
+            content[k] = v
+        msg.setContent(json.dumps(content))
+        self.send(msg)
+
+    def to_json(self):
+        return {
+            "id": self.agent_id,
+            "position": self.current_pos,
+            "dest": self.dest,
+            "status": self.status,
+            "path": self.path,
+            "passenger": self.current_passenger.getName() if self.current_passenger else None,
+            "assignments": self.num_assignments,
+            "distance": "{0:.2f}".format(sum(self.distances)),
+            "url": "http://127.0.0.1:{port}".format(port=self.port)
+        }
+
+
+class TaxiStrategyBehaviour(Behaviour):
+    def onStart(self):
+        self.logger = logging.getLogger("TaxiAgent")
+        self.logger.debug("Strategy {} started in taxi {}".format(type(self).__name__, self.myAgent.agent_id))
+
+    def pick_up_passenger(self, passenger_id, origin, dest):
+        self.logger.info("Taxi {} on route to passenger {}".format(self.myAgent.agent_id, passenger_id))
+        passenger_aid = build_aid(passenger_id)
+        reply = ACLMessage()
+        reply.addReceiver(passenger_aid)
+        reply.setPerformative(INFORM_PERFORMATIVE)
+        reply.setProtocol(TRAVEL_PROTOCOL)
+        content = {
+            "status": TAXI_MOVING_TO_PASSENGER
+        }
+        reply.setContent(json.dumps(content))
+        self.myAgent.status = TAXI_MOVING_TO_PASSENGER
+        self.myAgent.current_passenger = passenger_aid
+        self.myAgent.current_passenger_orig = origin
+        self.myAgent.current_passenger_dest = dest
+        self.myAgent.move_to(self.myAgent.current_passenger_orig)
+        self.myAgent.send(reply)
+        self.myAgent.num_assignments += 1
+
+    def send_proposal(self, passenger_id, content=None):
+        if content is None:
+            content = {}
+        passenger_aid = build_aid(passenger_id)
+        reply = ACLMessage()
+        reply.addReceiver(passenger_aid)
+        reply.setProtocol(REQUEST_PROTOCOL)
+        reply.setPerformative(PROPOSE_PERFORMATIVE)
+        reply.setContent(content)
+        self.logger.debug("Taxi {} sent proposal to passenger {}".format(self.myAgent.agent_id, passenger_id))
+        self.myAgent.send(reply)
+
+    def cancel_proposal(self, passenger_id, content=None):
+        if content is None:
+            content = {}
+        passenger_aid = build_aid(passenger_id)
+        reply = ACLMessage()
+        reply.addReceiver(passenger_aid)
+        reply.setProtocol(REQUEST_PROTOCOL)
+        reply.setPerformative(CANCEL_PERFORMATIVE)
+        reply.setContent(json.dumps(content))
+        self.logger.debug("Taxi {} sent cancel proposal to passenger {}".format(self.myAgent.agent_id, passenger_id))
+        self.myAgent.send(reply)
+
+    def _process(self):
+        raise NotImplementedError
