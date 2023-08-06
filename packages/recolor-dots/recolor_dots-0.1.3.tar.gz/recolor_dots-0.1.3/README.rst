9f867ffcc5761e10dbeed4d3bea986f1ac85e7fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:04 +0000

    Add package recolor-dots/0.1.3/recolor_dots-0.1.3.tar.gz

---
+++ packages/recolor-dots/recolor_dots-0.1.3.tar.gz/recolor_dots-0.1.3/README.rst
@@ -0,0 +1,18 @@
+recolor-dots
+============
+
+Templates for dotfile recoloring
+
+Installation
+------------
+
+::
+    
+    pip install recolor_dots
+
+Usage
+-----
+
+::
+
+    recolor --help
---
+++ packages/recolor-dots/recolor_dots-0.1.3.tar.gz/recolor_dots-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='recolor_dots',
+      version='0.1.3',
+      description='Templates for dotfile recoloring',
+      url='http://github.com/raghavsub/recolor-dots',
+      author='Raghav Subramaniam',
+      author_email='raghavs511@gmail.com',
+      license='MIT',
+      packages=['recolor_dots'],
+      entry_points={'console_scripts': ['recolor=recolor_dots.main:main']},
+      install_requires=['docopt', 'pystache', 'pyyaml'])
