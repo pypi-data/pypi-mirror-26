60c4c9b544fa1a6ef5ab5e13bfd55e65a31a3eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:53 +0000

    Add package pyEDFlib/0.1.11/pyEDFlib-0.1.11.tar.gz

---
+++ packages/pyEDFlib/pyEDFlib-0.1.11.tar.gz/pyEDFlib-0.1.11/demo/plotEDFFile.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+
+import pyedflib
+
+
+from stacklineplot import stackplot
+
+
+if __name__ == '__main__':
+    f = pyedflib.data.test_generator()
+    n = f.signals_in_file
+    signal_labels = f.getSignalLabels()
+
+    sigbufs = np.zeros((n, f.getNSamples()[0]))
+    for i in np.arange(n):
+        sigbufs[i, :] = f.readSignal(i)
+
+    f._close()
+    del f
+    # stackplot(sigbufs,seconds=10.0, start_time=0.0, ylabels=signal_labels)
+
+    stackplot(sigbufs[:, :2000], ylabels=signal_labels)
---
+++ packages/pyEDFlib/pyEDFlib-0.1.11.tar.gz/pyEDFlib-0.1.11/doc/source/index.rst
@@ -0,0 +1,25 @@
+.. PyEDFlib documentation master file, created by
+   sphinx-quickstart on Sat Nov 14 17:04:56 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to PyEDFlib's documentation!
+====================================
+
+.. include:: index_header.rst
+
+Contents:
+
+.. toctree::
+   :maxdepth: 1
+   ref/index
+   dev/index
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`search`
+
