a5adf8791382644246ca1a0d96f739a061078694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:48 +0000

    Add package testindicator/0.1/testindicator-0.1.tar.gz

---
+++ packages/testindicator/testindicator-0.1.tar.gz/testindicator-0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+	name='testindicator',
+	packages=['testindicator'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		'watchdog',
+		'zenipy',
+		'pyyaml'
+	],
+	entry_points={
+		'console_scripts': [
+			'testindicator = testindicator.application:main',
+		]
+	},
+	version='0.1',
+	description="Don't worry about running tests - just write code",
+	long_description="testindicator is an automatic test runner/monitor. It monitors a directory of your choice and runs your tests as soon as a file is changed",
+	author='Logi Leifsson',
+	author_email='logileifs@gmail.com',
+	url='https://github.com/logileifs/testindicator',
+)
