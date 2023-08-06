39d4a940103a3a41fab6f611f5f5f69ed6185b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:36 +0000

    Add package pyecharts/0.2.7/pyecharts-0.2.7.tar.gz

---
+++ packages/pyecharts/pyecharts-0.2.7.tar.gz/pyecharts-0.2.7/README.md
@@ -0,0 +1,95 @@
+# pyecharts
+
+pyecharts is a library to generate charts using Echarts. It simply provides the interface between Echarts and Python.
+
+[![Build Status](https://travis-ci.org/chenjiandongx/pyecharts.svg?branch=master)](https://travis-ci.org/chenjiandongx/pyecharts) [![codecov](https://codecov.io/gh/chenjiandongx/pyecharts/branch/master/graph/badge.svg)](https://codecov.io/gh/chenjiandongx/pyecharts) [![PyPI version](https://badge.fury.io/py/pyecharts.svg)](https://badge.fury.io/py/pyecharts) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Introduction
+[Echarts](https://github.com/ecomfe/echarts) is an open source library from Baidu for data visualization in javascript. It has awesome demo pages so I started to look out for an interface library so that I could use it in Python. I ended up with [echarts-python](https://github.com/yufeiminds/echarts-python) on github but it lacks of documentation and was not updated for a while. Just like many other Python projects, I started my own project, pyecharts, referencing echarts-python and another library [pygal](https://github.com/Kozea/pygal).
+
+## Installation
+pyecharts works on Python2 and Python3. For more information please refer to [changelog.md](https://github.com/chenjiandongx/pyecharts/blob/master/changelog.md)
+
+### Jupyter-Notebook
+Make sure you hava installed jupyter-notebook enviroment if you want to show your charts on notebook.   
+How to install it?
+```
+$ pip install notebook
+```
+### pyecharts
+You can install it via pip
+```
+$ pip install pyecharts
+```
+
+or clone it and install it
+```
+$ git clone --recursive https://github.com/chenjiandongx/pyecharts.git
+$ cd pyecharts
+$ python setup.py install
+```
+
+## Basic Usage
+```python
+from pyecharts import Bar
+
+attr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+v1 = [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+v2 = [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+bar = Bar("Bar chart", "precipitation and evaporation one year")
+bar.add("precipitation", attr, v1, mark_line=["average"], mark_point=["max", "min"])
+bar.add("evaporation", attr, v2, mark_line=["average"], mark_point=["max", "min"])
+bar.render()
+```
+
+It will create a file named render.html in the root directory, open file with your borwser.  
+
+![usage-0](https://github.com/chenjiandongx/pyecharts/blob/master/images/usage-0.gif)
+
+
+## Working with pandas & numpy
+
+![pandas_numpy](https://github.com/chenjiandongx/pyecharts/blob/master/images/pandas-numpy.png)
+
+
+## working with Flask & Django
+Flask
+
+![flask-0](https://github.com/chenjiandongx/pyecharts/blob/master/images/flask-0.gif)
+
+Django
+
+![django-0](https://github.com/chenjiandongx/pyecharts/blob/master/images/django-0.gif)
+
+* 中文文档
+    * [pyecharts + Flask](https://github.com/chenjiandongx/pyecharts/blob/master/docs/zh-cn/doc_flask.md)
+    * [pyecharts + Django](https://github.com/chenjiandongx/pyecharts/blob/master/docs/zh-cn/doc_django.md)
+* English
+    * [pyecharts + Flask](https://github.com/chenjiandongx/pyecharts/blob/master/docs/en-us/doc_flask.md)
+    * [pyecharts + Django](https://github.com/chenjiandongx/pyecharts/blob/master/docs/en-us/doc_django.md)
+
+
+## Documentation
+* 中文文档
+    * [pyecharts 文档-预览篇](https://github.com/chenjiandongx/pyecharts/blob/master/docs/zh-cn/doc_prepare.md)
+    * [pyecharts 文档-图形篇](https://github.com/chenjiandongx/pyecharts/blob/master/docs/zh-cn/doc_charts.md)
+    * [pyecharts 文档-API 篇](https://github.com/chenjiandongx/pyecharts/blob/master/docs/zh-cn/doc_api.md)
+    * [pyecharts 文档-版本篇](https://github.com/chenjiandongx/pyecharts/blob/master/changelog.md)
+    * [pyecharts 文档-画廊篇](https://github.com/chenjiandongx/pyecharts/blob/master/docs/zh-cn/doc_gallery.md)
+    * [pyecharts 文档-FAQ 篇](https://github.com/chenjiandongx/pyecharts/blob/master/docs/zh-cn/doc_faq.md)
+
+* [English](https://github.com/chenjiandongx/pyecharts/blob/master/docs/en-us/documentation.md)
+
+
+## Test
+```shell
+$ cd test
+$ nosetests --with-coverage --cover-package pyecharts --cover-package test
+```
+
+## Author
+pyecharts is developed and maintained by chenjiandongx ([chenjiandongx@qq.com](chenjiandongx@qq.com))
+
+## License
+pyecharts is released under the MIT License. See LICENSE for more information.
+
---
+++ packages/pyecharts/pyecharts-0.2.7.tar.gz/pyecharts-0.2.7/pyecharts/base.py
@@ -0,0 +1,196 @@
+# coding=utf-8
+
+import uuid
+import json
+import datetime
+
+import pyecharts.utils as utils
+import pyecharts.template as template
+import pyecharts.constants as constants
+
+
+class Base(object):
+    """
+    `Base`类是所有图形类的基类，提供部分初始化参数和基本的方法
+    """
+    def __init__(self,
+                 width=800,
+                 height=400,
+                 page_title=constants.PAGE_TITLE,
+                 jshost=None):
+        """
+
+        :param width:
+            画布宽度，默认为 800（px）
+        :param height:
+            画布高度，默认为 400（px）
+        :param page_title:
+            指定生成的 html 文件中 <title> 标签的值。默认为'Echarts'
+        :param jshost:
+            自定义每个实例的 JavaScript host
+        """
+        self._option = {}
+        self._js_dependencies = set()
+        self._chart_id = uuid.uuid4().hex
+        self.width, self.height = width, height
+        self._page_title = page_title
+        self._jshost = jshost if jshost else constants.CONFIGURATION['HOST']
+        self._js_dependencies = {'echarts'}
+
+    @property
+    def chart_id(self):
+        """ 设置 chart_id 属性为可读
+        """
+        return self._chart_id
+
+    @property
+    def options(self):
+        """ 设置 options 属性为可读
+        """
+        return self._option
+
+    @property
+    def js_dependencies(self):
+        """ 依赖的 js 文件列表
+        """
+        return self._js_dependencies
+
+    def show_config(self):
+        """ 打印输出图形所有配置项
+        """
+        print(json_dumps(self._option, indent=4))
+
+    def render_embed(self):
+        """ 渲染图表的所有配置项，为 web pages 服务，不过需先提供
+        所需要的js 依赖文件
+        """
+        embed = 'chart_component.html'
+        tmp = template.JINJA2_ENV.get_template(embed)
+        my_option = json_dumps(self._option, indent=4)
+        html = tmp.render(my_option=my_option,
+                          chart_id=self._chart_id,
+                          my_width=self.width,
+                          my_height=self.height)
+        return html
+
+    def get_js_dependencies(self):
+        """ 声明所有的 js 文件路径
+        """
+        return template.produce_html_script_list(self._js_dependencies)
+
+    def render(self, path="render.html"):
+        """ 渲染配置项并生成 html 文件
+
+        :param path:
+            文件保存路径
+        """
+        _tmp = "local.html"
+        my_option = json_dumps(self._option, indent=4)
+        tmp = template.JINJA2_ENV.get_template(_tmp)
+        script_list = template.produce_html_script_list(self._js_dependencies)
+        html = tmp.render(
+            my_option=my_option,
+            chart_id=self._chart_id,
+            script_list=script_list,
+            page_title=self._page_title,
+            my_width=self.width,
+            my_height=self.height)
+        html = utils.freeze_js(html)
+        utils.write_utf8_html_file(path, html)
+
+    @staticmethod
+    def cast(seq):
+        """ 转换数据序列，将带字典和元组类型的序列转换为 k_lst,v_lst 两个列表
+
+        元组列表
+            [(A1, B1), (A2, B2), ...] --> k_lst[ A[i1, i2...] ], v_lst[ B[i1, i2...] ]
+        字典列表
+            [{A1: B1}, {A2: B2}, ...] --> k_lst[ A[i1, i2...] ], v_lst[ B[i1, i2...] ]
+        字典
+            {A1: B1, A2: B2, ...} -- > k_lst[ A[i1, i2...] ], v_lst[ B[i1, i2...] ]
+
+        :param seq:
+            待转换的序列
+        :return:
+        """
+        k_lst, v_lst = [], []
+        if isinstance(seq, list):
+            for s in seq:
+                try:
+                    if isinstance(s, tuple):
+                        _attr, _value = s
+                        k_lst.append(_attr)
+                        v_lst.append(_value)
+                    elif isinstance(s, dict):
+                        for k, v in s.items():
+                            k_lst.append(k)
+                            v_lst.append(v)
+                except:
+                    raise
+        elif isinstance(seq, dict):
+            for k, v in seq.items():
+                k_lst.append(k)
+                v_lst.append(v)
+        return k_lst, v_lst
+
+    def _repr_html_(self):
+        """ 渲染配置项并将图形显示在 notebook 中
+        """
+        _tmp = 'notebook.html'
+        dom = self._render_notebook_dom_()
+        component = self._render_notebook_component_()
+        tmp = template.JINJA2_ENV.get_template(_tmp)
+        require_config = template.produce_require_configuration(
+            self._js_dependencies, self._jshost)
+        html = tmp.render(
+            single_chart=component, dom=dom, **require_config)
+        return html
+
+    def _render_notebook_dom_(self):
+        """ 为 notebook 渲染 dom 模板
+        """
+        _tmp = "notebook_dom.html"
+        tmp = template.JINJA2_ENV.get_template(_tmp)
+        component = tmp.render(
+            chart_id=self._chart_id,
+            chart_width=self.width,
+            chart_height=self.height)
+        return component
+
+    def _render_notebook_component_(self):
+        """ 为 notebook 渲染组件模板
+        """
+        _tmp = "notebook_chart_component.html"
+        my_option = json_dumps(self._option, indent=4)
+        tmp = template.JINJA2_ENV.get_template(_tmp)
+        component = tmp.render(
+            my_option=my_option, chart_id=self._chart_id)
+        return component
+
+
+class UnknownTypeEncoder(json.JSONEncoder):
+    """
+    `UnknownTypeEncoder`类用于处理数据的编码，使其能够被正常的序列化
+    """
+    def default(self, obj):
+        if isinstance(obj, (datetime.datetime, datetime.date)):
+            return obj.isoformat()
+        else:
+            try:
+                return obj.astype(float).tolist()
+            except:
+                try:
+                    return obj.astype(str).tolist()
+                except:
+                    return json.JSONEncoder.default(self, obj)
+
+
+def json_dumps(data, indent=0):
+    """ json 序列化编码处理
+
+    :param data: 字典数据
+    :param indent: 缩进量
+    :return:
+    """
+    return json.dumps(data, indent=indent,
+                      cls=UnknownTypeEncoder)
