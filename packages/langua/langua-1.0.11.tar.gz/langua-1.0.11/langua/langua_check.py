5a50f51015956cbc7d2eeea434a35e9d9b69156f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:00 +0000

    Add package langua/1.0.11/langua-1.0.11.tar.gz

---
+++ packages/langua/langua-1.0.11.tar.gz/langua-1.0.11/langua/langua_check.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+import sys
+
+from langua import  Predict
+p = Predict()
+
+print(p.get_lang(u"böcker > philipp richter Trigger Points and Muscle Chains in Osteopathy	REST"))
+print(p.get_lang(u"Ein, zwei, drei, vier"))
+print(p.get_lang(u"தாய்"))
+print(p.get_lang(u"English sentence"))
+
+
+
