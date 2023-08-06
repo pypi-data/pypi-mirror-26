2f4787e7fdb61615861a55562f52abd6150a0857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:36 +0000

    Add package bcompiler/1.2.0/bcompiler-1.2.0.tar.gz

---
+++ packages/bcompiler/bcompiler-1.2.0.tar.gz/bcompiler-1.2.0/bcompiler/analysers/utils.py
@@ -0,0 +1,44 @@
+import logging
+import os
+import sys
+
+from openpyxl import load_workbook
+
+from bcompiler.utils import ROOT_PATH, runtime_config
+
+MASTER_XLSX = os.path.join(ROOT_PATH, runtime_config['MasterForAnalysis']['name'])
+logger = logging.getLogger('bcompiler.compiler')
+
+
+def get_number_of_projects(source_wb) -> int:
+    """
+    Simple helper function to get an accurate number of projects in a master.
+    Also strips out any additional columns that openpyxl thinks exist actively
+    in the spreadsheet.
+
+    Returns an integer.
+    """
+    ws = source_wb.active
+    top_row = next(ws.rows)  # ws.rows produces a "generator"; use next() to get next value
+    top_row = list(top_row)[1:]  # we don't want the first column value
+    top_row = [i.value for i in top_row if i.value is not None]  # list comprehension to remove None values
+    return len(top_row)
+
+
+def projects_in_master(master: str):
+    """
+    Return list of project titles in master.
+    :type str: master
+    :return:
+    """
+    try:
+        wb = load_workbook(master)
+    except FileNotFoundError:
+        logger.critical("Please ensure you specify a master file in the command or use the correctly named"
+                        " master file in your auxiliary directory.")
+        sys.exit(1)
+    ws = wb.active
+    top_row = next(ws.rows)
+    top_row = list(top_row)[1:]
+    top_row = [i.value for i in top_row if i.value is not None]
+    return len(top_row)
