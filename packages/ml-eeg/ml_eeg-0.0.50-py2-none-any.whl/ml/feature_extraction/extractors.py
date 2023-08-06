46c82021eff4f08ba7e6fec15c7855592ee33e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:38 +0000

    Add package ml-eeg/0.0.50/ml_eeg-0.0.50-py2-none-any.whl

---
+++ packages/ml-eeg/ml_eeg-0.0.50-py2-none-any.whl/ml/feature_extraction/extractors.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+import ml.feature_extraction.eeg.windowed_extractor
+import ml.feature_extraction.eeg.raw_extractor
+import ml.feature_extraction.eeg.freq_extractor
+import ml.feature_extraction.eeg.trash_extractor
+
+import ml.feature_extraction.audio.acoustics_extractor
+
+from .. import utils
+
+
+def create(config):
+    params = utils.read_config(config)
+    if params["extraction_method"] == "raw":
+        return ml.feature_extraction.eeg.raw_extractor.RawExtractor(params)
+    elif params["extraction_method"] == "freqs":
+        return ml.feature_extraction.eeg.freq_extractor.FreqExtractor(params)
+    elif params["extraction_method"] == "trash":
+        return ml.feature_extraction.eeg.trash_extractor.TrashExtractor(params)
+    elif params["extraction_method"] == "window_mean":
+        return ml.feature_extraction.eeg.windowed_extractor.WindowedExtractor(params)
+    elif params["extraction_method"] == "wavelets":
+        return ml.feature_extraction.eeg.wavelets_extractor.WaveletsExtractor(params)
+    elif params["extraction_method"] == "acustics":
+        return ml.feature_extraction.audio.acoustics_extractor.AcousticsExtractor(params)
