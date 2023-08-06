a21d6542a126d2d7f46366280c20def1c71ffad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:10 +0000

    Add package python-derrick-springmvc-rigging/0.0.1/python-derrick-springmvc-rigging-0.0.1.tar.gz

---
+++ packages/python-derrick-springmvc-rigging/python-derrick-springmvc-rigging-0.0.1.tar.gz/python-derrick-springmvc-rigging-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-derrick-springmvc-rigging',
+    version='0.0.1',
+    description="derrick rigging for springmvc project",
+    keywords='',
+    author="injektto",
+    author_email="injekt.to@hotmail.com",
+    url="https://github.com/injekt/derrick-springmvc-rigging",
+    py_modules=['derrick'],
+    include_package_data=True,
+    install_requires=[
+        'jinja2',
+        'docopt',
+        'whaaaaat',
+        'pychalk',
+        'simplejson',
+        'setuptools-git'
+    ],
+    entry_points='''
+        [console_scripts]
+        derrick=derrick.derrick:main
+    ''',
+    packages=find_packages()
+)
