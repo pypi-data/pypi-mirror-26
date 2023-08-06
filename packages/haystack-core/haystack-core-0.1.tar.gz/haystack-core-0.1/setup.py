739aa4c09558a2f50a7890ae18d833c3403a5d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:04 +0000

    Add package haystack-core/0.1/haystack-core-0.1.tar.gz

---
+++ packages/haystack-core/haystack-core-0.1.tar.gz/haystack-core-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='haystack-core',
+      version='0.1',
+      description='Core files for haystack CMS',
+      url='http://github.com/storborg/funniest',
+      packages=['haystack-core'],
+      zip_safe=False)
