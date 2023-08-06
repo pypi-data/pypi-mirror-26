1f7f7dd93b66b600cd069490cd213911b9e5f5e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:51 +0000

    Add package libmf/0.4/libmf-0.4.tar.gz

---
+++ packages/libmf/libmf-0.4.tar.gz/libmf-0.4/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup, Extension
+import unittest
+
+
+def gen_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('tests', pattern='*_test.py')
+    return test_suite
+
+
+setup(name='libmf',
+      packages=['libmf'],
+      version='0.4',
+      test_suite='setup.gen_test_suite',
+      description='python bindings to libmf',
+      author='Sam Fox Royston (bindings), Chih-Jen Lin (c++ source)',
+      author_email='sfoxroyston@gmail.com',
+      url='https://github.com/PorkShoulderHolder/python-libmf',
+      ext_modules=[Extension('libmf',
+                             sources=['src_cpp/libmf_interface.cpp', 'src_cpp/mf.cpp', 'src_cpp/mf.h'],
+                             depends=['src_cpp/mf.h'],  
+                             extra_compile_args=['-std=c++11'])
+                   ],
+      keywords=["matrix factorization", "machine learning", "parallelism", "python", "out of core"],
+      )
