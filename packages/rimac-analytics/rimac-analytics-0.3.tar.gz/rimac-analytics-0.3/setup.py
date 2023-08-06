a6e8c5d8c7c5349d56f11e537b3d44625b64b5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:35 +0000

    Add package rimac-analytics/0.3/rimac-analytics-0.3.tar.gz

---
+++ packages/rimac-analytics/rimac-analytics-0.3.tar.gz/rimac-analytics-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='rimac-analytics',
+    version='0.3',
+    description='Rimac Analytics API',
+    author='Diego y Ricardito',
+    author_email='ddce.2005@gmail.com',
+    zip_safe=False,
+    url='https://bitbucket.org/rimac-analytics/rimac-analytics-api',
+    license='MIT',
+    packages=['utils', 'decorators', 'common'],
+    python_requires='>=3'
+)
