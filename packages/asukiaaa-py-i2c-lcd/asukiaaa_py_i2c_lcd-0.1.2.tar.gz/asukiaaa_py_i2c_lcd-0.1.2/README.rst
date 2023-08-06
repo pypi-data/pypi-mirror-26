f97416039288db4c1ffac93739bb916aff7ac36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:52 +0000

    Add package asukiaaa-py-i2c-lcd/0.1.2/asukiaaa_py_i2c_lcd-0.1.2.tar.gz

---
+++ packages/asukiaaa-py-i2c-lcd/asukiaaa_py_i2c_lcd-0.1.2.tar.gz/asukiaaa_py_i2c_lcd-0.1.2/README.rst
@@ -0,0 +1,9 @@
+asukiaaa_py_i2c_lcd
+================
+
+An i2c driver to control lcd from raspberry pi with python.
+
+References
+==========
+
+`raspberry piでLCD（AQM1602）を使い、IPを表示する方法 <http://asukiaaa.blogspot.jp/2016/09/raspberry-pilcdaqm1602ip.html>`_
---
+++ packages/asukiaaa-py-i2c-lcd/asukiaaa_py_i2c_lcd-0.1.2.tar.gz/asukiaaa_py_i2c_lcd-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='asukiaaa_py_i2c_lcd',
+    version='0.1.2',
+    description='An i2c library to control AQM1602',
+    url='https://github.com/asukiaaa/asukiaaa_py_i2c_lcd',
+    author='Asuki Kono',
+    author_email='asukiaaa@gmail.com',
+    license='MIT',
+    keywords='i2c lcd aqm1602 raspberry',
+    packages=[
+        'asukiaaa_py_i2c_lcd',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
+        # 'Programming Language :: Python :: 3.6',
+    ],
+)
