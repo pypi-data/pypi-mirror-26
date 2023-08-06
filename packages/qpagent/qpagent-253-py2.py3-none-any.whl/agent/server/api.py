0c0bbb50ca71795fe8f6c3ccf1f7d96c85305732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:07 +0000

    Add package qpagent/253/qpagent-253-py2.py3-none-any.whl

---
+++ packages/qpagent/qpagent-253-py2.py3-none-any.whl/agent/server/api.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import logging
+
+from bottle import Bottle, run
+from bottle import request
+
+import agent.config as config
+from agent.qualityplatform.api import get_task_by_id
+from agent.queue import TaskQueue
+
+q = TaskQueue(num_workers=5)
+
+app = Bottle()
+logger = logging.getLogger(__name__)
+
+
+def start_server():
+    run(app, host='', port=config.agent['port'])
+
+
+@app.post('/jobs')
+def task():
+    body = request.body
+    agent_task = json.load(body)
+    task_id = agent_task.get('task_id')
+    task = get_task_by_id(task_id)
+    worker_data = json.loads(task.get('params'))
+    worker_type = worker_data.get('worker_type')
+    q.add_task(worker_type, task_id)
+
+
+@app.get('/ping')
+def ping():
+    return json.dumps(True)
---
+++ packages/qpagent/qpagent-253-py2.py3-none-any.whl/agent/worker/base.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import logging
+import time
+
+import agent.qualityplatform.api as server_api
+from agent.consts import TaskStatus
+from agent.exc import WorkerException
+from agent.qualityplatform.api import get_task_by_id
+from agent.util.sentry import Sentry
+
+logger = logging.getLogger(__name__)
+
+sentry = Sentry()
+
+
+class BaseWorker(object):
+    def __init__(self, data):
+        task = get_task_by_id(data)
+        worker_data = json.loads(task.get('params'))
+        self.task_id = data
+        self.task_type = task.get('task_type')
+        self.retry_count = task.get('retry_count')
+        self.data = worker_data
+
+    def start_worker(self):
+        raise NotImplementedError
+
+    def complete(self):
+        server_api.update_task_status(self.task_id, TaskStatus.success.value)
+
+    def run(self):
+        try:
+            server_api.update_task_status(self.task_id, TaskStatus.running.value)
+            return self.start_worker()
+        except Exception as e:
+            logger.error(
+                'got worker exception in run worker task {}: {}'.format(
+                    self.task_id, repr(e))
+            )
+            sentry.client.captureException()
+            server_api.update_task_status(self.task_id, TaskStatus.failed.value)
+            return self.retry()
+
+    def retry(self):
+        count = 0
+        log = ''
+        while count < self.retry_count:
+            try:
+                time.sleep(1)
+                return self.start_worker()
+            except Exception as e:
+                log += 'retry {} time failed: {} | '.format(count, repr(e))
+                logger.warning(
+                    'got exception in retry worker task {} loop {}: {}'.format(
+                        self.task_id, count, e.message))
+                sentry.client.captureException()
+                server_api.update_task_status(self.task_id, TaskStatus.failed.value)
+            count += 1
+            if count == self.retry_count:
+                raise WorkerException(
+                    'retry worker task {} failed {} times : {}'.format(
+                        self.task_id, count, log))
