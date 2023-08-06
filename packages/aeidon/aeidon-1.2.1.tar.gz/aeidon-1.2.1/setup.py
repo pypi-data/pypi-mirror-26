fd6a1c3e2267eb2ecb589c255f90b20c5bd1a1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:51 +0000

    Add package aeidon/1.2.1/aeidon-1.2.1.tar.gz

---
+++ packages/aeidon/aeidon-1.2.1.tar.gz/aeidon-1.2.1/setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name="aeidon",
+      version="1.2.1",
+      packages=["aeidon",],
+      author="Osmo Salomaa",
+      author_email="otsaloma@iki.fi",
+      url="https://otsaloma.io/gaupol/",
+      license="GPLv3",
+      classifiers=["License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                   "Topic :: Multimedia :: Video",
+                   "Programming Language :: Python :: 3",]
+      )
+
