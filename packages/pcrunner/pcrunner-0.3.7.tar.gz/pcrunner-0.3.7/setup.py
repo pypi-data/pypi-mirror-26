859ba10900e2e1af423208d3d2bdae9b3c5cbd4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:10 +0000

    Add package pcrunner/0.3.7/pcrunner-0.3.7.tar.gz

---
+++ packages/pcrunner/pcrunner-0.3.7.tar.gz/pcrunner-0.3.7/setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# setup.py
+# -*- coding: utf-8 -*-
+# vim: ai et ts=4 sw=4 sts=4 fenc=UTF-8 ft=python
+
+import sys
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'PyYAML',
+]
+
+test_requirements = [
+    'pytest',
+    'pytest',
+    'tox',
+]
+
+# Add Python 2.6-specific dependencies
+if sys.version_info[:2] < (2, 7):
+    requirements.append('argparse')
+
+# Add Windows-specific dependencies
+if sys.platform == 'win32':
+    requirements.append('pywin32')
+
+setup(
+    name='pcrunner',
+    version='0.3.7',
+    description='Pcrunner (Passive Checks Runner)',
+    long_description=readme + '\n\n' + history,
+    author='Maarten Diemel',
+    author_email='maarten@maartendiemel.nl',
+    url='https://github.com/maartenq/pcrunner',
+    scripts=['scripts/check_dummy.py'],
+    packages=[
+        'pcrunner',
+    ],
+    package_dir={'pcrunner': 'pcrunner'},
+    entry_points={
+        'console_scripts': [
+            'pcrunner = pcrunner.main:main',
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="ISC license",
+    zip_safe=False,
+    keywords='pcrunner',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Utilities',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+)
