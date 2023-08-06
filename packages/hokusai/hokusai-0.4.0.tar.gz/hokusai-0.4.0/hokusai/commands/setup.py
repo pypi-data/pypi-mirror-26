b0fb8eb4d51c5f34a6233789dc2604b5c21b5c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:28 +0000

    Add package hokusai/0.4.0/hokusai-0.4.0.tar.gz

---
+++ packages/hokusai/hokusai-0.4.0.tar.gz/hokusai-0.4.0/hokusai/commands/setup.py
@@ -0,0 +1,161 @@
+import os
+import urllib
+
+from distutils.dir_util import mkpath
+from collections import OrderedDict
+
+import yaml
+
+from jinja2 import Environment, PackageLoader
+env = Environment(loader=PackageLoader('hokusai', 'templates'))
+
+from hokusai.lib.command import command
+from hokusai.lib.config import config
+from hokusai.services.ecr import ECR
+from hokusai.lib.common import print_green, build_service, build_deployment, YAML_HEADER
+from hokusai.lib.exceptions import HokusaiError
+
+@command
+def setup(aws_account_id, project_type, project_name, aws_ecr_region, port, internal):
+
+  mkpath(os.path.join(os.getcwd(), 'hokusai'))
+
+  config.create(project_name.lower().replace('_', '-'), aws_account_id, aws_ecr_region)
+
+  if project_type == 'ruby-rack':
+    dockerfile = env.get_template("Dockerfile-ruby.j2")
+    base_image = 'ruby:latest'
+    run_command = 'bundle exec rackup'
+    development_command = 'bundle exec rackup'
+    test_command = 'bundle exec rake'
+    runtime_environment = {
+      'development': ["RACK_ENV=development"],
+      'test': ["RACK_ENV=test"],
+      'production': [{'name': 'RACK_ENV', 'value': 'production'}]
+    }
+
+  elif project_type == 'ruby-rails':
+    dockerfile = env.get_template("Dockerfile-rails.j2")
+    base_image = 'ruby:latest'
+    run_command = 'bundle exec rails server'
+    development_command = 'bundle exec rails server'
+    test_command = 'bundle exec rake'
+    runtime_environment = {
+      'development': ["RAILS_ENV=development"],
+      'test': ["RAILS_ENV=test"],
+      'production': [{'name': 'RAILS_ENV', 'value': 'production'},
+                      {'name': 'RAILS_SERVE_STATIC_FILES', 'value': 'true'},
+                      {'name': 'RAILS_LOG_TO_STDOUT', 'value': 'true'}]
+    }
+
+  elif project_type == 'nodejs':
+    dockerfile = env.get_template("Dockerfile-node.j2")
+    base_image = 'node:latest'
+    run_command = 'node index.js'
+    development_command = 'node index.js'
+    test_command = 'npm test'
+    runtime_environment = {
+      'development': ["NODE_ENV=development"],
+      'test': ["NODE_ENV=test"],
+      'production': [{'name': 'NODE_ENV', 'value': 'production'}]
+    }
+
+  elif project_type == 'elixir':
+    dockerfile = env.get_template("Dockerfile-elixir.j2")
+    base_image = 'elixir:latest'
+    run_command = 'mix run --no-halt'
+    development_command = 'mix run'
+    test_command = 'mix test'
+    runtime_environment = {
+      'development': ["MIX_ENV=dev"],
+      'test': ["MIX_ENV=test"],
+      'production': [{'name': 'MIX_ENV', 'value': 'prod'}]
+    }
+
+  elif project_type == 'python-wsgi':
+    dockerfile = env.get_template("Dockerfile-python.j2")
+    base_image = 'python:latest'
+    run_command = "gunicorn -b 0.0.0.0:%s app" % port
+    development_command = "python -m werkzeug.serving -b 0.0.0.0:%s %s" % (port, project_name)
+    test_command = 'python -m unittest discover .'
+    runtime_environment = {
+      'development': ["CONFIG_FILE=config/development.py"],
+      'test': ["CONFIG_FILE=config/test.py"],
+      'production': [{'name': 'CONFIG_FILE', 'value': 'config/production.py'}]
+    }
+
+  with open(os.path.join(os.getcwd(), 'Dockerfile'), 'w') as f:
+    f.write(dockerfile.render(base_image=base_image, command=run_command, target_port=port))
+
+  with open(os.path.join(os.getcwd(), 'hokusai', "common.yml"), 'w') as f:
+    services = {
+      config.project_name: {
+        'build': {
+          'context': '../'
+        }
+      }
+    }
+    data = OrderedDict([
+        ('version', '2'),
+        ('services', services)
+      ])
+    payload = YAML_HEADER + yaml.safe_dump(data, default_flow_style=False)
+    f.write(payload)
+
+  for compose_environment in ['development', 'test']:
+    with open(os.path.join(os.getcwd(), 'hokusai', "%s.yml" % compose_environment), 'w') as f:
+      services = {
+        config.project_name: {
+          'extends': {
+            'file': 'common.yml',
+            'service': config.project_name
+          }
+        }
+      }
+
+      if compose_environment == 'development':
+        services[config.project_name]['command'] = development_command
+        services[config.project_name]['ports'] = ["%s:%s" % (port, port)]
+        services[config.project_name]['volumes'] = ['../:/app']
+
+      if compose_environment == 'test':
+        services[config.project_name]['command'] = test_command
+
+      services[config.project_name]['environment'] = runtime_environment[compose_environment]
+
+      data = OrderedDict([
+        ('version', '2'),
+        ('services', services)
+      ])
+      payload = YAML_HEADER + yaml.safe_dump(data, default_flow_style=False)
+      f.write(payload)
+
+  for remote_environment in ['staging', 'production']:
+    with open(os.path.join(os.getcwd(), 'hokusai', "%s.yml" % remote_environment), 'w') as f:
+      if remote_environment == 'production':
+        replicas = 2
+      else:
+        replicas = 1
+
+      deployment_data = build_deployment(config.project_name,
+                                          "%s:%s" % (config.aws_ecr_registry, remote_environment),
+                                          port, environment=runtime_environment['production'], always_pull=True, replicas=replicas)
+
+      service_data = build_service(config.project_name, port, target_port=port, internal=internal)
+
+      remote_environment_yaml = deployment_data + service_data
+
+      f.write(remote_environment_yaml)
+
+  print_green("Config created in ./hokusai")
+
+  ecr = ECR()
+  if ecr.project_repository_exists():
+    print_green("ECR repository %s found. Skipping creation." % config.project_name)
+    return 0
+
+  if ecr.create_project_repository():
+    print_green("Created ECR repository %s" % config.project_name)
+    return 0
+  else:
+    raise HokusaiError("Could not create ECR repository %s - check your credentials." % config.project_name)
---
+++ packages/hokusai/hokusai-0.4.0.tar.gz/hokusai-0.4.0/hokusai/lib/common.py
@@ -0,0 +1,165 @@
+import os
+import sys
+import signal
+import string
+import random
+import json
+
+from collections import OrderedDict
+
+from subprocess import call, check_call, check_output, Popen, STDOUT
+
+import yaml
+import boto3
+
+from termcolor import cprint
+
+from hokusai.lib.exceptions import CalledProcessError
+
+CONTEXT_SETTINGS = {
+  'terminal_width': 10000,
+  'max_content_width': 10000,
+  'help_option_names': ['-h', '--help']
+}
+
+EXIT_SIGNALS = [signal.SIGHUP, signal.SIGINT, signal.SIGQUIT, signal.SIGPIPE, signal.SIGTERM]
+
+YAML_HEADER = '---\n'
+
+VERBOSE = False
+
+def print_green(msg):
+  cprint(msg, 'green')
+
+def print_red(msg):
+  cprint(msg, 'red')
+
+def set_verbosity(v):
+  global VERBOSE
+  VERBOSE = v
+
+def get_verbosity():
+  global VERBOSE
+  return VERBOSE
+
+def verbose(msg):
+  if VERBOSE: cprint("==> hokusai exec `%s`" % msg, 'yellow')
+  return msg
+
+
+def returncode(command):
+  return call(verbose(command), stderr=STDOUT, shell=True)
+
+def shout(command, print_output=False):
+  if print_output:
+    return check_call(verbose(command), stderr=STDOUT, shell=True)
+  else:
+    return check_output(verbose(command), stderr=STDOUT, shell=True)
+
+def shout_concurrent(commands, print_output=False):
+  if print_output:
+    processes = [Popen(verbose(command), shell=True) for command in commands]
+  else:
+    processes = [Popen(verbose(command), shell=True, stdout=open(os.devnull, 'w'), stderr=STDOUT) for command in commands]
+
+  return_codes = []
+  try:
+    for p in processes:
+      return_codes.append(p.wait())
+  except KeyboardInterrupt:
+    for p in processes:
+      p.terminate()
+      return -1
+
+  for return_code in return_codes:
+    if return_code:
+      return return_code
+
+def k8s_uuid():
+  uuid = []
+  for i in range(0,5):
+    uuid.append(random.choice(string.lowercase))
+  return ''.join(uuid)
+
+def build_deployment(name, image, target_port, layer='application', component='web', environment=None, always_pull=False, replicas=1):
+  container = {
+    'name': "%s-%s" % (name, component),
+    'image': image,
+    'ports': [{'containerPort': target_port}]
+  }
+
+  if layer == 'application':
+    container['envFrom'] = [{'configMapRef': {'name': "%s-environment" % name}}]
+
+  if environment is not None:
+    container['env'] = environment
+
+  if always_pull:
+    container['imagePullPolicy'] = 'Always'
+
+  deployment = OrderedDict([
+    ('apiVersion', 'extensions/v1beta1'),
+    ('kind', 'Deployment'),
+    ('metadata', {'name': "%s-%s" % (name, component)}),
+    ('spec', {
+      'replicas': replicas,
+      'strategy': {
+        'rollingUpdate': {
+          'maxSurge': 1,
+          'maxUnavailable': 0
+        },
+        'type': 'RollingUpdate'
+      },
+      'template': {
+        'metadata': {
+          'labels': {
+            'app': name,
+            'layer': layer,
+            'component': component
+            },
+            'name': "%s-%s" % (name, component),
+            'namespace': 'default'
+          },
+          'spec': {
+            'containers': [container]
+          }
+        }
+      }
+    )
+  ])
+  return YAML_HEADER + yaml.safe_dump(deployment, default_flow_style=False)
+
+def build_service(name, port, layer='application', component='web', target_port=None, internal=False):
+  if target_port is None:
+    target_port = port
+
+  spec = {
+    'ports': [{'port': port, 'targetPort': target_port, 'protocol': 'TCP'}],
+    'selector': {
+      'app': name,
+      'layer': layer,
+      'component': component
+    }
+  }
+
+  if internal:
+    spec['type'] = 'ClusterIP'
+  else:
+    spec['type'] = 'LoadBalancer'
+    spec['sessionAffinity'] = 'None'
+
+  service = OrderedDict([
+    ('apiVersion', 'v1'),
+    ('kind', 'Service'),
+    ('metadata', {
+      'labels': {
+        'app': name,
+        'layer': layer,
+        'component': component
+      },
+      'name': "%s-%s" % (name, component),
+      'namespace': 'default'
+    }),
+    ('spec', spec)
+  ])
+  return YAML_HEADER + yaml.safe_dump(service, default_flow_style=False)
