97ba8b7537faf0ff1fff9c189c7b771204598d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:20 +0000

    Add package marvinhelp/0.1.4/marvinhelp-0.1.4-py3-none-any.whl

---
+++ packages/marvinhelp/marvinhelp-0.1.4-py3-none-any.whl/marvinhelp/marvinhelp.py
@@ -0,0 +1,9 @@
+"""
+Unhelpful advice from a robot 50 billion times smarter than the average person.
+"""
+
+def oddscalc():
+    return "I calculated your chance of survival, but I don't think you'll like it."
+
+def help():
+    return "It gives me a headache trying to think down to your level."
