7259f2cfec4fe8baa2983c03f22c024e154b6928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:28 +0000

    Add package awoxpy/0.6/awoxpy-0.6.zip

---
+++ packages/awoxpy/awoxpy-0.6.zip/awoxpy-0.6/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'awoxpy',
+  packages = ['awoxpy'],
+  version = '0.6',
+  description = 'A library that help you control Awox Smartlight with BTLE protocol',
+  author = 'Mika Benoit',
+  author_email = 'mika.benoit@gmail.com',
+  url = 'https://github.com/Rapace21/AwoxPy',
+  download_url = 'https://github.com/Rapace21/AwoxPy/archive/0.6.tar.gz',
+  keywords = ['awox', 'bluetooth', 'lib'],
+  classifiers = [],
+  include_package_data=True,
+  zip_safe=False,
+)
