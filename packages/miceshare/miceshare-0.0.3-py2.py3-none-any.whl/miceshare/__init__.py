2e933f65177b1b84aed9597b138a119a09a0c31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:12 +0000

    Add package miceshare/0.0.3/miceshare-0.0.3-py2.py3-none-any.whl

---
+++ packages/miceshare/miceshare-0.0.3-py2.py3-none-any.whl/miceshare/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+__author__ = 'unknow'
+__versioninfo__ = (0, 0, 3)
+__version__ = '.'.join(map(str, __versioninfo__))
+__title__ = 'miceshare'
+
+from .hson import hson_data
+from .kaipan import kaipan_data
