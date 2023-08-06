dff6b75d78b5e00efb01069b93784309b77bf5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:18 +0000

    Add package classutils/1.4.0/classutils-1.4.0.tar.gz

---
+++ packages/classutils/classutils-1.4.0.tar.gz/classutils-1.4.0/classutils/__init__.py
@@ -0,0 +1,7 @@
+__version__ = u'1.4.0'
+
+from observer import Observable, ObserverMixIn, ObserverError
+
+from singleton import SingletonType
+
+from json_api_helper import JsonApiPropertiesClass, JsonApiPropertiesClassCreator
