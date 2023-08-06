eadf22f9c9c07d982514f8dea3279fc9bbf28cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:12 +0000

    Add package miceshare/0.0.4/miceshare-0.0.4.tar.gz

---
+++ packages/miceshare/miceshare-0.0.4.tar.gz/miceshare-0.0.4/miceshare/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+__author__ = 'unknow'
+__versioninfo__ = (0, 0, 4)
+__version__ = '.'.join(map(str, __versioninfo__))
+__title__ = 'miceshare'
+
+from .hson import hson_data
+from .kaipan import kaipan_data
