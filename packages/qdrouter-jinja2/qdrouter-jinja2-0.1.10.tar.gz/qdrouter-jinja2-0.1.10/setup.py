39012a0dde2736bf65c56a501a6d285d4508be9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:11 +0000

    Add package qdrouter-jinja2/0.1.10/qdrouter-jinja2-0.1.10.tar.gz

---
+++ packages/qdrouter-jinja2/qdrouter-jinja2-0.1.10.tar.gz/qdrouter-jinja2-0.1.10/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+"""setup.py: setuptools control."""
+
+from setuptools import setup
+from pip.req import parse_requirements
+
+setup(
+    name='qdrouter-jinja2',
+    version='0.1.10',
+    packages=['qdrouterjinja2'],
+    entry_points={
+        "console_scripts": ['qdrouterjinja2 = qdrouterjinja2.qdrouterjinja2:main']
+    },
+    license='Apache 2.0',
+    description='Generate qpid-dispatch config (qdrouter.conf) as Jinja2 template from already'
+                ' installed qpid-dispatch.',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    install_requires=[
+        'argparse'
+    ],
+    url='https://github.com/rh-messaging-qe/qdrouterd-jinja2',
+    author='Dominik Lenoch <dlenoch@redhat.com>, Jakub Stejskal <jstejska@redhat.com>',
+    author_email='dlenoch@redhat.com'
+)
