23a7a5281d5ab9b095c0fb07da5b36e4856758dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:16 +0000

    Add package wsk-input-validator/0.0.5/wsk_input_validator-0.0.5.tar.gz

---
+++ packages/wsk-input-validator/wsk_input_validator-0.0.5.tar.gz/wsk_input_validator-0.0.5/wsk_input_validator/__init__.py
@@ -0,0 +1,27 @@
+import os.path
+import yaml
+
+def validate(name):
+    def decorator(function):
+        def wrapper(input):
+            type_map = {
+                "String" : ["str","string","String"],
+                "Integer": ["int","Integer","integer"],
+                "Object": ["dict", "Dictionary", "dictionary", "Object", "object"],
+                "Array": ["list", "Array", "array"],
+                "Boolean": ["bool", "Boolean", "boolean"]
+            }
+            if os.path.isfile("annotations.yaml"):
+                f = open("annotations.yaml", "r")
+            elif os.path.isfile("../annotations.yaml"):
+                f = open("../annotations.yaml", "r")
+            else:
+                raise IOError("Missing annotations file")
+            annos = yaml.load(f)
+            for param in annos[name].get("parameters",[]):
+                if input.get(param["name"],None) is not None:
+                    if(type(input[param["name"]]).__name__ not in type_map.get(param["type"],[])):
+                        raise TypeError(param["name"] + " has type " + type(input[param["name"]]).__name__ + " but require " + param["type"])
+            return function(input)
+        return wrapper
+    return decorator
