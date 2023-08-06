0337df1404a82177366ceb18eb290f6d3b06e7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:48 +0000

    Add package fpr-packer/0.2.2a14/fpr_packer-0.2.2a14-py2.py3-none-any.whl

---
+++ packages/fpr-packer/fpr_packer-0.2.2a14-py2.py3-none-any.whl/fpr_packer/pyinfra.py
@@ -0,0 +1,63 @@
+from pyinfra.api import FactBase, operation
+from pyinfra.api.exceptions import OperationError
+from pyinfra.modules import files, server
+
+
+class PythonVersion(FactBase):
+    """get python version"""
+
+    def command(self):
+        return 'cat /usr/local/lib/pyenv/_pyenv/version'
+
+    def process(self, output):
+        return output[0].strip()
+
+
+class NginxVersion(FactBase):
+    '''
+    Returns the nginx version installed.
+    '''
+
+    def command(self):
+        return "nginx -v 2>&1"
+
+    def process(self, output):
+        return str(output[0].split('/')[1].split()[0]).strip()
+
+
+class UwsgiVersion(FactBase):
+    '''
+    Returns the nginx version installed.
+    '''
+
+    def command(self):
+        return "uwsgi --version"
+
+    def process(self, output):
+        return str(output[0]).strip()
+
+
+@operation
+def move(state, host, src_file, dest_file, **kwargs):
+    """Move a file."""
+
+    # no source or destination
+    if src_file is None:
+        raise OperationError('source file not defined')
+    if dest_file is None:
+        raise OperationError('destination file not defined')
+
+    kwargs.pop('sudo', None)
+    server.shell(
+        state, host,
+        # {f"Copy {src} file to {dest}"},
+        f"mv ./packer/{src_file} {dest_file}",
+        sudo=True
+    )
+    files.file(
+        state, host,
+        # {f"Change file {dest} mode"},
+        dest_file,
+        **kwargs,
+        sudo=True
+    )
