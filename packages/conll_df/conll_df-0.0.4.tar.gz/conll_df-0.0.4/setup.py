53cecd5d64b1bb790a00e3f70aa00ec7f28c8f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:02 +0000

    Add package conll_df/0.0.4/conll_df-0.0.4.tar.gz

---
+++ packages/conll_df/conll_df-0.0.4.tar.gz/conll_df-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='conll_df',
+      version='0.0.4',
+      description='CONLL-U to Pandas DataFrame',
+      url='http://github.com/interrogator/conll-df',
+      packages=['conll_df'],
+      author='Daniel McDonald',
+      author_email="mcddjx@gmail.com",
+      license='MIT',
+      keywords=['corpus', 'linguistics', 'nlp', 'treebank', 'parsing', 'conll', 'ud'],
+      install_requires=["pandas>=0.19.2"])
