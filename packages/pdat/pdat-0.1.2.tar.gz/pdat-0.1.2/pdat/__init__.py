695695a557d25173fec84248202cbe9368b3bda2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:03 +0000

    Add package pdat/0.1.2/pdat-0.1.2.tar.gz

---
+++ packages/pdat/pdat-0.1.2.tar.gz/pdat-0.1.2/pdat/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for PulsarDataToolbox."""
+
+from .pdat import psrfits
+
+__author__ = """Jeffrey S Hazboun"""
+__email__ = 'jeffrey.hazboun@gmail.com'
+__version__ = '0.1.2'
