322fe3874c46a2d52401bb3b3587bf3f6cb8eccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:44 +0000

    Add package print_list_items/1.0.1/print_list_items-1.0.1.tar.gz

---
+++ packages/print_list_items/print_list_items-1.0.1.tar.gz/print_list_items-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='print_list_items',
+	version='1.0.1',
+	py_modules=['print_list_items'],
+	author='paul_wang',
+	description='A simple printer of nested lists',
+     )
+
