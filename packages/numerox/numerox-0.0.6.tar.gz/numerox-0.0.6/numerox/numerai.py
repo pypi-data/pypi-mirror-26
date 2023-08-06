924e7e73c571aa147483cf5f363ac019fc0d848c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:21 +0000

    Add package numerox/0.0.6/numerox-0.0.6.tar.gz

---
+++ packages/numerox/numerox-0.0.6.tar.gz/numerox-0.0.6/numerox/numerai.py
@@ -0,0 +1,48 @@
+import requests
+
+API_TOURNAMENT_URL = 'https://api-tournament.numer.ai'
+
+
+def download_dataset(saved_filename, verbose=False):
+    "Download the current Numerai dataset"
+    if verbose:
+        print("Download dataset {}".format(saved_filename))
+    api = Numerai()
+    query = "query {dataset}"
+    url = api.call(query)['data']['dataset']
+    r = requests.get(url)
+    if r.status_code != 200:
+        msg = 'failed to download dataset (staus code {}))'
+        raise IOError(msg.format(r.status_code))
+    with open(saved_filename, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=1024):
+            fd.write(chunk)
+
+
+class Numerai(object):
+
+    def __init__(self, public_id=None, secret_key=None):
+        if public_id and secret_key:
+            self.token = (public_id, secret_key)
+        elif not public_id and not secret_key:
+            self.token = None
+        else:
+            print("You supply both a public id and a secret key.")
+            self.token = None
+
+    def has_token(self):
+        if self.token is not None:
+            return True
+        return False
+
+    def call(self, query, variables=None):
+        body = {'query': query,
+                'variables': variables}
+        headers = {'Content-type': 'application/json',
+                   'Accept': 'application/json'}
+        if self.token:
+            public_id, secret_key = self.token
+            headers['Authorization'] = \
+                'Token {}${}'.format(public_id, secret_key)
+        r = requests.post(API_TOURNAMENT_URL, json=body, headers=headers)
+        return r.json()
