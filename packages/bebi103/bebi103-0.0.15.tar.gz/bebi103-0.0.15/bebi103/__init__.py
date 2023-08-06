efa9e8807afa6ddc423f3703ff50d3ec5c54445a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:18 +0000

    Add package bebi103/0.0.15/bebi103-0.0.15.tar.gz

---
+++ packages/bebi103/bebi103-0.0.15.tar.gz/bebi103-0.0.15/bebi103/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for bebi103."""
+
+from . import viz
+from . import image
+from . import pm
+try:
+    from . import emcee
+except:
+    pass
+
+__author__ = """Justin Bois"""
+__email__ = 'bois@caltech.edu'
+__version__ = '0.0.15'
