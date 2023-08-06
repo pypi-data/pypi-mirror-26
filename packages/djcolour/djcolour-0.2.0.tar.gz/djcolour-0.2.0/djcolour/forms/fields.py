f0956bf7fdd444048d050187e422c13c1d034ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:12 +0000

    Add package djcolour/0.2.0/djcolour-0.2.0.tar.gz

---
+++ packages/djcolour/djcolour-0.2.0.tar.gz/djcolour-0.2.0/djcolour/forms/fields.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms import widgets
+
+from ..base import Color
+
+
+class ColorInput(widgets.Input):
+    input_type = 'color'
+
+    def format_value(self, value):
+        if isinstance(value, Color):
+            value = value.get_hex_l()
+        return super(ColorInput, self).format_value(value)
+
+
+class ColorField(forms.Field):
+    widget = ColorInput
