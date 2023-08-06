ef5ac7fb06ecd0b8e3a1a39d091e40fcd1f85e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:35 +0000

    Add package yanport-client/1.0.0/yanport-client-1.0.0.tar.gz

---
+++ packages/yanport-client/yanport-client-1.0.0.tar.gz/yanport-client-1.0.0/yanport_client/apis/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .agences_api import AgencesApi
+from .annonces_api import AnnoncesApi
+from .biens_api import BiensApi
+from .bienssimilaires_api import BienssimilairesApi
+from .encadrementloyers_api import EncadrementloyersApi
+from .etudes_api import EtudesApi
+from .evenementsadbox_api import EvenementsadboxApi
+from .mandataires_api import MandatairesApi
+from .utilisateurs_api import UtilisateursApi
+from .villes_api import VillesApi
