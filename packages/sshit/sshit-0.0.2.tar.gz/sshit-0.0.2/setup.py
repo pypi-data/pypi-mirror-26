35a937c41b97c45f890dc41f48fc6978100b80a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:57 +0000

    Add package sshit/0.0.2/sshit-0.0.2.tar.gz

---
+++ packages/sshit/sshit-0.0.2.tar.gz/sshit-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='sshit',
+    packages=['src'],
+    version='0.0.2',
+    description='Simple SSH CLI',
+    author='Igor Brites',
+    author_email='igor.brites.87@gmail.com',
+    url='https://github.com/igorbrites/sshit',
+    download_url='https://github.com/igorbrites/sshit/archive/0.0.2.tar.gz',
+    keywords=['ssh', 'cli'],
+    classifiers=[]
+)
