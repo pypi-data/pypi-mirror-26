584dad723b3cec27b8613a0331c105ba576ad34d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:24 +0000

    Add package ib-insync/0.8.10/ib_insync-0.8.10-py3-none-any.whl

---
+++ packages/ib-insync/ib_insync-0.8.10-py3-none-any.whl/ib_insync/__init__.py
@@ -0,0 +1,33 @@
+import sys
+
+if sys.version_info < (3, 6, 0):
+    print("Python 3.6.0 or higher is required")
+    sys.exit()
+
+try:
+    import ibapi
+except ImportError:
+    print('IB API from http://interactivebrokers.github.io is required')
+    sys.exit()
+
+try:
+    from ibapi.common import RealTimeBar
+except ImportError:
+    print('Old version of ibapi module detected. '
+        'The newest version from http://interactivebrokers.github.io is required')
+    sys.exit()
+
+
+from .objects import *
+from .contract import *
+from .order import *
+from .ticker import *
+from .ib import *
+from .client import *
+from .wrapper import *
+from . import util
+
+
+__all__ = ['util']
+for _m in (objects, contract, order, ticker, ib, client, wrapper):
+    __all__ += _m.__all__
