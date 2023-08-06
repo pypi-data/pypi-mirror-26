f689308830d82d376cb24cd5f2dab3cd2e85617b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:10 +0000

    Add package historical/0.1.0/historical-0.1.0-py3-none-any.whl

---
+++ packages/historical/historical-0.1.0-py3-none-any.whl/historical/common/dynamodb.py
@@ -0,0 +1,103 @@
+import json
+import logging
+
+from deepdiff import DeepDiff
+from boto3.dynamodb.types import TypeDeserializer
+
+deser = TypeDeserializer()
+
+
+log = logging.getLogger('historical')
+
+
+def default_diff(latest_config, current_config):
+    """Determine if two revisions have actually changed."""
+    diff = DeepDiff(
+        latest_config,
+        current_config,
+        ignore_order=True
+    )
+    return diff
+
+
+def remove_current_specific_fields(obj):
+    """Remove all fields that belong to the Current table -- that don't belong in the Durable table"""
+    # TTL:
+    if obj.get('ttl'):
+        del obj['ttl']
+    return obj
+
+
+def modify_record(durable_model, current_revision, arn, event_time, diff_func):
+    """Handles a DynamoDB MODIFY event type."""
+    # We want the newest items first.
+    # See: http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Query.html
+    items = list(durable_model.query(
+        arn,
+        eventTime__le=event_time,
+        scan_index_forward=False,
+        limit=1,
+        consistent_read=True))
+
+    if items:
+        latest_revision = items[0]
+
+        latest_config = latest_revision._get_json()[1]['attributes']['configuration']
+        current_config = current_revision._get_json()[1]['attributes']['configuration']
+
+        # Determine if there is truly a difference, disregarding Ephemeral Paths
+        diff = diff_func(latest_config, current_config)
+        if diff:
+            log.debug(
+                'Difference found saving new revision to durable table. Arn: {} LatestConfig: {} CurrentConfig: {}'.format(
+                    arn, json.dumps(latest_config), json.dumps(current_config)
+                ))
+            current_revision.save()
+    else:
+        current_revision.save()
+        log.error('Got modify event but no current revision found. Arn: {arn}'.format(arn=arn))
+
+
+def delete_record(old_image, durable_model):
+    """Handles a DynamoDB DELETE event type."""
+    data = {}
+    for item in old_image:
+        data[item] = deser.deserialize(old_image[item])
+
+    data['configuration'] = {}
+
+    # we give our own timestamps for TTL deletions
+    del data['eventTime']
+    durable_model(**data).save()
+    log.debug('Adding deletion marker.')
+
+
+def process_dynamodb_record(record, durable_model, diff_func=None):
+    """Processes a group of DynamoDB NewImage records."""
+    diff_func = diff_func or default_diff
+
+    arn = record['dynamodb']['Keys']['arn']['S']
+
+    if record['eventName'] in ['INSERT', 'MODIFY']:
+        new = remove_current_specific_fields(record['dynamodb']['NewImage'])
+        data = {}
+
+        for item in new:
+            # this could end up as loss of precision
+            data[item] = deser.deserialize(new[item])
+
+        current_revision = durable_model(**data)
+        if record['eventName'] == 'INSERT':
+            current_revision.save()
+            log.debug('Saving new revision to durable table.')
+
+        elif record['eventName'] == 'MODIFY':
+            modify_record(durable_model, current_revision, arn, data['eventTime'], diff_func)
+
+    if record['eventName'] == 'REMOVE':
+        # only track deletes that are from the dynamodb TTL service
+        if record.get('userIdentity'):
+            if record['userIdentity']['type'] == 'Service':
+                if record['userIdentity']['principalId'] == 'dynamodb.amazonaws.com':
+                    old_image = remove_current_specific_fields(record['dynamodb']['OldImage'])
+                    delete_record(old_image, durable_model)
---
+++ packages/historical/historical-0.1.0-py3-none-any.whl/historical/s3/poller.py
@@ -0,0 +1,110 @@
+"""
+.. module: historical.s3.poller
+    :platform: Unix
+    :copyright: (c) 2017 by Netflix Inc., see AUTHORS for more
+    :license: Apache, see LICENSE for more details.
+.. author:: Mike Grima <mgrima@netflix.com>
+"""
+import os
+import uuid
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+
+from cloudaux.aws.s3 import list_buckets
+
+from raven_python_lambda import RavenLambdaWrapper
+
+from swag_client.backend import SWAGManager
+from swag_client.util import parse_swag_config_options
+
+from historical.constants import CURRENT_REGION, HISTORICAL_ROLE
+from historical.s3.models import s3_polling_schema
+
+logging.basicConfig()
+log = logging.getLogger("historical")
+log.setLevel(logging.INFO)
+
+
+def get_record(all_buckets, index, account):
+    return {
+        "Data": bytes(s3_polling_schema.serialize_me(account, {
+            "bucket_name": all_buckets[index]["Name"],
+            "creation_date": all_buckets[index]["CreationDate"].replace(tzinfo=None, microsecond=0).isoformat() + "Z"
+        }), "utf-8"),
+        "PartitionKey": uuid.uuid4().hex
+    }
+
+
+def create_polling_event(account, stream):
+    # Place onto the S3 Kinesis stream each S3 bucket for each account...
+    # This should probably fan out on an account-by-account basis (we'll need to examine if this is an issue)
+    all_buckets = list_buckets(account_number=account,
+                               assume_role=HISTORICAL_ROLE,
+                               session_name="historical-cloudwatch-s3list",
+                               region=CURRENT_REGION)["Buckets"]
+    client = boto3.client("kinesis", region_name=CURRENT_REGION)
+
+    # Need to add all buckets into the stream:
+    limiter = int(os.environ.get("MAX_BUCKET_BATCH", 50))
+    current_batch = 1
+    total_batch = int(len(all_buckets) / limiter)
+    remainder = len(all_buckets) % limiter
+    offset = 0
+    while current_batch <= total_batch:
+        records = []
+        while offset < (limiter * current_batch):
+            records.append(get_record(all_buckets, offset, account))
+            offset += 1
+
+        client.put_records(Records=records, StreamName=stream)
+        current_batch += 1
+
+    # Process remainder:
+    if remainder:
+        records = []
+        while offset < len(all_buckets):
+            records.append(get_record(all_buckets, offset, account))
+            offset += 1
+
+        client.put_records(Records=records, StreamName=stream)
+
+
+@RavenLambdaWrapper()
+def handler(event, context):
+    """
+    Historical S3 event poller.
+
+    This poller is run at a set interval in order to ensure that changes do not go undetected by historical.
+
+    Historical pollers generate `polling events` which simulate changes. These polling events contain configuration
+    data such as the account/region defining where the collector should attempt to gather data from.
+    """
+    log.debug('Running poller. Configuration: {}'.format(event))
+
+    # Get the queue that we are going to place the events in:
+    if os.environ['SWAG_BUCKET']:
+        swag_opts = {
+            'swag.type': 's3',
+            'swag.bucket_name': os.environ['SWAG_BUCKET'],
+            'swag.data_file': os.environ['SWAG_DATA_FILE'],
+            'swag.region': os.environ['SWAG_REGION'],
+            'swag.cache_expires': 0
+        }
+        swag = SWAGManager(**parse_swag_config_options(swag_opts))
+        accounts = [account["id"] for account in swag.get_all("[?provider=='aws']")]
+    else:
+        accounts = os.environ['ENABLED_ACCOUNTS']
+
+    for account in accounts:
+        # Skip accounts that have role assumption errors:
+        try:
+            create_polling_event(account, os.environ.get("HISTORICAL_STREAM", "HistoricalS3PollerStream"))
+        except ClientError as e:
+            log.warning('Unable to generate events for account. AccountId: {account_id} Reason: {reason}'.format(
+                account_id=account,
+                reason=e
+            ))
+
+    log.debug('Finished generating polling events. Events Created: {}'.format(len(accounts)))
