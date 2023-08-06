fe13569200642206c10fef732c0b355debadec17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:03 +0000

    Add package flytest/1.4.3/flytest-1.4.3.tar.gz

---
+++ packages/flytest/flytest-1.4.3.tar.gz/flytest-1.4.3/flytest.py
@@ -0,0 +1,14 @@
+import sys
+
+"""≤‚ ‘"""
+def print_lol(the_list,indent=False,level=0,fn=sys.stdout):
+    """≤‚"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1,fn)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='',flie=fn)
+            print(each_item,file=fn)
+            
---
+++ packages/flytest/flytest-1.4.3.tar.gz/flytest-1.4.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name		='flytest',
+		version		='1.4.3',
+		py_modules	=['flytest'],
+		author		='flynode',
+		author_emial='test@test.com',
+		url			='http://www.baidu.com',
+		description	='a simple',
+      )
