3c638de793ad2f4c638fea40b48fc66cd2ab9e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:38 +0000

    Add package pypi-task-demo/0.0.1/pypi-task-demo-0.0.1.tar.gz

---
+++ packages/pypi-task-demo/pypi-task-demo-0.0.1.tar.gz/pypi-task-demo-0.0.1/setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+
+NAME = "pypi-task-demo"
+VERSION = "0.0.1"
+
+REQUIRES = ["msrest>=0.2.0", 'mock']
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="PyPI demo",
+    author="Vinod Kumar",    
+    author_email="kuvinod@microsoft.com",
+    url="https://github.com/kuvinodms/vsts-pypi-demo",
+    keywords=["Microsoft", "VSTS", "Team Services", "SDK", "AzureTfs"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    """
+)
+
