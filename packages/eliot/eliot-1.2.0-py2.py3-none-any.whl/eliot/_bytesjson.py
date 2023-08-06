6c9f48831c03f7bad9fb120ddcb4a7edfda241b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:59 +0000

    Add package eliot/1.2.0/eliot-1.2.0-py2.py3-none-any.whl

---
+++ packages/eliot/eliot-1.2.0-py2.py3-none-any.whl/eliot/_bytesjson.py
@@ -0,0 +1,44 @@
+"""
+Python 2/3 JSON encoding/decoding, emulating Python 2's json module.
+
+Python 3 json module doesn't support decoding bytes or encoding. Rather than
+adding isinstance checks in main code path which would slow down Python 2,
+instead we write our encoder that can support those.
+"""
+
+import json as pyjson
+
+from six import PY2
+
+
+class JSONEncoder(pyjson.JSONEncoder):
+    """
+    JSON encoder that supports L{bytes}.
+    """
+
+    def default(self, o):
+        if isinstance(o, bytes):
+            return o.decode("utf-8")
+        return pyjson.JSONEncoder.default(self, o)
+
+
+_encoder = JSONEncoder()
+
+
+def _loads(s):
+    if isinstance(s, bytes):
+        s = s.decode("utf-8")
+    return pyjson.loads(s)
+
+
+def _dumps(obj):
+    return _encoder.encode(obj).encode("utf-8")
+
+
+if PY2:
+    # No need for the above on Python 2
+    loads, dumps = pyjson.loads, pyjson.dumps
+else:
+    loads, dumps = _loads, _dumps
+
+__all__ = ["loads", "dumps"]
