3aeb38a81a8c664af1e6039be745cd462b287ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:53 +0000

    Add package RLE/1.0/RLE-1.0.tar.gz

---
+++ packages/RLE/RLE-1.0.tar.gz/RLE-1.0/RLE/__init__.py
@@ -0,0 +1,2 @@
+#! usr/bin/env python
+from _RLE import RLE
---
+++ packages/RLE/RLE-1.0.tar.gz/RLE-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="RLE",
+    packages=["RLE"],
+    version=1.0,
+    description="RLE - RunLengthEncoding package for data compression",
+    author="Vijayasai",
+    author_email="vijay.sai005@gmail.com",
+    url="https://github.com/Vijaysai005/RLE",
+    download_url="https://github.com/Vijaysai005/RLE/archive/v1.0.tar.gz",
+    keywords=["RLE", "RunLengthEncoding", "data compression"],
+    classifiers=[],
+)
