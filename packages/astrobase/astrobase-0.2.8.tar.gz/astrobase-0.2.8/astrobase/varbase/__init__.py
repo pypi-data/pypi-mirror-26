4f39292ed30ac1acfe68ba2fe0670bb060c07e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:06 +0000

    Add package astrobase/0.2.8/astrobase-0.2.8.tar.gz

---
+++ packages/astrobase/astrobase-0.2.8.tar.gz/astrobase-0.2.8/astrobase/varbase/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''varbase.py - Waqas Bhatti (wbhatti@astro.princeton.edu) - Jan 2016
+
+Contains functions to deal with light curve variability, fitting functions,
+masking signals, calculating nonperiodic and periodic features, autocorrelation,
+etc.
+
+CURRENT SUBMODULES:
+
+varbase.autocorr: calculating the autocorrelation function of light curves
+varbase.features: calculating nonperiodic and periodic variability features
+varbase.lcfit: fitting Fourier series and splines to light curves
+varbase.signals: masking periodic signals, pre-whitening light curves
+
+'''
+
+# there's nothing else here in this top-level module
