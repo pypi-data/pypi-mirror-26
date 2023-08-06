25e014e3d8df799bcf8741e2349ec197e506ca3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:31 +0000

    Add package fluidPlaylists/0.1.4/fluidPlaylists-0.1.4.tar.gz

---
+++ packages/fluidPlaylists/fluidPlaylists-0.1.4.tar.gz/fluidPlaylists-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'fluidPlaylists',
+  packages = ['fluidPlaylists'],
+  version = '0.1.4',
+  description = 'A library to build fluid playlists in Spotify.',
+  author = 'Leonardo Menezes',
+  author_email = 'leonardotmenezes@gmail.com',
+  url = 'https://github.com/LTMenezes/fluid-playlist',
+  download_url = 'https://github.com/LTMenezes/fluid-playlist/archive/master.zip',
+  install_requires=[
+   'bcolors',
+   'matplotlib',
+   'numpy',
+   'scipy',
+   'spotipy'
+  ],
+  keywords = ['fluid', 'playlist', 'spotify'],
+  classifiers = [],
+)
