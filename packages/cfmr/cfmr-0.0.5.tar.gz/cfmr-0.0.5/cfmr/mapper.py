85ef3d548f26c196f1831f0547c613575fc02462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:51 +0000

    Add package cfmr/0.0.5/cfmr-0.0.5.tar.gz

---
+++ packages/cfmr/cfmr-0.0.5.tar.gz/cfmr-0.0.5/cfmr/mapper.py
@@ -0,0 +1,29 @@
+from .emitter import Emitter
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+def handle(event, ctx, mapperFunction):
+    logger.debug('got event{}'.format(event))
+
+    em = Emitter(
+        ctx.client_context.custom['outputBucket'],
+        ctx.client_context.custom['outputPrefix'],
+        ctx.client_context.custom['jobId'],
+        "mapper"
+    )
+
+    mapKey = event['key']
+    inputBucket = ctx.client_context.custom['inputBucket']
+
+    obj = s3.Object(inputBucket, mapKey)
+    dataObj = obj.get()['Body'].read()
+    logger.debug('dataObjType{}'.format(dataObj))
+    mapperResult = mapperFunction(mapKey, dataObj, em)
+    logger.debug('mapperResult{}'.format(mapperResult))
+    em.flushEmits()
+
+    return {
+        'mapperResult': mapperResult
+    }
