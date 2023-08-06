20f89b82c833425aebbf83996968b03e87882515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:35 +0000

    Add package algebraixlib/1.4/algebraixlib-1.4-py3-none-any.whl

---
+++ packages/algebraixlib/algebraixlib-1.4-py3-none-any.whl/algebraixlib/__init__.py
@@ -0,0 +1,31 @@
+r"""The top-level package for the library ``algebraixlib``.
+
+Sub-packages:
+
+-   :mod:`~.algebras`: Modules that represent :term:`algebra`\s and their operations.
+-   :mod:`~.io`: Modules with facilities for importing and exporting data.
+-   :mod:`~.mathobjects`: Modules that define the classes that represent data.
+-   :mod:`~.util`: Miscellaneous utility modules.
+
+Modules:
+
+-   :mod:`~.extension`: Facilities for extending operations from one :term:`algebra` to another.
+-   :mod:`~.partition`: Operations for partitioning :term:`set`\s and :term:`multiset`\s.
+-   :mod:`~.structure`: Representation of the structure of a `MathObject`.
+-   :mod:`~.undef`: Facilities for representing and working with the concept of "undefined".
+"""
+
+# Copyright Algebraix Data Corporation 2015 - 2017
+#
+# This file is part of algebraixlib <http://github.com/AlgebraixData/algebraixlib>.
+#
+# algebraixlib is free software: you can redistribute it and/or modify it under the terms of version
+# 3 of the GNU Lesser General Public License as published by the Free Software Foundation.
+#
+# algebraixlib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with algebraixlib.
+# If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------------------------
