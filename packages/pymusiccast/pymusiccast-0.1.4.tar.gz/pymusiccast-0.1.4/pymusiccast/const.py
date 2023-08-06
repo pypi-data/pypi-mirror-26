b471c2d6a40aa91c8618353fc8ffff3b188819eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:43 +0000

    Add package pymusiccast/0.1.4/pymusiccast-0.1.4.tar.gz

---
+++ packages/pymusiccast/pymusiccast-0.1.4.tar.gz/pymusiccast-0.1.4/pymusiccast/const.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""This file defines constants."""
+
+ENDPOINTS = {
+    "getDeviceInfo": "http://{}/YamahaExtendedControl/v1/system/getDeviceInfo",
+    "getFeatures": "http://{}/YamahaExtendedControl/v1/system/getFeatures",
+    "getLocationInfo": ("http://{}/YamahaExtendedControl"
+                        "/v1/system/getLocationInfo"),
+    "getPlayInfo": "http://{}/YamahaExtendedControl/v1/netusb/getPlayInfo",
+    "setPlayback": "http://{}/YamahaExtendedControl/v1/netusb/setPlayback",
+    "getStatus": "http://{}/YamahaExtendedControl/v1/{}/getStatus",
+    "setInput": "http://{}/YamahaExtendedControl/v1/{}/setInput",
+    "setMute": "http://{}/YamahaExtendedControl/v1/{}/setMute",
+    "setPower": "http://{}/YamahaExtendedControl/v1/{}/setPower",
+    "setVolume": "http://{}/YamahaExtendedControl/v1/{}/setVolume",
+}
