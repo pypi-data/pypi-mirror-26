e49f1963324afd65c07a5c25245ed3cd0915300a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:15 +0000

    Add package taxi-simulator/0.1.1/taxi_simulator-0.1.1.tar.gz

---
+++ packages/taxi-simulator/taxi_simulator-0.1.1.tar.gz/taxi_simulator-0.1.1/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+with open("requirements.txt") as f:
+    requirements = [req for req in f.readlines()]
+
+setup_requirements = [
+    'pytest-runner',
+    # TODO(javipalanca): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    'pytest',
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='taxi_simulator',
+    version='0.1.1',
+    description="Agent-based taxi simulator to test strategies",
+    long_description=readme + '\n\n' + history,
+    author="Javi Palanca",
+    author_email='jpalanca@gmail.com',
+    url='https://github.com/javipalanca/taxi_simulator',
+    packages=find_packages(include=['taxi_simulator']),
+    entry_points={
+        'console_scripts': [
+            'taxi_simulator=taxi_simulator.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='taxi_simulator',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
