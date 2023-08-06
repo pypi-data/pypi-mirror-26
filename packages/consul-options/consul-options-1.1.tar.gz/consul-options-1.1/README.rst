a0351b76210b85f2e03b51074ac50310b7c64b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:44 +0000

    Add package consul-options/1.1/consul-options-1.1.tar.gz

---
+++ packages/consul-options/consul-options-1.1.tar.gz/consul-options-1.1/README.rst
@@ -0,0 +1,143 @@
+consul-options - Define and use your project settings without pain
+===================================================================
+
+.. image:: https://travis-ci.org/Fatal1ty/consul-options.svg?branch=master
+    :target: https://travis-ci.org/Fatal1ty/consul-options
+
+.. image:: https://requires.io/github/Fatal1ty/consul-options/requirements.svg?branch=master
+    :target: https://requires.io/github/Fatal1ty/consul-options/requirements/?branch=master
+    :alt: Requirements Status
+
+.. image:: https://img.shields.io/pypi/v/consul-options.svg
+    :target: https://pypi.python.org/pypi/consul-options
+
+.. image:: https://img.shields.io/pypi/pyversions/consul-options.svg
+    :target: https://pypi.python.org/pypi/consul-options/
+
+.. image:: https://img.shields.io/badge/License-Apache%202.0-blue.svg
+    :target: https://opensource.org/licenses/Apache-2.0
+
+
+How often do you wonder where to store the project settings? When your project is small
+it is not a big problem. But if your big project consists of dozens of microservices
+then there is a problem of centralized configuration management. The Сonsul have a key/value
+storage that is ideal for this.
+
+With **consul-options** you can define and use options in your code by a simple and elegant way.
+Just take a look at the example:
+
+.. code-block:: python
+
+    class DB(ConsulKV):
+        pass
+
+
+    class Users(DB):
+        host = '127.0.0.1'
+        port = 5432
+        user = 'postgres'
+        password = 'postgres'
+
+        dbname = 'users'
+
+
+    class Orders(DB):
+        host = '127.0.0.1'
+        port = 5432
+        user = 'postgres'
+        password = 'postgres'
+
+        dbname = 'orders'
+
+Now you can access option values in a clear way:
+
+.. code-block:: python
+
+    from consul_options import options
+
+    print options.db.users.host
+    print options.db.orders.dbname
+
+**consul-options** automagically creates folders and keys with default values defined above
+in Consul and later read them from there. So if anyone will change the value of *db/orders/host* key
+to something different in Consul then you will get that value.
+
+How project options stored in Consul
+------------------------------------
+
+When you declare a new class based on **ConsulKV** default bahavior is
+creation a *folder* in Consul key/value storage with name of your class in lowercase.
+Each class attribute you define will have mapping to key *folder/key*.
+If you don't agree with generated folder name you are free to choose any other
+with reserved class attribute **__key__** as shown below:
+
+.. code-block:: python
+
+    class WorkerOptions(ConsulKV):
+        __key__ = 'worker'
+
+        host = '127.0.0.1'
+        port = 80
+
+After that you can access to the option with "worker" in path:
+
+.. code-block:: python
+
+    from consul_options import options
+
+    print options.worker.host
+    print options.worker.port
+
+To create hierarchical key structure you can take advantage of usual class hierarchy:
+
+.. code-block:: python
+
+    from consul_options import ConsulKV, options
+
+    class WorkerOptions(ConsulKV):
+        __key__ = 'worker'
+
+        host = '127.0.0.1'
+        port = 80
+
+    class DB(WorkerOptions):
+        host = '127.0.0.1'
+        port = 5432
+        user = 'postgres'
+        password = 'postgres'
+
+    print options.worker.db.host  # 'host'
+    print options.worker.db.port  # 5432
+
+It is also possible to create keys at root level with class attribute **__root__**:
+
+.. code-block:: python
+
+    class RootOptions(ConsulKV):
+        __root__ = True
+
+        host = '127.0.0.1'
+        port = 80
+
+    print options.host
+    print options.port
+
+
+Compatibility
+-------------
+
+consul-options is compatible with both Python 2 and Python 3.
+
+
+Installation
+------------
+
+Use pip to install::
+
+    $ pip install consul-options
+
+
+License
+-------
+
+consul-options is developed and distributed under the Apache 2.0 license.
---
+++ packages/consul-options/consul-options-1.1.tar.gz/consul-options-1.1/consul_options.py
@@ -0,0 +1,184 @@
+# encoding: utf8
+
+from __future__ import absolute_import
+try:
+    from urlparse import urlparse
+except ImportError:
+    from urllib.parse import urlparse
+
+import consul.base
+from consul.std import HTTPClient
+from six import with_metaclass
+
+
+class LazyHTTPClient(HTTPClient):
+    def setup(self, url=None, host=None, port=None, scheme=None):
+        if url:
+            o = urlparse(url)
+            self.host = o.hostname
+            self.port = o.port
+            self.scheme = o.scheme
+            self.base_uri = url
+        else:
+            self.host = host
+            self.port = port
+            self.scheme = scheme
+            self.base_uri = '%s://%s:%s' % (scheme, host, port)
+
+
+class LazyConsul(consul.base.Consul):
+    def connect(self, host, port, scheme, verify=True, cert=None):
+        return LazyHTTPClient(host, port, scheme, verify, cert)
+
+
+class KeyValue(object):
+    __root__ = False
+
+    def __getter__(self, key):
+        raise NotImplementedError
+
+    def __setter__(self, key, value):
+        raise NotImplementedError
+
+    def __getattribute__(self, item):
+        if item in ('__getter__', '__setter__', '__path__') +\
+                tuple(object.__getattribute__(self, '__dict__').keys()):
+            return object.__getattribute__(self, item)
+        value = self.__getter__(item)
+        if value is not None:
+            try:
+                default = object.__getattribute__(self, item)
+                value = str(value.decode('utf8'))  # for compatibility
+                return adapt_value(value, type(default))
+            except AttributeError:
+                return value
+        else:
+            try:
+                value = object.__getattribute__(self, item)
+            except AttributeError:
+                path = self.__path__
+                full_path = '%s/%s' % (path, item) if path else item
+                msg = "There is no such key '%s'" % full_path
+                raise AttributeError(msg)
+            self.__setter__(item, value)
+            return value
+
+    @property
+    def __path__(self):
+        mro = object.__getattribute__(self, '__class__').mro()
+        path = []
+        for base in mro:
+            if getattr(base, '__root__'):
+                break
+            if base in (ConsulKV, CachedConsulKV):
+                break
+            else:
+                try:
+                    name = object.__getattribute__(base, '__key__')
+                except AttributeError:
+                    name = base.__name__.lower()
+                path.append(name)
+        return '/'.join(reversed(path))
+
+
+class ConsulOptionsMeta(type):
+    def __init__(cls, name, bases, clsdict):
+        if name not in ('ConsulKV', 'CachedConsulKV'):
+            path = []
+            for c in cls.mro():
+                if c in (ConsulKV, CachedConsulKV):
+                    break
+                else:
+                    path.append(c)
+            po = ConsulOptions.__global__
+
+            is_root = getattr(cls, '__root__')
+            if is_root:
+                o = cls(po.__consul__)
+                po.__root_options__.append(o)
+            else:
+                for c in reversed(path):
+                    try:
+                        key = object.__getattribute__(c, '__key__')
+                    except AttributeError:
+                        key = object.__getattribute__(c, '__name__').lower()
+                    try:
+                        o = object.__getattribute__(po, key)
+                    except AttributeError:
+                        o = c(ConsulOptions.__global__.__consul__)
+                        object.__setattr__(po, key, o)
+                    object.__setattr__(o, '__key__', key)
+                    po = o
+        super(ConsulOptionsMeta, cls).__init__(name, bases, clsdict)
+
+
+class ConsulKV(with_metaclass(ConsulOptionsMeta, KeyValue)):
+    def __init__(self, server):
+        self.__consul__ = server
+
+    def __getter__(self, key):
+        path = self.__path__
+        path = path + '/' + key if path else key
+        index, data = self.__consul__.kv.get(path)
+        # index, data = None, None
+        value = data['Value'] if data else None
+        return value
+
+    def __setter__(self, key, value):
+        path = self.__path__
+        path = path + '/' + key if path else key
+        self.__consul__.kv.put(path, str(value))
+
+
+class CachedConsulKV(with_metaclass(ConsulOptionsMeta, ConsulKV)):
+    def __init__(self, *args, **kwargs):
+        super(CachedConsulKV, self).__init__(*args, **kwargs)
+        self.__cache__ = {}
+
+    def __getter__(self, key):
+        value = self.__cache__.get(key)
+        if not value:
+            value = super(CachedConsulKV, self).__getter__(key)
+            self.__cache__[key] = value
+        return value
+
+
+class ConsulOptions(object):
+    def __init__(self):
+        self.__consul__ = LazyConsul()
+        self.__root_options__ = []
+
+    def __getattr__(self, key):
+        for root_kv in self.__root_options__:
+            try:
+                value = getattr(root_kv, key)
+                return value
+            except AttributeError:
+                pass
+        else:
+            msg = "There is no such key '%s'" % key
+            raise AttributeError(msg)
+
+
+def adapt_value(value, which_type):
+    if which_type == bool:
+        value = str(value).lower()
+        if value == 'true':
+            return True
+        elif value == 'false':
+            return False
+        else:
+            raise ValueError(value)
+    else:
+        return which_type(value)
+
+
+def setup_consul(url=None, host=None, port=None, scheme=None):
+    options.__consul__.http.setup(url, host, port, scheme)
+
+
+options = ConsulOptions()
+ConsulOptions.__global__ = options
+
+
+__all__ = ['options', 'setup_consul', 'ConsulKV', 'CachedConsulKV']
