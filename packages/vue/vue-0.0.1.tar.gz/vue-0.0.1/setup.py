20d23eefbb64373281b4b9f93e28ab80bc9d8b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:25 +0000

    Add package vue/0.0.1/vue-0.0.1.tar.gz

---
+++ packages/vue/vue-0.0.1.tar.gz/vue-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+from setuptools import setup 
+
+
+setup(
+	name='vue',
+	version='0.0.1',
+	url='http://github.com/',
+	author='titan',
+	author_email='i@qtitan.com',
+	description='vue in python',
+	install_requires=[],
+)
