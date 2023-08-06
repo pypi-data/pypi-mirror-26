623deb0f555d18923dfd453f7c13d3c473ef8272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:16 +0000

    Add package inotify-simple/1.1.4/inotify_simple-1.1.4.tar.gz

---
+++ packages/inotify-simple/inotify_simple-1.1.4.tar.gz/inotify_simple-1.1.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# To upload a version to PyPI, run:
+#    python setup.py sdist upload
+# If the package is not registered with PyPI yet, do so with:
+#     python setup.py register
+
+import os
+from distutils.core import setup
+
+__version__ = '1.1.4'
+
+DESCRIPTION = ("A simple wrapper around inotify. No fancy bells and whistles, " +
+               "just a literal wrapper with ctypes. Only 118 lines of code!")
+
+# Auto generate a __version__ package for the package to import
+with open(os.path.join('inotify_simple', '__version__.py'), 'w') as f:
+    f.write("__version__ = '%s'\n" % __version__)
+
+setup(name='inotify_simple',
+      version=__version__,
+      description=DESCRIPTION,
+      author='Chris Billington',
+      author_email='chrisjbillington@gmail.com',
+      url='https://github.com/chrisjbillington/inotify_simple',
+      license="BSD",
+      packages=["inotify_simple"]
+      )
