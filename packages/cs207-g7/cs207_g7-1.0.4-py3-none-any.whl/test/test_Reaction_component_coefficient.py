90fff4a435075fb698f633a72d3959363cb4eb28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:04 +0000

    Add package cs207-g7/1.0.4/cs207_g7-1.0.4-py3-none-any.whl

---
+++ packages/cs207-g7/cs207_g7-1.0.4-py3-none-any.whl/test/test_Reaction_component_coefficient.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('src')
+
+import chemkin
+
+#Function: reaction_coeff_params
+## testing for type errors 
+
+parsed = chemkin.ReactionParser('test/xml/xml_homework.xml')
+
+def test_reaction_coeff_params_T_text():
+    try:
+        # read src
+        reactions = chemkin.Reaction(parsed, 'panama')
+    except Exception as err:
+        assert(type(err) == TypeError)
+
+def test_reaction_coeff_params_T_negative():
+    try:
+        reactions = chemkin.Reaction(parsed, -10**2)
+    except Exception as err:
+        assert(type(err) == ValueError)
+
+def test_reaction_coeff_params_T_flow():
+    try:
+        reactions = chemkin.Reaction(parsed, float('inf'))
+    except Exception as err:
+        assert(type(err) == ValueError)
