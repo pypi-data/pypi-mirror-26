136f738ebdf7f86483e494ac97ea86697f371cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:29 +0000

    Add package pwclip/1.1.5/pwclip-1.1.5.linux-x86_64.tar.gz

---
+++ packages/pwclip/pwclip-1.1.5.linux-x86_64.tar.gz/usr/local/lib/python3.5/dist-packages/pwclip/lib/colortext/__init__.py
@@ -0,0 +1,6 @@
+"""init of colortext just import all usability functions"""
+from colortext.colortext import \
+    blu, bblu, cya, bcya, gre, \
+    bgre, grn, bgrn, red, bred, \
+    vio, bvio, whi, bwhi, yel, byel, \
+    abort, error, fatal, tabs, tabl, tabd
