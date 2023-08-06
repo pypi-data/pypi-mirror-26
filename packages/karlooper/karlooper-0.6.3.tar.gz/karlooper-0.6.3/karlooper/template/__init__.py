c9c783cca1b9d067b00a7af95b030de1880c02e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:55 +0000

    Add package karlooper/0.6.3/karlooper-0.6.3.tar.gz

---
+++ packages/karlooper/karlooper-0.6.3.tar.gz/karlooper-0.6.3/karlooper/template/__init__.py
@@ -0,0 +1,36 @@
+# -*-coding:utf-8-*-
+
+from jinja2 import Template
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+__author__ = 'karlvorndoenitz@gmail.com'
+
+
+def render(template_path, **kwargs):
+    """read template from file and compile to html content
+
+    :param template_path: template file's path
+    :param kwargs: parameters
+    :return: html content
+
+    """
+    with open(template_path) as template_file:
+        template_data = template_file.read()
+    template = Template(template_data)
+    data = template.render(kwargs)
+    return data
+
+
+def render_string(template_string, **kwargs):
+    """compile template string to html content
+
+    :param template_string: template string
+    :param kwargs: parameters
+    :return: html content
+
+    """
+    template = Template(template_string)
+    return template.render(kwargs)
