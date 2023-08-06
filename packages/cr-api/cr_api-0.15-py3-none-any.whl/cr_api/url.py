0add435a3253f675fb59917b8b08d5acff6e0471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:39 +0000

    Add package cr-api/0.15/cr_api-0.15-py3-none-any.whl

---
+++ packages/cr-api/cr_api-0.15-py3-none-any.whl/cr_api/url.py
@@ -0,0 +1,8 @@
+class APIURL:
+    """
+    API URL
+    """
+    clan = 'http://api.cr-api.com/clan/{}'
+    top_clans = 'http://api.cr-api.com/top/clans'
+    profile = 'http://api.cr-api.com/profile/{}'
+    constants = 'http://api.cr-api.com/constants'
