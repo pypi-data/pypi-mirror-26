978a8dee72a2011771887142a78ceab25db5a728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:35 +0000

    Add package django-postgres-metrics/0.2.0/django-postgres-metrics-0.2.0.tar.gz

---
+++ packages/django-postgres-metrics/django-postgres-metrics-0.2.0.tar.gz/django-postgres-metrics-0.2.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+author = __import__('postgres_metrics').__author__
+version = __import__('postgres_metrics').__version__
+
+setup(
+    name='django-postgres-metrics',
+    version=version,
+    url='https://github.com/django-postgres-metrics/django-postgres-metrics',
+    author=author,
+    author_email='info+django-postgres-stats@markusholtermann.eu',
+    description='',
+    license='BSD',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    install_requires=['Django>=1.11', 'psycopg2'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
