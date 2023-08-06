d6e387de17dc5e7d007a38f198fbed071ab7d553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:20 +0000

    Add package neurodesign/0.1.5/neurodesign-0.1.5.tar.gz

---
+++ packages/neurodesign/neurodesign-0.1.5.tar.gz/neurodesign-0.1.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='neurodesign',
+      version='0.1.05',
+      description='Package for design optimisation for fMRI experiments',
+      author='Joke Durnez',
+      author_email='joke.durnez@gmail.com',
+      license='MIT',
+      packages=['neurodesign'],
+      package_dir={'neurodesign':'src'},
+      package_data={'neurodesign':['media/NeuroDes.png']},
+      zip_safe=False)
