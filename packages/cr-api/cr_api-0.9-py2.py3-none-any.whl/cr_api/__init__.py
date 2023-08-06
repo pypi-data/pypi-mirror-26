60a986a51dfd8b7bebe2738250427b4b8789cca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:03 +0000

    Add package cr-api/0.9/cr_api-0.9-py2.py3-none-any.whl

---
+++ packages/cr-api/cr_api-0.9-py2.py3-none-any.whl/cr_api/__init__.py
@@ -0,0 +1,10 @@
+"""
+Clash Royale wrapper for cr-api.com
+"""
+__version__ = "0.9"
+
+from .client_async import AsyncClient
+from .client import Client
+from .exceptions import APITimeoutError, APIClientResponseError, APIError
+from .url import APIURL
+from .models import Clan, Tag, Player, Constants
