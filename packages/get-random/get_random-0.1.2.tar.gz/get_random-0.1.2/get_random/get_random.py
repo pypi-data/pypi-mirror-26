3764df17f690ee1a75e7eee8819b616e5fe86b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:28 +0000

    Add package get-random/0.1.2/get_random-0.1.2.tar.gz

---
+++ packages/get-random/get_random-0.1.2.tar.gz/get_random-0.1.2/get_random/get_random.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import random
+
+def get_random():
+    return random.random()
+
+def print_random():
+    print(get_random())
+
+if __name__ == '__main__':
+    print_random()
