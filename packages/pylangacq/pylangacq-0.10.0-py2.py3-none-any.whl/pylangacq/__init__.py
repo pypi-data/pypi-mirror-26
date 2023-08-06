14e51317bb20fed9818328f7906a2f65fa69e85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:55 +0000

    Add package pylangacq/0.10.0/pylangacq-0.10.0-py2.py3-none-any.whl

---
+++ packages/pylangacq/pylangacq-0.10.0-py2.py3-none-any.whl/pylangacq/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+from pylangacq.chat import read_chat
+
+
+with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
+    __version__ = f.read().strip()
+
+
+__all__ = ['read_chat']
