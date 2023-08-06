cba1b89c9d3c1181cae47c3664c713f659b936ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:13 +0000

    Add package nimodinst/0.4.0.dev0/nimodinst-0.4.0.dev0.tar.gz

---
+++ packages/nimodinst/nimodinst-0.4.0.dev0.tar.gz/nimodinst-0.4.0.dev0/nimodinst/library_singleton.py
@@ -0,0 +1,54 @@
+# This file was generated
+
+import platform
+
+import ctypes
+from nimodinst import errors
+from nimodinst import library
+import threading
+
+
+_instance = None
+_instance_lock = threading.Lock()
+
+
+def _get_library_name():
+    try:
+        return {'Linux': {'64bit': {'name': 'libnimodinst.so', 'type': 'cdll'}},
+                'Windows': {'32bit': {'name': 'nimodinst.dll', 'type': 'windll'},
+                            '64bit': {'name': 'nimodinst_64.dll', 'type': 'cdll'}}}[platform.system()][platform.architecture()[0]]['name']
+    except KeyError:  # pragma: no cover
+        raise errors.UnsupportedConfigurationError
+
+
+def _get_library_type():
+    try:
+        return {'Linux': {'64bit': {'name': 'libnimodinst.so', 'type': 'cdll'}},
+                'Windows': {'32bit': {'name': 'nimodinst.dll', 'type': 'windll'},
+                            '64bit': {'name': 'nimodinst_64.dll', 'type': 'cdll'}}}[platform.system()][platform.architecture()[0]]['type']
+    except KeyError:  # pragma: no cover
+        raise errors.UnsupportedConfigurationError
+
+
+def get():
+    '''get
+
+    Returns the library.Library singleton for nimodinst.
+    '''
+    global _instance
+    global _instance_lock
+
+    with _instance_lock:
+        if _instance is None:
+            try:
+                library_type = _get_library_type()
+                if library_type == 'windll':
+                    ctypes_library = ctypes.WinDLL(_get_library_name())
+                else:  # pragma: no cover
+                    assert library_type == 'cdll'
+                    ctypes_library = ctypes.CDLL(_get_library_name())
+            except OSError:  # pragma: no cover
+                raise errors.DriverNotInstalledError()
+            _instance = library.Library(ctypes_library)
+        return _instance
+
