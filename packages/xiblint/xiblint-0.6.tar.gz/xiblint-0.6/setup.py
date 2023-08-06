f0a8f49059e7e92dda3db8c148790ac30ae30813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:32 +0000

    Add package xiblint/0.6/xiblint-0.6.tar.gz

---
+++ packages/xiblint/xiblint-0.6.tar.gz/xiblint-0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import xiblint
+
+setup(
+    name='xiblint',
+    version=xiblint.__version__,
+    description='Checks .xib and .storyboard files for compliance with best practices',
+    license='apache2',
+    url='https://github.com/lyft/xiblint',
+    author='Ilya Konstantinov',
+    author_email='ikonstantinov@lyft.com',
+    install_requires=[],
+    packages=find_packages(),
+    entry_points={'console_scripts': ['xiblint=xiblint.__main__:main']},
+)
