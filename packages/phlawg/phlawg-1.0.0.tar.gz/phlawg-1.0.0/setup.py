b89054abe68196b559a7f661100c13620959ffca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:00 +0000

    Add package phlawg/1.0.0/phlawg-1.0.0.tar.gz

---
+++ packages/phlawg/phlawg-1.0.0.tar.gz/phlawg-1.0.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+import os
+
+setup(
+    name = "phlawg",
+    version = "1.0.0",
+    author = "Ethan Rowe",
+    author_email = "ethan@the-rowes.com",
+    description = "python logging utilities for transmitting metrics via log streams",
+    license = "MIT",
+    keywords = "metrics logging configuration setup",
+    url = "https://github.com/ethanrowe/python-phlawg",
+    packages=['phlawg',
+              'phlawg.test',
+    ],
+    long_description="python logging utilities for transmitting metrics via log streams",
+    test_suite='phlawg.test',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Utilities",
+    ],
+    install_requires=[
+        'python-json-logger>=0.1.4',
+        'six>=1.9.0'
+    ],
+    tests_require=[
+        'mock',
+        'nose',
+        'six>=1.9.0'
+    ]
+)
+
