c02ba4b3fb409f978dfd373a2c927a9706fd0da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:10 +0000

    Add package printlol888/1.2.0/printlol888-1.2.0.tar.gz

---
+++ packages/printlol888/printlol888-1.2.0.tar.gz/printlol888-1.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+     name = "printlol888",
+	 version = "1.2.0",
+	 py_modules = ['printlol888'],
+	 author     = 'lh',
+	 author_email='119662230@qq.com',
+	 url ='https://pypi.python.org',
+	 description = 'a simple printer of test lists',
+
+)
+
