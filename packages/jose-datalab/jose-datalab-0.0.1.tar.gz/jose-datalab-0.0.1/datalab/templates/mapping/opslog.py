772e99cc1f2a3f391fb09e3a4b5c9caaec8e1b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:16 +0000

    Add package jose-datalab/0.0.1/jose-datalab-0.0.1.tar.gz

---
+++ packages/jose-datalab/jose-datalab-0.0.1.tar.gz/jose-datalab-0.0.1/datalab/templates/mapping/opslog.py
@@ -0,0 +1,136 @@
+
+OPSLOG_MAPPING = {
+    "template" : "opslog-*",
+    "mappings": {
+        "opslog": {
+            "_all": {
+                "enabled": "false"
+            },
+            "properties": {
+                "@timestamp": {
+                    "type": "date"
+                },
+                "envname": {
+                    "type": "text",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 20
+                        }
+                    }
+                },
+                "errlocation": {
+                    "type": "text",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 20
+                        }
+                    }
+                },
+                "errseverity": {
+                    "type": "text",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 20
+                        }
+                    }
+                },
+                "errstack": {
+                    "type": "text",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 20
+                        }
+                    }
+                },
+                "errstackidx": {
+                    "type": "long"
+                },
+                "keywname": {
+                    "type": "text",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 100
+                        }
+                    }
+                },
+                "keywvalue": {
+                    "type": "float"
+                },
+                "loghost": {
+                    "type": "text",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 20
+                        }
+                    }
+                },
+                "logtext": {
+                    "type": "text",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 256
+                        }
+                    }
+                },
+                "module": {
+                    "type": "text",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 20
+                        }
+                    }
+                },
+                "procid": {
+                    "type": "long"
+                },
+                "procname": {
+                    "type": "text",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 20
+                        }
+                    }
+                }
+            }
+        }
+    },
+    "settings": {
+        "index.number_of_shards": "4",
+        "index.number_of_replicas": "0",
+        "index.query.default_field": "logtext",
+        "index.write.wait_for_active_shards": "0"
+    },
+    "aliases": {
+        "opslog": {}
+    }
+}
+
+
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        # ELASTICSEARCH CLIENT PYTHON API BUG put_template doesn't work
+        # from elasticsearch import Elasticsearch
+        # es = Elasticsearch("http://134.171.189.10")
+        # es.put_template(id="opslog", body=OPSLOG_MAPPING)
+        # es.get_template(id="opslog")
+
+        import requests, json
+
+        url = "http://134.171.189.10:9200/_template/opslog"
+        headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+        r = requests.post(url, data=json.dumps(OPSLOG_MAPPING), headers=headers)
+
+
+
+
+
+
