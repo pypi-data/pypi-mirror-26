fffac31ddf0f074d56a933cf4d241b17f617f237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:33 +0000

    Add package Brian2/2.1.1/Brian2-2.1.1-cp27-cp27m-win32.whl

---
+++ packages/Brian2/Brian2-2.1.1-cp27-cp27m-win32.whl/brian2/codegen/runtime/__init__.py
@@ -0,0 +1,21 @@
+'''
+Runtime targets for code generation.
+'''
+# Register the base category before importing the indivial codegen targets with
+# their subcategories
+from brian2.core.preferences import prefs
+prefs.register_preferences('codegen.runtime',
+                           ('Runtime codegen preferences (see subcategories '
+                            'for individual targets)'))
+
+from .numpy_rt import *
+from .weave_rt import *
+from .GSLweave_rt import *
+try:
+    from .cython_rt import *
+except ImportError:
+    pass # todo: raise a warning?
+try:
+    from .GSLcython_rt import *
+except ImportError:
+    pass
