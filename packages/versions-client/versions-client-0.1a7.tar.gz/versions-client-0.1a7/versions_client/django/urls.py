ef345ff10da18e4af9454e14e2b6c37ded2e90a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:59 +0000

    Add package versions-client/0.1a7/versions-client-0.1a7.tar.gz

---
+++ packages/versions-client/versions-client-0.1a7.tar.gz/versions-client-0.1a7/versions_client/django/urls.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from django.conf.urls import url
+
+from .views import versions_view
+
+urlpatterns = [
+    url(r'^versionz$', versions_view, name='prometheus-versions-client'),
+]
