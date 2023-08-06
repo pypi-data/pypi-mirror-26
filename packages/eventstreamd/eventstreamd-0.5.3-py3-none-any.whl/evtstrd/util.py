b8b234d73c94084a87891c9d63baccd32a4707cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:50 +0000

    Add package eventstreamd/0.5.3/eventstreamd-0.5.3-py3-none-any.whl

---
+++ packages/eventstreamd/eventstreamd-0.5.3-py3-none-any.whl/evtstrd/util.py
@@ -0,0 +1,19 @@
+import json
+import logging
+from asyncio.streams import StreamReader
+from typing import Any
+
+from evtstrd.exc import DisconnectedError
+
+
+async def read_json_line(reader: StreamReader) -> Any:
+    while True:
+        line = await reader.readline()
+        if line:
+            logging.debug(f"read line from socket: {line}")
+            try:
+                return json.loads(line.decode("utf-8").strip())
+            except (ValueError, UnicodeDecodeError):
+                logging.warning("invalid JSON received")
+        if reader.at_eof():
+            raise DisconnectedError()
