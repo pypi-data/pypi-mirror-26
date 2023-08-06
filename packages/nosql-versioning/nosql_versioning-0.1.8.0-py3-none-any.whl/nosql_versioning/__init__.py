c7d7af2d6c435aa7532867b12abbed886e8caf0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:44 +0000

    Add package nosql-versioning/0.1.8.0/nosql_versioning-0.1.8.0-py3-none-any.whl

---
+++ packages/nosql-versioning/nosql_versioning-0.1.8.0-py3-none-any.whl/nosql_versioning/__init__.py
@@ -0,0 +1,78 @@
+__version__ = '0.1.8.0'
+
+
+class VersionConflictionException(Exception):
+    def __init__(self, n):
+        super().__init__('Version {} has already been specified'.format(n))
+
+
+def get_version(data):
+    return data.get('_ver')
+
+
+def decode(o):
+    return o
+
+
+def schema(version=get_version, decode=decode):
+    def instance():
+        versions = {}
+        latest = -1
+        staticmethods = {'ver': None, 'methods':[]}
+
+        def update_static_method():
+            nonlocal staticmethods
+            latest_ver = versions[latest]
+            if latest != staticmethods['ver'] and staticmethods['ver']:
+                unset_static_method()
+            staticmethods['ver'] = latest_ver
+            staticmethods['methods'] = []
+            for k, v in latest_ver.__dict__.items():
+                old = getattr(Record, k) if hasattr(Record, k) else None
+                if old is None and isinstance(v, staticmethod):
+                    setattr(Record, k, v)
+                    staticmethods['methods'].append(k)
+
+        def unset_static_method():
+            nonlocal staticmethods
+            for k in staticmethods['methods']:
+                delattr(Record, k)
+
+        def version_descriptor(n=0):
+            def wrapper(cls):
+                if n in versions:
+                    raise VersionConflictionException(n)
+                versions[n] = cls
+                nonlocal latest
+                latest = max(latest, n)
+                update_static_method()
+                return cls
+            return wrapper
+
+        class Record(object):
+            def __init__(self, *args, **kwargs):
+                data = None
+                if len(args) == 1 and len(kwargs) == 0:
+                    data = args[0]
+                elif len(args) == 0 and len(kwargs) == 1:
+                    data = kwargs.get('data')
+                if data:
+                    obj = decode(data)
+                    n = version(obj)
+                    if n is None:
+                        versions[0].migrate(obj)
+                        n = 0
+                    while n < latest:
+                        cls = versions[n + 1]
+                        cls.migrate(obj)
+                        n += 1
+                    self.obj = versions[latest](obj)
+                else:
+                    self.obj = versions[latest](*args, **kwargs)
+
+            def __getattr__(self, name):
+                return self.obj.__getattribute__(name)
+
+        return version_descriptor, Record
+
+    return instance()
---
+++ packages/nosql-versioning/nosql_versioning-0.1.8.0-py3-none-any.whl/tests/test_basic_example.py
@@ -0,0 +1,151 @@
+from .context import schema, VersionConflictionException
+
+import unittest
+import json
+
+
+class RecordTest(unittest.TestCase):
+    def test_only_default_record(self):
+        version, Record = schema(decode=json.loads)
+
+        @version()
+        class Recordv0(object):
+            def __init__(self, jobj):
+                self.value = jobj['value']
+
+        rec = Record('{"_ver": 0, "value": 1}')
+
+        self.assertEqual(1, rec.value)
+
+    def test_2_records_from_different_db_doesnt_conflict(self):
+        version1, Record1 = schema(decode=json.loads)
+
+        @version1()
+        class Record1v0(object):
+            def __init__(self, jobj):
+                self.value = jobj['value']
+
+        rec = Record1('{"_ver": 0, "value": 1}')
+
+        self.assertEqual(1, rec.value)
+
+        version2, Record2 = schema(decode=json.loads)
+
+        @version2()
+        class Record2v0(object):
+            def __init__(self, jobj):
+                self.value = jobj['value']
+
+        rec = Record2('{"_ver": 0, "value": 2}')
+
+        self.assertEqual(2, rec.value)
+
+    def test_raise_exception_if_a_version_specified_twice(self):
+        def wrapper():
+            version, Record = schema(decode=json.loads)
+
+            @version()
+            class Recordv0(object):
+                def __init__(self, jobj):
+                    self.value = jobj['value']
+
+            @version()
+            class Recordv1(object):
+                def __init__(self, jobj):
+                    self.value = jobj['value']
+
+        self.assertRaises(VersionConflictionException, wrapper)
+
+    def test_record_should_migrate_from_0_to_1(self):
+        version, Record = schema(decode=json.loads)
+
+        @version(0)
+        class Recordv0(object):
+            def __init__(self, jobj):
+                self.old_name = jobj['old_name']
+
+            @staticmethod
+            def migrate(jobj):
+                jobj['old_name'] = 42
+
+        @version(1)
+        class Recordv1(object):
+            def __init__(self, jobj):
+                self.new_name = jobj['new_name']
+
+            @staticmethod
+            def migrate(jobj):
+                jobj['new_name'] = jobj['old_name']
+                del jobj['old_name']
+
+        recvna = Record('{"old_name": 1}')
+        recv0 = Record('{"_ver": 0, "old_name": 1}')
+
+        self.assertEqual(1, recv0.new_name)
+        self.assertEqual(42, recvna.new_name)
+
+    def test_record_should_instantiate_the_latest_version(self):
+        version, Record = schema(decode=json.loads)
+
+        @version()
+        class Recordv0(object):
+            def __init__(self, jobj):
+                self.old_name = jobj['old_name']
+
+        @version(1)
+        class Recordv1(object):
+            def __init__(self, jobj=None, value=None):
+                if jobj:
+                    self.new_name = jobj['new_name']
+                else:
+                    self.new_name = value
+
+        rec = Record('{"_ver": 1, "new_name": 1}')
+
+        self.assertEqual(1, rec.new_name)
+
+        rec = Record(value=2)
+
+        self.assertEqual(2, rec.new_name)
+
+    def test_if_no_version_specified(self):
+        version, Record = schema(decode=json.loads)
+
+        @version()
+        class Recordv0(object):
+            def __init__(self, data):
+                self.value = data['value']
+
+            @staticmethod
+            def migrate(data):
+                data['_ver'] = 0
+                data['value'] = 42
+
+        rec = Record('{"value": 1}')
+
+        self.assertEqual(42, rec.value)
+
+    def test_static_method(self):
+        version, Record = schema(decode=json.loads)
+
+        @version(1)
+        class Recordv1(object):
+            def __init__(self, data):
+                self.value = data['value']
+
+            @staticmethod
+            def migrate(data):
+                data['_ver'] = 0
+                data['value'] = 42
+
+            @staticmethod
+            def foo():
+                return 42
+
+        @version(0)
+        class Recordv0(object):
+            pass
+
+        Record(data='{"_ver": 1, "value": 1}')
+
+        self.assertEqual(42, Record.foo())
