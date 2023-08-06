97ed136b1b7e8dc4ac3d82ac95c78054a46364b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:04 +0000

    Add package SMUTHI/0.7.0/SMUTHI-0.7.0-py3-none-any.whl

---
+++ packages/SMUTHI/SMUTHI-0.7.0-py3-none-any.whl/SMUTHI-0.7.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,57 @@
+SMUTHI
+=======================
+SMUTHI stands for 'scattering by multiple particles in thin-film systems'. The software allows to simulate light
+scattering by multiple particles near (or between) planar interfaces. It is based on the T-matrix method for the single
+particle scattering, and on the scattering-matrix method for the propagation through the layered medium.
+
+Target group: Scientists and engineers in the field of optics and optoelectronics.
+
+License: SMUTHI is provided under the MIT license.
+
+Author: Amos Egel. Mail to amos.egel@kit.edu for support or to report a problem.
+
+For a guide how to install and use the software, see the `documentation <http://smuthi.readthedocs.io>`_.
+
+What's new in version 0.7
+--------------------------
+Iterative solver (GMRES), lookup tables and GPU support were added for fast simulations including large particle
+numbers.
+
+What's new in version 0.6
+--------------------------
+Dipole sources are supported as initial field.
+
+What's new in version 0.5
+--------------------------
+Gaussian beams (more precisely: beams with transverse Gaussian footprint) are supported as initial field.
+
+What's new in version 0.4
+--------------------------
+The data structure has been updated to a more consequent object oriented approach, including a PlaneWaveExpansion class
+and a SphericalWaveExpansion class. Smuthi's API is now also `documented <http://smuthi.readthedocs.io>`_.
+
+What's new in version 0.3
+--------------------------
+The software now allows to compute the electric near field. The fields can be plotted as png figure files and as gif
+animations. All generated output can be stored as figure files or as text files. The simulation object can be exported
+as binary file.
+
+What's new in version 0.2.2
+--------------------------
+Finite cylinders were added.
+
+What's new in version 0.2
+--------------------------
+In addition to spherical particles, spheroids can now be selected as scattering particles, too.
+Spheroids are ellipsoidal particles with one axis of rotational symmetry (which is currently fixed
+to be the direction perpendicular to the layer interfaces).
+
+
+Planned updates for future versions
+------------------------------------
+Things to be implemented next:
+
+ - faster (and less memory intense) evaluation of near field
+ - packages for the evaluation of bidirectional scattering distrubtion function and emission efficiency
+
+
---
+++ packages/SMUTHI/SMUTHI-0.7.0-py3-none-any.whl/smuthi/particles.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""Provide class for the representation of scattering particles."""
+
+
+class Particle:
+    """Base class for scattering particles.
+
+    Args:
+        position (list):            Particle position in the format [x, y, z] (length unit)
+        euler_angles (list):        Particle Euler angles in the format [alpha, beta, gamma]
+        refractive_index (complex): Complex refractive index of particle
+        l_max (int):                Maximal multipole degree used for the spherical wave expansion of incoming and
+                                    scattered field
+        m_max (int):                Maximal multipole order used for the spherical wave expansion of incoming and
+                                    scattered field
+    """
+    def __init__(self, position=None, euler_angles=None, refractive_index=1+0j, l_max=None, m_max=None):
+        if position is None:
+            self.position = [0, 0, 0]
+        else:
+            self.position = position
+
+        if euler_angles is None:
+            self.euler_angles = [0, 0, 0]
+        else:
+            self.euler_angles = euler_angles
+
+        self.euler_angles=euler_angles
+        self.refractive_index = refractive_index
+        self.l_max = l_max
+        if m_max is not None:
+            self.m_max = m_max
+        else:
+            self.m_max = l_max
+        self.initial_field = None
+        self.scattered_field = None
+        self.t_matrix = None
+
+
+class Sphere(Particle):
+    """Particle subclass for spheres.
+
+    Args:
+        position (list):            Particle position in the format [x, y, z] (length unit)
+        refractive_index (complex): Complex refractive index of particle
+        radius (float):             Particle radius (length unit)
+        l_max (int):                Maximal multipole degree used for the spherical wave expansion of incoming and
+                                    scattered field
+        m_max (int):                Maximal multipole order used for the spherical wave expansion of incoming and
+                                    scattered field
+        t_matrix_method (dict):     Dictionary containing the parameters for the algorithm to compute the T-matrix
+    """
+    def __init__(self, position=None, refractive_index=1+0j, radius=1, l_max=None, m_max=None):
+
+        if position is None:
+            self.position = [0, 0, 0]
+        else:
+            self.position = position
+
+        Particle.__init__(self, position=position, refractive_index=refractive_index, l_max=l_max, m_max=m_max)
+        self.radius = radius
+
+
+class Spheroid(Particle):
+    """Particle subclass for spheroids.
+
+    Args:
+        position (list):            Particle position in the format [x, y, z] (length unit)
+        refractive_index (complex): Complex refractive index of particle
+        semi_axis_c (float):        Spheroid half axis in direction of axis of revolution (z-axis if not rotated)
+        semi_axis_a (float):        Spheroid half axis in lateral direction (x- and y-axis if not rotated)
+        l_max (int):                Maximal multipole degree used for the spherical wave expansion of incoming and
+                                    scattered field
+        m_max (int):                Maximal multipole order used for the spherical wave expansion of incoming and
+                                    scattered field
+        t_matrix_method (dict):     Dictionary containing the parameters for the algorithm to compute the T-matrix
+    """
+    def __init__(self, position=None, euler_angles=None, refractive_index=1+0j, semi_axis_c=1, semi_axis_a=1,
+                 l_max=None, m_max=None, t_matrix_method=None):
+
+        if position is None:
+            self.position = [0, 0, 0]
+        else:
+            self.position = position
+
+        if euler_angles is None:
+            self.euler_angles = [0, 0, 0]
+        else:
+            self.euler_angles = euler_angles
+
+        if t_matrix_method is None:
+            self.t_matrix_method = {}
+        else:
+            self.t_matrix_method = t_matrix_method
+
+        self.semi_axis_c = semi_axis_c
+        self.semi_axis_a = semi_axis_a
+
+        Particle.__init__(self, position=position, euler_angles=euler_angles, refractive_index=refractive_index,
+                          l_max=l_max, m_max=m_max)
+
+
+class FiniteCylinder(Particle):
+    """Particle subclass for finite cylinders.
+
+    Args:
+        position (list):            Particle position in the format [x, y, z] (length unit)
+        refractive_index (complex): Complex refractive index of particle
+        cylinder_radius (float):    Radius of cylinder (length unit)
+        cylinder_height (float):    Height of cylinder, in z-direction if not rotated (length unit)
+        l_max (int):                Maximal multipole degree used for the spherical wave expansion of incoming and
+                                    scattered field
+        m_max (int):                Maximal multipole order used for the spherical wave expansion of incoming and
+                                    scattered field
+    """
+    def __init__(self, position=None, euler_angles=None, refractive_index=1+0j, cylinder_radius=1,
+                 cylinder_height=1, l_max=None, m_max=None, t_matrix_method=None):
+
+        if position is None:
+            self.position = [0, 0, 0]
+        else:
+            self.position = position
+
+        if euler_angles is None:
+            self.euler_angles = [0, 0, 0]
+        else:
+            self.euler_angles = euler_angles
+
+        if t_matrix_method is None:
+            self.t_matrix_method = {}
+        else:
+            self.t_matrix_method = t_matrix_method
+
+        self.cylinder_radius = cylinder_radius
+        self.cylinder_height = cylinder_height
+
+        Particle.__init__(self, position=position, euler_angles=euler_angles, refractive_index=refractive_index,
+                          l_max=l_max, m_max=m_max)
