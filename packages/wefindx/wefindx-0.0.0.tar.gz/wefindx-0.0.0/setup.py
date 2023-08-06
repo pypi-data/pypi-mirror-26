c0b865eaf7ff39f27498e457c7c987eef380ccc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:27 +0000

    Add package wefindx/0.0.0/wefindx-0.0.0.tar.gz

---
+++ packages/wefindx/wefindx-0.0.0.tar.gz/wefindx-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wefindx',
+      version='0.0.0',
+      description='Infinity core engine.',
+      url='http://github.com/wefindx/wefindx',
+      author='Mindey',
+      author_email='mindey@qq.com',
+      license='REASONABLE',
+      packages=[''],
+      zip_safe=False)
