ff9613d5e776a46166902321577658094556887c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:41 +0000

    Add package aiohttp-remotes/0.0.4/aiohttp_remotes-0.0.4-py3-none-any.whl

---
+++ packages/aiohttp-remotes/aiohttp_remotes-0.0.4-py3-none-any.whl/aiohttp_remotes/allowed_hosts.py
@@ -0,0 +1,36 @@
+from aiohttp import web
+
+from .abc import ABC
+
+
+class ANY:
+    def __contains__(self, item):
+        return True
+
+
+class AllowedHosts(ABC):
+
+    def __init__(self, allowed_hosts=None, *, white_paths=()):
+        if allowed_hosts is None:
+            allowed_hosts = {'*'}
+        else:
+            allowed_hosts = set(allowed_hosts)
+
+        if '*' in allowed_hosts:
+            allowed_hosts = ANY()
+
+        self._allowed_hosts = allowed_hosts
+        self._white_paths = set(white_paths)
+
+    async def setup(self, app):
+        app.middlewares.append(self.middleware)
+
+    @web.middleware
+    async def middleware(self, request, handler):
+        if (
+            request.path not in self._white_paths and
+            request.host not in self._allowed_hosts
+        ):
+            await self.raise_error(request)
+
+        return await handler(request)
