12226a1da7106d7a2df76bd0276881a93fb14482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:59 +0000

    Add package datatyping/0.5.0/datatyping-0.5.0.tar.gz

---
+++ packages/datatyping/datatyping-0.5.0.tar.gz/datatyping-0.5.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='datatyping',
+    description='Pythonic type checking',
+    keywords='safe data validation typing type check',
+    url='https://github.com/Zaab1t/datatyping',
+    author='Carl Bordum Hansen',
+    author_email='carl@bordum.dk',
+    version='0.5.0',
+    copyright='Copyright (c) 2017 Carl Bordum Hansen',
+    license='MIT',
+    packages=['datatyping'],
+)
