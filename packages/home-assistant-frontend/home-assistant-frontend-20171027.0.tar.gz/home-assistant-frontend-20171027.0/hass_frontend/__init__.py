622240c20b5b98cfc73178bc3130f203d1d4315d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:34 +0000

    Add package home-assistant-frontend/20171027.0/home-assistant-frontend-20171027.0.tar.gz

---
+++ packages/home-assistant-frontend/home-assistant-frontend-20171027.0.tar.gz/home-assistant-frontend-20171027.0/hass_frontend/__init__.py
@@ -0,0 +1,31 @@
+"""AUTO-GENERATED. DO NOT MODIFY"""
+import os
+
+
+def where():
+    """Return path to the frontend."""
+    return os.path.dirname(__file__)
+
+VERSION = '758c808c32bb2b43977b1e2cf2b76cbe446d6dc6'
+CREATED_AT = 1509080781
+FINGERPRINTS = {
+    "compatibility.js": "1686167ff210e001f063f5c606b2e74b",
+    "core.js": "2a7d01e45187c7d4635da05065b5e54e",
+    "frontend.html": "4a4f657f453e5d115429312c7273ef07",
+    "mdi.html": "9b94bd3829d096322c760057dcd6ec09",
+    "panels/ha-panel-config.html": "cbdf16d4b8db4bd7537c2f742077f677",
+    "panels/ha-panel-dev-event.html": "d409e7ab537d9fe629126d122345279c",
+    "panels/ha-panel-dev-info.html": "45a7d59eccc68fffdece16feaaccc0fb",
+    "panels/ha-panel-dev-mqtt.html": "94b222b013a98583842de3e72d5888c6",
+    "panels/ha-panel-dev-service.html": "422b2c181ee0713fa31d45a64e605baf",
+    "panels/ha-panel-dev-state.html": "7948d3dba058f31517d880df8ed0e857",
+    "panels/ha-panel-dev-template.html": "dc4b19f63a2053197d71a9fb736313f2",
+    "panels/ha-panel-hassio.html": "b46e7619f3c355f872d5370741d89f6a",
+    "panels/ha-panel-history.html": "fe2daac10a14f51fa3eb7d23978df1f7",
+    "panels/ha-panel-iframe.html": "56930204d6e067a3d600cf030f4b34c8",
+    "panels/ha-panel-kiosk.html": "b40aa5cb52dd7675bea744afcf9eebf8",
+    "panels/ha-panel-logbook.html": "771afdcf48dc7e308b0282417d2e02d8",
+    "panels/ha-panel-mailbox.html": "a8cca44ca36553e91565e3c894ea6323",
+    "panels/ha-panel-map.html": "e7c3bc6068e0285c2767ba494471cc06",
+    "panels/ha-panel-shopping-list.html": "d8cfd0ecdb3aa6214c0f6908c34c7141"
+}
