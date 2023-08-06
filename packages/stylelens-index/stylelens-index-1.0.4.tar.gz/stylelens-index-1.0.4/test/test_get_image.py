453b96c2cb32ed629d11cb423085e6844019ccf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:35 +0000

    Add package stylelens-index/1.0.4/stylelens-index-1.0.4.tar.gz

---
+++ packages/stylelens-index/stylelens-index-1.0.4.tar.gz/stylelens-index-1.0.4/test/test_get_image.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+import time
+import stylelens_index
+from stylelens_index.rest import ApiException
+from pprint import pprint
+# create an instance of the API class
+api_instance = stylelens_index.ImageApi()
+body = stylelens_index.Image() # Image | Image object that needs to be added to the db.
+
+try:
+    # Add a new image
+    api_response = api_instance.get_image_by_id('59ffcc08f7f9f7001163df4f')
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling ImageApi->add_image: %s\n" % e)
