771332a3cc2e6ce51d3f6e768d99e8ea1a07cd01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:00 +0000

    Add package gaps-dataset/1.1.3/gaps_dataset-1.1.3.tar.gz

---
+++ packages/gaps-dataset/gaps_dataset-1.1.3.tar.gz/gaps_dataset-1.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='gaps_dataset',
+      version='1.1.3',
+      description='Download script for GAPs deep learning dataset from TU Ilmenau',
+      url='http://www.tu-ilmenau.de/neurob/data-sets-code/gaps/',
+      author='Markus Eisenbach',
+      author_email='markus.eisenbach@tu-ilmenau.de',
+      license='(for academic use only)',
+      packages=['gaps_dataset'],
+	  install_requires=['numpy'],
+      zip_safe=False)
