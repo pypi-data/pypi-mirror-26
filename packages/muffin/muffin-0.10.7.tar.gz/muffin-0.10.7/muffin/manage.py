24c9258b47b313e150c2ee3f6b3df87052bb7647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:31 +0000

    Add package muffin/0.10.7/muffin-0.10.7.tar.gz

---
+++ packages/muffin/muffin-0.10.7.tar.gz/muffin-0.10.7/muffin/manage.py
@@ -0,0 +1,280 @@
+"""CLI Support is here."""
+import argparse
+import asyncio
+import inspect
+import logging
+import multiprocessing
+import os
+import re
+import sys
+from shutil import copy
+
+from muffin import CONFIGURATION_ENVIRON_VARIABLE, __version__
+from muffin.utils import MuffinException
+
+
+PARAM_RE = re.compile(r'^\s+:param (\w+): (.+)$', re.M)
+
+
+class Manager(object):
+
+    """Provide simple interface to make application's commands.
+
+    ::
+
+        @app.manage.command
+        def hello(name, upper=False):
+            '''Say hello!
+
+            :param name: an user name
+            :param upper: to upper string
+
+            '''
+            message = 'Hello %s!' % name
+            if upper:
+                message = message.upper()
+            print(message)
+
+    ::
+
+        $ muffin app hello Mike
+        Hello Mike!
+
+        $ muffin app hello Mike --upper
+        HELLO MIKE!
+
+    """
+
+    def __init__(self, app):
+        """Initialize the commands."""
+        self.app = app
+        self.parser = argparse.ArgumentParser(description="Manage %s" % app.name.capitalize())
+        self.parsers = self.parser.add_subparsers(dest='subparser')
+        self.handlers = dict()
+
+        def shell_ctx():
+            ctx = {'app': app}
+            return ctx
+
+        app.cfg.setdefault('MANAGE_SHELL', shell_ctx)
+
+        @self.command
+        def shell(ipython: bool=True):
+            """Run the application shell.
+
+            :param ipython: Use IPython as shell
+            """
+            app.loop.run_until_complete(app.start())
+
+            banner = '\nInteractive Muffin Shell\n'
+            namespace = app.cfg.MANAGE_SHELL
+            if callable(namespace):
+                namespace = namespace()
+            banner += "Loaded objects: %s" % list(namespace.keys())
+            if ipython:
+                try:
+                    from IPython.terminal.embed import InteractiveShellEmbed
+                    sh = InteractiveShellEmbed(banner1=banner)
+                except ImportError:
+                    pass
+                else:
+                    sh(local_ns=namespace)
+                    return
+
+            from code import interact
+            interact(banner, local=namespace)
+
+            app.loop.run_until_complete(app.finish())
+
+        workers = 1
+        if not app.cfg.DEBUG:
+            workers = multiprocessing.cpu_count()
+
+        @self.command
+        def run(bind: str='127.0.0.1:5000', daemon: bool=False, pid: str=None,
+                reload: bool=self.app.cfg.DEBUG, timeout: int=30, name: str=self.app.name,
+                worker_class: str='muffin.worker.GunicornWorker', workers: int=workers,
+                log_file: str=None, access_logfile: str=self.app.cfg.ACCESS_LOG):
+            """Run the application.
+
+            :param bind: The socket to bind
+            :param daemon: Daemonize the program
+            :param log_file: The Error log file to write to
+            :param name: A base to use with setproctitle for process naming
+            :param pid: A filename to use for the PID file
+            :param reload: Restart workers when code changes
+            :param timeout: Workers silent for more than this many seconds are killed and restarted
+            :param worker_class: The type of workers to use.
+            :param workers: The number of worker processes for handling requests
+
+            """
+            from muffin.worker import GunicornApp
+
+            gapp = GunicornApp(
+                usage="%(prog)s run [OPTIONS]", config=self.app.cfg.CONFIG)
+            gapp.app_uri = app.uri or app
+            gapp.cfg.set('bind', bind)
+            gapp.cfg.set('daemon', daemon)
+            gapp.cfg.set('pidfile', pid)
+            gapp.cfg.set('proc_name', name)
+            gapp.cfg.set('reload', reload)
+            gapp.cfg.set('timeout', timeout)
+            gapp.cfg.set('worker_class', worker_class)
+            if workers:
+                gapp.cfg.set('workers', workers)
+
+            if log_file:
+                gapp.cfg.set('errorlog', log_file)
+            if access_logfile:
+                gapp.cfg.set('accesslog', access_logfile)
+            gapp.run()
+
+        @self.command
+        def collect(destination: str, source: list=app.cfg.STATIC_FOLDERS,
+                    replace=False, symlink=True):
+            """Collect static files from the application and plugins.
+
+            :param destination: Path where static files will be collected.
+            :param replace: Replace existed files
+            :param source: Sources from static files will be copied
+            :param symlink: Create symlinks except file copy
+
+            """
+            sources = dict()
+            for path in source:
+                path = os.path.abspath(path)
+                for root, _, files in os.walk(path):
+                    for f in files:
+                        fpath = os.path.join(root, f)
+                        rpath = os.path.relpath(fpath, path)
+                        if rpath in sources:
+                            app.logger.info('Already collected: %s' % fpath)
+                            continue
+                        sources[rpath] = fpath
+
+            destination = os.path.abspath(destination)
+            if not os.path.exists(destination):
+                raise MuffinException('Destination is not exist: %s' % destination)
+
+            for rpath, fpath in sources.items():
+                dpath = os.path.join(destination, rpath)
+                if fpath == dpath:
+                    continue
+
+                if os.path.exists(dpath):
+                    if not replace or os.path.getmtime(dpath) >= os.path.getmtime(fpath):
+                        continue
+                    os.remove(dpath)
+                ddir = os.path.dirname(dpath)
+                if not os.path.exists(ddir):
+                    os.makedirs(ddir)
+
+                if symlink:
+                    os.symlink(fpath, dpath)
+                    app.logger.info('Linked: %s' % rpath)
+
+                else:
+                    copy(fpath, dpath)
+                    app.logger.info('Copied: %s' % rpath)
+
+    def command(self, func):
+        """Define CLI command."""
+        header = '\n'.join([s for s in (func.__doc__ or '').split('\n')
+                            if not s.strip().startswith(':')])
+        parser = self.parsers.add_parser(func.__name__, description=header)
+        args, vargs, kw, defs, kwargs, kwdefs, anns = inspect.getfullargspec(func)
+        defs = defs or []
+        kwargs_ = dict(zip(args[-len(defs):], defs))
+        docs = dict(PARAM_RE.findall(func.__doc__ or ""))
+
+        for name in args:
+            argname = name.replace('_', '-').lower()
+            arghelp = docs.get(name, '')
+
+            if name not in kwargs_:
+                parser.add_argument(argname, help=arghelp)
+                continue
+
+            value = kwargs_[name]
+
+            if isinstance(value, bool):
+                if value:
+                    parser.add_argument("--no-" + argname, dest=name, action="store_false",
+                                        help="Disable %s" % (arghelp or name).lower())
+                else:
+                    parser.add_argument("--" + argname, dest=name, action="store_true",
+                                        help="Enable %s" % (arghelp or name).lower())
+                continue
+
+            if isinstance(value, list):
+                parser.add_argument("--" + argname, action="append",
+                                    default=value, help=arghelp)
+                continue
+
+            parser.add_argument("--" + argname, type=anns.get(name, type(value)),
+                                default=value, help=arghelp + ' [%s]' % repr(value))
+
+        self.handlers[func.__name__] = func
+        func.parser = parser
+        return func
+
+    def shell(self, func):
+        """Set shell context function."""
+        self.app.cfg.MANAGE_SHELL = func
+
+    def __call__(self, *args, prog=False):
+        """Parse arguments and run handler."""
+        if prog:
+            self.parser.prog = prog
+        if not args:
+            args = sys.argv[1:]
+        args_, _ = self.parser.parse_known_args(args)
+        kwargs = dict(args_._get_kwargs())
+
+        handler = self.handlers.get(kwargs.pop('subparser'))
+        if not handler:
+            self.parser.print_help()
+            sys.exit(1)
+
+        try:
+            res = handler(**kwargs)
+            if asyncio.iscoroutine(res):
+                loop = asyncio.get_event_loop()
+                loop.run_until_complete(res)
+
+            sys.exit(0)
+        except Exception as e:
+            sys.exit(e)
+
+
+def run():
+    """CLI endpoint."""
+    sys.path.insert(0, os.getcwd())
+    logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler()])
+
+    parser = argparse.ArgumentParser(description="Manage Application", add_help=False)
+    parser.add_argument('app', metavar='app',
+                        type=str, help='Application module path')
+    parser.add_argument('--config', type=str, help='Path to configuration.')
+    parser.add_argument('--version', action="version", version=__version__)
+
+    args_, subargs_ = parser.parse_known_args(sys.argv[1:])
+    if args_.config:
+        os.environ[CONFIGURATION_ENVIRON_VARIABLE] = args_.config
+
+    from gunicorn.util import import_app
+
+    app_uri = args_.app
+    if ':' not in app_uri:
+        app_uri += ':app'
+    try:
+        app = import_app(app_uri)
+        app.uri = app_uri
+        app.logger.info('Application is loaded: %s' % app.name)
+    except Exception as exc:
+        logging.exception(exc)
+        raise sys.exit(1)
+
+    app.manage(*subargs_, prog='muffin %s' % args_.app)
+
+# pylama:ignore=C901,W0612,W0703,W0212
---
+++ packages/muffin/muffin-0.10.7.tar.gz/muffin-0.10.7/muffin/urls.py
@@ -0,0 +1,291 @@
+"""URL helpers."""
+import re
+import asyncio
+from random import choice
+from string import printable
+from urllib.parse import unquote
+
+from aiohttp.hdrs import METH_ANY
+from aiohttp.web import AbstractRoute, Resource, StaticRoute as VanilaStaticRoute, UrlDispatcher
+
+
+DYNS_RE = re.compile(r'(\{[^{}]*\})')
+DYNR_RE = re.compile(r'^\{(?P<var>[a-zA-Z][_a-zA-Z0-9]*)(?::(?P<re>.+))*\}$')
+RETYPE = type(re.compile('@'))
+
+
+class RawReResource(Resource):
+
+    """Allow any regexp in routes."""
+
+    def __init__(self, pattern, name=None):
+        """Ensure that the pattern is regexp."""
+        if isinstance(pattern, str):
+            pattern = re.compile(pattern)
+        self._pattern = pattern
+        super().__init__(name=name)
+
+    def get_info(self):
+        """Get the resource's information."""
+        return {'name': self._name, 'pattern': self._pattern}
+
+    def _match(self, path):
+        match = self._pattern.match(path)
+        if match is None:
+            return None
+        return {key: unquote(value) for key, value in match.groupdict('').items()}
+
+    def url(self, *subgroups, query=None, **groups):
+        """Build URL."""
+        parsed = re.sre_parse.parse(self._pattern.pattern)
+        subgroups = {n:str(v) for n, v in enumerate(subgroups, 1)}
+        groups_ = dict(parsed.pattern.groupdict)
+        subgroups.update({
+            groups_[k0]: str(v0)
+            for k0, v0 in groups.items()
+            if k0 in groups_
+        })
+        url = ''.join(str(val) for val in Traverser(parsed, subgroups))
+        return self._append_query(url, query)
+
+    def __repr__(self):
+        """Fix representation."""
+        return "<RawReResource '%s' %s>" % (self.name or '', self._pattern)
+
+
+# TODO: Remove me when aiohttp > 0.21.2 will be relased. See #794
+class StaticResource(Resource):
+
+    def __init__(self, route):
+        super().__init__()
+
+        assert isinstance(route, AbstractRoute), \
+            'Instance of Route class is required, got {!r}'.format(route)
+        self._route = route
+        self._routes.append(route)
+
+    def url(self, **kwargs):
+        return self._route.url(**kwargs)
+
+    def get_info(self):
+        return self._route.get_info()
+
+    def _match(self, path):
+        return self._route.match(path)
+
+    def __len__(self):
+        return 1
+
+    def __iter__(self):
+        yield self._route
+
+
+class ParentResource(Resource):
+
+    def __init__(self, path, *, name=None):
+        super().__init__(name=name)
+        self._path = path.rstrip('/')
+        self.router = UrlDispatcher()
+
+    @asyncio.coroutine
+    def resolve(self, method, path):
+        allowed_methods = set()
+        if not path.startswith(self._path + '/'):
+            return None, allowed_methods
+
+        path = path[len(self._path):]
+
+        for resource in self.router._resources:
+            match_dict, allowed = yield from resource.resolve(method, path)
+            if match_dict is not None:
+                return match_dict, allowed_methods
+            else:
+                allowed_methods |= allowed
+        return None, allowed_methods
+
+    def add_resource(self, path, *, name=None):
+        """Add resource."""
+        return self.router.add_resource(path, name=name)
+
+    def get_info(self):
+        return {'path': self._path}
+
+    def url(self, name=None, **kwargs):
+        if name:
+            return self._path + self.router[name].url(**kwargs)
+        return self._path + '/'
+
+
+class StaticRoute(VanilaStaticRoute):
+
+    """Support multiple static resorces."""
+
+    def match(self, path):
+        """Check for file is exists."""
+        if not path.startswith(self._prefix):
+            return None
+
+        filename = path[self._prefix_len:]
+        try:
+            pp = self._directory.joinpath(filename)
+            if pp.is_dir() or pp.is_file():
+                return {'filename': filename}
+        except (ValueError, FileNotFoundError):
+            return None
+
+
+def routes_register(app, handler, *paths, methods=None, router=None, name=None):
+    """Register routes."""
+    if router is None:
+        router = app.router
+
+    resources = []
+
+    for path in paths:
+
+        # Register any exception to app
+        if isinstance(path, type) and issubclass(path, BaseException):
+            app._error_handlers[path] = handler
+            continue
+
+        # Ensure that names are unique
+        name = str(name or '')
+        rname, rnum = name, 2
+        while rname in router:
+            rname = "%s%d" % (name, rnum)
+            rnum += 1
+
+        path = parse(path)
+        if isinstance(path, RETYPE):
+            resource = RawReResource(path, name=rname)
+            router._reg_resource(resource)
+
+        else:
+            resource = router.add_resource(path, name=rname)
+
+        for method in methods or [METH_ANY]:
+            method = method.upper()
+            resource.add_route(method, handler)
+
+        resources.append(resource)
+
+    return resources
+
+
+def parse(path):
+    """Parse URL path and convert it to regexp if needed."""
+    parsed = re.sre_parse.parse(path)
+    for case, _ in parsed:
+        if case not in (re.sre_parse.LITERAL, re.sre_parse.ANY):
+            break
+    else:
+        return path
+
+    path = path.strip('^$')
+
+    def parse_(match):
+        [part] = match.groups()
+        match = DYNR_RE.match(part)
+        params = match.groupdict()
+        return '(?P<%s>%s)' % (params['var'], params['re'] or '[^{}/]+')
+
+    return re.compile('^%s$' % DYNS_RE.sub(parse_, path))
+
+
+class Traverser:
+
+    """Traverse parsed regexp and build string."""
+
+    literals = set(printable)
+
+    def __init__(self, parsed, groups=None):
+        """Initialize the traverser."""
+        self.parsed = parsed
+        self.groups = groups
+
+    def __iter__(self):
+        """Iterate builded parts."""
+        for state, value in self.parsed:
+            yield from getattr(self, "state_" + str(state).lower(), self.state_default)(value)
+
+    def __next__(self):
+        """Make self as generator."""
+        yield from self
+
+    @staticmethod
+    def state_default(value):
+        """Any unparsed state."""
+        yield ''
+
+    @staticmethod
+    def state_literal(value):
+        """Parse literal."""
+        yield chr(value)
+
+    def state_not_literal(self, value):
+        """Parse not literal."""
+        value = negate = chr(value)
+        while value == negate:
+            value = choice(self.literals)
+        yield value
+
+    def state_max_repeat(self, value):
+        """Parse repeatable parts."""
+        min_, max_, value = value
+        value = [val for val in Traverser(value, self.groups)]
+        if not min_ and max_:
+            for val in value:
+                if isinstance(val, required):
+                    min_ = 1
+                    break
+
+        for val in value * min_:
+            yield val
+
+    state_min_repeat = state_max_repeat
+
+    def state_in(self, value):
+        """Parse ranges."""
+        value = [val for val in Traverser(value, self.groups)]
+        if not value or not value[0]:
+            for val in self.literals - set(value):
+                return (yield val)
+        yield value[0]
+
+    state_branch = state_in
+
+    @staticmethod
+    def state_category(value):
+        """Parse categories."""
+        if value == re.sre_parse.CATEGORY_DIGIT:
+            return (yield '0')
+
+        if value == re.sre_parse.CATEGORY_WORD:
+            return (yield 'x')
+
+    def state_subpattern(self, value):
+        """Parse subpatterns."""
+        num, *_, parsed = value
+        if num in self.groups:
+            return (yield required(self.groups[num]))
+
+        yield from Traverser(parsed, groups=self.groups)
+
+
+class required:
+
+    """Mark string part as required."""
+
+    def __init__(self, string):
+        """Initialize the object."""
+        self.string = string
+
+    def __str__(self):
+        """Return self as string."""
+        return self.string
+
+    def __repr__(self):
+        """String representation."""
+        return "'%s'" % self
+
+#  pylama:ignore=W0212
