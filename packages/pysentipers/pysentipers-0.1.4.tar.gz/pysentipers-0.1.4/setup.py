0e50f4848206abeeaf76d14fbe813b8e09056f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:44 +0000

    Add package pysentipers/0.1.4/pysentipers-0.1.4.tar.gz

---
+++ packages/pysentipers/pysentipers-0.1.4.tar.gz/pysentipers-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pysentipers',
+    version='0.1.4',
+    description='Python API for SentiPers Dataset',
+    author='Javad Zarezadeh',
+    author_email='jayzee1992@gmail.com',
+    packages=['pysentipers'],
+    url='https://github.com/jayzee1992/pySentiPers',
+    keywords='sentipers sentiment analysis',
+    license='GPLv3',
+    python_requires='>=3',
+    install_requires=['beautifulsoup4']
+)
