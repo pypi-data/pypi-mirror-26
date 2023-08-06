ebea2b9445a7d051b0956a6f4acc7be85caecfba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:55 +0000

    Add package clmenu/0.6/clmenu-0.6.tar.gz

---
+++ packages/clmenu/clmenu-0.6.tar.gz/clmenu-0.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='clmenu',
+        packages = ['clmenu'],
+        version = '0.6',
+        description = 'A small tool for making an arrow menu in console applications',
+        author = 'Jordan Patterson',
+        author_email = 'jordanpatterson2398@gmail.com',
+        license='MIT',
+        url = 'https://github.com/jordantdkt/menu',
+        download_url='https://jordantdk@bitbucket.org/jordantdk/menu.git'
+)
