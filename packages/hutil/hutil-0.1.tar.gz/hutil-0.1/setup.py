7f3bf8659f289705b2438565e7ca700d94135876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:51 +0000

    Add package hutil/0.1/hutil-0.1.tar.gz

---
+++ packages/hutil/hutil-0.1.tar.gz/hutil-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hutil',
+    version='0.1',
+    description='Helper utilities',
+    url='http://github.com/hunan-rostomyan/hutil',
+    author='Hunan Rostomyan',
+    author_email='hunan131@gmail.com',
+    license='BSD 3-Clause',
+    packages=['hutil'],
+    zip_safe=False
+)
