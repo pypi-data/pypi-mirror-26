9b5e0eb58f4b290df2d00bd428705fee8ac7aee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:01 +0000

    Add package django-google-cloud-storage-lite/0.9/django-google-cloud-storage-lite-0.9.tar.gz

---
+++ packages/django-google-cloud-storage-lite/django-google-cloud-storage-lite-0.9.tar.gz/django-google-cloud-storage-lite-0.9/gcs_storage/storage.py
@@ -0,0 +1,46 @@
+from django.core.files.base import ContentFile
+from django.core.files.storage import Storage
+from django.conf import settings
+from django.utils.text import slugify
+from google.cloud import storage
+import os, uuid
+
+
+class GoogleCloudStorage(Storage):
+    def __init__(self):
+        bucket = settings.GOOGLE_CLOUD_STORAGE_BUCKET
+        self.client = storage.Client()
+        self.bucket = self.client.bucket(bucket)
+        self.base_url = "https://storage.googleapis.com/%s" % bucket
+
+    def get_available_name(self, name, max_length=None):
+        folderpath = os.path.dirname(name)
+        filepath = os.path.basename(name)
+
+        name, ext = os.path.splitext(filepath)
+
+        if ext:
+            return os.path.join(folderpath, "%s%s" % (slugify(name), ext))
+
+        return os.path.join(folderpath, slugify(name))
+
+    def _open(self, name, mode):
+        blob = self.bucket.get_blob(name)
+        return ContentFile(blob.download_as_string())
+
+    def _save(self, name, content):
+        blob = self.bucket.blob(name)
+        blob.upload_from_file(content.file, size=content.size)
+        return name
+
+    def delete(self, name):
+        self.bucket.delete_blob(name)
+
+    def url(self, name):
+        return self.base_url + "/" + name
+
+    def exists(self, name):
+        return self.bucket.blob(name).exists()
+
+    def size(self, name):
+        return self.bucket.get_blob(name).size
