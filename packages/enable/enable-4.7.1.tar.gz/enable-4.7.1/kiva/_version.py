de7ffc74f23ed26fa4a168a5af032d000b884ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:45 +0000

    Add package enable/4.7.1/enable-4.7.1.tar.gz

---
+++ packages/enable/enable-4.7.1.tar.gz/enable-4.7.1/kiva/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM ENABLE SETUP.PY
+version = '4.7.1'
+full_version = '4.7.1'
+git_revision = '39c9e021'
+is_released = True
+
+if not is_released:
+    version = full_version
