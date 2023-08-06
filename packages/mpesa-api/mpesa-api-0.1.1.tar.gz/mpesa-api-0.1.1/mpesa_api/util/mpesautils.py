270dc38d5b8e3e46f4f6e136da83141f255deafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:17 +0000

    Add package mpesa-api/0.1.1/mpesa-api-0.1.1.tar.gz

---
+++ packages/mpesa-api/mpesa-api-0.1.1.tar.gz/mpesa-api-0.1.1/mpesa_api/util/mpesautils.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+
+from mpesa_api.util.http import get
+
+
+def get_token():
+    """
+    fetch a new token
+    :return:
+    """
+    url = settings.GENERATE_TOKEN_URL
+    headers = {"Authorization": "Basic {}".format(settings.MPESA_APP_AUTHTOKEN)}
+    response = get(url, headers)
+    return response.json()
+
