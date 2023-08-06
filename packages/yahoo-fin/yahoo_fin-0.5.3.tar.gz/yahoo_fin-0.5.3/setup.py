509dcc9ed905680c3527d940dc16f5e03f46f238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:44 +0000

    Add package yahoo-fin/0.5.3/yahoo_fin-0.5.3.tar.gz

---
+++ packages/yahoo-fin/yahoo_fin-0.5.3.tar.gz/yahoo_fin-0.5.3/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='yahoo_fin',
+      version='0.5.3',
+      description='''Easy download of historical stock price data from Yahoo's new look.  Yahoo_fin also provides functionality to scrape income statements, cash flows, balance sheets, and analyst information.''',
+      url='http://theautomatic.net/yahoo_fin-documentation/',
+      author='Andrew Treadway',
+      author_email='opensourcecoder11@gmail.com',
+      license='',
+      packages=['yahoo_fin'],
+      zip_safe=False)
