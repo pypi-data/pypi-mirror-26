44c8a808d5b40ab1c457a7ae6d75bdfd1715879c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:03 +0000

    Add package nesteroneac/1.2.0/nesteroneac-1.2.0.tar.gz

---
+++ packages/nesteroneac/nesteroneac-1.2.0.tar.gz/nesteroneac-1.2.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesteroneac',
+    version = '1.2.0',
+    py_modules = ['nesteroneac'],
+    author = 'hfpython',
+    description = 'learning a simple printer of nested lists from hfp',
+    )
