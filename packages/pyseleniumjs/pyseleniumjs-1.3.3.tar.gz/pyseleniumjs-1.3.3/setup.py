96994ed97e7e54a4776930fb97165e03c0fb6f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:20 +0000

    Add package pyseleniumjs/1.3.3/pyseleniumjs-1.3.3.tar.gz

---
+++ packages/pyseleniumjs/pyseleniumjs-1.3.3.tar.gz/pyseleniumjs-1.3.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyseleniumjs',
+    description='Small library with javascript utilities for official Python selenium bindings.',
+    version='1.3.3',
+    url='https://github.com/neetjn/pyselenium-js',
+    author='John Nolette',
+    author_email='john@neetgroup.net',
+    license='Apache2',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    packages=['pyseleniumjs']
+)
