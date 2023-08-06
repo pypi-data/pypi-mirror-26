688dd0b1069fb4de8075929c633d13cd1ceed602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:32 +0000

    Add package yomo/0.0.1/yomo-0.0.1.tar.gz

---
+++ packages/yomo/yomo-0.0.1.tar.gz/yomo-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from setuptools import setup
+
+setup(name='yomo',
+      version='0.0.1',
+      author='Yohei Shinozuka',
+      author_email='Yohei.Shinozuka@nasa.gov',
+      packages=['yomo'],
+)
---
+++ packages/yomo/yomo-0.0.1.tar.gz/yomo-0.0.1/yomo/__init__.py
@@ -0,0 +1,22 @@
+"""
+=============
+Yo Modules
+=============
+
+Python 3 modules for atmospheric research, especially airborne sunphotometry.
+
+.. moduleauthor:: Yohei Shinozuka
+
+"""
+# from __future__ import division, absolute_import, print_function
+
+from .codes4data import *
+from .codes4figures import *
+
+# __all__ = ['codes4data', 'codes4figures']
+# __all__ += codes4data.__all__
+# __all__ += codes4figures.__all__
+
+# from numpy.testing import Tester
+# test = Tester().test
+# bench = Tester().bench
