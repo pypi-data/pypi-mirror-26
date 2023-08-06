55811d39b918d64230a11c03cba1bec5abe84145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:39 +0000

    Add package shankar-2017/3.6.5/shankar_2017-3.6.5.zip

---
+++ packages/shankar-2017/shankar_2017-3.6.5.zip/shankar_2017-3.6.5/shankar_2017/__init__.py
@@ -0,0 +1,5 @@
+from shankar_2017.add import sradd
+from shankar_2017.subtract import srsub
+from shankar_2017.multiply import srmul
+from shankar_2017.division import srdiv
+from shankar_2017.square import srsq
