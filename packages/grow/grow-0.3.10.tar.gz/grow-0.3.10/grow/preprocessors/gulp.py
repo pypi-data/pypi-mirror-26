9f310fd088a0d655e57388c10e1742f39a33c7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:31 +0000

    Add package grow/0.3.10/grow-0.3.10.tar.gz

---
+++ packages/grow/grow-0.3.10.tar.gz/grow-0.3.10/grow/preprocessors/gulp.py
@@ -0,0 +1,36 @@
+"""Preprocessor for running gulp tasks."""
+
+import os
+import shlex
+import subprocess
+from protorpc import messages
+from grow.common import sdk_utils
+from . import base
+
+
+class Config(messages.Message):
+    build_task = messages.StringField(1, default='build')
+    run_task = messages.StringField(2, default='')
+    command = messages.StringField(3, default='gulp')
+
+
+class GulpPreprocessor(base.BasePreprocessor):
+    KIND = 'gulp'
+    Config = Config
+
+    def run(self, build=True):
+        # Avoid restarting the Gulp subprocess if the preprocessor is
+        # being run as a result of restarting the server.
+        if 'RESTARTED' in os.environ:
+            return
+        args = sdk_utils.get_popen_args(self.pod)
+        task = self.config.build_task if build else self.config.run_task
+        raw_command = '{} {}'.format(self.config.command, task)
+        command = shlex.split(raw_command)
+        process = subprocess.Popen(command, **args)
+        if not build:
+            return
+        code = process.wait()
+        if code != 0:
+            text = 'Failed to run: {}'.format(raw_command)
+            raise base.PreprocessorError(text)
