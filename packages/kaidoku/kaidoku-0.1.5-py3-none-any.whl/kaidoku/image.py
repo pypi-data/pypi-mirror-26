4aad5006b5edebdef05b73733b36fa7412f4b9de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:13 +0000

    Add package kaidoku/0.1.5/kaidoku-0.1.5-py3-none-any.whl

---
+++ packages/kaidoku/kaidoku-0.1.5-py3-none-any.whl/kaidoku/image.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""Modules for generating sudoku figures."""
+import os.path
+import sys
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+def drawimage(s, p, label, size, textcolor, imgfile, mark):
+    import configparser
+    # Read font information from system.ini
+    inifile = configparser.ConfigParser()
+    here = os.path.abspath(os.path.dirname(__file__))
+    inifile.read(os.path.join(here, 'data/system.ini'))
+    font = inifile.get('file', 'font')
+
+    """Draw sudoku image."""
+    scalelist = {'small': 1, 'medium': 1.5, 'large': 2, 'x-large': 3}
+    if size in scalelist:
+        scale = scalelist[size]
+    else:
+        size = 'medium'
+        scale = 1.5
+
+    blankfile = os.path.abspath(
+        os.path.dirname(__file__)) + '/data/blank-' + size
+    if not os.path.isfile(blankfile + '.eps'):
+        blankimage(blankfile, scale)
+    c = Image.open(blankfile + '.eps')
+    draw = ImageDraw.Draw(c)
+    try:
+        tfont = ImageFont.truetype(font, int(16 * scale))
+    except Exception:
+        print ('Font file cannot be loaded: ', font)
+        print ('Rewrite [file] section of font variable in the following file.')
+        print (os.path.join(here, 'data/system.ini'))
+        sys.exit()
+    nfont = ImageFont.truetype(font, int(24 * scale))
+    sfont = ImageFont.truetype(font, int(9 * scale))   
+    draw.text((20 * scale, 10 * scale), label, font=tfont, fill='#000')
+    for i in range(9):
+        for j in range(9):
+            n = int(s[i * 9 + j])
+            if n == 0 and mark:
+                for m in range(9):
+                    if p[i * 9 + j][m] == 1:
+                        draw.text(((16 + j * 28.3 + (m % 3) * 8) * scale,
+                                   (36 + i * 28.3 +
+                                    (m // 3) * 8) * scale), str(m + 1),
+                                  font=sfont, fill=textcolor)
+            if n > 0:
+                draw.text(((20 + j * 28.3) * scale, (36 + i * 28.3)
+                           * scale), str(n), font=nfont, fill=textcolor)
+    c.save(imgfile, 'JPEG', quality=100, optimize=True)
+
+# make sudoku.eps
+
+
+def blankimage(file, scale):
+    """Draw blank image."""
+    import pyx
+    m = 0.4  # margin
+    tm = 1.2  # top margin
+    c = pyx.canvas.canvas()
+    c.stroke(pyx.path.rect(0, 0, (9 + 2 * m) * scale,
+                           (9 + m + tm) * scale), [pyx.color.grey(1)])
+    for i in range(10):
+        if i % 3 == 0:
+            c.stroke(pyx.path.line((i + m) * scale, m * scale,
+                                   (i + m) * scale, (9 + m) * scale),
+                     [pyx.style.linewidth.THIck])
+            c.stroke(pyx.path.line(m * scale, (i + m) * scale, (9 + m)
+                                   * scale, (i + m) * scale),
+                     [pyx.style.linewidth.THIck])
+        else:
+            c.stroke(pyx.path.line((i + m) * scale, m *
+                                   scale, (i + m) * scale, (9 + m) * scale))
+            c.stroke(pyx.path.line(m * scale, (i + m) *
+                                   scale, (9 + m) * scale, (i + m) * scale))
+    c.writeEPSfile(file)
