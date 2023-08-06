f02d4ec499c01f9a19f8073971cf648eb695bdef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:30 +0000

    Add package pyoperators/0.13.16/pyoperators-0.13.16.tar.gz

---
+++ packages/pyoperators/pyoperators-0.13.16.tar.gz/pyoperators-0.13.16/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import hooks
+import numpy as np
+import sys
+from numpy.distutils.core import setup
+from numpy.distutils.extension import Extension
+
+VERSION = '0.13'
+
+name = 'pyoperators'
+long_description = open('README.rst').read()
+keywords = 'scientific computing'
+platforms = 'MacOS X,Linux,Solaris,Unix,Windows'
+define_macros = [] if sys.version_info[0] == 2 else [('NPY_PY3K', None)]
+
+ext_modules = [Extension("pyoperators.utils.cythonutils",
+                         sources=["pyoperators/utils/cythonutils.pyx"],
+                         include_dirs=[np.get_include()]),
+               Extension("pyoperators.utils.ufuncs",
+                         sources=["pyoperators/utils/ufuncs.c.src"],
+                         define_macros=define_macros)]
+
+setup(name=name,
+      version=hooks.get_version(name, VERSION),
+      description='Operators and solvers for high-performance computing.',
+      long_description=long_description,
+      url='http://pchanial.github.com/pyoperators',
+      author='Pierre Chanial',
+      author_email='pierre.chanial@gmail.com',
+      maintainer='Pierre Chanial',
+      maintainer_email='pierre.chanial@gmail.com',
+      setup_requires=['numpy'],
+      install_requires=['numpy>=1.6', 'scipy>=0.9'],
+      extras_require = {
+          'fft': ['pyfftw'],
+          'mpi': ['mpi4py'],
+          'nonlinear':  ['numexpr>=2'],
+          'wavelet': ['pywt'],
+      },
+      packages=['pyoperators', 'pyoperators.iterative', 'pyoperators.utils'],
+      platforms=platforms.split(','),
+      keywords=keywords.split(','),
+      cmdclass=hooks.cmdclass,
+      ext_modules=ext_modules,
+      license='CeCILL-B',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: C',
+          'Programming Language :: Cython',
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'Operating System :: OS Independent',
+          'Topic :: Scientific/Engineering'])
---
+++ packages/pyoperators/pyoperators-0.13.16.tar.gz/pyoperators-0.13.16/test/common.py
@@ -0,0 +1,171 @@
+import numpy as np
+import pyoperators
+
+from pyoperators import Operator, flags
+from pyoperators.utils.testing import assert_eq
+
+DTYPES = [np.dtype(t) for t in (np.uint8, np.int8, np.uint16, np.int16,
+          np.uint32, np.int32, np.uint64, np.int64,
+          np.float16, np.float32, np.float64, np.complex64, np.complex128)]
+COMPLEX_DTYPES = [np.dtype(t) for t in (np.complex64, np.complex128)]
+if hasattr(np, 'float128'):
+    DTYPES += [np.dtype(np.float128)]
+if hasattr(np, 'complex256'):
+    DTYPES += [np.dtype(np.complex256)]
+    COMPLEX_DTYPES += [np.dtype(np.complex256)]
+
+
+class ndarray1(np.ndarray):
+    pass
+
+
+class ndarray2(np.ndarray):
+    pass
+
+attr1 = {'attr1': True, 'attr2': True}
+attr2 = {'attr1': False, 'attr3': False}
+
+
+@flags.linear
+class ExplExpl(Operator):
+    def __init__(self, shapein=3, shapeout=4, **keywords):
+        Operator.__init__(self, shapein=shapein, shapeout=shapeout,
+                          classout=ndarray1, attrout=attr1, **keywords)
+
+    def direct(self, input, output):
+        output[0:3] = input
+        output[3] = 10.
+
+
+@flags.linear
+class UncoExpl(Operator):
+    def __init__(self, shapein=3, **keywords):
+        Operator.__init__(self, shapein=shapein, classout=ndarray1,
+                          attrout=attr1, **keywords)
+
+    def direct(self, input, output):
+        output[0:3] = 2*input
+        output[3:] = 20
+
+
+@flags.linear
+class ImplImpl(Operator):
+    def __init__(self, **keywords):
+        Operator.__init__(self, classout=ndarray1, attrout=attr1, **keywords)
+
+    def direct(self, input, output):
+        output[0:input.size] = 3*input
+        output[-1] = 30
+
+    def reshapein(self, shapein):
+        return (shapein[0] + 1,)
+
+    def reshapeout(self, shapeout):
+        return (shapeout[0] - 1,)
+
+
+@flags.linear
+class UncoImpl(Operator):
+    def __init__(self, **keywords):
+        Operator.__init__(self, classout=ndarray1, attrout=attr1, **keywords)
+
+    def direct(self, input, output):
+        output[0:output.size-1] = 4*input
+        output[-1] = 40
+
+    def reshapeout(self, shapeout):
+        return (shapeout[0] - 1,)
+
+
+@flags.linear
+class ExplUnco(Operator):
+    def __init__(self, shapeout=4, **keywords):
+        Operator.__init__(self, shapeout=shapeout, classout=ndarray1,
+                          attrout=attr1, **keywords)
+
+    def direct(self, input, output):
+        output[0:3] = 5*input[0:3]
+        output[3] = 50
+
+
+@flags.linear
+class ImplUnco(Operator):
+    def __init__(self, **keywords):
+        Operator.__init__(self, classout=ndarray1, attrout=attr1, **keywords)
+
+    def direct(self, input, output):
+        output[0:input.size] = 6*input
+        output[-1] = 60
+
+    def reshapein(self, shapein):
+        return (shapein[0] + 1,)
+
+
+@flags.linear
+class UncoUnco(Operator):
+    def __init__(self, **keywords):
+        Operator.__init__(self, classout=ndarray1, attrout=attr1, **keywords)
+
+    def direct(self, input, output):
+        output[0:3] = 7*input[0:3]
+        output[3:] = 70
+
+OPS = ExplExpl, UncoExpl, ImplImpl, UncoImpl, ExplUnco, ImplUnco, UncoUnco
+
+ALL_OPS = [eval('pyoperators.' + op) for op in dir(pyoperators) if op.endswith(
+           'Operator')]
+
+
+@flags.linear
+@flags.square
+class IdentityOutplaceOperator(Operator):
+    def direct(self, input, output):
+        output[...] = input
+
+
+@flags.linear
+@flags.real
+@flags.square
+@flags.symmetric
+class HomothetyOutplaceOperator(Operator):
+    def __init__(self, value, **keywords):
+        Operator.__init__(self, **keywords)
+        self.value = value
+
+    def direct(self, input, output):
+        output[...] = self.value * input
+
+
+@flags.linear
+class Stretch(Operator):
+    """ Stretch input array by replicating it by a factor of 2. """
+    def __init__(self, axis, **keywords):
+        self.axis = axis
+        if self.axis < 0:
+            self.slice = [Ellipsis] + (-self.axis) * [slice(None)]
+        else:
+            self.slice = (self.axis+1) * [slice(None)] + [Ellipsis]
+        Operator.__init__(self, **keywords)
+
+    def direct(self, input, output):
+        self.slice[self.axis] = slice(0, None, 2)
+        output[self.slice] = input
+        self.slice[self.axis] = slice(1, None, 2)
+        output[self.slice] = input
+
+    def reshapein(self, shape):
+        shape_ = list(shape)
+        shape_[self.axis] *= 2
+        return shape_
+
+    def reshapeout(self, shape):
+        shape_ = list(shape)
+        shape_[self.axis] //= 2
+        return shape_
+
+
+def assert_inplace_outplace(op, v, expected):
+    w = op(v)
+    assert_eq(w, expected)
+    op(v, out=w)
+    assert_eq(w, expected)
