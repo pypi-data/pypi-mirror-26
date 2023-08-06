ff184a6186c46ccf99a366f0ad36e0f262c7aa5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:56 +0000

    Add package coop/0.8.0/coop-0.8.0.tar.gz

---
+++ packages/coop/coop-0.8.0.tar.gz/coop-0.8.0/coop/migrations/0001_remove_site.py
@@ -0,0 +1,29 @@
+"""
+Remove the automatically generated site and home page
+"""
+
+from django.db import migrations
+
+
+def remove_site(apps, schema):
+    Site = apps.get_model('wagtailcore', 'site')
+    Page = apps.get_model('wagtailcore', 'page')
+
+    # Remove all the pre-defined sites
+    Site.objects.all().delete()
+
+    # Delete the home page
+    Page.objects.get(pk=2).delete()
+    # Fix the root page to show it has no children
+    Page.objects.filter(pk=1).update(numchild=0)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailcore', '__latest__'),
+    ]
+
+    operations = [
+        migrations.RunPython(remove_site, migrations.RunPython.noop)
+    ]
