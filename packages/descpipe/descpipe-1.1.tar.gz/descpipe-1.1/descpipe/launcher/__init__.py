d6bc06582a94e252c8944dbd51dcf58767686db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:47 +0000

    Add package descpipe/1.1/descpipe-1.1.tar.gz

---
+++ packages/descpipe/descpipe-1.1.tar.gz/descpipe-1.1/descpipe/launcher/__init__.py
@@ -0,0 +1,3 @@
+from .local import LocalDockerLauncher
+from .nersc import NerscSerialLauncher
+from .pegasus import PegasusLauncher
---
+++ packages/descpipe/descpipe-1.1.tar.gz/descpipe-1.1/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='descpipe',
+      version='1.1',
+      scripts=['bin/descpipe'],
+      packages=['descpipe', 'descpipe.launcher'],
+      )
