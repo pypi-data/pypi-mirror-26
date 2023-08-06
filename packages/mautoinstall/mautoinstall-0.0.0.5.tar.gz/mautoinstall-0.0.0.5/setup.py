de81451ff87a90077b83d8b4391e865c4653c84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:49 +0000

    Add package mautoinstall/0.0.0.5/mautoinstall-0.0.0.5.tar.gz

---
+++ packages/mautoinstall/mautoinstall-0.0.0.5.tar.gz/mautoinstall-0.0.0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup( name = "mautoinstall",
+       url = "https://bitbucket.org/mjr129/mautoinstall",
+       version = "0.0.0.5",
+       description = "Auto installer for my apps.",
+       author = "Martin Rusilowicz",
+       license = "GNU AGPLv3",
+       packages = ["mautoinstall"],
+       entry_points = { "mautoinstall": ["mautoinstall = mautoinstall.__main__:main"] },
+       install_requires = ["typing"]
+       )
