7c7f2e11e4e1b4adaf8ccf778e3d8124845929b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:39 +0000

    Add package py-piwik/0.1.0/py-piwik-0.1.0.tar.gz

---
+++ packages/py-piwik/py-piwik-0.1.0.tar.gz/py-piwik-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import requests
+
+version = '0.1.0'
+
+
+def readme():
+    r = requests.post(url='http://c.docverter.com/convert',
+                      data={'to': 'rst', 'from': 'markdown'},
+                      files={'input_files[]': open('README.md', 'rb')})
+    if r.ok:
+        return r.content.encode()
+    else:
+        return 'error converting readme'
+
+
+setup(name='py-piwik',
+      version=version,
+      description='Piwik API Client',
+      # long_description=readme(),
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      keywords='piwik api client',
+      url='https://github.com/adminhead-tech/pypiwik',
+      author='AdminHead',
+      author_email='code@adminhead.tech',
+      license='MIT',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+      install_requires=[
+          'requests'
+      ],
+      extras_require={
+          'dev': ['pytest']
+      },
+      python_requires='>=3',
+      include_package_data=True,
+      zip_safe=False)
