bc8f6507c4881d52281321ede6f9384e0b2f0355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:57 +0000

    Add package ttkwidgets/0.8.0/ttkwidgets-0.8.0.tar.gz

---
+++ packages/ttkwidgets/ttkwidgets-0.8.0.tar.gz/ttkwidgets-0.8.0/ttkwidgets/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) The ttkwidgets authors 2017
+# Available under the license found in LICENSE
+from ttkwidgets.calendarwidget import Calendar
+from ttkwidgets.autohidescrollbar import AutoHideScrollbar
+from ttkwidgets.linklabel import LinkLabel
+from ttkwidgets.scrolledlistbox import ScrolledListbox
+from ttkwidgets.debugwindow import DebugWindow
+from ttkwidgets.checkboxtreeview import CheckboxTreeview
+from ttkwidgets.itemscanvas import ItemsCanvas
+from ttkwidgets.tickscale import TickScale
