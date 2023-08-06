e05284a0f07c10b3e6dfb144cf5880b233f17b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:52 +0000

    Add package serafin/0.9.1/serafin-0.9.1.tar.gz

---
+++ packages/serafin/serafin-0.9.1.tar.gz/serafin-0.9.1/src/serafin/__init__.py
@@ -0,0 +1,28 @@
+"""
+
+**serafin** is a serialization system that allows flexible serialization
+of any type of object according to a provided fieldspec. The fieldspec tells
+the serialize which attribute/fields/members of the given object should be
+serialized. This allows for a very flexible serialization system, especially in
+the context of API endpoints where we can write one endpoint and allow client
+to pass the fieldspec describing how he wants the output to be formatted.
+
+
+.. autoclass:: serafin.core.Priority
+    :members:
+
+
+.. autoclass:: serafin.core.Serializer
+    :members:
+
+
+.. autofunction:: serafin.core.serialize
+
+"""
+from .core import serialize
+from .serializers import *     # pylint: disable=wildcard-import
+from .fieldspec import Fieldspec
+__all__ = [
+    'serialize',
+    'Fieldspec',
+]
