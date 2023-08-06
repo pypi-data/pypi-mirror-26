5b266a5e5be1877d7bb442babfb6affdc91bd7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:08 +0000

    Add package drf-sideloading/0.1.7/drf_sideloading-0.1.7-py2.py3-none-any.whl

---
+++ packages/drf-sideloading/drf_sideloading-0.1.7-py2.py3-none-any.whl/drf_sideloading/mixins.py
@@ -0,0 +1,92 @@
+from rest_framework.response import Response
+
+from .serializers import SideLoadableSerializer
+
+
+class SideloadableRelationsMixin(object):
+    query_param_name = 'sideload'
+    default_primary_object_name = 'self'
+
+    relation_names = []
+
+    def __init__(self, **kwargs):
+        if not hasattr(self, 'sideloadable_relations'):
+            raise Exception("define `sideloadable_relations` class variable, while using `SideloadableRelationsMixin`")
+        self.primary_object_name = self.get_primary_relation_name()
+
+    def get_primary_relation_name(self):
+        """Determine name of the base(primary) relation"""
+        for relation_name, properties in self.sideloadable_relations.items():
+            if isinstance(properties, dict):
+                for name, value in properties.items():
+                    if name == 'primary' and value:
+                        if not properties.get('serializer'):
+                            raise Exception("if {} relation is primary then, it is required to  "
+                                            " specify serializer class by key "
+                                            " 'primary': True, 'serializer': ...  ".format(relation_name))
+                        return relation_name
+        raise Exception("It is required to define primary model {'primary': True, 'serializer': SerializerClass}")
+
+    def list(self, request, *args, **kwargs):
+        sideload = request.query_params.get(self.get_param_name(), None)
+        if not sideload:
+            # do nothing if there is no or empty parameter provided
+            return super(SideloadableRelationsMixin, self).list(request, *args, **kwargs)
+
+        self.parse_query_param(sideload)
+
+        if not self.relation_names:
+            # do nothing if there is intersection between provided parameters and defined `sideloadable_relations`
+            return super(SideloadableRelationsMixin, self).list(request, *args, **kwargs)
+
+        # After this `relation_names` is safe to use
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            sideloadable_page = self.get_sideloadable_page(page)
+
+            serializer = self.get_serializer(sideloadable_page)
+            return self.get_paginated_response(serializer.data)
+
+        sideloadable_page = self.get_sideloadable_page(queryset)
+
+        serializer = self.get_serializer(sideloadable_page)
+        return Response(serializer.data)
+
+    def get_param_name(self):
+        return self.query_param_name
+
+    def parse_query_param(self, sideload_relations):
+        """ Parse query param and take validated names
+
+        :param sideload_relations string
+        :return valid relation names list
+
+        comma separated relation names may contain invalid or unusable characters.
+        This function finds string match between requested names and defined relation in view
+
+        """
+        relation_names = sideload_relations.split(',')
+        self.relation_names = \
+            (set(relation_names) & set(self.sideloadable_relations.keys())) - set([self.primary_object_name])
+        return relation_names
+
+    def get_sideloadable_page(self, page):
+        sideloadable_page = {self.primary_object_name: page}
+        for rel in self.relation_names:
+            single_relation_set = set()
+            for row in page:
+                if hasattr(row, rel):
+                    if getattr(row, rel).__class__.__name__ == 'ManyRelatedManager':
+                        single_relation_set = single_relation_set | set(getattr(row, rel).all())
+                    else:
+                        single_relation_set.add(getattr(row, rel))
+            sideloadable_page[rel] = single_relation_set
+        return sideloadable_page
+
+    def get_serializer_class(self):
+        if self.relation_names and self.action == 'list':
+            return SideLoadableSerializer
+
+        return super(SideloadableRelationsMixin, self).get_serializer_class()
