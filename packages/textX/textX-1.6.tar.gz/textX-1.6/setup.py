3c99c919e2196162be14fd83394cdf898dc7faf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:53 +0000

    Add package textX/1.6/textX-1.6.tar.gz

---
+++ packages/textX/textX-1.6.tar.gz/textX-1.6/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import codecs
+from setuptools import setup
+
+NAME = 'textX'
+DESC = 'Meta-language for DSL implementation inspired by Xtext'
+VERSION = "1.6"
+AUTHOR = 'Igor R. Dejanovic'
+AUTHOR_EMAIL = 'igor.dejanovic@gmail.com'
+LICENSE = 'MIT'
+URL = 'https://github.com/igordejanovic/textX'
+DOWNLOAD_URL = 'https://github.com/igordejanovic/textX/archive/v%s.tar.gz' \
+    % VERSION
+README = codecs.open(os.path.join(os.path.dirname(__file__), 'README.rst'),
+                     'r', encoding='utf-8').read()
+
+
+if sys.argv[-1].startswith('publish'):
+    if os.system("pip list | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    if os.system("pip list | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    if sys.argv[-1] == 'publishtest':
+        os.system("twine upload -r test dist/*")
+    else:
+        os.system("twine upload dist/*")
+        print("You probably want to also tag the version now:")
+        print("  git tag -a {0} -m 'version {0}'".format(VERSION))
+        print("  git push --tags")
+    sys.exit()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESC,
+    long_description=README,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=AUTHOR,
+    maintainer_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    packages=["textx", "textx.commands"],
+    install_requires=["Arpeggio"],
+    keywords="parser meta-language meta-model language DSL",
+    entry_points={
+        'console_scripts': [
+            'textx = textx.commands.console:textx'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Interpreters',
+        'Topic :: Software Development :: Compilers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        ]
+
+)
