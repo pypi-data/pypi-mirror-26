9ff9c735b589763fa02e70b4c309c78aa8127f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:09 +0000

    Add package pokestarfansloggingsetup/1.0.0.1/pokestarfansloggingsetup-1.0.0.1-py3.6.egg

---
+++ packages/pokestarfansloggingsetup/pokestarfansloggingsetup-1.0.0.1-py3.6.egg/Setup/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import datetime
+import sys
+
+def setup_logger(name, loglevel=logging.INFO):
+	file_handler = logging.FileHandler(filename='logs/{}_{}.log'.format(name, datetime.now().strftime('%m_%d_%y')))
+	stdout_handler = logging.StreamHandler(sys.stdout)
+	handlers = [file_handler, stdout_handler]
+
+	logging.basicConfig(
+		level=loglevel,
+		format='[%(asctime)s] {%(filename)s:%(lineno)d} (%(funcName)s) %(levelname)s - %(message)s',
+		handlers=handlers
+	)
+
+	logger = logging.getLogger(__name__)
+	return logger
