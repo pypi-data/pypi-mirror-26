197a7ded7712ebc3846f475d7f1599419a243862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:13 +0000

    Add package blesonwin/0.0.9/blesonwin-0.0.9.tar.gz

---
+++ packages/blesonwin/blesonwin-0.0.9.tar.gz/blesonwin-0.0.9/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, Extension
+
+blesonwin_module = Extension('blesonwin',                              
+                             sources = ['blesonwin.cpp'],
+                             extra_compile_args = ["/std:c++latest"], 
+                             )
+
+setup(name = 'blesonwin', 
+    version = '0.0.9',
+    description = 'Windows 10 WinRT Extension for BluetoothLE',
+    license='MIT',
+    author='TheCellule',
+    author_email='thecellule@gmail.com',
+    url='https://github.com/TheCellule/blesonwin',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5'
+    ],
+    ext_modules = [blesonwin_module],
+)
