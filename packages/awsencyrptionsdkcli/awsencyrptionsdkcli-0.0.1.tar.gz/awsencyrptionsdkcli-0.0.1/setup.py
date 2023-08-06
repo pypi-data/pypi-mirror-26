6b3b4371b283abd3649eed75d91e528f7b63c88a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:39 +0000

    Add package awsencyrptionsdkcli/0.0.1/awsencyrptionsdkcli-0.0.1.tar.gz

---
+++ packages/awsencyrptionsdkcli/awsencyrptionsdkcli-0.0.1.tar.gz/awsencyrptionsdkcli-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import sys
+from setuptools import setup
+
+args = ' '.join(sys.argv).strip()
+if not any(args.endswith(suffix) for suffix in ['setup.py check -r -s', 'setup.py sdist']):
+    raise ImportError('Did you mean to install aws-encryption-sdk-cli?',)
+
+setup(
+    author='Amazon Web Services',
+    author_email='aws-cryptools@amazon.com',
+    classifiers=['Development Status :: 7 - Inactive'],
+    description='Did you mean to install aws-encryption-sdk-cli?',
+    long_description='\nThis package has been parked by Amazon Web Services to protect you against packages\nadopting names that might be common mistakes when looking for ours. You probably\nwanted to install aws-encryption-sdk-cli. For more information, see http://aws-encryption-sdk-cli.readthedocs.io/en/latest/.',
+    name='awsencyrptionsdkcli',
+    url='http://aws-encryption-sdk-cli.readthedocs.io/en/latest/',
+    version='0.0.1'
+)
