b4212031334289991035230de64aa41330b8a3da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:48 +0000

    Add package kv/0.3/kv-0.3.tar.gz

---
+++ packages/kv/kv-0.3.tar.gz/kv-0.3/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+
+summary = "KV provides a dictionary-like interface on top of SQLite."
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    readme_rst = f.read()
+
+
+setup(
+    name='kv',
+    url='https://github.com/mgax/kv',
+    description=summary,
+    long_description=readme_rst,
+    license="BSD License",
+    version='0.3',
+    author='Alex Morega',
+    py_modules=['kv'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Database :: Front-Ends',
+    ],
+    entry_points={
+        'console_scripts': [
+            'kv-cli = kv:main',
+        ],
+    },
+)
