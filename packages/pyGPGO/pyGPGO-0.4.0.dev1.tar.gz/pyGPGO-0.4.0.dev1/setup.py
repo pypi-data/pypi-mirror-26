6d221954fd7bb2945e7f7dca3397b8e8ca5502b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:55 +0000

    Add package pyGPGO/0.4.0.dev1/pyGPGO-0.4.0.dev1.tar.gz

---
+++ packages/pyGPGO/pyGPGO-0.4.0.dev1.tar.gz/pyGPGO-0.4.0.dev1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = {}
+with open('pyGPGO/version.py') as fp:
+    exec(fp.read(), version)
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+    
+setup(name='pyGPGO',
+    version=version['__version__'],
+    description='Bayesian Optimization tools in Python',
+    classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Scientific/Engineering :: Mathematics'
+    ],
+    keywords = ['machine-learning', 'optimization', 'bayesian'],
+    url='https://github.com/hawk31/pyGPGO',
+    author='Jose Jimenez',
+    author_email='jose.jimenez@upf.edu',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'joblib',
+        'scikit-learn',
+        'theano',
+        'pyMC3'
+    ],
+    zip_safe=False)
