f34408507664a40845d5b5e78e10339b7bf764d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:59 +0000

    Add package moxel-python-driver/0.0.1.post56/moxel-python-driver-0.0.1.post56.tar.gz

---
+++ packages/moxel-python-driver/moxel-python-driver-0.0.1.post56.tar.gz/moxel-python-driver-0.0.1.post56/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='moxel-python-driver',
+      version='0.0.1.post56', # to be replaced by sed.
+      description='python driver for moxel models',
+      # url='www.moxel.ai',
+      author='moxel',
+      author_email='support@moxel.ai',
+      license='MIT',
+      zip_safe=False,
+      packages=['moxel_python_driver'],
+      include_package_data=True,
+      package_data={'moxel_python_driver': ['moxel_python_driver/VERSION'] },
+      entry_points={
+          "console_scripts": [
+              "moxel-python-driver = moxel_python_driver.driver:main",
+          ]
+      }
+)
