7d075ff13e9206678cdf1c29abbf3005ae96067f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:18 +0000

    Add package postgpd/0.3/postgpd-0.3.tar.gz

---
+++ packages/postgpd/postgpd-0.3.tar.gz/postgpd-0.3/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='postgpd',
+	version='0.3',
+	description='test3 postgpd upload',
+	author= 'Mateusz Janczak',
+	author_email='mateusz.janczak@op.pl'
+)
