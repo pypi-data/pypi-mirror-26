118ef0bfcd81832475a988d46d9f0c1c78b3bb05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:12 +0000

    Add package ai/0.0.1/ai-0.0.1.tar.gz

---
+++ packages/ai/ai-0.0.1.tar.gz/ai-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ai",
+    author="Decalogue",
+    version="0.0.1",
+    author_email="1044908508@qq.com",
+    description="Ai lib.",
+    license="MIT",
+    url="https://github.com/Decalogue/ai",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Natural Language :: English",
+        "Natural Language :: Chinese (Simplified)",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Other OS",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
+    ]
+)
