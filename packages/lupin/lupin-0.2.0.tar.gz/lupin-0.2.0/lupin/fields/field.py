09e05c71eaee6a8f919048ab90ad5343831dffca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:42 +0000

    Add package lupin/0.2.0/lupin-0.2.0.tar.gz

---
+++ packages/lupin/lupin-0.2.0.tar.gz/lupin-0.2.0/lupin/fields/field.py
@@ -0,0 +1,61 @@
+class Field(object):
+    """Generic field that does not convert the values"""
+
+    def __init__(self, binding=None, default=None, validators=None):
+        """
+        Args:
+            binding (str): attribute name to map on object
+            default (object): default value if data is absent
+            validators (list): list of validators
+        """
+        self.binding = binding
+        self.default = default
+        self._validators = validators or []
+
+    def load(self, value):
+        """Loads python object from JSON value
+
+        Args:
+            value (object): a value
+
+        Returns:
+            object
+        """
+        return value
+
+    def dump(self, value):
+        """Dump value to its JSON representation
+
+        Args:
+            value (object): a value
+
+        Returns:
+            object
+        """
+        return value
+
+    def extract_attr(self, obj, key=None):
+        """Get value of the `key` attribute of object.
+        If field has been provided a `binding` then it will
+        override `key`
+
+        Args:
+            obj (object): object to get value from
+            key (str): attribute name
+
+        Returns:
+            object
+        """
+        key = self.binding or key
+        raw_value = getattr(obj, key)
+        return self.dump(raw_value)
+
+    def validate(self, value, path):
+        """Validate value againt field validators
+
+        Args:
+            value (object): value to validate
+            path (list): JSON path of value
+        """
+        for validator in self._validators:
+            validator(value, path)
