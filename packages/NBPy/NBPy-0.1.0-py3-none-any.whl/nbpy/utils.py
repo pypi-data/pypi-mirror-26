e0d8c2e7f2d6835d570f9f44caf64f3384126211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:38 +0000

    Add package NBPy/0.1.0/NBPy-0.1.0-py3-none-any.whl

---
+++ packages/NBPy/NBPy-0.1.0-py3-none-any.whl/nbpy/utils.py
@@ -0,0 +1,28 @@
+"""Various utilities."""
+
+from datetime import datetime
+from functools import wraps
+from collections import Sequence
+from nbpy.errors import DateFormattingError
+
+
+def validate_date(date):
+    """Check if date is datetime or properly formatted string (YYYY-MM-DD)."""
+    if not isinstance(date, datetime):
+        try:
+            datetime.strptime(date, "%Y-%m-%d")
+        except Exception:
+            raise DateFormattingError(
+                "{} not properly formatted date (YYYY-MM-DD)".format(date)
+            )
+
+
+def first_if_sequence(func):
+    """If func's result is a sequence, return only first element."""
+    @wraps(func)
+    def first(cls, *args, **kwargs):
+        result = func(cls, *args, **kwargs)
+        if isinstance(result, Sequence):
+            return result[0]
+        return result
+    return first
