624747063f5fa9cb01776b04543e9437d7180e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:15 +0000

    Add package django-powerbank/0.2.26/django-powerbank-0.2.26.tar.gz

---
+++ packages/django-powerbank/django-powerbank-0.2.26.tar.gz/django-powerbank-0.2.26/docs/installation.rst
@@ -0,0 +1,12 @@
+============
+Installation
+============
+
+At the command line::
+
+    $ easy_install django-powerbank
+
+Or, if you have virtualenvwrapper installed::
+
+    $ mkvirtualenv django-powerbank
+    $ pip install django-powerbank
---
+++ packages/django-powerbank/django-powerbank-0.2.26.tar.gz/django-powerbank-0.2.26/tests/test_models.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_django-powerbank
+------------
+
+Tests for `django-powerbank` models module.
+"""
+
+from django.test import TestCase
+
+from tests import factories
+
+
+class TestDjango_powerbank(TestCase):
+
+    def test_something(self):
+        factories.UserFactory
