2561f5ed5dc2b5a781946b913d35bde94c421d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:17 +0000

    Add package django-bread/0.4.1/django_bread-0.4.1-py2.py3-none-any.whl

---
+++ packages/django-bread/django_bread-0.4.1-py2.py3-none-any.whl/tests/factories.py
@@ -0,0 +1,23 @@
+import factory
+import factory.fuzzy
+
+from .models import BreadTestModel, BreadTestModel2, BreadLabelValueTestModel
+
+
+class BreadTestModel2Factory(factory.DjangoModelFactory):
+    FACTORY_FOR = BreadTestModel2
+
+    text = factory.fuzzy.FuzzyText(length=10)
+
+
+class BreadTestModelFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = BreadTestModel
+
+    name = factory.fuzzy.FuzzyText(length=10)
+    other = factory.SubFactory(BreadTestModel2Factory)
+
+
+class BreadLabelValueTestModelFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = BreadLabelValueTestModel
+
+    name = factory.fuzzy.FuzzyText(length=10)
