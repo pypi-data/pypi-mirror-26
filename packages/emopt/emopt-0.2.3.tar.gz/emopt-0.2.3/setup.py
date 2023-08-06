b1ab5a206b4686a71603d4be3c63fc566c3f33a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:56 +0000

    Add package emopt/0.2.3/emopt-0.2.3.tar.gz

---
+++ packages/emopt/emopt-0.2.3.tar.gz/emopt-0.2.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools.command.install import install as SetuptoolsInstall
+import subprocess
+
+class MakeInstall(SetuptoolsInstall):
+    def run(self):
+        subprocess.call('make')
+        SetuptoolsInstall.run(self)
+
+setup(name='emopt',
+      version='0.2.3',
+      description='A suite of tools for optimizing the shape and topology of ' \
+      'electromagnetic structures.',
+      url='https://github.com/anstmichaels/emopt',
+      author='Andrew Michaels',
+      author_email='amichaels@berkeley.edu',
+      license='Apache 2.0',
+      packages=['emopt'],
+      package_data={'emopt':['*.so', 'data', 'data/*']},
+      cmdclass={'install':MakeInstall},
+      zip_safe=False)
