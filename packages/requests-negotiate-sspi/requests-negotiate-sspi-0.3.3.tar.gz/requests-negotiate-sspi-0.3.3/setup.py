28d8b33a3b33ce51bf931192bf7339e6572d3487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:02 +0000

    Add package requests-negotiate-sspi/0.3.3/requests-negotiate-sspi-0.3.3.tar.gz

---
+++ packages/requests-negotiate-sspi/requests-negotiate-sspi-0.3.3.tar.gz/requests-negotiate-sspi-0.3.3/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+from os import chdir
+from os.path import abspath, dirname
+
+from setuptools import setup, find_packages
+
+chdir(dirname(abspath(__file__)))
+
+version = {}
+
+with open("requests_negotiate_sspi/version.py") as fp:
+    exec(fp.read(), version)
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE.txt') as f:
+    license = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='requests-negotiate-sspi',
+    version=version['__version__'],
+    packages=find_packages(exclude=('docs')),
+    install_requires=requirements,
+    provides=['requests_negotiate_sspi'],
+    author='Brandon Davidson',
+    url='https://github.com/brandond/requests-negotiate-sspi',
+    download_url='https://github.com/brandond/requests-negotiate-sspi/tarball/{}'.format(version['__version__']),
+    description='This package allows for Single-Sign On HTTP Negotiate authentication using the requests library on Windows.',
+    long_description=readme,
+    license=license,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+)
