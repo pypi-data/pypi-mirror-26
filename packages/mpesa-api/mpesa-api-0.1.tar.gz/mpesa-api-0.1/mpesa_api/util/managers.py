7a8a609a4e2e0af9544896a557fc06fbb85b99af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:44 +0000

    Add package mpesa-api/0.1/mpesa-api-0.1.tar.gz

---
+++ packages/mpesa-api/mpesa-api-0.1.tar.gz/mpesa-api-0.1/mpesa_api/util/managers.py
@@ -0,0 +1,33 @@
+import time
+
+from django.conf import settings
+from django.db import models
+
+from mpesa_api.util.mpesautils import get_token
+
+
+class AuthTokenManager(models.Manager):
+    """
+    Auth manager token
+    """
+    def get_token(self):
+        current_time = int(time.time())
+        if self.count() == 0:
+            # means we don't have a record yet so we create
+            token = get_token()
+            access_token = token['access_token']
+            expires = int(time.time()) + int(token['expires_in'])
+            self.create(access_token=access_token, expires_in=expires)
+            return access_token
+        else:
+            # check if it's expired if it update
+            obj = self.get(pk=1)
+            if current_time > (obj.expires_in - settings.TOKEN_THRESHOLD):
+                token = get_token()
+                #import ipdb;ipdb.set_trace()
+                access_token = token['access_token']
+                expires = int(time.time()) + int(token['expires_in'])
+                self.filter(pk=1).update(access_token=access_token, expires_in=expires)
+                return access_token
+            else:
+                return obj.access_token
